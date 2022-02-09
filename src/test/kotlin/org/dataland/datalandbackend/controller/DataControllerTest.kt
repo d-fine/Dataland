3f96225fd533da676fec7dae4f6378927b84e429
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-02-09 14:50:54 +0100

    Added first tests for DataController and InMemoryDataStoreTest

---
+++ src/test/kotlin/org/dataland/datalandbackend/controller/DataControllerTest.kt
@@ -0,0 +1,61 @@
+package org.dataland.datalandbackend.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.dataland.datalandbackend.model.DataSet
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class DataControllerTest {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+    fun uploadDataSet(mockMvc: MockMvc, dataSet: DataSet) {
+        mockMvc.perform(
+            post("/data")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(dataSet))
+        )
+            .andExpectAll(status().isOk, content().contentType("application/json"))
+    }
+
+    @Test
+    fun data_can_be_added() {
+        uploadDataSet(mockMvc, dataSets[0])
+    }
+
+    @Test
+    fun data_can_be_retrieved() {
+        val testSet = dataSets[0]
+        uploadDataSet(mockMvc, testSet)
+
+        mockMvc.perform(
+            get("/data/0")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("\$.name").value(testSet.name))
+            .andExpect(jsonPath("\$.payload").value(testSet.payload))
+    }
+
+    companion object {
+        val dataSets = listOf<DataSet>(
+            DataSet(name = "Company A", payload = "Data"),
+            DataSet(name = "Holding B", payload = "Information"),
+            DataSet(name = "Group C", payload = "Inputs")
+        )
+    }
+}
---
+++ src/test/kotlin/org/dataland/datalandbackend/service/InMemoryDataStoreTest.kt
@@ -0,0 +1,38 @@
+package org.dataland.datalandbackend.service
+
+import org.dataland.datalandbackend.model.DataSet
+import org.dataland.datalandbackend.model.Identifier
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InMemoryDataStoreTest {
+
+    @Test
+    fun check_name_after_adding_new_data() {
+        val testStore = InMemoryDataStore()
+        val identifier = testStore.addDataSet(dataSet = dataSets[0])
+        assertEquals(identifier.name, dataSets[0].name)
+    }
+
+    @Test
+    fun check_id_after_adding_multiple_new_data() {
+        val testStore = InMemoryDataStore()
+        var identifier = Identifier(name = "dummy", id = "0")
+        for (dataSet in dataSets) {
+            identifier = testStore.addDataSet(dataSet = dataSet)
+        }
+        assertEquals(identifier.id, (dataSets.size - 1).toString())
+    }
+
+
+    companion object {
+        val dataSets = listOf<DataSet>(
+            DataSet(name = "Company A", payload = "Data"),
+            DataSet(name = "Holding B", payload = "Information"),
+            DataSet(name = "Group C", payload = "Inputs")
+        )
+    }
+
+}
