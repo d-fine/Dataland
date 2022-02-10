b4b6ff06e9b2ee1c07d779b0900ae03c99bfa03d
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-10 09:54:49 +0100

    provoke a finding in sonarqube

--- src/main/kotlin/org/dataland/datalandbackend/model/DataSet.kt
+++ src/main/kotlin/org/dataland/datalandbackend/model/DataSet.kt
@@ -5,4 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class DataSet(
     @field:JsonProperty("name", required = true) val name: String,
     @field:JsonProperty("payload", required = true) val payload: String
+
+    fun doNothing() {
+        println("Doing nothing with name $name")
+    }
 )
