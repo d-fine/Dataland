cb8c984b755b631ec3d45c6fb9d96e4b6a68934b
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-10 10:30:54 +0100

    clean up code

--- src/main/kotlin/org/dataland/datalandbackend/model/DataSet.kt
+++ src/main/kotlin/org/dataland/datalandbackend/model/DataSet.kt
@@ -5,11 +5,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class DataSet(
     @field:JsonProperty("name", required = true) val name: String,
     @field:JsonProperty("payload", required = true) val payload: String
-) {
-    fun doNothing() {
-        println("Doing nothing with name $name")
-        var a = 5
-        a =+ 7
-        println("$a")
-    }
-}
+)
