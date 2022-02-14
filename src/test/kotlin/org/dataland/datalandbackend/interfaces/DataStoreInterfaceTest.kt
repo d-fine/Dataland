5c73abcab55fb753213c1e7cbb58ff05d8d47b83
Stephan Sinn <stephan.sinn@d-fine.de>
2022-02-14 12:01:16 +0100

    full coverage of backend interfaces

--- src/test/kotlin/org/dataland/datalandbackend/interfaces/DataStoreInterfaceTest.kt
+++ src/test/kotlin/org/dataland/datalandbackend/interfaces/DataStoreInterfaceTest.kt
@@ -1,10 +1,39 @@
 package org.dataland.datalandbackend.interfaces
 
+import org.dataland.datalandbackend.model.DataSet
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
-internal class DataStoreInterfaceTest {
+internal class DataStoreInterfaceTest : DataStoreInterface {
+
     @Test
     fun addDataSetNotImplemented() {
-
+        assertThrows<NotImplementedError> {
+            addDataSet(dataSets[0])
+        }
     }
+
+    @Test
+    fun listDataSetsNotImplemented() {
+        assertThrows<NotImplementedError> {
+            listDataSets()
+        }
+    }
+
+    @Test
+    fun getDataSet() {
+        assertThrows<NotImplementedError> {
+            getDataSet(dataSets[0].name)
+        }
+    }
+
+
+    companion object {
+        val dataSets = listOf<DataSet>(
+            DataSet(name = "Company A", payload = "Data"),
+            DataSet(name = "Holding B", payload = "Information"),
+            DataSet(name = "Group C", payload = "Inputs")
+        )
+    }
+
 }
