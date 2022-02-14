9023fed4a63b1f46237d4a7209057c09d86cb81d
Stephan Sinn <stephan.sinn@d-fine.de>
2022-02-14 11:31:37 +0100

    tests build, run and pass

---
+++ src/test/kotlin/org/dataland/datalandbackend/interfaces/DataStoreInterfaceTest.kt
@@ -0,0 +1,10 @@
+package org.dataland.datalandbackend.interfaces
+
+import org.junit.jupiter.api.Test
+
+internal class DataStoreInterfaceTest {
+    @Test
+    fun addDataSetNotImplemented() {
+
+    }
+}
