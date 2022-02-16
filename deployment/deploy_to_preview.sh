ee7643b560c4aea6ffa892fde42104de00377ae4
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-16 11:07:04 +0100

    use ssh-add to add private key

--- deployment/deploy_to_preview.sh
+++ deployment/deploy_to_preview.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ~/.ssh/
 echo "3.71.162.94 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNGocXXehCSfKoYwGdaYUpjvNm7gZE2LS7Nl/gGGXSxqwbGT+X6b+q7AGwhwZpFY9u17wv4NY3EOCK1cGaeot4k=" >  ~/.ssh/known_hosts
-echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
+ssh-add - <<< "$SSH_PRIVATE_KEY"
 chmod 600 ~/.ssh/id_rsa
 
 ssh ubuntu@3.71.162.94 ls
