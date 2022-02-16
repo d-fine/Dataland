cd684fef4aaf0fab16ed10e088db9ce006f54706
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-16 13:36:59 +0100

    run after test

--- deployment/deploy_to_preview.sh
+++ deployment/deploy_to_preview.sh
@@ -4,5 +4,7 @@ echo "3.71.162.94 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdH
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-ssh ubuntu@3.71.162.94 ls
-
+ssh ubuntu@3.71.162.94 ls -lisa
+scp "$1" ubuntu@3.71.162.94:/home/ubuntu/jar/dala-backend.jar
+ssh ubuntu@3.71.162.94 docker kill $(docker ps -q) && docker rm $(docker ps -a -q)
+ssh ubuntu@3.71.162.94 docker -d --restart always --name backend -v /home/ubuntu/jar:/jar -p80:8080 eclipse-temurin:17.0.2_8-jre-alpine java -jar /jar/dala-backend.jar
