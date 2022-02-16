45e4f5fb8f9f02f7c7bba2f6b3d5d8fc23a91ec4
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-16 14:23:59 +0100

    copy actual jar file, make docker commands more robust

--- deployment/deploy_to_preview.sh
+++ deployment/deploy_to_preview.sh
@@ -6,5 +6,5 @@ chmod 600 ~/.ssh/id_rsa
 
 ssh ubuntu@3.71.162.94 ls -lisa
 scp "$1" ubuntu@3.71.162.94:/home/ubuntu/jar/dala-backend.jar
-ssh ubuntu@3.71.162.94 docker kill $(docker ps -q) && docker rm $(docker ps -a -q)
+ssh ubuntu@3.71.162.94 docker kill $(docker ps -q) || docker rm $(docker ps -a -q)
 ssh ubuntu@3.71.162.94 docker -d --restart always --name backend -v /home/ubuntu/jar:/jar -p80:8080 eclipse-temurin:17.0.2_8-jre-alpine java -jar /jar/dala-backend.jar
