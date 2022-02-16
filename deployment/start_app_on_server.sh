e9b790dfdd4e332dff7d1b9543b35ea232050557
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-16 14:46:47 +0100

    fixes

--- deployment/start_app_on_server.sh
+++ deployment/start_app_on_server.sh
@@ -2,4 +2,4 @@
 
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
-docker -d --restart always --name backend -v /home/ubuntu/jar:/jar -p80:8080 eclipse-temurin:17.0.2_8-jre-alpine java -jar /jar/dala-backend.jar
+docker run -d --restart always --name dala-backend -v /home/ubuntu/jar:/jar -p80:8080 eclipse-temurin:17.0.2_8-jre-alpine java -jar /jar/dala-backend.jar
