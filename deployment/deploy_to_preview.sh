6102c0cdfd801f340bc48e64e8590e6f147f2a17
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-16 11:21:22 +0100

    test ci

--- deployment/deploy_to_preview.sh
+++ deployment/deploy_to_preview.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p ~/.ssh/
 echo "3.71.162.94 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNGocXXehCSfKoYwGdaYUpjvNm7gZE2LS7Nl/gGGXSxqwbGT+X6b+q7AGwhwZpFY9u17wv4NY3EOCK1cGaeot4k=" >  ~/.ssh/known_hosts
+echo "this is not masked"
 echo "$SSH_PRIVATE_KEY"
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa
