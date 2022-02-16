1bdaed9ffb7d977cde3800e7f1427e06da7bf075
ppuetsch <47553592+ppuetsch@users.noreply.github.com>
2022-02-16 16:40:19 +0100

    Dala 1 deploy to preview (#5)
    * add first attempt to connect to preview server
    
    * add .ssh folder
    
    * add preview host key to deployment
    
    * add public key
    
    * fix filename
    
    * protect key file
    
    * add a tag
    
    * fixes workflow yaml syntax
    
    * use ssh-add to add private key
    
    * make sure ssh file is written correctly by using quotation marks
    
    * line endings?
    
    * line endings?
    
    * test ci
    
    * remove test during dev
    
    * bugfix
    
    * manual trigger
    
    * test manual workflow
    
    * Update deploy.yaml
    
    * manual workflow
    
    * manual workflow
    
    * download artifact
    
    * exclude plain jar
    
    * exclude plain jar
    
    * indentation
    
    * run after test
    
    * run after test
    
    * run after test
    
    * run after small test
    
    * test
    
    * developing ci is always fun
    
    * copy actual jar file,
    make docker commands more robust
    
    * use dedicated start script
    
    * fixes
    
    * fixes
    
    * only run manually
    
    * fixes
    
    * start server and wait for it
    
    * fixes
    
    * run only manually
    
    Co-authored-by: Philip Puetsch <philip.puetsch@d-fine.de>

---
+++ deployment/deploy_to_preview.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ~/.ssh/
+echo "3.71.162.94 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNGocXXehCSfKoYwGdaYUpjvNm7gZE2LS7Nl/gGGXSxqwbGT+X6b+q7AGwhwZpFY9u17wv4NY3EOCK1cGaeot4k=" >  ~/.ssh/known_hosts
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+timeout 300 bash -c "while ! ssh -o ConnectTimeout=3 ubuntu@3.71.162.94 exit; do echo 'target server not yet there - retrying in 1s'; sleep 1; done" || exit
+
+scp ./deployment/start_app_on_server.sh ubuntu@3.71.162.94:/home/ubuntu/start_app_on_server.sh
+scp "$1" ubuntu@3.71.162.94:/home/ubuntu/jar/dala-backend.jar
+ssh ubuntu@3.71.162.94 "chmod +x /home/ubuntu/start_app_on_server.sh && sudo /home/ubuntu/start_app_on_server.sh"
