f964714fc5d0aa546bdccd6ce9b14d47215c7f21
Stephan Sinn <stephan.sinn@d-fine.de>
2022-02-18 17:27:45 +0100

    coverage with reports

--- dataland-frontend/jest.config.js
+++ dataland-frontend/jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  coverageDirectory: "reports",
+  coverageReporters: ["clover", "json", "lcov", ["text", {"skipFull": true}]]
 }
