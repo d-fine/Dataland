78e44259e1e417b56c58ad8096a27e720d2b9878
Stephan Sinn <stephan.sinn@d-fine.de>
2022-02-21 15:52:13 +0100

    jest better coverage

--- dataland-frontend/jest.config.js
+++ dataland-frontend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   collectCoverage: true,
+  collectCoverageFrom: [`src/**/*.{js,vue}`],
   coverageDirectory: "coverage/unit",
   coverageReporters: ["clover", "json", "lcov", ["text", {"skipFull": true}]]
 }
