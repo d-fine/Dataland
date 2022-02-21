cdab52a8dc35cc447b38efd7364a989adeef7b10
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-02-21 11:50:12 +0100

    Changes according to the comments by reviewer ppuetsch.

--- testing-shell-scripts/wait_for_backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#start compiling Kotlin
-./gradlew compileKotlin
-#Kotlin compiled, now checking if backend responds
-timeout 240 bash -c "while ! curl http://backend:8080/actuator/health; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; echo 'backend server responded'" || exit
-#backend responded, now starting end-to-end-tests
-./gradlew :dataland-e2etests:test
