d30ae75e67adb72b89abb4ab1c1ade04ac77f5de
Stephan Sinn <stephan.sinn@d-fine.de>
2022-02-21 11:24:35 +0100

    coverage by babel istanbul

--- dataland-frontend/babel.config.js
+++ dataland-frontend/babel.config.js
@@ -1,5 +1,17 @@
+const plugins = []
+if (process.env.NODE_ENV === 'test') {
+  plugins.push([
+    "babel-plugin-istanbul", {
+      // specify some options for NYC instrumentation here
+      // like tell it to instrument both JavaScript and Vue files
+      extension: ['.js', '.vue'],
+    }
+  ])
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  plugins
 }
