8f3e6439b8cf2c266e8dc5b28b5b3f1e4225ee70
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-23 11:17:34 +0100

    fixes include patterns

--- dataland-frontend/build.gradle.kts
+++ dataland-frontend/build.gradle.kts
@@ -9,7 +9,7 @@ node {
 }
 
 val sources = fileTree(projectDir)
-sources.include("src", "public", "tests")
+sources.include("src/**", "public/**", "tests/**")
 val sonarSources by extra(sources.files.map { it })
 val jacocoSources by extra(emptyList<File>())
 val jacocoClasses by extra(emptyList<File>())
