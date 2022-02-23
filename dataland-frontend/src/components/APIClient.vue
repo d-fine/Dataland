a67516fe11f1e962a145d4bac89d9aa8e1a76418
Stephan Sinn <stephan.sinn@d-fine.de>
2022-02-23 22:01:12 +0100

    displaying the data from backend

--- dataland-frontend/src/components/APIClient.vue
+++ dataland-frontend/src/components/APIClient.vue
@@ -1,23 +1,51 @@
 <template>
   <div id="app" class="container">
-    <div class="card">
-      <div class="card-header">Vue Axios GET - BezKoder.com</div>
-      <div class="card-body">
-        <div class="input-group input-group-sm">
-          <button class="btn btn-sm btn-primary" @click="getAllData">Get All Data</button>
+    <div class="row">
+      <div class="col s12 m12">
 
-          <button class="btn btn-sm btn-warning ml-2" @click="clearGetOutput">Clear</button>
+        <div class="card">
+          <div class="card-title">ESG Data Search</div>
+          <div class="card-content ">
+
+            <div class="row">
+              <div class="col m6">
+                <button class="btn btn-sm btn-warning ml-2" @click="clearGetOutput">Clear</button>
+              </div>
+              <div class="col m6">
+                <button class="btn btn-sm btn-primary" @click="getAllData">Get All Data</button>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12 m6">
+
+                <input type="text" v-model="get_id" class="autocomplete" placeholder="Search by ID"/>
+                <button class="btn btn-sm btn-primary" @click="getDataById">Get by Id</button>
+              </div>
+              <div class="input-field col s12 m6 ">
+                <input type="text" v-model="get_name" class="autocomplete" placeholder="Search by Name"/>
+                <button class="btn btn-sm btn-primary pulse" @click="getDataById">Get by Name</button>
+              </div>
+            </div>
+
+
+            <div v-if="getResult" class="alert alert-secondary mt-2" role="alert">
+              <pre>{{ getResult }}</pre>
+              <p> {{ getResult.data }} </p>
+              <p> Name: {{ getResult.data[0].name }} </p>
+              <p> ID: {{ getResult.data[0].id }} </p>
+              <p> {{ getResult["data"] }} </p>
+              <p> {{ getResult.status }} </p>
+            </div>
+            <div v-if="getResultByID || getResultByName" class="alert alert-secondary mt-2" role="alert">
+              <pre>{{ getResultByID }}</pre>
+              <p> Status: {{ getResultByID.status }} </p>
+              <p> Data: {{ getResultByID.status }} </p>
+              <p> Name: {{ getResultByID.status }} </p>
+              <p> Payload: {{ getResultByID.status }} </p>
+
+            </div>
+          </div>
         </div>
-
-        <div v-if="getResult" class="alert alert-secondary mt-2" role="alert">
-          <pre>{{getResult}}</pre>
-        <p> {{getResult.data}} </p>
-        <p> Name: {{getResult.data[0].name}} </p>
-        <p> ID: {{getResult.data[0].id}} </p>
-        <p> {{getResult["data"]}} </p>
-        <p> {{getResult.status}} </p>
-        </div>
-
       </div>
     </div>
   </div>
@@ -25,26 +53,53 @@
 <script>
 
 import http from "./http-common";
+
 export default {
   name: "APIClient",
   data() {
     return {
       getResult: null,
+      getResultByID: null,
+      getResultByName: null
     }
   },
   methods: {
 
     async getAllData() {
+      this.getResultByID = null
       try {
         const res = await http.get("/data");
-        const result = {
+        this.getResult = {
           status: res.status + "-" + res.statusText,
           headers: res.headers,
           data: res.data,
         };
-        this.getResult = result;
       } catch (err) {
-        this.getResult = this.fortmatResponse(err.response?.data) || err;
+        this.getResult = err.response?.data || err;
+      }
+    },
+    async getDataById() {
+      this.getResult = null
+
+      let id = this.get_id;
+      if (id) {
+        try {
+          const res = await http.get(`/data/${id}`);
+          // const result = {
+          //   data: res.data,
+          //   status: res.status,
+          //   statusText: res.statusText,
+          //   headers: res.headers,
+          //   config: res.config,
+          // };
+          this.getResultByID = {
+            status: res.status + "-" + res.statusText,
+            headers: res.headers,
+            data: res.data,
+          };
+        } catch (err) {
+          this.getResultByID = err.response?.data || err;
+        }
       }
     },
     clearGetOutput() {
