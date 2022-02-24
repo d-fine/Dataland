e79a7cc5931b5b272db0cc1e6184f855eed04937
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-24 10:00:24 +0100

    actually make the test fail if it can't access the backend

--- testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
+++ testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
@@ -3,8 +3,8 @@
 # needs to be up and responding is used efficiently. Then it continuously validates,
 # whether the backend-container is running. Only then it executes the end-to-end tests.
 
-set -x
+set -ex
 
 ./gradlew compileKotlin
-timeout 240 sh -c "while ! wget http://backend:8080/actuator/health; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'backend server responded'" || exit
+timeout 240 sh -c "while ! wget http://backend:8080/actuator/health; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'backend server responded'"
 ./gradlew :dataland-e2etests:test --no-daemon
