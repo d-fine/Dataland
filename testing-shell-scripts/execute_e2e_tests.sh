16a130f05392bea4aadbba73275d4a38871b4e51
Philip Puetsch <philip.puetsch@d-fine.de>
2022-02-24 10:08:37 +0100

    reduce timeout for stopping backend

--- testing-shell-scripts/execute_e2e_tests.sh
+++ testing-shell-scripts/execute_e2e_tests.sh
@@ -7,7 +7,7 @@ timeout 300 sh -c "docker logs dala-e2e-test_e2etests_1 --follow"
 
 # Stop Backend causing JaCoCo to write Coverage Report, get it to pwd
 docker stop dala-e2e-test_backend_1
-timeout 300 sh -c "docker logs dala-e2e-test_backend_1 --follow"
+timeout 90 sh -c "docker logs dala-e2e-test_backend_1 --follow"
 docker cp dala-e2e-test_backend_1:/app/dataland-backend/build/jacoco/bootRun.exec .
 
 # Check execution success of Test Container
