28162a6a9077594de310cf1a3b9f56a82cdf397a
harjmandi <hamid.arjmandi@d-fine.de>
2022-02-24 16:55:42 +0100

    Frontend: Add test for APIClient.vue

--- dataland-frontend/tests/unit/ComponentsTest.spec.js
+++ dataland-frontend/tests/unit/ComponentsTest.spec.js
@@ -2,6 +2,7 @@ import {shallowMount} from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 import APIClient from '@/components/APIClient.vue'
 import ESG from '@/components/ESG.vue'
+import {DataStore} from "@/service/DataStore";
 
 describe('HelloWorld.vue', () => {
     it('renders props.msg when passed', () => {
@@ -12,6 +13,7 @@ describe('HelloWorld.vue', () => {
         expect(wrapper.text()).toMatch(msg)
     })
 })
+
 describe('ESG.vue', () => {
     it('checks if there exist button in the component', () => {
         const wrapper = shallowMount(ESG)
@@ -23,5 +25,23 @@ describe('APIClient.vue', () => {
 
     it('checks if APIClient.data() and all its dataStore not undefined', () => {
         expect(typeof APIClient.data()).toBeDefined()
+    })
+
+    it('checks fields', () => {
+        const wrapper = shallowMount(APIClient, {
+            data() {
+                return {
+                    dataStore: new DataStore("http://localhost:8080"),
+                    get_id: "dummy_id",
+                    get_name: "dummy_name",
+                    getResult: null,
+                    getResultByID: null,
+                    getResultByName: null
+                }
+            }
+        })
+        expect(wrapper.vm.get_id).toMatch("dummy_id")
+        expect(wrapper.vm.get_name).toMatch("dummy_name")
+
     })
 })
