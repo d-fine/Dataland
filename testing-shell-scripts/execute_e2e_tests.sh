21dc594c9eef11543b486ef39fbb87d0fa491ee9
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-01 17:17:05 +0100

    Increase timeout time to 420

--- testing-shell-scripts/execute_e2e_tests.sh
+++ testing-shell-scripts/execute_e2e_tests.sh
@@ -3,7 +3,7 @@ set -ex
 
 #Start E2E Test and wait for E2E Test completion
 docker-compose --project-name dala-e2e-test up -d
-timeout 300 sh -c "docker logs dala-e2e-test_e2etests_1 --follow"
+timeout 420 sh -c "docker logs dala-e2e-test_e2etests_1 --follow"
 
 # Stop Backend causing JaCoCo to write Coverage Report, get it to pwd
 docker stop dala-e2e-test_backend_1
