175bd0ae99cee0b3936871372be368c4e09f0685
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-01 17:25:02 +0100

    pre structuring

--- dataland-frontend/tests/e2e/specs/pages/index.js
+++ dataland-frontend/tests/e2e/specs/pages/index.js
@@ -1,12 +1,3 @@
-// https://docs.cypress.io/api/table-of-contents
-
-describe('Root Setup', () => {
-  it('Visits the app root url and check H1', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to DataLand')
-  })
-})
-
 /*
 TODO: Stephan
 exists:
