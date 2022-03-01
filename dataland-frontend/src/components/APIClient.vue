a412b7c135df60676d18c5a64ee158bab48c781d
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-01 10:43:58 +0100

    well working frontend

--- dataland-frontend/src/components/APIClient.vue
+++ dataland-frontend/src/components/APIClient.vue
@@ -3,26 +3,26 @@
     <div class="row">
       <div class="col s12 m12">
         <div class="card">
-          <div class="card-title"><h3>ESG Data Search</h3>
+          <div class="card-title"><h2>Contact Data Search</h2>
           </div>
           <div class="card-content ">
-            <div class="row">
-              <div class="col m6">
-              </div>
-              <div class="col m6">
-                <button class="btn btn-sm" @click="clearGetOutput">Clear</button>
-              </div>
-            </div>
 
             <div class="row">
-              <div class="input-field col s12 m6">
-                <input type="text" v-model="data.code" class="autocomplete" id="searchByIdCode"/>
-                <label for="searchByIdCode">Please insert a 3 Letter country code</label>
+                <div class="input-field col s12 m6">
+                  <input type="text" v-model="data.code" class="autocomplete" id="countryCode" @keyup.enter="getSkyminderByName"/>
+                  <label for="countryCode">Please insert a 3 Letter country code</label>
+                </div>
+                <div class="input-field col s12 m6">
+                  <input type="text" v-model="data.name" class="autocomplete" id="companyName" @keyup.enter="getSkyminderByName"/>
+                  <label for="companyName">Please insert the name of the company</label>
+                </div>
+            </div>
+            <div class="row">
+              <div class="col m6">
+                  <button class="btn btn-sm" @click="clearGetOutput">Clear</button>
               </div>
-              <div class="input-field col s12 m6">
-                <input type="text" v-model="data.name" class="autocomplete" id="searchByNameInput"/>
-                <label for="searchByNameInput">Please insert the name of the company</label>
-                <button class="btn btn-sm" @click="getSkyminderByName">Get Skyminder by Name</button>
+              <div class="col m6">
+                  <button class="btn btn-sm" @click="getSkyminderByName">Get Skyminder by Name</button>
               </div>
             </div>
           </div>
@@ -46,7 +46,7 @@ export default {
   },
   data() {
     return {
-      dataStore: new DataStore("http://localhost:8080/"),
+      dataStore: new DataStore("http://localhost/api"),
       data: new Data()
     }
   },
