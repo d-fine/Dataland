1307a98093a21a93410bdf5f9fd77eedeb356bd3
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-02 16:05:50 +0100

    tests are running against dataland-skyminder API

--- dataland-frontend/build.gradle.kts
+++ dataland-frontend/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.gradle.node.npm.task.NpmTask
 plugins {
     id("com.github.node-gradle.node") version "3.2.1"
 }
@@ -13,11 +12,3 @@ sources.include("src/**", "public/**", "tests/**")
 val sonarSources by extra(sources.files.map { it })
 val jacocoSources by extra(emptyList<File>())
 val jacocoClasses by extra(emptyList<File>())
-
-tasks.register<NpmTask>("testNpm") {
-    dependsOn(tasks.npmInstall)
-    npmCommand.set(listOf("run", "testunit"))
-    args.set(listOf("--", "--out-dir", "$buildDir/npm-output"))
-    inputs.dir("src")
-    outputs.dir("$buildDir/npm-output")
-}
--- dataland-frontend/tests/e2e/specs/getSkymindersDataTest.js
+++ dataland-frontend/tests/e2e/specs/getSkymindersDataTest.js
@@ -9,26 +9,23 @@ describe('User interactive tests', () => {
         cy.get('input[id="companyName"]').type("BMW", {force: true})
         cy.get('button').contains('Get Skyminder by Name').click()
         cy.get('table').should('exist')
+        cy.get('table').contains('td', 'Teststr. 1 123456 Teststadt')
     })
 
     it('BMW address should be available upon request by pressing enter for countryCode input', () => {
         cy.get('input[id="companyName"]').type("BMW", {force: true})
         cy.get('input[id="countryCode"]').type("DEU{enter}", {force: true})
         cy.get('table').should('exist')
+        cy.get('table').contains('td', 'Teststr. 1 123456 Teststadt')
     })
 
     it('BMW address should be available upon request by pressing enter for companyName input', () => {
         cy.get('input[id="countryCode"]').type("DEU", {force: true})
         cy.get('input[id="companyName"]').type("BMW{enter}", {force: true})
         cy.get('table').should('exist')
+        cy.get('table').contains('td', 'Teststr. 1 123456 Teststadt')
     })
 
-    it('table should not exist in case of invalid input data', () => {
-        cy.get('input[id="countryCode"]').type("DUMMY", {force: true})
-        cy.get('input[id="companyName"]').type("DUMMY", {force: true})
-        cy.get('button').contains('Get Skyminder by Name').click()
-        cy.get('table').should('not.exist')
-    })
 
     it('checks Clear button', () => {
         cy.get('input[id="countryCode"]').type("DEU", {force: true})
