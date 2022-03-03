466e40a6b76bcf1a33657013f6a89fdd7f14bbe2
Philip Puetsch <philip.puetsch@d-fine.de>
2022-03-03 11:32:13 +0100

    removes debug messages

--- testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
+++ testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
@@ -7,5 +7,5 @@ set -ex
 ./gradlew compileKotlin
 # proxy durch localhost ersetzen?
 timeout 240 sh -c "while ! wget http://proxy:80/api/actuator/health; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'backend server responded'"
-#./gradlew :dataland-e2etests:test --no-daemon
-./gradlew :dataland-frontend:npm_run_testproductive --no-daemon --debug --stacktrace --rerun-tasks
+./gradlew :dataland-e2etests:test --no-daemon
+./gradlew :dataland-frontend:npm_run_testproductive --no-daemon
