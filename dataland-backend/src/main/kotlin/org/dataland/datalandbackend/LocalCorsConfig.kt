b4ba021cd02db2072ddcd29132455ef60058608a
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-03 15:39:50 +0100

    gradle ktlintformat applied

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/LocalCorsConfig.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/LocalCorsConfig.kt
@@ -4,11 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
-
-
 
 @Configuration
 @Profile("dev")
--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/DataAPI.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/DataAPI.kt
@@ -7,7 +7,6 @@ import org.dataland.datalandbackend.model.DataSet
 import org.dataland.datalandbackend.model.DataSetMetaInformation
 import org.dataland.skyminderClient.model.ContactInformation
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
