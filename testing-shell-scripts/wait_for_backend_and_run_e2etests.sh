202d2b031acd3998755fd948ad5deec2f9f2754a
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-03 11:55:57 +0100

    nginx.conf add skyminder-dummy-server + adjust waiting shell script

--- testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
+++ testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-#This script begins compiling the Kotlin code of the e2etests-project, so the time that the backend-container
-# needs to be up and responding is used efficiently. Then it continuously validates,
-# whether the backend-container is running. Only then it executes the end-to-end tests.
+# This script begins compiling the Kotlin code of the e2etests-project, so the time that the backend-container
+# and the skyminder-dummy-server-container need to be up and responding is used efficiently.
+# Then it continuously validates, whether the backend-container is running.
+# Then it continuously validates, whether the skyminder-dummy-server-container is running.
+# Only then it executes the end-to-end tests.
 
 set -ex
 ./gradlew compileKotlin
-# proxy durch localhost ersetzen?
 timeout 240 sh -c "while ! wget http://proxy:80/api/actuator/health; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'backend server responded'"
+timeout 120 sh -c "while ! wget http://proxy:80/skyminder-dummy-server/actuator/health; do echo 'skyminder dummy server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'skyminder dummy server responded'"
 ./gradlew :dataland-e2etests:test --no-daemon
 ./gradlew :dataland-frontend:npm_run_testproductive --no-daemon
