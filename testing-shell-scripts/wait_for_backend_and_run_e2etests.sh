94cd98202eb502f72948755e855e6876973c2524
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-03 08:37:12 +0100

    -y flag for apt install

--- testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
+++ testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
@@ -12,5 +12,5 @@ timeout 240 sh -c "while ! curl http://proxy:80/api/actuator/health; do echo 'ba
 
 dpkg --add-architecture i386
 apt-get update
-apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 ./gradlew :dataland-frontend:npm_run_testproductive --no-daemon
