f47ded88dc371e49404ec0049c34c0f2fa6c8e9c
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-04 15:47:08 +0100

    set babel config to prod env

--- dataland-frontend/babel.config.js
+++ dataland-frontend/babel.config.js
@@ -1,5 +1,5 @@
 const plugins = []
-if (process.env.NODE_ENV === 'e2e') {
+if (process.env.NODE_ENV === 'production') {
   plugins.push([
     "babel-plugin-istanbul", {
       // specify some options for NYC instrumentation here
