5fe40761eca74ce90690d59c714b7590d3c35934
harjmandi <hamid.arjmandi@d-fine.de>
2022-03-04 17:19:20 +0100

    skyminder: revert the last commit

--- dataland-skyminder-server/src/test/kotlin/org/dataland/skyminderDummyServer/SkyminderDummyServerTest.kt
+++ dataland-skyminder-server/src/test/kotlin/org/dataland/skyminderDummyServer/SkyminderDummyServerTest.kt
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.boot.runApplication
-import org.junit.jupiter.api.Assertions.assertNotNull
 
 @SpringBootTest
 @AutoConfigureMockMvc
