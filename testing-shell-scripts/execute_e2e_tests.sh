d4c72da184ea2f06a205192ae4e8d7f72e5865cc
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-04 18:11:26 +0100

    copy coverage report from e2e container

--- testing-shell-scripts/execute_e2e_tests.sh
+++ testing-shell-scripts/execute_e2e_tests.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -ex
-echo $SKYMINDER_URL $SKYMINDER_USER $SKYMINDER_PW | md5sum
 #Start E2E Test and wait for E2E Test completion
 docker-compose --project-name dala-e2e-test up -d
 # is 15 min long enough for all tests?
@@ -10,7 +9,7 @@ timeout 900 sh -c "docker logs dala-e2e-test_e2etests_1 --follow"
 docker stop dala-e2e-test_backend_1
 timeout 90 sh -c "docker logs dala-e2e-test_backend_1 --follow"
 docker cp dala-e2e-test_backend_1:/app/dataland-backend/build/jacoco/bootRun.exec .
-
+docker cp dala-e2e-test_e2etests_1:/app/dataland-frontend/coverage/**/lcov.info .
 # Check execution success of Test Container
 TEST_EXIT_CODE=`docker inspect -f '{{.State.ExitCode}}' dala-e2e-test_e2etests_1`
 echo "Docker E2E Testcontainer exited with code $TEST_EXIT_CODE"
