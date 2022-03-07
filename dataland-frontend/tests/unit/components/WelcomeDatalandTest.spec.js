21ac4155877f70b4e6a445f216a7cbd4c8491441
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-07 16:54:43 +0100

    welcome dataland renamed

--- dataland-frontend/tests/unit/components/WelcomeDatalandTest.spec.js
+++ dataland-frontend/tests/unit/components/WelcomeDatalandTest.spec.js
@@ -1,7 +1,7 @@
 import {shallowMount} from '@vue/test-utils'
 import HelloWorld from '@/components/WelcomeDataland.vue'
 
-describe('HelloWorldTest', () => {
+describe('WelcomeDatalandTest', () => {
     it('renders props.msg when passed', () => {
         const msg = 'new message'
         const wrapper = shallowMount(HelloWorld, {
