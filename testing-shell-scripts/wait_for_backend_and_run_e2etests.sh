77b354884fbd1489ab0195268da8a7189d9bf0fd
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-07 12:08:48 +0100

    wait for gradlew repo

--- testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
+++ testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
@@ -7,8 +7,8 @@
 
 set -ex
 ./gradlew compileKotlin
-timeout 240 sh -c "while ! wget http://proxy:80/api/actuator/health; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'backend server responded'"
-timeout 120 sh -c "while ! wget http://proxy:80/skyminder-dummy-server-actuator-health; do echo 'skyminder dummy server not yet there - retrying in 1s'; sleep 1; done; rm skyminder-dummy-server-actuator-health; echo 'skyminder dummy server responded'"
+timeout 1200 sh -c "while ! wget http://proxy:80/api/actuator/health; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'backend server responded'"
+timeout 1200 sh -c "while ! wget http://proxy:80/skyminder-dummy-server-actuator-health; do echo 'skyminder dummy server not yet there - retrying in 1s'; sleep 1; done; rm skyminder-dummy-server-actuator-health; echo 'skyminder dummy server responded'"
 ./gradlew :dataland-e2etests:test --no-daemon
 ./gradlew :dataland-frontend:npm_run_testpipeline --no-daemon
 ls -R
