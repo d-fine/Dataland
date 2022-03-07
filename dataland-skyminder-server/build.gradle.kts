8beb55a0273534a800d48ef805241d50a1c79d93
harjmandi <hamid.arjmandi@d-fine.de>
2022-03-07 08:01:52 +0100

    skyminder: attempt to get code cover

--- dataland-skyminder-server/build.gradle.kts
+++ dataland-skyminder-server/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.17.2")
     implementation("org.apache.logging.log4j:log4j-to-slf4j:2.17.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.powermock:powermock-module-junit4:1.7.3")
 }
 
 tasks.test {
