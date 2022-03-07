fb14014aeb79014dc12ca3e8bfea12040b993f10
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-07 18:36:08 +0100

    Increased time for Hamid

--- testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
+++ testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
@@ -8,5 +8,5 @@
 set -ex
 ./gradlew compileKotlin
 timeout 240 sh -c "while ! wget http://proxy:80/api/actuator/health; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'backend server responded'"
-timeout 120 sh -c "while ! wget http://proxy:80/api/actuator/health; do echo 'skyminder dummy server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'skyminder dummy server responded'"
+timeout 1200 sh -c "while ! wget http://proxy:80/api/actuator/health; do echo 'skyminder dummy server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'skyminder dummy server responded'"
 ./gradlew :dataland-e2etests:test :dataland-frontend:npm_run_testpipeline --no-daemon
