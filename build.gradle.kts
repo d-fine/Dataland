1e2ef61056199deb7466afbc8341058f683f6d8b
harjmandi <hamid.arjmandi@d-fine.de>
2022-03-07 13:54:00 +0100

    Exclude LocalCorsConfig.kt from sonar code coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ sonarqube {
         property("sonar.coverage.jacoco.xmlReportPaths", file("$buildDir/reports/jacoco/test/jacocoTestReport.xml"))
         property("sonar.qualitygate.wait", true)
         property("sonar.javascript.lcov.reportPaths", fileTree("$projectDir/fe-coverage").files)
-        property("sonar.coverage.exclusions", "**/test/**,**/tests/**")
+        property("sonar.coverage.exclusions", "**/test/**,**/tests/**,LocalCorsConfig.kt")
         property(
             "sonar.sources",
             subprojects.flatMap { project -> project.properties["sonarSources"] as Iterable<*> }
