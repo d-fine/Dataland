9fd4b7a73050e0a70fbae7540ce3b0eea5c8c084
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-07 18:32:06 +0100

    AddCompany has empty list + Company-to-Data-Relationships

---
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/health/HealthCheckSkyminderServer.kt
@@ -0,0 +1,48 @@
+package org.dataland.datalandbackend.health
+
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.apache.tomcat.util.json.JSONParser
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.boot.json.JsonParser
+import org.springframework.stereotype.Component
+
+
+@Component("HealthCheckerSkyminder")
+    class HealthCheckSkyminderServer : HealthIndicator {
+        private val messageKey = "Skyminder-Server"
+
+        override fun health(): Health {
+            return if (!isRunningSkyminderServer()) {
+                Health.down().withDetail(messageKey, "Not Available").build()
+            } else Health.up().withDetail(messageKey, "Available").build()
+        }
+
+        fun isRunningSkyminderServer(): Boolean {
+
+            val client = OkHttpClient()
+
+            var request : Request =
+
+                Request.Builder()
+                    .url("http://localhost/skyminder-dummy-server-actuator-health"/*"http://skyminder-server:8080/actuator/health"*/)
+                    .build();
+
+
+            var call : Call = client.newCall(request);
+            val response : Response = call.execute();
+            val responseString = response.body?.string()
+
+            println(responseString)
+
+            if (responseString != null) {
+                if (responseString.contains("UP")) {
+                    return true
+                }
+            }
+            return false
+        }
+}
