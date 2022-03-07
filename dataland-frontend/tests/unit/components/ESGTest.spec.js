3e89bf78120e95f9e58f4502ce1a1d202f564041
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-07 16:42:03 +0100

    deprecated esg component removed

--- dataland-frontend/tests/unit/components/ESGTest.spec.js
+++
@@ -1,9 +0,0 @@
-import {shallowMount} from '@vue/test-utils'
-import ESG from '@/components/ESG.vue'
-
-describe('ESGTest', () => {
-    it('checks if there exist button in the component', () => {
-        const wrapper = shallowMount(ESG)
-        expect(wrapper.find('button').exists()).toBe(true)
-    })
-})
