3c79521ca44ca99e826606ba26edf705ca5f15fb
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-03-08 09:56:25 +0100

    New health endpoints and develop container for testing and debugging

--- testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
+++ testing-shell-scripts/wait_for_backend_and_run_e2etests.sh
@@ -7,6 +7,6 @@
 
 set -ex
 ./gradlew compileKotlin
-timeout 240 sh -c "while ! wget http://proxy:80/api/actuator/health; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'backend server responded'"
-timeout 1200 sh -c "while ! wget http://proxy:80/api/actuator/health; do echo 'skyminder dummy server not yet there - retrying in 1s'; sleep 1; done; rm health; echo 'skyminder dummy server responded'"
+timeout 240 sh -c "while ! curl http://proxy:80/api/actuator/health/ping 2>/dev/null | grep -q UP; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; echo 'backend server responded'"
+timeout 1200 sh -c "while ! curl http://proxy:80/api/actuator/health/skyminderDummyServer 2>/dev/null | grep -q UP; do echo 'skyminder dummy server not yet there - retrying in 1s'; sleep 1; done; echo 'skyminder dummy server responded'"
 ./gradlew :dataland-e2etests:test :dataland-frontend:npm_run_testpipeline --no-daemon
