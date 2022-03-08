c0c1d25b8303f3653969c15038ac45dd0a6270aa
harjmandi <hamid.arjmandi@d-fine.de>
2022-03-08 08:15:50 +0100

    Frontend: Improve component unit tests

--- dataland-frontend/tests/unit/components/APIClientTest.spec.js
+++ dataland-frontend/tests/unit/components/APIClientTest.spec.js
@@ -1,8 +1,26 @@
 import APIClient from '@/components/APIClient.vue'
+import {mount} from "@vue/test-utils";
 
 describe('APIClientTest', () => {
 
-    it('checks if APIClient.data() and all its dataStore not undefined', () => {
-        expect(typeof APIClient.data()).toBeDefined()
+    const wrapper = mount(APIClient)
+
+    it('checks field properties', () => {
+        expect(wrapper.vm.data).toBeDefined()
+        expect(wrapper.vm.dataStore).toBeDefined()
+        expect(wrapper.vm.loading).toBeDefined()
     })
+
+    it('checks getSkyminderByName()', async () => {
+        expect(wrapper.vm.loading).toBe(false)
+        expect(wrapper.vm.getSkyminderByName()).toBeDefined()
+        expect(wrapper.vm.loading).toBe(true)
+        await new Promise(timerHandler => setTimeout(timerHandler, 4000)); /* sleep for 4s*/
+        expect(wrapper.vm.loading).toBe(false)
+    })
+
+    it('checks clearGetOutput()', () => {
+        expect(wrapper.vm.clearGetOutput()).toBeDefined()
+    })
+
 })
