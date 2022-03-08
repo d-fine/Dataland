7ff938b7c8843e9c542afab6a65e2afad752bc11
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-03-08 11:33:17 +0100

    Deployment changes

--- deployment/deploy_to_preview.sh
+++ deployment/deploy_to_preview.sh
@@ -7,5 +7,8 @@ chmod 600 ~/.ssh/id_rsa
 timeout 300 bash -c "while ! ssh -o ConnectTimeout=3 ubuntu@3.71.162.94 exit; do echo 'target server not yet there - retrying in 1s'; sleep 1; done" || exit
 
 scp ./deployment/start_app_on_server.sh ubuntu@3.71.162.94:/home/ubuntu/start_app_on_server.sh
+scp ./deployment/docker-compose.yml ubuntu@3.71.162.94:/home/ubuntu/docker-compose.yml
+scp ./nginx.conf ubuntu@3.71.162.94:/home/ubuntu/nginx.conf
 scp "$1" ubuntu@3.71.162.94:/home/ubuntu/jar/dala-backend.jar
-ssh ubuntu@3.71.162.94 "sudo /home/ubuntu/start_app_on_server.sh"
+scp -r "$2" ubuntu@3.71.162.94:/home/ubuntu/dist
+ssh ubuntu@3.71.162.94 "export SKYMINDER_URL=$SKYMINDER_URL; export SKYMINDER_PW=$SKYMINDER_PW; export SKYMINDER_USER=$SKYMINDER_USER; sudo /home/ubuntu/start_app_on_server.sh"
--- deployment/start_app_on_server.sh
+++ deployment/start_app_on_server.sh
@@ -2,4 +2,5 @@
 
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
-docker run -d --restart always --name dala-backend -v /home/ubuntu/jar:/jar -p80:8080 eclipse-temurin:17.0.2_8-jre-alpine java -jar /jar/dala-backend.jar
+
+docker compose up -d
