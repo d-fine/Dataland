d3ca889b42ee76dbb9bb1596755208fe511d24ad
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-08 11:31:01 +0100

    ktlint fix

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/health/SkyminderDummyServerHealthIndicator.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/health/SkyminderDummyServerHealthIndicator.kt
@@ -38,7 +38,7 @@ class SkyminderDummyServerHealthIndicator : HealthIndicator {
             if (response.body?.string().toString().contains("UP")) {
                 running = true
             }
-        } catch (_: Exception) {Unit}
+        } catch (_: Exception) { Unit }
         return running
     }
 }
