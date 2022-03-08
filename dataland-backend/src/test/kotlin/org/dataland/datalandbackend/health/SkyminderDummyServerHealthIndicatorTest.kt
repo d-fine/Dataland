4c30314ec47ae7a298a655ca5f5c54ecc4dfbb93
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-08 10:42:02 +0100

    Format fixes

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/health/SkyminderDummyServerHealthIndicatorTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/health/SkyminderDummyServerHealthIndicatorTest.kt
@@ -6,21 +6,26 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class InMemoryDataStoreTest {
+class SkyminderDummyServerHealthIndicatorTest {
     val skyminderDummyServerHealthIndicator = SkyminderDummyServerHealthIndicator()
+
     @Test
     fun `test if health of the Skyminder Dummy Server has the status DOWN because it is not responding`() {
         val status: String = skyminderDummyServerHealthIndicator.health().status.code
-        assertEquals(status, "DOWN",
+        assertEquals(
+            status, "DOWN",
             "The Skyminder Dummy Server Health Indicator is not returning the status \"DOWN\"." +
-                    " It should return \"DOWN\", since this is a unit test and no services should be reachable.")
+                " It should return \"DOWN\", since this is a unit test and no services should be reachable."
+        )
     }
 
     @Test
     fun `test if the Skyminder Dummy Server container is not responding and therefore the output is false`() {
         val isRunningSkyminderServer: Boolean = skyminderDummyServerHealthIndicator.isRunningSkyminderServer()
-        assertFalse(isRunningSkyminderServer,
+        assertFalse(
+            isRunningSkyminderServer,
             "The Skyminder Dummy Server Health Indicator is not returning the status \"DOWN\"." +
-                    " It should return \"DOWN\", since this is a unit test and no services should be reachable.")
+                " It should return \"DOWN\", since this is a unit test and no services should be reachable."
+        )
     }
 }
