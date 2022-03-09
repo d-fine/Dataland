f9be8923bb86f4c1395bb7162dcfa25175718dfd
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-09 13:25:49 +0100

    generate frontend openapi client

--- dataland-frontend/build.gradle.kts
+++ dataland-frontend/build.gradle.kts
@@ -1,14 +1,68 @@
-plugins {
-    id("com.github.node-gradle.node") version "3.2.1"
-}
-
-node {
-    download.set(true)
-    version.set("16.14.0")
-}
 
 val sources = fileTree(projectDir)
 sources.include("src/**", "public/**", "tests/**")
 val sonarSources by extra(sources.files.map { it })
 val jacocoSources by extra(emptyList<File>())
 val jacocoClasses by extra(emptyList<File>())
+
+
+plugins {
+    kotlin("jvm")
+    id("com.github.node-gradle.node") version "3.2.1"
+    id("org.openapi.generator") version "5.4.0"
+}
+
+
+node {
+    download.set(true)
+    version.set("16.14.0")
+}
+
+val backendOpenApiSpecConfig by configurations.creating {
+    isCanBeConsumed = false
+    isCanBeResolved = true
+}
+
+dependencies {
+    backendOpenApiSpecConfig(project(mapOf("path" to ":dataland-backend", "configuration" to "openApiSpec")))
+}
+
+val backendOpenApiJson = rootProject.extra["backendOpenApiJson"]
+val taskName = "generateBackendClient"
+//val clientOutputDir = "src/clients/backend"
+//val clientOutputDir = "$projectDir/src/clients/backend"
+val clientOutputDir = "$buildDir/Clients/backend"
+val apiSpecLocation = "$buildDir/$backendOpenApiJson"
+val destinationPackage = "org.dataland.datalandfrontend.openApiClient"
+
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    dependsOn(taskName)
+}
+
+tasks.register<Copy>("getBackendOpenApiSpec") {
+    from(backendOpenApiSpecConfig)
+    into("$buildDir")
+}
+
+
+tasks.register(taskName, org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class) {
+    input = project.file(apiSpecLocation).path
+    outputDir.set(clientOutputDir)
+    modelPackage.set("$destinationPackage.model")
+    apiPackage.set("$destinationPackage.api")
+    packageName.set(destinationPackage)
+    generatorName.set("typescript-axios")
+    dependsOn("getBackendOpenApiSpec")
+}
+
+sourceSets {
+    val main by getting
+    main.java.srcDir("$clientOutputDir/src/main/kotlin")
+}
+
+ktlint {
+    filter {
+        exclude("**/openApiClient/**")
+    }
+}
