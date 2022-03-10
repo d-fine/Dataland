70f862fa6a1ba0cc040b9ae88d048397a674bc6f
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-03-10 12:22:39 +0100

    Changes for fixing enum problem

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/EuTaxonomyDataSet.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/EuTaxonomyDataSet.kt
@@ -6,8 +6,8 @@ data class EuTaxonomyDataSet(
     @field:JsonProperty("Capex") val capex: EuTaxonomyData? = null,
     @field:JsonProperty("Opex") val opex: EuTaxonomyData? = null,
     @field:JsonProperty("Revenues") val revenues: EuTaxonomyData? = null,
-    @field:JsonProperty("Reporting Obligation", required = true) val reportObligation: YesNo,
-    @field:JsonProperty("Attestation", required = true) val attestation: AttestationOptions
+    @field:JsonProperty("Reporting Obligation", required = true) val reportObligation: YesNo? = null,
+    @field:JsonProperty("Attestation", required = true) val attestation: AttestationOptions? = null
 ) {
     enum class YesNo { Yes, No }
     enum class AttestationOptions { None, Some, Full }
