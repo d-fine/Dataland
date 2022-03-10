85a6ff700da9da2ec8cfc07a2582d4b507628917
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-10 10:41:07 +0100

    it works, all good

--- dataland-frontend/src/App.vue
+++ dataland-frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="@/assets/images/logos/dataland-logo-lr.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <ApiClient/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import ApiClient from './components/APIClient.vue';
 
 @Options({
   components: {
     HelloWorld,
+    ApiClient
   },
 })
 export default class App extends Vue {}
--- dataland-frontend/src/components/APIClient.vue
+++ dataland-frontend/src/components/APIClient.vue
@@ -1,75 +1,35 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col s12 m12">
-        <div class="card">
-          <div class="card-title"><h2>Contact Data Search</h2>
-          </div>
-          <div class="card-content ">
-
-            <div class="row">
-                <div class="input-field col s12 m6">
-                  <input type="text" v-model="data.code" class="autocomplete" id="countryCode" @keyup.enter="getSkyminderByName"/>
-                  <label for="countryCode">Please insert a 3 Letter country code</label>
-                </div>
-                <div class="input-field col s12 m6">
-                  <input type="text" v-model="data.name" class="autocomplete" id="companyName" @keyup.enter="getSkyminderByName"/>
-                  <label for="companyName">Please insert the name of the company</label>
-                </div>
-            </div>
-            <div class="row">
-              <div class="col m6">
-                  <button class="btn btn-sm" @click="clearGetOutput">Clear</button>
-              </div>
-              <div class="col m6">
-                  <button class="btn btn-sm" @click="getSkyminderByName">Get Skyminder by Name</button>
-              </div>
-            </div>
-
-            <div class="progress" v-if="loading">
-              <div class="indeterminate" ></div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-      <div v-if="data.result" class="col m12">
-        <ResultTable :headers="['Name', 'Address', 'Website', 'Email', 'Phone', 'Identifier']" :data="data.result"/>
-      </div>
-    </div>
+      <button class="btn btn-sm" @click="getSkyminderByName">Click </button>
   </div>
 </template>
 <script>
 
-import {DataStore} from "@/service/DataStore";
-import {Data} from "@/model/Data"
-import ResultTable from "@/components/ui/ResultTable"
-
-const apiClient = {
+import {DataControllerApi} from "@/clients/backend";
+export default {
   name: "APIClient",
   components: {
-    ResultTable
   },
   data() {
     return {
-      dataStore: new DataStore(process.env.VUE_APP_API_URL),
-      data: new Data(),
-      loading: false
+      dataStore: new DataControllerApi(),
+      loading: false,
+      data: null
     }
   },
   methods: {
     async getSkyminderByName() {
       this.loading = true
-      this.data.getResult(await this.dataStore.getByName(this.data.code, this.data.name))
+      this.data = await this.dataStore.getDataSkyminderRequest("DEU", "BMW")
       this.loading = false
     },
 
     async clearGetOutput() {
-      this.data.clearAll()
+      this.data = null
     },
   }
 }
 
-export default apiClient
+
 
 </script>
