ae24b703c389e655eeb13fe9f12052c8b5938022
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-14 21:43:00 +0100

    basic corperate styling

---
+++ dataland-frontend/src/components/ui/SuccessUpload.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <div class="card">
+      <div class="card-title green-text"><h4>You successfully uploaded the {{msg}}</h4>
+      </div>
+      <div class="card-content left-align">
+        <p v-for="(value, key) in data" :key="key">{{ humanize(key) }}: {{value}}</p>
+
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "SuccessUpload",
+  props: {
+    msg: String,
+    data: Object
+  },
+  methods: {
+    humanize(text) {
+      const processedText = text.replace(/([A-Z])/g, " $1");
+      return processedText.charAt(0).toUpperCase() + processedText.slice(1);
+    }
+}
+}
+</script>
+
+<style scoped>
+
+</style>
