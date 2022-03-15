87c9b3b3d27d890820fc82b27c243dda79b25f05
harjmandi <hamid.arjmandi@d-fine.de>
2022-03-15 10:33:14 +0100

    Frontend: Collect .ts for code coverage

--- dataland-frontend/jest.config.js
+++ dataland-frontend/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   collectCoverage: true,
-  collectCoverageFrom: [`src/**/*.{js,vue}`],
+  collectCoverageFrom: [`src/**/*.{js,vue,ts}`],
   coverageDirectory: "coverage/unit",
   coverageReporters: ["lcov", ["text", {"skipFull": false}]]
 }
