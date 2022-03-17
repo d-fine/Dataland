4feb49d6a0e36abe0be5fbbe82dde0b4cb432712
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-17 13:27:53 +0100

    Save

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/service/DataManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/service/DataManagerTest.kt
@@ -10,9 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest
 // TODO Add error messages to Assertions?
 
 @SpringBootTest
-class InMemoryDataStoreTest {
+class DataManagerTest {
 
-    val testStore = InMemoryDataStore()
+    val testManager = DataManager(dataStore = InMemoryDataStore())
     val testCompanyNamesToStore = listOf("Imaginary-Company_I", "Fantasy-Company_II", "Dream-Company_III")
 
     val testDataSetsToStore = listOf(
@@ -23,17 +23,17 @@ class InMemoryDataStoreTest {
 
     @Test
     fun `add the first company and check if its name is as expected by using the return value of addCompany`() {
-        val companyMetaInformation = testStore.addCompany(testCompanyNamesToStore[0])
+        val companyMetaInformation = testManager.addCompany(testCompanyNamesToStore[0])
         assertEquals(companyMetaInformation.companyName, testCompanyNamesToStore[0])
     }
 
     @Test
     fun `add all companies, retrieve them as a list and check for each company if its name is as expected`() {
         for (companyName in testCompanyNamesToStore) {
-            testStore.addCompany(companyName)
+            testManager.addCompany(companyName)
         }
 
-        val allCompaniesInStore = testStore.listAllCompanies()
+        val allCompaniesInStore = testManager.listCompaniesByName("")
         for ((counter, storedCompany) in allCompaniesInStore.withIndex()) {
             assertEquals(testCompanyNamesToStore[counter], storedCompany.companyName)
         }
@@ -42,11 +42,11 @@ class InMemoryDataStoreTest {
     @Test
     fun `add all companies and search for them one by one by using their names`() {
         for (companyName in testCompanyNamesToStore) {
-            testStore.addCompany(companyName)
+            testManager.addCompany(companyName)
         }
 
         for (companyName in testCompanyNamesToStore) {
-            val searchResponse = testStore.listCompaniesByName(companyName)
+            val searchResponse = testManager.listCompaniesByName(companyName)
             assertEquals(companyName, searchResponse.first().companyName)
         }
     }
@@ -55,14 +55,14 @@ class InMemoryDataStoreTest {
 
     @Test
     fun `post the first company and all dummy data sets for it and check if all data sets of it are listed`() {
-        val testCompanyId = testStore.addCompany(testCompanyNamesToStore[0]).companyId
+        val testCompanyId = testManager.addCompany(testCompanyNamesToStore[0]).companyId
 
         val listOfDataIds = mutableListOf<String>()
         for (dataSet in testDataSetsToStore) {
-            listOfDataIds.add(testStore.addDataSet(dataSet))
+            listOfDataIds.add(testManager.addDataSet(dataSet))
         }
 
-        val dataSetsOfTestCompany = testStore.listDataSetsByCompanyId(testCompanyId)
+        val dataSetsOfTestCompany = testManager.listDataSetsByCompanyId(testCompanyId)
 
         for (dataId in listOfDataIds) {
             assertEquals(
@@ -75,39 +75,39 @@ class InMemoryDataStoreTest {
     @Test
     fun `get companies with a name that does not exist yet in the store`() {
         assertThrows<IllegalArgumentException> {
-            testStore.listCompaniesByName("error")
+            testManager.listCompaniesByName("error")
         }
     }
 
     @Test
     fun `get the data sets for a company id that does not exist yet in the store`() {
         assertThrows<IllegalArgumentException> {
-            testStore.listDataSetsByCompanyId("error")
+            testManager.listDataSetsByCompanyId("error")
         }
     }
 
     @Test
     fun `add and get data set by identifier`() {
-        testStore.addCompany(testCompanyNamesToStore[0]).companyId
+        testManager.addCompany(testCompanyNamesToStore[0]).companyId
 
-        val testDataSetId = testStore.addDataSet(testDataSetsToStore[0])
+        val testDataSetId = testManager.addDataSet(testDataSetsToStore[0])
         assertEquals(
             testDataSetsToStore[0].data,
-            testStore.getDataSet(DataIdentifier(dataId = testDataSetId, dataType = testDataSetsToStore[0].dataType))
+            testManager.getDataSet(DataIdentifier(dataId = testDataSetId, dataType = testDataSetsToStore[0].dataType))
         )
     }
 
     @Test
     fun `add all test data sets and check if they appear in the list of all data sets`() {
-        testStore.addCompany(testCompanyNamesToStore[0]).companyId
+        testManager.addCompany(testCompanyNamesToStore[0]).companyId
 
         val listOfDataIds = mutableListOf<String>()
 
         for (dataSet in testDataSetsToStore) {
-            listOfDataIds.add(testStore.addDataSet(dataSet))
+            listOfDataIds.add(testManager.addDataSet(dataSet))
         }
 
-        val listOfAllDataSets = testStore.listDataSets()
+        val listOfAllDataSets = testManager.listDataSets()
 
         for (dataId in listOfDataIds) {
             assertEquals(
@@ -125,29 +125,29 @@ class InMemoryDataStoreTest {
     fun `get add data set error`() {
         val invalidDataSetToStore = StoredDataSet(companyId = "error", dataType = "someDataType", data = "random_data")
         assertThrows<IllegalArgumentException> {
-            testStore.addDataSet(invalidDataSetToStore)
+            testManager.addDataSet(invalidDataSetToStore)
         }
     }
 
     @Test
     fun `produce get data set error for invalid data id`() {
-        testStore.addCompany(testCompanyNamesToStore[0]).companyId
+        testManager.addCompany(testCompanyNamesToStore[0]).companyId
 
-        testStore.addDataSet(testDataSetsToStore[0])
+        testManager.addDataSet(testDataSetsToStore[0])
 
         assertThrows<IllegalArgumentException> {
-            testStore.getDataSet(DataIdentifier(dataId = "error", dataType = testDataSetsToStore[0].dataType))
+            testManager.getDataSet(DataIdentifier(dataId = "error", dataType = testDataSetsToStore[0].dataType))
         }
     }
 
     @Test
     fun `produce get data set error for invalid data type`() {
-        testStore.addCompany(testCompanyNamesToStore[0]).companyId
+        testManager.addCompany(testCompanyNamesToStore[0]).companyId
 
-        val testDataSetId = testStore.addDataSet(testDataSetsToStore[0])
+        val testDataSetId = testManager.addDataSet(testDataSetsToStore[0])
 
         assertThrows<IllegalArgumentException> {
-            testStore.getDataSet(DataIdentifier(dataId = testDataSetId, dataType = "error"))
+            testManager.getDataSet(DataIdentifier(dataId = testDataSetId, dataType = "error"))
         }
     }
 }
