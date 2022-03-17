1bb344aaea8830553d8af36760e4530d624c9bdf
Philip Puetsch <philip.puetsch@d-fine.de>
2022-03-17 17:01:43 +0100

    simplify as per review comment

--- deployment/deploy_to_preview.sh
+++ deployment/deploy_to_preview.sh
@@ -11,7 +11,7 @@ location=/home/ubuntu/dataland
 # shut down currently running dataland application and purge files on server
 ssh ubuntu@3.71.162.94 "cd $location && sudo docker-compose down; rm -rf $location; mkdir -p $location/jar"
 # make sure no remnants remain when docker-compose file changes
-ssh ubuntu@3.71.162.94 'sudo docker kill $(sudo docker ps -q); sudo docker rm $(sudo docker ps -a -q); sudo docker system prune --force; sudo docker info'
+ssh ubuntu@3.71.162.94 'sudo docker kill $(sudo docker ps -q); sudo docker system prune --force; sudo docker info'
 
 scp -r ./dataland-frontend/dist ./deployment/docker-compose.yml ./dataland-inbound-proxy/ ubuntu@3.71.162.94:$location
 scp ./dataland-backend/build/libs/dataland-backend*.jar ubuntu@3.71.162.94:$location/jar/dataland-backend.jar
