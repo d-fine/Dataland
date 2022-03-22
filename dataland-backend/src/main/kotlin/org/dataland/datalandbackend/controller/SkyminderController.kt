a82520b6918ae6634da328650a6160bb0dc1c43a
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-03-22 11:46:56 +0100

    Update to new skyminder version

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/SkyminderController.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/SkyminderController.kt
@@ -1,7 +1,6 @@
 package org.dataland.datalandbackend.controller
 
 import org.dataland.datalandbackend.api.SkyminderAPI
-import org.dataland.skyminderClient.interfaces.DataConnectorInterface
 import org.dataland.skyminderClient.model.ContactInformation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
@@ -9,16 +8,16 @@ import org.springframework.web.bind.annotation.RestController
 
 /**
  * Implementation of the API for using Skyminder
- * @param dataConnector implementation of the DataConnectorInterface that defines how to connect to the data
+ * @param skyminderClient implementation of the DataConnectorInterface that defines how to connect to the data
  * source (e.g. Skyminder)
  */
 
 @RestController
-class SkyminderController(@Autowired var dataConnector: DataConnectorInterface) : SkyminderAPI {
+class SkyminderController(@Autowired var skyminderClient: SkyminderConnectorInterface) : SkyminderAPI {
     override fun getDataSkyminderRequest(countryCode: String, companyName: String):
         ResponseEntity<List<ContactInformation>> {
         return ResponseEntity.ok(
-            this.dataConnector.getContactInformation(
+            this.skyminderClient.getContactInformation(
                 countryCode = countryCode,
                 name = companyName
             )
