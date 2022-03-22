c3a90fb47aec1d21ae21ff5e23e30b0319d7709d
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-22 10:34:43 +0100

    Deleted unnecessary annotations per ppuetsch comment

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/DataController.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/DataController.kt
@@ -6,19 +6,16 @@ import org.dataland.datalandbackend.interfaces.DataStoreInterface
 import org.dataland.datalandbackend.model.DataIdentifier
 import org.dataland.datalandbackend.model.DataSetMetaInformation
 import org.dataland.datalandbackend.model.StorableDataSet
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RestController
 
 /**
  * Implementation of the API for data exchange
  * @param dataStore implementation of the DataStoreInterface that defines how uploaded data is to be stored
  */
 
-@RestController
 abstract class DataController<T>(
-    @Autowired var dataStore: DataStoreInterface,
-    @Autowired var objectMapper: ObjectMapper,
+    var dataStore: DataStoreInterface,
+    var objectMapper: ObjectMapper,
     val clazz: Class<T>
 ) : DataAPI<T> {
     private val dataType = clazz.toString().substringAfterLast(".")
