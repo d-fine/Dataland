43fac4fb05d0a70e29d3fc99d83f049c2e2d720f
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-25 15:11:28 +0100

    Changes by d91136

--- dataland-backend/build.gradle.kts
+++ dataland-backend/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
     implementation("org.dataland:skyminder-client:0.1.2")
+    implementation("org.dataland:dataland-edc-client:0.0.3-SNAPSHOT")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("org.apache.logging.log4j:log4j:2.17.2")
     implementation("org.apache.logging.log4j:log4j-api:2.17.2")
