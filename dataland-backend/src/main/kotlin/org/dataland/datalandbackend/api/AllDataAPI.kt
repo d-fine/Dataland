e2d30026b844dba54801b5b9cbdd79d92504435a
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-03-25 10:33:57 +0100

    adopted interface

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/AllDataAPI.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/AllDataAPI.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.dataland.datalandbackend.model.DataSetMetaInformation
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -26,10 +27,30 @@ interface AllDataAPI {
     )
     /**
      * A method to search for meta data on all data sets registered by Dataland
-     * @param dataId as unique identifier for a specific data set
+     * @param companyId filters the requested meta data to a specific company.
      * @param dataType filters the requested meta data to a specific data type.
-     * @return the ID of the created entry in the data store
+     * @return a list of matching DataSetMetaInformation
      */
-    fun getData(@RequestParam dataId: String? = null, @RequestParam dataType: String? = null):
+    fun getData(@RequestParam companyId: String? = null, @RequestParam dataType: String? = null):
         List<DataSetMetaInformation>
+
+    @Operation(
+        summary = "Look up meta data on Dataland for specific data set.",
+        description = "Meta info of a specific data set identified by its data ID is retrieved."
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Successfully retrieved meta data.")
+        ]
+    )
+    @GetMapping(
+        value = ["/{dataId}"],
+        produces = ["application/json"]
+    )
+            /**
+             * A method to retrieve the meta data for a specific data set
+             * @param dataId as unique identifier for a specific data set
+             * @return the DataSetMetaInformation of the specified data set
+             */
+    fun getData(@PathVariable dataId: String): DataSetMetaInformation
 }
