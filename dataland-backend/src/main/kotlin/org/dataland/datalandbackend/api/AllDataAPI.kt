557c46226798313c325a225d0d3afb9131fca7d4
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-25 09:07:34 +0100

    Build Strawman

---
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/AllDataAPI.kt
@@ -0,0 +1,11 @@
+package org.dataland.datalandbackend.api
+
+// TODO: Endpoint to search and filter all data meta data
+
+interface AllDataAPI {
+
+    /* Inputs: Search and filter params
+    Output: List of DataSetMetaInformation => user can take the output and query a dataset via the respective
+    data endpoint (e.g. /eutaxonomies)
+     */
+}
