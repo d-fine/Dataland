ef51ee3855c676cd1d7dce3301581e92ae06f5c3
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-03-28 13:46:04 +0200

    Fixed Snapshot version, fixed application.properties, added test

---
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/controller/AllDataControllerTest.kt
@@ -0,0 +1,51 @@
+package org.dataland.datalandbackend.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.dataland.datalandbackend.model.PostCompanyRequestBody
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class AllDataControllerTest(
+    @Autowired var mockMvc: MockMvc,
+    @Autowired var objectMapper: ObjectMapper
+) {
+
+    val testCompanyName = "Imaginary-Company_I"
+
+    fun uploadCompany(mockMvc: MockMvc, companyName: String) {
+        val postCompanyRequestBody = PostCompanyRequestBody(companyName = companyName)
+        mockMvc.perform(
+            post("/companies")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(postCompanyRequestBody))
+        )
+            .andExpectAll(status().isOk, content().contentType(MediaType.APPLICATION_JSON))
+    }
+/*
+    @Test
+    fun `list of all data sets for a specific company Id can be retrieved and is empty because no data was posted`() {
+        uploadCompany(mockMvc, testCompanyName)
+
+        mockMvc.perform(
+            get("/data param blabla")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpectAll(
+                status().isOk,
+                content().contentType(MediaType.APPLICATION_JSON),
+                content().string("[]")
+            )
+    }
+}
