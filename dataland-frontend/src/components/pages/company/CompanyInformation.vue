33615f6adea7357303e3541622223bbcaf8aaad6
harjmandi <hamid.arjmandi@d-fine.de>
2022-03-31 11:40:50 +0200

    small changes

--- dataland-frontend/src/components/pages/company/CompanyInformation.vue
+++ dataland-frontend/src/components/pages/company/CompanyInformation.vue
@@ -42,7 +42,6 @@ export default {
       this.companyInfo = await companyStore.perform(this.companyID)
     },
     async getCompanyDataset() {
-      console.log(this.companyID)
       this.response = await dataStore.perform(this.companyID, "")
     }
   }
