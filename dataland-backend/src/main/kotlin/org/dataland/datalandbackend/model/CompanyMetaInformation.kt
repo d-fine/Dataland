278a01ecbf0372714dcbb95613fb0211554a32de
harjmandi <hamid.arjmandi@d-fine.de>
2022-03-31 17:04:31 +0200

    backend: new company models

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/CompanyMetaInformation.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/CompanyMetaInformation.kt
@@ -1,8 +1,6 @@
 package org.dataland.datalandbackend.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.math.BigDecimal
-import java.util.Date
 
 /**
  * --- API model ---
--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/service/DataManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/service/DataManagerTest.kt
@@ -1,17 +1,35 @@
 package org.dataland.datalandbackend.service
 
 import org.dataland.datalandbackend.edcClient.api.DefaultApi
+import org.dataland.datalandbackend.model.PostCompanyRequestBody
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.boot.test.context.SpringBootTest
+import java.math.BigDecimal
+import java.util.Date
 
 @SpringBootTest
 class DataManagerTest {
 
     val testManager = DataManager(edcClient = DefaultApi(basePath = "dummy"))
 
-    val testCompanyNamesToStore = listOf("Imaginary-Company_I", "Fantasy-Company_II", "Dream-Company_III")
+    val testCompanyList = listOf(
+        PostCompanyRequestBody(
+            companyName = "Test-Company_1",
+            headquarters = "Test-Headquarters_1",
+            industrialSector = "Test-IndustrialSector_1",
+            marketCap = BigDecimal(100),
+            reportingDateOfMarketCap = Date()
+        ),
+        PostCompanyRequestBody(
+            companyName = "Test-Company_2",
+            headquarters = "Test-Headquarters_2",
+            industrialSector = "Test-IndustrialSector_2",
+            marketCap = BigDecimal(200),
+            reportingDateOfMarketCap = Date()
+        )
+    )
 
     /*
     val testDataSetsToStore = listOf(
@@ -29,23 +47,23 @@ class DataManagerTest {
 
     @Test
     fun `add the first company and check if its name is as expected by using the return value of addCompany`() {
-        val companyMetaInformation = testManager.addCompany(testCompanyNamesToStore[0])
+        val companyMetaInformation = testManager.addCompany(testCompanyList[0])
         assertEquals(
-            companyMetaInformation.companyName, testCompanyNamesToStore[0],
+            companyMetaInformation.postCompanyRequestBody.companyName, testCompanyList[0].companyName,
             "The company name in the post-response does not match the actual name of the company to be posted."
         )
     }
 
     @Test
     fun `add all companies then retrieve them as a list and check for each company if its name is as expected`() {
-        for (companyName in testCompanyNamesToStore) {
-            testManager.addCompany(companyName)
+        for (company in testCompanyList) {
+            testManager.addCompany(company)
         }
 
         val allCompaniesInStore = testManager.listCompaniesByName("")
         for ((counter, storedCompany) in allCompaniesInStore.withIndex()) {
             assertEquals(
-                testCompanyNamesToStore[counter], storedCompany.companyName,
+                testCompanyList[counter].companyName, storedCompany.postCompanyRequestBody.companyName,
                 "The stored company name does not match the test company name."
             )
         }
@@ -53,14 +71,14 @@ class DataManagerTest {
 
     @Test
     fun `add all companies and search for them one by one by using their names`() {
-        for (companyName in testCompanyNamesToStore) {
-            testManager.addCompany(companyName)
+        for (company in testCompanyList) {
+            testManager.addCompany(company)
         }
 
-        for (companyName in testCompanyNamesToStore) {
-            val searchResponse = testManager.listCompaniesByName(companyName)
+        for (company in testCompanyList) {
+            val searchResponse = testManager.listCompaniesByName(company.companyName)
             assertEquals(
-                companyName, searchResponse.first().companyName,
+                company.companyName, searchResponse.first().postCompanyRequestBody.companyName,
                 "The posted company could not be found in the data store by searching for its name."
             )
         }
