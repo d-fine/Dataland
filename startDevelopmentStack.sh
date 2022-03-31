c702f2d04511826528fec84c4ed4e34651a74dfa
Stephan Sinn <stephan.sinn@d-fine.de>
2022-03-31 14:35:34 +0200

    easier development stack for backend and frontend dev

---
+++ startDevelopmentStack.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#start containers for skyminder and edc-dummyserver
+docker-compose --profile development up -d
+#start the backend
+./gradlew dataland-backend:bootRun --args='--spring.profiles.active=dev'
