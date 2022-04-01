972f819af47694df23c9e8c15f156d2bc0f23cb1
harjmandi <hamid.arjmandi@d-fine.de>
2022-04-01 11:18:22 +0200

    frontend: small changes

--- dataland-frontend/src/components/forms/RetrieveCompany.vue
+++ dataland-frontend/src/components/forms/RetrieveCompany.vue
@@ -27,7 +27,7 @@
           <caption><h4>Company Search</h4></caption>
           <thead>
           <tr>
-            <th v-for="(header, i) in ['Name', 'headquarter', 'sector','Market cap', 'market Cap date']" :key="i">
+            <th v-for="(header, i) in ['Name', 'Headquarter', 'Sector','Market cap', 'Market Cap date']" :key="i">
               {{ header }}
             </th>
           </tr>
