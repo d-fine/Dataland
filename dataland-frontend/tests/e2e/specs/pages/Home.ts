57383363ac2027078eab4ccda38ad8507044e963
Stephan Sinn <stephan.sinn@d-fine.de>
2022-04-01 16:11:45 +0200

    corrected home test

--- dataland-frontend/tests/e2e/specs/pages/Home.ts
+++ dataland-frontend/tests/e2e/specs/pages/Home.ts
@@ -4,11 +4,11 @@ describe('Index testsuite', () => {
         cy.get('#app').should('exist')
     })
     it('Logo and welcome message are present', () => {
-        cy.get('h1').should("have.text","Welcome to DataLand")
+        cy.get('h1').should("contain.text","CREATE A DATASET")
         cy.get('img[alt="Dataland logo"]')
             .should('be.visible')
             .should('have.attr', 'src')
-            .should('include','dataland-logo')
+            .should('include','vision')
 
     })
 
