786f89f8f92927e5f1b44006105d0cf5fba5f7a7
harjmandi <hamid.arjmandi@d-fine.de>
2022-04-01 11:40:15 +0200

    fix ktlint

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/CompanyAPI.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/CompanyAPI.kt
@@ -23,8 +23,7 @@ interface CompanyAPI {
 
     @Operation(
         summary = "Add a new company.",
-        description = "A new company is added using the provided information, the response includes " +
-                "the generated company ID."
+        description = "A new company is added using the provided information with the generated company ID in response."
     )
     @ApiResponses(
         value = [
