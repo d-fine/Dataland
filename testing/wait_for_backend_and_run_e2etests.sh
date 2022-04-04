5accf8a9c0924d8a2904786f05b57e80e55eaa7c
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-04-04 12:26:35 +0200

    Delete comment

--- testing/wait_for_backend_and_run_e2etests.sh
+++ testing/wait_for_backend_and_run_e2etests.sh
@@ -20,10 +20,3 @@ is_infrastructure_up () {
 export -f is_infrastructure_up
 timeout 240 bash -c "while ! is_infrastructure_up; do echo 'infrastructure not yet completely there - retrying in 1s'; sleep 1; done; echo 'infrastructure up!'"
 ./gradlew :dataland-e2etests:test :dataland-frontend:npm_run_testpipeline --no-daemon
-
-
-
-#timeout 240 sh -c "while ! curl http://proxy:80/api/actuator/health/ping 2>/dev/null | grep -q UP; do echo 'backend server not yet there - retrying in 1s'; sleep 1; done; echo 'backend server responded'"
-#timeout 240 sh -c "while ! curl http://proxy:80/api/actuator/health/skyminderDummyServer 2>/dev/null | grep -q UP; do echo 'skyminder dummy server not yet there - retrying in 1s'; sleep 1; done; echo 'skyminder dummy server responded'"
-#timeout 240 sh -c "while ! curl http://proxy:80/api/actuator/health/edcDummyServer 2>/dev/null | grep -q UP; do echo 'edc dummy server not yet there - retrying in 1s'; sleep 1; done; echo 'edc dummy server responded'"
-#./gradlew :dataland-e2etests:test :dataland-frontend:npm_run_testpipeline --no-daemon
