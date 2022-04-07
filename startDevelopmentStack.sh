1a99f5294b19dd58ea7e6ff3b22f24d65843a651
Philip Puetsch <philip.puetsch@d-fine.de>
2022-04-07 17:32:53 +0200

    align spring and docker-compose profile names

--- startDevelopmentStack.sh
+++ startDevelopmentStack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker login ghcr.io -u $DATALAND_SKYMINDERCLIENT_USER -p $DATALAND_SKYMINDERCLIENT_TOKEN
 #start containers for skyminder and edc-dummyserver
-PROXY_NGINX_CONFIG="/dataland-inbound-proxy/nginxdev.conf" docker-compose --profile development up -d
+docker-compose --profile development up -d
 #start the backend
-./gradlew dataland-frontend:generateAPIClientFrontend dataland-backend:bootRun --args='--spring.profiles.active=dev' --no-daemon
+./gradlew dataland-frontend:generateAPIClientFrontend dataland-backend:bootRun --args='--spring.profiles.active=development' --no-daemon
 
