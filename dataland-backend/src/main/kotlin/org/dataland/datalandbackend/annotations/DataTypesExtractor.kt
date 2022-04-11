56092957e60afed83dbba7f9e96ba76618b270e6
Philip Puetsch <philip.puetsch@d-fine.de>
2022-04-11 10:23:07 +0200

    formatting

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/annotations/DataTypesExtractor.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/annotations/DataTypesExtractor.kt
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.ClassPathScanningCandidateComponen
 import org.springframework.core.type.filter.RegexPatternTypeFilter
 import java.util.regex.Pattern
 
-
 /**
  * Class to extract DataType annotations
  */
 class DataTypesExtractor {
     private val logger = LoggerFactory.getLogger(javaClass)
+
     /**
      * Method to list all the permissible data types
      * @return list of all permissible data types
@@ -19,9 +19,9 @@ class DataTypesExtractor {
         val provider = ClassPathScanningCandidateComponentProvider(false)
         provider.addIncludeFilter(RegexPatternTypeFilter(Pattern.compile(".*")))
         val modelBeans = provider.findCandidateComponents("org.dataland.datalandbackend.model")
-        val dataTypes = modelBeans.map{Class.forName(it.beanClassName)}
+        val dataTypes = modelBeans.map { Class.forName(it.beanClassName) }
             .filter { it.isAnnotationPresent(DataType::class.java) }
-            .map{it.simpleName}
+            .map { it.simpleName }
         logger.info("Searching for known Datatypes. Datatypes found: $dataTypes")
         return dataTypes
     }
