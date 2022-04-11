a7c427ebfe63dbd24e2323dffd7116ed2ed943d3
Stephan Sinn <stephan.sinn@d-fine.de>
2022-04-11 11:55:59 +0200

    fixed unit tests

--- dataland-frontend/tests/unit/components/forms/RetrieveSkyminderTest.spec.ts
+++ dataland-frontend/tests/unit/components/forms/RetrieveSkyminderTest.spec.ts
@@ -6,7 +6,7 @@ describe('RetrieveSkyminderTest', () => {
     const wrapper = shallowMount(RetrieveSkyminder)
 
     it('checks field properties', () => {
-        expect(wrapper.vm.data).toBeDefined()
+        expect(wrapper.vm.model).toBeDefined()
         expect(wrapper.vm.schema).toBeDefined()
     })
 
