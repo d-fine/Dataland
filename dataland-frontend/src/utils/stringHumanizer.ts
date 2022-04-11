f26615a2d5c2723f9c888a00ab3a1510d697032f
harjmandi <hamid.arjmandi@d-fine.de>
2022-04-11 18:41:54 +0200

    frontend: improve regex of text humanizer part 1

--- dataland-frontend/src/utils/stringHumanizer.ts
+++ dataland-frontend/src/utils/stringHumanizer.ts
@@ -1,11 +1,9 @@
 export function humanizeString(text: string): string {
     // Split the sting to words
     // FixMe: improve the regex and the code
-    const processedText = text.replace(/((?!^)[A-Z])/g, " $1")
-        .replace(/([0-9][0-9]*)/g, " $1")
-        .replace(/(\s)([A-Z])(\s)/g, "$1$2")
-        .replace(/(\s)([A-Z])$/g, "$2")
-        .replace(/^([A-Z])(\s)/g, "$1")
+    const processedText = text.replace(/((?!^)[A-Z][a-z]+)/g, " $1")
+        .replace(/([A-Z]+)$/g, " $1")
+        .replace(/([0-9]+)/g, " $1")
     // uppercase the first letter of the first word
     return processedText.charAt(0).toUpperCase() + processedText.slice(1)
 }
