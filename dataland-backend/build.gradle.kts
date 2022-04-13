2d4b455d5029a381e345820f8f31307e7d5f81bf
Philip Puetsch <philip.puetsch@d-fine.de>
2022-04-13 11:21:05 +0200

    fix ktlint issues

--- dataland-backend/build.gradle.kts
+++ dataland-backend/build.gradle.kts
@@ -71,5 +71,5 @@ jacoco {
 }
 
 gitProperties {
-    keys =  listOf("git.branch", "git.commit.id", "git.commit.time", "git.commit.id.abbrev")
+    keys = listOf("git.branch", "git.commit.id", "git.commit.time", "git.commit.id.abbrev")
 }
