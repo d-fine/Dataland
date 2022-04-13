3019e8abfe21bc404e2c3a46bb57a994cba8293c
Philip Puetsch <philip.puetsch@d-fine.de>
2022-04-13 12:37:04 +0200

    remove commit id

--- dataland-frontend/tests/e2e/plugins/index.js
+++ dataland-frontend/tests/e2e/plugins/index.js
@@ -1,14 +1,8 @@
-const gitCommitId = require('git-commit-id')
-
-
 module.exports = (on, config) => {
     require('@cypress/code-coverage/task')(on, config)
     return Object.assign({}, config, {
         fixturesFolder: 'tests/e2e/fixtures',
         integrationFolder: 'tests/e2e/specs',
         supportFile: 'tests/e2e/support/index.js',
-        env: {
-            commit_id: gitCommitId({ cwd: ".."})
-        }
     })
 }
