69856ee91c77c997f38dbc27990d99b6edaaab82
Philip Puetsch <philip.puetsch@d-fine.de>
2022-04-13 11:01:59 +0200

    check deployment

--- dataland-frontend/tests/e2e/plugins/index.js
+++ dataland-frontend/tests/e2e/plugins/index.js
@@ -1,8 +1,14 @@
+const gitCommitId = require('git-commit-id')
+
+
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config)
-  return Object.assign({}, config, {
-    fixturesFolder: 'tests/e2e/fixtures',
-    integrationFolder: 'tests/e2e/specs',
-    supportFile: 'tests/e2e/support/index.js'
-  })
+    require('@cypress/code-coverage/task')(on, config)
+    return Object.assign({}, config, {
+        fixturesFolder: 'tests/e2e/fixtures',
+        integrationFolder: 'tests/e2e/specs',
+        supportFile: 'tests/e2e/support/index.js',
+        env: {
+            commit_id: gitCommitId({ cwd: ".."})
+        }
+    })
 }
--- dataland-frontend/tests/e2e/specs/verify_deployment/verify_deployment.ts
+++ dataland-frontend/tests/e2e/specs/verify_deployment/verify_deployment.ts
@@ -1,92 +1,11 @@
-let idList: any
-describe('Population Test', () => {
-    let eutaxonomiesData:any
-    let companiesData:any
-    before(function(){
-        cy.fixture('eutaxonomies').then(function(eutaxonomies){
-            eutaxonomiesData=eutaxonomies
-        });
-        cy.fixture('companies').then(function(companies){
-            companiesData=companies
-        });
-
-    });
-
-    it('Populate Companies', function (){
-        for (const index in companiesData) {
-            cy.request('POST', `${Cypress.env("API")}/companies`, companiesData[index]).its('status').should("equal", 200)
-
-        }
-        console.log(companiesData)
-    });
-
-    it('Populate EU Taxonomy Data', function (){
-        for (const index in eutaxonomiesData) {
-            cy.request('POST', `${Cypress.env("API")}/data/eutaxonomies`, eutaxonomiesData[index]).its('status').should("equal", 200)
-        }
-        console.log(eutaxonomiesData)
-    });
-
-    it('Retrieve data ID list', () => {
-        cy.request('GET', `${Cypress.env("API")}/metadata`).then((response) => {
-            idList = response.body.map(function (e:string){
-                return parseInt(Object.values(e)[0])
-            })
-        })
+describe('Check that the Health Endpoint returns and states "up"', () => {
+    it('retrieve health info and check that its up', function () {
+        cy.request('GET', `${Cypress.env("API")}/actuator/health`).then( healthInfo => {return healthInfo.body.status}).should("equal", "UP")
     });
 });
 
-describe('EU Taxonomy Data', () => {
-    it('Check Data Presence and Link route', () => {
-
-        cy.visit("/data/eutaxonomies/"+idList[0])
-        cy.get('h3').contains("Revenue")
-        cy.get('h3').contains("CapEx")
-        cy.get('h3').contains("OpEx")
-        cy.get('.d-card').should('contain', 'Eligible')
-        cy.get('.d-card .p-progressbar').should('exist')
-    });
-});
-
-describe('Company EU Taxonomy Data', () => {
-    it('Check Data Presence and Link route', () => {
-        cy.visit("/companies/1/eutaxonomies")
-        cy.get('h3').contains("Revenue")
-        cy.get('h3').contains("CapEx")
-        cy.get('h3').contains("OpEx")
-        cy.get('body').contains("Market Cap:")
-        cy.get('body').contains("Headquarter:")
-        cy.get('body').contains("Sector:")
-        cy.get('button.p-button.p-component').contains('Financial and sustainability')
-        cy.get('input[name=eu_taxonomy_search_input]').should('exist')
-    });
-});
-
-describe('Company Data', () => {
-    let companiesData:any
-    before(function(){
-        cy.fixture('companies').then(function(companies){
-            companiesData=companies
-        });
-
-    });
-    it('Company Name Input field exists and works', () => {
-        const inputValue = companiesData[0].companyName
-        cy.visit("/search")
-        cy.get('input[name=companyName]')
-            .should('not.be.disabled')
-            .type(inputValue, {force: true})
-            .should('have.value', inputValue)
-        cy.get('button[name=getCompanies]').click()
-        cy.get('td').contains("VIEW")
-            .contains('a', 'VIEW')
-            .click().url().should('include', '/companies/')
-    });
-
-    it('Show all companies button exists', () => {
-        cy.visit("/search")
-        cy.get('button.p-button').contains('Show all companies')
-            .should('not.be.disabled')
-            .click()
+describe('Check that the correct version is deployed', () => {
+    it('retrieve info endpoint and check commit', function () {
+        cy.request('GET', `${Cypress.env("API")}/actuator/info`).then( info => {return info.body.git.commit.id.full}).should("equal", Cypress.env('commit_id'))
     });
 });
