d162744c8c4762d5311cde6efebdade25d2e9693
Philip Puetsch <philip.puetsch@d-fine.de>
2022-04-13 12:38:56 +0200

    do the plugin differently

--- dataland-frontend/tests/e2e/plugins/index.js
+++ dataland-frontend/tests/e2e/plugins/index.js
@@ -1,8 +1,8 @@
 module.exports = (on, config) => {
     require('@cypress/code-coverage/task')(on, config)
-    return Object.assign({}, config, {
-        fixturesFolder: 'tests/e2e/fixtures',
-        integrationFolder: 'tests/e2e/specs',
-        supportFile: 'tests/e2e/support/index.js',
-    })
+    config.fixturesFolder = 'tests/e2e/fixtures'
+    config.integrationFolder = 'tests/e2e/specs'
+    config.supportFile = 'tests/e2e/support/index.js'
+    config.env.commit_id = 'bla'
+    return config
 }
