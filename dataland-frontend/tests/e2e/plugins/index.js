2753c00cb3b92cc04c5e6741fbf6c66c7916cf6e
Philip Puetsch <philip.puetsch@d-fine.de>
2022-04-13 12:40:31 +0200

    re-add commit-id to cypress env

--- dataland-frontend/tests/e2e/plugins/index.js
+++ dataland-frontend/tests/e2e/plugins/index.js
@@ -3,6 +3,6 @@ module.exports = (on, config) => {
     config.fixturesFolder = 'tests/e2e/fixtures'
     config.integrationFolder = 'tests/e2e/specs'
     config.supportFile = 'tests/e2e/support/index.js'
-    config.env.commit_id = 'bla'
+    config.env.commit_id = require('git-commit-id')({cwd: ".."})
     return config
 }
