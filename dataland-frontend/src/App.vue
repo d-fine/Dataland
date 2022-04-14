57dcf36ee608667f4463cc7ed6b3848af4203cc4
Stephan Sinn <stephan.sinn@d-fine.de>
2022-04-14 15:35:51 +0200

    logo looks good again

--- dataland-frontend/src/App.vue
+++ dataland-frontend/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="d-header grid fixed top-0 col-12 surface-900 align-items-center h-4rem">
-    <div class="col-1 ">
+    <div class="col-6 text-left">
       <router-link to="/">
 
-    <img src="@/assets/images/logos/logo_dataland_long.svg" alt="image" class="pl-3"/>
+    <img src="@/assets/images/logos/logo_dataland_long.svg" alt="image" class="pl-3 pt-2"/>
       </router-link>
 
     </div>
-    <div class="col-1 col-offset-10">
+    <div class="col-1 col-offset-5">
       <i class="material-icons text-white" aria-hidden="true">help_outline</i>
     </div>
   </div>
