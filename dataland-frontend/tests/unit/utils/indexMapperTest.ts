c5b0634b1f3d0aa3f53cb2bc79178f4ff40e822c
Stephan Sinn <stephan.sinn@d-fine.de>
2022-04-14 09:04:19 +0200

    index mapper test

---
+++ dataland-frontend/tests/unit/utils/indexMapperTest.ts
@@ -0,0 +1,9 @@
+import {indexObject} from "@/utils/indexMapper";
+import apiSpecs from "@/../build/clients/backend/backendOpenApi.json";
+describe("index Object ", () => {
+    it("verifies that the parsed indices are correct", () => {
+        const mappedIndices = indexObject()
+        expect(mappedIndices.keys()).toEqual(apiSpecs.components.schemas.CompanyInformation.properties["indices"].items.enum)
+    })
+
+})
