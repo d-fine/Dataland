8328f9e7f03d8fe6b3709536daa10addc110c9ec
ppuetsch <47553592+ppuetsch@users.noreply.github.com>
2022-04-14 16:15:41 +0200

    Dala 80 check that preview is running (#29)
    Added tests to verify deployed software is running and the correct version is deployed

--- dataland-backend/build.gradle.kts
+++ dataland-backend/build.gradle.kts
@@ -26,7 +26,7 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
     implementation("org.dataland:skyminder-client:0.1.2")
     implementation("org.dataland:dataland-edc-client:0.0.2")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
@@ -68,4 +68,8 @@ tasks.test {
 jacoco {
     toolVersion = "0.8.7"
     applyTo(tasks.bootRun.get())
+}
+
+gitProperties {
+    keys = listOf("git.branch", "git.commit.id", "git.commit.time", "git.commit.id.abbrev")
 }
