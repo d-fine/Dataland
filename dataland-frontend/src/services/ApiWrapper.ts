467d1ffa0edc1e2c8e14bbb7fdbef86a7175f877
harjmandi <hamid.arjmandi@d-fine.de>
2022-04-19 17:19:02 +0200

    frontend: add docs for the code. separate methods in StringHumanizer

--- dataland-frontend/src/services/ApiWrapper.ts
+++ dataland-frontend/src/services/ApiWrapper.ts
@@ -1,3 +1,6 @@
+/**
+ * Class to call api functions with axios input option
+ */
 export class ApiWrapper {
     private readonly axiosFunction: any
 
@@ -5,6 +8,11 @@ export class ApiWrapper {
         this.axiosFunction = axiosFunction
     }
 
+    /**
+     * call the function with axios option
+     *
+     * @param  {any} args      the input arguments of the api function
+     */
     perform(...args: any): any {
         return this.axiosFunction(...args, {baseURL: process.env.VUE_APP_BASE_API_URL})
     }
