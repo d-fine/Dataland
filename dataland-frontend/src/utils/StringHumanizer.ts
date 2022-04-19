da75098f25518749b41bafe42494135bbd9da5ad
harjmandi <hamid.arjmandi@d-fine.de>
2022-04-19 16:17:06 +0200

    frontend: delete complex regex für string humanizer

--- dataland-frontend/src/utils/StringHumanizer.ts
+++ dataland-frontend/src/utils/StringHumanizer.ts
@@ -2,7 +2,7 @@ export class StringHumanizer {
 
     private convertCamelCaseToSentenceCase(text: string): string {
         // Split the sting to words
-        const processedText = text.replace(/((?!^)[A-Z][a-z]+|([A-Z]+)$|[0-9]+)/g, " $1")
+        const processedText = text.replace(/([A-Z])/g, " $1")
         // uppercase the first letter of the first word
         return processedText.charAt(0).toUpperCase() + processedText.slice(1)
     }
