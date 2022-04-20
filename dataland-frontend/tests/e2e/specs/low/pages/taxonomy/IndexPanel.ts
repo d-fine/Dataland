26e3ba2835ef274fe1779d3d3564b3be834fd997
Stephan Sinn <stephan.sinn@d-fine.de>
2022-04-20 08:45:00 +0200

    tests for indexpanel and tabmenu

--- dataland-frontend/tests/e2e/specs/low/pages/taxonomy/IndexPanel.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/taxonomy/IndexPanel.ts
@@ -1,14 +1,16 @@
 import apiSpecs from "@/../build/clients/backend/backendOpenApi.json";
 const numberOfStockIndices = apiSpecs.components.schemas.CompanyInformation.properties["indices"].items.enum.length
 describe.only('Index Panel behavior', function () {
+    const indexPanel = '.p-card > .p-card-body > .p-card-content'
+    const tabMenu = '.p-tabmenu > .p-tabmenu-nav > .p-tabmenuitem'
     it('Index panel should be present on first visit and disappear', () => {
         cy.visit("/searchtaxonomy")
         cy.get('.grid')
             .contains('Choose by stock market index')
-        cy.get('.p-card > .p-card-body > .p-card-content')
+        cy.get(indexPanel)
             .should('have.length', numberOfStockIndices)
             .eq(1).click()
-        cy.get('.p-card > .p-card-body > .p-card-content').should('not.exist')
+        cy.get(indexPanel).should('not.exist')
         cy.get('h2')
             .should('contain', "Results")
         cy.get('table.p-datatable-table').should('exist')
@@ -20,11 +22,23 @@ describe.only('Index Panel behavior', function () {
             .click()
             .url().should('include', '/companies/')
             .url().should('include', '/eutaxonomies')
+        cy.get(indexPanel).should('not.exist')
+        cy.get(tabMenu).should('not.exist')
+    });
+
+    it('Index panel should not exist coming from singleton view', () => {
+        cy.get('input[name=eu_taxonomy_search_input]')
+            .should('not.be.disabled')
+            .click()
+            .type("ag{enter}")
+        cy.get(indexPanel).should('not.exist')
+        cy.get(tabMenu).should('exist')
     });
 
     it('Index panel should not be present with params', () => {
         cy.visit("/searchtaxonomy/?input=ag")
-        cy.get('.p-card > .p-card-body > .p-card-content').should('not.exist')
+        cy.get(tabMenu).should('exist')
+        cy.get(indexPanel).should('not.exist')
     })
 
 
--- dataland-frontend/tests/e2e/specs/low/pages/taxonomy/SearchTaxonomy.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/taxonomy/SearchTaxonomy.ts
@@ -20,28 +20,10 @@ describe('Search Taxonomy', function () {
             .invoke('attr', 'placeholder').should('contain', placeholder)
     });
 
-    it('Index panel should be present', () => {
-        cy.visit("/searchtaxonomy")
-        cy.get('.grid')
-            .contains('Choose by stock market index')
-        cy.get('.p-card > .p-card-body > .p-card-content')
-            .should('have.length', numberOfStockIndices)
-            .eq(1).click()
-        cy.get('h2')
-            .should('contain', "Results")
-        cy.get('table.p-datatable-table').should('exist')
-        cy.get('table.p-datatable-table').contains('th','COMPANY')
-        cy.get('table.p-datatable-table').contains('th','SECTOR')
-        cy.get('table.p-datatable-table').contains('th','MARKET CAP')
-        cy.get('table.p-datatable-table').contains('td','VIEW')
-            .contains('a', 'VIEW')
-            .click()
-            .url().should('include', '/companies/')
-            .url().should('include', '/eutaxonomies')
-    });
 
     it('Index tabmenu should be present', () => {
         cy.visit("/searchtaxonomy")
+        cy.get('.p-tabmenu > .p-tabmenu-nav > .p-tabmenuitem').should('not.exist')
         cy.get('.p-card > .p-card-body > .p-card-content')
             .eq(0).click()
         cy.get('.grid')
