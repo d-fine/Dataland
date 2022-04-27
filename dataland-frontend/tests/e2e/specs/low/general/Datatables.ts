6f188bcdeadc9bbfce5c68c58c6e1c53cb06ee74
Stephan Sinn <stephan.sinn@d-fine.de>
2022-04-27 18:22:12 +0200

    pagination is tested

---
+++ dataland-frontend/tests/e2e/specs/low/general/Datatables.ts
@@ -0,0 +1,18 @@
+describe.only('Datables test suite', () => {
+    it('Company Search by Name', () => {
+        cy.visit('/searchtaxonomy')
+        const inputValue = "a"
+        cy.get('input[name=eu_taxonomy_search_input]')
+            .should('not.be.disabled')
+            .click({force:true})
+            .type(inputValue)
+            .type('{enter}')
+            .should('have.value', inputValue)
+        cy.get('h2')
+            .should('contain', "Results")
+        cy.get('table.p-datatable-table').should('exist')
+        cy.get('.p-paginator-current').should('contain.text', "Showing 1 to 100 of").contains('entries')
+    });
+})
+
+
