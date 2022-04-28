4e1bbec0c7df11882f975e9352d6d5949cb89467
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-04-28 13:44:19 +0200

    reverted changes to test data provider

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/TestDataProvider.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/TestDataProvider.kt
@@ -1,6 +1,6 @@
 package org.dataland.datalandbackend
 
-import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.dataland.datalandbackend.model.CompanyInformation
 import org.springframework.beans.factory.annotation.Autowired
@@ -8,8 +8,10 @@ import org.springframework.core.io.ClassPathResource
 
 class TestDataProvider(@Autowired var objectMapper: ObjectMapper) {
     private val jsonFile = ClassPathResource("/CompanyInformation.json").file
-    private val testCompanyInformation: List<CompanyInformation> =
-        objectMapper.readValue(jsonFile, object : TypeReference<List<CompanyInformation>>() {})
+    private val type: JavaType = objectMapper.typeFactory.constructParametricType(
+        List::class.java, CompanyInformation::class.java
+    )
+    private val testCompanyInformation: List<CompanyInformation> = objectMapper.readValue(jsonFile, type)
 
     fun getCompanyInformation(requiredQuantity: Int): List<CompanyInformation> {
         return testCompanyInformation.slice(0 until requiredQuantity)
