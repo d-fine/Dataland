d780de0d947c6522b288e0ff022a986e61795000
Stephan Sinn <stephan.sinn@d-fine.de>
2022-05-10 13:14:25 +0200

    sample section wip

--- dataland-frontend/src/components/pages/WelcomeDataland.vue
+++ dataland-frontend/src/components/pages/WelcomeDataland.vue
@@ -1,12 +1,18 @@
 <template>
   <LandingLogin/>
+  <SampleSection/>
+  <MarketingSection/>
+  <LandingFooter/>
 </template>
 
 <script>
 import LandingLogin from "@/components/resources/landing/LandingLogin.vue"
+import SampleSection from "@/components/resources/landing/SampleSection";
+import MarketingSection from "@/components/resources/landing/MarketingSection";
+import LandingFooter from "@/components/resources/landing/LandingFooter";
 export default {
   name: 'WelcomeDataland',
-  components: {LandingLogin}
+  components: {LandingFooter, MarketingSection, SampleSection, LandingLogin}
 }
 </script>
 
