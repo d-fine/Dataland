8d2ef81e6ffd9d54407d505c510d113b28b0ac43
Stephan Sinn <stephan.sinn@d-fine.de>
2022-05-11 11:06:56 +0200

    styled sample section

--- dataland-frontend/src/components/resources/landing/SampleSection.vue
+++ dataland-frontend/src/components/resources/landing/SampleSection.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="surface-section">
+  <div class="surface-section mt-8">
     <div class="grid">
-      <div class="col-offset-4 col-2">
+      <div class="col-offset-3 col-3">
         <div class="grid">
-          <div class="col-12"><h2>Explore Dataland. No registration required.</h2></div>
-          <div class="col-12"><p>Preview EU Taxonomy Data of <strong>Adidas</strong>, before joining Dataland.</p></div>
-          <div class="col-12">
-            <Button class="bg-white border-primary border-2 text-900 mt-2 mb-3 h-3rem p-0">
-            <span class="uppercase text-primary">
+          <div class="col-10 text-left text-gray-100">
+            <h2 class="mb-0">Explore Dataland</h2>
+            <h2 class="mt-0">No registration required.</h2>
+          </div>
+          <div class="col-10 text-left text-gray-100"><p>Preview EU Taxonomy Data of <strong>Adidas</strong>, before joining Dataland.</p></div>
+          <div class="col-10 text-left p-fluid ">
+            <Button class="bg-white border-primary border-2 text-900 mt-2 mb-3 h-3rem p-0 justify-content-center">
+            <span class="uppercase text-primary d-letters font-semibold">
             EU Taxonomy sample data
           </span>
               <i class="material-icons ml-2 text-primary " aria-hidden="true">chevron_right</i>
@@ -15,7 +18,7 @@
           </div>
         </div>
       </div>
-      <div class="col-2">
+      <div class="col-3">
         <img src="@/assets/images/elements/eu_screenshot.png" alt="Dataland Logo" class="pl-3 pt-2"/>
 
       </div>
