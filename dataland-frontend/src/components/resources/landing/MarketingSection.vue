7e832847249705396673f70f2f96aa6d7e7e1f87
d92048 <stephan.waldow@d-fine.de>
2022-05-11 16:17:43 +0200

    finishes marketing section

--- dataland-frontend/src/components/resources/landing/MarketingSection.vue
+++ dataland-frontend/src/components/resources/landing/MarketingSection.vue
@@ -1,38 +1,61 @@
 <template>
   <div class="grid">
-    <div class = "col-offset-1 pt-5" >
-      <h2>Learn about our vision</h2>
+    <div class="col-5 col-offset-1 pt-5 text-left" >
+      <h2 class="uppercase font-light d-letters">Learn about our vision</h2>
     </div>
-    <div class="col-offset-3 pt-6">
-      <div>
-        <img src="@/assets/images/elements/orange_short_line.svg" alt="short orange line"/>
-      </div>
-      <div class = "col-offset-1 text-left">
-        Our platform enables organisations to get all the data they need by requesting it directly from data owners while
-        giving them a fair compensation.
-      </div>
+    <div class="col-1 pt-6 text-right">
+      <img src="@/assets/images/elements/orange_short_line.svg" alt="short orange line" class="pl-2"/>
+    </div>
+    <div class="col-3 text-left pt-6 font-semibold d-letters">
+      Our platform enables organisations to get all the data they need by requesting it directly from data owners while
+      giving them a fair compensation.
     </div>
    </div>
+
+
   <div class="grid align-items-center col-offset-1 col-10">
-    <img src="@/assets/images/elements/video_placeholder.svg" alt="Showcase Video" class="pt-2 pb-6 w-full"/>
+    <img src="@/assets/images/elements/video_placeholder.svg" alt="Showcase Video" class="pt-6 pb-6 w-full"/>
   </div>
   <div class="grid m-0">
     <div class="col-offset-1 col-5 p-0">
       <img src="@/assets/images/elements/Flow_Diagramm.svg" alt="Flow Diagramm" class="pl-0 w-full"/>
     </div>
-    <div class="col-5 p-0 bg-primary ">
-      <p>fsd</p>
+    <div class="col-5 p-0 bg-primary">
+      <div class="grid mt-6">
+        <div class="col-9 col-offset-1 align-content-center">
+          <h3 class="text-left d-letters">
+            Bring together who needs the data with who owns the data.
+          </h3>
+          <p class="text-left d-letters">
+            Dataland is a permissionless platform for data
+            exchange. We provide a protocol for allowing data users to purchase the data directly from its owners, open
+            to
+            <u></u>se by any organizations.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
   <div class="grid m-0">
-    <div class="col-5 col-offset-1 p-0 surface-700 ">
-      <p>placeholder</p>
+    <div class="col-5 col-offset-1 p-0 surface-700">
+     <div class="grid mt-6">
+       <div class="col-9 col-offset-1 align-content-center">
+         <h3 class="text-white text-left d-letters">
+           Maximize data coverage by requesting data collectively.
+         </h3>
+         <p class="text-white text-left d-letters">
+           Our platform enables organizations to directly request
+           data from companies, customers and suppliers. By joining many other data users requesting data through Dataland,
+           you can obtain data that is unavailable fom data providers and fill all your gaps.
+         </p>
+       </div>
+     </div>
     </div>
-    <div class= "col-5 p-0 ">
+    <div class="col-5 p-0 ">
       <img src="@/assets/images/elements/Data_Workflow.svg" alt="de" class="pl-0 pt-0 w-full"/>
     </div>
   </div>
-  <div class="text-center">
+  <div class="text-center pt-6">
     Dataland is not for you?
     <p class="text-primary text-center underline cursor-pointer">
       Let us know what we can do better.
