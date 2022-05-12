d199f9fd671f2178c1fe779f55e4176501d6bb77
Stephan Sinn <stephan.sinn@d-fine.de>
2022-05-12 08:42:49 +0200

    tests for sample section

--- dataland-frontend/tests/e2e/specs/low/pages/LandingPage.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/LandingPage.ts
@@ -12,20 +12,21 @@ describe('Login Section', () => {
     })
 
 })
-describe('Sample Section', () => {
-    it('Check if App is present', () => {
+describe.only('Sample Section', () => {
+    it('Check that the sample section works properly', () => {
         cy.visit("/")
-        cy.get('#app').should('exist')
-    })
-    it('Logo and welcome message are present', () => {
         cy.get('h2').should("contain.text","Explore Dataland")
-        cy.get('button[name="eu_taxonomy_sample_button"]')
+        cy.get('button[name=eu_taxonomy_sample_button]')
             .should('be.visible')
-            .should('contain','EU TAXONOMY')
+            .should("contain.text","EU Taxonomy")
             .click({force:true})
-            .url().should('include', '/companies/')
-            .url().should('include', '/eutaxonomies')
+            .url().should('include', '/taxonomysample')
         cy.get('h2').contains('EU Taxonomy Data')
+        cy.get('.p-button.p-button-rounded')
+            .should("contain.text","COMPANY DATA SAMPLE")
+        cy.get('body').should('contain.text', 'Join Dataland with others')
+        cy.get('[title=back_button').should('be.visible').click({force:true})
+        cy.get('h1').should("contain.text","CREATE A DATASET")
 
     })
 
