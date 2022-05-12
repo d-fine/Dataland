6cf15f9cf16fa3e350e3e496d90febdb9a89b294
Philip Puetsch <philip.puetsch@d-fine.de>
2022-05-12 09:12:24 +0200

    intercept
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- dataland-frontend/tests/e2e/specs/high/Prepopulation.ts
+++ dataland-frontend/tests/e2e/specs/high/Prepopulation.ts
@@ -116,16 +116,22 @@ describe('Company Data', () => {
 
     });
     it('Company Name Input field exists and works', () => {
+
+
+
         const inputValue = companiesData[0].companyName
         cy.visit("/search")
         cy.get('input[name=companyName]')
             .should('not.be.disabled')
             .type(inputValue, {force: true})
             .should('have.value', inputValue)
+        cy.intercept('**/api/companies/*').as('retrieveCompany')
         cy.get('button[name=getCompanies]').click()
-        cy.get('td').contains("VIEW")
-            .contains('a', 'VIEW')
-            .click().url().should('include', '/companies/')
+        cy.wait('@retrieveCompany', {timeout: 60000}).then(() => {
+            cy.get('td').contains("VIEW")
+                .contains('a', 'VIEW')
+                .click().url().should('include', '/companies/')
+        })
     });
 
     it('Show all companies button exists', () => {
