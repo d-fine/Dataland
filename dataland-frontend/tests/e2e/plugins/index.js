f67cc376b30e5d8bf774d88bbed2fcd7f9eae873
Philip Puetsch <philip.puetsch@d-fine.de>
2022-05-13 12:18:04 +0200

    reading docu helps
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- dataland-frontend/tests/e2e/plugins/index.js
+++ dataland-frontend/tests/e2e/plugins/index.js
@@ -4,13 +4,5 @@ module.exports = (on, config) => {
     config.integrationFolder = 'tests/e2e/specs'
     config.supportFile = 'tests/e2e/support/index.ts'
     config.env.commit_id = require('git-commit-id')({cwd: ".."})
-    on('before:browser:launch', (browser = {}, launchOptions) => {
-        launchOptions.args.push(
-            "--ignore-connections-limit=localhost:8090,proxy," +
-            "localhost,preview-dataland.duckdns.org,dev-dataland.duckdns.org"
-        )
-        console.log(launchOptions.args)
-        return launchOptions
-    })
     return config
 }
