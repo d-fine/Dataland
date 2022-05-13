ac77321025cff1414f94ba5c18e7d75399b41744
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-13 16:56:47 +0200

    Everything is read for Go-Live to new Deployment-strategy

--- deployment/start_and_deploy_to_server.sh
+++ deployment/start_and_deploy_to_server.sh
@@ -3,27 +3,11 @@ set -u
 
 environment=$1
 
-preview_server_url="preview-dataland.duckdns.org"
-preview_server_host_keys="$preview_server_url ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNGocXXehCSfKoYwGdaYUpjvNm7gZE2LS7Nl/gGGXSxqwbGT+X6b+q7AGwhwZpFY9u17wv4NY3EOCK1cGaeot4k="
-preview_server_startup_url=$PREVIEW_STARTUP_URL
-
-dev_server_url="dev-dataland.duckdns.org"
-dev_server_host_keys="$dev_server_url ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHcwEGUE3yhkWxJ/dwafmNrVZDMZn62o26CoNo4ScNgwEaAxfeHDddpROrghaZ/avibYmzAAU8bwR76QG01v2RI="
-dev_server_startup_url=$DEV_STARTUP_URL
-
-if [[ $environment == preview ]]; then
-  echo "Starting preview server"
-  curl $preview_server_startup_url > /dev/null
-  echo "Setting preview server as deployment target"
-  target_server_url=$preview_server_url
-  target_server_host_keys=$preview_server_host_keys
-elif [[ $environment == development ]]; then
-  echo "Starting development server"
-  curl $dev_server_startup_url > /dev/null
-  echo "Setting dev server as deployment target"
-  target_server_url=$dev_server_url
-  target_server_host_keys=$dev_server_host_keys
-fi
+echo "Starting ${environment} server"
+curl "${TARGETSERVER_STARTUP_URL}" > /dev/null
+echo "Setting ${environment} server as deployment target"
+target_server_url="${TARGETSERVER_URL}"
+target_server_host_keys="${TARGETSERVER_HOST_KEYS}"
 
 mkdir -p ~/.ssh/
 echo "$target_server_host_keys" >  ~/.ssh/known_hosts
@@ -39,7 +23,7 @@ ssh ubuntu@$target_server_url "cd $location && sudo docker-compose down"
 ssh ubuntu@$target_server_url 'sudo docker kill $(sudo docker ps -q); sudo docker system prune --force; sudo docker info'
 ssh ubuntu@$target_server_url "sudo rm -rf $location; mkdir -p $location/jar"
 
-envsubst < "environments/.env.${environment}" > .env
+envsubst < "environments/.env.template" > .env
 
 scp ./.env ubuntu@$target_server_url:$location
 scp -r ./dataland-frontend/dist ./docker-compose.yml ./dataland-inbound-proxy/ ./dataland-frontend/default.conf ubuntu@$target_server_url:$location
