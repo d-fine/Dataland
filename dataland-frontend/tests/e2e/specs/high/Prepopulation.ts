25c1cd58854f3adbbf7052cc1a8c4be39aa88562
harjmandi <hamid.arjmandi@d-fine.de>
2022-05-13 10:16:39 +0200

    fix upload cypress test

--- dataland-frontend/tests/e2e/specs/high/Prepopulation.ts
+++ dataland-frontend/tests/e2e/specs/high/Prepopulation.ts
@@ -4,9 +4,6 @@ describe('Population Test',  () => {
     })
 
     let companiesWithData: any
-    let eutaxonomiesData: any
-    let companiesData: any
-    const companyAssociatedEuTaxonomyData: any = []
 
     before(function () {
         cy.fixture('CompanyInformationWithEuTaxonomyData').then(function (companies) {
@@ -14,72 +11,64 @@ describe('Population Test',  () => {
         });
     });
 
-    async function uploadData(dataArray: Array<object>, endpoint: string) {
-        console.time(`The elapsed time to upload ${dataArray.length} items for '${endpoint}'`)
-        const chunkSize = 80;
-        for (let i = 0; i < dataArray.length; i += chunkSize) {
-            const chunk = dataArray.slice(i, i + chunkSize);
-            await Promise.all(chunk.map(async (element: object) => {
-                    await fetch(`${Cypress.env("API")}/${endpoint}`, {
+    async function uploadCompanyWithData(companiesWithData: Array<object>) {
+        console.time(`The elapsed time to upload ${companiesWithData.length} companies with data`)
+        const chunkSize = 50;
+        for (let i = 0; i < companiesWithData.length; i += chunkSize) {
+            const chunk = companiesWithData.slice(i, i + chunkSize);
+            await Promise.all(chunk.map(async (element: any) => {
+                    await fetch(`${Cypress.env("API")}/companies`, {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
                         },
-                        body: JSON.stringify(element)
-                    }).then(response => {
+                        body: JSON.stringify(element["companyInformation"])
+                    }).then( async (response) => {
                         assert(response.status.toString() === "200",
-                            `Got status code ${response.status.toString()} for index ${i}. Expected: 200`)
+                            `Got status code ${response.status.toString()} for companyInformation index ${i}. Expected: 200`)
+                        const euTaxonomyData = element["euTaxonomyData"]
+                        const data  = await response.json()
+                        euTaxonomyData["companyId"] = data.companyId
+                        console.log(euTaxonomyData)
+                        await fetch(`${Cypress.env("API")}/data/eutaxonomies`, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(euTaxonomyData)
+                        }).then(response => {
+                            assert(response.status.toString() === "200",
+                                `Got status code ${response.status.toString()} for euTaxonomyData index ${i}. Expected: 200`)
+                        })
                     })
                 })
             )
         }
-        console.timeEnd(`The elapsed time to upload ${dataArray.length} items for '${endpoint}'`)
+        console.timeEnd(`The elapsed time to upload ${companiesWithData.length} companies with data`)
     }
 
 
-    it('Populate Companies', async () => {
-        companiesData = companiesWithData.map((company:any) => {
-            return company["companyInformation"]
-        })
-        eutaxonomiesData = companiesWithData.map((company:any) => {
-            return company["euTaxonomyData"]
-        })
-        await uploadData(companiesData, "companies")
+    it.only('Populate Companies with Data', async () => {
+        await uploadCompanyWithData(companiesWithData)
     });
 
     it('Check if all the company ids can be retrieved', () => {
-        cy.retrieveCompanyIdsList().then((companyIdList: any) => {
-            assert(companyIdList.length >= companiesData.length, // >= to avoid problem with several runs in a row
-                `Uploaded ${companyIdList.length} out of ${companiesData.length} companies`)
-            for (const companyIdIndex in companyIdList) {
-                const companyId = companyIdList[companyIdIndex]
-                assert(typeof companyId !== 'undefined',
-                    `Validation of company number ${companyIdIndex}`)
-                if (typeof eutaxonomiesData[companyIdIndex] == "object") {
-                    companyAssociatedEuTaxonomyData.push({"companyId": companyId, "data": eutaxonomiesData[companyIdIndex]})
-                }
-            }
+        cy.retrieveCompanyIdsList().then((companyIdList: Array<any>) => {
+            assert(companyIdList.length >= companiesWithData.length, // >= to avoid problem with several runs in a row
+                `Uploaded ${companyIdList.length} out of ${companiesWithData.length} companies`)
+            assert(companyIdList.every(companyId => typeof companyId === "string"), "Validation of company Ids")
         })
-    });
-
-    it('Populate EU Taxonomy Data', async () => {
-        await uploadData(companyAssociatedEuTaxonomyData, "data/eutaxonomies")
     });
 
     it('Check if all the data ids can be retrieved', () => {
-        cy.retrieveDataIdsList().then((dataIdList: any) => {
-            assert(dataIdList.length >= eutaxonomiesData.length, // >= to avoid problem with several runs in a row
-                `Uploaded ${dataIdList.length} out of ${eutaxonomiesData.length} data`)
-            for (const dataIdIndex in dataIdList) {
-                assert(typeof dataIdList[dataIdIndex] !== 'undefined',
-                    `Validation of data number ${dataIdIndex}`)
-            }
+        cy.retrieveDataIdsList().then((dataIdList: Array<any>) => {
+            assert(dataIdList.length >= companiesWithData.length, // >= to avoid problem with several runs in a row
+                `Uploaded ${dataIdList.length} out of ${companiesWithData.length} data`)
+            assert(dataIdList.every(dataId => typeof dataId === "string"), "Validation of data Ids")
         })
     });
-});
 
-describe('EU Taxonomy Data', () => {
-    it('Check Data Presence and Link route', () => {
+    it('Check Data Presence and Link route for EU Taxonomy Data', () => {
         cy.retrieveDataIdsList().then((dataIdList: Array<string>) => {
             cy.visit("/data/eutaxonomies/" + dataIdList[0])
             cy.get('h3', { timeout: 60000 }).should('be.visible')
@@ -90,10 +79,8 @@ describe('EU Taxonomy Data', () => {
             cy.get('.d-card .p-progressbar').should('exist')
         });
     })
-});
 
-describe('Company EU Taxonomy Data', () => {
-    it('Check Data Presence and Link route', () => {
+    it('Check Data Presence and Link route for companies with eutaxonomies data', () => {
         cy.retrieveCompanyIdsList().then((companyIdList: Array<string>) => {
             cy.visit(`/companies/${companyIdList[0]}/eutaxonomies`)
             cy.get('h3', { timeout: 60000 }).should('be.visible')
@@ -108,18 +95,10 @@ describe('Company EU Taxonomy Data', () => {
             cy.get('input[name=eu_taxonomy_search_input]').should('exist')
         })
     });
-});
 
-describe('Company Data', () => {
-    let companiesData: any
-    before(function () {
-        cy.fixture('CompanyInformation').then(function (companies) {
-            companiesData = companies
-        });
 
-    });
-    it('Company Name Input field exists and works', () => {
-        const inputValue = companiesData[0].companyName
+    it.skip('Company Name Input field exists and works after population', () => {
+        const inputValue = companiesWithData[0].companyInformation.companyName
         cy.visit("/search")
         cy.get('input[name=companyName]')
             .should('not.be.disabled')
@@ -134,7 +113,7 @@ describe('Company Data', () => {
         })
     });
 
-    it('Show all companies button exists', () => {
+    it('Show all companies button exists after population', () => {
         cy.visit("/search")
         cy.get('button.p-button').contains('Show all companies')
             .should('not.be.disabled')
