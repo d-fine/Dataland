67e39f788ff69ff1fdeab826e7369b8a6a5b87c1
Philip Puetsch <philip.puetsch@d-fine.de>
2022-05-19 09:12:52 +0200

    increase timeout for previsit for real dataset
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- dataland-frontend/tests/e2e/specs/previsit/Previsit.ts
+++ dataland-frontend/tests/e2e/specs/previsit/Previsit.ts
@@ -1,9 +1,10 @@
 import {doThingsInChunks} from "../../support/utility";
+
 const chunkSize = 40
 
 describe('Visit all EuTaxonomy Data', () => {
     it('Visit all EuTaxonomy Data', () => {
-        cy.retrieveDataIdsList().then({timeout: 600 * 1000}, async (dataIdList) => {
+        cy.retrieveDataIdsList().then({timeout: 20 * 60 * 1000}, async (dataIdList) => {
             await doThingsInChunks(
                 dataIdList,
                 chunkSize,
