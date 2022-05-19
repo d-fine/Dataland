7b910e3020b9227f7e184627dd45156d2d59bdcf
d92048 <stephan.waldow@d-fine.de>
2022-05-19 11:36:58 +0200

    adjusts grid alignment

--- dataland-frontend/src/components/resources/taxonomy/TaxoInfoCard.vue
+++ dataland-frontend/src/components/resources/taxonomy/TaxoInfoCard.vue
@@ -2,19 +2,19 @@
   <Card class="bg-white d-infocard d-card mr-2" >
     <template #title></template>
     <template #content>
-      <div class="grid " v-tooltip.top="tooltipText ? {
-              value: tooltipText,
-              class: 'd-tooltip-mw25'
-            } : '' ">
-        <div class="col-12 text-left">
+      <div class="grid ">
+        <div class="col-6 text-left">
           <strong>{{ title }}</strong>
         </div>
-        <div class="col-12 text-left">
-          <span>{{value}}</span><i class="col-offset-10 material-icons" aria-hidden="true" v-tooltip.top="tooltipText ? {
+        <div class="col-6 text-left">
+          <div class="col-offset-10 material-icons" aria-hidden="true" v-tooltip.top="tooltipText ? {
                 value: tooltipText,
                 class: 'd-tooltip-mw25'
               } : '' ">info
-        </i>
+          </div>
+        </div>
+        <div class="col-12 text-left">
+          <span>{{value}}</span>
         </div>
       </div>
     </template>
