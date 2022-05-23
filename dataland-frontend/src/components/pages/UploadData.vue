f270d736c006c8b85edef1ac1374a8b0175aa8f5
stewal19 <104447806+stewal19@users.noreply.github.com>
2022-05-23 17:28:59 +0200

    Dala 5 landing page (#42)
    Introduction of new landing page
    
    Co-authored-by: Stephan Sinn <stephan.sinn@d-fine.de>
    Co-authored-by: SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>

--- dataland-frontend/src/components/pages/UploadData.vue
+++ dataland-frontend/src/components/pages/UploadData.vue
@@ -1,17 +1,24 @@
 <template>
+  <TheHeader/>
+  <TheContent>
   <div class="grid">
     <CreateCompany/>
     <CreateEUTaxonomy/>
   </div>
+  </TheContent>
 </template>
 
 <script>
 import CreateCompany from "@/components/forms/CreateCompany"
 import CreateEUTaxonomy from "@/components/forms/CreateEUTaxonomy";
+import TheHeader from "@/components/structure/TheHeader";
+import TheContent from "@/components/structure/TheContent";
 
 export default {
   name: "CreateData",
   components: {
+    TheContent,
+    TheHeader,
     CreateEUTaxonomy,
     CreateCompany
   }
