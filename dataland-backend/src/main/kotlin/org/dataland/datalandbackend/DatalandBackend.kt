a94a5b13a3c53c827ef0b3e8d38e098a4c2d2e1c
Philip Puetsch <philip.puetsch@d-fine.de>
2022-05-24 10:57:31 +0200

    add auth to swagger-ui
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/DatalandBackend.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/DatalandBackend.kt
@@ -1,5 +1,10 @@
 package org.dataland.datalandbackend
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,6 +12,8 @@ import org.springframework.boot.runApplication
  * Class to define the spring boot application
  */
 @SpringBootApplication
+@OpenAPIDefinition(info = Info(title = "Dataland API", version = "0.0.1", description = "The API for Dataland"))
+@SecurityScheme(name = "default-auth", scheme = "bearer", type = SecuritySchemeType.HTTP, `in` = SecuritySchemeIn.HEADER)
 class DatalandBackend
 
 /**
