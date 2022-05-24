81218d6823aff6a118eb1935a2a375dd615034ab
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-24 09:52:54 +0200

    "Adidas AG" is now public

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/Constants.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/Constants.kt
@@ -2,13 +2,22 @@ package org.dataland.datalandbackend
 
 import org.dataland.datalandbackend.interfaces.DataManagerInterface
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 
 const val RATIO_PRECISION = 4
 
-val PREVIEWCOMPANY_NAME = System.getenv("PREVIEWCOMPANY_NAME")
+val PREVIEWCOMPANY_NAME = "Adidas AG"//System.getenv("PREVIEWCOMPANY_NAME")
 
+@Component("PreviewStuff")
 class PreviewStuff(
     @Autowired var dataManager: DataManagerInterface,
 ) {
-    val previewCompanyId = "1"//dataManager.searchCompanies(PREVIEWCOMPANY_NAME, true).last().companyId
+    fun isCompanyPublic(companyId:String) : Boolean {
+        val searchResult = dataManager.searchCompanies(PREVIEWCOMPANY_NAME, true)
+        return if (searchResult.isEmpty()) {
+            false
+        }
+        //else if {searchResult.size > 1}
+        else companyId == searchResult.last().companyId
+    }
 }
