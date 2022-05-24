d04e05c0b941ab9cc3931f342cb64a8fb7fb3701
Stephan Sinn <stephan.sinn@d-fine.de>
2022-05-25 00:57:23 +0200

    unit tests also passed

--- dataland-frontend/tests/unit/utils/orderOfMagnitudeFormatterTest.spec.ts
+++ dataland-frontend/tests/unit/utils/orderOfMagnitudeFormatterTest.spec.ts
@@ -2,11 +2,11 @@ import {numberFormatter} from "@/utils/currencyMagnitude";
 describe("nFormatter", () => {
     it("verifies whether the implementation works correctly", () => {
         expect(numberFormatter(0, 1)).toEqual("0")
-        expect(numberFormatter(1234, 1)).toEqual("1.2 k")
-        expect(numberFormatter(100000000, 1)).toEqual("100 m")
-        expect(numberFormatter(299792458, 2)).toEqual("299.79 m")
-        expect(numberFormatter(3299792458, 2)).toEqual("3.3 b")
-        expect(numberFormatter(3293792458, 2)).toEqual("3.29 b")
+        expect(numberFormatter(1234, 1)).toEqual("1.2 K")
+        expect(numberFormatter(100000000, 1)).toEqual("100 M")
+        expect(numberFormatter(299792458, 2)).toEqual("299.79 M")
+        expect(numberFormatter(3299792458, 2)).toEqual("3.3 B")
+        expect(numberFormatter(3293792458, 2)).toEqual("3.29 B")
     })
 
 })
