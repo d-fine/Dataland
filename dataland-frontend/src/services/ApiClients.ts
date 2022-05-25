d6965add06cf00673a14080eb5298765afce7e8d
Philip Puetsch <philip.puetsch@d-fine.de>
2022-05-25 13:53:05 +0200

    improve apiclients
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

---
+++ dataland-frontend/src/services/ApiClients.ts
@@ -0,0 +1,32 @@
+import {Configuration} from "@/../build/clients/backend/configuration"
+import {CompanyDataControllerApi, EuTaxonomyDataControllerApi} from "@/../build/clients/backend/api"
+
+console.log(process.env.VUE_APP_BASE_API_URL )
+export const apiClients = new Configuration({
+    accessToken: () => window.sessionStorage.getItem('keycloakToken') ?? "undefined",
+    basePath: process.env.VUE_APP_BASE_API_URL + "/api"
+
+})
+console.log(apiClients.basePath)
+
+function getConfiguration() {
+    const keyCloakToken = window.sessionStorage.getItem('keycloakToken')
+    if (keyCloakToken) {
+        return new Configuration({accessToken: keyCloakToken})
+    }
+    else {
+        return undefined
+    }
+}
+
+function getConstructedApi<T>(constructor: new (configuration: Configuration | undefined, basePath: string) => T): T {
+    return new constructor(getConfiguration(), process.env.VUE_APP_BASE_API_URL + "/api")
+}
+
+export function getCompanyDataControllerApi() {
+    return getConstructedApi(CompanyDataControllerApi)
+}
+
+export function getEuTaxonomyDataControllerApi() {
+    return getConstructedApi(EuTaxonomyDataControllerApi)
+}
--- dataland-frontend/src/services/AxiosDefaultConfiguration.ts
+++
@@ -1,9 +0,0 @@
-import {Configuration} from "@/../build/clients/backend/configuration"
-
-console.log(process.env.VUE_APP_BASE_API_URL )
-export const axiosDefaultConfiguration = new Configuration({
-    accessToken: () => window.sessionStorage.getItem('keycloakToken') ?? "undefined",
-    basePath: process.env.VUE_APP_BASE_API_URL + "/api"
-
-})
-console.log(axiosDefaultConfiguration.basePath)
