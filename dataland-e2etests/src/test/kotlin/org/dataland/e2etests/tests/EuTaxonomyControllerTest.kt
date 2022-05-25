db5d8214ac5d93ca128c886be0094bdef671ccbc
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-25 15:22:10 +0200

    e2e tests are green now

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/EuTaxonomyControllerTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/EuTaxonomyControllerTest.kt
@@ -1,9 +1,12 @@
-package org.dataland.e2etests
+package org.dataland.e2etests.tests
 
 import org.dataland.datalandbackend.openApiClient.api.CompanyDataControllerApi
 import org.dataland.datalandbackend.openApiClient.api.EuTaxonomyDataControllerApi
-import org.dataland.datalandbackend.openApiClient.infrastructure.ApiClient
 import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataEuTaxonomyData
+import org.dataland.e2etests.BASE_PATH_TO_DATALAND_PROXY
+import org.dataland.e2etests.TestDataProvider
+import org.dataland.e2etests.acessmanagement.TokenRequester
+import org.dataland.e2etests.acessmanagement.UserType
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -11,15 +14,13 @@ class EuTaxonomyControllerTest {
     private val companyDataControllerApi = CompanyDataControllerApi(BASE_PATH_TO_DATALAND_PROXY)
     private val euTaxonomyDataControllerApi = EuTaxonomyDataControllerApi(BASE_PATH_TO_DATALAND_PROXY)
     private val testDataProvider = TestDataProvider()
-
-    init {
-        ApiClient.Companion.accessToken = "TODO: Add Access Token here!"
-    }
+    private val tokenRequester = TokenRequester()
 
     @Test
     fun `post a dummy company and a dummy data set for it and check if that dummy data set can be retrieved`() {
         val testCompanyInformation = testDataProvider.getCompanyInformation(1).first()
         val testData = testDataProvider.getEuTaxonomyData(1).first()
+        tokenRequester.requestTokenForUserType(UserType.Admin).setToken()
         val testCompanyId = companyDataControllerApi.postCompany(testCompanyInformation).companyId
         val testDataId = euTaxonomyDataControllerApi.postCompanyAssociatedData(
             CompanyAssociatedDataEuTaxonomyData(testCompanyId, testData)
--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/SkyminderControllerTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/SkyminderControllerTest.kt
@@ -1,19 +1,19 @@
-package org.dataland.e2etests
+package org.dataland.e2etests.tests
 
 import org.dataland.datalandbackend.openApiClient.api.SkyminderControllerApi
-import org.dataland.datalandbackend.openApiClient.infrastructure.ApiClient
+import org.dataland.e2etests.BASE_PATH_TO_DATALAND_PROXY
+import org.dataland.e2etests.acessmanagement.TokenRequester
+import org.dataland.e2etests.acessmanagement.UserType
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class SkyminderControllerTest {
     private val skyminderControllerApi = SkyminderControllerApi(BASE_PATH_TO_DATALAND_PROXY)
-
-    init {
-        ApiClient.Companion.accessToken = "TODO: Add Access Token here!"
-    }
+    private val tokenRequester = TokenRequester()
 
     @Test
     fun `get dummy company data by sending a request to dummy skyminder server`() {
+        tokenRequester.requestTokenForUserType(UserType.SomeUser).setToken()
         assertTrue(
             skyminderControllerApi.getDataSkyminderRequest("dummy", "dummy").isNotEmpty(),
             "The dummy skyminder server is returning an empty response."
