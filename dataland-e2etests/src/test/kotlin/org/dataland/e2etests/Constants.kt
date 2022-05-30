a9f5c8945c1da469067369ff68c63567f706a9eb
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-30 17:18:17 +0200

    Fix PATH_TOKEYCLOAK in e2etest

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/Constants.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/Constants.kt
@@ -2,7 +2,7 @@ package org.dataland.e2etests
 
 const val BASE_PATH_TO_DATALAND_PROXY = "http://proxy:80/api"
 
-const val PATH_TO_KEYCLOAK_TOKENENDPOINT = "http://keycloak:8080/realms/datalandsecurity/protocol/openid-connect/token"
+const val PATH_TO_KEYCLOAK_TOKENENDPOINT = "http://proxy:80/keycloak/realms/datalandsecurity/protocol/openid-connect/token"
 
 const val TOKENREQUEST_GRANT_TYPE = "password"
 const val TOKENREQUEST_CLIENT_ID = "public"
