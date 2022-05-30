8777eb71715a57ef9b83cd7894fe58c94e51e3fc
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-30 17:47:50 +0200

    WiP

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/EuTaxonomyControllerTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/EuTaxonomyControllerTest.kt
@@ -5,10 +5,12 @@ import org.dataland.datalandbackend.openApiClient.api.EuTaxonomyDataControllerAp
 import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataEuTaxonomyData
 import org.dataland.e2etests.BASE_PATH_TO_DATALAND_PROXY
 import org.dataland.e2etests.TestDataProvider
+import org.dataland.e2etests.accessmanagement.Token
 import org.dataland.e2etests.accessmanagement.TokenRequester
 import org.dataland.e2etests.accessmanagement.UserType
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class EuTaxonomyControllerTest {
     private val companyDataControllerApi = CompanyDataControllerApi(BASE_PATH_TO_DATALAND_PROXY)
@@ -35,4 +37,30 @@ class EuTaxonomyControllerTest {
             "The posted and the received eu taxonomy data sets and their company IDs are not equal."
         )
     }
+/*
+    fun `post a dummy company and a dummy data set for it and test if access to it is denied if no token is passed`() {
+        val testCompanyInformation = testDataProvider.getCompanyInformation(1).first()
+        val testData = testDataProvider.getEuTaxonomyData(1).first()
+        Token("").setToken()
+        val testCompanyId = companyDataControllerApi.postCompany(testCompanyInformation).companyId
+        val testDataId = euTaxonomyDataControllerApi.postCompanyAssociatedData(
+            CompanyAssociatedDataEuTaxonomyData(testCompanyId, testData)
+        ).dataId
+
+       //Throw unauthorized exception
+
+        assertThrows<IllegalArgumentException> {
+            euTaxonomyDataControllerApi.getCompanyAssociatedData(testDataId)
+        }
+    }*/
+
+    /*fun `post the teaser company and a dummy data set for it and test if access is possible even without token`() {
+        assertEquals(
+            "a",
+            "a",
+            "The posted and the received eu taxonomy data sets and their company IDs are not equal."
+        )
+    }
+
+ */
 }
