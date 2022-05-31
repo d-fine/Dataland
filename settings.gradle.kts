d18719738e973d9fd7cca84670569e396a55bd11
d92048 <stephan.waldow@d-fine.de>
2022-05-31 17:25:34 +0200

    fixes a type

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,7 +33,7 @@ dependencyResolutionManagement {
             library("slf4j-api", "org.slf4j:slf4j-api:1.7.36")
 
             library("jackson-dataformat-csv", "com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.13.3")
-            library("jackson-databind", "com.fasterxml.jackson.core:jackson-databind:2.13.2.3")
+            library("jackson-databind", "com.fasterxml.jackson.core:jackson-databind:2.13.3")
             library("jackson-datatype-jsr310", "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
         }
     }
