0af62d7c2f9b79dca6f6e3e7b4a609537332527b
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-05-31 13:47:50 +0200

    Some more detekt fixes

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/configurations/KeycloakConfig.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/configurations/KeycloakConfig.kt
@@ -4,9 +4,17 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+/**
+ * Keycloak Config Resolver for Spring Boot
+ */
 @Configuration
 class KeycloakConfig {
     @Bean
+
+    /**
+     * Method to return a Keycloak Spring Boot Resolver object
+     * @return Keycloak Config Resolver for Spring Boot
+     */
     fun keycloakConfigResolver(): KeycloakSpringBootConfigResolver? {
         return KeycloakSpringBootConfigResolver()
     }
