4d17dde15b6648e9c085748af2a7118b5649cc01
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-06-08 14:06:36 +0200

    add keycloak to url

--- testing/keycloak_local_test.sh
+++ testing/keycloak_local_test.sh
@@ -4,7 +4,7 @@
 # in case of teaser data).
 
 
-export keycloak_openid_token_endpoint="http://keycloak:8080/realms/datalandsecurity/protocol/openid-connect/token"
+export keycloak_openid_token_endpoint="http://keycloak:8080/keycloak/realms/datalandsecurity/protocol/openid-connect/token"
 export backend_url="http://backend:8080/api"
 
 export user_name="some_user"
