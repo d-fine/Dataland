19f2a9ecc9a04a2029d3d7adcd02850b80fecffe
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-06-14 17:55:07 +0200

    Cleanup ENVs which are no longer needed in code and CI/CD

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/Constants.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/Constants.kt
@@ -1,6 +1,3 @@
 package org.dataland.datalandbackend
 
 const val RATIO_PRECISION = 4
-
-val REALDATA: Boolean = System.getenv("REALDATA").toBoolean()
-val REAL_TEASER_COMPANY_PERM_ID: String = System.getenv("REAL_TEASER_COMPANY_PERM_ID")
