be445bc15263ce94feccd4823176e13765d0bcd1
d92048 <stephan.waldow@d-fine.de>
2022-06-15 15:21:46 +0200

    removes scrollbar on iframe

--- dataland-frontend/src/components/general/UserAuthenticationButtons.vue
+++ dataland-frontend/src/components/general/UserAuthenticationButtons.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="col-12" >
     <Button v-if="!authenticated" label="Join" class="d-letters d-button uppercase p-button p-button-sm justify-content-center w-5rem" name="join_dataland_button" @click="openRegister" />
-    <Dialog v-model:visible="displayRegister" class="col-4 lg:pt-0" :modal="true" :showHeader="false" :dismissableMask="true">
+    <Dialog v-model:visible="displayRegister" class="col-3 lg:pt-0" :modal="true" :showHeader="false" :dismissableMask="true">
       <p class="m-0"> <KeyCloakIframe url="/register"/> </p>
     </Dialog>
     <Button v-if="!authenticated" label="Login" class="uppercase p-button p-button-sm d-letters text-primary d-button justify-content-center bg-white-alpha-10 w-5rem ml-4" name="login_dataland_button" @click="openLogin" />
-    <Dialog v-model:visible="displayLogin" class="col-4 lg:pt-0" :modal="true" :showHeader="false" :dismissableMask="true">
+    <Dialog v-model:visible="displayLogin" class="col-3 lg:pt-0" :modal="true" :showHeader="false" :dismissableMask="true">
       <p class="m-0"> <KeyCloakIframe url="/login"/> </p>
     </Dialog>
     <Button v-if="authenticated" label="Logout" class="uppercase p-button p-button-sm d-letters text-primary d-button justify-content-center bg-white-alpha-10 w-5rem ml-4" name="login_dataland_button" @click="openLogout" />
