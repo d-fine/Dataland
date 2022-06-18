d55c13e15964879c85a6f45692d22a34cba67128
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-06-18 16:02:43 +0200

    add docker log files to artifacts

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -12,6 +12,13 @@ docker exec dala-e2e-test_backend_1 pkill -f spring
 timeout 90 sh -c "docker logs dala-e2e-test_backend_1 --follow"
 docker cp dala-e2e-test_backend_1:/app/dataland-backend/build/jacoco/bootRun.exec .
 
+# Write the logs of the docker container for later upload and analysis
+mkdir -p ./dockerLogs
+for docker_service in $(sudo docker ps --format "{{.Names}}");
+do
+  sudo docker logs "$docker_service" > ./dockerLogs/"$docker_service".log 2>&1
+done
+
 # Check execution success of Test Container
 TEST_EXIT_CODE=`docker inspect -f '{{.State.ExitCode}}' dala-e2e-test_e2etests_1`
 echo "Docker E2E Testcontainer exited with code $TEST_EXIT_CODE"
