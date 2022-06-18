10cc9a4ea37ce7628e84eed17ff6481a7a7247bb
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-06-18 16:23:14 +0200

    fix for e2e tests and write all container logs

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -14,7 +14,7 @@ docker cp dala-e2e-test_backend_1:/app/dataland-backend/build/jacoco/bootRun.exe
 
 # Write the logs of the docker container for later upload and analysis
 mkdir -p ./dockerLogs
-for docker_service in $(sudo docker ps --format "{{.Names}}");
+for docker_service in $(sudo docker ps --all --format "{{.Names}}");
 do
   sudo docker logs "$docker_service" > ./dockerLogs/"$docker_service".log 2>&1
 done
