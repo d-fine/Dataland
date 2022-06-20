d0059e00f9afa8788f3f6073b58dad3af9784943
Philip Puetsch <philip.puetsch@d-fine.de>
2022-06-20 14:46:42 +0200

    more debugging
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -14,9 +14,10 @@ docker cp dala-e2e-test_backend_1:/app/dataland-backend/build/jacoco/bootRun.exe
 
 # Write the logs of the docker container for later upload and analysis
 mkdir -p ./dockerLogs
+docker ps -a > ./dockerLogs/ps.log
 for docker_service in $(sudo docker ps --all --format "{{.Names}}");
 do
-  sudo docker logs "$docker_service" > ./dockerLogs/"$docker_service".log 2>&1
+  docker logs "$docker_service" > ./dockerLogs/"$docker_service".log 2>&1
 done
 
 # Check execution success of Test Container
