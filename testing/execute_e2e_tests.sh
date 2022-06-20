34a29950742c27f100d33ac795ec6d49e1f18682
Philip Puetsch <philip.puetsch@d-fine.de>
2022-06-20 14:56:06 +0200

    more debugging
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -x
 #Start E2E Test and wait for E2E Test completion
-docker-compose --project-name dala-e2e-test --profile testing up -d --build
+docker-compose --project-name dala-e2e-test --profile testing up -d --build || exit
 timeout 2400 sh -c "docker logs dala-e2e-test_e2etests_1 --follow"
+E2ETEST_TIMEOUT_EXIT_CODE=$?
 docker cp dala-e2e-test_e2etests_1:/app/dataland-frontend/coverage/e2e/lcov.info .
 docker cp dala-e2e-test_e2etests_1:/app/dataland-frontend/cypress/ .
 docker cp dala-e2e-test_e2etests_1:/app/dataland-e2etests/build/reports/ .
@@ -10,6 +11,7 @@ docker cp dala-e2e-test_e2etests_1:/app/dataland-e2etests/build/reports/ .
 # Stop Backend causing JaCoCo to write Coverage Report, get it to pwd
 docker exec dala-e2e-test_backend_1 pkill -f spring
 timeout 90 sh -c "docker logs dala-e2e-test_backend_1 --follow"
+BACKEND_TIMEOUT_EXIT_CODE=$?
 docker cp dala-e2e-test_backend_1:/app/dataland-backend/build/jacoco/bootRun.exec .
 
 # Write the logs of the docker container for later upload and analysis
@@ -22,5 +24,7 @@ done
 
 # Check execution success of Test Container
 TEST_EXIT_CODE=`docker inspect -f '{{.State.ExitCode}}' dala-e2e-test_e2etests_1`
+echo "E2ETEST Timeout exited with exit code $E2ETEST_TIMEOUT_EXIT_CODE"
+echo "BACKEND Timeout exited with exit code $BACKEND_TIMEOUT_EXIT_CODE"
 echo "Docker E2E Testcontainer exited with code $TEST_EXIT_CODE"
-exit $TEST_EXIT_CODE
+exit $((TEST_EXIT_CODE+E2ETEST_TIMEOUT_EXIT_CODE+BACKEND_TIMEOUT_EXIT_CODE))
