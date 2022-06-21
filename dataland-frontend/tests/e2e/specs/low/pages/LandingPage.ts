a3c1a22bd9bb0cf008d30ecce2e3a0c741c7e3ff
d92048 <stephan.waldow@d-fine.de>
2022-06-21 18:13:38 +0200

    adds sample section tests again

--- dataland-frontend/tests/e2e/specs/low/pages/LandingPage.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/LandingPage.ts
@@ -62,3 +62,22 @@ describe('Footer Section', () => {
         cy.get("h2").contains('Data Privacy')
     })
 })
+
+describe('Sample Section', () => {
+    it('Check that the sample section works properly', () => {
+        cy.visit("/")
+        cy.get('h2').should("contain.text","Explore Dataland")
+        cy.get('button[name=eu_taxonomy_sample_button]')
+            .should('be.visible')
+            .should("contain.text","EU Taxonomy")
+            .click({force:true})
+            .url().should('include', '/taxonomysample')
+        cy.get('h2').contains('EU Taxonomy Data')
+        cy.get('.p-button.p-button-rounded')
+            .should("contain.text","COMPANY DATA SAMPLE")
+        cy.get('body').should('contain.text', 'Join Dataland with other')
+        cy.get('[title=back_button').should('be.visible').click({force:true})
+        cy.get('h1').should("contain.text","CREATE A DATASET")
+    })
+})
+
