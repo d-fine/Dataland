39b6f9de8abbd5fe42cee28f0343ac763cfc36f3
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-06-21 14:09:45 +0200

    fix for bug in Prepopulation.ts

--- dataland-frontend/tests/e2e/specs/high/Prepopulation.ts
+++ dataland-frontend/tests/e2e/specs/high/Prepopulation.ts
@@ -14,7 +14,7 @@ describe('Population Test',
         let teaserCompaniesPermIds: Array<{ permId: string }> = []
 
         if (Cypress.env("REALDATA")) {
-            teaserCompaniesPermIds = Cypress.env("TEASER_COMPANY_PERM_IDS").cut(',')
+            teaserCompaniesPermIds = Cypress.env("TEASER_COMPANY_PERM_IDS").toString().split(",")
         }
 
         before(function () {
@@ -27,10 +27,18 @@ describe('Population Test',
             cy.restoreLoginSession()
         });
 
+        function getPermId(companyInformation: CompanyInformation) {
+            for (const identifier of companyInformation.identifiers) {
+                if (identifier.identifierType == "PermId"){
+                    return identifier.identifierValue
+                }
+            }
+            return ""
+        }
+
         function addCompanyIdToTeaserCompanies(companyInformation: CompanyInformation, json: any) {
-            if (Cypress.env("REALDATA")) {
-                if (companyInformation.identifiers.any((id: any) => {teaserCompaniesPermIds.includes({ permId: id.identifierValue })}))
-                    teaserCompanies.push(json.companyId)
+            if (Cypress.env("REALDATA") && teaserCompaniesPermIds.includes({ permId: getPermId(companyInformation) })) {
+                teaserCompanies.push(json.companyId)
             } else if (teaserCompanies.length == 0) {
                 teaserCompanies.push(json.companyId)
             }
