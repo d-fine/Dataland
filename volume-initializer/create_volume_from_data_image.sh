bc8029f8d8e32db79b630362f3d0991f313e37b1
Philip Puetsch <philip.puetsch@d-fine.de>
2022-06-23 17:33:36 +0200

    removes unnecessary files
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- volume-initializer/create_volume_from_data_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# A Script that will copy persistent data from a data image <image_tag> to a local volume <target_volume>
-# The data image should have been created with create_data_image_from_volume.sh
-# Usage: create_volume_from_data_image.sh <image_tag> <target_volume>
-set -ex
-
-docker volume rm $2 || true
-docker pull $1
-docker run --rm -v $2:/target_volume $1 sh -c "cp -r /data/* /target_volume"
