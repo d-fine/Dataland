17ce151a436941f1ca87aacc0d95fc9655b6d9cc
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-06-24 15:36:47 +0200

    added social login google

--- deployment/initialize_keycloak.sh
+++ deployment/initialize_keycloak.sh
@@ -10,7 +10,6 @@ ssh ubuntu@"$target_server_url" "mkdir -p $location/dataland-keycloak"
 scp -r "$script_dir"/../dataland-keycloak/realms ubuntu@"$target_server_url":"$location"/dataland-keycloak
 scp "$script_dir"/../dataland-keycloak/Dockerfile ubuntu@"$target_server_url":$location/DockerfileKeycloak
 scp "$script_dir"/../docker-compose.yml ubuntu@"$target_server_url":$location
-scp -r ./dataland-keycloak/dataland_theme ubuntu@$target_server_url:$location/dataland-keycloak
 
 old_volume=$(ssh ubuntu@"$target_server_url" "cd $location && sudo docker volume ls -q | grep keycloak_data") || true
 if [[ -n $old_volume ]]; then
@@ -28,6 +27,8 @@ ssh ubuntu@"$target_server_url" "cd $location; sudo docker-compose pull;
                                  export KEYCLOAK_ADMIN=\"$KEYCLOAK_ADMIN\";
                                  export KEYCLOAK_ADMIN_PASSWORD=\"$KEYCLOAK_ADMIN_PASSWORD\";
                                  export KEYCLOAK_DB_PASSWORD=\"$KEYCLOAK_DB_PASSWORD\";
+                                 export KEYCLOAK_GOOGLE_SECRET=\"$KEYCLOAK_GOOGLE_SECRET\";
+                                 export KEYCLOAK_GOOGLE_ID=\"$KEYCLOAK_GOOGLE_ID\";
                                  export KEYCLOAK_DOCKERFILE=DockerfileKeycloak;
                                  sudo -E docker-compose --profile init up -d --build"
 message="Profile prod activated."
