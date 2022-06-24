300a57ba56bd55ae1eac371529752e7c234443f1
d92048 <stephan.waldow@d-fine.de>
2022-06-24 15:53:50 +0200

    removes only in landing page tests

--- dataland-frontend/tests/e2e/specs/low/pages/LandingPage.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/LandingPage.ts
@@ -97,7 +97,7 @@ describe('Sample Section', () => {
     })
 })
 
-describe.only('Authentication Buttons', () => {
+describe('Authentication Buttons', () => {
     it('Checks that the authentication buttons are there and working' , () => {
         cy.visit("/")
         cy.login()
