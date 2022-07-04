faa594d60b9ca4eb7a47784f38a18b1caee96241
stewal19 <104447806+stewal19@users.noreply.github.com>
2022-07-04 13:02:39 +0200

    Dala 286 scroll to top (#63)
    Make search taxonomy page scroll to top if first displayed data point in table changes (f.e. because the page was switched via the paginator)

--- dataland-frontend/src/router/index.ts
+++ dataland-frontend/src/router/index.ts
@@ -69,6 +69,9 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior() {
+        window.scrollTo(0, 0);
+    }
 });
 
 export { routes }
