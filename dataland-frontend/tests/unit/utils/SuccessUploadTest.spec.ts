6cb5f2204f8ecab352fe0bb6a30f058567ae9989
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-07-04 16:04:28 +0200

    add new unit test to increase test coverage

--- dataland-frontend/tests/unit/utils/SuccessUploadTest.spec.ts
+++ dataland-frontend/tests/unit/utils/SuccessUploadTest.spec.ts
@@ -1,8 +1,11 @@
 import { expect } from "@jest/globals";
 import successUpload from "@/components/messages/SuccessUpload.vue";
+import { shallowMount } from "@vue/test-utils";
 
 describe("Negative test for Success message", () => {
+  const wrapper = shallowMount(successUpload);
+
   it("verify that non string input yields null", () => {
-    expect(successUpload.methods.humanize(5)).toBeNull();
+    expect(wrapper.vm.humanize(5)).toBeNull();
   });
 });
