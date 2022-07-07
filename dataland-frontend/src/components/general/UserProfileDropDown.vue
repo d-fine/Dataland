a462f8af4bffa7d5c6fc189a89fd17400fc27597
Marc Meyer <marc.meyer@d-fine.de>
2022-07-07 11:28:38 +0200

    Updated keycloak sync mode to FORCE to auto-update profile pictures on change Styled user profile drop down menu

--- dataland-frontend/src/components/general/UserProfileDropDown.vue
+++ dataland-frontend/src/components/general/UserProfileDropDown.vue
@@ -1,9 +1,13 @@
 <template>
   <div @click="toggle" class="max-w-full max-h-full flex justify-content-center">
-    <img ref="profile-picture" class="d-profile-picture" src="@/assets/images/logos/favicon-32x32.png">
-    <i class="pi pi-caret-down pt-2 ml-2" style="color:white;"></i>
+    <img ref="profile-picture" class="d-profile-picture" src="@/assets/images/logos/favicon-32x32.png"/>
+    <img src="@/assets/images/elements/triangle_down.svg" class="d-triangle-down"/>
   </div>
-  <Menu ref="menu" :model="items" :popup="true" />
+  <Menu ref="menu" :model="items" :popup="true" style="transform:translate(0px,1rem);" class="text-primary">
+    <template #item="{item}">
+      <a class="p-menuitem-link" role="menuitem" tabindex="0" @click="item.command()"><span class="p-menuitem-icon text-primary" :class="item.icon"></span><span class="p-menuitem-text text-primary">{{item.label}}</span></a>
+    </template>
+  </Menu>
 </template>
 
 <script>
@@ -50,5 +54,11 @@ export default {
     border-radius: 50%;
     height: 2rem;
     /* TODO: Make the profile picture adjust size to fit parent automatically */
+  }
+
+  .d-triangle-down {
+    height: 0.5rem;
+    margin-top: 1rem;
+    margin-left: 0.5rem;
   }
 </style>
