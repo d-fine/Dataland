d166f759144f78ace64c37181a4819e642eb0277
Marc Meyer <marc.meyer@d-fine.de>
2022-07-12 13:10:58 +0200

    Tried to fix CI pipeline

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/Constants.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/Constants.kt
@@ -1,9 +1,9 @@
 package org.dataland.e2etests
 
-const val BASE_PATH_TO_DATALAND_PROXY = "http://proxy:80/api"
+const val BASE_PATH_TO_DATALAND_PROXY = "https://dataland-local.duckdns.org:443/api"
 
 const val PATH_TO_KEYCLOAK_TOKENENDPOINT =
-    "http://proxy:80/keycloak/realms/datalandsecurity/protocol/openid-connect/token"
+    "https://dataland-local.duckdns.org:443/keycloak/realms/datalandsecurity/protocol/openid-connect/token"
 
 const val TOKENREQUEST_GRANT_TYPE = "password"
 const val TOKENREQUEST_CLIENT_ID = "dataland-public"
