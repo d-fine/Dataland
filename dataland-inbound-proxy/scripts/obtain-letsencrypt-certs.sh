81ff658cefe6ef3d82e0f5b986ac7c5b0c52e775
Marc Meyer <marc.meyer@d-fine.de>
2022-07-13 08:53:22 +0200

    Working on CD pipeline update for HTTPS

--- dataland-inbound-proxy/scripts/obtain-letsencrypt-certs.sh
+++ dataland-inbound-proxy/scripts/obtain-letsencrypt-certs.sh
@@ -2,6 +2,7 @@
 if [ ! -d "/etc/letsencrypt/live" ]; then
   echo "Requesting LetsEncrypt certificates"
   mkdir -p /var/www/certbot && certbot certonly --non-interactive --webroot --webroot-path /var/www/certbot --agree-tos --preferred-challenges http "$@"
+  sh /scripts/configure-certificate-link.sh
 else
   echo "It looks like LetsEncrypt is already configured. Skipping"
 fi
