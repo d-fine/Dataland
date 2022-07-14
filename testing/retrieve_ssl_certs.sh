3e10799fc850945cde2d93432cacc74d438a6bfe
Marc Meyer <marc.meyer@d-fine.de>
2022-07-14 12:49:13 +0200

    Started working on review comments

--- testing/retrieve_ssl_certs.sh
+++ testing/retrieve_ssl_certs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p ~/.ssh/
-echo "${{ secrets.DATALAND_LETSENCRYPT_HOST_KEYS }}" >>  ~/.ssh/known_hosts
-echo "${{ secrets.SSH_PRIVATE_KEY }}" > ~/.ssh/id_rsa
+echo "$DATALAND_LETSENCRYPT_HOST_KEYS" >>  ~/.ssh/known_hosts
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa && mkdir -p ./local/certs
 scp ubuntu@dataland-letsencrypt.duckdns.org:/etc/letsencrypt/live/dataland-local.duckdns.org/* ./local/certs
