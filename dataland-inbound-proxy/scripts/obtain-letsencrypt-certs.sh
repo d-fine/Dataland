938e5aa85c8fbedf42b93526fea333f91ac37745
Marc Meyer <marc.meyer@d-fine.de>
2022-07-14 16:43:25 +0200

    Implement proper way to wait for nginx to finish booting

--- dataland-inbound-proxy/scripts/obtain-letsencrypt-certs.sh
+++ dataland-inbound-proxy/scripts/obtain-letsencrypt-certs.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-echo "Giving NGINX some time to finish starting up..."
-sleep 7;
+while ! curl http://localhost/.well-known/d-statuscheck 2>/dev/null | grep -q UP; do
+  echo "Waiting for NGINX to finish booting..."
+  sleep 1;
+done
+
 if [ ! -d "/etc/letsencrypt/live" ] && [ ! -f "/certs/custom/privkey.pem" ]; then
   echo "Requesting LetsEncrypt certificates"
   mkdir -p /var/www/certbot && certbot certonly --non-interactive --webroot --webroot-path /var/www/certbot --agree-tos --preferred-challenges http $PROXY_LETSENCRYPT_ARGS
