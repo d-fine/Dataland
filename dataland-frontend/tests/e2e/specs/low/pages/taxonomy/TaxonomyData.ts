2d01b2d26aed0897d918d10271551bdca345acd1
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-07-14 16:23:21 +0200

    Test for newly discovered bug

--- dataland-frontend/tests/e2e/specs/low/pages/taxonomy/TaxonomyData.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/taxonomy/TaxonomyData.ts
@@ -1,14 +1,34 @@
 describe("EU Taxonomy Page", function () {
-  it("page should be present", function () {
-    cy.restoreLoginSession();
-    cy.retrieveDataIdsList().then((dataIdList: any) => {
-      cy.visitAndCheckAppMount("/companies/" + dataIdList[2] + "/eutaxonomies");
+    it("Page should be present", function () {
+        cy.restoreLoginSession();
+        cy.retrieveDataIdsList().then((dataIdList: any) => {
+            cy.visitAndCheckAppMount("/companies/" + dataIdList[2] + "/eutaxonomies");
+        });
+        cy.get("h2").should("contain", "EU Taxonomy Data");
+        const placeholder = "Search company by name or PermID";
+        cy.get("input[name=eu_taxonomy_search_bar_standard]")
+            .should("not.be.disabled")
+            .invoke("attr", "placeholder")
+            .should("contain", placeholder);
     });
-    cy.get("h2").should("contain", "EU Taxonomy Data");
-    const placeholder = "Search company by name or PermID";
-    cy.get("input[name=eu_taxonomy_search_bar_standard]")
-      .should("not.be.disabled")
-      .invoke("attr", "placeholder")
-      .should("contain", placeholder);
-  });
+
+    it("Type smth into search bar, wait 1 sec, type enter, and expect to see search results on new page", function () {
+        cy.restoreLoginSession();
+        cy.retrieveDataIdsList().then((dataIdList: any) => {
+            cy.visitAndCheckAppMount("/companies/" + dataIdList[2] + "/eutaxonomies");
+        });
+        cy.get("h2").should("contain", "EU Taxonomy Data");
+        const inputValue = "A"
+        cy.get("input[name=eu_taxonomy_search_bar_standard]")
+            .should("not.be.disabled")
+            .click({force: true})
+            .type(inputValue)
+            .should("have.value", inputValue)
+            .wait(1000)
+            .type("{enter}")
+      cy.url().should("include", "/searchtaxonomy?input=" + inputValue)
+        cy.get("h2").should("contain", "Results");
+        cy.get("table.p-datatable-table").should("exist");
+    });
+
 });
