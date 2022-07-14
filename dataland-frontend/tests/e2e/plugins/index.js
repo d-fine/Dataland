3b552cebe72513263ca37c13c765aa138c12ab93
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-07-14 16:22:36 +0200

    use ENVIRONMENT to select test specifications

--- dataland-frontend/tests/e2e/plugins/index.js
+++ dataland-frontend/tests/e2e/plugins/index.js
@@ -3,10 +3,7 @@ module.exports = (on, config) => {
   config.fixturesFolder = "../testing/data";
   config.supportFile = "tests/e2e/support/index.ts";
   config.env.commit_id = require("git-commit-id")({ cwd: "../" });
-  if (config.isTextTerminal) {
-    config.excludeSpecPattern = ["tests/e2e/specs/runAll.ts"];
-  }
-  switch (process.env.NODE_ENV) {
+  switch (process.env.ENVIRONMENT) {
     case "development": {
       config.specPattern = [
         "tests/e2e/specs/verify_deployment",
@@ -18,12 +15,14 @@ module.exports = (on, config) => {
       ];
       break;
     }
-    case "production": {
-      config.specPattern = ["tests/e2e/specs"];
-      break;
-    }
-    case "local": {
-      config.specPattern = ["tests/e2e/specs"];
+    case "preview": {
+      config.specPattern = [
+        "tests/e2e/specs/verify_deployment",
+        "tests/e2e/specs/high",
+        "tests/e2e/specs/low",
+        "tests/e2e/specs/medium",
+        "tests/e2e/specs/previsit",
+      ];
       break;
     }
     default: {
