0424f6a9896a2094ecf8a50c86e2a1d14e9ceb8b
MarcMDfine <108792843+MarcMDfine@users.noreply.github.com>
2022-07-15 08:55:11 +0200

    Dala 302 enable LetsEncrypt HTTPS (#72)
    Enable HTTPS by introducing Lets Encrypt

---
+++ dataland-inbound-proxy/scripts/configure-certificate-link.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# This script checks if LetsEncrypt certificates are available
+# and rewrites the symlink for the nginx certificates accordingly
+
+if [ -f "/certs/custom/privkey.pem" ]; then
+  echo "Found custom certificates folder. Rewriting symlink"
+  rm /certs/dataland
+  ln -s /certs/custom /certs/dataland
+elif [ -f "$PROXY_LETSENCRYPT_PATH/privkey.pem" ]; then
+  echo "LetsEncrypt is there. Rewriting symlink"
+  rm /certs/dataland
+  ln -s $PROXY_LETSENCRYPT_PATH /certs/dataland
+else
+  echo "LetsEncrypt is NOT there. Rewriting symlink"
+  rm /certs/dataland
+  ln -s /certs/dummy /certs/dataland
+fi
