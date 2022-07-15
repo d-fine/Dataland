21ee5a87f9c25ef81966786e35d00b088ec5b27b
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-07-15 11:13:51 +0200

    Finally implemented functioning test

--- dataland-frontend/tests/e2e/specs/low/pages/taxonomy/SearchTaxonomy.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/taxonomy/SearchTaxonomy.ts
@@ -29,27 +29,6 @@ describe("Search Taxonomy", function () {
     cy.restoreLoginSession();
   });
 
-  it("Use arrow+enter keys to select an autocomplete-suggestion and check if forwarded to taxonomy data page", () => {
-    cy.visitAndCheckAppMount("/searchtaxonomy");
-    cy.intercept("**/api/companies*").as("searchCompany");
-    cy.get("input[name=eu_taxonomy_search_bar_top]").click({ force: true }).type("b");
-    cy.wait("@searchCompany", { timeout: 2 * 1000 }).then(() => {
-      cy.wait(2000);
-      cy.get('li[class="p-autocomplete-item"]')
-        .eq(0)
-        //.trigger("keydown", { keyCode: 40 })
-        //.wait(500)
-        //.trigger("keyup", { keyCode: 40 })
-        .wait(1000)
-        .trigger("keydown", { keyCode: 13 })
-        .wait(1000)
-        .url()
-        .should("include", "/companies/")
-        .url()
-        .should("include", "/eutaxonomies");
-    });
-  });
-
   it("Check static layout of the search page", function () {
     cy.visitAndCheckAppMount("/searchtaxonomy");
     cy.get("h1").should("contain", "Search EU Taxonomy data");
@@ -108,6 +87,22 @@ describe("Search Taxonomy", function () {
         .should("include", "/companies/")
         .url()
         .should("include", "/eutaxonomies");
+    });
+  });
+
+  it("Use arrow+enter keys to select an autocomplete-suggestion and check if forwarded to taxonomy data page", () => {
+    cy.visitAndCheckAppMount("/searchtaxonomy");
+    cy.intercept("**/api/companies*").as("searchCompany");
+    cy.get("input[name=eu_taxonomy_search_bar_top]").click({ force: true }).type("b");
+    cy.wait("@searchCompany", { timeout: 2 * 1000 }).then(() => {
+      cy.wait(1000)
+          .get("input[name=eu_taxonomy_search_bar_top]")
+          .type("{downArrow}", { force: true })
+          .type("{enter}", { force: true })
+          .url()
+          .should("include", "/companies/")
+          .url()
+          .should("include", "/eutaxonomies");
     });
   });
 
