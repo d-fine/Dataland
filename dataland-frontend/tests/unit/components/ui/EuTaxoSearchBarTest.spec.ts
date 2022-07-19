60d89910b91b125b538b7bdaaeaab8c6d5ebb39a
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-07-19 13:06:02 +0200

    quick fix unit test, further discussion required and noted

--- dataland-frontend/tests/unit/components/ui/EuTaxoSearchBarTest.spec.ts
+++ dataland-frontend/tests/unit/components/ui/EuTaxoSearchBarTest.spec.ts
@@ -1,36 +1,42 @@
 import EuTaxoSearchBar from "@/components/resources/taxonomy/search/EuTaxoSearchBar.vue";
-import { mount } from "@vue/test-utils";
-import { createRouter, createMemoryHistory } from "vue-router";
-import { routes } from "@/router";
-import { expect } from "@jest/globals";
+import {mount, shallowMount} from "@vue/test-utils";
+import {createRouter, createMemoryHistory} from "vue-router";
+import {routes} from "@/router";
+import {expect} from "@jest/globals";
 
 describe("EuTaxoSearchBarTest", () => {
-  let wrapper: any;
-  it("checks field properties", async () => {
-    const router = createRouter({
-      history: createMemoryHistory(),
-      routes,
+    let wrapper: any;
+    it("checks field properties", async () => {
+        const router = createRouter({
+            history: createMemoryHistory(),
+            routes,
+        });
+        router.push("/searchtaxonomy");
+        await router.isReady();
+        wrapper = shallowMount(EuTaxoSearchBar, {
+            global: {
+                plugins: [router],
+                provide: {
+                    getKeycloakInitPromise() {
+                        return "hi"
+                    },
+                  keycloak_init: "his"
+                }
+            },
+        });
     });
-    router.push("/searchtaxonomy");
-    await router.isReady();
-    wrapper = mount(EuTaxoSearchBar, {
-      global: {
-        plugins: [router],
-      },
+
+    it("checks field properties", () => {
+        expect(wrapper.vm.autocompleteArray).toBeDefined();
+        expect(wrapper.vm.autocompleteArrayDisplayed).toBeDefined();
+        expect(wrapper.vm.loading).toBeDefined();
+        expect(wrapper.vm.modelValue).toBeDefined();
     });
-  });
 
-  it("checks field properties", () => {
-    expect(wrapper.vm.autocompleteArray).toBeDefined();
-    expect(wrapper.vm.autocompleteArrayDisplayed).toBeDefined();
-    expect(wrapper.vm.loading).toBeDefined();
-    expect(wrapper.vm.modelValue).toBeDefined();
-  });
-
-  it("checks queryCompany()", async () => {
-    jest.spyOn(console, "error");
-    expect(wrapper.vm.queryCompany("someCompanyToSearchFor")).toBeDefined();
-    await wrapper.vm.queryCompany("someCompanyToSearchFor");
-    expect(console.error).toHaveBeenCalled();
-  });
+    it("checks queryCompany()", async () => {
+        jest.spyOn(console, "error");
+        expect(wrapper.vm.queryCompany("someCompanyToSearchFor")).toBeDefined();
+        await wrapper.vm.queryCompany("someCompanyToSearchFor");
+        expect(console.error).toHaveBeenCalled();
+    });
 });
