52f76b53adceff889648505fc5b057dc440161c1
MarcMDfine <marc.meyer@d-fine.de>
2022-07-20 15:24:02 +0200

    Added KeyCloak OAuth Login flow to the OpenAPI documentation for a more streamlined Swagger UI experience

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/DatalandBackend.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/DatalandBackend.kt
@@ -1,8 +1,5 @@
 package org.dataland.datalandbackend
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
-import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -10,12 +7,6 @@ import org.springframework.boot.runApplication
  * Class to define the spring boot application
  */
 @SpringBootApplication
-@SecurityScheme(
-    name = "default-auth",
-    scheme = "bearer",
-    type = SecuritySchemeType.HTTP,
-    `in` = SecuritySchemeIn.HEADER
-)
 class DatalandBackend
 
 /**
--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/configurations/OpenAPIConfiguration.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/configurations/OpenAPIConfiguration.kt
@@ -1,7 +1,12 @@
 package org.dataland.datalandbackend.configurations
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.OAuthFlow
+import io.swagger.v3.oas.annotations.security.OAuthFlows
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.annotations.servers.Server
 
 /**
@@ -13,5 +18,21 @@ import io.swagger.v3.oas.annotations.servers.Server
         version = "1.0.0"
     ),
     servers = [Server(url = "/api")]
+)
+@SecurityScheme(
+    name = "default-bearer-auth",
+    scheme = "bearer",
+    type = SecuritySchemeType.HTTP,
+    `in` = SecuritySchemeIn.HEADER
+)
+@SecurityScheme(
+    name = "default-oauth",
+    type = SecuritySchemeType.OAUTH2,
+    flows = OAuthFlows(
+        authorizationCode = OAuthFlow(
+            authorizationUrl = "/keycloak/realms/datalandsecurity/protocol/openid-connect/auth",
+            tokenUrl = "/keycloak/realms/datalandsecurity/protocol/openid-connect/token"
+        )
+    )
 )
 class OpenAPIConfiguration
