d51e48d01b25ec1719a06b9414e0f5c69b27d22c
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-07-20 15:11:29 +0200

    Fix unauthenticated redirect test

--- dataland-frontend/tests/e2e/specs/low/pages/VisitPagesUnauth.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/VisitPagesUnauth.ts
@@ -1,9 +1,11 @@
-describe("Test that if unauthenticated will be redirected to landing page", () => {
+describe("Test that if unauthenticated will be redirected to login page", () => {
   it("test for each of given paths", () => {
     const pages = ["/upload", "/search", "/searchtaxonomy", "/companies/:companyID/eutaxonomies"];
     pages.forEach((page) => {
       cy.visitAndCheckAppMount(page);
-      cy.url().should("eq", Cypress.config("baseUrl") + "/");
+      cy.get("input[name=login]")
+          .should("exist")
+          .url().should("contain", "keycloak");
     });
   });
 });
