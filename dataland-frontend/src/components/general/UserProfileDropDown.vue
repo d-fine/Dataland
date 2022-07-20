eb52bf0138f7594cffa5165f3b5bd2843640f082
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-07-20 14:09:55 +0200

    Handling authenticated=null as redirect reason + Some refactoring

--- dataland-frontend/src/components/general/UserProfileDropDown.vue
+++ dataland-frontend/src/components/general/UserProfileDropDown.vue
@@ -13,7 +13,13 @@
     />
     <img src="@/assets/images/elements/triangle_down.svg" class="d-triangle-down" alt="Open drop down menu icon" />
   </div>
-  <PrimeMenu ref="menu" :model="dropdownMenuItems" :popup="true" style="transform: translate(0px, 1rem)" class="text-primary">
+  <PrimeMenu
+    ref="menu"
+    :model="dropdownMenuItems"
+    :popup="true"
+    style="transform: translate(0px, 1rem)"
+    class="text-primary"
+  >
     <template #item="{ item }">
       <a class="p-menuitem-link" role="menuitem" tabindex="0" @click="logoutViaDropdown" :id="item.id"
         ><span class="p-menuitem-icon text-primary" :class="item.icon"></span
