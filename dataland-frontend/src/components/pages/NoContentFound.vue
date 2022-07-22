ffc6174bae40662b77fd706c46cdb4245d818bc8
d92048 <stephan.waldow@d-fine.de>
2022-07-22 14:24:20 +0200

    adds header and content to error page

--- dataland-frontend/src/components/pages/NoContentFound.vue
+++ dataland-frontend/src/components/pages/NoContentFound.vue
@@ -1,9 +1,16 @@
 <template>
-  <p>Whoops! Something went wrong! Go back to <router-link to="/">Dataland</router-link></p>
+  <TheHeader/>
+  <TheContent>
+    <p>Whoops! Something went wrong!</p>
+    <p>Go back to <router-link to="/" class="text-primary">Dataland</router-link></p>
+  </TheContent>
 </template>
 
 <script>
+import TheHeader from "@/components/structure/TheHeader";
+import TheContent from "@/components/structure/TheContent";
 export default {
   name: "NoContentFound",
+  components: {TheContent, TheHeader},
 };
 </script>
