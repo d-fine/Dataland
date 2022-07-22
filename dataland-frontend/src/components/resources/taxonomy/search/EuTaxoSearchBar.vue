c55a731caae4dca92c8d29a5c1efdaaab252d409
MarcMDfine <marc.meyer@d-fine.de>
2022-07-22 12:27:03 +0200

    Fixed alignment of the searchbar on the EuTaxoSearch page

--- dataland-frontend/src/components/resources/taxonomy/search/EuTaxoSearchBar.vue
+++ dataland-frontend/src/components/resources/taxonomy/search/EuTaxoSearchBar.vue
@@ -1,52 +1,49 @@
 <template>
-  <MarginWrapper>
-    <div class="grid">
-      <div class="col-8 text-left">
-        <span class="p-fluid">
-          <span class="p-input-icon-left p-input-icon-right">
-            <i class="pi pi-search" aria-hidden="true" style="z-index: 20; color: #958d7c" />
-            <i v-if="loading" class="pi pi-spinner pi-spin" aria-hidden="true" style="z-index: 20; color: #e67f3f" />
-            <i v-else aria-hidden="true" />
-            <AutoComplete
-              :suggestions="autocompleteArrayDisplayed"
-              :name="taxoSearchBarName"
-              :modelValue="modelValue"
-              ref="autocomplete"
-              inputClass="h-3rem"
-              field="companyName"
-              style="z-index: 10"
-              placeholder="Search company by name or PermID"
-              @input="handleInput"
-              @complete="searchCompanyName"
-              @item-select="handleItemSelect"
-              @keyup.enter="handleKeyupEnter"
-            >
-              <template #footer>
-                <ul
-                  class="p-autocomplete-items pt-0"
-                  v-if="autocompleteArray && autocompleteArray.length >= maxNumAutoCompleteEntries"
-                >
-                  <li class="p-autocomplete-item text-primary font-semibold" @click="handleKeyupEnter">
-                    View all results.
-                  </li>
-                </ul>
-              </template>
-            </AutoComplete>
-          </span>
+  <div class="grid">
+    <div class="col-8 text-left">
+      <span class="p-fluid">
+        <span class="p-input-icon-left p-input-icon-right">
+          <i class="pi pi-search" aria-hidden="true" style="z-index: 20; color: #958d7c" />
+          <i v-if="loading" class="pi pi-spinner pi-spin" aria-hidden="true" style="z-index: 20; color: #e67f3f" />
+          <i v-else aria-hidden="true" />
+          <AutoComplete
+            :suggestions="autocompleteArrayDisplayed"
+            :name="taxoSearchBarName"
+            :modelValue="modelValue"
+            ref="autocomplete"
+            inputClass="h-3rem"
+            field="companyName"
+            style="z-index: 10"
+            placeholder="Search company by name or PermID"
+            @input="handleInput"
+            @complete="searchCompanyName"
+            @item-select="handleItemSelect"
+            @keyup.enter="handleKeyupEnter"
+          >
+            <template #footer>
+              <ul
+                class="p-autocomplete-items pt-0"
+                v-if="autocompleteArray && autocompleteArray.length >= maxNumAutoCompleteEntries"
+              >
+                <li class="p-autocomplete-item text-primary font-semibold" @click="handleKeyupEnter">
+                  View all results.
+                </li>
+              </ul>
+            </template>
+          </AutoComplete>
         </span>
-      </div>
+      </span>
     </div>
-  </MarginWrapper>
+  </div>
 </template>
 
 <script>
 import AutoComplete from "primevue/autocomplete";
-import MarginWrapper from "@/components/wrapper/MarginWrapper";
 import { getCompanyDataForTaxonomyPage } from "@/utils/SearchTaxonomyPageCompanyDataRequester";
 
 export default {
   name: "EuTaxoSearchBar",
-  components: { AutoComplete, MarginWrapper },
+  components: { AutoComplete },
 
   emits: ["companies-received", "update:modelValue", "rendered"],
 
