59441dd4ae9338d8c15204e410f75917a424415f
d92048 <stephan.waldow@d-fine.de>
2022-07-25 10:19:02 +0200

    modifies error page

--- dataland-frontend/src/components/pages/NoContentFound.vue
+++ dataland-frontend/src/components/pages/NoContentFound.vue
@@ -1,6 +1,7 @@
 <template>
   <TheHeader />
   <TheContent>
+    <h2>Sorry an error occurred!</h2>
     <p>Whoops! Something went wrong!</p>
     <p>Go back to <router-link to="/" class="text-primary" title="back to landing page">Dataland</router-link></p>
   </TheContent>
