e155dec4f6a86a7ef3d7695a3e6c24605c2b5466
MarcTM01 <marc.meyer@d-fine.de>
2022-08-03 14:08:49 +0200

    Updated test

--- dataland-frontend/tests/e2e/specs/low/pages/VisitPagesUnauth.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/VisitPagesUnauth.ts
@@ -2,7 +2,7 @@ describe("Test that if unauthenticated will be redirected to login page", () => 
   it("test for each of given paths", () => {
     const pages = ["/upload", "/search", "/searchtaxonomy", "/companies/:companyID/eutaxonomies"];
     pages.forEach((page) => {
-      cy.visitAndCheckAppMount(page);
+      cy.visit(page);
       cy.get("input[name=login]").should("exist").url().should("contain", "keycloak");
     });
   });
