eb8b7e4d86bed2280449883fc3de58b4eb157b59
Philip Puetsch <philip.puetsch@d-fine.de>
2022-08-04 18:12:28 +0200

    bugfix env name
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- dataland-frontend/tests/e2e/plugins/index.js
+++ dataland-frontend/tests/e2e/plugins/index.js
@@ -3,14 +3,14 @@ module.exports = (on, config) => {
   config.fixturesFolder = "../testing/data";
   config.supportFile = "tests/e2e/support/index.ts";
   config.env.commit_id = require("git-commit-id")({ cwd: "../" });
-  switch (process.env.REAL_DATA) {
+  switch (process.env.REALDATA) {
     case "false": {
-      console.log(`Processing runAll.ts - REAL_DATA=${process.env.REAL_DATA}`);
+      console.log(`Processing runAll.ts - REAL_DATA=${process.env.REALDATA}`);
       config.specPattern = ["tests/e2e/specs/runAll.ts"];
       break;
     }
     case "true": {
-      console.log(`Processing test for scope real data - REAL_DATA=${process.env.REAL_DATA}`);
+      console.log(`Processing test for scope real data - REAL_DATA=${process.env.REALDATA}`);
       config.specPattern = [
         "tests/e2e/specs/verify_deployment",
         "tests/e2e/specs/high",
@@ -21,7 +21,7 @@ module.exports = (on, config) => {
       break;
     }
     default: {
-      console.log(`Defaulting to all specs - REAL_DATA=${process.env.REAL_DATA}`);
+      console.log(`Defaulting to all specs - REAL_DATA=${process.env.REALDATA}`);
       config.specPattern = ["tests/e2e/specs"];
       break;
     }
