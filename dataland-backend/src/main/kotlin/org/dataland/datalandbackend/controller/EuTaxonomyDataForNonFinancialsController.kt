52f028edb65faf1047f2f90e2202b97b4cb2a3f0
d92048 <stephan.waldow@d-fine.de>
2022-08-04 10:10:48 +0200

    renames endpoints

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/EuTaxonomyDataForNonFinancialsController.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/EuTaxonomyDataForNonFinancialsController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
  * @param myDataManager data manager to be used
  * @param myObjectMapper object mapper used for converting data classes to strings and vice versa
  */
-@RequestMapping("/data/eutaxonomies/nonfinancial")
+@RequestMapping("/data/eutaxonomy/nonfinancials")
 @RestController
 class EuTaxonomyDataForNonFinancialsController(
     @Autowired var myDataManager: DataManagerInterface,
