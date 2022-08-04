6f8449e6567470122f96a0b044fbfd6f41305be7
d92048 <stephan.waldow@d-fine.de>
2022-08-04 09:41:47 +0200

    creates additional endpoint

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/EuTaxonomyDataForNonFinancialsController.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/EuTaxonomyDataForNonFinancialsController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
  * @param myDataManager data manager to be used
  * @param myObjectMapper object mapper used for converting data classes to strings and vice versa
  */
-@RequestMapping("/data/eutaxonomies")
+@RequestMapping("/data/eutaxonomies/nonfinancial")
 @RestController
 class EuTaxonomyDataForNonFinancialsController(
     @Autowired var myDataManager: DataManagerInterface,
