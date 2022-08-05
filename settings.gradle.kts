e3f1839119319149a41e1aed7fd68f5916fc4b9e
MarcTM01 <marc.meyer@d-fine.de>
2022-08-05 14:26:07 +0200

    Continued version update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,9 +19,9 @@ dependencyResolutionManagement {
             library("moshi-kotlin", "com.squareup.moshi:moshi-kotlin:1.13.0")
             library("moshi-adapters", "com.squareup.moshi:moshi-adapters:1.13.0")
 
-            library("swagger-jaxrs2-jakarta", "io.swagger.core.v3:swagger-jaxrs2-jakarta:2.2.0")
-            library("swagger-gradle-plugin", "io.swagger.core.v3:swagger-gradle-plugin:2.2.0")
-            library("swagger-annotations", "io.swagger.core.v3:swagger-annotations:2.2.0")
+            library("swagger-jaxrs2-jakarta", "io.swagger.core.v3:swagger-jaxrs2-jakarta:2.2.2")
+            library("swagger-gradle-plugin", "io.swagger.core.v3:swagger-gradle-plugin:2.2.2")
+            library("swagger-annotations", "io.swagger.core.v3:swagger-annotations:2.2.2")
 
             library("okhttp", "com.squareup.okhttp3:okhttp:4.10.0")
             library("rs-api", "jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
