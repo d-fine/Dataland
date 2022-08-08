f44f0611c736b42340134e753a42a4cf05fbb845
MarcTM01 <marc.meyer@d-fine.de>
2022-08-08 14:25:05 +0200

    CI pipeline speedup experimentation

--- testing/build_docker_compose.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./gradlew dataland-keycloak:dataland_theme:login:buildTheme --no-daemon --stacktrace
-docker-compose --project-name dala-e2e-test --profile testing pull
-docker-compose --project-name dala-e2e-test --profile testing build
