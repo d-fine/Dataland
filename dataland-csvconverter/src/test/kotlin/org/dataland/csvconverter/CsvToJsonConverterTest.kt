8b30ea8e006c10724828ccfb0d3c80519af1bcae
d92048 <stephan.waldow@d-fine.de>
2022-08-08 12:13:30 +0200

    fixes type

--- dataland-csvconverter/src/test/kotlin/org/dataland/csvconverter/CsvToJsonConverterTest.kt
+++ dataland-csvconverter/src/test/kotlin/org/dataland/csvconverter/CsvToJsonConverterTest.kt
@@ -21,7 +21,7 @@ class CsvToJsonConverterTest {
     @Test
     fun `read csv and check that the company information and EU Taxonomy objects are as expected`() {
         val actualCompanyInformationWithEuTaxonomyDataForNonFinancials =
-            getConverter("./build/resources/csvTestEUTaxonomyDataForNonFinancials.csv")
+            getConverter("./build/resources/csvTestEuTaxonomyDataForNonFinancials.csv")
                 .buildListOfCompanyInformationWithEuTaxonomyDataForNonFinancials()
         val expectedCompanyInformationWithEuTaxonomyDataForNonFinancials = testDataProvider
             .getAllCompanyInformationWithEuTaxonomyDataForNonFinancials()
