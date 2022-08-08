bd1e73e8923c9f42fd8f5b6f53577a6f118469a6
d92048 <stephan.waldow@d-fine.de>
2022-08-08 12:01:25 +0200

    changes name of csv test data file

--- dataland-csvconverter/src/test/kotlin/org/dataland/csvconverter/CsvToJsonConverterTest.kt
+++ dataland-csvconverter/src/test/kotlin/org/dataland/csvconverter/CsvToJsonConverterTest.kt
@@ -21,7 +21,7 @@ class CsvToJsonConverterTest {
     @Test
     fun `read csv and check that the company information and EU Taxonomy objects are as expected`() {
         val actualCompanyInformationWithEuTaxonomyDataForNonFinancials =
-            getConverter("./build/resources/csvTestData.csv")
+            getConverter("./build/resources/csvTestEUTaxonomyDataForNonFinancials.csv")
                 .buildListOfCompanyInformationWithEuTaxonomyDataForNonFinancials()
         val expectedCompanyInformationWithEuTaxonomyDataForNonFinancials = testDataProvider
             .getAllCompanyInformationWithEuTaxonomyDataForNonFinancials()
