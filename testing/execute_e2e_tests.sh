fd7f030b32bf009c0c7fc21f803dc55865691a70
MarcTM01 <marc.meyer@d-fine.de>
2022-08-08 17:32:13 +0200

    Switched to docker BuildX and docker compose 2 to hopefully increase performance

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -3,20 +3,19 @@ set -x
 ./gradlew dataland-keycloak:dataland_theme:login:buildTheme --no-daemon --stacktrace
 #Start E2E Test and wait for E2E Test completion
 docker compose --project-name dala-e2e-test --profile testing up -d --build || exit
-docker ps -a
-timeout 2400 sh -c "docker logs dala-e2e-test_e2etests_1 --follow"
+timeout 2400 sh -c "docker logs dala-e2e-test-e2etests-1 --follow"
 E2ETEST_TIMEOUT_EXIT_CODE=$?
 mkdir -p ./cypress/${CYPRESS_TEST_GROUP}
 mkdir -p ./reports/${CYPRESS_TEST_GROUP}
-docker cp dala-e2e-test_e2etests_1:/app/dataland-frontend/coverage/e2e/lcov.info ./lcov-${CYPRESS_TEST_GROUP}.info
-docker cp dala-e2e-test_e2etests_1:/app/dataland-frontend/cypress/* ./cypress/${CYPRESS_TEST_GROUP}/
-docker cp dala-e2e-test_e2etests_1:/app/dataland-e2etests/build/reports/* ./reports/${CYPRESS_TEST_GROUP}/
+docker cp dala-e2e-test-e2etests-1:/app/dataland-frontend/coverage/e2e/lcov.info ./lcov-${CYPRESS_TEST_GROUP}.info
+docker cp dala-e2e-test-e2etests-1:/app/dataland-frontend/cypress/* ./cypress/${CYPRESS_TEST_GROUP}/
+docker cp dala-e2e-test-e2etests-1:/app/dataland-e2etests/build/reports/* ./reports/${CYPRESS_TEST_GROUP}/
 
 # Stop Backend causing JaCoCo to write Coverage Report, get it to pwd
-docker exec dala-e2e-test_backend_1 pkill -f spring
-timeout 90 sh -c "docker logs dala-e2e-test_backend_1 --follow" > /dev/null
+docker exec dala-e2e-test-e2etests-1 pkill -f spring
+timeout 90 sh -c "docker logs dala-e2e-test-backend-1 --follow" > /dev/null
 BACKEND_TIMEOUT_EXIT_CODE=$?
-docker cp dala-e2e-test_backend_1:/app/dataland-backend/build/jacoco/bootRun.exec ./bootRun-${CYPRESS_TEST_GROUP}.exec
+docker cp dala-e2e-test-backend-1:/app/dataland-backend/build/jacoco/bootRun.exec ./bootRun-${CYPRESS_TEST_GROUP}.exec
 
 # Write the logs of the docker container for later upload and analysis
 mkdir -p ./dockerLogs/${CYPRESS_TEST_GROUP}
@@ -27,7 +26,7 @@ do
 done
 
 # Check execution success of Test Container
-TEST_EXIT_CODE=`docker inspect -f '{{.State.ExitCode}}' dala-e2e-test_e2etests_1`
+TEST_EXIT_CODE=`docker inspect -f '{{.State.ExitCode}}' dala-e2e-test-e2etests-1`
 echo "E2ETEST Timeout exited with exit code $E2ETEST_TIMEOUT_EXIT_CODE"
 echo "BACKEND Timeout exited with exit code $BACKEND_TIMEOUT_EXIT_CODE"
 echo "Docker E2E Testcontainer exited with code $TEST_EXIT_CODE"
