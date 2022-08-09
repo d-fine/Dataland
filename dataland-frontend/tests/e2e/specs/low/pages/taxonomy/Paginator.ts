c77f3ececd37b92d87082f7d4ec7d25ab4335a00
Philip Puetsch <philip.puetsch@d-fine.de>
2022-08-09 10:13:16 +0200

    review comment
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- dataland-frontend/tests/e2e/specs/low/pages/taxonomy/Paginator.ts
+++ dataland-frontend/tests/e2e/specs/low/pages/taxonomy/Paginator.ts
@@ -2,10 +2,18 @@ describe("Paginator test suite", () => {
   beforeEach(() => {
     cy.restoreLoginSession();
   });
-  it("Assure existence of paginator for search to 'e'", () => {
-    cy.visitAndCheckAppMount("/searchtaxonomy?input=e");
-    cy.get("h1").should("contain", "Search EU Taxonomy data");
-    cy.get("div.p-paginator").should("exist");
+  it("Search for all companies containing a and verify that results are paginated, only first 100 are shown", () => {
+    cy.visitAndCheckAppMount("/searchtaxonomy");
+    const inputValue = "a";
+    cy.get("input[name=eu_taxonomy_search_bar_top]")
+        .should("not.be.disabled")
+        .click({ force: true })
+        .type(inputValue)
+        .type("{enter}")
+        .should("have.value", inputValue);
+    cy.get("h2").should("contain", "Results");
+    cy.get("table.p-datatable-table").should("exist");
+    cy.get(".p-paginator-current").should("contain.text", "Showing 1 to 100 of").contains("entries");
   });
 
   it("Do a search with 0 matches, then assure that the paginator is gone", () => {
