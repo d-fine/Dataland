42330eca796ccb1c432a4e8bd6ee3cd3d7292d8e
MarcTM01 <marc.meyer@d-fine.de>
2022-08-10 09:25:17 +0200

    Removed DAX paginator search

--- dataland-frontend/tests/e2e/specs/company-metadata/SearchPagniation.ts
+++ dataland-frontend/tests/e2e/specs/company-metadata/SearchPagniation.ts
@@ -1,44 +1,32 @@
 import { describeIf } from "../../support/TestUtility";
 
-describeIf(
-  "As a user, I expect there to be multiple result pages if there are many results to be displayed",
-  {
-    dataEnvironments: ["fakeFixtures"],
-    executionEnvironments: ["development"],
-  },
-  () => {
-    beforeEach(() => {
-      cy.ensureLoggedIn();
-    });
-    it("Assure existence of paginator for Dax search", () => {
-      cy.visitAndCheckAppMount("/searchtaxonomy");
-      cy.get("h1").should("contain", "Search EU Taxonomy data");
-      cy.get("div.p-paginator").should("exist");
-    });
+describe("As a user, I expect there to be multiple result pages if there are many results to be displayed", () => {
+  beforeEach(() => {
+    cy.ensureLoggedIn();
+  });
 
-    it("Do a search with 0 matches, then assure that the paginator is gone", () => {
-      cy.visitAndCheckAppMount("/searchtaxonomy");
-      const inputValueThatWillResultInZeroMatches = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678987654321";
-      cy.get("input[name=eu_taxonomy_search_bar_top]")
-        .should("exist")
-        .type(inputValueThatWillResultInZeroMatches)
-        .type("{enter}")
-        .should("have.value", inputValueThatWillResultInZeroMatches);
-      cy.get("div.p-paginator").should("not.exist");
-    });
+  it("Do a search with 0 matches, then assure that the paginator is gone", () => {
+    cy.visitAndCheckAppMount("/searchtaxonomy");
+    const inputValueThatWillResultInZeroMatches = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678987654321";
+    cy.get("input[name=eu_taxonomy_search_bar_top]")
+      .should("exist")
+      .type(inputValueThatWillResultInZeroMatches)
+      .type("{enter}")
+      .should("have.value", inputValueThatWillResultInZeroMatches);
+    cy.get("div.p-paginator").should("not.exist");
+  });
 
-    it("Search for all companies containing 'a' and verify that results are paginated, only first 100 are shown", () => {
-      cy.visitAndCheckAppMount("/searchtaxonomy");
-      const inputValue = "a";
-      cy.get("input[name=eu_taxonomy_search_bar_top]")
-        .should("not.be.disabled")
-        .click({ force: true })
-        .type(inputValue)
-        .type("{enter}")
-        .should("have.value", inputValue);
-      cy.get("h2").should("contain", "Results");
-      cy.get("table.p-datatable-table").should("exist");
-      cy.get(".p-paginator-current").should("contain.text", "Showing 1 to 100 of").contains("entries");
-    });
-  }
-);
+  it("Search for all companies containing 'a' and verify that results are paginated, only first 100 are shown", () => {
+    cy.visitAndCheckAppMount("/searchtaxonomy");
+    const inputValue = "a";
+    cy.get("input[name=eu_taxonomy_search_bar_top]")
+      .should("not.be.disabled")
+      .click({ force: true })
+      .type(inputValue)
+      .type("{enter}")
+      .should("have.value", inputValue);
+    cy.get("h2").should("contain", "Results");
+    cy.get("table.p-datatable-table").should("exist");
+    cy.get(".p-paginator-current").should("contain.text", "Showing 1 to 100 of").contains("entries");
+  });
+});
