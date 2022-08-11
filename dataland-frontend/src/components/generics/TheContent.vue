faf07225c3eeb48af42674be9bf21af35a96eb29
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-08-11 13:28:23 +0200

    commit missing components

---
+++ dataland-frontend/src/components/generics/TheContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="d-content col-12">
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheContent",
+};
+</script>
---
+++ dataland-frontend/src/components/generics/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-header m-0 fixed top-0 surface-900 h-4rem w-full grid align-items-center">
+    <div class="col-4 text-left">
+      <router-link to="/">
+        <img src="@/assets/images/logos/logo_dataland_long.svg" alt="image" class="pl-3" />
+      </router-link>
+    </div>
+
+    <div class="col-4">
+      <slot></slot>
+    </div>
+
+    <div class="col-4 flex justify-content-end" v-if="showUserProfileDropdown">
+      <UserProfileDropDown />
+    </div>
+  </div>
+  <!-- This is a spacer div whose only purpose is to ensure that no elements get hidden behind the header -->
+  <div class="h-4rem" />
+</template>
+<script>
+import UserProfileDropDown from "@/components/general/UserProfileDropDown";
+export default {
+  name: "TheHeader",
+  components: { UserProfileDropDown },
+  props: {
+    showUserProfileDropdown: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
