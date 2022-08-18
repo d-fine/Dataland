a01e4a9866866838c63bc3a5e406e914bd05c9ec
MarcTM01 <marc.meyer@d-fine.de>
2022-08-18 09:52:50 +0200

    DALA-470

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/eutaxonomy/nonfinancials/EuTaxonomyDetailsPerCashFlowType.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/eutaxonomy/nonfinancials/EuTaxonomyDetailsPerCashFlowType.kt
@@ -8,7 +8,12 @@ import java.math.BigDecimal
  * Figures to be filled out for some of the EU-Taxonomy questionnaire fields
  */
 data class EuTaxonomyDetailsPerCashFlowType(
-    @field:JsonProperty("totalAmount") val totalAmount: BigDecimal? = null,
-    @field:JsonProperty("alignedPercentage") val alignedPercentage: BigDecimal? = null,
-    @field:JsonProperty("eligiblePercentage") val eligiblePercentage: BigDecimal? = null
+    @field:JsonProperty("totalAmount")
+    val totalAmount: BigDecimal? = null,
+
+    @field:JsonProperty("alignedPercentage")
+    val alignedPercentage: BigDecimal? = null,
+
+    @field:JsonProperty("eligiblePercentage")
+    val eligiblePercentage: BigDecimal? = null
 )
