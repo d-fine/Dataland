ac59b5c9d37f34f577a86287d3ae7652db791a84
MarcTM01 <marc.meyer@d-fine.de>
2022-08-19 16:14:33 +0200

    Fixed previsit test and ensured that the previsit test actually fails if an error occurs

--- dataland-frontend/tests/e2e/utils/Cypress.ts
+++ dataland-frontend/tests/e2e/utils/Cypress.ts
@@ -16,13 +16,8 @@ export function doThingsInChunks<T>(
   });
 }
 
-export function wrapPromiseToCypressPromise(promise: Promise<any>): Bluebird<any> {
+export function wrapPromiseToCypressPromise<T>(promise: Promise<T>): Bluebird<T> {
   return new Cypress.Promise((resolve, reject) => {
-    promise
-      .then(
-        () => resolve("done"),
-        (reason) => reject(reason)
-      )
-      .catch((reason) => reject(reason));
+    promise.then((result) => resolve(result)).catch((error) => reject(error));
   });
 }
