e18a35d193db61d09fe5a5fc274b76f00db3d13c
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-08-24 14:42:07 +0200

    WiP

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/StoredCompany.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/StoredCompany.kt
@@ -2,6 +2,7 @@ package org.dataland.datalandbackend.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import javax.persistence.Column
+// import javax.persistence.ElementCollection
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
@@ -25,6 +26,7 @@ data class StoredCompany(
     @field:JsonProperty(required = true)
     val companyInformation: CompanyInformation,
 
+    // @ElementCollection
     @Column(name = "data_registered_by_dataland", nullable = false)
     @field:JsonProperty(required = true)
     val dataRegisteredByDataland: MutableList<DataMetaInformation>
