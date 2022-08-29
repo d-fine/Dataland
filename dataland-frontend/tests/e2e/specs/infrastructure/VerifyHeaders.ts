a589a7c8af40a960ba106b5be1256e5a2345dfd3
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-08-29 08:26:31 +0200

    new headers and simplified test

--- dataland-frontend/tests/e2e/specs/infrastructure/VerifyHeaders.ts
+++ dataland-frontend/tests/e2e/specs/infrastructure/VerifyHeaders.ts
@@ -1,7 +1,7 @@
-import { createCompanyAndGetId } from "../../utils/CompanyUpload";
-
 describe("As a user, I want to ensure that security relevant headers are set.", () => {
   function checkCommonHeaders(response: Cypress.Response<any>) {
+    expect(response.headers).to.have.property("cross-origin-embedder-policy", "require-corp");
+    expect(response.headers).to.have.property("cross-origin-resource-policy", "same-site");
     expect(response.headers).to.have.property("feature-policy", "none");
     expect(response.headers).to.have.property("referrer-policy", "no-referrer");
     expect(response.headers).to.have.property("strict-transport-security", "max-age=31536000 ; includeSubDomains");
@@ -18,25 +18,14 @@ describe("As a user, I want to ensure that security relevant headers are set.", 
   });
 
   it("test for backend response", () => {
-    cy.ensureLoggedIn("data_uploader", Cypress.env("KEYCLOAK_UPLOADER_PASSWORD"));
-    createCompanyAndGetId("Dummy Company");
-    cy.getKeycloakToken("data_reader", Cypress.env("KEYCLOAK_READER_PASSWORD")).then((token) => {
-      cy.request({
-        url: "/api/companies",
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + token,
-        },
-      }).then((response) => {
-        expect(response.headers).to.have.property("cache-control", "no-cache, no-store, max-age=0, must-revalidate");
-        expect(response.headers).to.have.property(
-          "content-security-policy",
-          "frame-ancestors 'none'; default-src 'none'"
-        );
-        checkCommonHeaders(response);
-        expect(response.headers).to.have.property("x-frame-options", "DENY");
-      });
+    cy.request("GET", Cypress.config("baseUrl") + "/api/actuator/health").then((response) => {
+      expect(response.headers).to.have.property("cache-control", "no-cache, no-store, max-age=0, must-revalidate");
+      expect(response.headers).to.have.property(
+        "content-security-policy",
+        "frame-ancestors 'none'; default-src 'none'"
+      );
+      checkCommonHeaders(response);
+      expect(response.headers).to.have.property("x-frame-options", "DENY");
     });
   });
 });
