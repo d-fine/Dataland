3fe41c0fa3fccbaed65be4f4a71740ba02eb04de
MarcTM01 <marc.meyer@d-fine.de>
2022-08-31 10:05:35 +0200

    Fixed more bugs

--- dataland-frontend/tests/e2e/utils/CompanyUpload.ts
+++ dataland-frontend/tests/e2e/utils/CompanyUpload.ts
@@ -6,7 +6,7 @@ export function fillCompanyUploadFields(companyName: string): void {
   cy.get("input[name=countryCode]").type("DE", { force: true });
   cy.get("input[name=reportingDateOfMarketCap]").type("2021-09-02", { force: true });
   cy.get("select[name=identifierType]").select("ISIN");
-  cy.get("input[name=identifierValue]").type("IsinValueId", { force: true });
+  cy.get("input[name=identifierValue]").type(`IsinValueId:${crypto.randomUUID()}`, { force: true });
 }
 
 export function createCompanyAndGetId(companyName: string): Cypress.Chainable<string> {
