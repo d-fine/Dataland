cc11249046892e76fa2ffb3522bc52710ee57fcf
d92048 <stephan.waldow@d-fine.de>
2022-08-31 16:12:03 +0200

    adjustments after review

--- dataland-keycloak/start_keycloak.sh
+++ dataland-keycloak/start_keycloak.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -eu
 
-arguments="$1"
+argument="$1"
 
 script_dir="$(dirname "$0")"
 cd $script_dir
 
-if [[ "$arguments" == initialize ]]; then
+if [[ "$argument" == initialize ]]; then
   echo "Initializing new keycloak realms"
  ./kc.sh import --file /opt/keycloak/data/import/master-realm.json
  ./kc.sh start --import-realm
 else
-  echo "Starting keycloak using: $arguments"
-  ./kc.sh "$arguments"
+  echo "Starting keycloak using: $argument"
+  ./kc.sh "$argument"
 fi
