0394a8c2675a03d0785ef519b476340b8a1bf140
Philip Puetsch <philip.puetsch@d-fine.de>
2022-08-31 17:00:51 +0200

    make the script pass many arguments
    Signed-off-by: Philip Puetsch <philip.puetsch@d-fine.de>

--- dataland-keycloak/start_keycloak.sh
+++ dataland-keycloak/start_keycloak.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -eu
 
-arguments="$1"
-
 script_dir="$(dirname "$0")"
 cd $script_dir
 
-if [[ "$arguments" == initialize ]]; then
+if [[ "$1" == initialize ]]; then
   echo "Initializing new keycloak realms"
  ./kc.sh import --file /opt/keycloak/data/import/master-realm.json
  ./kc.sh start --import-realm
 else
-  echo "Starting keycloak using: $arguments"
-  ./kc.sh "$arguments"
+  echo "Starting keycloak using: $@"
+  ./kc.sh "$@"
 fi
