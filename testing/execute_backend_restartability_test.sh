3aae92e2d3c91a5144295de9b00765312959cf57
MarcTM01 <marc.meyer@d-fine.de>
2022-09-01 10:25:53 +0200

    Further cleanup & Fixing

--- testing/execute_backend_restartability_test.sh
+++ testing/execute_backend_restartability_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
 CYPRESS_TEST_GROUP=101 ./testing/execute_e2e_tests.sh
-CYPRESS_TEST_GROUP=102 ./testing/execute_e2e_tests.sh
+CYPRESS_TEST_GROUP=102 CYPRESS_SINGLE_POPULATE="true" ./testing/execute_e2e_tests.sh
