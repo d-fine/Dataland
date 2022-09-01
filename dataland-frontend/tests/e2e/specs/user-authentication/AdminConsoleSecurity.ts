5841fb17465925627d1d97fdd343b11053c8bb73
d92048 <stephan.waldow@d-fine.de>
2022-09-01 11:32:17 +0200

    explicit visit url

--- dataland-frontend/tests/e2e/specs/user-authentication/AdminConsoleSecurity.ts
+++ dataland-frontend/tests/e2e/specs/user-authentication/AdminConsoleSecurity.ts
@@ -1,7 +1,6 @@
 describe("As a user I expect the admin console only to be reachable from localhost and not from remote", () => {
   it(`Test Admin Console not reachable from remote`, () => {
-    cy.visit("/keycloak/admin");
-    cy.wait(2000);
+    cy.visit(Cypress.config("baseUrl") + "/keycloak/admin");
     cy.get("h2").should("exist").should("contain", "Sorry an error occurred!");
     cy.url().should("contain", "nocontent");
   });
