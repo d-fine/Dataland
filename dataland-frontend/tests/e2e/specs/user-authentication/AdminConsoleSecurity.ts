0bf0d621dd6dbcba96f2a428c95d5b1d1ed0d76f
d92048 <stephan.waldow@d-fine.de>
2022-09-01 12:12:15 +0200

    linter

--- dataland-frontend/tests/e2e/specs/user-authentication/AdminConsoleSecurity.ts
+++ dataland-frontend/tests/e2e/specs/user-authentication/AdminConsoleSecurity.ts
@@ -1,5 +1,4 @@
 describe("As a user I expect the admin console only to be reachable from localhost and not from remote", () => {
-
   it(`Test Admin Console not reachable from remote`, () => {
     cy.visit("/keycloak/admin");
     cy.get("h2").should("exist").should("contain", "Sorry an error occurred!");
@@ -11,6 +10,4 @@ describe("As a user I expect the admin console only to be reachable from localho
     cy.get("h1").should("exist").should("contain", "Sign in to your account");
     cy.url().should("contain", "realms/master");
   });
-
-
 });
