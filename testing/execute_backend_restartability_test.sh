40dea7fffe8650c387bba13734b378cb8b60219a
MarcTM01 <marc.meyer@d-fine.de>
2022-09-01 13:58:32 +0200

    Merged restartability test into GH Matrix

--- testing/execute_backend_restartability_test.sh
+++ testing/execute_backend_restartability_test.sh
@@ -2,4 +2,4 @@
 set -ex
 CYPRESS_TEST_GROUP=101 ./testing/execute_e2e_tests.sh
 docker compose --project-name dala-e2e-test --profile testing down
-CYPRESS_TEST_GROUP=102 CYPRESS_SINGLE_POPULATE="true" ./testing/execute_e2e_tests.sh
+CYPRESS_TEST_GROUP=102 CYPRESS_SINGLE_POPULATE="true" CYPRESS_AWAIT_PREPOPULATION_RETRIES=3 ./testing/execute_e2e_tests.sh
