104e7ac1cf5c8471e6c5a40db25fce92044e86af
MarcTM01 <marc.meyer@d-fine.de>
2022-09-02 15:22:03 +0200

    Updated CD to include host entry

--- deployment/setup_admin_ssh_tunnel.sh
+++ deployment/setup_admin_ssh_tunnel.sh
@@ -5,5 +5,6 @@ source ./deployment/deployment_utils.sh
 
 setup_ssh
 
+sudo echo "127.0.0.1 dataland-admin" | sudo tee -a /etc/hosts
 ssh -L 6789:localhost:6789 ubuntu@"$TARGETSERVER_URL" &
 wait_for_health "https://localhost:6789/health" "Tunneled Admin Server"
