0d366c883301e9c266517065ccad3fce7fad220c
MarcTM01 <marc.meyer@d-fine.de>
2022-09-02 15:32:13 +0200

    Updated wait command

--- deployment/setup_admin_ssh_tunnel.sh
+++ deployment/setup_admin_ssh_tunnel.sh
@@ -7,4 +7,4 @@ setup_ssh
 
 sudo echo "127.0.0.1 dataland-admin" | sudo tee -a /etc/hosts
 ssh -L 6789:localhost:6789 -N -f ubuntu@"$TARGETSERVER_URL"
-wait_for_health "https://localhost:6789/health" "Tunneled Admin Server"
+wait_for_health "http://dataland-admin:6789/health" "Tunneled Admin Server"
