492b05f06b90e924bb441c81151b191494286f8c
Philip Puetsch <philip.puetsch@d-fine.de>
2022-09-08 12:00:00 +0200

    remove chunksize even more

--- dataland-frontend/tests/e2e/specs/prepopulation/Prepopulation.ts
+++ dataland-frontend/tests/e2e/specs/prepopulation/Prepopulation.ts
@@ -5,7 +5,7 @@ import {
   EuTaxonomyDataForNonFinancials,
   EuTaxonomyDataForFinancials,
 } from "../../../../build/clients/backend";
-const chunkSize = 20;
+const chunkSize = 10;
 
 describe(
   "As a user, I want to be able to see some data on the DataLand webpage",
