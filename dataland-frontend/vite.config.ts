3325d3090e7a66b4e9630f035b821a9e5ca2f09f
MarcTM01 <marc.meyer@d-fine.de>
2022-09-09 08:44:36 +0200

    Updated configs to provide Cypress coverage information in the coverage directory

--- dataland-frontend/vite.config.ts
+++ dataland-frontend/vite.config.ts
@@ -1,19 +1,27 @@
-import { resolve } from "path";
+import { fileURLToPath, URL } from "node:url";
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    istanbul({
+      include: "src/*",
+      exclude: ["node_modules", "test/"],
+      extension: [".js", ".ts", ".vue"],
+      requireEnv: true,
+    }),
+  ],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
-      "@clients": resolve(__dirname, "./build/clients"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@clients": fileURLToPath(new URL("./build/clients", import.meta.url)),
     },
   },
-  server: {
-    watch: {
-      ignored: ["**/coverage/**"],
-    },
+  build: {
+    sourcemap: true,
   },
 });
