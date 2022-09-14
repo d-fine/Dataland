273cb88a96a7dccd2368c06614ae2aa8215100ac
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-09-14 17:29:47 +0200

    fix test frontend

--- dataland-frontend/tests/component/components/general/UserProfileDropDown.cy.ts
+++ dataland-frontend/tests/component/components/general/UserProfileDropDown.cy.ts
@@ -1,35 +1,27 @@
 import UserProfileDropDown from "@/components/general/UserProfileDropDown.vue";
 import { mount } from "@vue/test-utils";
-import { getInjectedKeycloakObjectsForTest, getRequiredPlugins } from "../../TestUtils";
 
 describe("Component test for UserProfileDropDown", () => {
   const TestImagePath = "https://url.to/testImage";
-  const WrapperComponent = {
-    template: '<UserProfileDropDown ref="profileDropdown"/>',
-    components: { UserProfileDropDown },
-    provide: {
-      authenticated: true,
-      getKeycloakPromise() {
-        return Promise.resolve({
-          authenticated: true,
-          idTokenParsed: {
-            picture: TestImagePath,
-          },
-        });
-      },
-    },
-  };
 
   it("Should display a profile picture if the keycloak authenticator provides one", (done) => {
-    const wrapper: any = mount(WrapperComponent, {
+    const wrapper: any = mount(UserProfileDropDown, {
       global: {
-        plugins: getRequiredPlugins(),
-        provide: getInjectedKeycloakObjectsForTest(),
+        provide: {
+          authenticated: true,
+          getKeycloakPromise() {
+            return Promise.resolve({
+              authenticated: true,
+              idTokenParsed: {
+                picture: TestImagePath,
+              },
+            });
+          },
+        },
       },
     });
-    const profileDropdown = wrapper.vm.$refs.profileDropdown;
     wrapper.vm.$nextTick(() => {
-      expect(profileDropdown.$refs["profile-picture"].src).to.be(TestImagePath);
+      expect(wrapper.vm.$refs["profile-picture"].src).to.be.equal(TestImagePath);
       done();
     });
   });
