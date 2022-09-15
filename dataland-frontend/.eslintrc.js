5ca037867cc5ae4afa71313b082cf1207f777871
MarcTM01 <marc.meyer@d-fine.de>
2022-09-15 17:54:12 +0200

    Review fixes & Cleanup

--- dataland-frontend/.eslintrc.js
+++ dataland-frontend/.eslintrc.js
@@ -6,14 +6,6 @@ module.exports = {
     node: true,
   },
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/typescript"],
-  overrides: [
-    {
-      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
-      env: {
-        jest: true,
-      },
-    },
-  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
