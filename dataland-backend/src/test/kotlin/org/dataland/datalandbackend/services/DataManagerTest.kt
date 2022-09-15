654ef3430ea14847a7e8904bdf2ca3815d5340a4
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-09-15 12:02:09 +0200

    First test is working fine, lets check coverage report

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/DataManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/DataManagerTest.kt
@@ -2,11 +2,15 @@ package org.dataland.datalandbackend.services
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.dataland.datalandbackend.DatalandBackend
-import org.dataland.datalandbackend.interfaces.DataManagerInterface
+import org.dataland.datalandbackend.edcClient.api.DefaultApi
+import org.dataland.datalandbackend.edcClient.model.InsertDataResponse
 import org.dataland.datalandbackend.model.DataType
 import org.dataland.datalandbackend.model.StorableDataSet
 import org.dataland.datalandbackend.utils.TestDataProvider
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
@@ -18,12 +22,13 @@ import javax.transaction.Transactional
 @Transactional
 class DataManagerTest(
     @Autowired val objectMapper: ObjectMapper,
-    @Autowired val dataManager: DataManagerInterface,
     @Autowired val dataMetaInformationManager: DataMetaInformationManager,
-    @Autowired val companyManager: CompanyManager
+    @Autowired val companyManager: CompanyManager,
 ) {
 
     val testDataProvider = TestDataProvider(objectMapper)
+    val edcClientMock = mock(DefaultApi::class.java)
+    val dataManager = DataManager(edcClientMock, objectMapper, companyManager, dataMetaInformationManager)
 
     @Test
     fun `check that an exception is thrown when non matching dataId to dataType pair is requested from data storage`() {
@@ -35,10 +40,11 @@ class DataManagerTest(
         val euTaxonomyDataForNonFinancialsAsString = objectMapper.writeValueAsString(euTaxonomyDataForNonFinancials)
         val storableDataSet =
             StorableDataSet(companyId, DataType("eutaxonomy-non-financials"), euTaxonomyDataForNonFinancialsAsString)
-        /*
+        val storableDataSetAsString = objectMapper.writeValueAsString(storableDataSet)
+        `when`(edcClientMock.insertData(storableDataSetAsString)).thenReturn(InsertDataResponse("abcdefghijkl"))
         val dataId = dataManager.addDataSet(storableDataSet)
         assertThrows<IllegalArgumentException> {
-            dataManager.getDataSet(dataId, DataType("eutaxonomy-financials"))*
-        }*/
+            dataManager.getDataSet(dataId, DataType("eutaxonomy-financials"))
+        }
     }
 }
