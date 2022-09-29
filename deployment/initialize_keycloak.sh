e40f222cb1b2d4f27dd7bd10b4006b583f82dc2f
Florian Soffel <florian.soffel@d-fine.de>
2022-09-29 11:26:32 +0200

    Fixed blocking container

--- deployment/initialize_keycloak.sh
+++ deployment/initialize_keycloak.sh
@@ -15,6 +15,7 @@ scp -r "$script_dir"/../dataland-keycloak/dataland_theme/login/dist ubuntu@"$tar
 
 ssh ubuntu@"$target_server_url" "cd $location && sudo docker-compose build keycloak-initializer" || exit 1
 ssh ubuntu@"$target_server_url" "cd $location && sudo docker-compose run keycloak-initializer export" || exit 1
+ssh ubuntu@"$target_server_url" "cd $location && sudo docker-compose down keycloak-initializer" || exit 1
 
 delete_docker_volume_if_existent "$target_server_url" "$location" "keycloak_data"
 
