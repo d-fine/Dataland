93485c8a753f0e42a151d746fb7d72dd9bdb3e9e
Felipe Reck <felipe.reck@d-fine.de>
2022-09-30 15:26:38 +0200

    Replace cypress.request in actuator

--- dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
+++ dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
@@ -1,9 +1,14 @@
-describe("As a developer, I want to ensure that the deployment is okay", () => {
-  it("retrieve health info and check that its up", function () {
-    cy.request("GET", "/api/actuator/health").its("body.status").should("equal", "UP");
+import { ActuatorApi } from "../../../../build/clients/backend";
+
+describe("As a developer, I want to ensure that the deployment is okay", async () => {
+  it("retrieve health info and check that its up", async () => {
+    const health = await new ActuatorApi().health();
+    expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP");
+    // new ActuatorApi().health().then((health) => expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP"));
   });
 
-  it("retrieve info endpoint and check commit", function () {
-    cy.request("GET", "/api/actuator/info").its("body.git.commit.id.full").should("equal", Cypress.env("commit_id"));
+  it("retrieve info endpoint and check commit", async () => {
+    const info = await new ActuatorApi().info();
+    expect(JSON.parse(JSON.stringify(info.data)).git.commit.id.full).to.equal(Cypress.env("commit_id"));
   });
 });
