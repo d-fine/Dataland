c85fb2c609bb0b6c8d9452ebbfe0ff60469d657d
Felipe Reck <felipe.reck@d-fine.de>
2022-09-30 15:26:50 +0200

    Replace cypress.request in actuator

--- dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
+++ dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
@@ -7,6 +7,12 @@ describe("As a developer, I want to ensure that the deployment is okay", async (
     // new ActuatorApi().health().then((health) => expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP"));
   });
 
+  it("retrieve health info and check that its up", async () => {
+    const health = await new ActuatorApi().health();
+    expect(JSON.parse(JSON.stringify(health.data)).status).not.to.equal("UP");
+    // new ActuatorApi().health().then((health) => expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP"));
+  });
+
   it("retrieve info endpoint and check commit", async () => {
     const info = await new ActuatorApi().info();
     expect(JSON.parse(JSON.stringify(info.data)).git.commit.id.full).to.equal(Cypress.env("commit_id"));
