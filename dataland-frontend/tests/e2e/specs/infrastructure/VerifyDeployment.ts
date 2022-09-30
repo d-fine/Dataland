652fcd0507e55aed0dac201dcfadd5c7dcec05e3
Felipe Reck <felipe.reck@d-fine.de>
2022-09-30 16:06:48 +0200

    Removed TestCase, that was supposed to fail for checking; Linting

--- dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
+++ dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
@@ -4,13 +4,8 @@ describe("As a developer, I want to ensure that the deployment is okay", async (
   it("retrieve health info and check that its up", async () => {
     const health = await new ActuatorApi().health();
     expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP");
-    // new ActuatorApi().health().then((health) => expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP"));
-  });
-
-  it("retrieve health info and check that its up", async () => {
-    const health = await new ActuatorApi().health();
-    expect(JSON.parse(JSON.stringify(health.data)).status).not.to.equal("UP");
-    // new ActuatorApi().health().then((health) => expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP"));
+    // new ActuatorApi().health().then((health)
+    // => expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP"));
   });
 
   it("retrieve info endpoint and check commit", async () => {
