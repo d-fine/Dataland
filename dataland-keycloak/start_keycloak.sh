e30a2eb6450e05453b1fbddf268aa908ece81df3
Florian Soffel <florian.soffel@d-fine.de>
2022-10-04 14:52:16 +0200

    Try bugfix

--- dataland-keycloak/start_keycloak.sh
+++ dataland-keycloak/start_keycloak.sh
@@ -9,13 +9,14 @@ cd $script_dir
 if [[ "$mode" == initialize ]]; then
   echo "Initializing new keycloak realms"
   mkdir -p /opt/keycloak/data/import
-  cp /keycloak_realms/* /opt/keycloak/data/import/
-  cp /keycloak_users/* /opt/keycloak/data/import/ || echo "No importable users exist"
- ./kc.sh import --file /opt/keycloak/data/import/master-realm.json
- ./kc.sh start --import-realm
+  cp /keycloak_realms/* /opt/keycloak/data/import
+  cp /keycloak_users/* /opt/keycloak/data/import || echo "No importable users exist"
+  ./kc.sh import --file /opt/keycloak/data/import/master-realm.json
+  ./kc.sh start --import-realm
 elif [[ "$mode" == export ]]; then
   echo "Exporting users"
-  ./kc.sh export --dir /keycloak_users/ --users same_file --realm datalandsecurity
+  rm /keycloak_users/datalandsecurity-realm.json
+  ./kc.sh export --dir /keycloak_users --users same_file --realm datalandsecurity
   rm /keycloak_users/datalandsecurity-realm.json
 else
   cp -r /keycloak_realms/ /opt/keycloak/data/import/
