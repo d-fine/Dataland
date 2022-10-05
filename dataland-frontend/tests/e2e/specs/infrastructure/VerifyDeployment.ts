8d18fd3b74b07fe32dd12e40120932240b6c16f4
MarcTM01 <marc.meyer@d-fine.de>
2022-10-05 10:53:03 +0200

    Updated verify deployment test to use api clients

--- dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
+++ dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
@@ -1,15 +1,17 @@
-import { ActuatorApi } from "../../../../build/clients/backend";
+import { ActuatorApi } from "@clients/backend";
 
-describe("As a developer, I want to ensure that the deployment is okay", async () => {
-  it("retrieve health info and check that its up", async () => {
-    const health = await new ActuatorApi().health();
-    expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP");
-    // new ActuatorApi().health().then((health)
-    // => expect(JSON.parse(JSON.stringify(health.data)).status).to.equal("UP"));
+describe("As a developer, I want to ensure that the deployment is okay", () => {
+  it("retrieve health info and check that its up", () => {
+    cy.browserThen(new ActuatorApi().health()).then((healthResponse) => {
+      const data = healthResponse.data as any;
+      expect(data.status).to.equal("UP");
+    });
   });
 
-  it("retrieve info endpoint and check commit", async () => {
-    const info = await new ActuatorApi().info();
-    expect(JSON.parse(JSON.stringify(info.data)).git.commit.id.full).to.equal(Cypress.env("commit_id"));
+  it("retrieve info endpoint and check commit", () => {
+    cy.browserThen(new ActuatorApi().info()).then((healthResponse) => {
+      const data = healthResponse.data as any;
+      expect(data.git.commit.id.full).to.equal(Cypress.env("commit_id"));
+    });
   });
 });
