3c2428e7053e55c4b242efdc56b04f539e1a1d15
Florian Soffel <florian.soffel@d-fine.de>
2022-10-05 15:51:56 +0200

    Debug commit

--- dataland-keycloak/start_keycloak.sh
+++ dataland-keycloak/start_keycloak.sh
@@ -8,14 +8,18 @@ cd $script_dir
 if [[ "$mode" == initialize ]]; then
   echo "Initializing new keycloak realms"
   mkdir -p /opt/keycloak/data/import
+  mkdir -p /keycloak_users/test
   cp /keycloak_realms/* /opt/keycloak/data/import
-  cp /keycloak_users/* /opt/keycloak/data/import || echo "No importable users exist"
+  cp /keycloak_users/datalandsecurity-users-0.json /opt/keycloak/data/import || echo "No importable users exist"
+  cp /keycloak_users/datalandsecurity-users-0.json /keycloak_users/test
+  cp /keycloak_realms/datalandsecurity-realm.json /keycloak_users/test
   ./kc.sh import --file /opt/keycloak/data/import/master-realm.json
+  ./kc.sh import --dir /keycloak_users/test
   ./kc.sh start --import-realm
-elif [[ "$mode" == export ]]; then
+ elif [[ "$mode" == export ]]; then
   echo "Exporting users"
   ./kc.sh export --dir /keycloak_users --users same_file --realm datalandsecurity
-  rm /keycloak_users/datalandsecurity-realm.json
+  #rm /keycloak_users/datalandsecurity-realm.json
 else
   cp /keycloak_realms/* /opt/keycloak/data/import || true
   echo "Starting keycloak using: $@"
