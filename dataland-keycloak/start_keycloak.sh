7d05739510766684619a069df0f8e8576a535317
Florian Soffel <florian.soffel@d-fine.de>
2022-10-05 16:45:39 +0200

    Debug commit

--- dataland-keycloak/start_keycloak.sh
+++ dataland-keycloak/start_keycloak.sh
@@ -13,6 +13,9 @@ if [[ "$mode" == initialize ]]; then
   cp /keycloak_users/datalandsecurity-users-0.json /opt/keycloak/data/import || echo "No importable users exist"
   cp /keycloak_users/datalandsecurity-users-0.json /keycloak_users/test
   cp /keycloak_realms/datalandsecurity-realm.json /keycloak_users/test
+  for variable in $(env | grep KEYCLOAK_ | cut -d'=' -f1); do
+    sed s/\$\{"$variable"\}/"${!variable}"/g -i /keycloak_users/test/datalandsecurity-realm.json
+  done
   ./kc.sh import --file /opt/keycloak/data/import/master-realm.json
   ./kc.sh import --dir /keycloak_users/test
   ./kc.sh start --import-realm
@@ -22,6 +25,6 @@ if [[ "$mode" == initialize ]]; then
   #rm /keycloak_users/datalandsecurity-realm.json
 else
   cp /keycloak_realms/* /opt/keycloak/data/import || true
-  echo "Starting keycloak using: $@"
+  echo Starting keycloak using: "$@"
   ./kc.sh "$@"
 fi
