c1ab979c9f78ca8aea78829efa6cdbbd434c2bb6
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-10-06 10:49:55 +0200

    mor debug

--- dataland-keycloak/start_keycloak.sh
+++ dataland-keycloak/start_keycloak.sh
@@ -11,11 +11,11 @@ if [[ "$mode" == initialize ]]; then
   echo "Initializing new keycloak realms" >> $dataland_realm_folder/inner.log
   cp /keycloak_users/datalandsecurity-users-0.json $dataland_realm_folder || echo "No importable users exist"
   cp /keycloak_realms/datalandsecurity-realm.json $dataland_realm_folder
+  env > $dataland_realm_folder/environment.log
   for variable in $(env | grep KEYCLOAK_ | cut -d'=' -f1); do
     echo "Replacing variable $variable" >> $dataland_realm_folder/inner.log
-    sed s/\$\{"$variable"\}/"${!variable}"/g -i $dataland_realm_folder/datalandsecurity-realm.json
+    sed s%\$\{"$variable"\}%"${!variable}"%g -i $dataland_realm_folder/datalandsecurity-realm.json >> $dataland_realm_folder/inner.log 2>>$dataland_realm_folder/inner.log
   done
-  env > $dataland_realm_folder/environment.log
   ./kc.sh import --file /keycloak_realms/master-realm.json >> $dataland_realm_folder/inner.log
   ./kc.sh import --dir $dataland_realm_folder >> $dataland_realm_folder/inner.log
   ./kc.sh start >> $dataland_realm_folder/inner.log
