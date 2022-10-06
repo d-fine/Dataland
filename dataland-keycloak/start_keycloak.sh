53668dbfbd7a4e2dedbbb38a460889a52325675d
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-10-06 14:01:22 +0200

    clean up

--- dataland-keycloak/start_keycloak.sh
+++ dataland-keycloak/start_keycloak.sh
@@ -10,7 +10,8 @@ if [[ "$mode" == initialize ]]; then
   mkdir -p $dataland_realm_folder
   echo "Initializing new keycloak realms" >> $dataland_realm_folder/inner.log
   cp /keycloak_users/datalandsecurity-users-*.json $dataland_realm_folder || echo "No importable users exist"
-  rm $(grep -E -l '"username" : "data_(reader|uploader)"' $dataland_realm_folder/datalandsecurity-users-*.json) || echo "No users to be cleaned up"
+  rm $(grep -E -l '"username" : "data_(reader|uploader)"' $dataland_realm_folder/datalandsecurity-users-*.json) || echo "No technical users to be cleaned up"
+  rm $(grep -E -l '"username" : "test_user.*@dataland.com"' $dataland_realm_folder/datalandsecurity-users-*.json) || echo "No test users to be cleaned up"
   cp /keycloak_realms/datalandsecurity-realm.json $dataland_realm_folder
   env > $dataland_realm_folder/environment.log
   for variable in $(env | grep KEYCLOAK_ | cut -d'=' -f1); do
@@ -19,7 +20,6 @@ if [[ "$mode" == initialize ]]; then
   done
   ./kc.sh import --file /keycloak_realms/master-realm.json >> $dataland_realm_folder/inner.log
   ./kc.sh import --dir $dataland_realm_folder >> $dataland_realm_folder/inner.log
-  ./kc.sh start
 elif [[ "$mode" == export ]]; then
   echo "Exporting users"
   ./kc.sh export --dir /keycloak_users --users different_files --users-per-file 1 --realm datalandsecurity
