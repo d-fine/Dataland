b1298abd43a7a705b6c4cb13d262a2b3e6b83b58
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-10-07 08:01:38 +0200

    fixed counting mechanism

--- deployment/initialize_keycloak.sh
+++ deployment/initialize_keycloak.sh
@@ -54,7 +54,7 @@ timeout 300 bash -c "while ! ssh ubuntu@\"$target_server_url\" \"cd $location &&
 echo "Testing if the number of current users matches the number of exported users"
 current_users=$(ssh ubuntu@"$target_server_url" "docker exec $container_name /opt/keycloak/bin/kcadm.sh get users -r datalandsecurity --server http://localhost:8080/keycloak --realm master --user $KEYCLOAK_ADMIN --password $KEYCLOAK_ADMIN_PASSWORD | grep -c '\"username\" :'")
 all_users=$(ssh ubuntu@"$target_server_url" "docker exec -t $container_name bash -c 'ls /keycloak_users/datalandsecurity-users-*.json | wc -l'")
-test_users=$(ssh ubuntu@"$target_server_url" "docker exec $container_name grep -E -l \"test_user.*@dataland.com\" /keycloak_users/datalandsecurity-users-*.json | wc -l")
+test_users=$(ssh ubuntu@"$target_server_url" "docker exec $container_name bash -c 'grep -E -l \"test_user.*@dataland.com\" /keycloak_users/datalandsecurity-users-*.json | wc -l'")
 expected_users=$((all_users-test_users))
 if [[ ! $expected_users -eq $current_users ]]; then
   echo "Found $current_users but $expected_users were expected."
