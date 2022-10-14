c12eed30ae58df1cb96c70453e663aefa5a37341
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-10-14 10:52:27 +0200

    Fix the broken swagger application.properties and extend the existing Swagger test to assure that Authorization in swagger UI works

--- dataland-frontend/tests/e2e/specs/swagger-ui/Swagger.ts
+++ dataland-frontend/tests/e2e/specs/swagger-ui/Swagger.ts
@@ -1,16 +1,24 @@
 describe("As a user, I want to be able to use the swagger UI to send requests to the backend", () => {
-  it("Checks that one is able to open swagger and send a request", () => {
+  it("Checks that one is able to open swagger, authorize and send a get company request", () => {
+    cy.ensureLoggedIn();
     cy.visit(`/api/swagger-ui/index.html`)
-      .get("#operations-Actuator-health button.opblock-summary-control")
+      .get('button[class="btn authorize unlocked"]')
+      .click()
+      .get('button[class="btn modal-btn auth authorize button"]')
+      .eq(1)
+      .click()
+      .get('button[class="close-modal"]')
+      .click()
+      .get("#operations-company-data-controller-getCompanies button.opblock-summary-control")
       .should("exist")
       .click()
-      .get("#operations-Actuator-health button.try-out__btn")
+      .get("#operations-company-data-controller-getCompanies button.try-out__btn")
       .should("exist")
       .click()
-      .get("#operations-Actuator-health button.execute")
+      .get("#operations-company-data-controller-getCompanies button.execute")
       .should("exist")
       .click()
-      .get("#operations-Actuator-health .live-responses-table td.response-col_status")
+      .get("#operations-company-data-controller-getCompanies .live-responses-table td.response-col_status")
       .contains("200");
   });
 });
