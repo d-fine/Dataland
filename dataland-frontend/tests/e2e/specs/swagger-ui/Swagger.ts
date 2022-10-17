d2affbf20adcf8d28a395ed52d0ba50c5d4ca686
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-10-17 09:20:26 +0200

    Revert the new swagger test because it randomly fails with no obvious reason

--- dataland-frontend/tests/e2e/specs/swagger-ui/Swagger.ts
+++ dataland-frontend/tests/e2e/specs/swagger-ui/Swagger.ts
@@ -1,28 +1,16 @@
 describe("As a user, I want to be able to use the swagger UI to send requests to the backend", () => {
-  it(
-    "Checks that one is able to open swagger, authorize and send a get company request",
-    { browser: "!electron" },
-    () => {
-      cy.ensureLoggedIn();
-      cy.visit(`/api/swagger-ui/index.html`)
-        .get('button[class="btn authorize unlocked"]')
-        .click()
-        .get('button[class="btn modal-btn auth authorize button"]')
-        .eq(1)
-        .click()
-        .get('button[class="close-modal"]')
-        .click()
-        .get("#operations-company-data-controller-getCompanies button.opblock-summary-control")
-        .should("exist")
-        .click()
-        .get("#operations-company-data-controller-getCompanies button.try-out__btn")
-        .should("exist")
-        .click()
-        .get("#operations-company-data-controller-getCompanies button.execute")
-        .should("exist")
-        .click()
-        .get("#operations-company-data-controller-getCompanies .live-responses-table td.response-col_status")
-        .contains("200");
-    }
-  );
+  it("Checks that one is able to open swagger and send a request", () => {
+    cy.visit(`/api/swagger-ui/index.html`)
+      .get("#operations-Actuator-health button.opblock-summary-control")
+      .should("exist")
+      .click()
+      .get("#operations-Actuator-health button.try-out__btn")
+      .should("exist")
+      .click()
+      .get("#operations-Actuator-health button.execute")
+      .should("exist")
+      .click()
+      .get("#operations-Actuator-health .live-responses-table td.response-col_status")
+      .contains("200");
+  });
 });
