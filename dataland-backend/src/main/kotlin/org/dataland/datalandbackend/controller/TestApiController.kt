90e21a04bc2cea07a65a1542882364555badcb77
MarcTM01 <marc.meyer@d-fine.de>
2022-10-25 13:07:03 +0200

    Ensured that the testing endpoints are only exposed in local development or ci environments, not in the production build

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/TestApiController.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/TestApiController.kt
@@ -3,6 +3,7 @@ package org.dataland.datalandbackend.controller
 import org.dataland.datalandbackend.api.TestApi
 import org.dataland.datalandbackend.exceptions.InternalServerErrorApiException
 import org.dataland.skyminderClient.model.ContactInformation
+import org.springframework.context.annotation.Profile
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
  * API controller used for testing
  */
 @RestController
+@Profile("development", "ci")
 class TestApiController : TestApi {
     override fun getDummy500Response(): ResponseEntity<List<ContactInformation>> {
         throw InternalServerErrorApiException("This is a requested dummy 500 response")
--- dataland-frontend/tests/e2e/specs/infrastructure/Test500Response.ts
+++ dataland-frontend/tests/e2e/specs/infrastructure/Test500Response.ts
@@ -1,18 +1,46 @@
 import { wrapPromiseToCypressPromise } from "@e2e/utils/Cypress";
 import { getKeycloakToken } from "@e2e/utils/Auth";
 import { reader_name, reader_pw } from "@e2e/utils/Cypress";
+import { describeIf } from "../../support/TestUtility";
 describe("As a developer, I want to ensure that cypress behaves as expected", () => {
-  it("Test that 500 request interception can be disabled for individual requests", () => {
-    cy.intercept("/api/testing/getDummy500Response", { middleware: true }, (req) => {
-      req.headers["DATALAND-ALLOW-5XX"] = "true";
-    }).as("Allow 500");
+  describeIf(
+    "In the CI pipeline, testing endpoint should be exposed",
+    {
+      executionEnvironments: ["development", "developmentLocal"],
+      dataEnvironments: ["fakeFixtures", "realData"],
+    },
+    () => {
+      it("Test that 500 request interception can be disabled for individual requests", () => {
+        cy.intercept("/api/testing/getDummy500Response", { middleware: true }, (req) => {
+          req.headers["DATALAND-ALLOW-5XX"] = "true";
+        }).as("Allow 500");
 
-    cy.then(() => getKeycloakToken(reader_name, reader_pw)).then((token) =>
-      wrapPromiseToCypressPromise(
-        fetch("/api/testing/getDummy500Response", { headers: { Authorization: `Bearer ${token}` } }).then((r) => {
-          assert(r.status >= 500, "Expected a 500 response");
-        })
-      )
-    );
-  });
+        cy.then(() => getKeycloakToken(reader_name, reader_pw)).then((token) =>
+          wrapPromiseToCypressPromise(
+            fetch("/api/testing/getDummy500Response", { headers: { Authorization: `Bearer ${token}` } }).then((r) => {
+              assert(r.status >= 500, "Expected a 500 response");
+            })
+          )
+        );
+      });
+    }
+  );
+  describeIf(
+    "In the CD pipeline, testing endpoint should NOT be exposed",
+    {
+      executionEnvironments: ["preview"],
+      dataEnvironments: ["fakeFixtures", "realData"],
+    },
+    () => {
+      it("Test that the testing endpoints are not available", () => {
+        cy.then(() => getKeycloakToken(reader_name, reader_pw)).then((token) =>
+          wrapPromiseToCypressPromise(
+            fetch("/api/testing/getDummy500Response", { headers: { Authorization: `Bearer ${token}` } }).then((r) => {
+              assert(r.status == 404, "Expected a 404 response");
+            })
+          )
+        );
+      });
+    }
+  );
 });
