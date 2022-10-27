5f28ed3119ba0239f69599f9921a4657e7f3fd1a
MarcTM01 <marc.meyer@d-fine.de>
2022-10-27 09:42:41 +0200

    Fixed issues

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/TestApi.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/TestApi.kt
@@ -1,7 +1,6 @@
 package org.dataland.datalandbackend.api
 
 import io.swagger.v3.oas.annotations.Hidden
-import org.dataland.skyminderClient.model.ContactInformation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,5 +15,5 @@ interface TestApi {
      * A dummy function that returns a 500 response to test the internal server error handling behaviour
      */
     @GetMapping("/getDummy500Response")
-    fun getDummy500Response(): ResponseEntity<List<ContactInformation>>
+    fun getDummy500Response(): ResponseEntity<Unit>
 }
--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/TestApiController.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/controller/TestApiController.kt
@@ -2,7 +2,6 @@ package org.dataland.datalandbackend.controller
 
 import org.dataland.datalandbackend.api.TestApi
 import org.dataland.datalandbackend.exceptions.InternalServerErrorApiException
-import org.dataland.skyminderClient.model.ContactInformation
 import org.springframework.context.annotation.Profile
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @Profile("development", "ci")
 class TestApiController : TestApi {
-    override fun getDummy500Response(): ResponseEntity<List<ContactInformation>> {
+    override fun getDummy500Response(): ResponseEntity<Unit> {
         throw InternalServerErrorApiException("This is a requested dummy 500 response")
     }
 }
