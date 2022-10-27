7dbf7ec52e09bb4ad2c4d43ca4d717fadd6e2a28
Philip Puetsch <philip.puetsch@d-fine.com>
2022-10-27 11:14:56 +0200

    bugfix where copying files from a container did not work

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -7,12 +7,12 @@ docker compose --project-name dala-e2e-test --profile testing up -d || exit
 timeout 2400 sh -c "docker logs dala-e2e-test-e2etests-1 --follow" && E2ETEST_TIMEOUT_EXIT_CODE=$? || E2ETEST_TIMEOUT_EXIT_CODE=$?
 mkdir -p ./cypress/${CYPRESS_TEST_GROUP}
 mkdir -p ./reports/${CYPRESS_TEST_GROUP}
-docker cp dala-e2e-test-e2etests-1:/app/dataland-frontend/coverage/lcov.info ./lcov-${CYPRESS_TEST_GROUP}.info
-docker cp dala-e2e-test-e2etests-1:/app/dataland-frontend/cypress/. ./cypress/${CYPRESS_TEST_GROUP}/
-docker cp dala-e2e-test-e2etests-1:/app/dataland-e2etests/build/reports/. ./reports/${CYPRESS_TEST_GROUP}/
+docker cp dala-e2e-test-e2etests-1:/app/dataland-frontend/coverage/lcov.info ./lcov-${CYPRESS_TEST_GROUP}.info || true
+docker cp dala-e2e-test-e2etests-1:/app/dataland-frontend/cypress/. ./cypress/${CYPRESS_TEST_GROUP}/ || true
+docker cp dala-e2e-test-e2etests-1:/app/dataland-e2etests/build/reports/. ./reports/${CYPRESS_TEST_GROUP}/ || true
 
 mkdir -p ./dbdumps/${CYPRESS_TEST_GROUP}
-docker exec -i dala-e2e-test-backend-db-1 /bin/bash -c "PGPASSWORD=${BACKEND_DB_PASSWORD} pg_dump --username backend backend" > ./dbdumps/${CYPRESS_TEST_GROUP}/backend-db.sql
+docker exec -i dala-e2e-test-backend-db-1 /bin/bash -c "PGPASSWORD=${BACKEND_DB_PASSWORD} pg_dump --username backend backend" > ./dbdumps/${CYPRESS_TEST_GROUP}/backend-db.sql || true
 
 # Stop Backend causing JaCoCo to write Coverage Report, get it to pwd
 docker exec dala-e2e-test-backend-1 pkill -f spring
