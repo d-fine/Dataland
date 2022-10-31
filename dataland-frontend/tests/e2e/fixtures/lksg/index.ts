620521ce70d53df015fa2fd33b564b8f7ad3e573
Florian Soffel <florian.soffel@d-fine.de>
2022-10-31 16:29:01 +0100

    Fixed main

--- dataland-frontend/tests/e2e/fixtures/lksg/index.ts
+++ dataland-frontend/tests/e2e/fixtures/lksg/index.ts
@@ -5,7 +5,6 @@ import { LksgData } from "@clients/backend";
 
 export function exportFixturesLksg(): void {
   const companyInformationWithLksgData = generateFixtureDataset<LksgData>(generateLksgData, 150);
-  companyInformationWithLksgData[0].companyInformation.isTeaserCompany = true;
   fs.writeFileSync(
     "../testing/data/CompanyInformationWithLksgData.json",
     JSON.stringify(companyInformationWithLksgData, null, "\t")
