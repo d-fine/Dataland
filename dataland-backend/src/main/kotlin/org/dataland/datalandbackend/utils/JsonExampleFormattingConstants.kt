2836d3e08120d46d9cd88e7e1f805a2a1642e6e9
Florian Soffel <florian.soffel@d-fine.de>
2022-11-02 11:38:52 +0100

    Trying to satisfy Linter

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/utils/JsonExampleFormattingConstants.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/utils/JsonExampleFormattingConstants.kt
@@ -3,48 +3,48 @@ package org.dataland.datalandbackend.utils
 object JsonExampleFormattingConstants {
     const val EligibilityKpisDefaultValue: String = "{" +
             "\"CreditInstitution\": {" +
-                "\"taxonomyEligibleActivity\": {" +
-                    "\"value\": 0," +
-                    "\"quality\": \"Audited\"," +
-                    "\"dataSource\": {" +
-                        "\"report\": \"string\"," +
-                        "\"page\": 0" +
-                    "}," +
-                    "\"comment\": \"string\"" +
-                "}" +
+            "\"taxonomyEligibleActivity\": {" +
+            "\"value\": 0," +
+            "\"quality\": \"Audited\"," +
+            "\"dataSource\": {" +
+            "\"report\": \"string\"," +
+            "\"page\": 0" +
+            "}," +
+            "\"comment\": \"string\"" +
+            "}" +
             "}," +
             "\"InsuranceOrReinsurance\": {" +
-                "\"taxonomyEligibleActivity\": {" +
-                    "\"value\": 0," +
-                    "\"quality\": \"Audited\"," +
-                    "\"dataSource\": {" +
-                        "\"report\": \"string\"," +
-                        "\"page\": 0" +
-                    "}," +
-                    "\"comment\": \"string\"" +
-                "}" +
+            "\"taxonomyEligibleActivity\": {" +
+            "\"value\": 0," +
+            "\"quality\": \"Audited\"," +
+            "\"dataSource\": {" +
+            "\"report\": \"string\"," +
+            "\"page\": 0" +
+            "}," +
+            "\"comment\": \"string\"" +
+            "}" +
             "}," +
             "\"AssetManagement\": {" +
-                "\"taxonomyEligibleActivity\": {" +
-                    "\"value\": 0," +
-                    "\"quality\": \"Audited\"," +
-                    "\"dataSource\": {" +
-                        "\"report\": \"string\"," +
-                        "\"page\": 0" +
-                    "}," +
-                    "\"comment\": \"string\"" +
-                "}" +
+            "\"taxonomyEligibleActivity\": {" +
+            "\"value\": 0," +
+            "\"quality\": \"Audited\"," +
+            "\"dataSource\": {" +
+            "\"report\": \"string\"," +
+            "\"page\": 0" +
+            "}," +
+            "\"comment\": \"string\"" +
+            "}" +
             "}," +
             "\"InvestmentFirm\": {" +
-                "\"taxonomyEligibleActivity\": {" +
-                    "\"value\": 0," +
-                    "\"quality\": \"Audited\"," +
-                    "\"dataSource\": {" +
-                        "\"report\": \"string\"," +
-                        "\"page\": 0" +
-                    "}," +
-                    "\"comment\": \"string\"" +
-                "}" +
+            "\"taxonomyEligibleActivity\": {" +
+            "\"value\": 0," +
+            "\"quality\": \"Audited\"," +
+            "\"dataSource\": {" +
+            "\"report\": \"string\"," +
+            "\"page\": 0" +
+            "}," +
+            "\"comment\": \"string\"" +
+            "}" +
             "}" +
             "}"
 }
