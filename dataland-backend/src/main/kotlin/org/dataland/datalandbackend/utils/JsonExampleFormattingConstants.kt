e1854a828e1fd8085eed5057279e3c4a987dfe2f
Florian Soffel <florian.soffel@d-fine.de>
2022-11-02 13:36:10 +0100

    Removed duplicate code

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/utils/JsonExampleFormattingConstants.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/utils/JsonExampleFormattingConstants.kt
@@ -4,50 +4,27 @@ package org.dataland.datalandbackend.utils
  * Stores constants which do not seem fit to be defined at point of use
  */
 object JsonExampleFormattingConstants {
+    const val DATA_POINT_CONTENT = "{" +
+        "\"value\": 0," +
+        "\"quality\": \"Audited\"," +
+        "\"dataSource\": {" +
+        "\"report\": \"string\"," +
+        "\"page\": 0" +
+        "}," +
+        "\"comment\": \"string\"" +
+        "}"
     const val ELIGIBILITY_KPIS_DEFAULT_VALUE: String = "{" +
         "\"CreditInstitution\": {" +
-        "\"taxonomyEligibleActivity\": {" +
-        "\"value\": 0," +
-        "\"quality\": \"Audited\"," +
-        "\"dataSource\": {" +
-        "\"report\": \"string\"," +
-        "\"page\": 0" +
-        "}," +
-        "\"comment\": \"string\"" +
-        "}" +
+        "\"taxonomyEligibleActivity\": " + DATA_POINT_CONTENT +
         "}," +
         "\"InsuranceOrReinsurance\": {" +
-        "\"taxonomyEligibleActivity\": {" +
-        "\"value\": 0," +
-        "\"quality\": \"Audited\"," +
-        "\"dataSource\": {" +
-        "\"report\": \"string\"," +
-        "\"page\": 0" +
-        "}," +
-        "\"comment\": \"string\"" +
-        "}" +
+        "\"taxonomyEligibleActivity\": " + DATA_POINT_CONTENT +
         "}," +
         "\"AssetManagement\": {" +
-        "\"taxonomyEligibleActivity\": {" +
-        "\"value\": 0," +
-        "\"quality\": \"Audited\"," +
-        "\"dataSource\": {" +
-        "\"report\": \"string\"," +
-        "\"page\": 0" +
-        "}," +
-        "\"comment\": \"string\"" +
-        "}" +
+        "\"taxonomyEligibleActivity\": " + DATA_POINT_CONTENT +
         "}," +
         "\"InvestmentFirm\": {" +
-        "\"taxonomyEligibleActivity\": {" +
-        "\"value\": 0," +
-        "\"quality\": \"Audited\"," +
-        "\"dataSource\": {" +
-        "\"report\": \"string\"," +
-        "\"page\": 0" +
-        "}," +
-        "\"comment\": \"string\"" +
-        "}" +
+        "\"taxonomyEligibleActivity\": " + DATA_POINT_CONTENT +
         "}" +
         "}"
 }
