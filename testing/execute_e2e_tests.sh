d004b381b4e97874e71da90ac2350af0bd84f85e
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-03 18:28:30 +0100

    remove debugging statements, add admin proxy do image recycling

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -2,11 +2,6 @@
 set -euxo pipefail
 
 #Start E2E Test and wait for E2E Test completion
-echo ghcr.io/d-fine/dataland/dataland_inbound_proxy_${PROXY_ENVIRONMENT:-development}:${DATALAND_INBOUND_PROXY_PRODUCTION_VERSION}
-docker pull ghcr.io/d-fine/dataland/dataland_inbound_proxy_${PROXY_ENVIRONMENT:-development}:${DATALAND_INBOUND_PROXY_PRODUCTION_VERSION}
-echo ghcr.io/d-fine/dataland/dataland_keycloak:${DATALAND_KEYCLOAK_VERSION}
-docker pull ghcr.io/d-fine/dataland/dataland_keycloak:${DATALAND_KEYCLOAK_VERSION}
-
 docker compose --project-name dala-e2e-test --profile testing up -d || exit
 timeout 2400 sh -c "docker logs dala-e2e-test-e2etests-1 --follow"
 mkdir -p ./cypress/${CYPRESS_TEST_GROUP}
