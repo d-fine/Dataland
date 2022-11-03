e275aa16c02dc6fcf39792ff267f73be42c64dc7
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-03 15:02:31 +0100

    bugfixes

--- build-utils/rebuild_docker_images.sh
+++ build-utils/rebuild_docker_images.sh
@@ -24,8 +24,8 @@ echo "${docker_image_name^^}_VERSION=$input_sha1" >> $GITHUB_ENV
 sha1_manifest=$(docker manifest inspect "$full_image_reference" || echo "no sha1 manifest")
 if [ "$sha1_manifest" == "no sha1 manifest" ]
 then
-  echo "Requirements already satisfied!"
-else
   docker build -f "$dockerfile" . -t "$full_image_reference"
   docker push "$full_image_reference"
+else
+  echo "Requirements already satisfied!"
 fi
