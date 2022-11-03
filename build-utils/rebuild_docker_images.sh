f4f775c21900aba55733d1e49d3c9baa8a2330b0
kmachurami001 <katarzyna.machura-mika@pwc.com>
2022-11-03 09:14:35 +0100

    DALA-464: names' changes in the script

--- build-utils/rebuild_docker_images.sh
+++ build-utils/rebuild_docker_images.sh
@@ -8,31 +8,32 @@ set -euxo pipefail
 #TODO stick to naming convention - local variables lower case
 # TODO: Change the cat for the md5 hash to: tar all input files, and the md5 the tar file
 # TODO: Store md5 hash as ci parameter/variable
-DOCKER_IMAGE_NAME=$1
+docker_image_name=$1
 
 # shift removes the first argument which is not a file.
 # Now $@ contains only filenames where a change in any file content should trigger a rebuild
 shift
-DOCKERFILE=$1
+dockerfile=$1
 echo Rebuilding docker image. Parameters: "$@"
 set +x # don't print out file contents
-INPUT_FILES_CONTENT=$(cat "$@" build-utils/rebuild_docker_images.sh)
-echo "$INPUT_FILES_CONTENT" > "${CI_PROJECT_DIR}"/logs/rebuild_"${DOCKER_IMAGE_NAME}"_file_content.log
-INPUT_MD5=$(echo "$INPUT_FILES_CONTENT" | md5sum | awk '{print $1}')
-echo Input MD5 Hash: "$INPUT_MD5"
+input_files_content=$(cat "$@" build-utils/rebuild_docker_images.sh)
+echo "$input_files_content" > "${CI_PROJECT_DIR}"/logs/rebuild_"${docker_image_name}"_file_content.log
+input_md5=$(echo "$input_files_content" | md5sum | awk '{print $1}')
+echo Input MD5 Hash: "$input_md5"
 echo slug commit ref name: "$CI_COMMIT_REF_SLUG"
 set -x
 
 # Only execute the "build" command if the manifests are different.
-MD5_MANIFEST=$(docker manifest inspect "$CI_REGISTRY_IMAGE"/"$DOCKER_IMAGE_NAME":"$INPUT_MD5" || echo "no MD5 manifest")
-COMMIT_REF_MANIFEST=$(docker manifest inspect "$CI_REGISTRY_IMAGE"/"$DOCKER_IMAGE_NAME":"$CI_COMMIT_REF_SLUG" || echo "no commit ref manifest")
-if [ "$MD5_MANIFEST" == "$COMMIT_REF_MANIFEST" ]
+md5_manifest=$(docker manifest inspect "$CI_REGISTRY_IMAGE"/"$docker_image_name":"$input_md5" || echo "no MD5 manifest")
+commit_ref_manifest=$(docker manifest inspect "$CI_REGISTRY_IMAGE"/"$docker_image_name":"$CI_COMMIT_REF_SLUG" || echo "no commit ref manifest")
+if [ "$md5_manifest" == "$commit_ref_manifest" ]
+
 then
   echo "Requirements already satisfied!"
 else
-  docker pull "$CI_REGISTRY_IMAGE"/"$DOCKER_IMAGE_NAME":"$INPUT_MD5" || \
-  docker build -f "$DOCKERFILE" . -t "$CI_REGISTRY_IMAGE"/"$DOCKER_IMAGE_NAME":"$INPUT_MD5"
-  docker push "$CI_REGISTRY_IMAGE"/"$DOCKER_IMAGE_NAME":"$INPUT_MD5"
-  docker tag "$CI_REGISTRY_IMAGE"/"$DOCKER_IMAGE_NAME":"$INPUT_MD5" "$CI_REGISTRY_IMAGE"/"$DOCKER_IMAGE_NAME":"$CI_COMMIT_REF_SLUG"
-  docker push "$CI_REGISTRY_IMAGE"/"$DOCKER_IMAGE_NAME":"$CI_COMMIT_REF_SLUG"
+  docker pull "$CI_REGISTRY_IMAGE"/"$docker_image_name":"$input_md5" || \
+  docker build -f "$dockerfile" . -t "$CI_REGISTRY_IMAGE"/"$docker_image_name":"$input_md5"
+  docker push "$CI_REGISTRY_IMAGE"/"$docker_image_name":"$input_md5"
+  docker tag "$CI_REGISTRY_IMAGE"/"$docker_image_name":"$input_md5" "$CI_REGISTRY_IMAGE"/"$docker_image_name":"$CI_COMMIT_REF_SLUG"
+  docker push "$CI_REGISTRY_IMAGE"/"$docker_image_name":"$CI_COMMIT_REF_SLUG"
 fi
