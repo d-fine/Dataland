dbfd4ba634f7ecf6023f9982e77a46f70bc71d19
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-11-03 10:46:13 +0100

    typo review

--- dataland-frontend/tests/e2e/specs/company-metadata/SearchCompaniesForFrameworkData.ts
+++ dataland-frontend/tests/e2e/specs/company-metadata/SearchCompaniesForFrameworkData.ts
@@ -184,7 +184,7 @@ describe("As a user, I expect the search functionality on the /companies page to
   });
 
   describeIf(
-    "As a user, I expect substrings the autocomplete suggestions to be highlighted if they match my search string",
+    "As a user, I expect substrings of the autocomplete suggestions to be highlighted if they match my search string",
     {
       executionEnvironments: ["developmentLocal", "ci", "developmentCd"],
       dataEnvironments: ["fakeFixtures"],
