f74cef6afc7e0f676927192c8b8063a8645bdd04
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-04 10:07:26 +0100

    refactoring

---
+++ build-utils/rebuild_dataland_images.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -euxo pipefail
+
+export GITHUB_ENV=${GITHUB_ENV:-./.env}
+./build-utils/rebuild_docker_images.sh dataland_keycloak ./dataland-keycloak/Dockerfile \
+         ./dataland-keycloak/dataland_theme/ ./dataland-keycloak/start_keycloak.sh \
+         ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
+./build-utils/rebuild_docker_images.sh dataland_proxy_base \
+        ./dataland-inbound-proxy/DockerfileBase ./dataland-inbound-proxy/ ./dataland-frontend/src/assets/images/
+./build-utils/rebuild_docker_images.sh dataland_backend_production ./dataland-backend/Dockerfile ./dataland-backend/ \
+         ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
+./build-utils/rebuild_docker_images.sh dataland_backend_test ./dataland-backend/DockerfileTest ./dataland-backend/ \
+         ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
+./build-utils/rebuild_docker_images.sh dataland_inbound_admin_proxy ./dataland-inbound-admin-proxy/Dockerfile
+./build-utils/rebuild_docker_images.sh dataland_e2etests ./dataland-e2etests/Dockerfile \
+       ./dataland-backend/ ./dataland-e2etests/ ./dataland-frontend/tests/ \
+       ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
+./build-utils/rebuild_docker_images.sh dataland_pgadmin ./dataland-pgadmin/Dockerfile
+shopt -s extglob
+./build-utils/rebuild_docker_images.sh dataland_frontend_production ./dataland-frontend/Dockerfile \
+         ./dataland-frontend/!(tests*) ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
+./build-utils/rebuild_docker_images.sh dataland_frontend_test ./dataland-frontend/DockerfileTest \
+         ./dataland-frontend/ ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
+set -o allexport
+source $GITHUB_ENV
+set +o allexport
+echo "{\"commit\":\"$(git rev-parse HEAD)\",\"time\":\"$(git show -s --format=%aI HEAD)\",\"branch\":\"$(git rev-parse --abbrev-ref HEAD)\"}" > gitinfo
+PROXY_ENVIRONMENT="production" ./build-utils/rebuild_docker_images.sh dataland_inbound_proxy_production \
+        ./dataland-inbound-proxy/Dockerfile ./dataland-inbound-proxy/ ./dataland-frontend/src/assets/images/ ./gitinfo
+PROXY_ENVIRONMENT="development" ./build-utils/rebuild_docker_images.sh dataland_inbound_proxy_development \
+        ./dataland-inbound-proxy/Dockerfile ./dataland-inbound-proxy/ ./dataland-frontend/src/assets/images/ ./gitinfo
+cp $GITHUB_ENV github_env.log
--- build-utils/rebuild_single_docker_image.sh
+++ build-utils/rebuild_single_docker_image.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 # This Script checks if a docker image for a given set of input files already exists in the registry.
 # If not, it will be rebuilt and pushed
 # usage:
-# rebuild_docker_images.sh image_name dockerfile [additional_relevant_files...]
-# e.g.: rebuild_docker_images.sh <image-name> <path to Dockerfile> <first file that is relevant> <second file that is relevant> ...
+# rebuild_single_docker_image.sh image_name dockerfile [additional_relevant_files...]
+# e.g.: rebuild_single_docker_image.sh <image-name> <path to Dockerfile> <first file that is relevant> <second file that is relevant> ...
 # TODO: test that image names dont contain "-"
 docker_image_name=$1
 
@@ -21,8 +21,9 @@ echo Input sha1 Hash: "$input_sha1"
 full_image_reference="ghcr.io/d-fine/dataland/$docker_image_name:$input_sha1"
 echo "${docker_image_name^^}_VERSION=$input_sha1" >> $GITHUB_ENV
 sha1_manifest=$(docker manifest inspect "$full_image_reference" || echo "no sha1 manifest")
-if [ "$sha1_manifest" == "no sha1 manifest" ]
+if [ "$sha1_manifest" == "no sha1 manifest" ] || [ "${FORCE_BUILD:-}" == "true" ] || [[ "${COMMIT_MESSAGE:-}" == *"FORCE_BUILD"* ]];
 then
+  echo "rebuilding image $full_image_reference"
   docker build -f "$dockerfile" . -t "$full_image_reference" --build-arg PROXY_ENVIRONMENT="${PROXY_ENVIRONMENT:-}" --build-arg GITHUB_TOKEN="${GITHUB_TOKEN:-}" --build-arg GITHUB_USER="${GITHUB_USER:-}" --build-arg DATALAND_PROXY_BASE_VERSION="${DATALAND_PROXY_BASE_VERSION:-}"
   docker push "$full_image_reference"
 else
