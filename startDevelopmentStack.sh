947b44fd5ecc213102e3c4292b0a6cd6b359c740
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-04 11:58:45 +0100

    extract version from matrix job

--- startDevelopmentStack.sh
+++ startDevelopmentStack.sh
@@ -8,7 +8,6 @@ docker login ghcr.io -u $GITHUB_USER -p $GITHUB_TOKEN
 mkdir -p ./local/certs
 scp ubuntu@dataland-letsencrypt.duckdns.org:/etc/letsencrypt/live/dataland-local.duckdns.org/* ./local/certs
 
-export GITHUB_ENV=./.env
 ./build-utils/rebuild_backend_production_image.sh
 ./build-utils/rebuild_backend_test_image.sh
 ./build-utils/rebuild_e2etests_image.sh
@@ -23,7 +22,7 @@ source ./.env
 set +o allexport
 
 set -o allexport
-source ./github_env.log
+source ./*github_env.log
 set +o allexport
 
 # start containers with the stack except frontend and backend
