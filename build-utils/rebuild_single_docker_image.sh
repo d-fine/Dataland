68268e2fa6459990286e516f8e003393552555a5
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-04 12:19:39 +0100

    bugfix

--- build-utils/rebuild_single_docker_image.sh
+++ build-utils/rebuild_single_docker_image.sh
@@ -20,7 +20,7 @@ dockerfile=$1
 echo Rebuilding docker image. Parameters: "$@"
 input_sha1=$( \
   tar \
-  --exclude="node_modules/**" --exclude="build/**" --exclude=".gradle/**" --exclude="dist/**" \
+  --exclude="node_modules" --exclude="build" --exclude=".gradle" --exclude="dist" \
   --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2019-01-01' -cvf - "$0" "$@" \
   | sha1sum | awk '{print $1}' \
 )
