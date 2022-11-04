19ea6dbc8a6a9c988276e05858188a2ce9e68c83
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-04 08:40:25 +0100

    bugfix gitinfo file

--- dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
+++ dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
@@ -5,6 +5,10 @@ interface HealthResponse {
   status: string;
 }
 
+interface GitInfoResponse {
+  status: string;
+}
+
 describe("As a developer, I want to ensure that the deployment is okay", () => {
   it("retrieve health info and check that its up", () => {
     cy.browserThen(new ActuatorApi().health()).then((healthResponse) => {
@@ -15,7 +19,7 @@ describe("As a developer, I want to ensure that the deployment is okay", () => {
 
   it("retrieve info endpoint and check commit", () => {
     cy.request(`${getBaseUrl()}/gitinfo`)
-      .should("have.a.property", "body")
+      .then((response) => JSON.parse(response.body as string) as GitInfoResponse)
       .should("have.a.property", "commit")
       .should("eq", "Dummy");
     /**
