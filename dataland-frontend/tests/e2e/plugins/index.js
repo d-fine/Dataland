04afab3f4a702cf595356d79ce6ef212e3660216
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-08 21:23:58 +0100

    fix a test

--- dataland-frontend/tests/e2e/plugins/index.js
+++ dataland-frontend/tests/e2e/plugins/index.js
@@ -2,7 +2,6 @@ module.exports = (on, config) => {
   require("@cypress/code-coverage/task")(on, config);
   config.fixturesFolder = "../testing/data";
   config.supportFile = "tests/e2e/support/index.ts";
-  config.env.commit_id = require("git-commit-id")({ cwd: "../" });
 
   if (process.env.REALDATA === "true") {
     config.env["DATA_ENVIRONMENT"] = "realData";
--- dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
+++ dataland-frontend/tests/e2e/specs/infrastructure/VerifyDeployment.ts
@@ -21,6 +21,11 @@ describe("As a developer, I want to ensure that the deployment is okay", () => {
     cy.request(`${getBaseUrl()}/gitinfo`)
       .then((response) => JSON.parse(response.body as string) as GitInfoResponse)
       .should("have.a.property", "commit")
-      .should("eq", Cypress.env("commit_id") as string);
+      .then((commit_id) =>
+        assert(
+          Cypress.env("COMMIT_ID") === "ignore" || commit_id === Cypress.env("COMMIT_ID"),
+          "checking that commit_id of deployment matches the current one"
+        )
+      );
   });
 });
