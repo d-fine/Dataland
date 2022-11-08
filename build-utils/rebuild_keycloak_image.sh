972be7544937ec1599111d69ee75d3c8473c20b8
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-08 11:46:27 +0100

    include realms in input files for docker image

--- build-utils/rebuild_keycloak_image.sh
+++ build-utils/rebuild_keycloak_image.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 
 ./build-utils/rebuild_single_docker_image.sh dataland_keycloak ./dataland-keycloak/Dockerfile \
-         ./dataland-keycloak/dataland_theme/ ./dataland-keycloak/start_keycloak.sh \
+         ./dataland-keycloak/dataland_theme/ ./dataland-keycloak/start_keycloak.sh ./dataland-keycloak/realms/ \
          ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
