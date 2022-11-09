a982b23ddc8aa121f91920f475354d5d7b6fd919
Marc <108792843+MarcTM01@users.noreply.github.com>
2022-11-09 15:20:42 +0100

    DALA-547 Move from duckdns.org to dataland.com domains (#121)
    get rid of duckdns domains

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/Constants.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/Constants.kt
@@ -1,11 +1,10 @@
 package org.dataland.e2etests
-
 import org.dataland.datalandbackend.openApiClient.model.DataTypeEnum
 
-const val BASE_PATH_TO_DATALAND_BACKEND = "https://dataland-local.duckdns.org:443/api"
+const val BASE_PATH_TO_DATALAND_BACKEND = "https://local-dev.dataland.com:443/api"
 
 const val PATH_TO_KEYCLOAK_TOKENENDPOINT =
-    "https://dataland-local.duckdns.org:443/keycloak/realms/datalandsecurity/protocol/openid-connect/token"
+    "https://local-dev.dataland.com:443/keycloak/realms/datalandsecurity/protocol/openid-connect/token"
 
 const val TOKENREQUEST_GRANT_TYPE = "password"
 const val TOKENREQUEST_CLIENT_ID = "dataland-public"
