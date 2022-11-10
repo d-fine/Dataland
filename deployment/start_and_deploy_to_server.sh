0051b4bb0af624798424b51d0d0bdbaf723d3f22
Florian Soffel <florian.soffel@d-fine.de>
2022-11-10 12:00:49 +0100

    bugfix: not all containers have been deleted on deploy

--- deployment/start_and_deploy_to_server.sh
+++ deployment/start_and_deploy_to_server.sh
@@ -31,9 +31,9 @@ persistent_keycloak_backup_dir=/home/ubuntu/persistent_keycloak_backup
 keycloak_user_dir=$location/dataland-keycloak/users
 
 # shut down currently running dataland application and purge files on server
-ssh ubuntu@"$target_server_url" "cd \"$location\" && sudo docker compose down" || true
+ssh ubuntu@"$target_server_url" "cd \"$location\" && sudo docker compose down --remove-orphans" || true
 # make sure no remnants remain when docker-compose file changes
-ssh ubuntu@"$target_server_url" "docker kill $(docker ps -q); docker system prune --force; docker info"
+ssh ubuntu@"$target_server_url" "docker kill $(docker ps -q -a); docker rm $(docker ps -q -a); docker system prune --force; docker info"
 # delete pgadmin_config volume
 delete_docker_volume_if_existent_remotely "pgadmin_config" "$target_server_url" "$location"
 
