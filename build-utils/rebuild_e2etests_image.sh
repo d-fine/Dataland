50620315ca24d619d15ac7524304db3922593316
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-10 10:48:53 +0100

    Revert "remove .nyc_output"
    This reverts commit a94d0b839aa9e3ff7d9a29a3bf1c581179788c33.

--- build-utils/rebuild_e2etests_image.sh
+++ build-utils/rebuild_e2etests_image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#adapted
 set -euxo pipefail
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_e2etests ./dataland-e2etests/Dockerfile \
-       ./dataland-backend/ ./dataland-e2etests/ ./dataland-frontend/ ./testing/ \
+       ./dataland-backend/ ./dataland-e2etests/ ./dataland-frontend/tests/ ./testing/ \
        ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
