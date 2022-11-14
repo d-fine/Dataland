a371b797fdb5d5595abfee28e30fc0eb33afeb61
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-14 17:30:08 +0100

    fix shell scripts

--- build-utils/base_rebuild_gradle_dockerfile.sh
+++ build-utils/base_rebuild_gradle_dockerfile.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_gradle_base ./base-dockerfiles/GradleDockerfile \
-         ./gradle/ ./gradlew /app/
+         ./gradle/ ./gradlew ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
