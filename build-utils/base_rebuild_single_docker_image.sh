b3545c1bfa5187eb86b57266e8e099fa64b5865f
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-14 16:54:48 +0100

    Use build cache, only push if in GH actions

--- build-utils/base_rebuild_single_docker_image.sh
+++ build-utils/base_rebuild_single_docker_image.sh
@@ -36,22 +36,25 @@ full_image_reference="ghcr.io/d-fine/dataland/$docker_image_name:$input_sha1"
 echo "${docker_image_name^^}_VERSION=$input_sha1" >> ./${BUILD_SCRIPT:-default}_github_env.log
 echo "${docker_image_name^^}_VERSION=$input_sha1" >> ${GITHUB_OUTPUT:-/dev/null}
 sha1_manifest=$(docker manifest inspect "$full_image_reference" || echo "no sha1 manifest")
-if [ "$sha1_manifest" == "no sha1 manifest" ] || [ "${FORCE_BUILD:-}" == "true" ] || [[ "${COMMIT_MESSAGE:-}" == *"FORCE_BUILD"* ]]; then
+if [[ "$sha1_manifest" == "no sha1 manifest" ]] || [[ "${FORCE_BUILD:-}" == "true" ]] || [[ "${COMMIT_MESSAGE:-}" == *"FORCE_BUILD"* ]]; then
+  if [[ ${GITHUB_ACTIONS:-} == "true" ]]; then
+    echo "Running in Github Actions - using gha-type cache and --push flag"
+    docker_push_commandline_fragment=(--push)
+    docker_cache_commandline_fragment=(--cache-to "type=gha,scope=$GITHUB_REF_NAME-$docker_image_name" --cache-from "type=gha,scope=$GITHUB_REF_NAME-$docker_image_name")
+  else
+    echo "Running outside Github Actions - using no cache and no --push flag"
+    docker_push_commandline_fragment=( )
+    docker_cache_commandline_fragment=( )
+  fi
   echo "rebuilding image $full_image_reference"
-  docker build -f "$dockerfile" . -t "$full_image_reference" \
+  docker buildx build -f "$dockerfile" . -t "$full_image_reference" \
      --build-arg PROXY_ENVIRONMENT="${PROXY_ENVIRONMENT:-}" \
      --build-arg GITHUB_TOKEN="${GITHUB_TOKEN:-}" \
      --build-arg GITHUB_USER="${GITHUB_USER:-}" \
      --build-arg DATALAND_PROXY_BASE_VERSION="${DATALAND_PROXY_BASE_VERSION:-}" \
      --build-arg DATALAND_E2ETESTS_CORE_VERSION="${DATALAND_E2ETESTS_CORE_VERSION:-}" \
-     --build-arg DATALAND_BACKEND_BASE_VERSION="${DATALAND_BACKEND_BASE_VERSION:-}"
-  if [ "${GITHUB_ACTIONS:-}" == "true" ]; then
-    echo "Running in CI - Pushing images to ghcr"
-    docker push "$full_image_reference"
-  else
-    echo "Running outside Github Actions - skipping docker push"
-  fi
-
+     --build-arg DATALAND_BACKEND_BASE_VERSION="${DATALAND_BACKEND_BASE_VERSION:-}" \
+     "${docker_push_commandline_fragment[@]}" "${docker_cache_commandline_fragment[@]}"
 else
   echo "No Rebuild for $docker_image_name required"
 fi
