9d7b5a896120465e8be8cb75ad07edf75091de10
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-11-15 13:38:21 +0100

    fix descriptions

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/enums/sme/EnergyEfficiencyBracket.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/enums/sme/EnergyEfficiencyBracket.kt
@@ -3,7 +3,7 @@ package org.dataland.datalandbackend.model.enums.sme
 import io.swagger.v3.oas.annotations.media.Schema
 
 /**
- * An enum with the possible values for the age brackets of a company in the SME framework
+ * An enum with the possible values for the energy efficiency brackets of a company in the SME framework
  */
 @Schema(
     enumAsRef = true
