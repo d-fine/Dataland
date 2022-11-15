8d517137d6e9262f6cc1f685776e477ee1b9589a
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-15 11:09:00 +0100

    update base-rebuild-script.sh

--- build-utils/base_rebuild_single_docker_image.sh
+++ build-utils/base_rebuild_single_docker_image.sh
@@ -21,7 +21,7 @@ echo Rebuilding docker image. Parameters: "$@"
 
 input_sha1=$( \
   find "$0" "$@" -type f | \
-  grep -v '/node_modules/\|/dist/\|coverage\|/.gradle/\|/.git/\|/build/\|package-lock.json\|.log\|/local/\|/.nyc_output/' | \
+  grep -v '/node_modules/\|/dist/\|coverage\|/.gradle/\|/.git/\|/build/\|package-lock.json\|.log\|/local/\|/.nyc_output/\|/cypress/' | \
   sort -u | \
   xargs sha1sum | \
   awk '{print $1}' | \
