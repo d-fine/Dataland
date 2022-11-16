3d2c0acf702b8a97f532e5550aa1271d14049178
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-11-16 15:51:24 +0100

    Fix test which was not strict enough

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/annotations/DataTypesExtractorTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/annotations/DataTypesExtractorTest.kt
@@ -7,10 +7,11 @@ class DataTypesExtractorTest {
 
     @Test
     fun `check if extracting data types works as expected`() {
-        val expectedTypes = listOf("eutaxonomy-financials", "eutaxonomy-non-financials", "sfdr", "lksg")
+        val expectedTypes = listOf("eutaxonomy-financials", "eutaxonomy-non-financials", "lksg", "sfdr", "sme")
         val dataTypes = DataTypesExtractor().getAllDataTypes()
-        Assertions.assertTrue( // TODO
-            dataTypes.containsAll(expectedTypes),
+        Assertions.assertEquals(
+            expectedTypes.toSet(),
+            dataTypes.toSet(),
             "Found $dataTypes instead of the expected $expectedTypes"
         )
     }
