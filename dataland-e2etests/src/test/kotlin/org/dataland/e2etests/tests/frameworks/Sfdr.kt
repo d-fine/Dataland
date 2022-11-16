c0d66e5e187306293400e19032136b6b5b958e7b
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-11-16 14:38:35 +0100

    Switch Sfdr test to use api accessor

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/frameworks/Sfdr.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/frameworks/Sfdr.kt
@@ -5,29 +5,33 @@ import org.dataland.datalandbackend.openApiClient.api.SfdrDataControllerApi
 import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataSfdrData
 import org.dataland.datalandbackend.openApiClient.model.SfdrData
 import org.dataland.e2etests.BASE_PATH_TO_DATALAND_BACKEND
+import org.dataland.e2etests.utils.ApiAccessor
 import org.dataland.e2etests.utils.FrameworkTestDataProvider
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Sfdr {
 
-    private val dataControllerApi = SfdrDataControllerApi(BASE_PATH_TO_DATALAND_BACKEND)
-    private val testDataProvider = FrameworkTestDataProvider(SfdrData::class.java)
-    private val metaDataControllerApi = MetaDataControllerApi(BASE_PATH_TO_DATALAND_BACKEND)
+    private val apiAccessor = ApiAccessor()
 
     @Test
     fun `post a company with Sfdr data and check if the data can be retrieved correctly`() {
-        val (receivedDataMetaInformation, uploadedData) = postOneCompanyAndItsData(
-            testDataProvider,
-            { data: CompanyAssociatedDataSfdrData -> dataControllerApi.postCompanyAssociatedSfdrData(data) }
-        ) { companyId: String, data: SfdrData -> CompanyAssociatedDataSfdrData(companyId, data) }
-
-        val downloadedAssociatedData = dataControllerApi
-            .getCompanyAssociatedSfdrData(receivedDataMetaInformation.dataId)
-        val downloadedAssociatedDataType = metaDataControllerApi.getDataMetaInfo(receivedDataMetaInformation.dataId)
+        val listOfOneSfdrDataSet = apiAccessor.testDataProviderForSfdrData.getTData(1)
+        val listOfOneCompanyInformation = apiAccessor.testDataProviderForSfdrData
+            .getCompanyInformationWithoutIdentifiers(1)
+        val listOfUploadInfo = apiAccessor.uploadCompanyAndFrameworkDataForOneFramework(
+            listOfOneCompanyInformation,
+            listOfOneSfdrDataSet,
+            apiAccessor.sfdrUploaderFunction
+        )
+        val receivedDataMetaInformation = listOfUploadInfo[0].actualStoredDataMetaInfo
+        val downloadedAssociatedData = apiAccessor.dataControllerApiForSfdrData
+            .getCompanyAssociatedSfdrData(receivedDataMetaInformation!!.dataId)
+        val downloadedAssociatedDataType = apiAccessor.metaDataControllerApi
+            .getDataMetaInfo(receivedDataMetaInformation.dataId).dataType
 
         Assertions.assertEquals(receivedDataMetaInformation.companyId, downloadedAssociatedData.companyId)
-        Assertions.assertEquals(receivedDataMetaInformation.dataType, downloadedAssociatedDataType.dataType)
-        Assertions.assertEquals(uploadedData, downloadedAssociatedData.data)
+        Assertions.assertEquals(receivedDataMetaInformation.dataType, downloadedAssociatedDataType)
+        Assertions.assertEquals(listOfOneSfdrDataSet[0], downloadedAssociatedData.data)
     }
 }
