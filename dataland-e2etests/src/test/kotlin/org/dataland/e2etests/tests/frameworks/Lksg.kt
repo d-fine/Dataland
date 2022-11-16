70f62bd688718457a1e5db5842baf4b6df15c4d5
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-11-16 14:34:01 +0100

    Switch Lksg test to use api accessor

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/frameworks/Lksg.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/frameworks/Lksg.kt
@@ -1,33 +1,31 @@
 package org.dataland.e2etests.tests.frameworks
 
-import org.dataland.datalandbackend.openApiClient.api.LksgDataControllerApi
-import org.dataland.datalandbackend.openApiClient.api.MetaDataControllerApi
-import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataLksgData
-import org.dataland.datalandbackend.openApiClient.model.LksgData
-import org.dataland.e2etests.BASE_PATH_TO_DATALAND_BACKEND
-import org.dataland.e2etests.utils.FrameworkTestDataProvider
+import org.dataland.e2etests.utils.ApiAccessor
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Lksg {
 
-    private val lksgDataControllerApi = LksgDataControllerApi(BASE_PATH_TO_DATALAND_BACKEND)
-    private val testDataProviderForLksgData = FrameworkTestDataProvider(LksgData::class.java)
-    private val metaDataControllerApi = MetaDataControllerApi(BASE_PATH_TO_DATALAND_BACKEND)
+    private val apiAccessor = ApiAccessor()
 
     @Test
     fun `post a company with Lksg data and check if the data can be retrieved correctly`() {
-        val (receivedDataMetaInformation, uploadedData) = postOneCompanyAndItsData(
-            testDataProviderForLksgData,
-            { data: CompanyAssociatedDataLksgData -> lksgDataControllerApi.postCompanyAssociatedLksgData(data) }
-        ) { companyId: String, data: LksgData -> CompanyAssociatedDataLksgData(companyId, data) }
-
-        val downloadedAssociatedData = lksgDataControllerApi
-            .getCompanyAssociatedLksgData(receivedDataMetaInformation.dataId)
-        val downloadedAssociatedDataType = metaDataControllerApi.getDataMetaInfo(receivedDataMetaInformation.dataId)
+        val listOfOneLksgDataSet = apiAccessor.testDataProviderForLksgData.getTData(1)
+        val listOfOneCompanyInformation = apiAccessor.testDataProviderForLksgData
+            .getCompanyInformationWithoutIdentifiers(1)
+        val listOfUploadInfo = apiAccessor.uploadCompanyAndFrameworkDataForOneFramework(
+            listOfOneCompanyInformation,
+            listOfOneLksgDataSet,
+            apiAccessor.lksgUploaderFunction
+        )
+        val receivedDataMetaInformation = listOfUploadInfo[0].actualStoredDataMetaInfo
+        val downloadedAssociatedData = apiAccessor.dataControllerApiForLksgData
+            .getCompanyAssociatedLksgData(receivedDataMetaInformation!!.dataId)
+        val downloadedAssociatedDataType = apiAccessor.metaDataControllerApi
+            .getDataMetaInfo(receivedDataMetaInformation.dataId).dataType
 
         Assertions.assertEquals(receivedDataMetaInformation.companyId, downloadedAssociatedData.companyId)
-        Assertions.assertEquals(receivedDataMetaInformation.dataType, downloadedAssociatedDataType.dataType)
-        Assertions.assertEquals(uploadedData, downloadedAssociatedData.data)
+        Assertions.assertEquals(receivedDataMetaInformation.dataType, downloadedAssociatedDataType)
+        Assertions.assertEquals(listOfOneLksgDataSet[0], downloadedAssociatedData.data)
     }
 }
