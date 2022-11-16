32834464ff9011aa993f41dfdbb9af15f148b4ea
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-11-16 14:46:54 +0100

    Switch Sme test to use api accessor

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/frameworks/Sme.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/frameworks/Sme.kt
@@ -5,29 +5,36 @@ import org.dataland.datalandbackend.openApiClient.api.SmeDataControllerApi
 import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataSmeData
 import org.dataland.datalandbackend.openApiClient.model.SmeData
 import org.dataland.e2etests.BASE_PATH_TO_DATALAND_BACKEND
+import org.dataland.e2etests.utils.ApiAccessor
 import org.dataland.e2etests.utils.FrameworkTestDataProvider
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Sme {
 
+    private val apiAccessor = ApiAccessor()
     private val smeDataControllerApi = SmeDataControllerApi(BASE_PATH_TO_DATALAND_BACKEND)
     private val testDataProviderForSmeData = FrameworkTestDataProvider(SmeData::class.java)
     private val metaDataControllerApi = MetaDataControllerApi(BASE_PATH_TO_DATALAND_BACKEND)
 
     @Test
     fun `post a company with SME data and check if the data can be retrieved correctly`() {
-        val (receivedDataMetaInformation, uploadedData) = postOneCompanyAndItsData(
-            testDataProviderForSmeData,
-            { data: CompanyAssociatedDataSmeData -> smeDataControllerApi.postCompanyAssociatedSmeData(data) }
-        ) { companyId: String, data: SmeData -> CompanyAssociatedDataSmeData(companyId, data) }
-
-        val downloadedAssociatedData = smeDataControllerApi
-            .getCompanyAssociatedSmeData(receivedDataMetaInformation.dataId)
-        val downloadedAssociatedDataType = metaDataControllerApi.getDataMetaInfo(receivedDataMetaInformation.dataId)
+        val listOfOneSmeDataSet = apiAccessor.testDataProviderForSmeData.getTData(1)
+        val listOfOneCompanyInformation = apiAccessor.testDataProviderForSmeData
+            .getCompanyInformationWithoutIdentifiers(1)
+        val listOfUploadInfo = apiAccessor.uploadCompanyAndFrameworkDataForOneFramework(
+            listOfOneCompanyInformation,
+            listOfOneSmeDataSet,
+            apiAccessor.smeUploaderFunction
+        )
+        val receivedDataMetaInformation = listOfUploadInfo[0].actualStoredDataMetaInfo
+        val downloadedAssociatedData = apiAccessor.dataControllerApiForSmeData
+            .getCompanyAssociatedSmeData(receivedDataMetaInformation!!.dataId)
+        val downloadedAssociatedDataType = apiAccessor.metaDataControllerApi
+            .getDataMetaInfo(receivedDataMetaInformation.dataId).dataType
 
         Assertions.assertEquals(receivedDataMetaInformation.companyId, downloadedAssociatedData.companyId)
-        Assertions.assertEquals(receivedDataMetaInformation.dataType, downloadedAssociatedDataType.dataType)
-        Assertions.assertEquals(uploadedData, downloadedAssociatedData.data)
+        Assertions.assertEquals(receivedDataMetaInformation.dataType, downloadedAssociatedDataType)
+        Assertions.assertEquals(listOfOneSmeDataSet[0], downloadedAssociatedData.data)
     }
 }
