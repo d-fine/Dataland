de3312543bf4ca51d259a3a36cbcd34eb52f9d3c
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2022-11-18 14:41:27 +0100

    add hashing algorithm

--- dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/controller/ApiKeyController.kt
+++ dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/controller/ApiKeyController.kt
@@ -2,10 +2,9 @@ package org.dataland.datalandapikeymanager.controller
 
 import org.dataland.datalandapikeymanager.api.ApiKeyAPI
 import org.dataland.datalandapikeymanager.model.ApiKeyData
+import org.dataland.datalandapikeymanager.utils.ApiKeyGenerator
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
 
 /**
  * Controller for the api key manager
@@ -14,9 +13,7 @@ import java.time.LocalDate
 @RestController
 class ApiKeyController : ApiKeyAPI {
     override fun generateApiKey(daysValid: Long?): ResponseEntity<ApiKeyData> {
-        val username = SecurityContextHolder.getContext().authentication.principal.toString()
-        val expiryDate: LocalDate? = if (daysValid == null || daysValid <= 0) null else LocalDate.now().plusDays(daysValid)
-        return ResponseEntity.ok(ApiKeyData(username, expiryDate, "5678"))
+        return ResponseEntity.ok(ApiKeyGenerator().getNewApiKey(daysValid))
     }
 
     override fun validateApiKey(apiKey: String?): ResponseEntity<Boolean> {
--- dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/utils/ApiKeyGenerator.kt
+++ dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/utils/ApiKeyGenerator.kt
@@ -1,31 +1,62 @@
 package org.dataland.datalandapikeymanager.utils
 
+import org.bouncycastle.crypto.generators.Argon2BytesGenerator
+import org.bouncycastle.crypto.params.Argon2Parameters
 import org.dataland.datalandapikeymanager.model.ApiKeyData
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import java.security.SecureRandom
 import java.time.LocalDate
+import java.util.Base64
 import java.util.HexFormat
-import javax.servlet.http.HttpServletRequest
+
 
 class ApiKeyGenerator {
 
     private val keyByteLength = 40
+    private val saltByteLength = 16
+    private val hashByteLength = 32
+
+    private fun generateRandomByteArray(length: Int): ByteArray {
+        val bytes = ByteArray(length)
+        SecureRandom().nextBytes(bytes)
+        return bytes
+    }
+
+    private fun generateSalt(): ByteArray {
+        return generateRandomByteArray(saltByteLength)
+    }
 
     private fun generateApiKey(): String {
-        val bytes = ByteArray(keyByteLength)
-        SecureRandom().nextBytes(bytes)
-        return HexFormat.of().formatHex(bytes)
+        return HexFormat.of().formatHex(generateRandomByteArray(keyByteLength))
     }
 
-    fun getNewApiKey(daysValid: Long?, request: HttpServletRequest): ApiKeyData {
-        val expiryDate: LocalDate? = if (daysValid == null) null else LocalDate.now().plusDays(daysValid)
-        return ApiKeyData(getKeycloakUsername(request), expiryDate, generateApiKey())
+    private fun encodeToStorageFormat(input: ByteArray): String {
+        return Base64.getEncoder().encodeToString(input)
     }
 
-    private fun getKeycloakUsername(request: HttpServletRequest): String {
-        //return "NOT IMPLEMENTED"
-        val authenticationToken = request.userPrincipal as KeycloakAuthenticationToken
-        return authenticationToken.account.keycloakSecurityContext.idToken.subject
+    private fun decodeFromStorageFormat(input: String): ByteArray {
+        return Base64.getDecoder().decode(input)
     }
 
+    private fun hashApiKey(apiKey: String): String {
+        val salt = generateSalt()
+        val builder = Argon2Parameters.Builder(Argon2Parameters.ARGON2_id)
+            .withVersion(Argon2Parameters.ARGON2_VERSION_13)
+            .withIterations(3)
+            .withMemoryPowOfTwo(12)
+            .withParallelism(1)
+            .withSalt(salt)
+        val generator = Argon2BytesGenerator()
+        generator.init(builder.build())
+        val hash = ByteArray(hashByteLength)
+        generator.generateBytes(apiKey.toByteArray(), hash, 0, hash.size)
+        return encodeToStorageFormat(hash)
+    }
+
+    fun getNewApiKey(daysValid: Long?): ApiKeyData {
+        val username = SecurityContextHolder.getContext().authentication.principal.toString()
+        val expiryDate: LocalDate? = if (daysValid == null || daysValid <= 0) null else LocalDate.now().plusDays(daysValid)
+        val newApiKey = generateApiKey()
+        return ApiKeyData(username, expiryDate, newApiKey)
+    }
 }
