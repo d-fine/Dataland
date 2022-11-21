128a4ea81d86342b1cd3ed408d01d77305a321a1
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-21 15:52:47 +0100

    fixes a name

--- dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/model/ApiKeyMetaInfo.kt
+++ dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/model/ApiKeyMetaInfo.kt
@@ -7,16 +7,16 @@ import java.time.LocalDate
  * --- API model ---
  * Class for defining the meta info associated to an API key
  * @param username of the owning user
- * @param role defines the role of the user
+ * @param roles defines the role of the user
  * @param expiryDate specifies how long the API key can be used
  */
 data class ApiKeyMetaInfo(
-    @field:JsonProperty(required = true)
+        @field:JsonProperty(required = true)
     val username: String,
 
-    @field:JsonProperty(required = true)
-    val role: List<String>,
+        @field:JsonProperty(required = true)
+    val roles: List<String>,
 
-    @field:JsonProperty(required = true)
+        @field:JsonProperty(required = true)
     val expiryDate: LocalDate?,
 )
