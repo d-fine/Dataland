a39b12821439f88926a5109c99e31cd58d50ee04
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-23 11:33:13 +0100

    fix a bug

--- dataland-keycloak-adapter/src/main/kotlin/org/dataland/keycloakAdapter/support/keycloak/KeycloakGrantedAuthoritiesConverter.kt
+++ dataland-keycloak-adapter/src/main/kotlin/org/dataland/keycloakAdapter/support/keycloak/KeycloakGrantedAuthoritiesConverter.kt
@@ -1,5 +1,6 @@
 package org.dataland.keycloakAdapter.support.keycloak
 
+import org.slf4j.LoggerFactory
 import org.springframework.core.convert.converter.Converter
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -11,11 +12,13 @@ import org.springframework.stereotype.Component
  */
 @Component
 class KeycloakGrantedAuthoritiesConverter : Converter<Jwt, Collection<GrantedAuthority?>> {
+    private val logger = LoggerFactory.getLogger(javaClass)
     /**
      * the convert method
      */
     override fun convert(jwt: Jwt): Collection<GrantedAuthority> {
         val realmRoles = jwt.getClaimAsMap("realm_access")["roles"] as Collection<String>?
+        logger.info("converted roles $realmRoles")
         return realmRoles?.map { SimpleGrantedAuthority(it) } ?: listOf()
     }
 }
