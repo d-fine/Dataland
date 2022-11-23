96fb4a3e691c65482619ee6638092ac9473c944b
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-23 08:27:25 +0100

    fixes Dockerfiles and buildscripts

--- build-utils/rebuild_e2etests_image.sh
+++ build-utils/rebuild_e2etests_image.sh
@@ -8,5 +8,5 @@ source ./*github_env.log
 set +o allexport
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_e2etests ./dataland-e2etests/Dockerfile \
-       ./dataland-backend/ ./dataland-e2etests/ ./dataland-frontend/ ./testing/ \
+       ./dataland-backend/ ./dataland-keycloak-adapter/ ./dataland-e2etests/ ./dataland-frontend/ ./testing/ \
        ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
