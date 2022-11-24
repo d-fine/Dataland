bc6377980a1c2c6e141971e777f93b7d2ccd2833
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-24 13:22:57 +0100

    fix merge error

--- dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/configurations/UnprotectedSecurityConfig.kt
+++ dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/configurations/UnprotectedSecurityConfig.kt
@@ -1,16 +1,12 @@
-<<<<<<<< HEAD:dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/configurations/UnprotectedSecurityConfig.kt
 package org.dataland.datalandapikeymanager.configurations
-========
-package org.dataland.keycloakAdapter.config
->>>>>>>> main:dataland-keycloak-adapter/src/main/kotlin/org/dataland/keycloakAdapter/config/UnprotectedSecurityConfig.kt
 
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
-import org.springframework.stereotype.Component
 
 /**
  * This class contains all security configurations for the api key manager to make all endpoints public.
@@ -18,31 +14,20 @@ import org.springframework.stereotype.Component
  * the need of any token-authorization.
  */
 @Profile("unprotected")
-<<<<<<<< HEAD:dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/configurations/UnprotectedSecurityConfig.kt
 @KeycloakConfiguration
 // TODO: get rid of deprecated WebSecurityConfigurerAdapter
 class UnprotectedSecurityConfig : KeycloakWebSecurityConfigurerAdapter() {
 
-========
-@Component
-class UnprotectedSecurityConfig {
-    /**
-     * Defines the Session Authentication Strategy
-     */
->>>>>>>> main:dataland-keycloak-adapter/src/main/kotlin/org/dataland/keycloakAdapter/config/UnprotectedSecurityConfig.kt
     @Bean
-    fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
+    @Override
+    override fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy {
         return NullAuthenticatedSessionStrategy()
     }
 
-    /**
-     * Defines the default Security Filter Chain
-     */
-    @Bean
-    fun unprotectedFilterChain(http: HttpSecurity): DefaultSecurityFilterChain? {
+    @Override
+    override fun configure(http: HttpSecurity) {
         http
-            .authorizeRequests().anyRequest().permitAll()
+                .authorizeRequests().anyRequest().permitAll()
         http.csrf().disable()
-        return http.build()
     }
 }
