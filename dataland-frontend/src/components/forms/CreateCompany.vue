8d8a9be9b42688403121832796efe0a20e72b885
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-24 15:59:30 +0100

    fixes a bug again

--- dataland-frontend/src/components/forms/CreateCompany.vue
+++ dataland-frontend/src/components/forms/CreateCompany.vue
@@ -68,7 +68,8 @@ import { FormKit, FormKitSchema } from "@formkit/vue";
 import SuccessUpload from "@/components/messages/SuccessUpload.vue";
 import { SchemaGenerator } from "@/services/SchemaGenerator";
 import { ApiClientProvider } from "@/services/ApiClients";
-import backend from "@backendOpenApiSpec";
+
+import backend from "@projectRoot/dataland-backend/backendOpenApi.json";
 import FailedUpload from "@/components/messages/FailedUpload.vue";
 import Card from "primevue/card";
 import PrimeButton from "primevue/button";
