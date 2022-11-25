ae31118dd571f997dd2c1a50c8bbaec1c679479f
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-11-25 12:30:05 +0100

    TODO

--- dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/api/ApiKeyAPI.kt
+++ dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/api/ApiKeyAPI.kt
@@ -31,9 +31,11 @@ interface ApiKeyAPI {
         value = ["/generateApiKey"],
         produces = ["application/json"]
     )
-    @PreAuthorize("hasRole('ROLE_USER')") // TODO why?  (Emanuel asking)
-    // TODO  PPU: welche Rolle man hat ist eigentlich egal
-    // TODO - aber alle Rollen die man hat müssen dann auch mit dem API-Key assoziiert werden.
+    @PreAuthorize("hasRole('ROLE_USER')") /* TODO why?  (Emanuel asking)
+    TODO PPU: welche Rolle man hat ist eigentlich egal
+    TODO aber alle Rollen die man hat müssen dann auch mit dem API-Key assoziiert werden.
+    TODO Emanuel: Aber warum Oauth zusätzlich? Ist das wegen der swagger UI Authorize Geschichte?
+    */
     @SecurityRequirement(name = "default-bearer-auth")
     @SecurityRequirement(name = "default-oauth")
     /**
