f524f469ea413fb5c5f5fa44fde0651e0b40bdd9
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-11-25 11:55:31 +0100

    active field to ApiKeyMetaInfo.kt

--- dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/model/ApiKeyMetaInfo.kt
+++ dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/model/ApiKeyMetaInfo.kt
@@ -9,14 +9,19 @@ import java.time.LocalDate
  * @param keycloakUserId of the owning Keycloak user
  * @param keycloakRoles defines the roles of the owning Keycloak user
  * @param expiryDate specifies until which date the API key can be used
+ * @param active specifies if the associated api key
  */
 data class ApiKeyMetaInfo(
-    @field:JsonProperty(required = true)
-    val keycloakUserId: String,
+    @field:JsonProperty(required = false)
+    val keycloakUserId: String? = null,
 
-    @field:JsonProperty(required = true)
-    val keycloakRoles: List<String>,
+    @field:JsonProperty(required = false)
+    val keycloakRoles: List<String>? = null,
 
+    @field:JsonProperty(required = false)
+    val expiryDate: LocalDate? = null,
+
+    // TODO This should not be stored in DB, because it is calculated on runtime via expiryDate
     @field:JsonProperty(required = true)
-    val expiryDate: LocalDate?,
+    val active: Boolean,
 )
