611afdab71d713cfaa996e22a98750001abe2b7f
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-29 17:30:23 +0100

    restartability test for apikey

--- testing/execute_backend_restartability_test.sh
+++ testing/execute_backend_restartability_test.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
-set -ex
+#!/bin/bash
+set -euxo pipefail
+source ./testing/get_keycloak_token.sh
+api_key=$(getApiKeyWithUsernamePassword data_reader "$KEYCLOAK_READER_PASSWORD" "https://local-dev.dataland.com")
+curl -X 'GET' \
+  'https://local-dev.dataland.com/api/companies' \
+  -H 'accept: application/json' \
+  -H "dataland-api-key: $api_key"
 CYPRESS_TEST_GROUP=101 ./testing/execute_e2e_tests.sh
 docker compose --project-name dala-e2e-test --profile testing down
 CYPRESS_TEST_GROUP=102 CYPRESS_SINGLE_POPULATE="true" CYPRESS_AWAIT_PREPOPULATION_RETRIES=3 ./testing/execute_e2e_tests.sh
+curl -X 'GET' \
+  'https://local-dev.dataland.com/api/companies' \
+  -H 'accept: application/json' \
+  -H "dataland-api-key: $api_key"
