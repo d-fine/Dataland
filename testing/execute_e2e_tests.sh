acab95c992d70ed79d580a92e4362b1c1e295b2e
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-30 16:01:05 +0100

    adaptions to include coverage of api-key-manager

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -29,8 +29,8 @@ docker exec -i dala-e2e-test-api-key-manager-db-1 /bin/bash -c "PGPASSWORD=${API
 # Stop Backend causing JaCoCo to write Coverage Report, get it to pwd
 docker exec dala-e2e-test-backend-1 pkill -f spring
 timeout 90 sh -c "docker logs dala-e2e-test-backend-1 --follow" > /dev/null
-docker cp dala-e2e-test-backend-1:/app/dataland-backend/build/jacoco/bootRun.exec ./bootRun-${CYPRESS_TEST_GROUP}.exec
-
+docker cp dala-e2e-test-backend-1:/app/dataland-backend/build/jacoco/bootRun.exec ./backend-bootRun-${CYPRESS_TEST_GROUP}.exec
+docker cp dala-e2e-test-api-key-manager-1:/app/dataland-api-key-manager/build/jacoco/bootRun.exec ./api-key-manager-bootRun-${CYPRESS_TEST_GROUP}.exec
 
 # This test exists, because an update of SLF4J-API lead to no logging output after the spring logo was printed.
 # This was discovered only after the PR was merged.
