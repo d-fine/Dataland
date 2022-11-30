2afa8ed67ff940e9ab2fd2241f82ef941391c36d
Philip Puetsch <philip.puetsch@d-fine.com>
2022-11-30 12:09:05 +0100

    changes the ApiKeyManager

--- dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/services/ApiKeyManager.kt
+++ dataland-api-key-manager/src/main/kotlin/org/dataland/datalandapikeymanager/services/ApiKeyManager.kt
@@ -1,80 +1,55 @@
 package org.dataland.datalandapikeymanager.services
 
-import org.bouncycastle.crypto.generators.Argon2BytesGenerator
-import org.bouncycastle.crypto.params.Argon2Parameters
-import org.dataland.datalandapikeymanager.entities.StoredHashedAndBase64EncodedApiKeyEntity
+import org.dataland.datalandapikeymanager.entities.ApiKeyEntity
 import org.dataland.datalandapikeymanager.model.ApiKeyAndMetaInfo
 import org.dataland.datalandapikeymanager.model.ApiKeyMetaInfo
 import org.dataland.datalandapikeymanager.model.RevokeApiKeyResponse
-import org.dataland.datalandapikeymanager.model.StoredHashedAndBase64EncodedApiKey
-import org.dataland.datalandapikeymanager.repositories.StoredHashedAndBase64EncodedApiKeyRepository
-import org.dataland.datalandbackendutils.apikey.ApiKeyPrevalidator
-import org.dataland.datalandbackendutils.utils.EncodingUtils
-import org.dataland.datalandbackendutils.utils.EncodingUtils.encodeToBase64
+import org.dataland.datalandapikeymanager.repositories.ApiKeyRepository
+import org.dataland.datalandbackendutils.apikey.ApiKeyUtility
+import org.dataland.datalandbackendutils.apikey.ParsedApiKey
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder
 import org.springframework.stereotype.Component
 import java.security.SecureRandom
-import java.time.Instant
-import java.time.temporal.ChronoUnit
+import java.time.*
 import java.util.HexFormat
+
 /**
  * A class for handling the generation, validation and revocation of an api key
  */
 @Component("ApiKeyManager")
 class ApiKeyManager
-(@Autowired private val storedHashedAndBase64EncodedApiKeyRepository: StoredHashedAndBase64EncodedApiKeyRepository) {
+(@Autowired private val apiKeyRepository: ApiKeyRepository) {
 
-    private companion object {
-        private const val keyByteLength = 40
-        private const val saltByteLength = 16
-        private const val hashByteLength = 32
+    private val keyByteLength = 40
+    private val saltByteLength = 16
+    private val hashByteLength = 32
 
-        private const val argon2Iterations = 3
-        private const val argon2MemoryPowOfTwo = 16
-        private const val argon2Parallelisms = 1
-    }
-
-    private val apiKeyPrevalidator = ApiKeyPrevalidator()
+    private val argon2Iterations = 3
+    private val argon2MemoryPowOfTwo = 4
+    private val argon2Parallelisms = 1
 
     private val validationMessageNoApiKeyRegistered = "Your Dataland account has no API key registered. " +
-        "Please generate one."
+            "Please generate one."
     private val validationMessageWrongApiKey = "The API key you provided for your Dataland account is not correct."
     private val validationMessageExpiredApiKey = "The API key you provided for your Dataland account is expired."
     private val validationMessageSuccess = "The API key you provided was successfully validated."
 
     private val revokementMessageNonExistingApiKey = "Your Dataland account has no API key registered. Therefore no " +
-        "revokement took place."
+            "revokement took place."
     private val revokementMessageSuccess = "The API key for your Dataland account was successfully revoked."
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    private val utf8Charset = Charsets.UTF_8
-
-    private fun hashString(inputString: String, salt: ByteArray): ByteArray {
-        val builder = Argon2Parameters.Builder(Argon2Parameters.ARGON2_id)
-            .withVersion(Argon2Parameters.ARGON2_VERSION_13)
-            .withIterations(argon2Iterations)
-            .withMemoryPowOfTwo(argon2MemoryPowOfTwo)
-            .withParallelism(argon2Parallelisms)
-            .withSalt(salt)
-        val generator = Argon2BytesGenerator()
-        generator.init(builder.build())
-        val hash = ByteArray(hashByteLength)
-        generator.generateBytes(inputString.toByteArray(), hash, 0, hash.size)
-        return hash
-    }
+    private val apiKeyUtility = ApiKeyUtility()
 
     private fun generateRandomByteArray(length: Int): ByteArray {
         val bytes = ByteArray(length)
         SecureRandom().nextBytes(bytes)
         return bytes
-    }
-
-    private fun generateSalt(): ByteArray {
-        return generateRandomByteArray(saltByteLength)
     }
 
     private fun generateApiKeySecretAndEncodeToHex(): String {
@@ -85,14 +60,11 @@ class ApiKeyManager
         return SecurityContextHolder.getContext().authentication
     }
 
-    private fun getKeycloakUserIdBase64Encoded(authentication: Authentication): String {
-        return encodeToBase64(authentication.name.toByteArray(utf8Charset))
-    }
-
     private fun calculateExpiryDate(daysValid: Int?): Long? {
-        return when (daysValid) {
-            null -> null
-            else -> Instant.now().plus(daysValid.toLong(), ChronoUnit.DAYS).epochSecond
+        return if (daysValid == null) {
+            null
+        } else {
+            (daysValid * 86400) + Instant.now().epochSecond
         }
     }
 
@@ -111,60 +83,21 @@ class ApiKeyManager
     fun generateNewApiKey(daysValid: Int?): ApiKeyAndMetaInfo {
         val apiKeyMetaInfo = generateApiKeyMetaInfo(daysValid)
 
-        val newSalt = generateSalt()
-        val keycloakUserIdBase64Encoded = encodeToBase64(apiKeyMetaInfo.keycloakUserId!!.toByteArray(Charsets.UTF_8))
-        val newApiKeyWithoutCrc32Value = keycloakUserIdBase64Encoded + "_" + generateApiKeySecretAndEncodeToHex()
-        val newCrc32Value = EncodingUtils.calculateCrc32Value(newApiKeyWithoutCrc32Value.toByteArray(utf8Charset))
-        val newApiKey = newApiKeyWithoutCrc32Value + "_" + newCrc32Value
-        val newApiKeyHashedAndBase64Encoded = encodeToBase64(hashString(newApiKey, newSalt))
-
-        val hashedAndBase64EncodedApiKeyEntityToStore = StoredHashedAndBase64EncodedApiKeyEntity(
-            newApiKeyHashedAndBase64Encoded,
-            keycloakUserIdBase64Encoded,
-            apiKeyMetaInfo.keycloakRoles!!,
-            apiKeyMetaInfo.expiryDate,
-            encodeToBase64(newSalt)
-        )
-
-        storedHashedAndBase64EncodedApiKeyRepository.save(hashedAndBase64EncodedApiKeyEntityToStore)
+        val secret = generateApiKeySecretAndEncodeToHex()
+        val parsedApiKey = ParsedApiKey(apiKeyMetaInfo.keycloakUserId!!, secret)
+        val encodedSecret = Argon2PasswordEncoder(
+                saltByteLength,
+                hashByteLength,
+                argon2Parallelisms,
+                argon2MemoryPowOfTwo,
+                argon2Iterations
+        ).encode(secret)
+        val apiKeyEntity = ApiKeyEntity(encodedSecret, apiKeyMetaInfo)
+        apiKeyRepository.save(apiKeyEntity)
         logger.info(
-            "Generated Api Key with hashed value $newApiKeyHashedAndBase64Encoded and meta info $apiKeyMetaInfo."
+                "Generated Api Key with encoded secret value $encodedSecret and meta info $apiKeyMetaInfo."
         )
-        return ApiKeyAndMetaInfo(newApiKey, apiKeyMetaInfo)
-    }
-
-    private fun checkIfApiKeyForUserIsCorrectAndReturnApiKeyMetaInfoWithActivityStatus(
-        receivedApiKeyHashedAndBase64Encoded: String,
-        storedHashedAndBase64EncodedApiKeyOfUser: StoredHashedAndBase64EncodedApiKey,
-        keycloakUserIdBase64Encoded: String
-    ): ApiKeyMetaInfo {
-        if (receivedApiKeyHashedAndBase64Encoded !=
-            storedHashedAndBase64EncodedApiKeyOfUser.apiKeyHashedAndBase64Encoded
-        ) {
-            logger.info(
-                "The provided Api Key " +
-                    "for the encoded Keycloak user Id $keycloakUserIdBase64Encoded is not correct."
-            )
-            return ApiKeyMetaInfo(active = false, validationMessage = validationMessageWrongApiKey)
-        } else {
-            val expiryDateOfApiKey = storedHashedAndBase64EncodedApiKeyOfUser.apiKeyMetaInfo.expiryDate
-            val currentTime = Instant.now().epochSecond
-            val activityStatus =
-                (expiryDateOfApiKey ?: currentTime) >= currentTime
-            logger.info(
-                "Validated Api Key with encoded salt ${storedHashedAndBase64EncodedApiKeyOfUser.saltBase64Encoded} " +
-                    "and calculated hashed and encoded value $receivedApiKeyHashedAndBase64Encoded. " +
-                    "The activity status of the API key is $activityStatus."
-            )
-            var validationMessageToReturn = validationMessageSuccess
-            if (!activityStatus) {
-                validationMessageToReturn = validationMessageExpiredApiKey
-            }
-            return storedHashedAndBase64EncodedApiKeyOfUser.apiKeyMetaInfo.copy(
-                active = activityStatus,
-                validationMessage = validationMessageToReturn
-            )
-        }
+        return ApiKeyAndMetaInfo(apiKeyUtility.convertToApiKey(parsedApiKey), apiKeyMetaInfo)
     }
 
     /**
@@ -173,30 +106,37 @@ class ApiKeyManager
      * @return the found api keys meta info
      */
     fun validateApiKey(receivedApiKey: String): ApiKeyMetaInfo {
-        val receivedAndParsedApiKey = apiKeyPrevalidator.parseApiKey(receivedApiKey)
+        val receivedAndParsedApiKey = apiKeyUtility.parseApiKey(receivedApiKey)
 
-        val storedHashedAndBase64EncodedApiKeyOfUserOptional = storedHashedAndBase64EncodedApiKeyRepository.findById(
-            receivedAndParsedApiKey.parsedKeycloakUserIdBase64Encoded
+        val apiKeyEntityOptional = apiKeyRepository.findById(
+                receivedAndParsedApiKey.keycloakUserId
         )
 
-        if (storedHashedAndBase64EncodedApiKeyOfUserOptional.isEmpty) {
+        if (apiKeyEntityOptional.isEmpty) {
             logger.info(
-                "Dataland user with the encoded Keycloak user Id " +
-                    "${receivedAndParsedApiKey.parsedKeycloakUserIdBase64Encoded} has no API key registered."
+                    "Dataland user with the encoded Keycloak user Id " +
+                            "${receivedAndParsedApiKey.keycloakUserId} has no API key registered."
             )
             return ApiKeyMetaInfo(active = false, validationMessage = validationMessageNoApiKeyRegistered)
         }
-
-        val storedHashedAndBase64EncodedApiKeyOfUser =
-            storedHashedAndBase64EncodedApiKeyOfUserOptional.get().toApiModel()
-        val salt = EncodingUtils.decodeFromBase64(storedHashedAndBase64EncodedApiKeyOfUser.saltBase64Encoded)
-        val receivedApiKeyHashedAndBase64Encoded = encodeToBase64(hashString(receivedApiKey, salt))
-
-        return checkIfApiKeyForUserIsCorrectAndReturnApiKeyMetaInfoWithActivityStatus(
-            receivedApiKeyHashedAndBase64Encoded,
-            storedHashedAndBase64EncodedApiKeyOfUser,
-            receivedAndParsedApiKey.parsedKeycloakUserIdBase64Encoded
+        val apiKeyEntity = apiKeyEntityOptional.get()
+        if (!Argon2PasswordEncoder().matches(receivedAndParsedApiKey.apiKeySecret, apiKeyEntity.encodedSecret)) {
+            return ApiKeyMetaInfo(active = false, validationMessage = validationMessageWrongApiKey)
+        }
+        val expiryDateOfApiKey = apiKeyEntity.expiryDate
+        val currentTime = Instant.now().epochSecond
+        val active =
+                (expiryDateOfApiKey ?: currentTime) >= currentTime
+        logger.info(
+                "Validated Api Key ${receivedApiKey} " +
+                        "The activity status of the API key is $active."
         )
+
+        var validationMessage = validationMessageSuccess
+        if (!active) {
+            validationMessage = validationMessageExpiredApiKey
+        }
+        return ApiKeyMetaInfo(apiKeyEntity.keycloakUserId, apiKeyEntity.keycloakRoles, apiKeyEntity.expiryDate, active, validationMessage)
     }
 
     /**
@@ -205,24 +145,24 @@ class ApiKeyManager
      */
     fun revokeApiKey(): RevokeApiKeyResponse {
         val authentication = getAuthentication()
-        val keycloakUserIdBase64Encoded = getKeycloakUserIdBase64Encoded(authentication)
         val revokementProcessSuccessful: Boolean
         val revokementProcessMessage: String
 
-        if (storedHashedAndBase64EncodedApiKeyRepository.findById(keycloakUserIdBase64Encoded).isEmpty) {
+        val keycloakUserId = authentication.name!!
+        val apiKeyEntityOptional = apiKeyRepository.findById(keycloakUserId)
+        if (apiKeyEntityOptional.isEmpty
+        ) {
             revokementProcessSuccessful = false
             revokementProcessMessage = revokementMessageNonExistingApiKey
             logger.info(
-                "No revokement took place since there is no API key registered for the encoded " +
-                    "Keycloak user Id $keycloakUserIdBase64Encoded."
+                    "Revokement failed, no API key registered for the Keycloak user Id $keycloakUserId"
             )
         } else {
-            storedHashedAndBase64EncodedApiKeyRepository.deleteById(keycloakUserIdBase64Encoded)
+            apiKeyRepository.delete(apiKeyEntityOptional.get())
             revokementProcessSuccessful = true
             revokementProcessMessage = revokementMessageSuccess
             logger.info(
-                "The API key for the encoded Keycloak user Id $keycloakUserIdBase64Encoded was successfully " +
-                    "removed from storage."
+                    "The API key for the  Keycloak user Id $keycloakUserId was successfully removed from storage."
             )
         }
         return RevokeApiKeyResponse(revokementProcessSuccessful, revokementProcessMessage)
