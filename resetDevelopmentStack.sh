10aeab1152cb5dfbcba2a7fb1df871e20aee8508
Florian Soffel <florian.soffel@d-fine.de>
2022-12-02 11:50:53 +0100

    Fixed uppercase issue with resetDevelopmentStack.sh

--- resetDevelopmentStack.sh
+++ resetDevelopmentStack.sh
@@ -31,7 +31,8 @@ echo "Initializing Keycloak..."
 docker compose --profile init up --build &
 
 project_name=$(basename "$(pwd)")
-keycloak_initializer_container="$project_name"-keycloak-initializer-1
+lowercase_project_name=$(echo "$project_name" | tr '[:upper:]' '[:lower:]')
+keycloak_initializer_container="$lowercase_project_name"-keycloak-initializer-1
 while ! docker logs $keycloak_initializer_container 2>/dev/null | grep -q "Added user 'admin' to realm 'master'"; do
  echo "Waiting for Keycloak to finish initializing..."
  sleep 5
