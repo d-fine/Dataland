b67132e7a15429dbaa655331174bd78c65c2b644
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2022-12-05 16:39:09 +0100

    Intentionally break download test to check if CI shows it

--- dataland-frontend/tests/e2e/specs/data-request/DataRequestProcess.ts
+++ dataland-frontend/tests/e2e/specs/data-request/DataRequestProcess.ts
@@ -18,7 +18,7 @@ describe("As a user I expect a data request page where I can download an excel t
 
     const downloadedFilename = Cypress.config("downloadsFolder") + "/Dataland_Request_Template.xlsx";
 
-    cy.readFile("./src/assets/excel-files/Dataland_Request_Template.xlsx", "base64", { timeout: 15000 }).then(
+    cy.readFile("./src/assets/excel-files/Dataland_Request_Template_broken.xlsx", "base64", { timeout: 15000 }).then(
       (expectedBase64EncodedExcelTemplate) => {
         cy.readFile(downloadedFilename, "base64", { timeout: 15000 }).should("eq", expectedBase64EncodedExcelTemplate);
       }
