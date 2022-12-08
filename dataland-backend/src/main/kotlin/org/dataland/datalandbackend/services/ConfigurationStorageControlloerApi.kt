6dcace36f853950978c7ddae7899a8fb707a0298
d92048 <stephan.waldow@d-fine.de>
2022-12-08 16:02:35 +0100

    modifies ConfigurationStorageControlloerApi.kt to see if it runs locally and in CI

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/ConfigurationStorageControlloerApi.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/ConfigurationStorageControlloerApi.kt
@@ -3,6 +3,7 @@ package org.dataland.datalandbackend.services
 import org.dataland.datalandinternalstorage.openApiClient.api.StorageControllerApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.io.File
 
 /**
  * This class holds the configuration properties and the beans to auto-configure this library in a spring-boot
@@ -15,8 +16,17 @@ class ConfigurationStorageControlloerApi {
      */
     @Bean
     fun getApiInternalClient(): StorageControllerApi {
-        return StorageControllerApi(
-            basePath = "http://internal-storage:8080/internal-storage"
-        )
+        val fileName ="/.dockerenv"
+        var file = File(fileName)
+        var fileExists =file.exists()
+        return if (fileExists) {
+            StorageControllerApi(
+                basePath = "http://internal-storage:8080/internal-storage"
+            )
+        } else{
+            StorageControllerApi(
+                basePath = "http://localhost:8082/internal-storage"
+            )
+        }
     }
 }
