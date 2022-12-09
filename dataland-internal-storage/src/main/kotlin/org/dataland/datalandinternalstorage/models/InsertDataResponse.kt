78390c5026dbce3d8441c2756d51bdb6dd9eb36f
d92048 <stephan.waldow@d-fine.de>
2022-12-09 10:23:12 +0100

    modifies description in InsertDataResponse.kt

--- dataland-internal-storage/src/main/kotlin/org/dataland/datalandinternalstorage/models/InsertDataResponse.kt
+++ dataland-internal-storage/src/main/kotlin/org/dataland/datalandinternalstorage/models/InsertDataResponse.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
  * --- API model for Dataland-internal-storage-API ---
- * Response model for the case that data is inserted into the internal storage via the Dataland
+ * Response model for the case that data is inserted into the internal storage via Dataland
  * @param dataId unique identifier consisting of all information needed to identify the data (asset) in the storage
  */
 data class InsertDataResponse(
