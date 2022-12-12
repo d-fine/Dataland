166e4fac2ae05e36f9fdd91b9399ba66af6d3c52
Marcin Stanczyk (PL) <marcin.stanczyk@pwc.com>
2022-12-12 10:53:38 +0100

    changes in unit test to avoid counting from midnight

--- dataland-frontend/tests/component/utils/DateFormatUtils.cy.ts
+++ dataland-frontend/tests/component/utils/DateFormatUtils.cy.ts
@@ -14,11 +14,12 @@ describe("Unit test for Date Formatting", () => {
   });
 
   it("It should return the number of days to the given date", () => {
+    // we add 2.7h to avoid counting from midnight
     // startDate: 1639263600000 - 12 Dec 2021
-    expect(formatExpiryDate(365, 1639263600000)).to.eql("Mon, Dec 12, 2022");
+    expect(formatExpiryDate(365, 1639263600000 + 21100000)).to.eql("Mon, Dec 12, 2022");
     // startDate: 1670777487587 - 11 Dec 2022
-    expect(formatExpiryDate(3, 1670777487587)).to.eql("Wed, Dec 14, 2022");
+    expect(formatExpiryDate(0, 1670777487587 + 21100000)).to.eql("Sun, Dec 11, 2022");
     // startDate: 1670281200000 - 6 Dec 2022
-    expect(formatExpiryDate(6, 1670281200000)).to.eql("Mon, Dec 12, 2022");
+    expect(formatExpiryDate(6, 1670281200000 + 21100000)).to.eql("Mon, Dec 12, 2022");
   });
 });
