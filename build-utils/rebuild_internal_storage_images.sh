77ccc584378c320acf431e7270341acb9092abcf
Florian Soffel <florian.soffel@d-fine.de>
2022-12-14 10:58:33 +0100

    Removed dependency

--- build-utils/rebuild_internal_storage_images.sh
+++ build-utils/rebuild_internal_storage_images.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_internal_storage_base \
           ./dataland-internal-storage/DatalandInternalStorageBaseDockerfile ./dataland-internal-storage/ \
-          ./dataland-backend-utils/ ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
+          ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
 
 set -o allexport
 source ./*github_env.log
@@ -11,8 +11,8 @@ set +o allexport
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_internal_storage_test \
           ./dataland-internal-storage/DockerfileTest ./dataland-internal-storage/ \
-          ./dataland-backend-utils/ ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
+          ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_internal_storage_production \
           ./dataland-internal-storage/Dockerfile ./dataland-internal-storage/ \
-          ./dataland-backend-utils/ ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
+          ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
