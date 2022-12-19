c61c08fb272179331f950a975fe37bc861a9fedc
Marcin Stanczyk (PL) <marcin.stanczyk@pwc.com>
2022-12-19 09:44:29 +0100

    combine tests

--- dataland-frontend/tests/component/components/general/UserRolesBadgesTest.cy.ts
+++ dataland-frontend/tests/component/components/general/UserRolesBadgesTest.cy.ts
@@ -1,68 +1,33 @@
-import CreateApiKeyCard from "@/components/general/apiKey/CreateApiKeyCard.vue";
-import ApiKeyCard from "@/components/general/apiKey/ApiKeyCard.vue";
+import UserRolesBadges from "@/components/general/apiKey/UserRolesBadges.vue";
 import { mount } from "cypress/vue";
 
 describe("Component test for UserRolesBadges", () => {
-  it("Should display proper user roles in CreateApiKeyCard", () => {
-    mount(CreateApiKeyCard, {
+  it("Should display proper user roles", () => {
+    mount(UserRolesBadges, {
       props: {
         userRoles: ["ROLE_USER", "ROLE_ADMIN"],
       },
     });
-    cy.get("[data-test=userRoles]").should("be.visible");
     cy.get("[data-test=userRoleUser]").should("have.text", "READ");
     cy.get("[data-test=userRoleAdmin]").should("have.text", "WRITE");
   });
 
-  it("Should display only user role in CreateApiKeyCard", () => {
-    mount(CreateApiKeyCard, {
+  it("Should display only user role", () => {
+    mount(UserRolesBadges, {
       props: {
         userRoles: ["ROLE_USER"],
       },
     });
-    cy.get("[data-test=userRoles]").should("be.visible");
     cy.get("[data-test=userRoleUser]").should("have.text", "READ");
     cy.get("[data-test=userRoleAdmin]").should("not.exist");
   });
 
-  it("Should display only admin role in CreateApiKeyCard", () => {
-    mount(CreateApiKeyCard, {
+  it("Should display only admin role", () => {
+    mount(UserRolesBadges, {
       props: {
         userRoles: ["ROLE_ADMIN"],
       },
     });
-    cy.get("[data-test=userRoles]").should("be.visible");
-    cy.get("[data-test=userRoleUser]").should("not.exist");
-    cy.get("[data-test=userRoleAdmin]").should("have.text", "WRITE");
-  });
-  it("Should display proper user roles in ApiKeyCard", () => {
-    mount(ApiKeyCard, {
-      props: {
-        userRoles: ["ROLE_USER", "ROLE_ADMIN"],
-      },
-    });
-    cy.get("[data-test=userRoles]").should("be.visible");
-    cy.get("[data-test=userRoleUser]").should("have.text", "READ");
-    cy.get("[data-test=userRoleAdmin]").should("have.text", "WRITE");
-  });
-  it("Should display only user role in ApiKeyCard", () => {
-    mount(ApiKeyCard, {
-      props: {
-        userRoles: ["ROLE_USER"],
-      },
-    });
-    cy.get("[data-test=userRoles]").should("be.visible");
-    cy.get("[data-test=userRoleUser]").should("have.text", "READ");
-    cy.get("[data-test=userRoleAdmin]").should("not.exist");
-  });
-
-  it("Should display only admin role in ApiKeyCard", () => {
-    mount(ApiKeyCard, {
-      props: {
-        userRoles: ["ROLE_ADMIN"],
-      },
-    });
-    cy.get("[data-test=userRoles]").should("be.visible");
     cy.get("[data-test=userRoleUser]").should("not.exist");
     cy.get("[data-test=userRoleAdmin]").should("have.text", "WRITE");
   });
