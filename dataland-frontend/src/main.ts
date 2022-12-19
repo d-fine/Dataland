6e73f55dfeae3141430566ae2173c1db53416e33
Marcin Stanczyk (PL) <marcin.stanczyk@pwc.com>
2022-12-20 00:16:05 +0100

    purge css

--- dataland-frontend/src/main.ts
+++ dataland-frontend/src/main.ts
@@ -5,7 +5,7 @@ import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css"; // use this for icons embedded in inputs and declarative elements such as menus -> decent alignment
 import "material-icons/iconfont/material-icons.css"; // prefer these icons in most cases such as stand-alone or in buttons -> recommended by UI/UX
 import "primeflex/primeflex.css";
-import "./assets/scss/global.scss";
+import "./assets/theme/theme.css";
 import "./assets/css/ibm-plex-sans.scss";
 import "@formkit/themes/genesis";
 import { plugin, defaultConfig } from "@formkit/vue";
