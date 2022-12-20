00834d6da64c03e08b40456558cedbf72e38bbba
Philip Puetsch <philip.puetsch@d-fine.com>
2022-12-20 11:51:28 +0100

    fix e2etest

--- dataland-frontend/tests/e2e/specs/user-authentication/VerifyEmailLogoExists.ts
+++ dataland-frontend/tests/e2e/specs/user-authentication/VerifyEmailLogoExists.ts
@@ -1,6 +1,6 @@
 describe("As a user I expect to find the dataland logo for emails", () => {
   it(`Test availability of dataland logo`, () => {
-    cy.request(`${Cypress.config("baseUrl")!} + "/images/logos/logo_dataland_long.png`).then((response) => {
+    cy.request(`${Cypress.config("baseUrl")!!}/images/logos/logo_dataland_long.png`).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.have.length(2033);
     });
