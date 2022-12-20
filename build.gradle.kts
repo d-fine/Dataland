aa7c8b02c898e0add6d41e3d90371987fa433f0e
Philip Puetsch <philip.puetsch@d-fine.com>
2022-12-20 15:16:53 +0100

    bugfix sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,7 +106,7 @@ tasks.jacocoTestReport {
         xml.required.set(true)
         csv.required.set(false)
     }
-    executionData.setFrom(fileTree("$projectDir").include("**/*.exec").include("**.exec"))
+    executionData.setFrom(fileTree("$projectDir") { include("**/*.exec")}.files)
 }
 
 detekt {
