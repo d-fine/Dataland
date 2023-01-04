e48962b08cd656f1475a52ecc6602cc790ddb50a
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-01-04 13:51:37 +0100

    Integrated eslint into keycloak login theme

--- dataland-keycloak/dataland_theme/login/src/utils/InputField.ts
+++ dataland-keycloak/dataland_theme/login/src/utils/InputField.ts
@@ -1,21 +1,26 @@
 export class InputField {
-    public readonly inputField: HTMLInputElement;
-    public readonly inputErrorContainer: HTMLDivElement;
-    public readonly inputErrorTextSpan: HTMLSpanElement;
+  public readonly inputField: HTMLInputElement;
+  public readonly inputErrorContainer: HTMLDivElement;
+  public readonly inputErrorTextSpan: HTMLSpanElement;
 
-    constructor(public readonly inputFieldContainer: HTMLDivElement) {
-        this.inputField = inputFieldContainer.querySelector<HTMLInputElement>(`input`)!
-        this.inputErrorContainer = inputFieldContainer.querySelector<HTMLDivElement>(`div.input-error-container`)!
-        this.inputErrorTextSpan = inputFieldContainer.querySelector<HTMLSpanElement>(`span.input-error`)!
+  constructor(public readonly inputFieldContainer: HTMLDivElement) {
+    this.inputField =
+      inputFieldContainer.querySelector<HTMLInputElement>(`input`)!;
+    this.inputErrorContainer =
+      inputFieldContainer.querySelector<HTMLDivElement>(
+        `div.input-error-container`
+      )!;
+    this.inputErrorTextSpan =
+      inputFieldContainer.querySelector<HTMLSpanElement>(`span.input-error`)!;
+  }
+  overrideFieldErrorMessage(errorMessage?: string) {
+    if (errorMessage) {
+      this.inputErrorTextSpan.textContent = errorMessage;
+      this.inputField.classList.add("error");
+      this.inputErrorContainer.classList.remove("hidden");
+    } else {
+      this.inputField.classList.remove("error");
+      this.inputErrorContainer.classList.add("hidden");
     }
-    overrideFieldErrorMessage(errorMessage?: string) {
-        if (errorMessage) {
-            this.inputErrorTextSpan.textContent = errorMessage
-            this.inputField.classList.add("error")
-            this.inputErrorContainer.classList.remove("hidden")
-        } else {
-            this.inputField.classList.remove("error")
-            this.inputErrorContainer.classList.add("hidden")
-        }
-    }
+  }
 }
