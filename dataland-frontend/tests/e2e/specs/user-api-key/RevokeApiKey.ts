120dfc4c7309d2dda28bd0f164f22c735d4abc75
Marcin Stanczyk (PL) <marcin.stanczyk@pwc.com>
2023-01-04 14:17:26 +0100

    tests improvement

--- dataland-frontend/tests/e2e/specs/user-api-key/RevokeApiKey.ts
+++ dataland-frontend/tests/e2e/specs/user-api-key/RevokeApiKey.ts
@@ -6,8 +6,11 @@ describe("As a user I expect my api key will be revoke correctly", () => {
       "apiKeyInfo"
     );
     cy.get("div#existingApiKeyCard").should("exist");
-    cy.get("button.p-button-danger").should("contain.text", "DELETE").click();
+    cy.get("button").contains("DELETE").click();
     cy.get("div#revokeModal").should("be.visible");
+    cy.get("button").contains("CANCEL").click();
+    cy.get("div#revokeModal").should("not.exist");
+    cy.get("button").contains("DELETE").click();
     cy.get("button#confirmRevokeButton").should("contain.text", "CONFIRM").click();
   });
 });
