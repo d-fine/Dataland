6b596f54d9b685c68903d4e19f9feaef5a72275d
Florian Soffel <florian.soffel@d-fine.de>
2023-01-04 16:11:36 +0100

    Fixed unit test

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/InviteManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/InviteManagerTest.kt
@@ -8,32 +8,47 @@ import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.InjectMocks
+import org.mockito.Mock
 import org.mockito.Mockito
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.mock.web.MockMultipartFile
-import org.springframework.web.multipart.MultipartFile
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.jwt.Jwt
 
 @SpringBootTest(classes = [DatalandBackend::class])
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class InviteManagerTest {
+    @Mock lateinit var mockEmail: Email
+    @Mock lateinit var mockEmailSender: EmailSender
+    @Mock lateinit var mockInviteMetaInfoRepository: InviteMetaInfoRepository
+    @Mock lateinit var mockSecurityContext: SecurityContext
+    @Mock lateinit var mockAuthentication: Authentication
+    @Mock lateinit var mockJwt: Jwt
+    @InjectMocks lateinit var inviteManager: InviteManager
+
     @Test
     fun `check for correct email error handling`() {
-        val mockEmailSender = Mockito.mock(EmailSender::class.java)
-        val mockInviteMetaInfoRepository = Mockito.mock(InviteMetaInfoRepository::class.java)
-
-        Mockito.`when`(mockEmailSender.sendEmail(any(Email::class.java))).thenReturn(
+        Mockito.`when`(mockEmailSender.sendEmail(any(Email::class.java) ?: mockEmail)).thenReturn(
             false
         )
 
         Mockito.`when`(mockInviteMetaInfoRepository.save(any(InviteMetaInfoEntity::class.java)))
             .thenAnswer { invocation -> invocation.arguments[0] as InviteMetaInfoEntity }
 
-        val inviteManager = InviteManager(mockEmailSender, mockInviteMetaInfoRepository)
-        val file: MultipartFile = MockMultipartFile("test.xlsx", "this is content".toByteArray())
-        val inviteMetaInfo = inviteManager.submitInvitation(file, false)
+        Mockito.`when`(mockJwt.getClaimAsString(anyString())).thenReturn("")
+        Mockito.`when`(mockAuthentication.name).thenReturn("")
+        Mockito.`when`(mockAuthentication.principal).thenReturn(mockJwt)
+        Mockito.`when`(mockSecurityContext.authentication).thenReturn(mockAuthentication)
+        SecurityContextHolder.setContext(mockSecurityContext)
 
+        val file = MockMultipartFile("test.xlsx", "test.xlsx", "plain/text", "this is content".toByteArray())
+        val inviteMetaInfo = inviteManager.submitInvitation(file, false)
         assertFalse(inviteMetaInfo.wasInviteSuccessful)
         assertTrue(inviteMetaInfo.inviteResultMessage.contains("sending an email"))
     }
