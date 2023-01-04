523104445474acc17da530f50017edf84fe0e078
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-01-04 10:02:38 +0100

    Made api-key-manager boot with new spring boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,13 +54,13 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 plugins {
     id("io.gitlab.arturbosch.detekt") version "1.20.0"
     id("com.github.node-gradle.node") version "3.5.0" apply false
-    id("org.springframework.boot") version "2.7.5" apply false
+    id("org.springframework.boot") version "3.0.1" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.spring") version "1.7.21" apply false
     id("org.sonarqube") version "3.4.0.2513"
     jacoco
-    id("org.springdoc.openapi-gradle-plugin") version "1.4.0" apply false
+    id("org.springdoc.openapi-gradle-plugin") version "1.6.0" apply false
     id("com.gorylenko.gradle-git-properties") version "2.4.1" apply false
     id("org.openapi.generator") version "6.2.1" apply false
     id("com.github.ben-manes.versions") version "0.43.0"
--- dataland-api-key-manager/build.gradle.kts
+++ dataland-api-key-manager/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("junit:junit:4.13.1")
     runtimeOnly(libs.database.postgres)
     runtimeOnly(libs.database.h2)
