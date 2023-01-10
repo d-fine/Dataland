6723fb57e95b558cb5899030d2b27d3549506c2e
Florian Soffel <florian.soffel@d-fine.de>
2023-01-10 17:41:12 +0100

    clean up

--- dataland-frontend/src/utils/Constants.ts
+++ dataland-frontend/src/utils/Constants.ts
@@ -8,8 +8,6 @@ export const ARRAY_OF_FRONTEND_INCLUDED_FRAMEWORKS = Object.values(DataTypeEnum)
   (frameworkName) => ["lksg", "sfdr", "sme"].indexOf(frameworkName) === -1
 ) as Array<DataTypeEnum>;
 
-export const EXCEL_TEMPLATE_FILE_NAME = "Dataland_Request_Template.xlsx";
-
 export const UPLOAD_FILE_SIZE_DISPLAY_DECIMALS = 2;
 
 export const UPLOAD_MAX_FILE_SIZE_IN_BYTES = 2000000;
