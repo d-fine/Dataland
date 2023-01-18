df17725bd14a3fc03eb1999429c004b86263ef3a
d92048 <stephan.waldow@d-fine.de>
2023-01-18 14:59:26 +0100

    bug fix

--- dataland-dummy-qa-service/src/main/kotlin/org/dataland/datalanddummyqaservice/QaService.kt
+++ dataland-dummy-qa-service/src/main/kotlin/org/dataland/datalanddummyqaservice/QaService.kt
@@ -4,10 +4,18 @@ import org.springframework.amqp.rabbit.annotation.RabbitHandler
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Service
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
+@SpringBootApplication
+class ConsumerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConsumerApplication>(*args)
+}
 @Service
 @RabbitListener(queues = ["qa_queue"])
-class Qa_Service(val rabbitTemplate: RabbitTemplate) {
+class QaService(val rabbitTemplate: RabbitTemplate) {
     @RabbitHandler
     fun receive(message: String) {
         println("Received data upload on QA message queue with Correlation ID:")
