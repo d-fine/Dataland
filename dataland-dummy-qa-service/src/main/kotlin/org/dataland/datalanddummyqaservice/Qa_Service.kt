fc5d679c40e98580336bde1651c0598532e07b5c
d92048 <stephan.waldow@d-fine.de>
2023-01-18 14:12:05 +0100

    small clean up

--- dataland-dummy-qa-service/src/main/kotlin/org/dataland/datalanddummyqaservice/Qa_Service.kt
+++ dataland-dummy-qa-service/src/main/kotlin/org/dataland/datalanddummyqaservice/Qa_Service.kt
@@ -3,16 +3,7 @@ package org.dataland.datalanddummyqaservice
 import org.springframework.amqp.rabbit.annotation.RabbitHandler
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 import org.springframework.stereotype.Service
-
-@SpringBootApplication
-class ConsumerApplication
-
-fun main(args: Array<String>) {
-    runApplication<ConsumerApplication>(*args)
-}
 
 @Service
 @RabbitListener(queues = ["qa_queue"])
@@ -25,13 +16,3 @@ class ReceiverAndSender(val rabbitTemplate: RabbitTemplate) {
         }
     }
 }
-/*
-@Service
-@RabbitListener(queues = ["upload_queue"])
-class Receiver {
-    @RabbitHandler
-    fun receive(message: String) {
-        println("Received data ready to be stored on upload queue. Correlation ID:")
-    }
-}
-*/
