9d1eceb2e4afa9aabfaa9fe2a8a754017d948f88
Florian Soffel <florian.soffel@d-fine.de>
2023-01-18 17:15:21 +0100

    linter

--- dataland-frontend/src/utils/DataUtils.ts
+++ dataland-frontend/src/utils/DataUtils.ts
@@ -1,5 +1,5 @@
 import { DataMetaInformation } from "@clients/backend";
 
-export function convertListOfDataMetaInfoToListOfDataIds(receivedListOfDataMetaInfo: []) {
+export function convertListOfDataMetaInfoToListOfDataIds(receivedListOfDataMetaInfo: []): string[] {
   return receivedListOfDataMetaInfo.map((dataMetaInfo) => (dataMetaInfo as DataMetaInformation).dataId);
 }
