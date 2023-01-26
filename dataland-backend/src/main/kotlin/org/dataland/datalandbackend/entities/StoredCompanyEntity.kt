f652b35e1fd69d4f4eeb09b5674fccddffcc0ec6
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-01-26 10:16:43 +0100

    add new fields to company data model

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/entities/StoredCompanyEntity.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/entities/StoredCompanyEntity.kt
@@ -53,7 +53,10 @@ data class StoredCompanyEntity(
     var countryCode: String,
 
     @Column(name = "is_teaser_company")
-    var isTeaserCompany: Boolean
+    var isTeaserCompany: Boolean,
+
+    @Column(name = "website")
+    var website: String?
 ) : ApiModelConversion<StoredCompany> {
     @JsonValue
     override fun toApiModel(viewingUser: DatalandAuthentication?): StoredCompany {
@@ -69,6 +72,7 @@ data class StoredCompanyEntity(
                 identifiers = identifiers.map { it.toApiModel(viewingUser) }.toList(),
                 countryCode = countryCode,
                 isTeaserCompany = isTeaserCompany,
+                website = website,
             ),
             dataRegisteredByDataland = dataRegisteredByDataland.map { it.toApiModel(viewingUser) }.toMutableList()
         )
