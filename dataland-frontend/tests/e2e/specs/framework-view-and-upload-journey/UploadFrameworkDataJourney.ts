d12029c61a6ff4df2e75d2fb519efb4d4658b9d2
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-01-30 17:13:45 +0100

    write down test plan as comment to discuss with colleagues

--- dataland-frontend/tests/e2e/specs/framework-view-and-upload-journey/UploadFrameworkDataJourney.ts
+++ dataland-frontend/tests/e2e/specs/framework-view-and-upload-journey/UploadFrameworkDataJourney.ts
@@ -6,12 +6,18 @@ describe("As a user, I expect the search functionality on the /companies page to
   });
 
   // TODO Stuff to test:
-  /*      - Switch between Available Datasets and My Datasets and check for the page in general (is everything there?)
-            - Assure that some dataset is not available, then upload it, the check that it's in "My Datasets"
-            - Use the search to find a specific dataset from all of the users datasets
-            - paginator?
-            - CREATE button?
-            - ...            open for more ideas
+  /*      - Before: Create an fresh company with a specific name, but do not upload any framework data for it
+          - Click on CREATE DATASET on search page and assure that you get redirected to the ChooseCompanyPage
+          - Verify the search bar there for option 1, and assure that the created company appears there
+          - Don't use that company. Instead, click on "add it" and add a new company via the form, assure that you get automatically redirected
+          - Click on CREATE DATASET for some framwork and assure that you are redirected to the form
+
+          - Before: Create Company with lots of framework data for it
+          - Click on CREATE DATASET on search page and assure that you get redirected to the ChooseCompanyPage
+          - Choose that company via keys on the autocomplete dropdown and assure that you are redirected to the ChooseFrameworkPage
+          - Now verify the ChooseCompanyPage (are the existing datasets displayed?)
+          - Click on one existing non-lksg dataset and check if you get redirected to the exakt framework-view-page for it
+          - Click on one existing lksg dataset and check if you get redirected to the general lksg view page for that company
 
      */
 
