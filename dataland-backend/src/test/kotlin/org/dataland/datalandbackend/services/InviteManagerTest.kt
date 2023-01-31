282452fec05982bfd858830e6c43cb0d7ea4d574
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-01-31 18:19:12 +0100

    Updated backend unit tests

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/InviteManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/InviteManagerTest.kt
@@ -4,11 +4,12 @@ import org.dataland.datalandbackend.DatalandBackend
 import org.dataland.datalandbackend.entities.InviteMetaInfoEntity
 import org.dataland.datalandbackend.model.email.Email
 import org.dataland.datalandbackend.repositories.InviteMetaInfoRepository
+import org.dataland.datalandbackend.utils.AuthenticationMock
+import org.dataland.keycloakAdapter.auth.DatalandRealmRole
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentMatchers.any
-import org.mockito.ArgumentMatchers.anyString
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
@@ -16,10 +17,8 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.mock.web.MockMultipartFile
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.oauth2.jwt.Jwt
 
 @SpringBootTest(classes = [DatalandBackend::class])
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -31,8 +30,6 @@ class InviteManagerTest {
     @Mock lateinit var mockEmailSender: EmailSender
     @Mock lateinit var mockInviteMetaInfoRepository: InviteMetaInfoRepository
     @Mock lateinit var mockSecurityContext: SecurityContext
-    @Mock lateinit var mockAuthentication: Authentication
-    @Mock lateinit var mockJwt: Jwt
     @InjectMocks lateinit var inviteManager: InviteManager
 
     @Test
@@ -44,9 +41,11 @@ class InviteManagerTest {
         Mockito.`when`(mockInviteMetaInfoRepository.save(any(InviteMetaInfoEntity::class.java)))
             .thenAnswer { invocation -> invocation.arguments[0] as InviteMetaInfoEntity }
 
-        Mockito.`when`(mockJwt.getClaimAsString(anyString())).thenReturn("")
-        Mockito.`when`(mockAuthentication.name).thenReturn("")
-        Mockito.`when`(mockAuthentication.principal).thenReturn(mockJwt)
+        val mockAuthentication = AuthenticationMock.mockJwtAuthentication(
+            "mocked_uploader",
+            "mocked_uploader_id",
+            setOf(DatalandRealmRole.ROLE_USER, DatalandRealmRole.ROLE_UPLOADER)
+        )
         Mockito.`when`(mockSecurityContext.authentication).thenReturn(mockAuthentication)
         SecurityContextHolder.setContext(mockSecurityContext)
 
--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/utils/CompanyUploader.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/utils/CompanyUploader.kt
@@ -1,10 +1,11 @@
 package org.dataland.datalandbackend.utils
-
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.dataland.datalandbackend.model.CompanyInformation
 import org.dataland.datalandbackend.model.StoredCompany
+import org.dataland.keycloakAdapter.auth.DatalandRealmRole
 import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -16,11 +17,17 @@ class CompanyUploader {
         objectMapper: ObjectMapper,
         companyInformation: CompanyInformation
     ): StoredCompany {
+        val mockAuthentication = AuthenticationMock.mockJwtAuthentication(
+            "mocked_uploader",
+            "mocked_uploader_id",
+            setOf(DatalandRealmRole.ROLE_USER, DatalandRealmRole.ROLE_UPLOADER)
+        )
         val request = mockMvc.perform(
             MockMvcRequestBuilders.post("/companies")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsBytes(companyInformation))
+                .with(authentication(mockAuthentication))
         )
             .andExpectAll(
                 MockMvcResultMatchers.status().isOk,
