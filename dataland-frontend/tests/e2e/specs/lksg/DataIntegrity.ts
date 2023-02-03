108076a55ac6900804bab6f95a914a9018ca6575
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-02-03 09:53:51 +0100

    Just set timeout to long to fix this for good

--- dataland-frontend/tests/e2e/specs/lksg/DataIntegrity.ts
+++ dataland-frontend/tests/e2e/specs/lksg/DataIntegrity.ts
@@ -170,7 +170,7 @@ describeIf(
                 cy.get("ul[class=p-autocomplete-items]").should("exist");
                 cy.intercept(`**/api/data/${DataTypeEnum.Lksg}/company/*`).as("retrieveLksgData");
                 cy.get("input[id=framework_data_search_bar_standard]").type("{downArrow}").type("{enter}");
-                cy.wait("@retrieveLksgData", { timeout: Cypress.env("medium_timeout_in_ms") as number }).then(() => {
+                cy.wait("@retrieveLksgData", { timeout: Cypress.env("long_timeout_in_ms") as number }).then(() => {
                   cy.url().should("include", "/companies/").url().should("include", "/frameworks/");
 
                   cy.get("table.p-datatable-table")
