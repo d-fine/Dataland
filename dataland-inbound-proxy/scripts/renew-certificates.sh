c43752ab9ea39de91fc12fb0192added8090a46d
Philip Puetsch <philip.puetsch@d-fine.com>
2023-02-07 16:05:09 +0100

    simplify things as certbot creates its own cronjob

--- dataland-inbound-proxy/scripts/renew-certificates.sh
+++ dataland-inbound-proxy/scripts/renew-certificates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ -d "/etc/letsencrypt/live" ]; then
   # Renew certbot certificate (and reload nginx if required)
-  certbot renew --post-hook "sh /scripts/reload-certificates.sh"
+  certbot renew
 fi
