7080450c380309cba8a6fd5ca280604fc62a7d8c
Philip Puetsch <philip.puetsch@d-fine.com>
2023-02-07 14:33:04 +0100

    add owasp plugin, amend manual_maintenance.md

--- dataland-inbound-proxy/scripts/obtain-letsencrypt-certs.sh
+++ dataland-inbound-proxy/scripts/obtain-letsencrypt-certs.sh
@@ -12,5 +12,6 @@ if [ ! -d "/etc/letsencrypt/live" ] && [ ! -f "/certs/custom/privkey.pem" ]; the
   mkdir -p /var/www/certbot && certbot certonly --non-interactive --webroot --webroot-path /var/www/certbot --agree-tos --preferred-challenges http $PROXY_LETSENCRYPT_ARGS
   sh /scripts/reload-certificates.sh
 else
-  echo "It looks like LetsEncrypt is already configured or custom certificates have been provided. Skipping"
+  echo "It looks like LetsEncrypt is already configured or custom certificates have been provided. Renewing"
+  /scripts/renew-certificates.sh
 fi
