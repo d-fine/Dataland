52224a1b5674a08dc952f0190e387786cefbbb5c
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-02-08 10:42:06 +0100

    Assure that youtube is the problem

--- dataland-frontend/tests/e2e/specs/user-authentication/Register.ts
+++ dataland-frontend/tests/e2e/specs/user-authentication/Register.ts
@@ -7,10 +7,10 @@ describe("As a user I want to be able to register for an account and be able to 
   const randomHexPassword = [...passwordBytes].map((x): string => x.toString(16).padStart(2, "0")).join("");
 
   it("Checks that the Dataland password-policy gets respected", () => {
-    cy.intercept("https://www.youtube-nocookie.com/**").as("postYoutubeStatistics");
+   // cy.intercept("https://www.youtube-nocookie.com/**").as("postYoutubeStatistics");
     cy.visitAndCheckAppMount("/")
       // TODO waiting for the youtube POST request to finish
-      .wait("@postYoutubeStatistics", { timeout: Cypress.env("long_timeout_in_ms") as number })
+    //  .wait("@postYoutubeStatistics", { timeout: Cypress.env("long_timeout_in_ms") as number })
         .wait(20000)
       .get("button[name='join_dataland_button']")
       .click()
@@ -43,10 +43,10 @@ describe("As a user I want to be able to register for an account and be able to 
   it("Checks that registering works", () => {
     cy.task("setEmail", email);
     cy.task("setPassword", randomHexPassword);
-    cy.intercept("https://www.youtube-nocookie.com/**").as("postYoutubeStatistics");
+   // cy.intercept("https://www.youtube-nocookie.com/**").as("postYoutubeStatistics");
     cy.visitAndCheckAppMount("/")
       // TODO waiting for the youtube POST request to finish
-      .wait("@postYoutubeStatistics", { timeout: Cypress.env("long_timeout_in_ms") as number })
+    //  .wait("@postYoutubeStatistics", { timeout: Cypress.env("long_timeout_in_ms") as number })
         .wait(20000)
         .get("button[name='join_dataland_button']")
       .click()
