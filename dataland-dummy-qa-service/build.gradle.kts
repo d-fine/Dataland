62c26ccaa573fa11da0223230650911117537d31
Florian Soffel <florian.soffel@d-fine.de>
2023-02-08 15:14:02 +0100

    Actuator available

--- dataland-dummy-qa-service/build.gradle.kts
+++ dataland-dummy-qa-service/build.gradle.kts
@@ -29,6 +29,7 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.testng:testng:7.7.0")
     implementation("junit:junit:4.13.1")
