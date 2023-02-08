e9671590f2608a3cd71c655ee4eff90cb407e9b9
Philip Puetsch <philip.puetsch@d-fine.com>
2023-02-08 09:10:22 +0100

    fixes a bug

---
+++ dataland-inbound-proxy/docker-entrypoint.d/02-generate-reload-hook.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euxo pipefail
+# Ensure /certs director is present and permissions are configured accordingly
+echo Creating Certificate Reload Hook
+mkdir -p /etc/letsencrypt/renewal-hooks/deploy/
+ln -s /scripts/reload-certificates.sh /etc/letsencrypt/renewal-hooks/deploy/reload-certificates.sh
