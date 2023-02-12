6def0ddeac12c14e0dbe72efd3d7f8aa560bb212
Marcin Stanczyk (PL) <marcin.stanczyk@pwc.com>
2023-02-13 00:47:26 +0100

    add tests to index

--- dataland-frontend/tests/e2e/specs/index.ts
+++ dataland-frontend/tests/e2e/specs/index.ts
@@ -46,6 +46,7 @@ if (testGroupingDisabled || cypressTestGroup === 2) {
   require("./eu-taxonomy-non-financials");
   require("./eu-taxonomy-financials");
   require("./lksg");
+  require("./sfdr");
 }
 
 if (testGroupingDisabled || cypressTestGroup === 3) {
