afb255ad1f2e38b49b4a7eb2fd9d7bea2580e1e1
ANDREAS HOECHERL <andreas.hoecherl@d-fine.com>
2023-02-14 20:08:03 +0100

    Addressing one more detekt issue after merge

--- dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/exceptions/MessageQueueException.kt
+++ dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/exceptions/MessageQueueException.kt
@@ -1,3 +1,7 @@
 package org.dataland.datalandmessagequeueutils.exceptions
 
+/**
+ * A MessageQueueException should be thrown if message received from the RabbitMQ Queue does not have
+ * the expected format
+ */
 class MessageQueueException(message: String) : Exception(message)
