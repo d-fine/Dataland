a0cc64f46eb13bb15b36fa8e97779ce12ce07c5e
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-02-15 08:25:40 +0100

    Try out something

--- dataland-frontend/tests/e2e/utils/Auth.ts
+++ dataland-frontend/tests/e2e/utils/Auth.ts
@@ -73,6 +73,7 @@ export function ensureLoggedIn(username?: string, password?: string): void {
         cy.visit("/")
           .url()
           .should("eq", getBaseUrl() + "/companies");
+        cy.wait(3000);
       },
       cacheAcrossSpecs: true,
     }
