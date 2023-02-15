3e0a64c5499f494827914785725e4c16265b24ac
d92048 <stephan.waldow@d-fine.de>
2023-02-15 16:00:20 +0100

    more testing

--- testing/wait_for_backend_and_run_e2etests.sh
+++ testing/wait_for_backend_and_run_e2etests.sh
@@ -4,28 +4,28 @@
 
 set -euxo pipefail
 
-is_infrastructure_up () {
-  declare -A services
-  services["backend"]=https://local-dev.dataland.com/api/actuator/health/ping
-  services["internal-storage"]=http://local-dev.dataland.com/internal-storage/actuator/health/ping
-  services["keycloak"]=http://local-dev.dataland.com/keycloak/realms/datalandsecurity/
-  services["api-key-manager"]=http://local-dev.dataland.com/api-keys/actuator/health/ping
-  services["qa-service"]=http://local-dev.dataland.com/qa/actuator/health/ping
+#is_infrastructure_up () {
+ # declare -A services
+  #services["backend"]=https://local-dev.dataland.com/api/actuator/health/ping
+  #services["internal-storage"]=http://local-dev.dataland.com/internal-storage/actuator/health/ping
+  #services["keycloak"]=http://local-dev.dataland.com/keycloak/realms/datalandsecurity/
+  #services["api-key-manager"]=http://local-dev.dataland.com/api-keys/actuator/health/ping
+  #services["qa-service"]=http://local-dev.dataland.com/qa/actuator/health/ping
 
-  for service in "${!services[@]}"; do
-    if ! curl -L ${services[$service]} 2>/dev/null | grep -q 'UP\|alive\|datalandsecurity'; then
-      echo "$service not yet there"
-      return 1
-    fi
-  done
-  echo "-----------------------------"
-  echo "All infrastructure is up now!"
-  echo "-----------------------------"
-}
-export -f is_infrastructure_up
+  #for service in "${!services[@]}"; do
+   # if ! curl -L ${services[$service]} 2>/dev/null | grep -q 'UP\|alive\|datalandsecurity'; then
+    #  echo "$service not yet there"
+     # return 1
+    #fi
+  #done
+  #echo "-----------------------------"
+  #echo "All infrastructure is up now!"
+  #echo "-----------------------------"
+#}
+#export -f is_infrastructure_up
 
-timeout 240 bash -c "while ! is_infrastructure_up; do echo 'infrastructure not yet completely there - retrying in 1s'; sleep 1; done; echo 'infrastructure up!'"
-
+#timeout 240 bash -c "while ! is_infrastructure_up; do echo 'infrastructure not yet completely there - retrying in 1s'; sleep 1; done; echo 'infrastructure up!'"
+sleep 240
 if [[ $CYPRESS_TEST_GROUP -eq 0 ]]; then
   ./gradlew :dataland-e2etests:test --no-daemon --stacktrace
 else
