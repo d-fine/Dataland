a32e0736deedcb354a54f07e37e936a3cbd45da8
Florian Soffel <florian.soffel@d-fine.de>
2023-02-16 16:05:25 +0100

    new open api file verification

--- testing/verifyOpenApiFiles.sh
+++ testing/verifyOpenApiFiles.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-function getOpenApiSha1Sum() {
-  find * -name "*OpenApi.json" -type f -exec bash -c 'jq -S . $1 > Formatted$1' shell {} \;
-
-  find * -name "Formatted*OpenApiFormatted.json" -type f | \
-  sort -u | \
-  xargs sha1sum | \
-  awk '{print $1}' | \
-  sha1sum | \
-  awk '{print $1}'
-}
-
-sha1SumBeforeRegenerate=$(getOpenApiSha1Sum)
+mkdir formatted
+find * -name "*OpenApi.json" -type f -exec bash -c 'mv -t formatted $1' shell {} \;
 ./gradlew generateOpenApiDocs --no-daemon --stacktrace
-sha1SumAfterRegenerate=$(getOpenApiSha1Sum)
-echo "sha1sum before regenerate: $sha1SumBeforeRegenerate"
-echo "sha1sum after regenerate: $sha1SumAfterRegenerate"
-
-if [[ "$sha1SumBeforeRegenerate" == "$sha1SumAfterRegenerate" ]]; then
-  echo "OpenApi Files OK!"
-  exit 0
-else
-  echo "OpenApi Files not OK!"
-  exit 1
-fi
+find * -name "*OpenApi.json" -type f -exec bash -c 'jd -set $1 formatted/$1' shell {} \;
