af2302986fa34895ba7cbfbda2f1f6b777322ea6
Florian Soffel <florian.soffel@d-fine.de>
2023-02-17 10:29:12 +0100

    Added component tests

--- dataland-frontend/tests/component/components/resources/frameworkDataSearch/lksg/LksgPanel.cy.ts
+++ dataland-frontend/tests/component/components/resources/frameworkDataSearch/lksg/LksgPanel.cy.ts
@@ -1,6 +1,7 @@
 import LksgPanel from "@/components/resources/frameworkDataSearch/lksg/LksgPanel.vue";
-import { LksgData } from "@clients/backend";
+import { DataAndMetaInformationLksgData, DatasetQualityStatus, LksgData } from "@clients/backend";
 import { mount } from "cypress/vue";
+import Keycloak from "keycloak-js";
 
 describe("Component test for LksgPanel", () => {
   it("Should display the total revenue kpi in the correct format", () => {
@@ -9,7 +10,7 @@ describe("Component test for LksgPanel", () => {
       data() {
         return {
           waitingForData: false,
-          lksgDataAndMetaInfo: [{ metaInfo: {}, data: pseudoLksgData as LksgData }],
+          lksgDataAndMetaInfo: [{ data: pseudoLksgData as LksgData } as DataAndMetaInformationLksgData],
         };
       },
       created() {
@@ -17,5 +18,38 @@ describe("Component test for LksgPanel", () => {
       },
     });
     cy.get("td:contains('1.23 MM')").should("exist");
+  });
+
+  it("Should display only accepted datasets", () => {
+    cy.intercept("**/api/data/lksg/company/*", {
+      statusCode: 200,
+      body: [
+        {
+          metaInfo: { dataId: "1", qualityStatus: DatasetQualityStatus.Accepted },
+          data: { social: { general: { dataDate: "2023-01-01" } } },
+        },
+        {
+          metaInfo: { dataId: "2", qualityStatus: DatasetQualityStatus.Pending },
+          data: { social: { general: { dataDate: "2024-01-01" } } },
+        },
+      ],
+    }).as("alias");
+    mount(LksgPanel, {
+      setup() {
+        return {
+          getKeycloakPromise: (): Promise<Keycloak> => {
+            return Promise.resolve(new Keycloak());
+          },
+        };
+      },
+      props: {
+        companyId: {
+          type: String,
+          default: "",
+        },
+      },
+    });
+    cy.get("td:contains('2023')").should("exist");
+    cy.get("td:contains('2024')").should("not.exist");
   });
 });
