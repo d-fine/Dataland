d497fb955d0c296175d89d4df98af154ab71b009
Florian Soffel <florian.soffel@d-fine.de>
2023-02-21 11:51:19 +0100

    refactoring

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/ConfigurationStorageControllerApi.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/ConfigurationStorageControllerApi.kt
@@ -1,35 +1,24 @@
 package org.dataland.datalandbackend.services
 
 import org.dataland.datalandinternalstorage.openApiClient.api.StorageControllerApi
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.io.File
 
 /**
  * This class holds the configuration properties and the beans to auto-configure this library in a spring-boot
  * environment
  */
 @Configuration
-class ConfigurationStorageControllerApi {
+class ConfigurationStorageControllerApi(
+    @Value("\${dataland.internalstorage.base-url:http://internal-storage:8080/internal-storage}")
+    private val internalStorageBaseUrl: String,
+) {
     /**
      * The bean to configure the internal client StorageControllerApi
      */
     @Bean
     fun getApiInternalClient(): StorageControllerApi {
-        return StorageControllerApi(
-            basePath = if (isBackendInsideDockerContainer()) {
-                "http://internal-storage:8080/internal-storage"
-            } else {
-                "https://local-dev.dataland.com/internal-storage"
-            },
-        )
-    }
-
-    /**
-     * The function determines if it is run in a dockercontainer or not
-     */
-    fun isBackendInsideDockerContainer(): Boolean {
-        val file = File("/.dockerenv")
-        return file.exists()
+        return StorageControllerApi(basePath = internalStorageBaseUrl)
     }
 }
