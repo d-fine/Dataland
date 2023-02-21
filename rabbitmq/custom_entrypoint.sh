77c3c11421f491519f8cb28977f400e97459f258
ahoecherl <andreas.hoecherl@d-fine.de>
2023-02-21 21:13:40 +0100

    shorten shell script

--- rabbitmq/custom_entrypoint.sh
+++ rabbitmq/custom_entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sed 's/RABBITMQ_USER_PLACEHOLDER/'"$RABBITMQ_USER"'/g' /etc/rabbitmq/definitions.json | \
-  sed 's/RABBITMQ_PASS_HASH_PLACEHOLDER/'"$RABBITMQ_PASS_HASH"'/g' >etc/rabbitmq/updated_definitions.json
-rm /etc/rabbitmq/definitions.json
-mv /etc/rabbitmq/updated_definitions.json /etc/rabbitmq/definitions.json
+sed -i 's/RABBITMQ_USER_PLACEHOLDER/'"$RABBITMQ_USER"'/g' /etc/rabbitmq/definitions.json
+sed -i 's/RABBITMQ_PASS_HASH_PLACEHOLDER/'"$RABBITMQ_PASS_HASH"'/g' /etc/rabbitmq/definitions.json
 echo Definitions file used for initiation of RabbitMQ:
 cat /etc/rabbitmq/definitions.json
 docker-entrypoint.sh $1
