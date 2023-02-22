11e0bbb05abdf4fcde7bffb2827ef586c7d13545
Florian Soffel <florian.soffel@d-fine.de>
2023-02-22 11:09:59 +0100

    renamed rabbitmq

--- build-utils/rebuild_rabbitmq_image.sh
+++ build-utils/rebuild_rabbitmq_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_rabbitmq ./rabbitmq/Dockerfile ./rabbitmq/
+./build-utils/base_rebuild_single_docker_image.sh dataland_rabbitmq ./dataland-rabbitmq/Dockerfile ./dataland-rabbitmq/
