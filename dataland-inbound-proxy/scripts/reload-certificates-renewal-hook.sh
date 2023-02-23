6b53ce767a9ae30034084de417028614a49ca732
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-02-23 10:41:17 +0100

    Review changes

--- dataland-inbound-proxy/scripts/reload-certificates-renewal-hook.sh
+++ dataland-inbound-proxy/scripts/reload-certificates-renewal-hook.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Certbot obtained new certificate. Deploying..."
-rm /certs/dataland
-ln -s $RENEWED_LINEAGE /certs/dataland
+ln -sTf $RENEWED_LINEAGE /certs/dataland
 
 echo "Done. Reloading NGINX"
 nginx -s reload
