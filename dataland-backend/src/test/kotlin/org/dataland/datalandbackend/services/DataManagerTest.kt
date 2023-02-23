19bf97a6d183f668ed8bc72423bccc5f596b2603
Florian Soffel <florian.soffel@d-fine.de>
2023-02-23 15:58:49 +0100

    lint

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/DataManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/DataManagerTest.kt
@@ -43,7 +43,7 @@ class DataManagerTest(
     val testDataProvider = TestDataProvider(objectMapper)
     val dataManager = DataManager(
         objectMapper, companyManager, dataMetaInformationManager,
-        mockStorageClient, mockCloudEventMessageHandler, "dummy-queue-name", "dummy-queue-name"
+        mockStorageClient, mockCloudEventMessageHandler, "dummy-queue-name", "dummy-queue-name",
     )
     val spyDataManager: DataManager = spy(dataManager)
     val correlationId = IdUtils.generateUUID()
