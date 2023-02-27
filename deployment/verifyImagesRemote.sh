65750663944b28da0671024bcd65b63ef98a994e
Florian Soffel <florian.soffel@d-fine.de>
2023-02-27 12:30:55 +0100

    fix

--- deployment/verifyImagesRemote.sh
+++ deployment/verifyImagesRemote.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 target_server_url="$TARGETSERVER_URL"
-ssh ubuntu@"$target_server_url" 'if [[ "$(docker ps | awk '"'{print $2}'"' | grep -c _test:)" -eq 0 ]]; then exit 0; else exit 1; fi'
+ssh ubuntu@"$target_server_url" 'if [[ "$(docker ps | awk '\''{print $2}'\'' | grep -c _test:)" -eq 0 ]]; then exit 0; else exit 1; fi'
