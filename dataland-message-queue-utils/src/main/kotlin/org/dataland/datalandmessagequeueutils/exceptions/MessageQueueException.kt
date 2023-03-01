ec65cc5c0cf914c2974840994cdc93f5d3562218
swaldow <stephan.waldow@d-fine.de>
2023-03-01 14:05:37 +0100

    linting

--- dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/exceptions/MessageQueueException.kt
+++ dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/exceptions/MessageQueueException.kt
@@ -6,4 +6,4 @@ import org.springframework.amqp.AmqpRejectAndDontRequeueException
  * A MessageQueueException should be thrown if message received from the RabbitMQ Queue does not have
  * the expected format
  */
-class MessageQueueException() : AmqpRejectAndDontRequeueException("Message could not be processed - Message rejected")
+class MessageQueueException : AmqpRejectAndDontRequeueException("Message could not be processed - Message rejected")
