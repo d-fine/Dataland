d343e9d147db74870d5fa7f9c2fa1ea8b6fb75d5
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-03-02 11:41:12 +0100

    Introduce ViewMultipleDatasetsDisplayBase.vue to avoid duplicate code for the view-pages of multi-view frameworks

---
+++ dataland-frontend/src/components/generics/ViewMultipleDatasetsDisplayBase.vue
@@ -0,0 +1,63 @@
+<template>
+  <ViewFrameworkBase
+    :companyID="companyID"
+    :dataType="dataType"
+    @updateActiveDataMetaInfoForChosenFramework="handleUpdateActiveDataMetaInfo"
+  >
+    <template v-slot:content>
+      <div class="grid">
+        <div class="col-12">
+          <slot> </slot>
+        </div>
+      </div>
+      <div v-if="loading" class="col-12 text-left">
+        <h2>{{ "Checking if " + humanizeString(dataType) + " data is available..." }}</h2>
+      </div>
+      <div v-if="!loading && receivedDataIds.length === 0" class="col-12 text-left">
+        <h2>{{ "No " + humanizeString(dataType) + " data available" }}</h2>
+      </div>
+    </template>
+  </ViewFrameworkBase>
+</template>
+
+<script lang="ts">
+import ViewFrameworkBase from "@/components/generics/ViewFrameworkBase.vue";
+import { defineComponent } from "vue";
+import { DataMetaInformation } from "@clients/backend";
+import { humanizeString } from "@/utils/StringHumanizer";
+
+export default defineComponent({
+  name: "ViewMultipleDatasetsDisplayBase",
+  components: { ViewFrameworkBase },
+  props: {
+    companyID: {
+      type: String,
+    },
+    dataType: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      loading: true,
+      receivedDataIds: [] as string[],
+      humanizeString: humanizeString,
+    };
+  },
+  methods: {
+    /**
+     * TODO Stores the received data IDs from the "updateDataId" event and terminates the loading-state of the component.
+     *
+     * @param receivedMapOfReportingPeriodsToActiveDataMetaInfo 1-to-1 map between reporting periods and corresponding
+     * active data meta information objects
+     */
+    handleUpdateActiveDataMetaInfo(receivedMapOfReportingPeriodsToActiveDataMetaInfo: DataMetaInformation[]) {
+      this.receivedDataIds = Array.from(receivedMapOfReportingPeriodsToActiveDataMetaInfo.values()).map(
+        (dataMetaInfo) => dataMetaInfo.dataId
+      );
+      // TODO can we remove the sorting logic for same-year datasets from the lksg-view-page now?  because we receive only one per year now (because of "latest" setting)
+      this.loading = false;
+    },
+  },
+});
+</script>
--- dataland-frontend/src/components/pages/ViewSfdr.vue
+++ dataland-frontend/src/components/pages/ViewSfdr.vue
@@ -1,59 +1,29 @@
 <template>
-  <ViewFrameworkBase
-    :companyID="companyID"
-    dataType="sfdr"
-    @updateActiveDataMetaInfoForChosenFramework="handleUpdateActiveDataMetaInfo"
-  >
-    <template v-slot:content>
-      <div class="grid">
-        <div class="col-12">
-          <SfdrPanel :companyId="companyID" />
-        </div>
-      </div>
-      <div v-if="loading" class="col-12 text-left">
-        <h2>Checking if SFDR data is available...</h2>
-      </div>
-      <div v-if="!loading && receivedSfdrDataIds.length === 0" class="col-12 text-left">
-        <h2>No SFDR data</h2>
-      </div>
-    </template>
-  </ViewFrameworkBase>
+  <ViewMultipleDatasetsDisplayBase :companyID="companyID" :dataType="DataTypeEnum.Lksg">
+    <SfdrPanel :companyId="companyID" />
+  </ViewMultipleDatasetsDisplayBase>
 </template>
 
 <script lang="ts">
 import ViewFrameworkBase from "@/components/generics/ViewFrameworkBase.vue";
-import SfdrPanel from "@/components/resources/frameworkDataSearch/sfdr/SfdrPanel.vue";
+import ViewMultipleDatasetsDisplayBase from "@/components/generics/ViewMultipleDatasetsDisplayBase.vue";
+import SfdrPanel from "@/components/resources/frameworkDataSearch/lksg/SfdrPanel.vue";
 import { defineComponent } from "vue";
-import { DataMetaInformation } from "@clients/backend";
+import { DataTypeEnum } from "@clients/backend";
 
 export default defineComponent({
-  name: "ViewSfdr",
-  components: { ViewFrameworkBase, SfdrPanel },
+  name: "ViewLksg",
+  components: { ViewFrameworkBase, ViewMultipleDatasetsDisplayBase, SfdrPanel },
   props: {
     companyID: {
       type: String,
     },
   },
+
   data() {
     return {
-      loading: true,
-      receivedSfdrDataIds: [] as string[],
+      DataTypeEnum,
     };
-  },
-  methods: {
-    /**
-     * TODO Stores the received data IDs from the "updateDataId" event and terminates the loading-state of the component.
-     *
-     * @param receivedMapOfReportingPeriodsToActiveDataMetaInfo 1-to-1 map between reporting periods and corresponding
-     * active data meta information objects
-     */
-    handleUpdateActiveDataMetaInfo(receivedMapOfReportingPeriodsToActiveDataMetaInfo: DataMetaInformation[]) {
-      this.receivedSfdrDataIds = Array.from(receivedMapOfReportingPeriodsToActiveDataMetaInfo.values()).map(
-        (dataMetaInfo) => dataMetaInfo.dataId
-      );
-      // TODO can we remove the sorting logic for same-year datasets from the lksg-view-page now?  because we receive only one per year now (because of "latest" setting)
-      this.loading = false;
-    },
   },
 });
 </script>
