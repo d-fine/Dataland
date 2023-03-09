479461d08a6eaf2d9b27cdbe56692426eda4ca21
janherre <jan-niklas.herre@d-fine.de>
2023-03-09 16:20:47 +0100

    Make Lei visible in frontend

--- dataland-frontend/src/components/forms/CreateCompany.vue
+++ dataland-frontend/src/components/forms/CreateCompany.vue
@@ -109,6 +109,16 @@
               validationVisibility: 'live',
             }"
           >
+
+            <UploadFormHeader :name="companyDataNames.lei" :explanation="companyDataExplanations.lei" />
+            <FormKit
+                name="lei"
+                v-model="lei"
+                type="text"
+                :placeholder="companyDataNames.lei"
+                validation="identifierDoesNotExistValidator:Lei"
+            />
+
             <UploadFormHeader :name="companyDataNames.isin" :explanation="companyDataExplanations.isin" />
             <FormKit
               name="isin"
