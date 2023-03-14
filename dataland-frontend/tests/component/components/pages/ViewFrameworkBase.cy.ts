e68e7f76002c6a7091f6c3be8e1114276383e21f
PaulChr99 <p.chr.1999@gmail.com>
2023-03-14 11:46:33 +0100

    Removed nonsense component test

--- dataland-frontend/tests/component/components/pages/ViewFrameworkBase.cy.ts
+++ dataland-frontend/tests/component/components/pages/ViewFrameworkBase.cy.ts
@@ -1,57 +1,12 @@
 import ViewFrameworkBase from "@/components/generics/ViewFrameworkBase.vue";
-import { DataMetaInformation, QAStatus, DataTypeEnum } from "@clients/backend";
+import { DataMetaInformation, DataTypeEnum } from "@clients/backend";
 import Keycloak from "keycloak-js";
-import { assertDefined } from "@/utils/TypeScriptUtils";
 import { shallowMount } from "@vue/test-utils";
 import { nextTick } from "vue";
 import { humanizeString } from "@/utils/StringHumanizer";
 import { minimalKeycloakMock } from "@ct/testUtils/keycloak";
 
 describe("Component test for ViewFrameworkBase", () => {
-  it("Should display only accepted datasets", () => {
-    const approvedDatasetId = "dataset-1";
-    cy.intercept("**/api/metadata*", {
-      statusCode: 200,
-      body: [
-        {
-          dataId: approvedDatasetId,
-          qaStatus: QAStatus.Accepted,
-        },
-        {
-          dataId: "dataset-2",
-          qaStatus: QAStatus.Pending,
-        },
-      ],
-    });
-    const wrapper = shallowMount(ViewFrameworkBase, {
-      global: {
-        provide: {
-          authenticated: true,
-          getKeycloakPromise() {
-            return Promise.resolve({
-              authenticated: true,
-            });
-          },
-        },
-      },
-      setup() {
-        return {
-          getKeycloakPromise: (): Promise<Keycloak> => {
-            return Promise.resolve({} as Keycloak);
-          },
-        };
-      },
-      props: {
-        companyID: "",
-      },
-    });
-    cy.wait(1000).then(() => {
-      const firstUpdateDataIdEmit = (assertDefined(wrapper.emitted().updateDataId) as string[][][])[0][0];
-      expect(firstUpdateDataIdEmit).to.have.length(1);
-      expect(firstUpdateDataIdEmit[0]).to.equal(approvedDatasetId);
-    });
-  });
-
   it("Should proper set the component based on data", () => {
     cy.fixture("MetaInfoDataForCompany.json").then((newData) => {
       cy.intercept("**/api/metadata*", {
