042724d87c899a7efbae3885905b56eb9d10b6a3
janherre <jan-niklas.herre@d-fine.de>
2023-03-15 17:19:02 +0100

    linting

--- dataland-frontend/tests/e2e/specs/landing-page/FooterSection.ts
+++ dataland-frontend/tests/e2e/specs/landing-page/FooterSection.ts
@@ -10,18 +10,14 @@ describe("As a user, I expect the footer section to be present and contain relev
     cy.get("body").should("contain.text", "Legal");
     cy.get("body").should("contain.text", "Copyright © 2023 Dataland");
     cy.intercept("**/*").as("imprintLink");
-    cy.get('a span[title="imprint"]')
-      .should("contain.text", "Imprint")
-      .click({ force: true })
-    cy.wait("@imprintLink", { timeout: Cypress.env("medium_timeout_in_ms") as number })
+    cy.get('a span[title="imprint"]').should("contain.text", "Imprint").click({ force: true });
+    cy.wait("@imprintLink", { timeout: Cypress.env("medium_timeout_in_ms") as number });
     cy.url().should("include", "/imprint");
     cy.get("h2").contains("Imprint");
     cy.get("[title=back_button").click({ force: true });
     cy.intercept("**/*").as("privacyLink");
-    cy.get('a p[title="data privacy"]')
-      .should("contain.text", "Data Privacy")
-      .click({ force: true })
-    cy.wait("@privacyLink", { timeout: Cypress.env("medium_timeout_in_ms") as number })
+    cy.get('a p[title="data privacy"]').should("contain.text", "Data Privacy").click({ force: true });
+    cy.wait("@privacyLink", { timeout: Cypress.env("medium_timeout_in_ms") as number });
     cy.url().should("include", "/dataprivacy");
     cy.get("h2").contains("Data Privacy");
   });
