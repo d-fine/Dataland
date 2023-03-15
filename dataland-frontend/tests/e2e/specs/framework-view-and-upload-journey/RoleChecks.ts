c554d1c05ff7fe0e3a0c5687400f7fa0a73f08a3
janherre <jan-niklas.herre@d-fine.de>
2023-03-15 16:39:35 +0100

    Stabilizing RoleChecks test

--- dataland-frontend/tests/e2e/specs/framework-view-and-upload-journey/RoleChecks.ts
+++ dataland-frontend/tests/e2e/specs/framework-view-and-upload-journey/RoleChecks.ts
@@ -5,69 +5,78 @@ import { uploadCompanyViaApi } from "@e2e/utils/CompanyUpload";
 import { getKeycloakToken } from "@e2e/utils/Auth";
 import { generateCompanyInformation } from "@e2e/fixtures/CompanyFixtures";
 
-describe("Check if each page is visitable if and only if the corresponding role is given", () => {
-  describeIf(
-    "",
-    {
-      executionEnvironments: ["developmentLocal", "ci"],
-      dataEnvironments: ["fakeFixtures"],
-    },
-    () => {
-      let allPages = [] as string[];
-      let uploaderPages = [] as string[];
-      const noUploaderRightsMessageSelector = "h1:contains('no uploader status')";
+describeIf(
+  "Check if each page is visitable if and only if the corresponding role is given",
+  {
+    executionEnvironments: ["developmentLocal", "ci"],
+    dataEnvironments: ["fakeFixtures"],
+  },
+  () => {
+    let allPages = [] as string[];
+    let uploaderPages = [] as string[];
+    let companyId: string;
+    const noUploaderRightsMessageSelector = "h1:contains('no uploader status')";
 
-      before(() => {
-        getKeycloakToken(uploader_name, uploader_pw).then((token) => {
-          void uploadCompanyViaApi(token, generateCompanyInformation()).then((company) => {
-            allPages = [
-              "/",
-              `/samples/${DataTypeEnum.EutaxonomyNonFinancials}`,
-              "/companies",
-              "/datasets",
-              "/requests",
-              "/api-key",
-              "/dataprivacy",
-              "/imprint",
-              "/nocontent",
-              `/companies/${company.companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}`,
-              `/companies/${company.companyId}/frameworks/${DataTypeEnum.EutaxonomyNonFinancials}`,
-              `/companies/${company.companyId}/frameworks/${DataTypeEnum.Lksg}`,
-            ];
-            uploaderPages = [
-              "/companies/choose",
-              `/companies/${company.companyId}/frameworks/upload`,
-              `/companies/${company.companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}/upload`,
-              `/companies/${company.companyId}/frameworks/${DataTypeEnum.EutaxonomyNonFinancials}/upload`,
-              `/companies/${company.companyId}/frameworks/${DataTypeEnum.Lksg}/upload`,
-            ];
-          });
-        });
+    before(() => {
+      getKeycloakToken(uploader_name, uploader_pw).then(async (token) => {
+        const storedCompany = await uploadCompanyViaApi(token, generateCompanyInformation());
+        companyId = storedCompany.companyId;
+        allPages = [
+          "",
+          `samples/${DataTypeEnum.EutaxonomyNonFinancials}`,
+          "companies",
+          "datasets",
+          "requests",
+          "/api-key",
+          "/dataprivacy",
+          "/imprint",
+          "/nocontent",
+          `/companies/${companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}`,
+          `/companies/${companyId}/frameworks/${DataTypeEnum.EutaxonomyNonFinancials}`,
+          `/companies/${companyId}/frameworks/${DataTypeEnum.Lksg}`,
+        ];
+        uploaderPages = [
+          "/companies/choose",
+          `/companies/${companyId}/frameworks/upload`,
+          `/companies/${companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}/upload`,
+          `/companies/${companyId}/frameworks/${DataTypeEnum.EutaxonomyNonFinancials}/upload`,
+          `/companies/${companyId}/frameworks/${DataTypeEnum.Lksg}/upload`,
+        ];
       });
+    });
 
-      it("Check if a non uploader user can access only the corresponding pages", () => {
-        cy.ensureLoggedIn(reader_name, reader_pw);
-        allPages.forEach((page) => {
+    it("Check if a non uploader user can access only the corresponding pages", () => {
+      cy.ensureLoggedIn(reader_name, reader_pw);
+      allPages.forEach((page) => {
+        it(`Non uploader should be able to access ${page}`, () => {
+          cy.intercept(`**${page}`).as("loadPage");
           cy.visit(page);
-          cy.get(noUploaderRightsMessageSelector).should("not.exist");
-        });
-        uploaderPages.forEach((page) => {
-          cy.visit(page);
-          cy.get(noUploaderRightsMessageSelector).should("exist");
-        });
-      });
-
-      it("Check if an uploader user can access the corresponding pages", () => {
-        cy.ensureLoggedIn(uploader_name, uploader_pw);
-        allPages.forEach((page) => {
-          cy.visit(page);
-          cy.get(noUploaderRightsMessageSelector).should("not.exist");
-        });
-        uploaderPages.forEach((page) => {
-          cy.visit(page);
+          cy.wait("@loadPage", { timeout: Cypress.env("medium_timeout_in_ms") as number });
           cy.get(noUploaderRightsMessageSelector).should("not.exist");
         });
       });
-    }
-  );
-});
+      uploaderPages.forEach((page) => {
+        cy.intercept(`**${page}`).as("loadPage");
+        cy.visit(page);
+        cy.wait("@loadPage", { timeout: Cypress.env("medium_timeout_in_ms") as number });
+        cy.get(noUploaderRightsMessageSelector).should("exist");
+      });
+    });
+
+    it("Check if an uploader user can access the corresponding pages", () => {
+      cy.ensureLoggedIn(uploader_name, uploader_pw);
+      allPages.forEach((page) => {
+        cy.intercept(`**${page}`).as("loadPage");
+        cy.visit(page);
+        cy.wait("@loadPage", { timeout: Cypress.env("medium_timeout_in_ms") as number });
+        cy.get(noUploaderRightsMessageSelector).should("not.exist");
+      });
+      uploaderPages.forEach((page) => {
+        cy.intercept(`**${page}`).as("loadPage");
+        cy.visit(page);
+        cy.wait("@loadPage", { timeout: Cypress.env("medium_timeout_in_ms") as number });
+        cy.get(noUploaderRightsMessageSelector).should("not.exist");
+      });
+    });
+  }
+);
