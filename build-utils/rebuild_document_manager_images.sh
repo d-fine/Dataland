2f299fb07293d9976917774fdcf2ed107849b2bb
Florian Soffel <florian.soffel@d-fine.de>
2023-03-20 14:45:44 +0100

    bugfix

--- build-utils/rebuild_document_manager_images.sh
+++ build-utils/rebuild_document_manager_images.sh
@@ -11,14 +11,14 @@ set -o allexport
 source ./*github_env.log
 set +o allexport
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_document_manager_production ./dataland-document-manager/Dockerfile ./dataland-document-manager/ \
-         ./dataland-backend-utils/ ./dataland-keycloak-adapter/ ./dataland-api-key-manager/apiKeyManagerOpenApi.json \
+./build-utils/base_rebuild_single_docker_image.sh dataland_document_manager_production ./dataland-document-manager/Dockerfile \
+         ./dataland-document-manager/ ./dataland-backend-utils/ ./dataland-keycloak-adapter/ \
          ./dataland-message-queue-utils/ \
          ./dataland-internal-storage/internalStorageOpenApi.json \
          ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_document_manager_test ./dataland-document-manager/DockerfileTest ./dataland-document-manager/ \
-         ./dataland-backend-utils/ ./dataland-keycloak-adapter/ \
+./build-utils/base_rebuild_single_docker_image.sh dataland_document_manager_test ./dataland-document-manager/DockerfileTest \
+         ./dataland-document-manager/ ./dataland-backend-utils/ ./dataland-keycloak-adapter/ \
          ./dataland-message-queue-utils/ \
          ./dataland-internal-storage/internalStorageOpenApi.json \
          ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
