ebdc0f5f48824ffd90e6fd19a92175a6c8d85e22
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-03-21 09:43:27 +0100

    Implemented temporarily cached data controller + API

---
+++ dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/controller/TemporarilyCachedDataController.kt
@@ -0,0 +1,21 @@
+package org.dataland.documentmanager.controller
+
+import org.dataland.documentmanager.api.TemporarilyCachedDataApi
+import org.dataland.documentmanager.services.InMemoryDocumentStore
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Implementation of the controller for delivering and removing temporarily stored data
+ * @param inMemoryStore service to manage datasets from the in memory store
+ */
+@RestController
+class TemporarilyCachedDataController(
+    @Autowired private val inMemoryStore: InMemoryDocumentStore
+): TemporarilyCachedDataApi {
+    override fun getReceivedData(sha256hash: String): ResponseEntity<ByteArray> {
+        val dataset = inMemoryStore.retrieveDataFromMemoryStore(sha256hash)
+        return ResponseEntity.ok(dataset)
+    }
+}
