4f618af1430c8d1e5c208692882290132a74319f
Florian Soffel <florian.soffel@d-fine.de>
2023-03-28 12:53:29 +0200

    Display submit button hidden, when form is not valid

--- dataland-frontend/src/components/forms/parts/SubmitFormBar.vue
+++ dataland-frontend/src/components/forms/parts/SubmitFormBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-header fixed bottom-0 left-0 right-0 m-0 p-1 pr-3 w-full surface-900 grid justify-content-end">
-    <PrimeButton data-test="submitButton" type="submit" :label="label" class="p-disabled" />
+    <PrimeButton data-test="submitButton" type="submit" :label="label" :class="dynamicClasses" />
   </div>
 </template>
 
@@ -17,6 +17,16 @@ export default defineComponent({
     label: {
       type: String,
       default: "SUBMIT FORM",
+    },
+  },
+  computed: {
+    dynamicClasses() {
+      const formIsInvalid = !this.$formkit.get("createLkSGForm")?.context?.state.valid;
+      if (formIsInvalid) {
+        return "button-disabled";
+      } else {
+        return "";
+      }
     },
   },
 });
