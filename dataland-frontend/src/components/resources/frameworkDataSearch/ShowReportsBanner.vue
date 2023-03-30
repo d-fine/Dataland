c5bb151ef28b5e473075aed163366d5beae3d8f8
janherre <jan-niklas.herre@d-fine.de>
2023-03-30 11:29:14 +0200

    add links in banner

--- dataland-frontend/src/components/resources/frameworkDataSearch/ShowReportsBanner.vue
+++ dataland-frontend/src/components/resources/frameworkDataSearch/ShowReportsBanner.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="col-11 text-left bg-white mb-4 mp-0 all-sides-blue-border" style="padding-right: 50pt">
+  <div class="col-12 text-left bg-white mb-4 mp-0 all-sides-blue-border" style="width: 99%">
     <p class="font-bold text-gray-800 mt-0 mb-0 mp-0 ml-0 mr-0" style="font-size: 11pt">
-      Data extracted from the company report. Company reports: {{ listReports }}
+      Data extracted from the company report. Company reports:
+    </p>
+    <p id="reportList">
+      {{ resetReportsCount() }}
+      <span v-for="[name, rep] in Object.entries(reports)" :key="name">
+        {{ reportPlus() }}
+        <a :href="rep.reference">{{ name }}</a> <span v-if="reportCounter < Object.keys(reports).length">, </span>
+      </span>
     </p>
   </div>
 </template>
@@ -15,33 +22,30 @@ export default defineComponent({
   props: {
     reports: { type: Map<string, CompanyReport> },
   },
-  computed: {
-    listReports() {
-      let reportsString = "";
-      if (this.reports != undefined) {
-        Object.keys(this.reports).forEach(function (name) {
-          reportsString = reportsString + name + ", ";
-        });
-      }
-      //const link = rep.reference; //should become link to document manager endpoint let link = documentManagerApi.getDocument(rep.reference)
-      return reportsString;
+  data() {
+    return {
+      reportCounter: 10,
+      test: "test",
+    };
+  },
+  computed: {},
+  methods: {
+    reportPlus() {
+      this.reportCounter++;
+    },
+    resetReportsCount() {
+      this.reportCounter = 0;
+    },
+    getReferencedDocument(hash: string) {
+      return 0;
     },
   },
 });
 </script>
 
 <style>
-.d-progressbar {
-  height: 0.25rem;
-  border-radius: 0.25rem;
-}
-
-.d-card {
-  border-radius: 0.5rem;
-  box-shadow: 0 0 32px 8px rgba(30, 30, 31, 0.08);
-}
-
-.d-card > .p-card-body > .p-card-content {
-  padding: 0;
+a:link {
+  color: var(--yellow-700);
+  text-decoration: none;
 }
 </style>
