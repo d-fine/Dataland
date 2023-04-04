cef68080be705fbb5c4ee209493a782dedeffd88
ahoecherl <andreas.hoecherl@d-fine.de>
2023-04-04 08:46:26 +0200

    Revert "testcommit 2"
    This reverts commit 134527dd5a63539f831e8bb371d7a5165c424c3e.

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/DataControllerTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/DataControllerTest.kt
@@ -42,7 +42,7 @@ class DataControllerTest {
                 testDataEuTaxonomyNonFinancials,
             ),
             companyAssociatedDataEuTaxonomyDataForNonFinancials,
-            "The posted and the received eu taxonomy data sets and/or their company IDs are not equalzz.",
+            "The posted and the received eu taxonomy data sets and/or their company IDs are not equal.",
         )
     }
 
