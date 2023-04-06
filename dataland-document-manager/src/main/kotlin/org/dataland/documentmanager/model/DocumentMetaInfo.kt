5b06999ff1baa30a19f5b58180f8ce05f8f6109c
janherre <jan-niklas.herre@d-fine.de>
2023-04-06 18:04:59 +0200

    conversations

--- dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/model/DocumentMetaInfo.kt
+++ dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/model/DocumentMetaInfo.kt
@@ -5,11 +5,11 @@ import org.dataland.datalandbackendutils.model.QAStatus
 /**
  * --- API model ---
  * Holds the meta info of a document
- * @param documentId document ID
- * @param displayTitle title displayed
- * @param uploaderId uploader ID
- * @param uploadTime timestamp of the upload
- * @param qaStatus qa status of the document
+ * @param [documentId] document ID
+ * @param [displayTitle] title displayed
+ * @param [uploaderId] uploader ID
+ * @param [uploadTime] timestamp of the upload
+ * @param [qaStatus] qa status of the document
  */
 data class DocumentMetaInfo(
     val documentId: String,
--- dataland-frontend/tests/e2e/utils/DocumentUpload.ts
+++ dataland-frontend/tests/e2e/utils/DocumentUpload.ts
@@ -2,33 +2,6 @@ import { Configuration } from "@clients/backend";
 import { DocumentControllerApi, DocumentUploadResponse } from "@clients/documentmanager";
 
 /**
- * Fills the company for a company with the specified name with dummy values.
- *
- * @param companyName the company name to fill into the form
- */
-export function fillCompanyUploadFields(companyName: string): void {
-  cy.get("input[name=companyName]").type(companyName, { force: true });
-  cy.get("input[name=alternativeName]").type("Name to remove", { force: true });
-  cy.get("button[name=addAlternativeName]").click({ force: true });
-  cy.get(`span.form-list-item em`).click();
-  cy.get("span.form-list-item").should("not.exist");
-  cy.get("input[name=alternativeName]").type("Another Name", { force: true });
-  cy.get("button[name=addAlternativeName]").click({ force: true });
-  cy.get("input[name=headquarters]").type("Capitol City", { force: true });
-  cy.get("select[name=countryCode]").select("DE", { force: true });
-  cy.get("input[name=headquartersPostalCode]").type("123456", { force: true });
-  cy.get("input[name=companyLegalForm]").type("Enterprise Ltd.", { force: true });
-  cy.get("input[name=website]").type("www.company.com", { force: true });
-  cy.get("input[name=lei]").type(`LeiValueId:${crypto.randomUUID()}`, { force: true });
-  cy.get("input[name=isin]").type(`IsinValueId:${crypto.randomUUID()}`, { force: true });
-  cy.get("input[name=ticker]").type(`TickerValueId:${crypto.randomUUID()}`, { force: true });
-  cy.get("input[name=permId]").type(`PermValueId:${crypto.randomUUID()}`, { force: true });
-  cy.get("input[name=duns]").type(`DunsValueId:${crypto.randomUUID()}`, { force: true });
-  cy.get("input[name=companyRegistrationNumber]").type(`RegValueId:${crypto.randomUUID()}`, { force: true });
-  cy.get("select[name=sector]").select("Energy");
-}
-
-/**
  * Uploads all documents provided in the <> folder and saves the hashes in a file for later referencing in the datasets
  *
  * @param token the keycloak token for authentication
