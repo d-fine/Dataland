2efc75a8dcb60eb6733805e28f975c7c5862dbbb
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-04-06 12:08:34 +0200

    changes for streamlining setup of proxy

--- build-utils/rebuild_inbound_proxy_images.sh
+++ build-utils/rebuild_inbound_proxy_images.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_proxy_base \
-        ./dataland-inbound-proxy/DockerfileBase ./dataland-inbound-proxy/ ./dataland-frontend/src/assets/images/
-set -o allexport
-source ./*github_env.log
-set +o allexport
 echo "{\"commit\":\"$(git rev-parse HEAD)\",\"time\":\"$(git show -s --format=%aI HEAD)\",\"branch\":\"$(git rev-parse --abbrev-ref HEAD)\"}" > gitinfo
-PROXY_ENVIRONMENT="production" ./build-utils/base_rebuild_single_docker_image.sh dataland_inbound_proxy_production \
-        ./dataland-inbound-proxy/Dockerfile ./dataland-inbound-proxy/ ./dataland-frontend/src/assets/images/ ./gitinfo
-PROXY_ENVIRONMENT="testing" ./build-utils/base_rebuild_single_docker_image.sh dataland_inbound_proxy_testing \
-        ./dataland-inbound-proxy/Dockerfile ./dataland-inbound-proxy/ ./dataland-frontend/src/assets/images/ ./gitinfo
-PROXY_ENVIRONMENT="development" ./build-utils/base_rebuild_single_docker_image.sh dataland_inbound_proxy_development \
+
+./build-utils/base_rebuild_single_docker_image.sh dataland_proxy_base \
         ./dataland-inbound-proxy/Dockerfile ./dataland-inbound-proxy/ ./dataland-frontend/src/assets/images/ ./gitinfo
