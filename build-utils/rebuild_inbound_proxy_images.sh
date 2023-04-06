b9ebe4340cdc38bd7484e3139a1cfe391dfc3fd7
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-04-06 13:08:15 +0200

    fix name

--- build-utils/rebuild_inbound_proxy_images.sh
+++ build-utils/rebuild_inbound_proxy_images.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 
 echo "{\"commit\":\"$(git rev-parse HEAD)\",\"time\":\"$(git show -s --format=%aI HEAD)\",\"branch\":\"$(git rev-parse --abbrev-ref HEAD)\"}" > gitinfo
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_proxy_base \
+./build-utils/base_rebuild_single_docker_image.sh dataland_inbound_proxy \
         ./dataland-inbound-proxy/Dockerfile ./dataland-inbound-proxy/ ./dataland-frontend/src/assets/images/ ./gitinfo
