177b40a43b7c7cb9a7e602fb1a15226445c10b88
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-04-17 08:42:37 +0200

    Move scripts

--- developer-tools/generateMappings.sh
+++ developer-tools/generateMappings.sh
@@ -13,20 +13,22 @@ convertToCamelCase () {
 }
 
 csv_file="$1"
-workdir=$(dirname "$0")
-cd "$workdir"
-
-description_mapping=description.csv
-name_mapping=name.csv
-variable_file=variables.csv
-type_mapping=types.csv
-
-rm "$description_mapping" "$name_mapping" "$variable_file" 2>/dev/null || true
-
 if [[ ! -f "$csv_file" ]]; then
   echo "Error: Expected file $csv_file does not exist."
   exit 1
 fi
+
+work_dir=$(dirname "$0")
+cd "$work_dir"
+
+output_dir=mappings
+rm -r ./$output_dir 2>/dev/null || true
+mkdir -p ./$output_dir
+
+description_mapping=$output_dir/description.csv
+name_mapping=$output_dir/name.csv
+variable_file=$output_dir/variables.csv
+type_mapping=$output_dir/types.csv
 
 while read -r line; do
   name=$(echo "$line" | cut -d';' -f1 | tr -cd '[:alnum:] ')
