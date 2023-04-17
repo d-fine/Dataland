08d6211b4ad3886d865051f99f8f6902cdc88ee5
swaldow <stephan.waldow@d-fine.de>
2023-04-17 16:31:15 +0200

    removes last logging

--- dataland-frontend/src/components/resources/frameworkDataSearch/euTaxonomy/EuTaxonomyPanelFinancials.vue
+++ dataland-frontend/src/components/resources/frameworkDataSearch/euTaxonomy/EuTaxonomyPanelFinancials.vue
@@ -4,7 +4,6 @@
     <i class="pi pi-spinner pi-spin" aria-hidden="true" style="z-index: 20; color: #e67f3f" />
   </div>
   <div v-if="dataSet && !waitingForData">
-    {{ console.log(dataSet.referencedReports.toString()) }}
     <ShowReportsBanner :reports="dataSet.referencedReports" />
     <div class="grid">
       <div class="col-6">
