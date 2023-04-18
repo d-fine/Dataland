00aea5ec8fdd7dcea55d7c601b7c910c99bb7968
janherre <jan-niklas.herre@d-fine.de>
2023-04-18 11:21:32 +0200

    fix formatBytesUserFriendly test

--- dataland-frontend/tests/component/utils/DataFormat.cy.ts
+++ dataland-frontend/tests/component/utils/DataFormat.cy.ts
@@ -2,10 +2,11 @@ import { formatBytesUserFriendly } from "@/utils/NumberConversionUtils";
 
 describe("Unit test for file size display", () => {
   it("Check if file size display in more readable format", () => {
-    const bytesValues = [1000000, 1500000, 500000, 30000, 0];
+    const bytesValues = [1024 * 1024, 1.5 * 1024 * 1024, 500 * 1024, 30 * 1024, 0];
     const outputArray = bytesValues.map((el) => {
-      return formatBytesUserFriendly(el, 3);
+      return formatBytesUserFriendly(el, 1);
     });
-    expect(outputArray).to.deep.equal(["1 MB", "1.5 MB", "500 KB", "30 KB", "0 B"]);
+    console.log(outputArray);
+    expect(outputArray).to.deep.equal(["1 MB", "1.5 MB", "500 KB", "30 KB", "0 Bytes"]);
   });
 });
