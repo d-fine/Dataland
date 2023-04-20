9868b1a942c13ae78e5c6384c69ea30f9598f111
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-04-20 11:55:36 +0200

    Reintroduce production site object

---
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/lksg/LksgAddress.kt
@@ -0,0 +1,17 @@
+package org.dataland.datalandbackend.model.lksg
+
+/**
+ * --- API model ---
+ * Addresses for LkSG framework
+ */
+data class LksgAddress(
+    val streetAndHouseNumber: String,
+
+    val city: String,
+
+    val state: String,
+
+    val postalCode: String,
+
+    val country: String,
+)
