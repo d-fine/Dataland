42c6e5ef2c5c1505ce760fe4808474a877e9c5a9
Florian Soffel <florian.soffel@d-fine.de>
2023-04-24 15:49:30 +0200

    test is working?

--- dataland-frontend/tests/e2e/specs/eu-taxonomy-financials/DataUpload.ts
+++ dataland-frontend/tests/e2e/specs/eu-taxonomy-financials/DataUpload.ts
@@ -1,19 +1,18 @@
 import { describeIf } from "@e2e/support/TestUtility";
 import { generateDummyCompanyInformation, uploadCompanyViaApi } from "@e2e/utils/CompanyUpload";
-import {
-  fillEuTaxonomyForFinancialsUploadForm,
-} from "@e2e/utils/EuTaxonomyFinancialsUpload";
+import { fillEuTaxonomyForFinancialsUploadForm } from "@e2e/utils/EuTaxonomyFinancialsUpload";
 import {
   CompanyInformation,
   EuTaxonomyDataForFinancials,
-  DataTypeEnum, CompanyAssociatedDataEuTaxonomyDataForFinancials,
+  DataTypeEnum,
+  CompanyAssociatedDataEuTaxonomyDataForFinancials,
 } from "@clients/backend";
 import { FixtureData, getPreparedFixture } from "@sharedUtils/Fixtures";
 import { uploader_name, uploader_pw } from "@e2e/utils/Cypress";
 import { getKeycloakToken } from "@e2e/utils/Auth";
-import {uploadReports} from "@sharedUtils/components/UploadReports";
-import {assertDefined} from "@/utils/TypeScriptUtils";
-import {CyHttpMessages} from "cypress/types/net-stubbing";
+import { uploadReports } from "@sharedUtils/components/UploadReports";
+import { assertDefined } from "@/utils/TypeScriptUtils";
+import { CyHttpMessages } from "cypress/types/net-stubbing";
 
 describeIf(
   "As a user, I expect that the upload form works correctly when editing and uploading a new dataset",
@@ -26,18 +25,18 @@ describeIf(
       cy.ensureLoggedIn(uploader_name, uploader_pw);
     });
 
-    let preparedFixtures: Array<FixtureData<EuTaxonomyDataForFinancials>>;
+    let testData: FixtureData<EuTaxonomyDataForFinancials>;
 
     before(function () {
       cy.fixture("CompanyInformationWithEuTaxonomyDataForFinancialsPreparedFixtures").then(function (jsonContent) {
-        preparedFixtures = jsonContent as Array<FixtureData<EuTaxonomyDataForFinancials>>;
+        const preparedFixtures = jsonContent as Array<FixtureData<EuTaxonomyDataForFinancials>>;
+        testData = getPreparedFixture("company-for-all-types", preparedFixtures);
       });
     });
 
     /**
      * Uploads a company via POST-request, then an EU Taxonomy dataset for financial companies for the uploaded company
      * via the form in the frontend, and then visits the view page where that dataset is displayed
-     * and
      *
      * @param companyInformation Company information to be used for the company upload
      * @param testData EU Taxonomy dataset for financial companies to be uploaded
@@ -50,9 +49,9 @@ describeIf(
       companyInformation: CompanyInformation,
       testData: EuTaxonomyDataForFinancials,
       beforeFormFill: () => void,
-      afterFormFill:() => void,
+      afterFormFill: () => void,
       submissionDataIntercept: (request: CyHttpMessages.IncomingHttpRequest) => void,
-      afterDatasetSubmission: (companyId: string) => void,
+      afterDatasetSubmission: (companyId: string) => void
     ): void {
       getKeycloakToken(uploader_name, uploader_pw).then((token: string) => {
         return uploadCompanyViaApi(token, generateDummyCompanyInformation(companyInformation.companyName)).then(
@@ -64,9 +63,9 @@ describeIf(
             beforeFormFill();
             fillEuTaxonomyForFinancialsUploadForm(testData);
             afterFormFill();
-            cy.intercept("POST", `**/api/data/**`, submissionDataIntercept).as("postCompanyAssociatedData");
+            cy.intercept("POST", `**/api/data/**`, submissionDataIntercept).as(postRequestAlias);
             cy.get('button[data-test="submitButton"]').click();
-            cy.wait("@postCompanyAssociatedData", { timeout: 100000 }).then((interception) => {
+            cy.wait(`@${postRequestAlias}`, { timeout: 100000 }).then((interception) => {
               expect(interception.response?.statusCode).to.eq(200);
             });
             afterDatasetSubmission(storedCompany.companyId);
@@ -75,81 +74,91 @@ describeIf(
       });
     }
 
-    function gotoEditForm(companyId: string, expectIncludedFile: boolean) {
-      cy.intercept("GET", "**/api/data/**").as("dataView")
-      cy.visit(
-        `/companies/${companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}`
-      );
-      cy.wait("@dataView");
-      cy.intercept("GET", `**/api/data/**`).as("loadTemplate")
+    const postRequestAlias = "postData";
+
+    /**
+     * Visits the edit page for the eu taxonomy dataset for financial companies via navigation.
+     *
+     * @param companyId the id of the company for which to edit a dataset
+     * @param expectIncludedFile specifies if the file pdfTest.pdf is expected to be in the server response
+     */
+    function gotoEditForm(companyId: string, expectIncludedFile: boolean): void {
+      const getRequestAlias = "getData";
+      cy.intercept("GET", "**/api/data/**").as(getRequestAlias);
+      cy.visit(`/companies/${companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}`);
+      cy.wait(`@${getRequestAlias}`, { timeout: 10000 });
       cy.get('[data-test="editDatasetButton"]').click();
-      cy.wait(`@loadTemplate`).its("response").then((response) => {
-        const data = assertDefined((response?.body as CompanyAssociatedDataEuTaxonomyDataForFinancials)?.data);
-        expect(assertDefined(data.referencedReports).pdfTest !== undefined).to.equal(expectIncludedFile);
-        expect(assertDefined(data.referencedReports).pdfTest2 !== undefined).to.equal(true); // TODO this should not be expected
-        expect(assertDefined(data.referencedReports).pdfTest3 !== undefined).to.equal(false);
-      });
+      cy.wait(`@${getRequestAlias}`, { timeout: 10000 })
+        .its("response")
+        .then((response) => {
+          const data = assertDefined((response?.body as CompanyAssociatedDataEuTaxonomyDataForFinancials)?.data);
+          expect(assertDefined(data.referencedReports).pdfTest !== undefined).to.equal(expectIncludedFile);
+          expect(assertDefined(data.referencedReports).pdfTest2 !== undefined).to.equal(true); // TODO this should not be expected
+          expect(assertDefined(data.referencedReports).pdfTest3 !== undefined).to.equal(false);
+        });
     }
 
     it(
       "Create an Eu Taxonomy Financial dataset via upload form with all financial company types selected to assure " +
       "that the upload form works fine with all options",
       () => {
-        const testData = getPreparedFixture("company-for-all-types", preparedFixtures);
+        testData.companyInformation.companyName = "financials-upload-form";
         uploadCompanyViaApiAndEuTaxonomyDataForFinancialsViaForm(
           testData.companyInformation,
           testData.t,
           () => {
             const filename = "pdfTest.pdf";
-            uploadReports.uploadFile(filename)
-            uploadReports.validateSingleFileInUploadedList(filename, "KB")
-            uploadReports.validateSingleFileInfo()
-            uploadReports.removeSingleUploadedFileFromUploadedList()
-            uploadReports.checkNoReportIsListed()
+            uploadReports.uploadFile(filename);
+            uploadReports.validateSingleFileInUploadedList(filename, "KB");
+            uploadReports.validateSingleFileInfo();
+            uploadReports.removeSingleUploadedFileFromUploadedList();
+            uploadReports.checkNoReportIsListed();
           },
           () => undefined,
           () => undefined,
-          () => undefined,
+          () => undefined
         );
       }
     );
 
-    it.only(
-      "Check if the file upload info remove button works as expected",
-      () => {
-        const testData = getPreparedFixture("company-for-all-types", preparedFixtures);
-        uploadCompanyViaApiAndEuTaxonomyDataForFinancialsViaForm(
-          testData.companyInformation,
-          testData.t,
-          () => undefined,
-          () => {
-            uploadReports.uploadFile("pdfTest.pdf")
-            uploadReports.uploadFile("pdfTest2.pdf")
-            uploadReports.fillAllReportInfoForms()
-            cy.get(`[data-test="assetManagementKpis"]`).find(`[data-test="banksAndIssuers"]`).find('select[name="report"]').select(2);
-          },
-          (request) => {
-            const data = assertDefined((request.body as CompanyAssociatedDataEuTaxonomyDataForFinancials).data);
-            expect(assertDefined(data.referencedReports).pdfTest !== undefined).to.equal(true);
-            expect(assertDefined(data.referencedReports).pdfTest2 !== undefined).to.equal(true); // TODO this should not be expected
-            expect(assertDefined(data.referencedReports).pdfTest3 !== undefined).to.equal(false);
-          },
-          (companyId) => {
-            gotoEditForm(companyId, true)
-            // TODO the following breaks the test before it???
-            // cy.intercept("POST", `**/api/data/**`, (request) => {
-            //   const data = assertDefined((request.body as CompanyAssociatedDataEuTaxonomyDataForFinancials).data);
-            //   expect(assertDefined(data.referencedReports).pdfTest !== undefined).to.equal(false);
-            //   expect(assertDefined(data.referencedReports).pdfTest2 !== undefined).to.equal(true); // TODO this should not be expected
-            //   expect(assertDefined(data.referencedReports).pdfTest3 !== undefined).to.equal(false);
-            // }).as("postModifiedData");
-            // cy.get('button[data-test="submitButton"]').click();
-            // cy.wait("@postModifiedData", { timeout: 100000 }).then((interception) => {
-            //   expect(interception.response?.statusCode).to.eq(200);
-            // });
-          }
-        );
-      },
-    );
+    it("Check if the file upload info remove button works as expected", () => {
+      testData.companyInformation.companyName = "financials-upload-form-remove-document-button";
+      let areBothDocumentsStillUploaded = true;
+      uploadCompanyViaApiAndEuTaxonomyDataForFinancialsViaForm(
+        testData.companyInformation,
+        testData.t,
+        () => undefined,
+        () => {
+          uploadReports.uploadFile("pdfTest.pdf");
+          uploadReports.uploadFile("pdfTest2.pdf");
+          uploadReports.fillAllReportInfoForms();
+          cy.get(`[data-test="assetManagementKpis"]`)
+            .find(`[data-test="banksAndIssuers"]`)
+            .find('select[name="report"]')
+            .select(2);
+        },
+        (request) => {
+          const data = assertDefined((request.body as CompanyAssociatedDataEuTaxonomyDataForFinancials).data);
+          expect(assertDefined(data.referencedReports).pdfTest !== undefined).to.equal(areBothDocumentsStillUploaded);
+          expect(assertDefined(data.referencedReports).pdfTest2 !== undefined).to.equal(true); // TODO this should not be expected
+          expect(assertDefined(data.referencedReports).pdfTest3 !== undefined).to.equal(false);
+        },
+        (companyId) => {
+          gotoEditForm(companyId, true);
+          cy.contains('[data-test="report-info-name"]', "pdfTest")
+            .parent()
+            .find("button")
+            .click()
+            .then(() => {
+              areBothDocumentsStillUploaded = false;
+            });
+          cy.get('button[data-test="submitButton"]').click();
+          cy.wait(`@${postRequestAlias}`, { timeout: 100000 }).then((interception) => {
+            expect(interception.response?.statusCode).to.eq(200);
+          });
+          gotoEditForm(companyId, false);
+        }
+      );
+    });
   }
 );
--- dataland-frontend/tests/sharedUtils/components/UploadReports.ts
+++ dataland-frontend/tests/sharedUtils/components/UploadReports.ts
@@ -1,32 +1,34 @@
 export const uploadReports = {
-  uploadFile(filename: string) {
+  uploadFile(filename: string): void {
     cy.get('button[data-test="upload-files-button"]').click();
     cy.get("input[type=file]").selectFile(`tests/e2e/fixtures/${filename}`, { force: true });
   },
-  fillAllReportInfoForms() {
+  fillAllReportInfoForms(): void {
     cy.get('[data-test="report-info"]').each((element) => {
       cy.wrap(element).find(`[data-test="reportDate"] button`).should("have.class", "p-datepicker-trigger").click();
       cy.get("div.p-datepicker").find('button[aria-label="Previous Month"]').click();
       cy.get("div.p-datepicker").find(`span:contains("12")`).click();
-      cy.wrap(element).find(`input[name="currency"]`).type("zzz")
-      cy.wrap(element).find(`input[value="No"]`).click()
-    })
+      cy.wrap(element).find(`input[name="currency"]`).type("zzz");
+      cy.wrap(element).find(`input[value="No"]`).click();
+    });
   },
-  validateSingleFileInUploadedList(filename = ".pdf", fileDimension: string) {
+  validateSingleFileInUploadedList(filename = ".pdf", fileDimension: string): void {
     cy.get('div[data-test="uploaded-files"]')
       .should("exist")
       .find('[data-test="uploaded-files-title"]')
       .should("contain", filename);
-    cy.get('div[data-test="uploaded-files"]').find('[data-test="uploaded-files-size"]').should("contain", fileDimension);
+    cy.get('div[data-test="uploaded-files"]')
+      .find('[data-test="uploaded-files-size"]')
+      .should("contain", fileDimension);
   },
-  validateSingleFileInfo() {
+  validateSingleFileInfo(): void {
     cy.get('[data-test="report-info"]').find('input[name="currency"]').type("www");
   },
-  removeSingleUploadedFileFromUploadedList() {
+  removeSingleUploadedFileFromUploadedList(): void {
     cy.get('button[data-test="uploaded-files-remove"]').click();
   },
-  checkNoReportIsListed() {
+  checkNoReportIsListed(): void {
     cy.get('div[data-test="uploaded-files"]').should("not.exist");
     cy.get('[data-test="report-info"]').should("not.exist");
   },
-}
+};
