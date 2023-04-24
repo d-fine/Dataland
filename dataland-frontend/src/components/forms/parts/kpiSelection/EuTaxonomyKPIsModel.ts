b5647709c4960e7f9012d1f914faad343ed95060
swaldow <stephan.waldow@d-fine.de>
2023-04-24 11:49:27 +0200

    adds full stops to tooltips

--- dataland-frontend/src/components/forms/parts/kpiSelection/EuTaxonomyKPIsModel.ts
+++ dataland-frontend/src/components/forms/parts/kpiSelection/EuTaxonomyKPIsModel.ts
@@ -91,89 +91,89 @@ export const euTaxonomyKpiNameMappings = {
   totalOpEx: "Total OpEx",
 };
 export const euTaxonomyKpiInfoMappings = {
-  investmentNonNfrd: "For financial companies (FS), the percentage of total assets exposed to non-NFRD entities",
+  investmentNonNfrd: "For financial companies (FS), the percentage of total assets exposed to non-NFRD entities.",
   banksAndIssuers:
-    "For financial companies (FS), the percentage of total assets exposed to central governments, central banks, supranational issuers",
-  derivatives: "For financial companies (FS), the percentage of total assets exposed to derivatives",
+    "For financial companies (FS), the percentage of total assets exposed to central governments, central banks, supranational issuers.",
+  derivatives: "For financial companies (FS), the percentage of total assets exposed to derivatives.",
   taxonomyNonEligibleActivity:
-    "For financial companies (FS), the percentage of total assets exposed to taxonomy non-eligible economic activities. i.e., to types of economic activity that is not described and does not have technical screening criteria set out in the EU Taxonomy Regulation",
+    "For financial companies (FS), the percentage of total assets exposed to taxonomy non-eligible economic activities. i.e., to types of economic activity that is not described and does not have technical screening criteria set out in the EU Taxonomy Regulation.",
   taxonomyEligibleActivity:
-    "For financial companies (FS), the percentage of total assets exposed to taxonomy-eligible economic activities",
+    "For financial companies (FS), the percentage of total assets exposed to taxonomy-eligible economic activities.",
   taxonomyEligibleNonLifeInsuranceActivities:
-    "For Insurance/Reinsurance companies, the percentage of Taxonomy-eligible non-life insurance economics activities. Insurance and reinsurance undertakings other than life insurance undertakings shall calculate the KPI related to underwriting activities and present the ‘gross premiums written’ non-life insurance revenue or, as applicable, reinsurance revenue corresponding to Taxonomy-aligned insurance or reinsurance activities",
+    "For Insurance/Reinsurance companies, the percentage of Taxonomy-eligible non-life insurance economics activities. Insurance and reinsurance undertakings other than life insurance undertakings shall calculate the KPI related to underwriting activities and present the ‘gross premiums written’ non-life insurance revenue or, as applicable, reinsurance revenue corresponding to Taxonomy-aligned insurance or reinsurance activities.",
   greenAssetRatio:
-    "The proportion of assets financing and invested in taxonomy-aligned economic activities as a proportion of total covered assets",
-  tradingPortfolio: "For Credit Institutions, the trading portfolio as a percentage of total assets",
+    "The proportion of assets financing and invested in taxonomy-aligned economic activities as a proportion of total covered assets.",
+  tradingPortfolio: "For Credit Institutions, the trading portfolio as a percentage of total assets.",
   tradingPortfolioAndInterbankLoans:
-    "For Credit Institutions, the trading portfolio and the on demand interbank loans as a percentage of total assets",
-  interbankLoans: "For Credit Institutions, the on demand interbank loans as a percentage of total assets",
-  reportDate: "The date until which the information presented in the report is valid",
-  reportingPeriod: "The reporting period the dataset belongs to (e.g. a fiscal year)",
-  reportingObligation: "The reporting obligation for companies whose number of employees is greater or equal to 500",
+    "For Credit Institutions, the trading portfolio and the on demand interbank loans as a percentage of total assets.",
+  interbankLoans: "For Credit Institutions, the on demand interbank loans as a percentage of total assets.",
+  reportDate: "The date until which the information presented in the report is valid.",
+  reportingPeriod: "The reporting period the dataset belongs to (e.g. a fiscal year).",
+  reportingObligation: "The reporting obligation for companies whose number of employees is greater or equal to 500.",
   totalAmount: "Total Amount",
   alignedPercentage: "Aligned Percentage",
   eligiblePercentage: "Eligible Percentage",
 
-  isfs: "Distinguishes between non-financial companies (IS) and financial companies (FS). There are two possible values expected: 1- IS, 2- FS",
+  isfs: "Distinguishes between non-financial companies (IS) and financial companies (FS). There are two possible values expected: 1- IS, 2- FS.",
   financialServicesTypes:
-    "Defines the type of financial company. Credit Institution (1), Insurance/Reinsurance (2), Asset Manager (3), Investment Firm (4)",
+    "Defines the type of financial company. Credit Institution (1), Insurance/Reinsurance (2), Asset Manager (3), Investment Firm (4).",
 
   fiscalYearDeviation: "Does the fiscal year deviate from the calender year?",
   fiscalYear: "Fiscal Year (Deviation/ No Deviation)",
-  fiscalYearEnd: "The date at which the fiscal year ends",
+  fiscalYearEnd: "The date at which the fiscal year ends.",
 
-  annualReport: "Link to Annual Report",
+  annualReport: "Link to Annual Report.",
   groupLevelAnnualReport: "Is the Annual Report on a Group level?",
-  annualReportDate: "The date until which the information presented in the report is valid",
-  annualReportCurrency: "The 3-letter alpha code that represents the currency used in the report",
+  annualReportDate: "The date until which the information presented in the report is valid.",
+  annualReportCurrency: "The 3-letter alpha code that represents the currency used in the report.",
 
-  activityLevelReporting: "Does the company report on the EU Taxonomy",
+  activityLevelReporting: "Does the company report on the EU Taxonomy?",
   numberOfEmployees: "Total number of employees (including temporary workers)",
-  scopeOfEntities: "Are all Group legal entities covered in the report",
-  currency: "The 3-letter alpha code that represents the currency used in the report",
+  scopeOfEntities: "Are all Group legal entities covered in the report?",
+  currency: "The 3-letter alpha code that represents the currency used in the report.",
 
-  sustainabilityReport: "Link to Sustainability Report",
+  sustainabilityReport: "Link to Sustainability Report.",
   groupLevelSustainabilityReport: "Is the Sustainability Report on a Group level?",
-  sustainabilityReportDate: "The date until which the information presented in the report is valid",
-  sustainabilityReportCurrency: "The 3-letter alpha code that represents the currency used in the report",
-  integratedReport: "Link to Integrated Report",
+  sustainabilityReportDate: "The date until which the information presented in the report is valid.",
+  sustainabilityReportCurrency: "The 3-letter alpha code that represents the currency used in the report.",
+  integratedReport: "Link to Integrated Report.",
   groupLevelIntegratedReport: "Is the Integrated Report on a Group level?",
-  integratedReportDate: "The date until which the information presented in the report is valid",
-  integratedReportCurrency: "The 3-letter alpha code that represents the currency used in the report",
-  esefReport: "Link to ESEF Report",
+  integratedReportDate: "The date until which the information presented in the report is valid.",
+  integratedReportCurrency: "The 3-letter alpha code that represents the currency used in the report.",
+  esefReport: "Link to ESEF Report.",
   groupLevelEsefReport: "Is the ESEF Report on a Group level?",
-  esefReportDate: "The date until which the information presented in the report is valid",
-  esefReportCurrency: "The 3-letter alpha code that represents the currency used in the report",
+  esefReportDate: "The date until which the information presented in the report is valid.",
+  esefReportCurrency: "The 3-letter alpha code that represents the currency used in the report.",
   nfrdMandatory: "Does the company fall under NFRD?",
 
-  quality: "The level of confidence associated to the value",
-  page: "The page number of the document from where the information was sourced",
-  report: "The report from where the information was sourced",
-  tagName: "The name of the tag where the information is in the pdf",
+  quality: "The level of confidence associated to the value.",
+  page: "The page number of the document from where the information was sourced.",
+  report: "The report from where the information was sourced.",
+  tagName: "The name of the tag where the information is in the pdf.",
   comment: "Free optional text",
 
-  assurance: "Level of Assurance of the EU Taxonomy disclosure (Reasonable Assurance, Limited Assurance, None)",
-  provider: "Name of the Audit company which provides assurance to EU Taxonomy data points",
+  assurance: "Level of Assurance of the EU Taxonomy disclosure (Reasonable Assurance, Limited Assurance, None).",
+  provider: "Name of the Audit company which provides assurance to EU Taxonomy data points.",
   AssurancePage:
-    "The page number of the text proving that the EU Taxonomy disclosures are included in Assurance statement",
-  AssuranceReport: "Link to Assurance Report",
+    "The page number of the text proving that the EU Taxonomy disclosures are included in Assurance statement.",
+  AssuranceReport: "Link to Assurance Report.",
   eligibleRevenue:
-    "Percentage of the revenue where the economic activity meets taxonomy criteria for substantial contribution to climate change mitigation and does no serious harm to the other environmental objectives (DNSH criteria)",
+    "Percentage of the revenue where the economic activity meets taxonomy criteria for substantial contribution to climate change mitigation and does no serious harm to the other environmental objectives (DNSH criteria).",
   alignedRevenue:
-    "Percentage of the revenue that is taxonomy-aligned, i.e., generated by an eligible economic activity that is making a substantial contribution to at least one of the climate and environmental objectives, while also doing no significant harm to the remaining objectives and meeting minimum standards on human rights and labour standards",
+    "Percentage of the revenue that is taxonomy-aligned, i.e., generated by an eligible economic activity that is making a substantial contribution to at least one of the climate and environmental objectives, while also doing no significant harm to the remaining objectives and meeting minimum standards on human rights and labour standards.",
   totalRevenue:
-    "Total revenue in MM (Millions) for the financial year. i.e., income arising in the course of an entity's ordinary activities, the amounts derived from the sale of products and the provision of services after deducting sales rebates and value added tax and other taxes directly linked to turnover. Overall turnover is equivalent to a firm's total revenues over some period of time",
+    "Total revenue in MM (Millions) for the financial year. i.e., income arising in the course of an entity's ordinary activities, the amounts derived from the sale of products and the provision of services after deducting sales rebates and value added tax and other taxes directly linked to turnover. Overall turnover is equivalent to a firm's total revenues over some period of time.",
   eligibleCapEx:
     "Percentage of the CapEx that is part of a plan to meet taxonomy criteria for substantial contribution to climate change adaptation and relevant DNSH criteria",
   alignedCapEx:
-    "Percentage of the CapEx that is either already taxonomy-aligned or is part of a credible plan to extend or reach taxonomy alignment. i.e., an eligible economic activity that is making a substantial contribution to at least one of the climate and environmental objectives, while also doing no significant harm to the remaining objectives and meeting minimum standards on human rights and labour standards",
+    "Percentage of the CapEx that is either already taxonomy-aligned or is part of a credible plan to extend or reach taxonomy alignment. i.e., an eligible economic activity that is making a substantial contribution to at least one of the climate and environmental objectives, while also doing no significant harm to the remaining objectives and meeting minimum standards on human rights and labour standards.",
   totalCapEx:
-    "Total CapEx in MM (Millions) for the financial year. A capital expenditure (CapEx) is a payment for goods or services recorded, or capitalized, on the balance sheet instead of expensed on the income statement",
+    "Total CapEx in MM (Millions) for the financial year. A capital expenditure (CapEx) is a payment for goods or services recorded, or capitalized, on the balance sheet instead of expensed on the income statement.",
 
   eligibleOpEx:
-    "Percentage of the OpEx that is part of a plan to meet taxonomy criteria for substantial contribution to climate change adaptation and relevant DNSH criteria",
+    "Percentage of the OpEx that is part of a plan to meet taxonomy criteria for substantial contribution to climate change adaptation and relevant DNSH criteria.",
   alignedOpEx:
-    "Percentage of the OpEx that is associated with taxonomy-aligned activities. i.e., for an eligible economic activity that is making a substantial contribution to at least one of the climate and environmental objectives, while also doing no significant harm to the remaining objectives and meeting minimum standards on human rights and labour standards",
+    "Percentage of the OpEx that is associated with taxonomy-aligned activities. i.e., for an eligible economic activity that is making a substantial contribution to at least one of the climate and environmental objectives, while also doing no significant harm to the remaining objectives and meeting minimum standards on human rights and labour standards.",
   totalOpEx:
-    "Total OpEx in MM (Millions) for the financial year. Operating expenses (OpEx) are shorter term expenses required to meet the ongoing operational costs of running a business",
+    "Total OpEx in MM (Millions) for the financial year. Operating expenses (OpEx) are shorter term expenses required to meet the ongoing operational costs of running a business.",
 };
