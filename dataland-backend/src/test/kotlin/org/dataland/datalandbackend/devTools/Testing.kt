4626e10a9ebdc26891e0d756663c81a28da6e031
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-05-02 12:58:42 +0200

    Implemented Prototype reflection export in FQJP format for easy comparison with frontend generated models

---
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/devTools/Testing.kt
@@ -0,0 +1,32 @@
+package org.dataland.datalandbackend.devTools
+
+import org.dataland.datalandbackend.model.lksg.LksgData
+import java.io.File
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.memberProperties
+
+fun main() {
+    val model = LksgData::class
+    val propertyList = model.listAllProperties("").toList()
+    File("./fqjpList.txt").writeText(propertyList.sorted().joinToString("\n"))
+}
+
+fun KClass<*>.listAllProperties(baseName: String): Sequence<String> = sequence {
+    if (isTerminal(this@listAllProperties)) {
+       yield("$baseName: $simpleName")
+    }  else {
+        val prefix = if (baseName.isBlank()) "" else "${baseName}."
+        memberProperties.forEach {
+            val childBaseName = "$prefix${it.name}"
+            val propetryKClass = it.returnType.classifier as? KClass<*>
+            yieldAll(propetryKClass!!.listAllProperties(childBaseName))
+        }
+    }
+}
+
+fun isTerminal(propertyClass: KClass<*>): Boolean {
+    val isEnum = propertyClass.isSubclassOf(Enum::class)
+    val isInDatalandPackage = propertyClass.qualifiedName?.contains("org.dataland") ?: false
+    return isEnum || !isInDatalandPackage
+}
