64a1048178b4a281cc47cc09b5b5b6d646c21168
swaldow <stephan.waldow@d-fine.de>
2023-05-04 17:16:27 +0200

    linting

--- dataland-frontend/tests/e2e/specs/eu-taxonomy-financials/DataUpload.ts
+++ dataland-frontend/tests/e2e/specs/eu-taxonomy-financials/DataUpload.ts
@@ -145,9 +145,11 @@ describeIf(
             areBothDocumentsStillUploaded = false;
           });
           cy.get('button[data-test="submitButton"]').click();
-          cy.wait(`@${postRequestAlias}`, { timeout: Cypress.env("short_timeout_in_ms") as number }).then((interception) => {
-            expect(interception.response?.statusCode).to.eq(200);
-          });
+          cy.wait(`@${postRequestAlias}`, { timeout: Cypress.env("short_timeout_in_ms") as number }).then(
+            (interception) => {
+              expect(interception.response?.statusCode).to.eq(200);
+            }
+          );
           gotoEditForm(companyId, false);
         }
       );
