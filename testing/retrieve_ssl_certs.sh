b17bfb43b611f3e7883249d21958e07e87a363bf
Philip Puetsch <philip.puetsch@d-fine.com>
2023-05-05 16:22:52 +0200

    changes secrets to vars

--- testing/retrieve_ssl_certs.sh
+++ testing/retrieve_ssl_certs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p ~/.ssh/
-echo "$DATALAND_LETSENCRYPT_HOST_KEYS" >>  ~/.ssh/known_hosts
+echo "TARGETSERVER_HOST_KEYS" >>  ~/.ssh/known_hosts
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa && mkdir -p ./local/certs
 scp ubuntu@letsencrypt.dataland.com:/etc/letsencrypt/live/local-dev.dataland.com/* ./local/certs
