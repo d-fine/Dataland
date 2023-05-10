7b2d8f12ddde50a314017f08d2816ba267cd73d0
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2023-05-10 14:24:14 +0200

    Dala 1574 avoid publishing secrets (#218)
    Bug fixes for deployment
    
    ---------
    
    Co-authored-by: Florian Soffel <florian.soffel@d-fine.de>
    Co-authored-by: Andreas Hoecherl <35459207+ahoecherl@users.noreply.github.com>
    Co-authored-by: swaldow <stephan.waldow@d-fine.de>

--- dataland-rabbitmq/custom_entrypoint.sh
+++ dataland-rabbitmq/custom_entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sed -i 's/RABBITMQ_USER_PLACEHOLDER/'"$RABBITMQ_USER"'/g' /etc/rabbitmq/definitions.json
-sed -i 's/RABBITMQ_PASS_HASH_PLACEHOLDER/'"$RABBITMQ_PASS_HASH"'/g' /etc/rabbitmq/definitions.json
-echo Definitions file used for initiation of RabbitMQ:
-cat /etc/rabbitmq/definitions.json
+sed -i 's%RABBITMQ_PASS_HASH_PLACEHOLDER%'"$RABBITMQ_PASS_HASH"'%g' /etc/rabbitmq/definitions.json
+echo "Replacing placeholders in rabbitmq definitions.json"
 docker-entrypoint.sh $1
