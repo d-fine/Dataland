86674ebd14895986654c7eebf80cb0e2af738e8b
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-05-11 13:15:10 +0200

    only create api once and delete data after upload

--- dataland-frontend/tests/e2e/utils/DocumentUpload.ts
+++ dataland-frontend/tests/e2e/utils/DocumentUpload.ts
@@ -7,30 +7,15 @@ import { DocumentControllerApi } from "@clients/documentmanager";
  */
 export function uploadAllDocuments(token: string): void {
   const documentDirectory = "../testing/data/documents/";
+  const api = new DocumentControllerApi(new Configuration({ accessToken: token }));
   cy.task("readdir", documentDirectory).then((fileNames) => {
     (fileNames as string[]).forEach((name: string) => {
       cy.task<{ [type: string]: ArrayBuffer }>("readFile", documentDirectory + name).then((bufferObject) => {
-        uploadDocumentViaApi(token, bufferObject.data, name).catch((error) => console.log(error));
+        const arr = new Uint8Array(bufferObject.data);
+        const file = new File([arr], name, { type: "application/pdf" });
+        api.postDocument(file).catch((error) => console.log(error));
+        delete bufferObject.data;
       });
-      cy.clearLocalStorage();
-      cy.clearAllLocalStorage();
-      cy.clearAllSessionStorage();
     });
   });
-}
-
-/**
- * Uses the Dataland API to upload a document
- * @param token the bearer token used to authorize the API requests
- * @param buffer the pdf document as an arrayBuffer to be uploaded as File
- * @param name the file name
- */
-export async function uploadDocumentViaApi(token: string, buffer: ArrayBuffer, name: string): Promise<void> {
-  const arr = new Uint8Array(buffer);
-  const file = new File([arr], name, { type: "application/pdf" });
-  await new DocumentControllerApi(
-    new Configuration({
-      accessToken: token,
-    })
-  ).postDocument(file);
 }
