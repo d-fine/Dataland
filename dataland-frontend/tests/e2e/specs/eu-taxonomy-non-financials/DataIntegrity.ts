70a19f0bb81b340cce36971ddf16f03fcfb56ec0
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-05-11 16:05:02 +0200

    remove only-tag from test which was accidentally pushed

--- dataland-frontend/tests/e2e/specs/eu-taxonomy-non-financials/DataIntegrity.ts
+++ dataland-frontend/tests/e2e/specs/eu-taxonomy-non-financials/DataIntegrity.ts
@@ -105,8 +105,7 @@ describeIf(
       });
     });
 
-    it.only(
-      // TODO remove "only"
+    it(
       "Upload EU Taxonomy Dataset via form with no values for revenue and assure that it can be viewed on the framework " +
         "data view page with an appropriate message shown for the missing revenue data",
       () => {
