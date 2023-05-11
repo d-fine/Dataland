86a45db39c03aa575f8d970ac682ef7953e84533
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-05-11 11:18:10 +0200

    remove TODO

--- dataland-frontend/src/components/forms/parts/fields/NumberFormField.vue
+++ dataland-frontend/src/components/forms/parts/fields/NumberFormField.vue
@@ -23,6 +23,4 @@ export default defineComponent({
   components: { FormKit, UploadFormHeader },
   props: FormFieldPropsWithPlaceholder,
 });
-
-// TODO what about props for e.g. min and step size
 </script>
