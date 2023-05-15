facc8843d62880eff2a53feb5972e180a5419742
Florian Soffel <florian.soffel@d-fine.de>
2023-05-15 13:07:49 +0200

    empty list of goods and services string after processing

--- dataland-frontend/src/components/forms/parts/elements/derived/ProductionSiteFormElement.vue
+++ dataland-frontend/src/components/forms/parts/elements/derived/ProductionSiteFormElement.vue
@@ -101,6 +101,7 @@ export default defineComponent({
     addNewItemsToListOfGoodsOrServices() {
       const items = this.listOfGoodsOrServicesString.split(",").map((element) => element.trim());
       this.listOfGoodsOrServices = [...new Set([...this.listOfGoodsOrServices, ...items])];
+      this.listOfGoodsOrServicesString = "";
     },
   },
 });
