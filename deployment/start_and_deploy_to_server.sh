556fac45ead574a3ae575b63f6b030275324266f
Philip Puetsch <philip.puetsch@d-fine.com>
2023-05-17 14:26:07 +0200

    bugfix

--- deployment/start_and_deploy_to_server.sh
+++ deployment/start_and_deploy_to_server.sh
@@ -48,7 +48,7 @@ ssh ubuntu@"$target_server_url" "set -o allexport; source \"$location\"/.env; se
 echo "Cleaning up exported user files."
 ssh ubuntu@"$target_server_url" "(cp $keycloak_user_dir/*-users-*.json $persistent_keycloak_backup_dir; rm $keycloak_user_dir/*.json) || true"
 
-if [[ $RESET_BACKEND_DATABASE_AND_REPOPULATE == true ]]; then
+if [[ $RESET_STACK_AND_REPOPULATE == true ]]; then
   echo "Deleting relevant Volumes"
   delete_docker_volume_if_existent_remotely "backend_data" "$target_server_url" "$location"
   delete_docker_volume_if_existent_remotely "document_manager_data" "$target_server_url" "$location"
