2f0e3116e8503b18fc2d93dd8b37593a4ae21456
swaldow <stephan.waldow@d-fine.de>
2023-05-24 17:55:01 +0200

    adds dummy page

---
+++ dataland-frontend/src/components/pages/QualityAssurance.vue
@@ -0,0 +1,135 @@
+<template>
+  <AuthenticationWrapper>
+    <TheHeader />
+
+    <TheContent class="paper-section flex">
+      <div class="col-12 text-left pb-0">
+        <BackButton />
+        <h1>"TEST"</h1>
+      </div>
+      <MiddleCenterDiv class="col-12">
+        <div>
+          <PrimeButton label="Accept Dataset" />
+        </div>
+        <div>
+          <PrimeButton label="Reject Dataset" />
+        </div>
+      </MiddleCenterDiv>
+    </TheContent>
+
+    <TheFooter />
+  </AuthenticationWrapper>
+</template>
+
+<script lang="ts">
+import BackButton from "@/components/general/BackButton.vue";
+import MiddleCenterDiv from "@/components/wrapper/MiddleCenterDivWrapper.vue";
+import TheFooter from "@/components/general/TheFooter.vue";
+import PrimeButton from "primevue/button";
+import TheContent from "@/components/generics/TheContent.vue";
+import TheHeader from "@/components/generics/TheHeader.vue";
+import AuthenticationWrapper from "@/components/wrapper/AuthenticationWrapper.vue";
+import { defineComponent, inject } from "vue";
+import Keycloak from "keycloak-js";
+import { DataTypeEnum, LksgData, SfdrData } from "@clients/backend";
+import { ApiClientProvider } from "@/services/ApiClients";
+import { assertDefined } from "@/utils/TypeScriptUtils";
+export default defineComponent({
+  name: "QualityAssurance",
+  components: { TheFooter, MiddleCenterDiv, BackButton, TheContent, TheHeader, AuthenticationWrapper, PrimeButton },
+  setup() {
+    return {
+      getKeycloakPromise: inject<() => Promise<Keycloak>>("getKeycloakPromise"),
+    };
+  },
+  data() {
+    return {
+      dataId: "loading",
+      waitingForData: true,
+      dataSet: null | undefined,
+    };
+  },
+  mounted() {
+    // void this.getDataID();
+  },
+
+  methods: {
+    /**
+     * Uses the dataland API to retrieve the companyId of the first teaser company and the dataId
+     * of the eutaxonomy-non-financials framework of that company.
+     */
+    async getDataSet() {
+      try {
+        const metaDataControllerApi = await new ApiClientProvider(
+          assertDefined(this.getKeycloakPromise)()
+        ).getMetaDataControllerApi();
+        const apiResponse = await metaDataControllerApi.getDataMetaInfo(this.dataId);
+        const filteredData = apiResponse.data.dataType;
+        if (filteredData === DataTypeEnum.EutaxonomyNonFinancials) {
+          try {
+            this.waitingForData = true;
+            if (this.dataId != "loading") {
+              const euTaxonomyDataForNonFinancialsControllerApi = await new ApiClientProvider(
+                assertDefined(this.getKeycloakPromise)()
+              ).getEuTaxonomyDataForNonFinancialsControllerApi();
+              const companyAssociatedData =
+                await euTaxonomyDataForNonFinancialsControllerApi.getCompanyAssociatedEuTaxonomyDataForNonFinancials(
+                  assertDefined(this.dataId)
+                );
+              this.dataSet = companyAssociatedData.data.data;
+              this.waitingForData = false;
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        } else if (filteredData === DataTypeEnum.EutaxonomyFinancials) {
+          try {
+            this.waitingForData = true;
+            if (this.dataId != "loading") {
+              const euTaxonomyDataForFinancialsControllerApi = await new ApiClientProvider(
+                assertDefined(this.getKeycloakPromise)()
+              ).getEuTaxonomyDataForFinancialsControllerApi();
+              const companyAssociatedData =
+                await euTaxonomyDataForFinancialsControllerApi.getCompanyAssociatedEuTaxonomyDataForFinancials(
+                  assertDefined(this.dataId)
+                );
+              this.dataSet = companyAssociatedData.data.data;
+              this.waitingForData = false;
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        } else if (filteredData === DataTypeEnum.Lksg) {
+          try {
+            this.waitingForData = true;
+            const lksgDataControllerApi = await new ApiClientProvider(
+              assertDefined(this.getKeycloakPromise)()
+            ).getLksgDataControllerApi();
+            const singleLksgData = (await lksgDataControllerApi.getCompanyAssociatedLksgData(this.dataId)).data
+              .data as LksgData;
+
+            this.waitingForData = false;
+          } catch (error) {
+            console.error(error);
+          }
+        } else if (filteredData === DataTypeEnum.Sfdr) {
+          try {
+            this.waitingForData = true;
+            const sfdrDataControllerApi = await new ApiClientProvider(
+              assertDefined(this.getKeycloakPromise)()
+            ).getSfdrDataControllerApi();
+
+            const singleSfdrData = (await sfdrDataControllerApi.getCompanyAssociatedSfdrData(this.dataId)).data
+              .data as SfdrData;
+            this.waitingForData = false;
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+});
+</script>
