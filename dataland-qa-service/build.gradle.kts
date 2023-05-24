a0049cb9e7e88337a14374a8d173396a8bb268be
Florian Soffel <florian.soffel@d-fine.de>
2023-05-24 16:15:37 +0200

    Make swagger ui and endpoints public

--- dataland-qa-service/build.gradle.kts
+++ dataland-qa-service/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.amqp:spring-rabbit-test")
 
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation(libs.springdoc.openapi.ui)
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
