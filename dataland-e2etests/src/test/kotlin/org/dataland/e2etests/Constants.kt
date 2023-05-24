fcedcd341c8a932359468f64dd1f4ab0672c56f6
Florian Soffel <florian.soffel@d-fine.de>
2023-05-24 10:33:51 +0200

    Added keycloak reviewer role and user and added role to admin user

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/Constants.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/Constants.kt
@@ -13,6 +13,16 @@ const val PATH_TO_KEYCLOAK_TOKENENDPOINT =
 const val TOKENREQUEST_GRANT_TYPE = "password"
 const val TOKENREQUEST_CLIENT_ID = "dataland-public"
 
+const val ADMIN_USER_NAME = "data_admin"
+const val ADMIN_USER_ID = "136a9394-4873-4a61-a25b-65b1e8e7cc2f"
+val ADMIN_USER_PASSWORD: String = System.getenv("KEYCLOAK_DATALAND_ADMIN_PASSWORD")
+val ADMIN_EXTENDED_ROLES = listOf("ROLE_ADMIN")
+
+const val REVIEWER_USER_NAME = "data_reviewer"
+const val REVIEWER_USER_ID = "f7a02ff1-0dab-4e10-a908-7d775c1014ae"
+val REVIEWER_USER_PASSWORD: String = System.getenv("KEYCLOAK_REVIEWER_PASSWORD")
+val REVIEWER_EXTENDED_ROLES = listOf("ROLE_REVIEWER")
+
 const val UPLOADER_USER_NAME = "data_uploader"
 const val UPLOADER_USER_ID = "c5ef10b1-de23-4a01-9005-e62ea226ee83"
 val UPLOADER_USER_PASSWORD: String = System.getenv("KEYCLOAK_UPLOADER_PASSWORD")
@@ -21,11 +31,6 @@ val UPLOADER_EXTENDED_ROLES = listOf("ROLE_UPLOADER")
 const val READER_USER_NAME = "data_reader"
 const val READER_USER_ID = "18b67ecc-1176-4506-8414-1e81661017ca"
 val READER_USER_PASSWORD: String = System.getenv("KEYCLOAK_READER_PASSWORD")
-
-const val ADMIN_USER_NAME = "data_admin"
-const val ADMIN_USER_ID = "136a9394-4873-4a61-a25b-65b1e8e7cc2f"
-val ADMIN_USER_PASSWORD: String = System.getenv("KEYCLOAK_DATALAND_ADMIN_PASSWORD")
-val ADMIN_EXTENDED_ROLES = listOf("ROLE_ADMIN")
 
 val MUTUAL_ROLES_DATALAND_USERS =
     listOf("default-roles-datalandsecurity", "ROLE_USER", "offline_access", "uma_authorization")
