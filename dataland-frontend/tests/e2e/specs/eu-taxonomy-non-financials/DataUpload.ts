02c1d03e485ccf3b7674e1148774978cc963907e
Piotr Skonieczny <piotr.skonieczny@pwc.com>
2023-05-29 10:26:11 +0200

    Fixed lint warning

--- dataland-frontend/tests/e2e/specs/eu-taxonomy-non-financials/DataUpload.ts
+++ dataland-frontend/tests/e2e/specs/eu-taxonomy-non-financials/DataUpload.ts
@@ -103,7 +103,7 @@ describeIf(
               uploadReports.selectFile(TEST_PDF_FILE_NAME);
               uploadReports.selectFile(`${TEST_PDF_FILE_NAME}2`);
               uploadReports.fillAllReportsToUploadForms(2);
-            
+
               cy.get(`[data-test="capexSection"] [data-test="total"] select[name="report"]`).select(TEST_PDF_FILE_NAME);
               cy.get(`[data-test="opexSection"] [data-test="total"] select[name="report"]`).select(
                 `${TEST_PDF_FILE_NAME}2`
