74159afa866641836e61be681e8057617e81e216
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-05-30 16:55:47 +0200

    fix for issue

--- dataland-frontend/tests/e2e/fixtures/lksg/LksgDataFixtures.ts
+++ dataland-frontend/tests/e2e/fixtures/lksg/LksgDataFixtures.ts
@@ -87,13 +87,14 @@ export function randomShareOfTemporaryWorkersInterval(): ShareOfTemporaryWorkers
 }
 
 /**
- * Generates a random list of Nace codes but sorted
- * @returns random list of goods or services
+ * Generates a random list of Nace codes (unique and sorted)
+ * @returns random list of Nace codes
  */
 export function generateListOfNaceCodes(): string[] {
-  return Array.from({ length: faker.datatype.number({ min: 0, max: 5 }) }, () => {
+  const values = Array.from({ length: faker.datatype.number({ min: 0, max: 5 }) }, () => {
     return faker.helpers.arrayElement(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]);
   }).sort((a, b) => a.localeCompare(b));
+  return [...new Set(values)];
 }
 
 /**
