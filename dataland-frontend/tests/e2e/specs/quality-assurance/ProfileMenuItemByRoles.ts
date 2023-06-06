9f8d7dd48345ec7da2bc17aabc5352a49d8d3ab1
Florian Soffel <florian.soffel@d-fine.de>
2023-06-06 09:36:41 +0200

    fixed hamburger menu test

--- dataland-frontend/tests/e2e/specs/quality-assurance/ProfileMenuItemByRoles.ts
+++ dataland-frontend/tests/e2e/specs/quality-assurance/ProfileMenuItemByRoles.ts
@@ -1,17 +1,17 @@
 import { login } from "@e2e/utils/Auth";
-import { reader_name, reader_pw, uploader_name, uploader_pw } from "@e2e/utils/Cypress";
+import { reviewer_name, reviewer_pw, uploader_name, uploader_pw } from "@e2e/utils/Cypress";
 
 describe("As a user, I expect the Quality Assurance link in the profile dropdown to be hidden or visible depending on my role", () => {
   const profileDropdownToggleSelector = "div[id='profile-picture-dropdown-toggle']";
   const qaAnchorSelector = "a[id='profile-picture-dropdown-qa-services-anchor']";
 
   it("Check wether the Quality Assurance link is visible when the user has the appropriate role", () => {
-    login(uploader_name, uploader_pw);
+    login(reviewer_name, reviewer_pw);
     cy.get(profileDropdownToggleSelector).click().wait(1000).get(qaAnchorSelector).should("exist").should("be.visible");
   });
 
   it("Check wether the Quality Assurance link is not visible when the user does not have the appropriate role", () => {
-    login(reader_name, reader_pw);
+    login(uploader_name, uploader_pw);
     cy.get(profileDropdownToggleSelector).click().wait(1000).get(qaAnchorSelector).should("not.exist");
   });
 });
