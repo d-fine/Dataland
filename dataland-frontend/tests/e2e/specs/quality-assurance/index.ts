f471249e7f902a5e8162d73435fae691cffacdfb
Florian Soffel <florian.soffel@d-fine.de>
2023-06-06 11:38:46 +0200

    moved e2e test to component test

--- dataland-frontend/tests/e2e/specs/quality-assurance/index.ts
+++ dataland-frontend/tests/e2e/specs/quality-assurance/index.ts
@@ -2,5 +2,5 @@
  * As a user, I want hamburger menu to behave as expected depending on user roles
  */
 describe("Quality Assurance tests", () => {
-  require("./ProfileMenuItemByRoles");
+  require("./ProfileMenuItemByRoles"); // TODO remove this and corresponding file and even this file if no e2e tests are added
 });
