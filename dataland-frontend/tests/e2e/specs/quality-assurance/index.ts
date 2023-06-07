b25b5d85da4e50ca7140b7a616ef50ffeae5bf83
Florian Soffel <florian.soffel@d-fine.de>
2023-06-07 16:13:28 +0200

    integrated new e2e tests

--- dataland-frontend/tests/e2e/specs/quality-assurance/index.ts
+++ dataland-frontend/tests/e2e/specs/quality-assurance/index.ts
@@ -2,5 +2,6 @@
  * As a user, I want hamburger menu to behave as expected depending on user roles
  */
 describe("Quality Assurance tests", () => {
-  // TODO this file must be assigned a test group or removed if no e2e tests are added
+  require("./DatasetPendingCheck")
+  require("./VerifyRejectedBadge")
 });
