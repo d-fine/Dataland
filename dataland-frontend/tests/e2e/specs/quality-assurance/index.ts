323731a7e93cc2feda4ff8dc24c2798251009918
Florian Soffel <florian.soffel@d-fine.de>
2023-06-12 15:10:53 +0200

    bugfix

--- dataland-frontend/tests/e2e/specs/quality-assurance/index.ts
+++ dataland-frontend/tests/e2e/specs/quality-assurance/index.ts
@@ -2,6 +2,6 @@
  * As a user, I want QA to work as expected
  */
 describe("Quality Assurance tests", () => {
-  require("./DatasetPendingCheck");
+  require("./CheckQaProcedure");
   require("./VerifyRejectedBadge");
 });
