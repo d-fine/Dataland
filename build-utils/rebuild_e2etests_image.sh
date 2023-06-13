be6c074f465a816c379b22495deeca826910db47
Florian Soffel <florian.soffel@d-fine.de>
2023-06-13 09:45:35 +0200

    removed dependency on frontend

--- build-utils/rebuild_e2etests_image.sh
+++ build-utils/rebuild_e2etests_image.sh
@@ -9,7 +9,7 @@ source ./*github_env.log
 set +o allexport
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_e2etests ./dataland-e2etests/Dockerfile \
-       ./dataland-e2etests/ ./dataland-frontend/ ./testing/ \
+       ./dataland-e2etests/ ./testing/ \
        ./dataland-backend/backendOpenApi.json  ./dataland-api-key-manager/apiKeyManagerOpenApi.json \
        ./dataland-document-manager/documentManagerOpenApi.json  ./dataland-qa-service/qaServiceOpenApi.json \
        ./build.gradle.kts ./gradle.properties ./settings.gradle.kts
