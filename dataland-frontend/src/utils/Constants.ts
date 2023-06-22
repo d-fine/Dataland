1fbae8d410a243d221c4edd199070bf8a4f905f4
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-06-22 18:31:47 +0200

    another todo

--- dataland-frontend/src/utils/Constants.ts
+++ dataland-frontend/src/utils/Constants.ts
@@ -5,18 +5,14 @@ import { DataTypeEnum } from "@clients/backend";
  */
 
 // ----------------------DATALAND SETTINGS----------------------
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-export const DATA_REQUEST_UPLOAD_MAX_FILE_SIZE_IN_BYTES = import.meta.env.VITE_DATA_REQUEST_UPLOAD_MAX_FILE_SIZE_IN_MEGABYTES * 1000000;
+export const DATA_REQUEST_UPLOAD_MAX_FILE_SIZE_IN_BYTES =
+  import.meta.env.VITE_DATA_REQUEST_UPLOAD_MAX_FILE_SIZE_IN_MEGABYTES * 1000000;
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-export const DOCUMENT_UPLOAD_MAX_FILE_SIZE_IN_BYTES = import.meta.env.VITE_DOCUMENT_UPLOAD_MAX_FILE_SIZE_IN_MEGABYTES * 1000000;
+export const DOCUMENT_UPLOAD_MAX_FILE_SIZE_IN_BYTES =
+  import.meta.env.VITE_DOCUMENT_UPLOAD_MAX_FILE_SIZE_IN_MEGABYTES * 1000000;
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-export const MAX_NUMBER_OF_DAYS_SELECTABLE_FOR_API_KEY_VALIDITY = import.meta.env.VITE_MAX_NUMBER_OF_DAYS_SELECTABLE_FOR_API_KEY_VALIDITY;
-
-
-
-
+export const MAX_NUMBER_OF_DAYS_SELECTABLE_FOR_API_KEY_VALIDITY = import.meta.env
+  .VITE_MAX_NUMBER_OF_DAYS_SELECTABLE_FOR_API_KEY_VALIDITY as number;
 
 // ----------------------FRONTEND SETTINGS ONLY----------------------
 
@@ -28,7 +24,6 @@ export const ARRAY_OF_FRAMEWORKS_WITH_VIEW_PAGE = Object.values(DataTypeEnum).fi
 export const ARRAY_OF_FRAMEWORKS_WITH_UPLOAD_FORM = Object.values(DataTypeEnum).filter(
   (frameworkName) => [DataTypeEnum.Sfdr as string, DataTypeEnum.Sme as string].indexOf(frameworkName) === -1
 ) as Array<DataTypeEnum>;
-
 
 // - Keycloak settings
 
@@ -45,11 +40,7 @@ export const TIME_DELAY_BETWEEN_SUBMIT_AND_NEXT_ACTION_IN_MS = 2000;
 export const TIME_BEFORE_REFRESH_TOKEN_EXPIRY_TO_DISPLAY_SESSION_WARNING_IN_MS = 2 * 60 * 1000;
 // => This always has to be shorter than the ssoSessionIdleTimeout value in the realm settings.
 
-
 // - Other settings
 export const UPLOAD_FILE_SIZE_DISPLAY_DECIMALS = 2;
 
 export const MS_PER_DAY = 24 * 60 * 60 * 1000;
-
-
-
