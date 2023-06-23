5d2e854d7369258d1986b6ad9922a4e5d196d837
Piotr Skonieczny <piotr.skonieczny@pwc.com>
2023-06-23 14:11:54 +0200

    Changed dataset upload to use API instead of fill form

--- dataland-frontend/tests/e2e/specs/eu-taxonomy-financials/EditButtonWithMultipleReportingPeriods.ts
+++ dataland-frontend/tests/e2e/specs/eu-taxonomy-financials/EditButtonWithMultipleReportingPeriods.ts
@@ -3,7 +3,7 @@ import { describeIf } from "@e2e/support/TestUtility";
 import { getKeycloakToken } from "@e2e/utils/Auth";
 import { generateDummyCompanyInformation, prepareUniqueCompany, uploadCompanyViaApi } from "@e2e/utils/CompanyUpload";
 import { admin_name, admin_pw } from "@e2e/utils/Cypress";
-import { addCreditInstitutionDataset, prepareFixture } from "@e2e/utils/EuTaxonomyFinancialsUpload";
+import { prepareFixture, uploadOneEuTaxonomyFinancialsDatasetViaApi } from "@e2e/utils/EuTaxonomyFinancialsUpload";
 import { FixtureData, getPreparedFixture } from "@sharedUtils/Fixtures";
 
 describeIf(
@@ -28,11 +28,24 @@ describeIf(
         return uploadCompanyViaApi(token, generateDummyCompanyInformation(testCompany.companyName)).then(
           (storedCompany): void => {
             cy.ensureLoggedIn(admin_name, admin_pw);
-            cy.visitAndCheckAppMount(
-              `/companies/${storedCompany.companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}/upload`
+
+            const datasetFor2022 = uploadOneEuTaxonomyFinancialsDatasetViaApi(
+              token,
+              storedCompany.companyId,
+              "2022",
+              testData.t
             );
-            addTwoDatasetsWithDifferentReportingPeriods(testData.t, storedCompany);
-            testEditDataButton(storedCompany);
+
+            const datasetFor2021 = uploadOneEuTaxonomyFinancialsDatasetViaApi(
+              token,
+              storedCompany.companyId,
+              "2021",
+              testData.t
+            );
+
+            Promise.all([datasetFor2022, datasetFor2021])
+              .then(() => testEditDataButton(storedCompany))
+              .catch(console.error);
           }
         );
       });
@@ -41,36 +54,11 @@ describeIf(
 );
 
 /**
- * Adds two new datasets with different reporting periods
- * @param data the data to fill the form with
- * @param storedCompany details of the company that was created
- */
-function addTwoDatasetsWithDifferentReportingPeriods(
-  data: EuTaxonomyDataForFinancials,
-  storedCompany: StoredCompany
-): void {
-  addCreditInstitutionDataset(data, "2022");
-
-  cy.visit("/companies").wait(4000);
-  cy.visitAndCheckAppMount(
-    `/companies/${storedCompany.companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}/upload`
-  );
-
-  addCreditInstitutionDataset(data, "2021");
-}
-
-/**
  * Tests that the item was added and is visible on the QA list
  * @param storedCompany details of the company that was created
  */
 function testEditDataButton(storedCompany: StoredCompany): void {
-  cy.visit("/companies").wait(4000);
-
-  cy.get('[data-test="search-result-framework-data"] .p-datatable-tbody')
-    .first()
-    .should("exist")
-    .should("contain", storedCompany.companyInformation.companyName)
-    .click();
+  cy.visitAndCheckAppMount(`/companies/${storedCompany.companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}`);
 
   cy.get('[data-test="editDatasetButton"').should("exist").click();
   cy.get('[data-test="select-reporting-period-dialog"')
