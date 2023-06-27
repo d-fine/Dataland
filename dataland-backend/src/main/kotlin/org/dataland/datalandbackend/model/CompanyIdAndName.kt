2b3d3d602b21c77ca7557650aa193396fe57ad37
Florian Soffel <florian.soffel@d-fine.de>
2023-06-27 12:42:21 +0200

    made properties required

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/CompanyIdAndName.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/CompanyIdAndName.kt
@@ -1,10 +1,14 @@
 package org.dataland.datalandbackend.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 /**
  * --- API model ---
  * Class containing only a company's id and name
  */
 interface CompanyIdAndName {
+    @get:JsonProperty(required = true)
     val companyId: String
+    @get:JsonProperty(required = true)
     val companyName: String
 }
