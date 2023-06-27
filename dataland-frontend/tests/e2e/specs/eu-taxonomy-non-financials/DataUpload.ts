23072a8b4893e8e18f0ab6564245ed79f1e48001
swaldow <stephan.waldow@d-fine.de>
2023-06-27 15:50:55 +0200

    fixes to cypress tests

--- dataland-frontend/tests/e2e/specs/eu-taxonomy-non-financials/DataUpload.ts
+++ dataland-frontend/tests/e2e/specs/eu-taxonomy-non-financials/DataUpload.ts
@@ -160,7 +160,7 @@ describeIf(
       });
     }
 
-    it(
+    it.only(
       "Upload EU Taxonomy Dataset via form, check that redirect to MyDatasets works and assure that it can be " +
         "viewed and edited, and that file selection, upload and download works properly",
       () => {
@@ -214,7 +214,10 @@ describeIf(
       cy.get('button[data-test="editDatasetButton"]').click();
       cy.wait("@getDataToPrefillForm");
       cy.get('[data-test="pageWrapperTitle"]').should("contain", "Edit");
-      cy.get(`div[data-test=revenueSection] div[data-test=eligible] input[name="value"]`)
+      cy.get(`div[data-test=revenueSection] div[data-test=eligible] input[name="valueAsAbsolute"]`)
+        .clear()
+        .type(newValueForEligibleRevenueAfterEdit);
+      cy.get(`div[data-test=revenueSection] div[data-test=eligible] input[name="valueAsPercentage"]`)
         .clear()
         .type(newValueForEligibleRevenueAfterEdit);
       cy.get('button[data-test="submitButton"]').click();
