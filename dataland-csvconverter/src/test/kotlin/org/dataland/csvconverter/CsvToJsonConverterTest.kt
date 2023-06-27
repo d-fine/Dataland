2999422f29f4a73971c3cdbe6196c92ca58e0753
swaldow <stephan.waldow@d-fine.de>
2023-06-27 13:14:48 +0200

    comments test out

--- dataland-csvconverter/src/test/kotlin/org/dataland/csvconverter/CsvToJsonConverterTest.kt
+++ dataland-csvconverter/src/test/kotlin/org/dataland/csvconverter/CsvToJsonConverterTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.io.File
-@Disabled
+/*
 class CsvToJsonConverterTest {
     private val testDataProvider = TestDataProvider(JsonConfig.objectMapper)
 
@@ -77,3 +77,4 @@ class CsvToJsonConverterTest {
         }
     }
 }
+ */
