d1568d71fe41134ec10516feabb308dc48cfe05b
Piotr Skonieczny <piotr.skonieczny@pwc.com>
2023-06-28 17:35:40 +0200

    Fixed QA Test

--- dataland-frontend/tests/e2e/specs/quality-assurance/CheckQaProcedure.ts
+++ dataland-frontend/tests/e2e/specs/quality-assurance/CheckQaProcedure.ts
@@ -1,8 +1,8 @@
-import { EuTaxonomyDataForFinancials, LksgData, StoredCompany } from "@clients/backend";
+import { DataMetaInformation, EuTaxonomyDataForFinancials, LksgData, StoredCompany } from "@clients/backend";
 import { describeIf } from "@e2e/support/TestUtility";
 import { getKeycloakToken, login } from "@e2e/utils/Auth";
 import { generateDummyCompanyInformation, uploadCompanyViaApi } from "@e2e/utils/CompanyUpload";
-import { getBaseUrl, reviewer_name, reviewer_pw, uploader_name, uploader_pw } from "@e2e/utils/Cypress";
+import { reviewer_name, reviewer_pw, uploader_name, uploader_pw } from "@e2e/utils/Cypress";
 import { uploadOneEuTaxonomyFinancialsDatasetViaApi } from "@e2e/utils/EuTaxonomyFinancialsUpload";
 import { uploadOneLksgDatasetViaApi } from "@e2e/utils/LksgUpload";
 import { FixtureData, getPreparedFixture } from "@sharedUtils/Fixtures";
@@ -19,6 +19,10 @@ describeIf(
     let preparedLksgFixtures: Array<FixtureData<LksgData>>;
 
     before(function () {
+      cy.fixture("CompanyInformationWithEuTaxonomyDataForFinancialsPreparedFixtures").then(function (jsonContent) {
+        preparedEuTaxonomyFixtures = jsonContent as Array<FixtureData<EuTaxonomyDataForFinancials>>;
+      });
+
       cy.fixture("CompanyInformationWithLksgPreparedFixtures").then(function (jsonContent) {
         preparedLksgFixtures = jsonContent as Array<FixtureData<LksgData>>;
       });
@@ -29,12 +33,10 @@ describeIf(
       });
     });
 
-    beforeEach(() => cy.ensureLoggedIn(uploader_name, uploader_pw));
-    afterEach(() => safeLogout());
-
     it("Check whether newly added dataset has Pending status and can be approved by a reviewer", () => {
       const data = getPreparedFixture("company-for-all-types", preparedEuTaxonomyFixtures);
       getKeycloakToken(uploader_name, uploader_pw).then(async (token: string) => {
+        cy.ensureLoggedIn(uploader_name, uploader_pw);
         await uploadOneEuTaxonomyFinancialsDatasetViaApi(token, storedCompany.companyId, "2022", data.t, false);
         testSubmittedDatasetIsInReviewListAndPending(storedCompany.companyInformation.companyName);
       });
@@ -43,16 +45,9 @@ describeIf(
     it("Check whether newly added dataset has Rejected status and can be edited", () => {
       const data = getPreparedFixture("LkSG-date-2023-04-18", preparedLksgFixtures);
       getKeycloakToken(uploader_name, uploader_pw).then(async (token: string) => {
+        cy.ensureLoggedIn(uploader_name, uploader_pw);
         const lksgDataset = await uploadOneLksgDatasetViaApi(token, storedCompany.companyId, "2022", data.t, false);
-        testSubmittedDatasetIsInReviewListAndRejected(storedCompany.companyInformation.companyName);
-
-        cy.get('[data-test="editDatasetButton"').should("exist").click();
-
-        cy.url().should(
-          "eq",
-          getBaseUrl() +
-            `/companies/${storedCompany.companyId}/frameworks/lksg/upload?templateDataId=${lksgDataset.dataId}`
-        );
+        testSubmittedDatasetIsInReviewListAndRejected(storedCompany, lksgDataset);
       });
     });
   }
@@ -66,7 +61,6 @@ function testSubmittedDatasetIsInReviewListAndPending(companyName: string): void
   testDatasetPresentWithCorrectStatus(companyName, "PENDING");
 
   safeLogout();
-
   login(reviewer_name, reviewer_pw);
 
   cy.visitAndCheckAppMount("/qualityassurance");
@@ -91,14 +85,20 @@ function testSubmittedDatasetIsInReviewListAndPending(companyName: string): void
 
 /**
  * Tests that the item was added and is visible on the QA list
- * @param companyName The name of the company
+ * @param storedCompany the stored company uploading the dataset
+ * @param dataset the data meta information that wa suploaded
  */
-function testSubmittedDatasetIsInReviewListAndRejected(companyName: string): void {
-  safeLogout();
-
+function testSubmittedDatasetIsInReviewListAndRejected(
+  storedCompany: StoredCompany,
+  dataset: DataMetaInformation
+): void {
   login(reviewer_name, reviewer_pw);
-
   cy.visitAndCheckAppMount("/qualityassurance");
+
+  cy.intercept(`**/api/metadata/${dataset.dataId}`).as("getMetadata");
+  cy.intercept(`**/api/companies/${storedCompany.companyId}`).as("getCompanyInformation");
+
+  cy.wait("@getMetadata").wait("@getCompanyInformation");
 
   cy.get('[data-test="qa-review-section"] .p-datatable-tbody').last().click();
   cy.get(".p-dialog").get('button[id="reject-button"]').should("exist").click();
@@ -106,7 +106,11 @@ function testSubmittedDatasetIsInReviewListAndRejected(companyName: string): voi
   safeLogout();
   login(uploader_name, uploader_pw);
 
-  testDatasetPresentWithCorrectStatus(companyName, "REJECTED");
+  testDatasetPresentWithCorrectStatus(storedCompany.companyInformation.companyName, "REJECTED");
+
+  cy.visitAndCheckAppMount(`/companies/${storedCompany.companyId}/frameworks/lksg/${dataset.dataId}`)
+    .get('[data-test="editDatasetButton"')
+    .should("exist");
 }
 
 /**
@@ -130,9 +134,10 @@ function testDatasetPresentWithCorrectStatus(companyName: string, status: string
  * Logs the user out without testing the url
  */
 function safeLogout(): void {
-  cy.visitAndCheckAppMount("/companies");
-  cy.get('div[id="profile-picture-dropdown-toggle"]')
+  cy.visitAndCheckAppMount("/")
+    .wait(1000)
+    .get("div[id='profile-picture-dropdown-toggle']")
     .click()
-    .get('a[id="profile-picture-dropdown-logout-anchor"]')
+    .get("a[id='profile-picture-dropdown-logout-anchor']")
     .click();
 }
