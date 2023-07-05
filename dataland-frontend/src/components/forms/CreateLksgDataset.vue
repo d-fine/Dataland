e68c4535084709e82d56d9d1f80777e7e00c91d2
Florian Soffel <florian.soffel@d-fine.de>
2023-07-05 09:46:28 +0200

    bugfix

--- dataland-frontend/src/components/forms/CreateLksgDataset.vue
+++ dataland-frontend/src/components/forms/CreateLksgDataset.vue
@@ -6,7 +6,7 @@
         <p class="font-medium text-xl">Loading LkSG data...</p>
         <em class="pi pi-spinner pi-spin" aria-hidden="true" style="z-index: 20; color: #e67f3f" />
       </div>
-      <div v-else class="grid uploadFormWrapper">
+      <div class="grid uploadFormWrapper">
         <div id="uploadForm" class="text-left uploadForm col-9">
           <FormKit
             v-model="companyAssociatedLksgData"
