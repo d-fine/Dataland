1f20cdd6449682980ff710fa791c7c75f581a60f
Florian Soffel <florian.soffel@d-fine.de>
2023-07-06 15:32:57 +0200

    fix one test

--- dataland-frontend/src/components/forms/parts/fields/MultiSelectFormField.vue
+++ dataland-frontend/src/components/forms/parts/fields/MultiSelectFormField.vue
@@ -1,15 +1,18 @@
 <template>
-  <UploadFormHeader :label="label" :description="description" :is-required="required" />
-  <MultiSelectFormElement
-    :name="name"
-    :validation="validation"
-    :validation-label="validationLabel ?? label"
-    :placeholder="placeholder"
-    :options="options"
-    :inner-class="innerClass"
-    :optionLabel="optionLabel"
-    @selectedValuesChanged="selectedValuesChanged"
-  />
+  <div class="form-field" :data-test="name">
+    <UploadFormHeader :label="label" :description="description" :is-required="required" />
+    <MultiSelectFormElement
+      :name="name"
+      :validation="validation"
+      :validation-value="validationValue"
+      :validation-label="validationLabel"
+      :placeholder="placeholder"
+      :options="options"
+      :inner-class="innerClass"
+      :optionLabel="optionLabel"
+      @selectedValuesChanged="selectedValuesChanged"
+    />
+  </div>
 </template>
 
 <script lang="ts">
@@ -27,6 +30,11 @@ export default defineComponent({
     ...DropdownOptionFormFieldProps,
     optionLabel: {
       type: String,
+      default: "label",
+    },
+    optionValue: {
+      type: String,
+      default: "value",
     },
   },
   methods: {
