04a808c29428274c05a9d3cbaa7b73edc75244b2
Florian Soffel <florian.soffel@d-fine.de>
2023-07-10 09:08:06 +0200

    Added index creation script

---
+++ dataland-backend/src/main/kotlin/db/migration/V2__GenerateCompanyNameIdentifierIndex.kt
@@ -0,0 +1,18 @@
+package db.migration
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+class V2__GenerateCompanyNameIdentifierIndex : BaseJavaMigration() {
+    override fun migrate(context: Context?) {
+        context!!.connection.createStatement().execute(
+            "CREATE EXTENSION IF NOT EXISTS pg_trgm;\n" +
+                "CREATE INDEX IF NOT EXISTS idx_company_identifier ON company_identifiers USING gin (identifier_value gin_trgm_ops);\n" +
+                "CREATE INDEX IF NOT EXISTS idx_company_name ON stored_companies USING gin (company_name gin_trgm_ops);\n" +
+                "CREATE INDEX IF NOT EXISTS idx_company_alt_name ON stored_company_entity_company_alternative_names USING gin (company_alternative_names gin_trgm_ops);\n" +
+                "CREATE INDEX IF NOT EXISTS idx_company_id_alt_name ON stored_company_entity_company_alternative_names(stored_company_entity_company_id);\n" +
+                "CREATE INDEX IF NOT EXISTS idx_company_id_identifiers ON company_identifiers(company_id);\n" +
+                "CREATE INDEX IF NOT EXISTS idx_company_id ON stored_companies(company_id);\n",
+        )
+    }
+}
