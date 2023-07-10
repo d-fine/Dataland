6e6f32fa2d3138d02c939713130a81c085fd3523
swaldow <stephan.waldow@d-fine.de>
2023-07-10 18:06:24 +0200

    adds a refactoring todo

--- dataland-frontend/src/components/resources/frameworkDataSearch/p2p/P2pPanel.vue
+++ dataland-frontend/src/components/resources/frameworkDataSearch/p2p/P2pPanel.vue
@@ -23,7 +23,8 @@
       >
       </Column>
     </DataTable>
-    <div v-for="(arrayOfKpiDataObject, index) in mapOfKpiKeysToDataObjectsArrays" :key="index" style="font-size: 16px; text-align: left;"> //TODO: @click einfügen und probieren oder link auf innere a umgebung für virtuelles klicken
+    <div v-for="(arrayOfKpiDataObject, index) in mapOfKpiKeysToDataObjectsArrays" :key="index" style="font-size: 16px; text-align: left;">
+        <!-- //TODO: @click einfügen und probieren oder link auf innere a umgebung für virtuelles klicken -->
       <div v-if="shouldCategoryBeRendered(arrayOfKpiDataObject[0])">
         <div :class="`p-badge badge-${colorOfCategory(arrayOfKpiDataObject[0])}`">
           <a @click="toggleExpansion(index)">{{ arrayOfKpiDataObject[0].toUpperCase() }}</a>
