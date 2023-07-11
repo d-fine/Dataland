ff15520e888fb43e678a2268347ad20429fa2d70
Florian Soffel <florian.soffel@d-fine.de>
2023-07-11 12:29:06 +0200

    TEST to be reverted

--- testing/e2e_test_launcher.sh
+++ testing/e2e_test_launcher.sh
@@ -1,3 +1,8 @@
+
+echo "TEST_STH" $TEST_STH
+echo "TEST_STH_ELSE" $TEST_STH_ELSE
+echo "TEST_STH_ELSEE" $TEST_STH_ELSEE
+
 set -ex
 if [[ $TEST_EXECUTOR = "CYPRESS" ]]; then
   ./testing/execute_e2e_tests.sh
