9536a1b61d9e5cc47315475ff725ee664e2cc457
Florian Soffel <florian.soffel@d-fine.de>
2023-07-11 16:57:45 +0200

    debugging

--- dataland-internal-storage/src/main/kotlin/db/migration/V2__MigrateEuTaxonomyNonFinancialsWithAbsoluteValues.kt
+++ dataland-internal-storage/src/main/kotlin/db/migration/V2__MigrateEuTaxonomyNonFinancialsWithAbsoluteValues.kt
@@ -8,35 +8,43 @@ import org.json.JSONObject
 import java.math.BigDecimal
 
 class V2__MigrateEuTaxonomyNonFinancialsWithAbsoluteValues : BaseJavaMigration() {
+    private val cashFlowTypes = listOf("capex", "opex", "revenue")
+    private val fieldsToMigrate = mapOf("alignedPercentage" to "alignedData", "eligiblePercentage" to "eligibleData")
+
     override fun migrate(context: Context?) {
         val objectMapper = ObjectMapper()
         val getQueryResultSet = context!!.connection.createStatement().executeQuery("SELECT data from data_items WHERE data LIKE '%\\\\\\\"dataType\\\\\\\":\\\\\\\"eutaxonomy-non-financials\\\\\\\"%'")
         val companyAssociatedDataSets = mutableListOf<JSONObject>()
-        while(getQueryResultSet.next()) {
-            companyAssociatedDataSets.add(JSONObject(
-                objectMapper.readValue(
-                    getQueryResultSet.getString("data"), String::class.java
-                )
-            ))
+        while (getQueryResultSet.next()) {
+            companyAssociatedDataSets.add(
+                JSONObject(
+                    objectMapper.readValue(
+                        getQueryResultSet.getString("data"), String::class.java,
+                    ),
+                ),
+            )
         }
         companyAssociatedDataSets.filter { it.getString("dataType") == DataTypeEnum.eutaxonomyMinusNonMinusFinancials.value }
-        val cashflowTypes = listOf("capex", "opex", "revenue")
-        val fieldsToMigrate = mapOf("alignedPercentage" to "alignedData", "eligiblePercentage" to "eligibleData")
         companyAssociatedDataSets.forEach {
             println("Migrating dataset")
-            val data = JSONObject(it.getString("data"))
-            println("OLD ${data.toString(2)}")
-            cashflowTypes.forEach { cashflowType ->
-                val cashFlow = (data.opt(cashflowType) ?: return@forEach) as JSONObject
-                fieldsToMigrate.keys.forEach { fieldToMigrate ->
-                    val dataToMigrate = (cashFlow.opt(fieldToMigrate)?.run { if (it == JSONObject.NULL) null else it } ?: return@forEach) as JSONObject
-                    dataToMigrate.append("valueAsPercentage", cashFlow.opt("value") as BigDecimal?)
-                    dataToMigrate.remove("value")
-                    cashFlow.remove(fieldToMigrate)
-                    cashFlow.append(fieldsToMigrate[fieldToMigrate]!!, dataToMigrate)
-                }
-            }
-            println("NEW ${data.toString(2)}")
+            migrateDataset(it.getString("data"))
         }
+    }
+
+    private fun migrateDataset(datasetString: String): String {
+        val dataset = JSONObject(datasetString)
+        println("OLD $dataset")
+        cashFlowTypes.forEach { cashflowType ->
+            val cashFlow = (dataset.opt(cashflowType) ?: return@forEach) as JSONObject
+            fieldsToMigrate.keys.forEach { fieldToMigrate ->
+                val dataToMigrate = (cashFlow.opt(fieldToMigrate)?.let { if (it == JSONObject.NULL) null else it } ?: return@forEach) as JSONObject
+                dataToMigrate.put("valueAsPercentage", dataToMigrate.opt("value") as BigDecimal?)
+                dataToMigrate.remove("value")
+                cashFlow.put(fieldsToMigrate[fieldToMigrate]!!, dataToMigrate)
+                cashFlow.remove(fieldToMigrate)
+            }
+        }
+        println("NEW $dataset")
+        return dataset.toString()
     }
 }
