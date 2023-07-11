2197511eafaeba206e6b8624c025888ae4c7cbf5
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-07-11 15:25:56 +0200

    WIP added dummy buttons as arrows on the right side with spans

--- dataland-frontend/src/components/resources/frameworkDataSearch/p2p/P2pPanel.vue
+++ dataland-frontend/src/components/resources/frameworkDataSearch/p2p/P2pPanel.vue
@@ -20,17 +20,14 @@
     <div v-for="(arrayOfKpiDataObject, index) in mapOfKpiKeysToDataObjectsArrays" :key="index" class="d-table-style">
       <div v-if="shouldCategoryBeRendered(arrayOfKpiDataObject[0])">
         <!--//TODO fix the height of the category row -->
-          <div class="w-full d-dataset-toggle" @click="toggleExpansion(index)">
-        <div :class="`p-badge badge-${colorOfCategory(arrayOfKpiDataObject[0])}`" >
-          <a >{{ arrayOfKpiDataObject[0].toUpperCase() }}</a>
-        </div>
-            <div  v-if="!isExpanded(index)">
-              <button class="p-row-toggler p-link" type="button" style="text-align:left"> asdf </button>
-            </div>
-            <div  v-if="isExpanded(index)">
-              <button class="p-row-toggler p-link" type="button" style="text-align:left"> qewrtz </button>
-            </div>
+        <div class="w-full d-dataset-toggle" @click="toggleExpansion(index)">
+          <div :class="`p-badge badge-${colorOfCategory(arrayOfKpiDataObject[0])}`">
+            <span>{{ arrayOfKpiDataObject[0].toUpperCase() }}</span>
           </div>
+          <div style="text-align:right">
+            <span >Tsdsdsd</span>
+          </div>
+        </div>
         <div v-show="isExpanded(index)">
           <P2pCompanyDataTable
             :arrayOfKpiDataObjects="arrayOfKpiDataObject[1]"
