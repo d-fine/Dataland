e3c77c53b0a30dc556e3d64c03764fec19a263ee
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-07-12 11:34:02 +0200

    WIP height of category creates a scrolling problem when cateogries are extended.

--- dataland-frontend/src/components/resources/frameworkDataSearch/p2p/P2pPanel.vue
+++ dataland-frontend/src/components/resources/frameworkDataSearch/p2p/P2pPanel.vue
@@ -18,15 +18,15 @@
       </Column>
     </DataTable>
     <div v-for="(arrayOfKpiDataObject, index) in mapOfKpiKeysToDataObjectsArrays" :key="index" class="d-table-style">
-      <div v-if="shouldCategoryBeRendered(arrayOfKpiDataObject[0])">
+      <div v-if="shouldCategoryBeRendered(arrayOfKpiDataObject[0])" style="height: 40px">
         <!--//TODO fix the height of the category row -->
-        <div class="w-full d-cursor-pointer" @click="toggleExpansion(index)">
-          <div v-if="!isExpanded(index)" class="d-category">
-            <span :class="`p-badge badge-${colorOfCategory(arrayOfKpiDataObject[0])}`" style="display: inline-block; vertical-align: middle">{{
+        <div class="pt-1 w-full d-cursor-pointer" @click="toggleExpansion(index)">
+          <div v-if="!isExpanded(index)">
+            <span :class="`p-badge badge-${colorOfCategory(arrayOfKpiDataObject[0])}`">{{
               arrayOfKpiDataObject[0].toUpperCase()
             }}</span>
-            <button class="pt-2 pr-3 d-cursor-pointer d-chevron-style">
-             <span class="pr-1 pi pi-chevron-right d-chevron-font"></span>
+            <button class="pt-1 pr-3 d-cursor-pointer d-chevron-style">
+             <span class="pr-1 pt-1 pi pi-chevron-right d-chevron-font"></span>
             </button>
           </div>
           <div v-if="isExpanded(index)">
