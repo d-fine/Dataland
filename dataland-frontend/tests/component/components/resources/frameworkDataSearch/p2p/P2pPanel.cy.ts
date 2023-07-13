72c17af77297f1bec5900d6a45e7e748a959a613
swaldow <stephan.waldow@d-fine.de>
2023-07-13 15:23:50 +0200

    adds data test id

--- dataland-frontend/tests/component/components/resources/frameworkDataSearch/p2p/P2pPanel.cy.ts
+++ dataland-frontend/tests/component/components/resources/frameworkDataSearch/p2p/P2pPanel.cy.ts
@@ -81,7 +81,8 @@ describe("Component test for P2pPanel", () => {
         };
       },
     });
-
+    cy.get(`span[data-test=General]`).click();
+    cy.get(`span[data-test=_general]`).click();
     cy.get(`span.p-column-title`).should("contain.text", p2pData.general.general.dataDate.substring(0, 4));
     cy.get("tbody").find(`span:contains(${p2pData.general.general.dataDate})`).should("exist");
 
@@ -154,6 +155,8 @@ describe("Component test for P2pPanel", () => {
         };
       },
     });
+    cy.get(`span[data-test=General]`).click();
+    cy.get(`span[data-test=_general]`).click();
     cy.get("table").find(`tr:contains("Data Date")`).find(`span`).eq(6).contains("2023");
 
     for (let indexOfColumn = 1; indexOfColumn <= 6; indexOfColumn++) {
