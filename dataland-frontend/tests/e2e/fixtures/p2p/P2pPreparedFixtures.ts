b1d3b041b3df4084ac77cf1394929de268150dac
swaldow <stephan.waldow@d-fine.de>
2023-07-13 15:25:04 +0200

    modifies prepared fakefixtures

--- dataland-frontend/tests/e2e/fixtures/p2p/P2pPreparedFixtures.ts
+++ dataland-frontend/tests/e2e/fixtures/p2p/P2pPreparedFixtures.ts
@@ -1,6 +1,7 @@
 import { FixtureData } from "@sharedUtils/Fixtures";
 import { PathwaysToParisData } from "@clients/backend";
 import { generateP2pFixture } from "./P2pDataFixtures";
+import {generateLksgFixture} from "@e2e/fixtures/lksg/LksgDataFixtures";
 
 /**
  * Generates LkSG prepared fixtures by generating random LkSG datasets and afterwards manipulating some fields
@@ -13,6 +14,7 @@ export function generateP2pPreparedFixtures(): Array<FixtureData<PathwaysToParis
   preparedFixtures.push(manipulateFixtureForDate(generateP2pFixture(1)[0], "2023-04-18"));
   preparedFixtures.push(manipulateFixtureForDate(generateP2pFixture(1)[0], "2023-06-22"));
   preparedFixtures.push(manipulateFixtureForDate(generateP2pFixture(1)[0], "2022-07-30"));
+  preparedFixtures.push(manipulateFixtureForOneP2pDataSetWithProductionSites(generateP2pFixture(1)[0]));
   return preparedFixtures;
 }
 
@@ -45,5 +47,18 @@ function manipulateFixtureForDate(
   input.companyInformation.companyName = "P2p-date-" + date;
   input.t.general.general.dataDate = date;
   input.reportingPeriod = date.split("-")[0];
+  return input;
+}
+
+/**
+ * Sets the company name in the fixture data to a specific string, the field "employeeUnder18Apprentices" to "No", and
+ * sets exactly two production sites for the "listOfProductionSites" field.
+ * @param input Fixture data to be manipulated
+ * @returns the manipulated fixture data
+ */
+function manipulateFixtureForOneP2pDataSetWithProductionSites(input: FixtureData<PathwaysToParisData>): FixtureData<PathwaysToParisData> {
+  input.companyInformation.companyName = "one-lksg-data-set-with-two-production-sites";
+  input.t.general.general.dataDate = "2022-01-01";
+  input.t.general.general.sector = ["Ammonia", "Cement"];
   return input;
 }
