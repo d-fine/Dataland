ae0f87dbea355f056660f14c83a159948df76c47
Florian Soffel <florian.soffel@d-fine.de>
2023-07-13 12:24:47 +0200

    doing it the fly way

--- dataland-backend/src/main/kotlin/db/migration/V2__GenerateCompanyNameIdentifierIndex.kt
+++ dataland-backend/src/main/kotlin/db/migration/V2__GenerateCompanyNameIdentifierIndex.kt
@@ -9,21 +9,21 @@ import org.flywaydb.core.api.migration.Context
 class V2__GenerateCompanyNameIdentifierIndex : BaseJavaMigration() {
     override fun migrate(context: Context?) {
         context!!.connection.createStatement().execute(
-            "CREATE EXTENSION IF NOT EXISTS pg_trgm; " +
-                "CREATE INDEX IF NOT EXISTS idx_company_identifier " +
+            "CREATE EXTENSION pg_trgm; " +
+                "CREATE INDEX idx_company_identifier " +
                 "ON company_identifiers " +
                 "USING gin (identifier_value gin_trgm_ops); " +
-                "CREATE INDEX IF NOT EXISTS idx_company_name " +
+                "CREATE INDEX idx_company_name " +
                 "ON stored_companies " +
                 "USING gin (company_name gin_trgm_ops); " +
-                "CREATE INDEX IF NOT EXISTS idx_company_alt_name " +
+                "CREATE INDEX idx_company_alt_name " +
                 "ON stored_company_entity_company_alternative_names " +
                 "USING gin (company_alternative_names gin_trgm_ops); " +
-                "CREATE INDEX IF NOT EXISTS idx_company_id_alt_name " +
+                "CREATE INDEX idx_company_id_alt_name " +
                 "ON stored_company_entity_company_alternative_names(stored_company_entity_company_id); " +
-                "CREATE INDEX IF NOT EXISTS idx_company_id_identifiers " +
+                "CREATE INDEX idx_company_id_identifiers " +
                 "ON company_identifiers(company_id); " +
-                "CREATE INDEX IF NOT EXISTS idx_company_id " +
+                "CREATE INDEX idx_company_id " +
                 "ON stored_companies(company_id);",
         )
     }
