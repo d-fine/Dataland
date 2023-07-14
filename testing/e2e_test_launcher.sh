32b5007d8afc45c9b4837bc3ee4f86c0f3cc5c61
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-07-14 14:47:33 +0200

    expirment to try to install docker-compose 2.19.0 on the runner before e2e tests are run

--- testing/e2e_test_launcher.sh
+++ testing/e2e_test_launcher.sh
@@ -1,4 +1,10 @@
 set -ex
+
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.19.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
+
 if [[ $TEST_EXECUTOR = "CYPRESS" ]]; then
   ./testing/execute_e2e_tests.sh
 elif [[ $TEST_EXECUTOR = "E2ETESTS" ]]; then
