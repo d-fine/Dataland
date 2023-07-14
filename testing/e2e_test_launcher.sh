887d92ddf02ee74b81173c814c310d75f4ed733f
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-07-14 14:51:48 +0200

    even more logs

--- testing/e2e_test_launcher.sh
+++ testing/e2e_test_launcher.sh
@@ -1,12 +1,13 @@
 set -ex
 
 echo "Current docker compose version:"
+docker compose version
 which docker-compose
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.19.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-docker compose version
 echo "Now, current docker compose version:"
+docker compose version
 which docker-compose
 
 if [[ $TEST_EXECUTOR = "CYPRESS" ]]; then
