3f419dc4872369461c5d0b0c0b63324e0b118504
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-07-14 14:59:24 +0200

    delete logs, write comment

--- testing/e2e_test_launcher.sh
+++ testing/e2e_test_launcher.sh
@@ -1,14 +1,9 @@
 set -ex
 
-echo "Current docker compose version:"
-docker compose version
-which docker-compose
+#Install docker-compose version 2.19.0, because the version 2.19.1 from the github runner leads to network errors
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.19.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-echo "Now, current docker compose version:"
-docker compose version
-which docker-compose
 
 if [[ $TEST_EXECUTOR = "CYPRESS" ]]; then
   ./testing/execute_e2e_tests.sh
