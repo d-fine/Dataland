7573180f5789c71f31d3e0f5494bcd4745702ac6
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-07-17 16:11:09 +0200

    refactoring of method name

--- dataland-frontend/src/components/resources/frameworkDataSearch/p2p/P2pPanel.vue
+++ dataland-frontend/src/components/resources/frameworkDataSearch/p2p/P2pPanel.vue
@@ -182,7 +182,7 @@ export default defineComponent({
             if (categoryObject == null) continue;
             const listOfDataObjects: Array<KpiDataObject> = [];
             const categoryResult = assertDefined(p2pDataModel.find((category) => category.name === categoryKey));
-            this.iteratesThroughSubcategories(
+            this.iterateThroughSubcategories(
               categoryObject,
               categoryKey,
               categoryResult,
@@ -206,7 +206,7 @@ export default defineComponent({
      * @param dataIdOfP2pDataset  the data ID of the P2P dataset
      * @param listOfDataObjects a map containing the category and it's corresponding Kpis
      */
-    iteratesThroughSubcategories(
+    iterateThroughSubcategories(
       categoryObject,
       categoryKey,
       categoryResult: Category,
