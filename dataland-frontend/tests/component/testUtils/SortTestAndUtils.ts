f8972b6b74be8e05829129931a9e474b3b434d68
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-07-18 12:12:44 +0200

    outsorced body of test function to utils file

---
+++ dataland-frontend/tests/component/testUtils/SortTestAndUtils.ts
@@ -0,0 +1,53 @@
+import {
+    ReportingPeriodOfDataSetWithId,
+    sortReportingPeriodsToDisplayAsColumns
+} from "@/utils/DataTableDisplay";
+
+/**
+ * Calls the testfunction for sorting and swaps the list entries if necessary.
+ * @param  listOfDataDateToDisplayAsColumns list of objects to sort
+ * @param shouldSwap toogles the swap of both list elements in listOfDataDateToDisplayAsColumns (in case there are two.
+ * Shortens the test-function and avoids code duplications.
+ * @returns sorted list
+ */
+export function swapAndSortReportingPeriodsToDisplayAsColumns(
+    listOfDataDateToDisplayAsColumns: ReportingPeriodOfDataSetWithId[],
+    shouldSwap = false
+): ReportingPeriodOfDataSetWithId[] {
+    let swappedList: ReportingPeriodOfDataSetWithId[];
+    if (shouldSwap && listOfDataDateToDisplayAsColumns.length == 2) {
+        swappedList = listOfDataDateToDisplayAsColumns.slice();
+        swappedList[0] = listOfDataDateToDisplayAsColumns[1];
+        swappedList[1] = listOfDataDateToDisplayAsColumns[0];
+        listOfDataDateToDisplayAsColumns = swappedList.slice();
+    }
+    return sortReportingPeriodsToDisplayAsColumns(listOfDataDateToDisplayAsColumns);
+}
+
+/**
+ * @param firstYearObject first object used in sorting test
+ * @param secondYearObject second object used in sorting test
+ * @param firstOtherObject third object used in sorting test
+ * @param firstOtherObject fourth object used in sorting test
+ * @param shouldSwapList list of booleans: gives the instructions in the for loop to swap the inputs (saves lines of code).
+ */
+export function sortReportingPeriodsToDisplayAsColumnsTest(
+    firstYearObject : { dataId: string, reportingPeriod: string},
+    secondYearObject : { dataId: string, reportingPeriod: string},
+    firstOtherObject : { dataId: string, reportingPeriod: string},
+    secondOtherObject : { dataId: string, reportingPeriod: string},
+    shouldSwapList: boolean[]
+    ) : void  {
+    for (let i = 0; i < 2; i++) {
+        expect(
+            swapAndSortReportingPeriodsToDisplayAsColumns([secondYearObject, firstYearObject], shouldSwapList[i])
+        ).to.deep.equal([firstYearObject, secondYearObject]);
+
+        expect(
+            swapAndSortReportingPeriodsToDisplayAsColumns([secondOtherObject, firstOtherObject], shouldSwapList[i])
+        ).to.deep.equal([firstOtherObject, secondOtherObject]);
+    }
+    expect(
+        sortReportingPeriodsToDisplayAsColumns([firstYearObject, secondOtherObject, firstOtherObject])
+    ).to.deep.equal([firstYearObject, firstOtherObject, secondOtherObject]);
+}
