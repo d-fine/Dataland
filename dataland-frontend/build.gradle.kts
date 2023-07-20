8c19d6138990bbd43e835f6802f1e69141957fed
Aliabdoli77 <aliabdoli77@yahoo.com>
2023-07-20 17:21:07 +0200

    fixes typo

--- dataland-frontend/build.gradle.kts
+++ dataland-frontend/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 node {
     download.set(true)
-    version.set("20.4.1")
+    version.set("20.4.0")
 }
 
 val backendOpenApiFile = "${project.rootDir}/dataland-backend/backendOpenApi.json"
