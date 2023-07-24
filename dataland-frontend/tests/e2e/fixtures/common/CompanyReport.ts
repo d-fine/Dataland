624e5e28503668d1a1deb171a93fcbf8084b900a
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-07-24 13:13:46 +0200

    WIP added company reports fixture

---
+++ dataland-frontend/tests/e2e/fixtures/common/CompanyReport.ts
@@ -0,0 +1,7 @@
+import { faker } from "@faker-js/faker";
+import {CompanyReport} from "@clients/backend";
+
+
+export function randomCompanyReport(): CompanyReport {
+    return faker.helpers.arrayElement(Object.values(CompanyReport));
+}
