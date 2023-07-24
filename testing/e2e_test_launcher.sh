b4ba4de0bdf754b3297f0548fce1d5c034cabd46
Florian Soffel <florian.soffel@d-fine.de>
2023-07-24 14:56:10 +0200

    remove installing specific docker compose version

--- testing/e2e_test_launcher.sh
+++ testing/e2e_test_launcher.sh
@@ -1,10 +1,5 @@
 set -ex
 
-#Install docker-compose version 2.20.0, because the version 2.19.1 from the github runner leads to network errors
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
-
 if [[ $TEST_EXECUTOR = "CYPRESS" ]]; then
   ./testing/execute_e2e_tests.sh
 elif [[ $TEST_EXECUTOR = "E2ETESTS" ]]; then
