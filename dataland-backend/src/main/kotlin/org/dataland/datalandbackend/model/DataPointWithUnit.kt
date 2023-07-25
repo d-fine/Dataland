db7b9088ad4e8ec63f625fea3b582012d90b5469
swaldow <stephan.waldow@d-fine.de>
2023-07-25 11:50:05 +0200

    adds DataPointWithUnit

---
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/DataPointWithUnit.kt
@@ -0,0 +1,21 @@
+package org.dataland.datalandbackend.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.dataland.datalandbackend.model.enums.data.QualityOptions
+
+/**
+ * --- API model ---
+ * Fields of a generic data point and its source
+ */
+data class DataPointWithUnit<T>(
+    val value: T? = null,
+
+    @field:JsonProperty(required = true)
+    val quality: QualityOptions,
+
+    val dataSource: CompanyReportReference? = null,
+
+    val comment: String? = null,
+
+    val unit: String,
+)
