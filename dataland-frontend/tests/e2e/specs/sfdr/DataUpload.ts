88dd27626611c7a11f188be53a5abc50d31ca44d
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-07-28 10:43:20 +0200

    WIP tests sfdr upload via form

--- dataland-frontend/tests/e2e/specs/sfdr/DataUpload.ts
+++ dataland-frontend/tests/e2e/specs/sfdr/DataUpload.ts
@@ -5,6 +5,7 @@ import { DataTypeEnum, SfdrData } from "@clients/backend";
 import { getKeycloakToken } from "@e2e/utils/Auth";
 import { generateDummyCompanyInformation, uploadCompanyViaApi } from "@e2e/utils/CompanyUpload";
 import { uploadSfdrDataViaForm } from "@e2e/utils/SfdrUpload";
+import {uploadLksgDataViaForm} from "@e2e/utils/LksgUpload";
 
 describeIf(
   "As a user, I expect that the upload form works correctly when editing and uploading a new SFDR dataset",
@@ -28,33 +29,50 @@ describeIf(
     const keycloakToken = "";
     const frontendDocumentHash = "";
 
-    it(
-      "Upload Sfdr Dataset via form, check that redirect to MyDatasets works and assure that it can be " +
-        "viewed and edited, and that file selection, upload and download works properly",
-      () => {
-        getKeycloakToken(admin_name, admin_pw).then((token) => {
-          return uploadCompanyViaApi(token, generateDummyCompanyInformation("All fields filled")).then(
-            (storedCompany) => {
-              cy.intercept(`**/companies**`).as("getDataForMyDatasetsPage");
-              uploadSfdrDataViaForm(storedCompany.companyId);
-              cy.url().should("eq", getBaseUrl() + "/datasets");
-              cy.wait("@getDataForMyDatasetsPage");
+    it("Create a company via api and upload an Sfdr dataset via the LkSG upload form", () => {
+        const uniqueCompanyMarker = Date.now().toString();
+        const testCompanyName = "Company-Created-In-DataJourney-Form-" + uniqueCompanyMarker;
+        getKeycloakToken(admin_name, admin_pw)
+            .then((token: string) => {
+                return uploadCompanyViaApi(token, generateDummyCompanyInformation(testCompanyName));
+            })
+            .then((storedCompany) => {
+                cy.intercept("**/api/companies/" + storedCompany.companyId).as("getCompanyInformation");
+                cy.visitAndCheckAppMount(
+                    "/companies/" + storedCompany.companyId + "/frameworks/" + DataTypeEnum.Sfdr + "/upload",
+                );
+                cy.wait("@getCompanyInformation", { timeout: Cypress.env("medium_timeout_in_ms") as number });
+                cy.url().should(
+                    "eq",
+                    getBaseUrl() + "/companies/" + storedCompany.companyId + "/frameworks/" + DataTypeEnum.Sfdr + "/upload",
+                );
+                cy.get("h1").should("contain", testCompanyName);
+                uploadSfdrDataViaForm(storedCompany.companyId);
+                validateFormUploadedData(storedCompany.companyId);
+            });
+    });
 
-              cy.visitAndCheckAppMount(`/companies/${storedCompany.companyId}/frameworks/${DataTypeEnum.Sfdr}`);
-
-              cy.get("[data-test='companyNameTitle']").contains("All fields filled");
-              checkAllDataProvided();
-              clickEditButtonAndEditAndValidateChange(storedCompany.companyId).then((templateDataId) => {
-                checkFileWithExistingFilenameCanNotBeResubmitted();
-                checkExistingFilenameDialogDidNotBreakSubsequentSelection();
-                checkThatFilesMustBeReferenced();
-                checkThatFilesWithSameContentDontGetReuploaded(storedCompany.companyId, templateDataId);
-                checkIfLinkedReportsAreDownloadable(storedCompany.companyId);
-              });
-            }
-          );
-        });
+      /**
+       * validates that the data uploaded via the function `uploadSfdrDataViaForm` is displayed correctly for a company
+       * @param companyId the company associated to the data uploaded via form
+       */
+      function validateFormUploadedData(companyId: string): void {
+          cy.visit("/companies/" + companyId + "/frameworks/" + DataTypeEnum.Sfdr);
+          cy.get('td > [data-test="productionSpecificOwnOperations"]').click();
+          cy.contains('Show "Most Important Products"').click();
+          cy.get(".p-dialog").find(".p-dialog-title").should("have.text", "Most Important Products");
+          cy.get(".p-dialog th").eq(0).should("have.text", "Product Name");
+          cy.get(".p-dialog th").eq(1).should("have.text", "Production Steps");
+          cy.get(".p-dialog th").eq(2).should("have.text", "Related Corporate Supply Chain");
+          cy.get(".p-dialog tr").should("have.length", 3);
+          cy.get(".p-dialog tr").eq(1).find("td").eq(0).should("have.text", "Test Product 1");
+          cy.get(".p-dialog tr").eq(1).find("td").eq(1).find("li").should("have.length", 2);
+          cy.get(".p-dialog tr").eq(1).find("td").eq(1).find("li").eq(0).should("have.text", "first");
+          cy.get(".p-dialog tr").eq(1).find("td").eq(1).find("li").eq(1).should("have.text", "second");
+          cy.get(".p-dialog tr").eq(1).find("td").eq(2).should("have.text", "Description of something");
+          cy.get(".p-dialog tr").eq(2).find("td").eq(0).should("have.text", "Test Product 2");
       }
-    );
+
+
   }
 );
