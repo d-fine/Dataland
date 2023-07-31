af2c9f19629c1870a2a5cca41d54539c4d9ea400
swaldow <stephan.waldow@d-fine.de>
2023-07-31 17:31:26 +0200

    tests if this makes ci faster

--- dataland-frontend/tests/e2e/specs/index.ts
+++ dataland-frontend/tests/e2e/specs/index.ts
@@ -34,13 +34,13 @@ if (testGroupingDisabled || cypressTestGroup === 1) {
   require("./swagger-ui");
   require("./user-api-key");
   require("./framework-view-and-upload-journey");
+  require("./sfdr");
 }
 
 if (testGroupingDisabled || cypressTestGroup === 2) {
   require("./eu-taxonomy-non-financials");
   require("./eu-taxonomy-financials");
   require("./lksg");
-  require("./sfdr");
 }
 
 if (testGroupingDisabled || cypressTestGroup === 3) {
