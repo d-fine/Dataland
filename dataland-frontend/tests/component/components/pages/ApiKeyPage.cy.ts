f4f188b2d34d98cfff65263ddb9e3e4ba4894ea6
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-08-07 09:06:08 +0200

    Revert intentional failures

--- dataland-frontend/tests/component/components/pages/ApiKeyPage.cy.ts
+++ dataland-frontend/tests/component/components/pages/ApiKeyPage.cy.ts
@@ -17,8 +17,6 @@ describe("Component test for ApiKeyCard.vue", () => {
       },
     });
 
-    cy.get('[data-test="ThisShouldNotExist"]').find("textarea").should("have.attr", "readonly");
-
     cy.get('[data-test="apiKeyInfo"]').find("textarea").should("have.attr", "readonly");
     cy.get('[data-test="apiKeyInfo"]').find("textarea").invoke("val").should("eq", "abcdefghijklmnoprstwxyz123456789");
   });
