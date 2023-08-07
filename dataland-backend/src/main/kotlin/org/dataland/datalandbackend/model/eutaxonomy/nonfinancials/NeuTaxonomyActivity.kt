903a17673ab4c453c297a0ca718beafc1b288685
Florian Soffel <florian.soffel@d-fine.de>
2023-08-07 15:12:22 +0200

    activity name added

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/eutaxonomy/nonfinancials/NeuTaxonomyActivity.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/eutaxonomy/nonfinancials/NeuTaxonomyActivity.kt
@@ -7,12 +7,14 @@ import java.math.BigDecimal
 
 // TODO for which of these fields is actual evidence required?
 open class NeuTaxonomyActivity(
+    val activityId: String, // TODO use an enum instead
     val naceCodes: List<String>,
     val share: DataPointAbsoluteAndPercentage<BigDecimal>?,
 )
 
 // TODO for which of these fields is actual evidence required?
 class NeuTaxonomyAlignedActivity(
+    activityId: String, // TODO use an enum instead
     naceCodes: List<String>,
     share: DataPointAbsoluteAndPercentage<BigDecimal>?,
     val substantialContributionCriteria: Map<NeuTaxonomyCriterion, DataPoint<BigDecimal>>?,
@@ -22,4 +24,4 @@ class NeuTaxonomyAlignedActivity(
 //    val alignedShareForLastYear: DataPoint<BigDecimal>?, // TODO neccessary?
     // TODO enabling or transitional flags must not be provided
     //  because they can be obtained from the provided json
-) : NeuTaxonomyActivity(naceCodes, share)
+) : NeuTaxonomyActivity(activityId, naceCodes, share)
