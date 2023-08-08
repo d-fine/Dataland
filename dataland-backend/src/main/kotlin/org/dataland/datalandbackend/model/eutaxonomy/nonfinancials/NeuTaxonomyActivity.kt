bc6887d2482d129ffd8642e9b3e92ca6ba1d3904
Florian Soffel <florian.soffel@d-fine.de>
2023-08-08 14:37:30 +0200

    update

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/eutaxonomy/nonfinancials/NeuTaxonomyActivity.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/eutaxonomy/nonfinancials/NeuTaxonomyActivity.kt
@@ -7,21 +7,17 @@ import java.math.BigDecimal
 
 // TODO for which of these fields is actual evidence required?
 open class NeuTaxonomyActivity(
-    val activityId: String, // TODO use an enum instead
+    val activityName: String, // TODO use an enum instead
     val naceCodes: List<String>,
     val share: DataPointAbsoluteAndPercentage<BigDecimal>?,
 )
 
 // TODO for which of these fields is actual evidence required?
 class NeuTaxonomyAlignedActivity(
-    activityId: String, // TODO use an enum instead
+    activityName: String, // TODO use an enum instead
     naceCodes: List<String>,
     share: DataPointAbsoluteAndPercentage<BigDecimal>?,
     val substantialContributionCriteria: Map<NeuTaxonomyCriterion, DataPoint<BigDecimal>>?,
     val dnshCriteria: Map<NeuTaxonomyCriterion, YesNo>?,
     val minimumSafeguards: YesNo?,
-//    val alignedShareForThisYear: DataPoint<BigDecimal>?, // TODO redundant to field "share"?
-//    val alignedShareForLastYear: DataPoint<BigDecimal>?, // TODO neccessary?
-    // TODO enabling or transitional flags must not be provided
-    //  because they can be obtained from the provided json
-) : NeuTaxonomyActivity(activityId, naceCodes, share)
+) : NeuTaxonomyActivity(activityName, naceCodes, share)
--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/eutaxonomy/nonfinancials/NeuTaxonomyDetailsPerCashFlowType.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/eutaxonomy/nonfinancials/NeuTaxonomyDetailsPerCashFlowType.kt
@@ -1,15 +1,17 @@
 package org.dataland.datalandbackend.model.eutaxonomy.nonfinancials
 
+import org.dataland.datalandbackend.model.DataPoint
 import org.dataland.datalandbackend.model.DataPointAbsoluteAndPercentage
 import java.math.BigDecimal
 
 data class NeuTaxonomyDetailsPerCashFlowType(
-    val alignedActivities: List<NeuTaxonomyAlignedActivity>, // TODO FE convert this list to an object properties of the parent
-    val alignedShare: DataPointAbsoluteAndPercentage<BigDecimal>,
+    val totalShare: DataPoint<BigDecimal>,
+    val totalNonEligibleShare: DataPointAbsoluteAndPercentage<BigDecimal>, // TODO this seems redundant
+    val totalEligibleShare: DataPointAbsoluteAndPercentage<BigDecimal>, // TODO is this confusing with eligibleNotAlignedShare?
+    val totalEligibleNonAlignedShare: DataPointAbsoluteAndPercentage<BigDecimal>, // TODO is this confusing with eligibleNotAlignedShare?
     val eligibleNotAlignedActivities: List<NeuTaxonomyActivity>, // TODO FE convert this list to an object properties of the parent
-    val eligibleShare: DataPointAbsoluteAndPercentage<BigDecimal>, // TODO is this confusing with eligibleNotAlignedShare?
-//    val nonEligibleShare: DataPointAbsoluteAndPercentage<BigDecimal>, // TODO this seems redundant
-    val totalShare: DataPointAbsoluteAndPercentage<BigDecimal>,
-//    val enablingAlignedShare: DataPointAbsoluteAndPercentage<BigDecimal>,
-//    val transitionalAlignedShare: DataPointAbsoluteAndPercentage<BigDecimal>,
+    val totalAlignedShare: DataPointAbsoluteAndPercentage<BigDecimal>,
+    val alignedActivities: List<NeuTaxonomyAlignedActivity>, // TODO FE convert this list to an object properties of the parent
+    val enablingAlignedShare: DataPointAbsoluteAndPercentage<BigDecimal>,
+    val transitionalAlignedShare: DataPointAbsoluteAndPercentage<BigDecimal>,
 )
