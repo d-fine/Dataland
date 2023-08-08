1dc8200f946a896760ea33afe67dd8992427ac28
Florian Soffel <florian.soffel@d-fine.de>
2023-08-08 14:44:50 +0200

    test3

--- dataland-frontend/tests/component/testUtils/MountWithPlugins.ts
+++ dataland-frontend/tests/component/testUtils/MountWithPlugins.ts
@@ -141,7 +141,8 @@ function mountWithDialog<T extends DefineComponent<any, any, any, any, any>>(
   });
   const wrapperOptions = options as ComponentMountingOptions<typeof componentWrapper>;
   wrapperOptions.global ??= {};
-  wrapperOptions.global.stubs = { transition: false};
+  wrapperOptions.global.stubs ??= {};
+  (wrapperOptions.global.stubs as Record<string, boolean>).transition = false
   return mountWithPlugins(componentWrapper, wrapperOptions);
 }
 
