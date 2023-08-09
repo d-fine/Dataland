f177aa934e17b2f0d0884955025b2385c48a7988
swaldow <stephan.waldow@d-fine.de>
2023-08-09 09:40:31 +0200

    one small change to the test

--- dataland-frontend/tests/e2e/utils/LksgUpload.ts
+++ dataland-frontend/tests/e2e/utils/LksgUpload.ts
@@ -273,7 +273,7 @@ function checkIfUploadedFilesAreReferencedInTheDataset(): void {
   cy.intercept("POST", "**/api/data/lksg", (request) => {
     request.reply(200, {});
   }).as("postLksgData");
-  cy.get('button[data-test="submitButton"]').should("not.have.class", "button-disabled").click();
+  submitButton.clickButton()
   cy.wait("@postLksgData").then((interception) => {
     const postedObject = interception.request.body as CompanyAssociatedDataLksgData;
     const postedLksgDataset = postedObject;
