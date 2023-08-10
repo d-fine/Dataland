e4ebc5fa50d3806d295ba3d11df46e89d47da6b6
Piotr Skonieczny <piotr.skonieczny@pwc.com>
2023-08-10 14:33:00 +0200

    Sort indexes in reverse and make sure they're unique before running loop

--- dataland-frontend/src/components/forms/parts/elements/basic/UploadDocumentsForm.vue
+++ dataland-frontend/src/components/forms/parts/elements/basic/UploadDocumentsForm.vue
@@ -134,7 +134,8 @@ export default defineComponent({
      * @param indexesOfFilesToRemove index list of numbers of the files to remove
      */
     removeDocumentsFromDocumentsToUpload(indexesOfFilesToRemove: number[]) {
-      indexesOfFilesToRemove.forEach((indexOfFileToRemove) => {
+      const sortedAndUniqueIndexes = [...new Set(indexesOfFilesToRemove.sort((a, b) => b - a))];
+      sortedAndUniqueIndexes.forEach((indexOfFileToRemove) => {
         ((this.$refs.fileUpload as FileUpload).remove as (index: number) => void)(indexOfFileToRemove);
         this.documentsToUpload.splice(indexOfFileToRemove, 1);
       });
