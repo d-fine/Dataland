6ca71330307292d5524726c77c5168787f4ab067
Florian Soffel <florian.soffel@d-fine.de>
2023-08-14 09:48:57 +0200

    cleanup

--- dataland-frontend/tests/e2e/utils/Auth.ts
+++ dataland-frontend/tests/e2e/utils/Auth.ts
@@ -29,7 +29,7 @@ let globalJwt: string = ""
  */
 export function login(username = reader_name, password = reader_pw, otpGenerator?: () => string): void {
   cy.intercept("https://www.youtube-nocookie.com/**", {forceNetworkError: false}).as("youtube");
-  cy.intercept({times: 1, url: "/api/companies*"}).as("asdf")
+  cy.intercept({times: 1, url: "/api/companies*"}).as("getCompanies")
   cy.visitAndCheckAppMount("/")
     .wait("@youtube", {timeout: Cypress.env("medium_timeout_in_ms") as number})
     .get("button[name='login_dataland_button']")
@@ -57,22 +57,9 @@ export function login(username = reader_name, password = reader_pw, otpGenerator
       .click();
   }
   cy.url().should("eq", getBaseUrl() + "/companies");
-  cy.wait("@asdf").then((interception) => {
+  cy.wait("@getCompanies").then((interception) => {
     globalJwt = interception.request.headers["authorization"] as string
   })
-}
-
-function createUUID() {
-  var s = [];
-  var hexDigits = '0123456789abcdef';
-  for (var i = 0; i < 36; i++) {
-    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
-  }
-  s[14] = '4';
-  s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);
-  s[8] = s[13] = s[18] = s[23] = '-';
-  var uuid = s.join('');
-  return uuid;
 }
 
 /**
@@ -90,59 +77,12 @@ export function ensureLoggedIn(username?: string, password?: string): void {
     },
     {
       validate: () => {
-        // const kcRoot = 'https://local-dev.dataland.com';
-        // const kcRealm = 'datalandsecurity';
-        // const kcClient = 'dataland-public';
-        // const kcRedirectUri = 'https://local-dev.dataland.com/';
-        // const loginPageRequest = {
-        //   url: `${kcRoot}/auth/realms/${kcRealm}/protocol/openid-connect/auth`,
-        //   qs: {
-        //     client_id: kcClient,
-        //     redirect_uri: kcRedirectUri,
-        //     nonce: createUUID(),
-        //     response_mode: 'fragment',
-        //     response_type: 'code',
-        //     scope: 'openid',
-        //     code_challenge: "L4ex8hTvHo-vYBbPDwvKCr3hmGsPj1V3iKHLTJEfibM",
-        //     code_challenge_method: "S256"
-        //   }
-        // };
-        // // Open the KC login page, fill in the form with username and password and submit.
-        // cy.visit(loginPageRequest)
-        // cy.url().should("contain", "/companies")
-
         cy.request({
-          // url: '/api/companies',
           url: '/api/token',
           headers: {
             Authorization: globalJwt,
           }
         }).its('status').should('eq', 200)
-
-        // cy.visit("/companies")
-        // cy.contains("AVAILABLE DATASETS").should("exist")
-        // cy.wait(10000)
-
-        // console.log("step 1")
-        // cy.visit("/keycloak/realms/datalandsecurity/protocol/openid-connect/3p-cookies/step1.html")
-        //   .url()
-        //   .should(
-        //     "eq",
-        //     getBaseUrl() + "/keycloak/realms/datalandsecurity/protocol/openid-connect/3p-cookies/step2.html",
-        //   ).then(() => {
-        //   console.log("step 2")
-        // })
-        // cy.window()
-        //   .then((window): boolean => {
-        //     if ("hasAccess" in window) {
-        //       console.log("maybe true")
-        //       return window.hasAccess as boolean;
-        //     } else {
-        //       console.log("false")
-        //       return false;
-        //     }
-        //   })
-        //   .should("be.true");
       },
       cacheAcrossSpecs: true,
     },
