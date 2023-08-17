3afda147fde8cbc21de9d2d72f01dd78d0b73031
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-08-17 14:22:04 +0200

    make parameter optional

--- dataland-frontend/tests/component/testUtils/Keycloak.ts
+++ dataland-frontend/tests/component/testUtils/Keycloak.ts
@@ -4,7 +4,7 @@ export interface KeycloakMockConfiguration {
   userId?: string;
   roles?: Array<string>;
   idTokenParsed?: { picture: string };
-  authenticated: boolean;
+  authenticated?: boolean;
 }
 
 /**
