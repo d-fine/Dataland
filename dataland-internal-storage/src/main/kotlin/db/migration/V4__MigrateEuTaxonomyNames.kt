0e36e760d8aff445e8edd4565676d90afb4a91c4
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-08-18 15:21:58 +0200

    fix migration

--- dataland-internal-storage/src/main/kotlin/db/migration/V4__MigrateEuTaxonomyNames.kt
+++ dataland-internal-storage/src/main/kotlin/db/migration/V4__MigrateEuTaxonomyNames.kt
@@ -24,13 +24,15 @@ class V4__MigrateEuTaxonomyNames : BaseJavaMigration() {
         dataTypesToMigrate.forEach { dataType: DataTypeEnum ->
             val companyAssociatedDatasets = getCompanyAssociatedDatasetsForDataType(context, dataType)
             companyAssociatedDatasets.forEach {
-                var dataset = JSONObject(it.companyAssociatedData.getString("data"))
+                val companyAssociatedDatasetAsString = it.companyAssociatedData.toString()
+                val companyAssociatedDatasetWithEscapedSingleQuotes =
+                    JSONObject(companyAssociatedDatasetAsString.replace("'", "''"))
+                var euTaxoDataset = JSONObject(companyAssociatedDatasetWithEscapedSingleQuotes.getString("data"))
                 mapOfOldToNewFieldNames.forEach {
-                    val datasetWithoutSingleQuotes = dataset.get(it.key).toString().replace("'", "''")
-                    dataset.put(it.value, datasetWithoutSingleQuotes)
-                    dataset.remove(it.key)
+                    euTaxoDataset.put(it.value, euTaxoDataset)
+                    euTaxoDataset.remove(it.key)
                 }
-                it.companyAssociatedData.put("data", dataset.toString())
+                it.companyAssociatedData.put("data", euTaxoDataset.toString())
                 context!!.connection.createStatement().execute(it.getWriteQuery())
             }
         }
