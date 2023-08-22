15a2b1a053ea38cce0008aef3f3164d9cccf9d74
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-08-22 18:23:56 +0200

    WIP component test order of subcategories

--- dataland-frontend/tests/component/components/pages/EUTaxonomyKpiOrder.cy.ts
+++ dataland-frontend/tests/component/components/pages/EUTaxonomyKpiOrder.cy.ts
@@ -19,7 +19,20 @@ describe("Component test for the NewEUTaxonomy Page", () => {
     //const kpiList:string[] = ["GENERAL", "REVENUE", "CAPEX", "OPEX"]; These 2 lists have to be used when running the cypress test locally, because the fixtures are different.
     //const kpiListOrderChanged = ["REVENUE", "GENERAL", "OPEX", "CAPEX"];
     const kpiList:string[] = ["BASIC INFORMATION","ASSURANCE","REVENUE","CAPEX", "OPEX"];
+    const dataTestList:string[] = ["Basic Information","Assurance","Revenue","CapEx", "OpEx"];
     const kpiListOrderChanged = ["ASSURANCE","OPEX", "BASIC INFORMATION", "REVENUE", "CAPEX"];
+
+    const subcategoryList:string[] = ["Basic Information",
+                    "Assurance",
+                    "Total Aligned Revenue", "Total Revenue", "Total Eligible Revenue", "Total Non-Aligned Revenue", "Total Non-Eligible Revenue",
+                    "Total Aligned CapEx", "Total CapEx", "Total Eligible CapEx", "Total Non-Aligned CapEx", "Total Non-Eligible CapEx",
+        "Total Aligned OpEx", "Total OpEx", "Total Eligible OpEx", "Total Non-Aligned OpEx", "Total Non-Eligible OpEx"];
+
+    const subcategoryDataTestList:string[] = ["_basicInformation",
+        "assurance",
+        "totalAlignedShare", "totalAmount", "totalEligibleShare", "totalNonAlignedShare", "totalNonEligibleShare",
+        "totalAlignedShare", "totalAmount", "totalEligibleShare", "totalNonAlignedShare", "totalNonEligibleShare",
+        "totalAlignedShare", "totalAmount", "totalEligibleShare", "totalNonAlignedShare", "totalNonEligibleShare"];
 
     it("Check order of the displayed KPIs and category entries", () => {
         const preparedFixture = getPreparedFixture("only-eligible-numbers", preparedFixtures);
@@ -47,7 +60,7 @@ describe("Component test for the NewEUTaxonomy Page", () => {
                 };
             },
         });
-    /**
+
         cy.get("[data-test='ThreeLayerTableTest']").get(".d-table-style")
             .each((element, index) =>  {
                 cy.wrap(element).eq(0).eq(0).get(".p-badge").eq(index).should("have.text",kpiList[index]);
@@ -59,8 +72,18 @@ describe("Component test for the NewEUTaxonomy Page", () => {
             .each((element, index) =>  {
                 cy.wrap(element).eq(0).eq(0).get(".p-badge").eq(index).should("not.have.text", kpiListOrderChanged[index]);
             });
-     **/
-    cy.get("[data-test='TwoLayerTest']").eq(2);
+
+        for (let i = 1; i < 5; i++) {
+            cy.get(`[data-test='${dataTestList[i]}']`).click();
+        }
+
+        cy.get(".p-rowgroup-header");
+        cy.get("[data-test='TwoLayerTest']").eq(0).get(" [data-test='_basicInformation'").should("contain","Basic Information");
+        for (let i = 0; i < 17; i++) {
+            cy.get(".p-rowgroup-header").eq(i).get(`[data-test="${subcategoryDataTestList[i]}"]`).should("contain",`${subcategoryList[i]}`);
+        }
+
+
     });
 
 
