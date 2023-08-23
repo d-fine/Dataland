08aa2f632cb805bc32e668df0caa85be63a208bb
swaldow <stephan.waldow@d-fine.de>
2023-08-23 09:28:22 +0200

    first draft of converted component test

---
+++ dataland-frontend/tests/component/components/forms/CreateNewEuTaxonomyForNonFinancials.cy.ts
@@ -0,0 +1,194 @@
+import CreateNewEuTaxonomyForNonFinancials from "@/components/forms/CreateNewEuTaxonomyForNonFinancials.vue";
+import { minimalKeycloakMock } from "@ct/testUtils/Keycloak";
+import { TEST_PDF_FILE_BASEPATH, TEST_PDF_FILE_NAME } from "@sharedUtils/ConstantsForPdfs";
+import { uploadDocuments } from "@sharedUtils/components/UploadDocuments";
+import { type CompanyAssociatedDataNewEuTaxonomyDataForNonFinancials } from "@clients/backend";
+import { submitButton } from "@sharedUtils/components/SubmitButton";
+import DataPointFormWithToggle from "@/components/forms/parts/kpiSelection/DataPointFormWithToggle.vue";
+
+describe("Component tests for the CreateP2pDataset that test dependent fields", () => {
+  /**
+   * On the eu taxonomy for non-financial services edit page, this method checks that there can not be a file uploaded
+   * whose name equals the one of a file selected before
+   */
+  function checkFileWithExistingFilenameIsNotBeingAdded(): void {
+    const reportThatCanBeUploaded = "test-report";
+    const reportThatAlreadyExists = TEST_PDF_FILE_NAME;
+    uploadDocuments.selectFile(reportThatCanBeUploaded, "referencedReports");
+    uploadDocuments.validateReportToUploadIsListedInFileSelectorAndHasInfoForm(reportThatCanBeUploaded);
+    uploadDocuments.selectFile(reportThatAlreadyExists, "referencedReports");
+    uploadDocuments.validateReportIsListedAsAlreadyUploaded(reportThatAlreadyExists);
+    uploadDocuments.validateReportIsNotInFileSelectorAndHasNoInfoForm(reportThatAlreadyExists);
+    uploadDocuments.validateNumberOfReportsSelectedForUpload(1);
+  }
+
+  /**
+   * On the eu taxonomy for non-financial services edit page, this method checks that there can not be a file uploaded
+   * whose name equals the one of a file selected before
+   */
+  function checkFileWithExistingFilenameOpensDialogWithWarning(): void {
+    uploadDocuments.selectFile(TEST_PDF_FILE_NAME, "referencedReports");
+    cy.get(`button[data-test='upload-files-button-referencedReports']`).click();
+    cy.get("input[type=file]").selectFile(
+      `../${TEST_PDF_FILE_BASEPATH}/more-pdfs-in-seperate-directory/${TEST_PDF_FILE_NAME}.pdf`,
+      { force: true },
+    );
+    cy.get(".p-dialog-content").should("contain.text", "already uploaded");
+    cy.get(".p-dialog-header-close").click();
+    cy.get(`[data-test="${TEST_PDF_FILE_NAME}ToUploadContainer"]`).should("have.length", 1);
+  }
+
+  /**
+   * Adds a report to upload and removes it again afterwards checking that no dialog regarding a duplicate file name
+   * is wrongly triggered and that the file is correctly removed.
+   */
+  function checkExistingFilenameDialogDidNotBreakSubsequentSelection(): void {
+    const reportNameA = TEST_PDF_FILE_NAME;
+    const reportNameB = `${TEST_PDF_FILE_NAME}2`;
+    uploadDocuments.selectFile(reportNameB, "referencedReports");
+
+    cy.get(".p-dialog-content").should("not.exist");
+    uploadDocuments.validateReportToUploadIsListedInFileSelectorAndHasInfoForm(reportNameA);
+    uploadDocuments.validateReportToUploadIsListedInFileSelectorAndHasInfoForm(reportNameB);
+
+    uploadDocuments.removeReportFromSelectionForUpload(TEST_PDF_FILE_NAME);
+
+    uploadDocuments.validateReportIsNotAlreadyUploadedOrSelectedForUpload(reportNameA);
+    uploadDocuments.validateReportToUploadIsListedInFileSelectorAndHasInfoForm(reportNameB);
+    uploadDocuments.validateNumberOfReportsSelectedForUpload(1);
+
+    uploadDocuments.removeReportFromSelectionForUpload(reportNameB);
+
+    uploadDocuments.validateReportIsNotAlreadyUploadedOrSelectedForUpload(reportNameB);
+    uploadDocuments.validateNumberOfReportsSelectedForUpload(0);
+  }
+
+  /**
+   * On the eu taxonomy for non-financial services edit page, this method checks that submission is denied
+   * if a report is not referenced
+   */
+  function checkThatFilesMustBeReferenced(): void {
+    uploadDocuments.fillAllFormsOfReportsSelectedForUpload();
+    submitButton.clickButton();
+    cy.get('[data-test="failedUploadMessage"]').should("exist").should("contain.text", "test-report");
+    cy.get('[data-test="failedUploadMessage"]')
+      .should("exist")
+      .should("satisfy", (element: JQuery<HTMLElement>) => {
+        const expectedStrings = ["Not all uploaded reports are used", "test-report"];
+        const elementText = element.text();
+        return expectedStrings.every((expectedString) => elementText.includes(expectedString));
+      });
+  }
+
+  /**
+   * This method returns a mocked dataset for eu taxo non financials with some fields filled.
+   * @returns the dataset
+   */
+  function createMockCompanyAssociatedDataNewEuTaxoNonFinancials(): CompanyAssociatedDataNewEuTaxonomyDataForNonFinancials {
+    return {
+      companyId: "abc",
+      reportingPeriod: "2020",
+      data: {
+        capex: {
+          totalAmount: {
+            quality: "Estimated",
+            dataSource: {
+              report: `${TEST_PDF_FILE_NAME}FileCopy`,
+              page: 12,
+            },
+            comment: "test",
+            value: 12000000,
+            unit: "EUR",
+          },
+        },
+        opex: {
+          totalAmount: {
+            quality: "Estimated",
+            dataSource: {
+              report: "None...",
+              page: 12,
+            },
+            comment: "test",
+            value: 10000000,
+            unit: "EUR",
+          },
+        },
+        revenue: {
+          totalAmount: {
+            quality: "Estimated",
+            dataSource: {
+              report: "None...",
+              page: 12,
+            },
+            comment: "test",
+            value: 40000000,
+            unit: "EUR",
+          },
+        },
+        general: {
+          fiscalYearDeviation: "Deviation",
+          fiscalYearEnd: "2023-09-11",
+          scopeOfEntities: "Yes",
+          nfrdMandatory: "Yes",
+          euTaxonomyActivityLevelReporting: "Yes",
+          assurance: {
+            assurance: "None",
+            provider: "Assurance Provider",
+            dataSource: {
+              report: TEST_PDF_FILE_NAME,
+              page: 1,
+            },
+          },
+          numberOfEmployees: 333,
+          referencedReports: {
+            [`${TEST_PDF_FILE_NAME}FileCopy`]: {
+              reference: "bbebf6077b4ab868fd3e5f83ac70c864fc301c9ab9b3e1a53f52ac8a31b97ff7",
+              isGroupLevel: "No",
+              reportDate: "2023-07-12",
+              currency: "EUR",
+            },
+            [TEST_PDF_FILE_NAME]: {
+              reference: "bbebf6077b4ab868fd3e5f83ac70c864fc301c9ab9b3e1a53f52ac8a31b97ff7",
+              isGroupLevel: "No",
+              reportDate: "2023-07-12",
+              currency: "EUR",
+            },
+          },
+        },
+      },
+    };
+  }
+
+  const companyAssociatedNewEuTaxoFinancialsData = createMockCompanyAssociatedDataNewEuTaxoNonFinancials();
+
+  it("Check that warning appears if two pdf files with same name are selected for upload", () => {
+    cy.stub(DataPointFormWithToggle);
+    cy.mountWithDialog(
+      CreateNewEuTaxonomyForNonFinancials,
+      {
+        keycloak: minimalKeycloakMock({}),
+      },
+      { companyID: "company-id-does-not-matter-in-this-test" },
+    ).then(() => {
+      checkFileWithExistingFilenameOpensDialogWithWarning();
+      checkExistingFilenameDialogDidNotBreakSubsequentSelection();
+    });
+  });
+
+  it("Open upload page prefilled and assure that only the sections that the dataset holds are displayed", () => {
+    cy.stub(DataPointFormWithToggle);
+    cy.mountWithPlugins(CreateNewEuTaxonomyForNonFinancials, {
+      keycloak: minimalKeycloakMock({}),
+      data() {
+        return {
+          formInputsModel: companyAssociatedNewEuTaxoFinancialsData,
+          templateDataset: companyAssociatedNewEuTaxoFinancialsData.data,
+        };
+      },
+    }).then(() => {
+      checkFileWithExistingFilenameIsNotBeingAdded();
+      checkThatFilesMustBeReferenced();
+    });
+  });
+});
+//TODO This file has to be modified to work
