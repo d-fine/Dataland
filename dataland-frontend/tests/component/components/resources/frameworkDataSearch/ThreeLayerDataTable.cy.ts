2de832c48446f6b352840f133d19269753983827
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-08-24 10:49:39 +0200

    refactoring of ThreeLayerDataTable.cy.ts + linter issues

---
+++ dataland-frontend/tests/component/components/resources/frameworkDataSearch/ThreeLayerDataTable.cy.ts
@@ -0,0 +1,106 @@
+import ThreeLayerDataTable from "@components/resources/frameworkDataSearch/ThreeLayerDataTable.vue";
+import { minimalKeycloakMock } from "@ct/testUtils/Keycloak";
+import { newEuTaxonomyForNonFinancialsDisplayDataModel } from "@components/resources/frameworkDataSearch/euTaxonomy/NewEuTaxonomyForNonFinancialsDisplayDataModel";
+import { DataAndMetaInformationNewEuTaxonomyForNonFinancialsViewModel } from "@components/resources/frameworkDataSearch/euTaxonomy/NewEuTaxonomyForNonFinancialsViewModel";
+import { mockData } from "@ct/utils/mockDataNewEuTaxonomyForNonFinancials";
+
+describe("Component test for the NewEUTaxonomy Page", () => {
+  const expectedOrderOfCategories: string[] = ["BASIC INFORMATION", "ASSURANCE", "REVENUE", "CAPEX", "OPEX"];
+  const dataTestTagsOfCategories: string[] = ["Basic Information", "Assurance", "Revenue", "CapEx", "OpEx"];
+
+  /**
+   * Creates a list with the labels of the subcategories inside a cash flow category in the right order.
+   * @param categoryName is the name of the cash flow category
+   * @returns an array with the subcategory labels in the right order
+   */
+  function buildExpectedOrderOfSubcategoriesForCategory(categoryName: "Revenue" | "CapEx" | "OpEx"): string[] {
+    return [
+      `Total Aligned ${categoryName}`,
+      `Total ${categoryName}`,
+      `Total Eligible ${categoryName}`,
+      `Total Non-Aligned ${categoryName}`,
+      `Total Non-Eligible ${categoryName}`,
+    ];
+  }
+
+  const expectedOrderOfSubcategoriesGroupedByCategories: string[][] = [
+    ["Basic Information"],
+    ["Assurance"],
+    buildExpectedOrderOfSubcategoriesForCategory("Revenue"),
+    buildExpectedOrderOfSubcategoriesForCategory("CapEx"),
+    buildExpectedOrderOfSubcategoriesForCategory("OpEx"),
+  ];
+
+  const dataTestTagsOfCashFlowSubcategory = [
+    "totalAlignedShare",
+    "totalAmount",
+    "totalEligibleShare",
+    "totalNonAlignedShare",
+    "totalNonEligibleShare",
+  ];
+
+  const dataTestTagsOfSubcategoriesGroupedByCategories: string[][] = [
+    ["_basicInformation"],
+    ["assurance"],
+    dataTestTagsOfCashFlowSubcategory,
+    dataTestTagsOfCashFlowSubcategory,
+    dataTestTagsOfCashFlowSubcategory,
+  ];
+
+  /**
+   * Toggle a category by clicking on it via its data-test tag
+   */
+  function toggleCategoryByClick(dataTestTagOfCategory:string): void{
+    cy.get(`[data-test='${dataTestTagOfCategory}']`).click();
+  }
+
+  it("Check order of the displayed KPIs and its entries", () => {
+    const singleMockDataAndMetaInfo = new DataAndMetaInformationNewEuTaxonomyForNonFinancialsViewModel(mockData); // TODO Can't we put mockData to the testing folder as json?
+    const dataAndMetaInfo: Array<DataAndMetaInformationNewEuTaxonomyForNonFinancialsViewModel> = [
+      singleMockDataAndMetaInfo,
+    ];
+
+    cy.mountWithPlugins(ThreeLayerDataTable, {
+      keycloak: minimalKeycloakMock({}),
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      props: {
+        dataModel: newEuTaxonomyForNonFinancialsDisplayDataModel,
+        dataAndMetaInfo: dataAndMetaInfo,
+      },
+    }).then(() => {
+      cy.get("[data-test='TwoLayerTest']")
+        .get(".d-table-style")
+        .each((element, index) => {
+          cy.wrap(element).eq(0).eq(0).get(".p-badge").eq(index).should("have.text", expectedOrderOfCategories[index]);
+        });
+
+      cy.wait(50); // TODO we should try to avoid waits with hardcoded times
+
+      toggleCategoryByClick(dataTestTagsOfCategories[0])
+
+      /**
+       * The goal for the loop is to expand one KPI at a time and check the order of the entries.
+       */
+      cy.wait(300); // TODO we should try to avoid waits with hardcoded times
+
+      let subcategoriesForCurrentCategory;
+      for (let categoryIndex = 1; categoryIndex < dataTestTagsOfCategories.length; categoryIndex++) {
+        subcategoriesForCurrentCategory = dataTestTagsOfSubcategoriesGroupedByCategories[categoryIndex];
+
+        toggleCategoryByClick(dataTestTagsOfCategories[categoryIndex])
+
+        for (let subCategoryIndex = 0; subCategoryIndex < subcategoriesForCurrentCategory.length; subCategoryIndex++) {
+          cy.get(".p-rowgroup-header")
+            .filter(":visible")
+            .eq(subCategoryIndex)
+            .get(`span[id="${subcategoriesForCurrentCategory[subCategoryIndex]}"]`)
+            .should("contain", `${expectedOrderOfSubcategoriesGroupedByCategories[categoryIndex][subCategoryIndex]}`);
+        }
+        toggleCategoryByClick(dataTestTagsOfCategories[categoryIndex])
+
+        cy.wait(50); // TODO we should try to avoid waits with hardcoded times
+      }
+    });
+  });
+});
