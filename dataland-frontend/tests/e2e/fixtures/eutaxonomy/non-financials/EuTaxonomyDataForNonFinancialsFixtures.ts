7cd02acdd84938e52df26f73134abe691a886e4b
swaldow <stephan.waldow@d-fine.de>
2023-08-28 12:36:14 +0200

    fixes fake fixtures

--- dataland-frontend/tests/e2e/fixtures/eutaxonomy/non-financials/EuTaxonomyDataForNonFinancialsFixtures.ts
+++ dataland-frontend/tests/e2e/fixtures/eutaxonomy/non-financials/EuTaxonomyDataForNonFinancialsFixtures.ts
@@ -13,7 +13,7 @@ import {
   getRandomNumberOfDistinctElementsFromArray,
   type ReferencedDocuments,
 } from "@e2e/fixtures/FixtureUtils";
-import { generateDatapoint } from "@e2e/fixtures/common/DataPointFixtures";
+import { generateDatapoint, generateNumericOrEmptyDatapoint } from "@e2e/fixtures/common/DataPointFixtures";
 import { generateEuTaxonomyWithBaseFields } from "@e2e/fixtures/eutaxonomy/EuTaxonomySharedValuesFixtures";
 import { randomEuroValue } from "@e2e/fixtures/common/NumberFixtures";
 import { assertDefined } from "@/utils/TypeScriptUtils";
@@ -108,7 +108,7 @@ function generateAlignedActivity(): EuTaxonomyAlignedActivity {
  */
 export function generateEuTaxonomyPerCashflowType(reports: ReferencedDocuments): EuTaxonomyDetailsPerCashFlowType {
   return {
-    totalAmount: valueOrUndefined(generateDatapoint(valueOrUndefined(generateAmountWithCurrency()), reports)),
+    totalAmount: valueOrUndefined(generateNumericOrEmptyDatapoint(reports, randomEuroValue(0, 100))),
     totalNonEligibleShare: valueOrUndefined(generateFinancialShare()),
     totalEligibleShare: valueOrUndefined(generateFinancialShare()),
     totalNonAlignedShare: valueOrUndefined(generateFinancialShare()),
