6acda42694b57d2113615e8b448c3ff12676b844
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-09-13 08:04:46 +0200

    deactivated further tests

--- dataland-frontend/tests/e2e/specs/landing-page/FooterSection.ts
+++ dataland-frontend/tests/e2e/specs/landing-page/FooterSection.ts
@@ -5,37 +5,37 @@ import { reader_name, reader_pw } from "@e2e/utils/Cypress";
 import { checkFooter } from "@sharedUtils/ElementChecks";
 import { ARRAY_OF_FRAMEWORKS_WITH_VIEW_PAGE } from "@/utils/Constants";
 
-
 describeIf(
-    "As a user, I expect the footer section to be present and contain relevant legal links",
-    {
-      executionEnvironments: ["developmentLocal", "ci"],
-    },
-    () => {
-  describe("Checks that the footer section is present on many pages", () => {
-    beforeEach(() => {
-      cy.ensureLoggedIn();
-    });
-
-    const pagesToCheck = ["/companies", `/preview`];
-
-    pagesToCheck.forEach((page) => {
-      it(`Checks that the footer is present on ${page}`, () => {
-        cy.visitAndCheckAppMount(page);
-        checkFooter();
+  "As a user, I expect the footer section to be present and contain relevant legal links",
+  {
+    executionEnvironments: ["developmentLocal", "ci"],
+  },
+  () => {
+    describe("Checks that the footer section is present on many pages", () => {
+      beforeEach(() => {
+        cy.ensureLoggedIn();
       });
-    });
 
-    ARRAY_OF_FRAMEWORKS_WITH_VIEW_PAGE.forEach((framework) => {
-      it(`Checks that the footer is present on ${framework}`, () => {
-        getKeycloakToken(reader_name, reader_pw).then((token) => {
-          cy.browserThen(getStoredCompaniesForDataType(token, framework)).then((storedCompanies) => {
-            const companyId = storedCompanies[0].companyId;
-            cy.visitAndCheckAppMount(`/companies/${companyId}/frameworks/${framework}`);
-            checkFooter();
+      const pagesToCheck = ["/companies", `/preview`];
+
+      pagesToCheck.forEach((page) => {
+        it(`Checks that the footer is present on ${page}`, () => {
+          cy.visitAndCheckAppMount(page);
+          checkFooter();
+        });
+      });
+
+      ARRAY_OF_FRAMEWORKS_WITH_VIEW_PAGE.forEach((framework) => {
+        it(`Checks that the footer is present on ${framework}`, () => {
+          getKeycloakToken(reader_name, reader_pw).then((token) => {
+            cy.browserThen(getStoredCompaniesForDataType(token, framework)).then((storedCompanies) => {
+              const companyId = storedCompanies[0].companyId;
+              cy.visitAndCheckAppMount(`/companies/${companyId}/frameworks/${framework}`);
+              checkFooter();
+            });
           });
         });
       });
     });
-  });
-});
+  },
+);
