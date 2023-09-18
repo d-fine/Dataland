748237aced2fa59fb452af629254a81d1876492d
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-09-18 09:55:23 +0200

    WIP adjustment of QualityAssurance.cy.ts

--- dataland-frontend/tests/component/components/pages/QualityAssurance.cy.ts
+++ dataland-frontend/tests/component/components/pages/QualityAssurance.cy.ts
@@ -1,88 +1,90 @@
 import QualityAssurance from "@/components/pages/QualityAssurance.vue";
 import { minimalKeycloakMock } from "@ct/testUtils/Keycloak";
 import { type FixtureData, getPreparedFixture } from "@sharedUtils/Fixtures";
-import { type DataMetaInformation, DataTypeEnum, type PathwaysToParisData, type SmeData } from "@clients/backend";
+import { type DataMetaInformation, DataTypeEnum, type PathwaysToParisData } from "@clients/backend";
 import { QaStatus } from "@clients/qaservice";
+import ViewFrameworkData from "@/components/pages/ViewFrameworkData.vue";
 
 describe("Component tests for the Quality Assurance page", () => {
-  const keycloakMock = minimalKeycloakMock({
-    roles: ["ROLE_USER", "ROLE_UPLOADER", "ROLE_REVIEWER"],
-  });
+  let p2pFixtureForTest: FixtureData<PathwaysToParisData>;
+  let mockDataMetaInfoForActiveDataset: DataMetaInformation;
 
-  /**
-   * Mock a data meta information object for a dataset specified by its fixture data, data Id and data type
-   * @param fixtureData The fixture data corresponding to review
-   * @param dataId The Id corresponding to the dataset to review
-   * @param dataType The type of the dataset to review
-   * @returns the data meta information object created
-   */
-  function mockDataMetaInformationForFixtureDataWithIdAndType(
-    fixtureData: FixtureData<PathwaysToParisData | SmeData>,
-    dataId: string,
-    dataType: DataTypeEnum,
-  ): DataMetaInformation {
-    return {
-      dataId: dataId,
-      companyId: "testCompanyId",
-      dataType: dataType,
-      uploadTime: 1672531200 /* 01.01.2023 */,
-      reportingPeriod: fixtureData.reportingPeriod,
-      currentlyActive: false,
-      qaStatus: QaStatus.Pending,
-    } as DataMetaInformation;
-  }
-
-  /**
-   * Create a QA data object from the fixture data and the data id, configure the intercepts, mount the component and
-   * check that clicking leads to the expected request
-   * @param fixtureData The fixture data corresponding to review
-   * @param dataId The Id corresponding to the dataset to review
-   * @param dataType The type of the dataset to review
-   */
-  function createQaDataObjectAndPerformRequestCheck(
-    fixtureData: FixtureData<PathwaysToParisData | SmeData>,
-    dataId: string,
-    dataType: DataTypeEnum,
-  ): void {
-    const qaDataObject = {
-      dataId: dataId,
-      metaInformation: mockDataMetaInformationForFixtureDataWithIdAndType(fixtureData, dataId, dataType),
-      companyInformation: fixtureData.companyInformation,
-    };
-    cy.intercept("**/qa/datasets", [dataId]);
-    cy.intercept("**/api/metadata/*", qaDataObject.metaInformation);
-    cy.intercept("**/api/companies/*", {
-      companyId: qaDataObject.metaInformation.companyId,
-      companyInformation: qaDataObject.companyInformation,
-      dataRegisteredByDataland: [qaDataObject.metaInformation],
-    });
-    cy.intercept(`**/api/data/${dataType}/*`, {
-      companyId: qaDataObject.metaInformation.companyId,
-      reportingPeriod: fixtureData.reportingPeriod,
-      data: fixtureData.t,
-    }).as("fetchData");
-    cy.mountWithPlugins<typeof QualityAssurance>(QualityAssurance, {
-      keycloak: keycloakMock,
-    });
-    cy.contains("td", `${dataId}`).click();
-    cy.wait("@fetchData");
-  }
-
-  it("Checks that P2P datasets can be reviewed as expected", () => {
+  before(function () {
     cy.fixture("CompanyInformationWithP2pPreparedFixtures").then(function (jsonContent) {
       const preparedP2pFixtures = jsonContent as Array<FixtureData<PathwaysToParisData>>;
-      const p2pFixtureData = getPreparedFixture("P2p-date-2023-04-18", preparedP2pFixtures);
-      const p2pDataId = "p2pTestDataId";
-      createQaDataObjectAndPerformRequestCheck(p2pFixtureData, p2pDataId, DataTypeEnum.P2p);
+      p2pFixtureForTest = getPreparedFixture("P2p-date-2023-04-18", preparedP2pFixtures);
+      cy.fixture("MetaInfoDataForCompany.json").then((metaInfos: Array<DataMetaInformation>) => {
+        mockDataMetaInfoForActiveDataset = metaInfos[0];
+      });
     });
   });
 
-  it("Checks that SME datasets can be reviewed as expected", () => {
-    cy.fixture("CompanyInformationWithSmePreparedFixtures").then(function (jsonContent) {
-      const preparedSmeFixtures = jsonContent as Array<FixtureData<SmeData>>;
-      const smeFixtureData = getPreparedFixture("SME-year-2023", preparedSmeFixtures);
-      const smeDataId = "smeTestDataId";
-      createQaDataObjectAndPerformRequestCheck(smeFixtureData, smeDataId, DataTypeEnum.Sme);
+  const keycloakMockWithUploaderAndReviewerRoles = minimalKeycloakMock({
+    roles: ["ROLE_USER", "ROLE_REVIEWER"],
+  });
+  const mockDataMetaInfoForP2pTestDataset = {
+    dataId: "p2pTestDataId",
+    companyId: "testCompanyId",
+    dataType: DataTypeEnum.P2p,
+    uploadTime: 1672531200,
+    reportingPeriod: "2023",
+    currentlyActive: false,
+    qaStatus: QaStatus.Pending,
+  } as DataMetaInformation;
+
+  it("Mock a pending P2P-dataset to check if datasets appear on the QA-overview-page", () => {
+    cy.intercept("**/qa/datasets", [mockDataMetaInfoForP2pTestDataset.dataId]);
+    cy.intercept(`**/api/metadata/${mockDataMetaInfoForP2pTestDataset.dataId}`, mockDataMetaInfoForP2pTestDataset);
+    cy.intercept(`**/api/companies/${mockDataMetaInfoForP2pTestDataset.companyId}`, {
+      companyId: mockDataMetaInfoForP2pTestDataset.companyId,
+      companyInformation: p2pFixtureForTest.companyInformation,
+      dataRegisteredByDataland: [mockDataMetaInfoForP2pTestDataset],
+    });
+    cy.mountWithPlugins<typeof QualityAssurance>(QualityAssurance, {
+      keycloak: keycloakMockWithUploaderAndReviewerRoles,
+    });
+    cy.contains("td", `${mockDataMetaInfoForP2pTestDataset.dataId}`);
+  });
+
+  it.only("Mock a pending P2P-dataset to check if dataset can be reviewed", () => {
+    // TODO remove "Only" tag
+    cy.intercept(`**/api/metadata?companyId=${mockDataMetaInfoForP2pTestDataset.companyId}`, [
+      mockDataMetaInfoForActiveDataset,
+    ]);
+    cy.intercept(`**/api/companies/${mockDataMetaInfoForP2pTestDataset.companyId}`, {
+      companyId: mockDataMetaInfoForP2pTestDataset.companyId,
+      companyInformation: p2pFixtureForTest.companyInformation,
+      dataRegisteredByDataland: [mockDataMetaInfoForActiveDataset, mockDataMetaInfoForP2pTestDataset],
+    });
+    cy.intercept(`**/api/metadata/${mockDataMetaInfoForP2pTestDataset.dataId}`, mockDataMetaInfoForP2pTestDataset);
+    cy.intercept(`**/api/data/${DataTypeEnum.P2p}/${mockDataMetaInfoForP2pTestDataset.dataId}`, {
+      companyId: mockDataMetaInfoForP2pTestDataset.companyId,
+      reportingPeriod: mockDataMetaInfoForP2pTestDataset.reportingPeriod,
+      data: p2pFixtureForTest.t,
+    }).as("fetchP2pData");
+
+    cy.mountWithPlugins<typeof ViewFrameworkData>(ViewFrameworkData, {
+      keycloak: keycloakMockWithUploaderAndReviewerRoles,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      props: {
+        companyId: mockDataMetaInfoForP2pTestDataset.companyId,
+        dataType: DataTypeEnum.P2p,
+        dataId: mockDataMetaInfoForP2pTestDataset.dataId,
+      },
+    }).then(() => {
+      // cy.get('#framework_data_search_bar_standard').should('not.exist');
+      cy.get("#chooseFrameworkDropdown").should("not.exist");
+      // cy.get('a[data-test="gotoNewDatasetButton"]')
+      //   .should('not.exist');
+
+      cy.wait(90000);
+
+      // TODO test absence of hidden stuff (search bar, dropdowns...)
+      // TODO check existence of expected stuff (pending-dataset-info, REJECT and APPROVE button)
+      // TODO test that REJECT and APPROVE buttons stick while scrolling  =>  First test if cypress can actually assert this
+      // TODO click "REJECT" and spy on the correct and expected API-call
+      // TODO revisit the page and this time click on "APPROVE" and spy on the correct and expected API-call
     });
   });
 });
