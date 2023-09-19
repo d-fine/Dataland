1d9b3424c563f0cfae67aac31ed8111336acf781
Piotr Skonieczny <piotr.skonieczny@pwc.com>
2023-09-19 08:40:09 +0200

    Minor description change

--- dataland-frontend/src/components/general/ModalsComponent.vue
+++ dataland-frontend/src/components/general/ModalsComponent.vue
@@ -64,7 +64,7 @@ export function findModalComponent(componentName: string): FieldTriggerAndDataCo
 }
 /**
  * @param componentName name of component to be checked
- * @returns whether passed component is a modal
+ * @returns boolean whether passed component is a modal
  */
 export function isModal(componentName: string): boolean {
   return !!findModalComponent(componentName);
