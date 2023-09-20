d3998e5e5409f320c1ee9a5a6d0310f5265eec47
swaldow <stephan.waldow@d-fine.de>
2023-09-20 18:13:38 +0200

    corrects typo

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/DataPointBaseInformation.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/DataPointBaseInformation.kt
@@ -6,10 +6,22 @@ import org.dataland.datalandbackend.model.enums.data.QualityOptions
  * --- API model ---
  * Fields of a generic basic data point with the minimal necessary information
  */
+interface DataPointBaseInformationInterface {
+    val quality: QualityOptions
+
+    val dataSource: CompanyReportReference?
+
+    val comment: String?
+}
+
+/**
+ * --- API model ---
+ * Fields of a generic basic data point with the minimal necessary information
+ */
 open class DataPointBaseInformation(
-    open val quality: QualityOptions,
+    override val quality: QualityOptions,
 
-    open val dataSource: CompanyReportReference? = null,
+    override val dataSource: CompanyReportReference? = null,
 
-    open val comment: String? = null,
-)
+    override val comment: String? = null,
+): DataPointBaseInformationInterface
