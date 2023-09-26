e5b420e244979e6136abdc47a650625acca0364e
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-09-26 16:53:35 +0200

    adjust AddressFixtures.ts

--- dataland-frontend/tests/e2e/fixtures/common/AddressFixtures.ts
+++ dataland-frontend/tests/e2e/fixtures/common/AddressFixtures.ts
@@ -1,22 +1,27 @@
 import { faker } from "@faker-js/faker";
 import { generateIso2CountryCode } from "@e2e/fixtures/common/CountryFixtures";
 import { type Address } from "@clients/backend";
-import { DEFAULT_PROBABILITY, valueOrUndefined } from "@e2e/utils/FakeFixtureUtils";
+import { DEFAULT_PROBABILITY, valueOrMissing } from "@e2e/utils/FakeFixtureUtils";
 
 /**
  * Generates a random address
- * @param undefinedProbability the probability (as number between 0 and 1) for "undefined" values in nullable fields
+ * @param missingValueProbability the probability (as number between 0 and 1) for missing values in optional fields
+ * @param setMissingValuesToNull
  * @returns a random address
  */
-export function generateAddress(undefinedProbability = DEFAULT_PROBABILITY): Address {
+export function generateAddress(
+  missingValueProbability = DEFAULT_PROBABILITY,
+  setMissingValuesToNull: boolean,
+): Address {
   return {
-    streetAndHouseNumber: valueOrUndefined(
+    streetAndHouseNumber: valueOrMissing(
       faker.location.street() + " " + faker.location.buildingNumber(),
-      undefinedProbability,
+      missingValueProbability,
+      setMissingValuesToNull,
     ),
-    postalCode: valueOrUndefined(faker.location.zipCode(), undefinedProbability),
+    postalCode: valueOrMissing(faker.location.zipCode(), missingValueProbability, setMissingValuesToNull),
     city: faker.location.city(),
-    state: valueOrUndefined(faker.location.state(), undefinedProbability),
+    state: valueOrMissing(faker.location.state(), missingValueProbability, setMissingValuesToNull),
     country: generateIso2CountryCode(),
   };
 }
