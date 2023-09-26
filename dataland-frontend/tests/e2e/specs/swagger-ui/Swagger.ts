1480036711a2b0def807e5bc3eaff22c9c394340
Florian Soffel <florian.soffel@d-fine.de>
2023-09-26 13:37:39 +0200

    see if test does only fail locally

--- dataland-frontend/tests/e2e/specs/swagger-ui/Swagger.ts
+++ dataland-frontend/tests/e2e/specs/swagger-ui/Swagger.ts
@@ -1,4 +1,7 @@
-import { getBaseUrl } from "@e2e/utils/Cypress";
+import {admin_name, admin_pw, getBaseUrl} from "@e2e/utils/Cypress";
+import Chainable = Cypress.Chainable;
+import {generateDummyCompanyInformation, uploadCompanyViaApi} from "@e2e/utils/CompanyUpload";
+import {getKeycloakToken} from "@e2e/utils/Auth";
 
 describe("As a user, I want to be able to use the swagger UI to send requests to the backend", () => {
   it("Checks that one is able to open swagger and send a request", () => {
@@ -21,4 +24,52 @@ describe("As a user, I want to be able to use the swagger UI to send requests to
       .url()
       .should("eq", getBaseUrl() + "/nocontent");
   });
+
+  it.only("Checks that POST requests of the individual frameworks work with the default values", async () => {
+    getKeycloakToken(admin_name, admin_pw).then((token) => {
+      // console.log("XXXX")
+      // cy.log("XXXX")
+      return uploadCompanyViaApi(token, generateDummyCompanyInformation(`Swagger Corp. ${new Date().getTime()}`))
+    })
+  //     .then((company) => {
+  //     console.log(company.companyId)
+  //     cy.log("BBBB")
+  //     cy.log(company.companyId)
+  //     cy.ensureLoggedIn(admin_name, admin_pw);
+  //     cy.visit("/api/swagger-ui/index.html");
+  //
+  //     cy.get("button.authorize").click();
+  //     cy.intercept("**/keycloak/realms/datalandsecurity/protocol/openid-connect/token*").as("authorize")
+  //     cy.get(".dialog-ux .auth-container button.authorize").last().click()
+  //     cy.wait("@authorize")
+  //     cy.get(".dialog-ux button.close-modal").click()
+  //     cy.log("ASDF")
+  //   });
+  //
+  //   // const postCompanyRequestId = "#operations-company-data-controller-postCompany";
+  //   // toggleRequestPanel(postCompanyRequestId)
+  //   // tryItOut(postCompanyRequestId)
+  //   // // TODO randomize lei
+  //   // executeRequest(postCompanyRequestId)
+  //   // validateSuccess(postCompanyRequestId)
+  //   // // TODO get company id
+  //   //
+  //   // // TODO for each framework upload default
+  });
+
+  function toggleRequestPanel(requestId: string): void {
+    cy.get(requestId).find(".opblock-summary-control").click();
+  }
+
+  function tryItOut(requestId: string): void {
+    cy.get(requestId).find("button.try-out__btn").click();
+  }
+
+  function executeRequest(requestId: string): void {
+    cy.get(requestId).find("button.execute").click();
+  }
+
+  function validateSuccess(requestId: string): void {
+    cy.get(requestId).find(".live-responses-table tbody td").first().should("contain.text", "200");
+  }
 });
