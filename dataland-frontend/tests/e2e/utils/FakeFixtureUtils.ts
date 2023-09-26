815e9d55c59ff3d2ac44d29a861bc929964d2414
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-09-26 17:05:07 +0200

    adjust FakeFixtureUtils.ts

--- dataland-frontend/tests/e2e/utils/FakeFixtureUtils.ts
+++ dataland-frontend/tests/e2e/utils/FakeFixtureUtils.ts
@@ -15,58 +15,91 @@ export const DEFAULT_PROBABILITY = 0.2;
 /**
  * Randomly returns the specified value or undefined
  * @param value the value to return
- * @param undefinedProbability the probability (as number between 0 and 1) for "undefined" values in nullable fields
+ * @param undefinedProbability the probability (as number between 0 and 1) for "undefined" values in optional fields
  * @returns the value or undefined
  */
 export function valueOrUndefined<T>(value: T, undefinedProbability = DEFAULT_PROBABILITY): T | undefined {
   return Math.random() > undefinedProbability ? value : undefined;
 }
 
+/**
+ * Randomly returns the specified value or null
+ * @param value the value to return
+ * @param nullProbability the probability (as number between 0 and 1) for "null" values in optional fields
+ * @returns the value or null
+ */
+export function valueOrNull<T>(value: T, nullProbability = DEFAULT_PROBABILITY): T | null {
+  return Math.random() > nullProbability ? value : null;
+}
+
+/**
+ * Randomly returns the specified value or a missing value, which can be represented by "null" or "undefined".
+ * @param value the value to (potentially) return
+ * @param missingValueProbability the probability (as number between 0 and 1) for a missing value in optional fields
+ * @param setMissingValuesToNull sets missing values to "null" if set to "true", else to "undefined"
+ * @returns the value or null or undefined
+ */
+export function valueOrMissing<T>(
+  value: T,
+  missingValueProbability: number,
+  setMissingValuesToNull = false,
+): T | undefined | null {
+  return setMissingValuesToNull
+    ? valueOrNull(value, missingValueProbability)
+    : valueOrUndefined(value, missingValueProbability);
+}
+
 export class Generator {
-  undefinedProbability: number;
+  missingValueProbability: number;
+  setMissingValuesToNull: boolean;
   reports: ReferencedDocuments;
   documents: ReferencedDocuments;
 
-  constructor(undefinedProbability = DEFAULT_PROBABILITY) {
-    this.undefinedProbability = undefinedProbability;
-    this.reports = generateReferencedReports(undefinedProbability);
+  constructor(undefinedProbability = DEFAULT_PROBABILITY, setMissingValuesToNull = false) {
+    // TODO renaming
+    this.missingValueProbability = undefinedProbability;
+    this.setMissingValuesToNull = setMissingValuesToNull;
+    this.reports = generateReferencedReports(undefinedProbability, setMissingValuesToNull);
     this.documents = generateReferencedDocuments();
   }
 
-  valueOrUndefined<T>(value: T): T | undefined {
-    return valueOrUndefined(value, this.undefinedProbability);
+  valueOrMissing<T>(value: T): T | undefined | null {
+    return this.setMissingValuesToNull
+      ? valueOrNull(value, this.missingValueProbability)
+      : valueOrUndefined(value, this.missingValueProbability);
   }
 
-  randomYesNo(): YesNo | undefined {
-    return this.valueOrUndefined(generateYesNo());
+  randomYesNo(): YesNo | undefined | null {
+    return this.valueOrMissing(generateYesNo());
   }
 
-  randomYesNoNa(): YesNoNa | undefined {
-    return this.valueOrUndefined(generateYesNoNa());
+  randomYesNoNa(): YesNoNa | undefined | null {
+    return this.valueOrMissing(generateYesNoNa());
   }
 
-  randomPercentageValue(): number | undefined {
-    return this.valueOrUndefined(generatePercentageValue());
+  randomPercentageValue(): number | undefined | null {
+    return this.valueOrMissing(generatePercentageValue());
   }
 
-  randomInt(max = 10000): number | undefined {
-    return this.valueOrUndefined(generateInt(max));
+  randomInt(max = 10000): number | undefined | null {
+    return this.valueOrMissing(generateInt(max));
   }
 
-  randomCurrencyValue(): number | undefined {
-    return this.valueOrUndefined(generateCurrencyValue());
+  randomCurrencyValue(): number | undefined | null {
+    return this.valueOrMissing(generateCurrencyValue());
   }
 
-  randomBaseDataPoint<T>(input: T): GenericBaseDataPoint<T> | undefined {
-    const document = this.valueOrUndefined(pickOneElement(Object.values(this.documents)));
-    return this.valueOrUndefined({ value: input, dataSource: document } as GenericBaseDataPoint<T>);
+  randomBaseDataPoint<T>(input: T): GenericBaseDataPoint<T> | undefined | null {
+    const randomDocument = this.valueOrMissing(pickOneElement(Object.values(this.documents)));
+    return this.valueOrMissing({ value: input, dataSource: randomDocument });
   }
 
-  randomDataPoint<T>(input: T, unit?: string): GenericDataPoint<T> | undefined {
-    return this.valueOrUndefined(generateDatapoint(this.valueOrUndefined(input), this.reports, unit));
+  randomDataPoint<T>(input: T, unit?: string): GenericDataPoint<T> | undefined | null {
+    const randomInput = this.valueOrMissing(input);
+    return this.valueOrMissing(generateDatapoint(randomInput, this.reports, unit));
   }
 
-  randomArray<T>(generator: () => T, min = 0, max = 5): T[] | undefined {
-    return this.valueOrUndefined(generateArray(generator, min, max));
+  randomArray<T>(generator: () => T, min = 0, max = 5): T[] | undefined | null {
+    return this.valueOrMissing(generateArray(generator, min, max));
   }
 }
