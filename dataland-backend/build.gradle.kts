e9a1d465e48646c7f5527555597fe23e2636ecbb
swaldow <stephan.waldow@d-fine.de>
2023-09-29 16:13:16 +0200

    reverts changes to the realestate classes in p2p

--- dataland-backend/build.gradle.kts
+++ dataland-backend/build.gradle.kts
@@ -64,7 +64,7 @@ openApi {
     customBootRun {
         args.set(listOf("--spring.profiles.active=nodb", "--server.port=8482"))
     }
-    outputFileName.set("$projectDir/backendOpenApi.json")
+    outputFileName.set("$projectDir/backendOpenAp755i.json")
     waitTimeInSeconds.set(openApiGeneratorTimeOutThresholdInSeconds.toInt())
 }
 
