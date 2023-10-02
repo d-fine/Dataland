ec6793e1f5b58c1cc6c176b4f5bb3c02a6b084f3
Aliabdoli77 <aliabdoli77@yahoo.com>
2023-10-02 09:19:17 +0200

    linting

--- dataland-frontend/tests/e2e/utils/LksgUpload.ts
+++ dataland-frontend/tests/e2e/utils/LksgUpload.ts
@@ -62,11 +62,11 @@ function selectANaceCode(fieldName: string): void {
   cy.get(`div[data-test='${fieldName}'] input`).click();
 
   cy.get(".p-treenode-label")
-      .contains("A - AGRICULTURE, FORESTRY AND FISHING")
-      .parents(".p-treenode-label")
-      .last()
-      .find("div.p-checkbox-box")
-      .should('not.exist');
+    .contains("A - AGRICULTURE, FORESTRY AND FISHING")
+    .parents(".p-treenode-label")
+    .last()
+    .find("div.p-checkbox-box")
+    .should("not.exist");
 
   cy.get("button.p-tree-toggler.p-link").eq(0).click();
   cy.get("button.p-tree-toggler.p-link").eq(1).click();
