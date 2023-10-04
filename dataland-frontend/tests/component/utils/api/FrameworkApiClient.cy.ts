5e4baa1a0e22d696d4f5daeff0f1b708188e643c
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-10-04 15:17:51 +0200

    Implemented unit test for Unified Api Clients

--- dataland-frontend/tests/component/utils/api/FrameworkApiClient.cy.ts
+++ dataland-frontend/tests/component/utils/api/FrameworkApiClient.cy.ts
@@ -1,21 +1,27 @@
-import {Configuration, DataTypeEnum} from "@clients/backend";
-import {getUnifiedFrameworkDataControllerFromConfiguration} from "@/utils/api/FrameworkApiClient";
+import { Configuration, DataTypeEnum } from "@clients/backend";
+import { getUnifiedFrameworkDataControllerFromConfiguration } from "@/utils/api/FrameworkApiClient";
 
 describe("It should be possible to construct Unified API Clients for every framework", () => {
-   for (const frameworkIdentifier of Object.keys(DataTypeEnum)) {
-      const frameworkDataType = DataTypeEnum[frameworkIdentifier as keyof typeof DataTypeEnum];
+  for (const frameworkIdentifier of Object.keys(DataTypeEnum)) {
+    const frameworkDataType = DataTypeEnum[frameworkIdentifier as keyof typeof DataTypeEnum];
 
-      it(`Should be possible to construct Unified API Clients for the ${frameworkIdentifier} framework`, async () => {
-         const frameworkApiClient = getUnifiedFrameworkDataControllerFromConfiguration(frameworkDataType, new Configuration());
+    it(`Should be possible to construct Unified API Clients for the ${frameworkIdentifier} framework and request data using them`, () => {
+      const frameworkApiClient = getUnifiedFrameworkDataControllerFromConfiguration(
+        frameworkDataType,
+        new Configuration(),
+      );
 
-         cy.intercept({
-            method: "GET",
-            url: `/api/data/${frameworkDataType}/test-data-id`
-         }, {}).as("api-client-test-request")
+      cy.intercept(
+        {
+          method: "GET",
+          url: `/api/data/${frameworkDataType}/test-data-id`,
+        },
+        {},
+      ).as("api-client-test-request");
 
-         void await frameworkApiClient.getFrameworkData("test-data-id")
+      void frameworkApiClient.getFrameworkData("test-data-id");
 
-         cy.wait("@api-client-test-request")
-      });
-   }
+      cy.wait("@api-client-test-request");
+    });
+  }
 });
