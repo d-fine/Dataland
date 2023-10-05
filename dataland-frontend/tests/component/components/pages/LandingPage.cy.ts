563173579d7747896b5318b4006c557f0aa65415
Florian Soffel <florian.soffel@d-fine.de>
2023-10-05 12:35:42 +0200

    Component test frame

---
+++ dataland-frontend/tests/component/components/pages/LandingPage.cy.ts
@@ -0,0 +1,90 @@
+import { checkButton, checkImage, checkLinkByName, checkLinkByTarget } from "@ct/testUtils/ExistenceChecks";
+import NewLandingPage from "@/components/pages/NewLandingPage.vue";
+import { minimalKeycloakMock } from "@ct/testUtils/Keycloak";
+
+describe("Component test for the landing page", () => {
+  it("Check if essential elements are present", () => {
+    cy.mountWithPlugins(NewLandingPage, {
+      keycloak: minimalKeycloakMock({}),
+    });
+
+    validateTopBar();
+    validateIntroSection();
+    validateBrandsSection();
+    checkLinkByTarget("/mission", "OUR MISSION");
+    // TODO slide show test
+
+    // TODO test targets
+    cy.get("button.campaigns__button").should("have.length", 4);
+
+    checkNewFooter();
+
+    // TODO check redirecting buttons that work for redirection
+    // TODO unfunctional buttons for staying on the page
+  });
+});
+
+/**
+ * Validates the elements of the top bar
+ */
+function validateTopBar(): void {
+  /**
+   * Gets the top bar element
+   * @returns the top bar element
+   */
+  function getTopBar(): Cypress.Chainable {
+    return cy.get("header");
+  }
+  checkImage("Dataland banner logo", "gfx_logo_dataland_orange_S.svg", getTopBar());
+  // TODO validate targets after clicking
+  checkButton("signup_dataland_button", "Sign Up", getTopBar());
+  checkLinkByName("login_dataland_button", "Login", getTopBar());
+  checkLinkByTarget("/mission", "Mission", getTopBar());
+  checkLinkByTarget("/community", "Community", getTopBar());
+  checkLinkByTarget("/campaigns", "Campaigns", getTopBar());
+}
+
+/**
+ * Validates the elements of the intro section
+ */
+function validateIntroSection(): void {
+  const title1 = "Liberate Data";
+  const title2 = "Empower Autonomy. Break Monopolies.";
+  checkImage(`${title1} -  ${title2}`, "gfx_logo_d_orange_S.svg");
+  cy.get("h1").should("contain.text", title1);
+  cy.get("h1").should("contain.text", title2);
+  // TODO check searchbar and button
+}
+
+/**
+ * Validates the images of the brands trusting in dataland
+ */
+function validateBrandsSection(): void {
+  checkImage("Brand 1", "img_chom_capital.png");
+  checkImage("Brand 2", "img_envoria.png");
+  checkImage("Brand 3", "img_fuerstl_bank.png");
+  checkImage("Brand 4", "img_ampega.png");
+  checkImage("Brand 5", "img_metzler.png");
+  checkImage("Brand 6", "img_deka.png");
+}
+
+/**
+ * Check the new footer
+ */
+function checkNewFooter(): void {
+  /**
+   * Gets the footer element
+   * @returns the footer element
+   */
+  function getFooter(): Cypress.Chainable {
+    return cy.get("footer");
+  }
+  getFooter().should("exist");
+  checkImage("Copyright ©   Dataland", "gfx_logo_dataland_orange_S.svg", getFooter());
+  cy.get(".footer__copyright").should("contain.text", "Copyright © 2023 Dataland");
+
+  // TODO check targets
+  checkLinkByTarget("/legal", "Legal", getFooter());
+  checkLinkByTarget("/imprint", "Imprint", getFooter());
+  checkLinkByTarget("/dataprivacy", "Data Privacy", getFooter());
+}
--- dataland-frontend/tests/component/testUtils/ExistenceChecks.ts
+++ dataland-frontend/tests/component/testUtils/ExistenceChecks.ts
@@ -2,9 +2,11 @@
  * Checks if an image is present
  * @param name the "alt" identifier of the image
  * @param fileName the file the image is expected to display
+ * @param parentElement the element whose content to search for the image
  */
-export function checkImage(name: string, fileName: string): void {
-  cy.get(`img[alt="${name}"]`)
+export function checkImage(name: string, fileName: string, parentElement: Cypress.Chainable = cy.get("body")): void {
+  parentElement
+    .find(`img[alt="${name}"]`)
     .should("be.visible")
     .should("have.attr", "src")
     .should("match", new RegExp(`.*/${fileName}$`));
@@ -14,7 +16,38 @@ export function checkImage(name: string, fileName: string): void {
  * Checks if a button is present
  * @param name the "name" identifier of the button
  * @param message the string expected to be contained
+ * @param parentElement the element whose content to search for the button
  */
-export function checkButton(name: string, message: string): void {
-  cy.get(`button[name=${name}]`).should("be.visible").should("contain.text", message);
+export function checkButton(name: string, message: string, parentElement: Cypress.Chainable = cy.get("body")): void {
+  parentElement.find(`button[name="${name}"]`).should("be.visible").should("contain.text", message);
+}
+
+/**
+ * Checks if a link is present
+ * @param name the "name" identifier of the button
+ * @param message the string expected to be contained
+ * @param parentElement the element whose content to search for the link
+ * @returns the chainable on the link element
+ */
+export function checkLinkByName(
+  name: string,
+  message: string,
+  parentElement: Cypress.Chainable = cy.get("body"),
+): Cypress.Chainable {
+  return parentElement.find(`a[name="${name}"]`).should("be.visible").should("contain.text", message);
+}
+
+/**
+ * Checks if a link is present
+ * @param href the "href" identifier of the button
+ * @param message the string expected to be contained
+ * @param parentElement the element whose content to search for the link
+ * @returns the chainable on the link element
+ */
+export function checkLinkByTarget(
+  href: string,
+  message: string,
+  parentElement: Cypress.Chainable = cy.get("body"),
+): Cypress.Chainable {
+  return parentElement.find(`a[href="${href}"]`).should("be.visible").should("contain.text", message);
 }
