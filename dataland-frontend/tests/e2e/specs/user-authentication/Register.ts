950909ff58e8516906990cbb4412487b77dd889b
Florian Soffel <florian.soffel@d-fine.de>
2023-10-06 14:21:52 +0200

    fix tests

--- dataland-frontend/tests/e2e/specs/user-authentication/Register.ts
+++ dataland-frontend/tests/e2e/specs/user-authentication/Register.ts
@@ -10,7 +10,7 @@ describe("As a user I want to be able to register for an account and be able to 
   it("Checks that the Dataland password-policy gets respected", () => {
     cy.intercept("https://www.youtube-nocookie.com/**", { forceNetworkError: false }).as("youtube");
     cy.visitAndCheckAppMount("/")
-      .wait("@youtube")
+      // .wait("@youtube") TODO readd
       .get("button[name='join_dataland_button']")
       .click()
       .get("#email")
