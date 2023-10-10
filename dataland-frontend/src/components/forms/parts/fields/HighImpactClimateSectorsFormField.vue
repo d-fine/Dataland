d455c68b58ba3ae3686eb3a836817f61b8d46b08
marcinStanczyk <marcin.stanczyk@pwc.com>
2023-10-10 19:22:05 +0200

    styled component

--- dataland-frontend/src/components/forms/parts/fields/HighImpactClimateSectorsFormField.vue
+++ dataland-frontend/src/components/forms/parts/fields/HighImpactClimateSectorsFormField.vue
@@ -2,7 +2,7 @@
   <div class="form-field">
     <!-- TODO card around this -->
     <UploadFormHeader :label="label" :description="description" :is-required="required" />
-    <div :class="innerClass">
+    <div>
       <MultiSelect
         v-model="selections"
         :options="options"
@@ -10,23 +10,27 @@
         option-label="label"
         option-value="value"
         :show-toggle-all="false"
+        :class="innerClass"
       />
-      <div v-for="selection of selections" :key="selection">
-        <h4>{{ options.find((option) => option.value == selection).label }}</h4>
-        <UploadFormHeader
-          :label="`Sector ${selection} Energy Consumption`"
-          :description="`Total energy consumption for high impact climate sector ${selection}`"
-          :is-required="true"
-        />
-        <div class="next-to-each-other">
-          <FormKit
-            type="text"
-            :name="`sector${selection}EnergyConsumption`"
-            :placeholder="`Sector ${selection} Energy Consumption`"
-            validation-label="Energy Consumption"
-            validation="number|required"
+      <div class="grid2 mt-3">
+        <div v-for="selection of selections" :key="selection" class="bordered-box p-3 positionRelative">
+          <em @click="removeItem(selection)" class="material-icons gray-closeIcon">close</em>
+          <h4 class="gray-text fw-normal">{{ options.find((option) => option.value == selection).label }}</h4>
+          <UploadFormHeader
+            :label="`Sector ${selection} Energy Consumption`"
+            :description="`Total energy consumption for high impact climate sector ${selection}`"
+            :is-required="true"
           />
-          <span>in GWh</span>
+          <div class="next-to-each-other">
+            <FormKit
+              type="text"
+              :name="`sector${selection}EnergyConsumption`"
+              :placeholder="`Sector ${selection} Energy Consumption`"
+              validation-label="Energy Consumption"
+              validation="number|required"
+            />
+            <span class="middle-next-to-field">in GWh</span>
+          </div>
         </div>
       </div>
     </div>
@@ -51,6 +55,15 @@ export default defineComponent({
         .map((sector) => ({ label: sector.label, value: sector.key })),
       selections: [] as string[],
     };
+  },
+  methods: {
+    /**
+     * Unselected item from selections
+     * @param selection item to unselect
+     */
+    removeItem(selection: string) {
+      this.selections = this.selections.filter((el: string) => el !== selection);
+    },
   },
 });
 </script>
