5c73259f575077c30ac33bcaae7578d61ed5533b
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-10-11 15:19:43 +0200

    migration fix with recursive script

--- dataland-internal-storage/src/test/kotlin/db/migration/V11__MigrateRefactoredDataPointClassesEuTaxoFinancialsTest.kt
+++ dataland-internal-storage/src/test/kotlin/db/migration/V11__MigrateRefactoredDataPointClassesEuTaxoFinancialsTest.kt
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.Test
 class V11__MigrateRefactoredDataPointClassesEuTaxoFinancialsTest {
     @Test
     fun `test migration of refactored data point classes in Eu Taxonomy financials framework`() {
-        /**
-         TestUtils().testMigrationOfSingleDataset(
-         "V11/oldDatabaseEntryFromBackendResponse.json",
-         "V11/transformedDatabaseEntryFromBackendResponse.json",
-         V11__MigrateRefactoredDataPointClassesEuTaxoFinancials()::migrateRefactoredDataPointClasses,
-         )
-         TestUtils().testMigrationOfSingleDataset(
-         "V11/oldDatabaseEntryFromBackendResponse2.json",
-         "V11/transformedDatabaseEntryFromBackendResponse2.json",
-         V11__MigrateRefactoredDataPointClassesEuTaxoFinancials()::migrateRefactoredDataPointClasses,
-         ) */
+        TestUtils().testMigrationOfSingleDataset(
+            "V11/oldDatabaseEntryFromBackendResponse.json",
+            "V11/transformedDatabaseEntryFromBackendResponse.json",
+            V11__MigrateRefactoredDataPointClassesEuTaxoFinancials()::migrateRefactoredDataPointClasses,
+        )
+        TestUtils().testMigrationOfSingleDataset(
+            "V11/oldDatabaseEntryFromBackendResponse2.json",
+            "V11/transformedDatabaseEntryFromBackendResponse2.json",
+            V11__MigrateRefactoredDataPointClassesEuTaxoFinancials()::migrateRefactoredDataPointClasses,
+        )
         TestUtils().testMigrationOfSingleDataset(
             "V11/brokenJson2.json",
             "V11/transformedBrokenJson2.json",
