cf4e07daabe407891b8e9cba7f21bea902f8949d
Florian Soffel <florian.soffel@d-fine.de>
2023-10-16 09:06:48 +0200

    fix building images

--- build-utils/rebuild_frontend_test_image.sh
+++ build-utils/rebuild_frontend_test_image.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 ./build-utils/base_rebuild_single_docker_image.sh dataland_frontend_test ./dataland-frontend/DockerfileTest \
          ./dataland-frontend/ ./dataland-backend/backendOpenApi.json ./dataland-document-manager/documentManagerOpenApi.json \
          ./dataland-api-key-manager/apiKeyManagerOpenApi.json ./dataland-qa-service/qaServiceOpenApi.json \
-         ./build.gradle.kts ./gradle.properties ./settings.gradle.kts ./env
+         ./build.gradle.kts ./gradle.properties ./settings.gradle.kts ./.env
