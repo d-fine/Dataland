d9ebec1a08e4bf6ebe5b8d7855d57022d060323e
Florian Soffel <florian.soffel@d-fine.de>
2023-10-18 17:15:18 +0200

    fix test

--- dataland-frontend/tests/component/components/pages/LandingPage.cy.ts
+++ dataland-frontend/tests/component/components/pages/LandingPage.cy.ts
@@ -56,7 +56,7 @@ function validateIntroSection(): void {
  */
 function validateBrandsSection(): void {
   const images = getLandingPageSection("Brands").image;
-  expect(images?.length).to.eq(6);
+  expect(images?.length).to.eq(11);
   images!.forEach((image, index) => {
     const filename = image.split("/").slice(-1)[0];
     checkImage(`Brand ${index + 1}`, filename);
