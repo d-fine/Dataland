c28be14a003c0fb0bed4655525dfd3fafd28f8a4
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-10-20 10:57:09 +0200

    mobile warning page with proper warning

--- dataland-frontend/src/components/pages/MobileWarningPage.vue
+++ dataland-frontend/src/components/pages/MobileWarningPage.vue
@@ -1,26 +1,48 @@
 <template>
-  <TheHeader :landingPage="landingPage" />
   <main role="main">
-    <h1>hellohelloHIhello</h1>
+    <LandingLoginMobile />
   </main>
   <TheFooter :sections="landingPage?.sections" />
 </template>
 
 <script setup lang="ts">
-import TheHeader from "@/components/layout/TheHeader.vue";
 import TheFooter from "@/components/layout/TheFooter.vue";
 import contentData from "@/assets/content.json";
+import LandingLoginMobile from "@/components/resources/newLandingPage/LandingLoginMobile.vue";
 import type { Content, Page } from "@/types/ContentTypes";
 
 const content: Content = contentData;
 const landingPage: Page | undefined = content.pages.find((page) => page.url === "/");
 </script>
 
-<style lang="scss" scoped>
-main {
-  margin-top: 132px;
-  @media only screen and (max-width: $small) {
-    margin-top: 52px;
+<style scoped lang="scss">
+.d-text-register {
+  color: #5a4f36;
+}
+.mobile-logo {
+  text-align: center;
+  margin: 2rem;
+  width: 100%;
+}
+.center-divs-row-wrapper {
+  justify-content: center;
+  .center-divs-row {
+    display: flex;
+    text-align: left;
+    width: 80%;
+    justify-content: space-around;
+    max-width: 400px;
+    margin: 1rem;
+  }
+}
+.imgs-in-line {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 60px;
+  align-items: flex-end;
+  img {
+    margin: 0.5rem 2vw 0.5rem 0;
   }
 }
 </style>
