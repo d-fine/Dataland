ebb4f5e1f58187b9da2d23f46d8aff3a990316cd
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-10-23 10:26:12 +0200

    altering configure robots script

--- dataland-inbound-proxy/docker-entrypoint.d/03-configure_robots_file.sh
+++ dataland-inbound-proxy/docker-entrypoint.d/03-configure_robots_file.sh
@@ -10,13 +10,14 @@ echo "Deploying to server: $currentEnv"
 fileName="robots.txt"
 
 blockingText="User-agent: *\n Disallow: /"
-defaultText="User-agent: *\n Allow: / \n Disallow: /keycloak/ \n"
-
-#rm fileName
+defaultText="User-agent: *\n Allow: / \n Disallow: /keycloak/ \n test"
+cd ..
+rm fileName
 if [[ $currentEnv == $datalandServerDomain ]]; then
   echo "This should be dataland.com"
-  #printf defaultText > "$fileName"
+  printf defaultText > "$fileName"
 else
   echo "This should not be dataland.com"
-  #printf blockingText > "$fileName"
+  printf blockingText > "$fileName"
 fi
+
