357e5746e1f7b14db604b85ec9493d70bd4940b1
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-10-24 12:34:29 +0200

    attempt to make youtube request stable in tests

--- dataland-frontend/tests/e2e/specs/landing-page/LandingPage.ts
+++ dataland-frontend/tests/e2e/specs/landing-page/LandingPage.ts
@@ -1,6 +1,7 @@
 describe("Check that the Landing Page to work properly", () => {
   it("Check the links and buttons", () => {
-    cy.visitAndCheckAppMount("/");
+    cy.intercept({ url: "https://www.youtube.com/**" }, { forceNetworkError: false }).as("youtube");
+    cy.visitAndCheckAppMount("/").wait("@youtube");
 
     cy.get("a:contains('Login')").click();
     cy.url().should("include", "/keycloak/realms/datalandsecurity/protocol/openid-connect/auth");
