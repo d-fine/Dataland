515eae9dc7815b20376e72f295d38ceae4c44d7a
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-10-30 16:01:50 +0100

    opt in opt out switch

--- dataland-frontend/src/components/resources/newLandingPage/TheQuotes.vue
+++ dataland-frontend/src/components/resources/newLandingPage/TheQuotes.vue
@@ -14,12 +14,15 @@
     >
       <div v-for="(card, index) in cards" :key="index" role="listitem" class="quotes__slide">
         <div class="quotes__slide-videoContainer">
-          <div class="cookieconsent-optout-marketing">
-            Please
-            <a href="javascript:Cookiebot.renew()">accept marketing-cookies</a>
-            to watch this video.
-          </div>
           <iframe
+            class="cookieconsent-optout-marketing"
+            :src="'https://www.youtube-nocookie.com/embed/' + card.icon + '?rel=0'"
+            title="Youtube video player"
+            allowfullscreen
+            :class="{ 'quotes__slide-video--zoom-out': currentSlide !== index - 1, 'quotes__slide-video': true }"
+          ></iframe>
+          <iframe
+            class="cookieconsent-optin-marketing"
             :data-src="'https://www.youtube.com/embed/' + card.icon + '?rel=0'"
             data-cookieconsent="marketing"
             title="Youtube video player"
