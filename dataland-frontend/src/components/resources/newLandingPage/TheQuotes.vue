67b8dce94da8d59f57ac1af24ca416d1954bc1ff
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-10-30 11:29:14 +0100

    trying to remove switch

--- dataland-frontend/src/components/resources/newLandingPage/TheQuotes.vue
+++ dataland-frontend/src/components/resources/newLandingPage/TheQuotes.vue
@@ -20,9 +20,7 @@
             to watch this video.
           </div>
           <iframe
-            data-add-placeholder
             :data-src="'https://www.youtube.com/embed/' + card.icon + '?rel=0'"
-            data-cookieconsent="marketing"
             title="Youtube video player"
             allowfullscreen
             :class="{ 'quotes__slide-video--zoom-out': currentSlide !== index - 1, 'quotes__slide-video': true }"
