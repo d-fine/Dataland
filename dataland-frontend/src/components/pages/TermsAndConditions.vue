cc071e4b9ff2c10f10ce5fcc54cf390b2fbe46cc
Florian Soffel <florian.soffel@d-fine.de>
2023-11-01 12:19:53 +0100

    first part tnc and styling

--- dataland-frontend/src/components/pages/TermsAndConditions.vue
+++ dataland-frontend/src/components/pages/TermsAndConditions.vue
@@ -1,5 +1,137 @@
 <template>
-  <LegalPage> In Zukunft sind hier die Allgemeinen Geschäftsbedingungen zu finden. </LegalPage>
+  <LegalPage>
+    <h1>Allgemeine Bedingungen für die Teilnahme an Dataland</h1>
+    <h1>(„AGB-Dataland“)</h1>
+    <div>
+      <h2>Vorbemerkung</h2>
+      <p>
+        Die Dataland GmbH (nachfolgend: „Dataland“) betreibt eine Plattform (nachfolgend: „Plattform-Dataland“, um
+        unternehmensbezogene Daten, insbesondere in den Bereichen Umwelt („Environmental“), Soziales („Social“) und
+        Unternehmensführung („Governance“) zusammenzutragen und für ihre Nutzer (nachfolgend: „Daten-User“) verfügbar zu
+        machen (nachfolgend „Unternehmens-Daten“).
+      </p>
+      <p>
+        Die auf der Plattform-Dataland registrierten (natürlichen und juristischen) Personen (nachfolgend:
+        „Dataland-Teilnehmer“) können als sogenannte Daten-Sammler Unternehmens-Daten an Dataland übertragen. Das
+        Gleiche können Unternehmen mit den Unternehmens-Daten tun, die sie selbst betreffen (sog. „Daten-Owner“). Die
+        Unternehmens-Daten auf der Plattform-Dataland können von den Daten-Usern heruntergeladen und genutzt werden.
+      </p>
+      <p>
+        Ziel von Dataland ist es, eine allen offenstehende Wissensplattform zu schaffen, und die Unternehmens-Daten
+        allen Daten-Usern zur Nutzung zur Verfügung zu stellen. Dataland prüft die eingegebenen Unternehmens-Daten mit
+        automatisierten Tests. Darüber hinaus werden Stichproben mit Einzeldaten durchgeführt. Ansonsten erfolgt keine
+        inhaltliche Überprüfung der von den Daten-Sammlern oder den Daten-Ownern an die Plattform-Dataland übermittelten
+        und dort zur Verfügung gestellten Unternehmens-Daten.
+      </p>
+      <p>
+        Die folgenden Bedingungen regeln die allgemeine Registrierung als Dataland-Teilnehmer sowie die Registrierung
+        und Teilnahme an Dataland als Daten-User, als Daten-Sammler und als Daten-Owner.
+      </p>
+    </div>
+    <div>
+      <h2>
+        Die Registrierung auf der Plattform-Dataland
+      </h2>
+      <p>
+        Für die Teilnahme an Dataland ist es zunächst erforderlich, dass eine Registrierung als Dataland-Teilnehmer
+        erfolgt.
+      </p>
+      <p>
+        Zusätzlich zu der Registrierung als Dataland-Teilnehmer (jedoch nicht ohne diese) können Registrierungen als
+        Daten-Sammler, Daten-Owner und Daten-User erfolgen.
+      </p>
+      <p>
+        Voraussetzung für die Registrierung der (natürlichen oder juristischen) Person ist deren volle
+        Geschäftsfähigkeit.
+      </p>
+      <p>
+        Eine (natürliche oder juristische) Person darf sich nur als ein Dataland-Teilnehmer registrieren und basierend
+        auf dieser Registrierung jeweils nur eine Registrierung als Daten-Sammler, Daten-Owner und/oder Daten-User
+        vornehmen.
+      </p>
+      <p>
+        Eine Registrierung bei Dataland erfolgt zunächst in der Weise, dass die mit einem „*“ versehenen Pflichtfelder
+        vollständig und wahrheitsgemäß ausgefüllt werden. Zusätzlich können weitere (als optional gekennzeichnete)
+        Angaben gemacht werden.
+      </p>
+      <p>
+        Der Registrierende kann sich hierbei im Zuge der Registrierung selbst einen Login-Namen und ein Passwort geben.
+        Das Passwort ist geheim zu halten und darf nicht Dritten zur Verfügung gestellt werden. Soweit ein
+        Dataland-Teilnehmer davon Kenntnis erlangt, dass Dritte sein Passwort erfahren haben oder sich unberechtigt
+        unter dessen Login-Namen Zugang zu Dataland verschafft haben, ist Dataland unverzüglich hierüber zu informieren.
+      </p>
+      <p>
+        Nachdem die Pflichtfelder ausgefüllt und der entsprechende Antrag auf Registrierung durch Anklicken des Feldes
+        „Registrierung anfragen“ abgesendet wurde, überprüft Dataland den Antrag auf Registrierung. Nach erfolgter
+        Überprüfung und positiver Entscheidung über den Antrag auf Registrierung wird dem Antragsteller die
+        Registrierung als Dataland-Teilnehmer bzw. bei entsprechenden Anträgen als Daten-Sammler, Daten-Owner oder
+        Daten-User bestätigt.
+      </p>
+      <p>
+        Seitens Dataland besteht keine Pflicht, eine Registrierung zu akzeptieren und diese zu bestätigen.
+      </p>
+      <p>
+        Soweit sich nach einer Registrierung Umstände ändern, die die dort gemachten Angaben unrichtig werden lassen,
+        ist der Dataland-Teilnehmer verpflichtet, Dataland hierüber unverzüglich zu unterrichten, und zwar – soweit
+        möglich – durch Anpassung des persönlichen Profils des Dataland-Teilnehmers auf der Plattform-Dataland.
+      </p>
+      <p>
+        Unabhängig von sonstigen Kündigungsrechten ist Dataland berechtigt, die Teilnahme an Dataland und alle
+        Registrierungen mit sofortiger Wirkung zu kündigen oder zu widerrufen, wenn der Dataland-Teilnehmer bei der
+        Registrierung falsche Angaben gemacht hat, sich doppelt registriert hat oder gegen ihm obliegende
+        Vertragspflichten verstoßen hat.
+      </p>
+    </div>
+    <div>
+      <h2>
+        Für Dataland-Teilnehmer geltende Bedingungen:
+      </h2>
+      <div>
+        <h3>
+          Die Nutzungsrechte des Dataland-Teilnehmers
+        </h3>
+        <p>
+          Der Dataland-Teilnehmer erhält mit seiner Registrierung das Zugriffsrecht auf die Plattform-Dataland. Dies
+          umfasst insbesondere die reine Ansicht der Unternehmens-Daten auf der Website (reines Leserecht). Die
+          Unternehmens-Daten dürfen nicht kopiert oder heruntergeladen werden. Die Unternehmens-Daten und die daraus
+          gewonnenen Informationen dürfen nicht als Grundlage für Investitionsentscheidungen genutzt werden.
+        </p>
+        <p>
+          Als Dataland-Teilnehmer hat er die Möglichkeit gemäß den Maßgaben in § 1 Registrierungen als Daten-User,
+          Daten-Owner oder Daten-Sammler vorzunehmen.
+        </p>
+      </div>
+      <div>
+        <h3>
+          Die Vergütungspflichten des Dataland-Teilnehmers
+        </h3>
+        <p>
+          Die Registrierung als Dataland-Teilnehmer und die damit verbundene Nutzung der Plattform-Dataland als
+          Dataland-Teilnehmer sind kostenlos, unabhängig davon, ob es sich um eine Privatnutzung oder um eine
+          gewerbliche Nutzung handelt.
+        </p>
+      </div>
+      <div>
+        <h3>
+          Laufzeit der Registrierung als Dataland-Teilnehmer
+        </h3>
+        <p>
+          Die Registrierung als Dataland-Teilnehmer erfolgt für eine unbestimmte Laufzeit.
+        </p>
+        <p>
+          Die Registrierung kann von jeder Partei mit einer Frist von einem Monat zum Ende eines Kalendermonats
+          gekündigt werden.
+        </p>
+        <p>
+          Das Recht zur Kündigung aus wichtigem Grund bleibt in jedem Fall unberührt.
+        </p>
+        <p>
+          Mit dem Ende der Registrierung als Dataland-Teilnehmer enden automatisch auch alle weiteren Registrierungen
+          des jeweiligen Dataland-Teilnehmers.
+        </p>
+      </div>
+    </div>
+  </LegalPage>
 </template>
 
 <script lang="ts">
@@ -7,6 +139,43 @@ import LegalPage from "@/components/generics/LegalPage.vue";
 
 export default {
   name: "TermsAndConditions",
-  components: { LegalPage },
+  components: {LegalPage},
 };
 </script>
+
+<style scoped>
+h1 {
+  counter-reset: section;
+}
+
+div:not(:first-of-type) h2::before {
+  counter-increment: section;
+  content: counter(section) ". ";
+}
+
+h2 {
+  counter-reset: subsection;
+}
+
+div:not(:first-of-type) h2 ~ p::before {
+  counter-increment: subsection;
+  content: counter(section) "." counter(subsection) ". ";
+  font-weight: bold;
+}
+
+h3::before {
+  counter-increment: subsection;
+  content: counter(section) "." counter(subsection) ". ";
+}
+
+h3 {
+  counter-reset: subsubsection;
+}
+
+h3 ~ p:not(:only-of-type)::before {
+  counter-increment: subsubsection;
+  content: counter(section) "." counter(subsection) "." counter(subsubsection) ". ";
+  font-weight: bold;
+}
+
+</style>
