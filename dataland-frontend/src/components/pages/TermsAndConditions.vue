91a26013cb58d510cdc30543ed72ac4a9effed6e
fsoffel <113936582+fsoffel@users.noreply.github.com>
2023-11-01 16:56:33 +0100

    Dala 343 legal pages (#335)
    * Adding Data Privacy page
    * Updating Imprint
    * Add placeholder for Terms and Conditions

---
+++ dataland-frontend/src/components/pages/TermsAndConditions.vue
@@ -0,0 +1,12 @@
+<template>
+  <LegalPage> In Zukunft sind hier die Allgemeinen Geschäftsbedingungen zu finden. </LegalPage>
+</template>
+
+<script lang="ts">
+import LegalPage from "@/components/generics/LegalPage.vue";
+
+export default {
+  name: "TermsAndConditions",
+  components: { LegalPage },
+};
+</script>
