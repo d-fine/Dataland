81cff44c3ca5722095f1ca9714184d92317f0f7f
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-11-03 08:33:46 +0100

    alternative code for test (maybe ci works)

--- dataland-frontend/tests/e2e/specs/landing-page/CookieBanner.ts
+++ dataland-frontend/tests/e2e/specs/landing-page/CookieBanner.ts
@@ -2,13 +2,17 @@ describe("As a user, I expect the cookie banner to render and to be functional",
   it("Check that the cookie banner renders", () => {
     cy.visitAndCheckAppMount("/");
 
-    cy.get("#CybotCookiebotDialog", { timeout: Cypress.env("short_time_in_ms") as number }).should("exist");
-    cy.get("#CybotCookiebotDialogPoweredbyImage").invoke("attr", "src").should("contain", "data:image/png;base64,");
-    cy.get("#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").should("exist");
-    cy.get("#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection").should("exist");
-    cy.get("#CybotCookiebotDialogBodyButtonDecline").should("exist");
-    cy.get("#CybotCookiebotDialogBodyFieldsetInnerContainer").should("exist");
-    cy.get("#CybotCookiebotDialogBodyContentTitle").should("exist").should("contain", "This website uses cookies");
+    cy.get("div[id='CybotCookiebotDialog']").should("exist");
+    cy.get("img[id='CybotCookiebotDialogPoweredbyImage']")
+      .invoke("attr", "src")
+      .should("contain", "data:image/png;base64,");
+    cy.get("button[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']").should("exist");
+    cy.get("button[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection']").should("exist");
+    cy.get("button[id='CybotCookiebotDialogBodyButtonDecline']").should("exist");
+    cy.get("div[id='CybotCookiebotDialogBodyFieldsetInnerContainer']").should("exist");
+    cy.get("h2[id='CybotCookiebotDialogBodyContentTitle']")
+      .should("exist")
+      .should("contain", "This website uses cookies");
 
     cy.get("#CybotCookiebotDialogBodyEdgeMoreDetailsLink")
       .should("exist")
