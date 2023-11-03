76798c0a496d80da3ab71ac1ecc83a40dadb860c
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-11-03 12:55:33 +0100

    remove cookie banner test from ci

--- dataland-frontend/tests/e2e/specs/landing-page/CookieBanner.ts
+++ dataland-frontend/tests/e2e/specs/landing-page/CookieBanner.ts
@@ -1,29 +1,39 @@
+import { describeIf } from "@e2e/support/TestUtility";
+
 describe("As a user, I expect the cookie banner to render and to be functional", () => {
-  it("Check that the cookie banner renders", () => {
-    cy.visitAndCheckAppMount("/");
+  describeIf(
+    "Do not execute these tests in the CI pipeline, because the github actions server is not registered on the cookiebot.com website",
+    {
+      executionEnvironments: ["developmentLocal", "developmentCd", "previewCd"],
+    },
+    () => {
+      it("Check that the cookie banner renders", () => {
+        cy.visitAndCheckAppMount("/");
 
-    cy.get("div[id='CybotCookiebotDialog']").should("exist");
-    cy.get("#CybotCookiebotDialogPoweredbyImage").invoke("attr", "src").should("contain", "data:image/png;base64,");
-    cy.get("button[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']").should("exist");
-    cy.get("button[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection']").should("exist");
-    cy.get("button[id='CybotCookiebotDialogBodyButtonDecline']").should("exist");
-    cy.get("div[id='CybotCookiebotDialogBodyFieldsetInnerContainer']").should("exist");
-    cy.get("h2[id='CybotCookiebotDialogBodyContentTitle']")
-      .should("exist")
-      .should("contain", "This website uses cookies");
+        cy.get("div[id='CybotCookiebotDialog']").should("exist");
+        cy.get("#CybotCookiebotDialogPoweredbyImage").invoke("attr", "src").should("contain", "data:image/png;base64,");
+        cy.get("button[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']").should("exist");
+        cy.get("button[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowallSelection']").should("exist");
+        cy.get("button[id='CybotCookiebotDialogBodyButtonDecline']").should("exist");
+        cy.get("div[id='CybotCookiebotDialogBodyFieldsetInnerContainer']").should("exist");
+        cy.get("h2[id='CybotCookiebotDialogBodyContentTitle']")
+          .should("exist")
+          .should("contain", "This website uses cookies");
 
-    cy.get("#CybotCookiebotDialogBodyEdgeMoreDetailsLink")
-      .should("exist")
-      .click()
-      .get("#CybotCookiebotDialogDetailBodyContentCookieContainerTypes")
-      .should("exist")
-      .get("li.CookieCard")
-      .should("have.length", 5)
-      .first()
-      .get("label")
-      .should("contain", "Necessary");
+        cy.get("#CybotCookiebotDialogBodyEdgeMoreDetailsLink")
+          .should("exist")
+          .click()
+          .get("#CybotCookiebotDialogDetailBodyContentCookieContainerTypes")
+          .should("exist")
+          .get("li.CookieCard")
+          .should("have.length", 5)
+          .first()
+          .get("label")
+          .should("contain", "Necessary");
 
-    cy.get("#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click();
-    cy.get("#CybotCookiebotDialog").should("not.be.visible");
-  });
+        cy.get("#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click();
+        cy.get("#CybotCookiebotDialog").should("not.be.visible");
+      });
+    },
+  );
 });
