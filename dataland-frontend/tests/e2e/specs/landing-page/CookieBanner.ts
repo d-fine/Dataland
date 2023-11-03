e2a5a9df698da1d74490e4b5e6dfd8e75d786f4b
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-11-03 15:41:55 +0100

    blank commit

--- dataland-frontend/tests/e2e/specs/landing-page/CookieBanner.ts
+++ dataland-frontend/tests/e2e/specs/landing-page/CookieBanner.ts
@@ -9,7 +9,7 @@ describe("As a user, I expect the cookie banner to render and to be functional",
     () => {
       it("Check that the cookie banner renders", () => {
         cy.visitAndCheckAppMount("/");
-
+        //blank
         cy.get("div[id='CybotCookiebotDialog']").should("exist");
         cy.get("#CybotCookiebotDialogPoweredbyImage").invoke("attr", "src").should("contain", "data:image/png;base64,");
         cy.get("button[id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']").should("exist");
