4210017898ee3ab76c7fcda8160340bd980acc73
marcinStanczyk <marcin.stanczyk@pwc.com>
2023-11-04 12:35:23 +0100

    all element for request form

--- dataland-frontend/src/components/pages/RequestData.vue
+++ dataland-frontend/src/components/pages/RequestData.vue
@@ -2,54 +2,38 @@
   <AuthenticationWrapper>
     <TheHeader />
     <TheContent class="paper-section no-ui-message">
-      <FormKit
-        :actions="false"
-        type="form"
-        @submit="submitRequest"
-        @submit-invalid="handleInvalidInput"
-        id="requestDataFormId"
-        name="requestDataFormName"
-      >
+      <FormKit :actions="false" type="form" @submit="submitRequest" id="requestDataFormId" name="requestDataFormName">
         <div class="grid p-8 uploadFormWrapper">
           <div class="col-12" v-if="postBulkDataRequestObjectProcessed">
-                        <SuccessMessage :message="message" :messageId="messageCounter" />
-                        <FailMessage :message="message" :messageId="messageCounter" />
-            <div >
-              <MessageComponent data-test="" severity="light-success">
-                <template #text-info
-                  >🎉 <span class="fw-semi-bold">All identifiers have been submitted successfully.</span></template
-                >
+            <div v-if="submittingSucceded">
+              <MessageComponent v-if="acceptedCompanyIdentifiers.length" data-test="" severity="light-success">
+                <template #left-icon>
+                  <em class="material-icons info-icon p-message-icon green-text">check_circle</em>
+                </template>
+                <template #text-info>
+                  <h4>Data request submitted succesfully.</h4>
+                  <p class="fw-semi-bold" v-if="!rejectedCompanyIdentifiers.length">
+                    All identifiers have been submitted successfully.
+                  </p>
+                  <p class="fw-semi-bold red-text" v-if="rejectedCompanyIdentifiers.length">
+                    However, some identifiers couldn’t be recognised.
+                  </p>
+                </template>
               </MessageComponent>
-            </div>
-
-            <div >
-              <MessageComponent data-test="" severity="light-success">
-                <template #text-info
-                  >🎉
-                  <span class="fw-semi-bold"
-                    >{{ acceptedCompanyIdentifiers.length }} out of {{ identifiers.length }} identifiers have been
-                    submitted successfully.</span
-                  ></template
-                >
-              </MessageComponent>
-              <MessageComponent data-test="" severity="light-error">
-                <template #text-info
-                  >😢
-                  <span class="fw-semi-bold"
-                    >{{ rejectedCompanyIdentifiers.length }} identifiers failed to be recognised:</span
-                  >
-                  <p class="m-4">
-                    <span v-for="it in rejectedCompanyIdentifiers" :key="it"> {{ it }}, </span>
+              <MessageComponent v-else data-test="" severity="light-error">
+                <template #left-icon>
+                  <em class="material-icons info-icon p-message-icon red-text">error</em>
+                </template>
+                <template #text-info>
+                  <h4>Data request couldn’t be submitted.</h4>
+                  <p class="fw-semi-bold">
+                    Check the format of the identifiers and try again. Accepted identifiers are: LEI, ISIN & permID.
+                    Expected in comma, semicolon, linebreaks and spaces separted format.
                   </p>
                 </template>
               </MessageComponent>
             </div>
-
-            <div >
-              <MessageComponent data-test="" severity="light-error">
-                <template #text-info>😢 None of the identifiers have been recognised.</template>
-              </MessageComponent>
-            </div>
+            <FailMessage v-else :message="message" :messageId="messageCounter" />
           </div>
 
           <div class="col-12" v-if="submittingSucceded">
@@ -59,8 +43,11 @@
                   <h4 class="p-0">Data Request Summary</h4>
                   <hr />
                 </div>
-                <div class="col-6">
-                  <h4>{{ selectedFrameworks.length ?? 0 }} Frameworks</h4>
+                <div class="col-4">
+                  <div class="next-to-each-other align-items-center">
+                    <em class="material-icons info-icon green-text">check_circle</em>
+                    <h4>{{ selectedFrameworks.length ?? 0 }} Frameworks</h4>
+                  </div>
                   <div class="paper-section radius-1 p-2 w-full selected-frameworks">
                     <span v-if="!selectedFrameworks.length" class="gray-text no-framework"
                       >No frameworks have been submitted.</span
@@ -70,13 +57,33 @@
                     </p>
                   </div>
                 </div>
-                <div class="col-6">
-                  <h4>{{ acceptedCompanyIdentifiers.length ?? 0 }} Identifiers</h4>
+                <div class="col-4">
+                  <div class="next-to-each-other align-items-center">
+                    <em class="material-icons info-icon green-text">check_circle</em>
+                    <h4>{{ acceptedCompanyIdentifiers.length ?? 0 }} Accepted Company Identifiers</h4>
+                  </div>
                   <div class="paper-section radius-1 p-2 w-full selected-frameworks">
-                    <span v-if="!selectedFrameworks.length" class="gray-text no-framework"
-                      >No identifiers have been submitted.</span
+                    <span v-if="!acceptedCompanyIdentifiers.length" class="gray-text no-framework"
+                      >No accepted identifiers have been submitted.</span
                     >
                     <span v-for="it in acceptedCompanyIdentifiers" :key="it"> {{ it }}, </span>
+                  </div>
+                </div>
+                <div class="col-4">
+                  <div class="next-to-each-other align-items-center">
+                    <em class="material-icons info-icon red-text">error</em>
+                    <h4 :class="rejectedCompanyIdentifiers.length ? 'red-text' : null">
+                      {{ rejectedCompanyIdentifiers.length ?? 0 }} Rejected Company Identifiers
+                    </h4>
+                  </div>
+                  <div
+                    class="paper-section radius-1 p-2 w-full selected-frameworks"
+                    :class="rejectedCompanyIdentifiers.length ? 'red-border' : null"
+                  >
+                    <span v-if="!rejectedCompanyIdentifiers.length" class="gray-text no-framework"
+                      >No rejected identifiers.</span
+                    >
+                    <span v-for="it in rejectedCompanyIdentifiers" :key="it"> {{ it }}, </span>
                   </div>
                 </div>
                 <div class="col-12 text-center">
@@ -89,7 +96,7 @@
                   </PrimeButton>
                   <br />
                   <PrimeButton
-                    type="submit"
+                    @click="goToCompanies"
                     label="Submit"
                     class="p-button p-button-text p-button-sm d-letters place-self-center ml-auto"
                     name="back_to_companies_button"
@@ -116,7 +123,7 @@
               </div>
               <div class="col-6">
                 <div class="bg-white radius-1 p-4">
-                  <h4 class="p-0">Please select the framework(s) for which you want to request data:</h4>
+                  <h4 class="p-0">Select at least one framework</h4>
                   <MultiSelectFormFieldBindData
                     label="Frameworks"
                     placeholder="Select framework"
@@ -165,8 +172,8 @@
                     placeholder="Insert identifiers here. Separated by either comma, space, semicolon or linebreak."
                   />
                   <span class="gray-text font-italic"
-                    >Accepted identifier types are: DUNS Number, LEI, ISIN & permID. Expected in comma separted
-                    format.</span
+                    >Accepted identifier types are: LEI, ISIN & permID. Expected in comma, semicolon, linebreaks and
+                    spaces separted format.</span
                   >
                 </div>
               </div>
@@ -185,7 +192,6 @@ import PrimeButton from "primevue/button";
 import { defineComponent, inject } from "vue";
 import type Keycloak from "keycloak-js";
 import { type DataTypeEnum } from "@clients/backend";
-import { type FrameworkSelectableItem } from "@/utils/FrameworkDataSearchDropDownFilterTypes";
 import { ARRAY_OF_FRAMEWORKS_WITH_VIEW_PAGE } from "@/utils/Constants";
 import TheContent from "@/components/generics/TheContent.vue";
 import TheHeader from "@/components/generics/TheHeader.vue";
@@ -194,19 +200,17 @@ import TheFooter from "@/components/generics/TheFooter.vue";
 import MultiSelectFormFieldBindData from "@/components/forms/parts/fields/MultiSelectFormFieldBindData.vue";
 import { assertDefined } from "@/utils/TypeScriptUtils";
 import { ApiClientProvider } from "@/services/ApiClients";
-import { formatAxiosErrorMessage } from "@/utils/AxiosErrorMessageFormatter";
 import { humanizeStringOrNumber } from "@/utils/StringHumanizer";
 import { type BulkDataRequest } from "@clients/communitymanager";
-import SuccessMessage from "@/components/messages/SuccessMessage.vue";
 import FailMessage from "@/components/messages/FailMessage.vue";
 import MessageComponent from "@/components/messages/MessageComponent.vue";
+import { AxiosError } from "axios";
 
 export default defineComponent({
   name: "RequestData",
   components: {
     MessageComponent,
     FailMessage,
-    SuccessMessage,
     MultiSelectFormFieldBindData,
     AuthenticationWrapper,
     TheHeader,
@@ -228,15 +232,12 @@ export default defineComponent({
       identifiersInString: "",
       identifiers: [] as Array<string>,
       messageCounter: 0,
-
       acceptedCompanyIdentifiers: [] as Array<string>,
       rejectedCompanyIdentifiers: [] as Array<string>,
-
       submittingSucceded: false,
       submittingInProgress: false,
-      postBulkDataRequestObjectProcessed: true,
+      postBulkDataRequestObjectProcessed: false,
       message: "",
-      //isFormFilledCorrect: false, TODO will adjust based on if the form is filled correctly (similar to upload page)
     };
   },
 
@@ -244,29 +245,6 @@ export default defineComponent({
     humanizedSelectedFrameworks(): string[] {
       return this.selectedFrameworks.map((it) => humanizeStringOrNumber(it));
     },
-
-    // selectedFrameworksInt: {
-    //   get(): Array<FrameworkSelectableItem> {
-    //     return this.availableFrameworks.filter((frameworkSelectableItem) =>
-    //       this.selectedFrameworks.includes(frameworkSelectableItem.frameworkDataType),
-    //     );
-    //   },
-    //   set() {
-    //     console.log("TODO");
-    //   },
-    // },
-
-    /*submissionProgressTitle() {
-      if (this.submissionFinished) {
-        if (this.isInviteSuccessful) {
-          return "Success";
-        } else {
-          return "Submission failed";
-        }
-      } else {
-        return "Submitting file";
-      }
-    },*/
   },
 
   methods: {
@@ -294,11 +272,6 @@ export default defineComponent({
       const uniqueIdentifiers = new Set(this.identifiersInString.replace(/(\r\n|\n|\r|;| )/gm, ",").split(","));
       uniqueIdentifiers.delete("");
       this.identifiers = [...uniqueIdentifiers];
-      console.log(this.identifiers);
-    },
-
-    handleInvalidInput() {
-      console.log("IVALID", this.postBulkDataRequestObjectProcessed);
     },
 
     /**
@@ -313,23 +286,22 @@ export default defineComponent({
         const requestDataControllerApi = await new ApiClientProvider(
           assertDefined(this.getKeycloakPromise)(),
         ).getRequestDataControllerApi();
-        // const response = await requestDataControllerApi.postBulkDataRequest(bulkDataRequestObject);
-        const response = {
-          message: "Message from response",
-          rejectedCompanyIdentifiers: ["reject-1", "reject-2", "reject-3"],
-          acceptedCompanyIdentifiers: ["accepted-1", "accepted-2", "accepted-55"],
-        };
-        console.log("response----------->", response);
+        const response = await requestDataControllerApi.postBulkDataRequest(bulkDataRequestObject);
 
         this.messageCounter++;
-        this.message = response.message;
-        this.rejectedCompanyIdentifiers = response.rejectedCompanyIdentifiers;
-        this.acceptedCompanyIdentifiers = response.acceptedCompanyIdentifiers;
+        this.message = response.data.message;
+        this.rejectedCompanyIdentifiers = response.data.rejectedCompanyIdentifiers;
+        this.acceptedCompanyIdentifiers = response.data.acceptedCompanyIdentifiers;
         this.submittingSucceded = true;
       } catch (error) {
         this.messageCounter++;
         console.error(error);
-        this.message = error.message;
+        if (error instanceof AxiosError) {
+          this.message = error.message;
+        } else {
+          this.message =
+            "An unexpected error occurred. Please try again or contact the support team if the issue persists.";
+        }
       } finally {
         this.submittingInProgress = false;
         this.postBulkDataRequestObjectProcessed = true;
@@ -353,29 +325,20 @@ export default defineComponent({
      */
     resetForm() {
       this.acceptedCompanyIdentifiers = [];
-          this.rejectedCompanyIdentifiers = [];
+      this.rejectedCompanyIdentifiers = [];
       this.selectedFrameworks = [];
       this.identifiersInString = "";
       this.identifiers = [];
+      this.postBulkDataRequestObjectProcessed = false;
       this.submittingSucceded = false;
     },
 
     /**
-     * Refreshes the page to allow the user to make a new data request
+     * Go to companies page
      */
-    /*createNewRequest() {
-      this.$router.go();
-    },*/
-
-    /**
-     * Called when the user hits submit. Enables the progress bar and uploads the file.
-     */
-    /*async handleSubmission() {
-      this.submissionInProgress = true;
-    // TODO: some POST request is sent
-      this.submissionFinished = true;
-      this.submissionInProgress = false;
-    },*/
+    goToCompanies() {
+      void this.$router.push("/companies");
+    },
   },
   mounted() {
     void this.retrieveAvailableFrameworks();
