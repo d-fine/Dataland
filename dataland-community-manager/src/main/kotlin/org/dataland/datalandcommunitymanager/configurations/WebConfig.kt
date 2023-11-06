e8124e026ba686ce34f0853e27474866ba23abfa
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-11-06 18:20:09 +0100

    ktlint + detekt

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/configurations/WebConfig.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/configurations/WebConfig.kt
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+/**
+ * This class is used to configure the web server started by Spring
+ */
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addFormatters(registry: FormatterRegistry) {
--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/converters/StringToDataTypeEnumConverter.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/converters/StringToDataTypeEnumConverter.kt
@@ -3,6 +3,9 @@ package org.dataland.datalandcommunitymanager.converters
 import org.dataland.datalandbackend.openApiClient.model.DataTypeEnum
 import org.springframework.core.convert.converter.Converter
 
+/**
+ * This class is used to define a converter to tell Spring how to convert strings into DataTypeEnums in requests
+ */
 class StringToDataTypeEnumConverter : Converter<String?, DataTypeEnum?> {
     override fun convert(inputString: String): DataTypeEnum {
         return DataTypeEnum.values().find { it.value == inputString }
