13cb9d3291015de51fdbd67bfd297de1ffeae386
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-11-07 16:26:32 +0100

    removing tests

--- dataland-frontend/tests/e2e/specs/landing-page/LandingPage.ts
+++ dataland-frontend/tests/e2e/specs/landing-page/LandingPage.ts
@@ -1,7 +1,4 @@
-import { describeIf } from "@e2e/support/TestUtility";
-
-// describe("Check that the Landing Page to work properly", () => {
-describe("Overall landing page tests", () => {
+describe("Check that the Landing Page to work properly", () => {
   it("Check the links and buttons", () => {
     cy.intercept({ url: "https://www.youtube.com/**" }, { forceNetworkError: false }).as("youtube");
     cy.visitAndCheckAppMount("/");
@@ -31,51 +28,3 @@ describe("Overall landing page tests", () => {
     cy.get("a[href='/']").click();
   });
 });
-describeIf(
-  "Execute the test with clicking on the decline button on the cookie banner",
-  {
-    executionEnvironments: ["developmentLocal"],
-  },
-  () => {
-    it("Check if vertical scroll is locked when dragging horizontally", () => {
-      performScrollingTest(true);
-    });
-  },
-);
-
-describeIf(
-  "Execute the test without clicking on the nonexistent cookie banner",
-  {
-    executionEnvironments: ["ci", "developmentCd", "previewCd"],
-  },
-  () => {
-    it("Check if vertical scroll is locked when dragging horizontally", () => {
-      performScrollingTest(false);
-    });
-  },
-);
-
-/**
- * Performs the actual scrolling test with or without the cookie banner
- * @param clickBanner tells the function if the cookie banner is present or not and thus needs to be clicked away
- */
-function performScrollingTest(clickBanner: boolean) : void {
-  cy.viewport(400, 800);
-  cy.visitAndCheckAppMount("/");
-  if (clickBanner) {
-    cy.get("#CybotCookiebotDialogBodyButtonDecline").click();
-  }
-  cy.window().then((win) => {
-    const scrollYBeforeScrolling = win.scrollY;
-    cy.get('[data-test="howitworks"]')
-      .scrollIntoView()
-      .trigger("pointerdown", 100, 400, { button: 0 })
-      .trigger("pointermove", { eventConstructor: "MouseEvent", clientX: 200, clientY: 440 })
-      .trigger("pointermove", { eventConstructor: "MouseEvent", clientX: 300, clientY: 340 })
-      .trigger("pointermove", { eventConstructor: "MouseEvent", clientX: 50, clientY: 440 })
-      .trigger("pointerup", { button: 0 });
-    const scrollYAfterScrolling = win.scrollY;
-    expect(scrollYBeforeScrolling).equals(scrollYAfterScrolling);
-  });
-}
-// });
