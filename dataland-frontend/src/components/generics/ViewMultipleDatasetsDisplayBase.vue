aa7edfe3a3a87d4643d32fe5eef6dfe2971cfb8a
marcinStanczyk <marcin.stanczyk@pwc.com>
2023-11-09 09:44:22 +0100

    base config

--- dataland-frontend/src/components/generics/ViewMultipleDatasetsDisplayBase.vue
+++ dataland-frontend/src/components/generics/ViewMultipleDatasetsDisplayBase.vue
@@ -27,6 +27,14 @@
               :singleDataMetaInfoToDisplay="singleDataMetaInfoToDisplay"
             />
             <MultiLayerDataTableFrameworkPanel
+              v-if="dataType === DataTypeEnum.EutaxonomyFinancials"
+              :frameworkIdentifier="DataTypeEnum.EutaxonomyFinancials"
+              :companyId="companyId"
+              :display-configuration="configForEutaxonomyFinancialsMLDT"
+              :singleDataMetaInfoToDisplay="singleDataMetaInfoToDisplay"
+              :inReviewMode="slotProps.inReviewMode"
+            />
+            <MultiLayerDataTableFrameworkPanel
               v-if="dataType === DataTypeEnum.Lksg"
               :frameworkIdentifier="DataTypeEnum.Lksg"
               :companyId="companyId"
@@ -101,6 +109,9 @@ import { convertDataModelToMLDTConfig } from "@/components/resources/dataTable/c
 import { sfdrDataModel } from "@/components/resources/frameworkDataSearch/sfdr/SfdrDataModel";
 import { lksgDataModel } from "@/components/resources/frameworkDataSearch/lksg/LksgDataModel";
 import { p2pDataModel } from "@/components/resources/frameworkDataSearch/p2p/P2pDataModel";
+import {
+  configForEutaxonomyFinancialsMLDT
+} from "@/components/resources/frameworkDataSearch/euTaxonomy/configMLDT/configForEutaxonomyFinancialsMLDT";
 
 export default defineComponent({
   name: "ViewMultipleDatasetsDisplayBase",
@@ -113,6 +124,9 @@ export default defineComponent({
     },
     p2pDataModel() {
       return p2pDataModel;
+    },
+    configForEutaxonomyFinancialsMLDT() {
+      return configForEutaxonomyFinancialsMLDT
     },
   },
   components: {
--- dataland-frontend/src/components/resources/frameworkDataSearch/euTaxonomy/configMLDT/configForEutaxonomyFinancialsMLDT.ts
+++ dataland-frontend/src/components/resources/frameworkDataSearch/euTaxonomy/configMLDT/configForEutaxonomyFinancialsMLDT.ts
@@ -1,97 +1,184 @@
+import {
+  AvailableMLDTDisplayObjectTypes,
+  MLDTDisplayComponentName
+} from "@/components/resources/dataTable/MultiLayerDataTableCellDisplayer";
+import MultiSelectModal from "@/components/resources/dataTable/modals/MultiSelectModal.vue";
+import {
+  euTaxonomyKpiInfoMappings,
+  euTaxonomyKpiNameMappings,
+} from "@/components/forms/parts/kpiSelection/EuTaxonomyKPIsModel";
+import {formatNumberToReadableFormat} from "@/utils/Formatter";
+
 export const configForEutaxonomyFinancialsMLDT = [
-    {
+  {
+    type: "cell",
+    label: euTaxonomyKpiNameMappings.financialServicesTypes,
+    explanation: euTaxonomyKpiInfoMappings.financialServicesTypes,
+    shouldDisplay: (): boolean => true,
+    valueGetter: (dataset) => ({
+      displayComponentName: MLDTDisplayComponentName.ModalLinkDisplayComponent,
+      displayValue: {
+        label: `Show ${dataset.financialServicesTypes.length} value${dataset.financialServicesTypes.length > 1 ? "s" : ""}`,
+        modalComponent: MultiSelectModal,
+        modalOptions: {
+          props: {
+            header: euTaxonomyKpiNameMappings.financialServicesTypes,
+            modal: true,
+            dismissableMask: true,
+          },
+          data: {
+            label: euTaxonomyKpiNameMappings.financialServicesTypes,
+            values: dataset.financialServicesTypes,
+          },
+        },
+  },
+    }),
+  },
+  {
+    type: "section",
+    label: "Eligibility Kpis",
+    labelBadgeColor: "orange",
+    expandOnPageLoad: true,
+    shouldDisplay: () => true,
+    children: [
+      {
+        type: "cell",
+        label: "Credit Institution",
+        shouldDisplay: (dataset) => (dataset.financialServicesTypes.includes("CreditInstitution")),
+        valueGetter: (dataset) => ({
+          displayComponentName: MLDTDisplayComponentName.DataPointDisplayComponent,
+          displayValue: dataset.eligibilityKpis.CreditInstitution.taxonomyEligibleActivityInPercent,
+        }),
+      },
+      {
+        type: "cell",
+        label: "Investment Firm",
+        shouldDisplay: (dataset) => (dataset.financialServicesTypes.includes("InvestmentFirm")),
+        valueGetter: (dataset) => {
+
+          if (dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent?.quality ||
+              dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent?.comment?.length ||
+              dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent?.dataSource?.page != null
+          ) {
+            return {
+              displayComponentName: MLDTDisplayComponentName.DataPointDisplayComponent,
+              displayValue: {
+                fieldLabel: euTaxonomyKpiNameMappings.taxonomyEligibleActivityInPercent,
+                value: dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent.value,
+                dataSource: dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent.dataSource,
+                quality: dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent.quality,
+                comment: dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent.comment,
+              },
+            }
+          } else if (!!dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent?.dataSource?.fileReference?.trim().length) {
+            return {
+              displayComponentName: MLDTDisplayComponentName.DocumentLinkDisplayComponent,
+              displayValue: {
+                label: dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent.value,
+                dataSource: dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent.dataSource,
+              },
+            }
+          } else {
+            return {
+              displayComponentName: MLDTDisplayComponentName.StringDisplayComponent,
+              displayValue: dataset.eligibilityKpis.InvestmentFirm.taxonomyEligibleActivityInPercent?.value,
+            }
+          }
+        }},
+
+
+      {
+        type: "cell",
+        label: "Investment Firm",
+        shouldDisplay: (dataset) => (dataset.financialServicesTypes.includes("InvestmentFirm")),
+        valueGetter: (dataset) => {
+
+          if (dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent?.quality ||
+              dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent?.comment?.length ||
+              dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent?.dataSource?.page != null
+          ) {
+            return {
+              displayComponentName: MLDTDisplayComponentName.DataPointDisplayComponent,
+              displayValue: {
+                fieldLabel: euTaxonomyKpiNameMappings.taxonomyNonEligibleActivityInPercent,
+                value: dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent.value,
+                dataSource: dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent.dataSource,
+                quality: dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent.quality,
+                comment: dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent.comment,
+              },
+            }
+          } else if (!!dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent?.dataSource?.fileReference?.trim().length) {
+            return {
+              displayComponentName: MLDTDisplayComponentName.DocumentLinkDisplayComponent,
+              displayValue: {
+                label: dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent.value,
+                dataSource: dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent.dataSource,
+              },
+            }
+          } else {
+            return {
+              displayComponentName: MLDTDisplayComponentName.StringDisplayComponent,
+              displayValue: dataset.eligibilityKpis.InvestmentFirm.taxonomyNonEligibleActivityInPercent?.value,
+            }
+          }
+        }},
+
+
+      {
+        type: "cell",
+        label: "Investment Firm",
+        shouldDisplay: (dataset) => (dataset.financialServicesTypes.includes("InvestmentFirm")),
+        valueGetter: (dataset) => {
+
+          if (dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent?.quality ||
+              dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent?.comment?.length ||
+              dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent?.dataSource?.page != null
+          ) {
+            return {
+              displayComponentName: MLDTDisplayComponentName.DataPointDisplayComponent,
+              displayValue: {
+                fieldLabel: euTaxonomyKpiNameMappings.banksAndIssuersInPercent,
+                value: dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent.value,
+                dataSource: dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent.dataSource,
+                quality: dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent.quality,
+                comment: dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent.comment,
+              },
+            }
+          } else if (!!dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent?.dataSource?.fileReference?.trim().length) {
+            return {
+              displayComponentName: MLDTDisplayComponentName.DocumentLinkDisplayComponent,
+              displayValue: {
+                label: dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent.value,
+                dataSource: dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent.dataSource,
+              },
+            }
+          } else {
+            return {
+              displayComponentName: MLDTDisplayComponentName.StringDisplayComponent,
+              displayValue: dataset.eligibilityKpis.InvestmentFirm.banksAndIssuersInPercent?.value,
+            }
+          }
+        }},
+
+
+
+      {
         type: "section",
-        label: "General",
-        expandOnPageLoad: true,
-        children: [
-            {
-                type: "section",
-                label: "General",
-                expandOnPageLoad: true,
-                children: [
-                    {
-                        type: "cell",
-                        label: "Data Date",
-                        explanation: "The date until when the information collected is valid",
-                    },
-                    {
-                        type: "cell",
-                        label: "Fiscal Year Deviation",
-                        explanation: "Does the fiscal year deviate from the calender year?",
-                    },
-                    {
-                        type: "cell",
-                        label: "Fiscal Year End",
-                        explanation: "The date the fiscal year ends",
-                    },
-                    {
-                        type: "cell",
-                        label: "Scope Of Entities",
-                        explanation:
-                            "Does a list of legal entities covered by Sust./Annual/Integrated report match with a list of legal entities covered by Audited Consolidated Financial Statement",
-                    },
-                    {
-                        type: "cell",
-                        label: "EU Taxonomy Activity Level Reporting",
-                        explanation: "Activity Level disclosure",
-                    },
-                    {
-                        type: "cell",
-                        label: "Number Of Employees",
-                        explanation: "Total number of employees (including temporary workers)",
-                    },
-                    {
-                        type: "cell",
-                        label: "NFRD Mandatory",
-                        explanation: "The reporting obligation for companies whose number of employees is greater or equal to 500",
-                    },
-                ],
-            },
-        ],
-        labelBadgeColor: "orange",
-    },
-    {
-        type: "section",
-        label: "Environmental",
+        label: "Subsection 1",
         expandOnPageLoad: false,
+        shouldDisplay: () => true,
         children: [
-            {
-                type: "section",
-                label: "Greenhouse gas emissions ",
-                expandOnPageLoad: false,
-                children: [
-                    {
-                        type: "cell",
-                        label: "Trading Portfolio",
-                        explanation: "For Credit Institutions, the trading portfolio as a percentage of total assets",
-                    },
-                    {
-                        type: "cell",
-                        label: "Scope 2 GHG emissions",
-                        explanation: "What is the amount of the company's Scope 2 emissions?",
-                    },
-                    {
-                        type: "cell",
-                        label: "Scope 3 GHG emissions",
-                        explanation: "What is the amount of the company's Scope 3 emissions ?",
-                    },
-                    {
-                        type: "cell",
-                        label: "Enterprise Value",
-                        explanation: "Company Enterprise Value",
-                    },
-                    {
-                        type: "cell",
-                        label: "Total Revenue",
-                        explanation: "Company Total Revenue ",
-                    },
-                    {
-                        type: "cell",
-                        label: "Fossil Fuel Sector Exposure",
-                        explanation: "Does the company derive any revenues from fossil fuels?",
-                    },
-                ],
-            },
+          {
+            type: "cell",
+            label: "Level 3 - String",
+            shouldDisplay: () => true,
+            valueGetter: (dataset) => ({
+              displayComponentName: MLDTDisplayComponentName.StringDisplayComponent,
+              displayValue: dataset.financialServicesTypes,
+            }),
+          },
         ],
-    },
+      },
+    ],
+  },
 ];
