fb7578fc0c53bb6dee594fb1ce74aa5e152409da
Florian Soffel <florian.soffel@d-fine.de>
2023-11-10 13:24:05 +0100

    bugfix

--- dataland-inbound-proxy/docker-entrypoint.d/03-configure_robots_file.sh
+++ dataland-inbound-proxy/docker-entrypoint.d/03-configure_robots_file.sh
@@ -9,6 +9,7 @@ serverName="$PROXY_PRIMARY_URL"
 echo "Configuring robots.txt on $serverName"
 
 if [[ $serverName == "dataland.com" ]]; then
+  timeout 120 bash -c "while [[ ! -f $prodFilePath ]]; do echo 'robots-prod.txt not available for moving yet'; sleep 5; done;"
   echo "Replacing robots.txt for production on dataland.com"
   mv "$prodFilePath" "$targetFilePath"
 fi
