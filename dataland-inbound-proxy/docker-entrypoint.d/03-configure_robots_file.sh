8079d12eb26e98b793f7b8a4d3adf3a5a5ee4ff8
Florian Soffel <florian.soffel@d-fine.de>
2023-11-10 13:58:06 +0100

    test commit to be reverted

--- dataland-inbound-proxy/docker-entrypoint.d/03-configure_robots_file.sh
+++ dataland-inbound-proxy/docker-entrypoint.d/03-configure_robots_file.sh
@@ -8,7 +8,7 @@ targetFilePath="/var/www/html/robots.txt"
 serverName="$PROXY_PRIMARY_URL"
 echo "Configuring robots.txt on $serverName"
 
-if [[ $serverName == "dataland.com" ]]; then
+if [[ $serverName == "dev1.dataland.com" ]]; then
   timeout 120 bash -c "while [[ ! -f $prodFilePath ]]; do echo 'robots-prod.txt not available for moving yet'; sleep 5; done;"
   echo "Replacing robots.txt for production on dataland.com"
   mv "$prodFilePath" "$targetFilePath"
