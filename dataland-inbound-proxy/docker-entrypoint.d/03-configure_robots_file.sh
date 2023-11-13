c49c3e21a2bb9cf5a2ca97f7b043ec04aadfa15d
Florian Soffel <florian.soffel@d-fine.de>
2023-11-13 09:40:27 +0100

    make entrypoint scripts rerunnable

--- dataland-inbound-proxy/docker-entrypoint.d/03-configure_robots_file.sh
+++ dataland-inbound-proxy/docker-entrypoint.d/03-configure_robots_file.sh
@@ -8,8 +8,7 @@ targetFilePath="/var/www/html/robots.txt"
 serverName="$PROXY_PRIMARY_URL"
 echo "Configuring robots.txt on $serverName"
 
-if [[ $serverName == "dev2.dataland.com" ]]; then
-  timeout 120 bash -c "while [[ ! -f $prodFilePath ]]; do echo 'robots-prod.txt not available for moving yet'; sleep 5; done;"
+if [[ $serverName == "dataland.com" ]]; then
   echo "Replacing robots.txt for production on dataland.com"
-  mv "$prodFilePath" "$targetFilePath"
+  cp "$prodFilePath" "$targetFilePath"
 fi
