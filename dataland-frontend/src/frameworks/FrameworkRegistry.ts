730be96e5020e17122db5b6e7107f3c7a2ed1579
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2023-11-14 12:15:03 +0100

    Implemented automatic prepopulation for framework-registry maintained frameworks.

--- dataland-frontend/src/frameworks/FrameworkRegistry.ts
+++ dataland-frontend/src/frameworks/FrameworkRegistry.ts
@@ -1,20 +1,13 @@
 import { type FrameworkDefinition } from "@/frameworks/FrameworkDefinition";
-
-// Ignored as TSC does not know of this vite-specific import possibility
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const frameworkModules: Record<string, FrameworkDefinition<unknown>> = import.meta.glob("./*/index.ts", {
-  import: "default",
-  eager: true,
-});
+import { FrameworkDefinitions } from "@/frameworks/FrameworkRegistryImports.generated";
 
 /**
  * Retrieve the framework definition for the provided framework identifier if it exists
  * @param identifier the identifier of the framework to retrieve
  * @returns the framework definition if it exists
  */
-export function getFrameworkDefinition(identifier: string): FrameworkDefinition<unknown> | undefined {
-  return frameworkModules[`./${identifier}/index.ts`];
+export function getFrameworkDefinition(identifier: string): FrameworkDefinition<object> | undefined {
+  return FrameworkDefinitions[identifier];
 }
 
 /**
@@ -22,5 +15,5 @@ export function getFrameworkDefinition(identifier: string): FrameworkDefinition<
  * @returns a list of all available framework identifiers
  */
 export function getAllFrameworkIdentifiers(): string[] {
-  return Object.values(frameworkModules).map((it) => it.identifier);
+  return Object.values(FrameworkDefinitions).map((it) => it.identifier);
 }
