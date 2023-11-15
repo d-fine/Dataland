70fa85673f05f882ee7f07b461ede6272ad995bf
Florian Soffel <florian.soffel@d-fine.de>
2023-11-15 12:05:21 +0100

    Revert "remove test"
    This reverts commit 02e85155f1e47a586b780ac6ea055868e6201101.

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/CompanyManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/CompanyManagerTest.kt
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.transaction.Transactional
 import org.dataland.datalandbackend.DatalandBackend
 import org.dataland.datalandbackend.model.StoredCompany
+import org.dataland.datalandbackend.model.companies.CompanyInformation
+import org.dataland.datalandbackend.model.enums.company.IdentifierType
 import org.dataland.datalandbackend.utils.TestDataProvider
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
@@ -13,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
 
 @SpringBootTest(classes = [DatalandBackend::class], properties = ["spring.profiles.active=nodb"])
 @Transactional
@@ -133,5 +137,68 @@ class CompanyManagerTest(
             "No matches starting with the search string " +
                 "$searchString were returned. At least one was expected.",
         )
+    }
+
+    @Test
+    fun `check if the companies search via name and ids sorts the results as expected`() {
+        val testString = "unique-test-string-${UUID.randomUUID()}"
+        uploadCompaniesInReverseToExpectedOrder(testString)
+        val sortedCompanyNames = testCompanyQueryManager.searchCompaniesByNameOrIdentifierAndGetApiModel(
+            searchString = testString,
+        ).map { it.companyName }
+        assertEquals(
+            listOf(testString, company2, "${testString}2", company5, company6, "3$testString", company9),
+            sortedCompanyNames.filter { it != company8 && it != company3 },
+        )
+        assertEquals(
+            listOf(company3, company2, "${testString}2", company5, company6, company8, company9),
+            sortedCompanyNames.filter { it != "3$testString" && it != testString },
+        )
+
+        val otherCompanyNames = testCompanyQueryManager.searchCompaniesByNameOrIdentifierAndGetApiModel(
+            searchString = "other_name",
+        ).map { it.companyName }
+        assertTrue(otherCompanyNames.contains(company8))
+        Assertions.assertFalse(otherCompanyNames.contains("Company 7"))
+    }
+
+    private val company2 = "Company 2"
+    private val company3 = "Company 3"
+    private val company5 = "Company 5"
+    private val company6 = "Company 6"
+    private val company8 = "Company 8"
+    private val company9 = "Company 9"
+    val baseCompanyInformation = CompanyInformation(
+        companyName = "Placholder Inc.",
+        countryCode = "DE",
+        companyAlternativeNames = null,
+        companyLegalForm = null,
+        headquarters = "Frankfurt",
+        headquartersPostalCode = "60313",
+        sector = null,
+        website = null,
+        identifiers = mapOf(),
+    )
+    private fun uploadCompaniesInReverseToExpectedOrder(expectedSearchString: String) {
+        uploadModifiedBaseCompany(company9, null, "3$expectedSearchString")
+        uploadModifiedBaseCompany(company8, listOf("3$expectedSearchString", "other_name"), null)
+        uploadModifiedBaseCompany("3$expectedSearchString", null, null)
+        uploadModifiedBaseCompany(company6, null, "${expectedSearchString}2")
+        uploadModifiedBaseCompany(company5, listOf("${expectedSearchString}2"), null)
+        uploadModifiedBaseCompany("${expectedSearchString}2", null, null)
+        uploadModifiedBaseCompany(company3, null, expectedSearchString)
+        uploadModifiedBaseCompany(company2, listOf(expectedSearchString), null)
+        uploadModifiedBaseCompany(expectedSearchString, null, null)
+    }
+
+    private fun uploadModifiedBaseCompany(name: String, alternativeNames: List<String>?, identifier: String?): String {
+        val companyInformation = baseCompanyInformation.copy(
+            companyName = name,
+            companyAlternativeNames = alternativeNames,
+            identifiers = mapOf(
+                IdentifierType.Isin to listOf(identifier ?: UUID.randomUUID().toString()),
+            ),
+        )
+        return testCompanyAlterationManager.addCompany(companyInformation).companyId
     }
 }
