e5f0d08bf30b77f2dc4ce98a2eb8edac729a3545
swaldow <stephan.waldow@d-fine.de>
2023-11-16 16:40:11 +0100

    refactors wildcard import

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/CompanyManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/CompanyManagerTest.kt
@@ -16,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
-import java.util.*
+import java.util.UUID
 
 @SpringBootTest(classes = [DatalandBackend::class], properties = ["spring.profiles.active=nodb"])
 @Transactional
