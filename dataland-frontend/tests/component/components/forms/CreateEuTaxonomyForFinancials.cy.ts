bc144b24f109e3d2f1b700b4343d9a4912c5ef6c
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-11-16 10:42:50 +0100

    test if build works

--- dataland-frontend/tests/component/components/forms/CreateEuTaxonomyForFinancials.cy.ts
+++ dataland-frontend/tests/component/components/forms/CreateEuTaxonomyForFinancials.cy.ts
@@ -413,9 +413,9 @@ describe("Component tests for the Eu Taxonomy Financials", () => {
 
   it("Open upload page, fill out and validate the upload form", () => {
     cy.mountWithPlugins(CreateEuTaxonomyForFinancials, {
-      keycloak: minimalKeycloakMock({}),
       props: {
         companyID: companyAssociatedEuTaxoFinancialsData.companyId,
+        keycloak: minimalKeycloakMock({}),
       },
       data() {
         return {
