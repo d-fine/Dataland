16f123abeeba0b1d62b75f38b385fb03cc6bf491
marcinStanczyk <marcin.stanczyk@pwc.com>
2023-11-16 14:44:00 +0100

    tests cleanup

--- dataland-frontend/tests/e2e/specs/eu-taxonomy-financials/index.ts
+++ dataland-frontend/tests/e2e/specs/eu-taxonomy-financials/index.ts
@@ -2,6 +2,5 @@
  * As a user, I want the EU Taxonomy company page for financials to exist and behave like I expect
  */
 describe("EU Taxonomy for financials Company Page tests", () => {
-  require("./DataIntegrity");
   require("./FileUploadAndLinking");
 });
