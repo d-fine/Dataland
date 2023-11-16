32a72caeea2b05641022c46ac4dc88a3e3db94ff
d93171 <hussain.roker@d-fine.com>
2023-11-16 14:35:57 +0100

    fixed bugs, works in chrome but not in electron

--- dataland-frontend/tests/component/components/pages/SearchPagination.cy.ts
+++ dataland-frontend/tests/component/components/pages/SearchPagination.cy.ts
@@ -2,29 +2,31 @@ import SearchCompaniesForFrameworkData from "@/components/pages/SearchCompaniesF
 import { minimalKeycloakMock } from "@ct/testUtils/Keycloak";
 import { prepareSimpleDataSearchStoredCompanyArray } from "@ct/testUtils/PrepareDataSearchStoredCompanyArray";
 
-describe("As a user, I expect there to be multiple result pages if there are many results to be displayed", () => {
-    const mockDataSearchStoredCompanyArray = prepareSimpleDataSearchStoredCompanyArray(200);
-    before(() => {
-        cy.intercept("**/api/companies?**", mockDataSearchStoredCompanyArray);
-        cy.intercept("**/api/companies/meta-information", mockDataSearchStoredCompanyArray[0].dataRegisteredByDataland[0]);
-        const keycloakMock = minimalKeycloakMock({
-            roles: ["ROLE_USER", "ROLE_UPLOADER", "ROLE_REVIEWER"],
-        });
 
-        cy.mountWithPlugins<typeof SearchCompaniesForFrameworkData>(SearchCompaniesForFrameworkData, {
-            keycloak: keycloakMock,
-        }).then((mounted) => {
-            void mounted.wrapper.setData({
-                resultArray: mockDataSearchStoredCompanyArray,
-            });
-            cy.wait(500);
-        });
+function intercept() {
+    const mockDataSearchStoredCompanyArray = prepareSimpleDataSearchStoredCompanyArray(200);
+    cy.intercept("**/api/companies?**", mockDataSearchStoredCompanyArray);
+    cy.intercept("**/api/companies/meta-information", mockDataSearchStoredCompanyArray[0].dataRegisteredByDataland[0]);
+    const keycloakMock = minimalKeycloakMock({
+        roles: ["ROLE_USER", "ROLE_UPLOADER", "ROLE_REVIEWER"],
     });
+
+    cy.mountWithPlugins<typeof SearchCompaniesForFrameworkData>(SearchCompaniesForFrameworkData, {
+        keycloak: keycloakMock,
+    }).then((mounted) => {
+        void mounted.wrapper.setData({
+            resultArray: mockDataSearchStoredCompanyArray,
+        });
+        cy.wait(500);
+    });
+}
+
+describe("As a user, I expect there to be multiple result pages if there are many results to be displayed", () => {
 
 
     it("Do a search with 0 matches, then assure that the paginator is gone and the page text says no results", () => {
+        intercept();
         const inputValueThatWillResultInZeroMatches = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678987654321";
-        cy.wait(5000);
         cy.get("input[id=search_bar_top]")
             .should("exist")
             .type(inputValueThatWillResultInZeroMatches)
@@ -36,8 +38,8 @@ describe("As a user, I expect there to be multiple result pages if there are man
     });
 
     it("Search for all companies containing 'a' and verify that results are paginated, only first 100 are shown", () => {
+        intercept();
         const inputValue = "a";
-        cy.wait(5000);
         cy.get("input[id=search_bar_top]")
             .should("not.be.disabled")
             .click({ force: true })
@@ -46,11 +48,12 @@ describe("As a user, I expect there to be multiple result pages if there are man
             .should("have.value", inputValue);
         cy.get("table.p-datatable-table").should("exist");
         cy.get(".p-paginator-current").should("contain.text", "Showing 1 to 100 of").contains("entries");
+        cy.wait(5000);
         cy.contains("span", "1-100 of");
     });
 
     it("Search for all companies, go to page 2 of the search results, then run a another query and verify that paginator and the page text are reset", () => {
-        cy.visitAndCheckAppMount("/companies");
+        intercept();
         cy.get("table.p-datatable-table").should("exist");
         cy.get('button[class="p-paginator-page p-paginator-element p-link"]').eq(0).should("contain.text", "2").click();
         cy.get("table.p-datatable-table").should("exist");
@@ -62,6 +65,7 @@ describe("As a user, I expect there to be multiple result pages if there are man
             .type("{enter}")
             .should("have.value", inputValue);
         cy.get(".p-paginator-current").should("contain.text", "Showing 1 to 100 of").contains("entries");
+        cy.wait(5000);
         cy.contains("span", "1-100 of");
     });
 });
