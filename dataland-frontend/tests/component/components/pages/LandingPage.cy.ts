5f9e7c8c66d3d144779b7f617cda7e55e01de2cd
Florian Soffel <florian.soffel@d-fine.de>
2023-11-17 14:16:10 +0100

    fix video positioning

--- dataland-frontend/tests/component/components/pages/LandingPage.cy.ts
+++ dataland-frontend/tests/component/components/pages/LandingPage.cy.ts
@@ -142,7 +142,7 @@ function validateStruggleSection(): void {
  */
 function validateQuotesSection(): void {
   cy.get("section.quotes").should("exist");
-  cy.get(".quotes__slide").should("have.length", 6);
+  cy.get(".quotes__slide").should("have.length", 7);
 }
 
 /**
