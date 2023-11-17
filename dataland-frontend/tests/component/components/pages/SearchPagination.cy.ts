31c97024cc1cb5dd86cdb3820e963b80b3233c3e
d93171 <hussain.roker@d-fine.com>
2023-11-17 14:43:11 +0100

    fixed linting/sonar issues,

--- dataland-frontend/tests/component/components/pages/SearchPagination.cy.ts
+++ dataland-frontend/tests/component/components/pages/SearchPagination.cy.ts
@@ -23,49 +23,49 @@ function intercept(arr: undefined | [] = undefined): undefined {
   });
 }
 
+/**
+ * enter text into search bar
+ * @param input search string
+ */
+function enterSearch(input: string): undefined {
+  cy.get("input[id=search_bar_top]").should("exist").type(input).type("{enter}").should("have.value", input).clear();
+}
+
+/**
+ * checks if paginator exists
+ */
+function paginatorShouldExist(): undefined {
+  cy.get("table.p-datatable-table").should("exist");
+  cy.get(".p-paginator-current").should("contain.text", "Showing 1 to 100 of").contains("entries");
+  cy.scrollTo("top");
+  cy.contains("span", "1-100 of");
+}
+
+/**
+ * checks if paginator does not exist
+ */
+function paginatorShouldNotExist(): undefined {
+  cy.get("div.p-paginator").should("not.exist");
+  cy.contains("span", "No results");
+}
+
 describe("As a user, I expect there to be multiple result pages if there are many results to be displayed", () => {
   it("Do a search with 0 matches, then assure that the paginator is gone and the page text says no results", () => {
     intercept([]);
-    const inputValueThatWillResultInZeroMatches = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678987654321";
-    cy.get("input[id=search_bar_top]")
-      .should("exist")
-      .type(inputValueThatWillResultInZeroMatches)
-      .type("{enter}")
-      .should("have.value", inputValueThatWillResultInZeroMatches)
-      .clear();
-    cy.get("div.p-paginator").should("not.exist");
-    cy.contains("span", "No results");
+    enterSearch("ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678987654321");
+    paginatorShouldNotExist();
   });
 
   it("Search for all companies containing 'a' and verify that results are paginated, only first 100 are shown", () => {
     intercept();
-    const inputValue = "a";
-    cy.get("input[id=search_bar_top]")
-      .should("not.be.disabled")
-      .click({ force: true })
-      .type(inputValue)
-      .type("{enter}")
-      .should("have.value", inputValue);
-    cy.get("table.p-datatable-table").should("exist");
-    cy.get(".p-paginator-current").should("contain.text", "Showing 1 to 100 of").contains("entries");
-    cy.scrollTo("top");
-    cy.contains("span", "1-100 of");
+    enterSearch("a");
+    paginatorShouldExist();
   });
 
   it("Search for all companies, go to page 2 of the search results, then run a another query and verify that paginator and the page text are reset", () => {
     intercept();
-    cy.get("table.p-datatable-table").should("exist");
     cy.get('button[class="p-paginator-page p-paginator-element p-link"]').eq(0).should("contain.text", "2").click();
-    cy.get("table.p-datatable-table").should("exist");
-    const inputValue = "a";
-    cy.get("input[id=search_bar_top]")
-      .should("not.be.disabled")
-      .click({ force: true })
-      .type(inputValue)
-      .type("{enter}")
-      .should("have.value", inputValue);
-    cy.get(".p-paginator-current").should("contain.text", "Showing 1 to 100 of").contains("entries");
-    cy.scrollTo("top");
-    cy.contains("span", "1-100 of");
+    enterSearch("a");
+    paginatorShouldExist();
   });
 });
