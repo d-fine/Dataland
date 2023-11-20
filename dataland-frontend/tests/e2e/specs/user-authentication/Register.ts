e608e4361b130d9cfd74c166424bf55fc89ff4de
Stefano Ferrario <stefano.ferrario@experienceone.com>
2023-11-20 13:46:19 +0100

    test fix

--- dataland-frontend/tests/e2e/specs/user-authentication/Register.ts
+++ dataland-frontend/tests/e2e/specs/user-authentication/Register.ts
@@ -8,7 +8,6 @@ describe("As a user I want to be able to register for an account and be able to 
   const randomHexPassword = [...passwordBytes].map((x): string => x.toString(16).padStart(2, "0")).join("");
 
   it("Checks that the Dataland password-policy gets respected", () => {
-    cy.intercept({ url: "https://www.youtube-nocookie.com/**" }, { forceNetworkError: false }).as("youtube");
     cy.visitAndCheckAppMount("/")
       .wait("@youtube")
       .get("button[name='signup_dataland_button']")
