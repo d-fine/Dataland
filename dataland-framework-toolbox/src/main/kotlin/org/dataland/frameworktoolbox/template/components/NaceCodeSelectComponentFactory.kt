31a7913661cde664f76b2e47b5f4e417cf64e33d
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-11-29 12:35:34 +0100

    more of the current state

---
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/template/components/NaceCodeSelectComponentFactory.kt
@@ -0,0 +1,41 @@
+package org.dataland.frameworktoolbox.template.components
+
+import org.dataland.frameworktoolbox.intermediate.components.ComponentBase
+import org.dataland.frameworktoolbox.intermediate.components.NaceCodeSelectComponent
+import org.dataland.frameworktoolbox.intermediate.group.ComponentGroupApi
+import org.dataland.frameworktoolbox.intermediate.group.create
+import org.dataland.frameworktoolbox.template.TemplateDiagnostic
+import org.dataland.frameworktoolbox.template.model.TemplateRow
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+/**
+ * Generates StringComponents from rows with the component "String"
+ */
+@Component
+class NaceCodeSelectComponentFactory(@Autowired val templateDiagnostic: TemplateDiagnostic) : TemplateComponentFactory {
+    override fun canGenerateComponent(row: TemplateRow): Boolean = row.component == "Nace-Code-Selector"
+
+    override fun generateComponent(
+        row: TemplateRow,
+        utils: ComponentGenerationUtils,
+        componentGroup: ComponentGroupApi,
+    ): ComponentBase {
+        templateDiagnostic.optionsNotUsed(row)
+        templateDiagnostic.unitNotUsed(row)
+
+        return componentGroup.create<NaceCodeSelectComponent>(
+            utils.generateFieldIdentifierFromRow(row),
+        ) {
+            utils.setCommonProperties(row, this)
+        }
+    }
+
+    override fun updateDependency(
+        row: TemplateRow,
+        utils: ComponentGenerationUtils,
+        componentIdentifierMap: Map<String, ComponentBase>,
+    ) {
+        utils.defaultDependencyConfiguration(row, componentIdentifierMap, templateDiagnostic)
+    }
+}
