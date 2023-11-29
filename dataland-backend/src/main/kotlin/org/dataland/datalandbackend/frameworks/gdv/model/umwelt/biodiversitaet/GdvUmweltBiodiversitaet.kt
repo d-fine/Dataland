ef49a7c9b0609d8eac4d24e02c51b84ee62a778a
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-11-29 17:51:43 +0100

    forgot some files

---
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/frameworks/gdv/model/umwelt/biodiversitaet/GdvUmweltBiodiversitaet.kt
@@ -0,0 +1,9 @@
+package org.dataland.datalandbackend.frameworks.gdv.model.umwelt.biodiversitaet
+
+/**
+ * The data-model for the Biodiversitaet section
+ */
+data class GdvUmweltBiodiversitaet(
+    val negativeMassnahmenFuerDieBiologischeVielfalt: String?,
+    val positiveMassnahmenFuerDieBiologischeVielfalt: String?,
+)
