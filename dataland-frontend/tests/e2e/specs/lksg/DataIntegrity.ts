877e3209fb5123ee5916ac345e87824e65d4d6af
swaldow <stephan.waldow@d-fine.de>
2023-12-01 13:37:25 +0100

    small refactoring

--- dataland-frontend/tests/e2e/specs/lksg/DataIntegrity.ts
+++ dataland-frontend/tests/e2e/specs/lksg/DataIntegrity.ts
@@ -2,11 +2,11 @@ import { describeIf } from "@e2e/support/TestUtility";
 import { admin_name, admin_pw, getBaseUrl } from "@e2e/utils/Cypress";
 import { getKeycloakToken } from "@e2e/utils/Auth";
 import {
-  Configuration,
-  type DataMetaInformation,
-  DataTypeEnum,
-  type LksgData,
-  LksgDataControllerApi,
+    Configuration,
+    type DataMetaInformation,
+    DataTypeEnum,
+    type LksgData,
+    LksgDataControllerApi, StoredCompany,
 } from "@clients/backend";
 import { generateDummyCompanyInformation, uploadCompanyViaApi } from "@e2e/utils/CompanyUpload";
 import { type FixtureData, getPreparedFixture } from "@sharedUtils/Fixtures";
@@ -48,22 +48,7 @@ describeIf(
               "2021",
               lksgFixtureWithNoNullFields.t,
             ).then((dataMetaInformation) => {
-              cy.intercept("**/api/companies/" + storedCompany.companyId + "/info").as("getCompanyInformation");
-              cy.visitAndCheckAppMount(
-                "/companies/" +
-                  storedCompany.companyId +
-                  "/frameworks/" +
-                  DataTypeEnum.Lksg +
-                  "/upload?templateDataId=" +
-                  dataMetaInformation.dataId,
-              );
-              cy.wait("@getCompanyInformation", { timeout: Cypress.env("medium_timeout_in_ms") as number });
-              cy.get("h1").should("contain", testCompanyName);
-              cy.intercept({
-                url: `**/api/data/${DataTypeEnum.Lksg}`,
-                times: 1,
-              }).as("postCompanyAssociatedData");
-              submitButton.clickButton();
+                interceptsAndSubmitsDataset(storedCompany, dataMetaInformation, testCompanyName);
               cy.wait("@postCompanyAssociatedData", { timeout: Cypress.env("medium_timeout_in_ms") as number }).then(
                 (postInterception) => {
                   cy.url().should("eq", getBaseUrl() + "/datasets");
@@ -88,9 +73,29 @@ describeIf(
         });
       },
     );
-    it(
-      "Create a company and a Lksg dataset via api, then re-upload it with the upload form in Edit mode and " +
-        "assure that the re-uploaded dataset equals the pre-uploaded one",
+
+      function interceptsAndSubmitsDataset(storedCompany: StoredCompany, dataMetaInformation: DataMetaInformation, testCompanyName: string) {
+          cy.intercept("**/api/companies/" + storedCompany.companyId + "/info").as("getCompanyInformation");
+          cy.visitAndCheckAppMount(
+              "/companies/" +
+              storedCompany.companyId +
+              "/frameworks/" +
+              DataTypeEnum.Lksg +
+              "/upload?templateDataId=" +
+              dataMetaInformation.dataId,
+          );
+          cy.wait("@getCompanyInformation", {timeout: Cypress.env("medium_timeout_in_ms") as number});
+          cy.get("h1").should("contain", testCompanyName);
+          cy.intercept({
+              url: `**/api/data/${DataTypeEnum.Lksg}`,
+              times: 1,
+          }).as("postCompanyAssociatedData");
+          submitButton.clickButton();
+      }
+
+      it(
+      "Create a company and a Lksg dataset via api with most entries being null and then verify that it can be" +
+        "reuploaded.",
       () => {
         cy.ensureLoggedIn(admin_name, admin_pw);
         const uniqueCompanyMarker = Date.now().toString();
@@ -105,23 +110,8 @@ describeIf(
               "2021",
               lksgFixtureWithMinimalFields.t,
             ).then((dataMetaInformation) => {
-              cy.intercept("**/api/companies/" + storedCompany.companyId + "/info").as("getCompanyInformation");
-              cy.visitAndCheckAppMount(
-                "/companies/" +
-                  storedCompany.companyId +
-                  "/frameworks/" +
-                  DataTypeEnum.Lksg +
-                  "/upload?templateDataId=" +
-                  dataMetaInformation.dataId,
-              );
-              cy.wait("@getCompanyInformation", { timeout: Cypress.env("medium_timeout_in_ms") as number });
-              cy.get("h1").should("contain", testCompanyName);
-              cy.intercept({
-                url: `**/api/data/${DataTypeEnum.Lksg}`,
-                times: 1,
-              }).as("postCompanyAssociatedData");
-              submitButton.clickButton();
-              cy.wait("@postCompanyAssociatedData", { timeout: Cypress.env("medium_timeout_in_ms") as number }).then(
+                interceptsAndSubmitsDataset(storedCompany, dataMetaInformation, testCompanyName);
+                cy.wait("@postCompanyAssociatedData", { timeout: Cypress.env("medium_timeout_in_ms") as number }).then(
                 (postInterception) => {
                   cy.url().should("eq", getBaseUrl() + "/datasets");
                   const dataMetaInformationOfReuploadedDataset = postInterception.response?.body as DataMetaInformation;
