55cd4d1b8e276c5fd23113a287445d26bf1c6676
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2023-12-04 06:51:53 +0100

    change to template for config

--- testing/data-migration/migrationConfigTemplate.sh
+++ testing/data-migration/migrationConfigTemplate.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 #Configuration for the migrateData.sh script
 
-export SOURCE=
+export SOURCE=dataland.com
 export SOURCE_TOKEN=
-export TARGET=
+export TARGET=local-dev.dataland.com
 export TARGET_TOKEN=
 export ONLY_ACTIVE=false
 #to migrate all frameworks use: eutaxonomy-financials,eutaxonomy-non-financials,lksg,sfdr,p2p,sme
