9fbca3a941993314763eaec4c8a5a54e5073971a
Florian Soffel <florian.soffel@d-fine.de>
2023-12-07 08:29:54 +0100

    refactoring step 2

--- dataland-frontend/tests/sharedUtils/components/UploadDocuments.ts
+++ dataland-frontend/tests/sharedUtils/components/UploadDocuments.ts
@@ -2,29 +2,29 @@ import { TEST_PDF_FILE_BASEPATH } from "@sharedUtils/ConstantsForPdfs";
 
 export class UploadDocuments {
   private name: string;
-  private selector: string;
-  private addButtonSelector: string;
+  private uploadDocumentsSelector: string;
+  private addDocumentButtonSelector: string;
   constructor(name: string = "UploadReports") {
     this.name = name;
-    this.selector = `div[data-test='upload-documents-${name}']`;
-    this.addButtonSelector = `button[data-test='upload-files-button-${name}']`;
+    this.uploadDocumentsSelector = `div[data-test='upload-documents-${name}']`;
+    this.addDocumentButtonSelector = `button[data-test='upload-files-button-${name}']`;
   }
 
   selectFile(filename: string): void {
-    cy.get(this.addButtonSelector).click();
-    cy.get(this.selector)
+    cy.get(this.addDocumentButtonSelector).click();
+    cy.get(this.uploadDocumentsSelector)
       .find("input[type=file]")
       .selectFile(`../${TEST_PDF_FILE_BASEPATH}/${filename}.pdf`, { force: true });
   }
 
   selectMultipleFilesAtOnce(filenames: string[]): void {
-    cy.get(this.addButtonSelector).click();
+    cy.get(this.addDocumentButtonSelector).click();
     const filenamePaths = filenames.map((filename) => `../testing/data/documents/${filename}.pdf`);
-    cy.get(this.selector).find("input[type=file]").selectFile(filenamePaths, { force: true });
+    cy.get(this.uploadDocumentsSelector).find("input[type=file]").selectFile(filenamePaths, { force: true });
   }
   selectDummyFile(filename: string, contentSize: number): void {
-    cy.get(this.addButtonSelector).click();
-    cy.get(this.selector)
+    cy.get(this.addDocumentButtonSelector).click();
+    cy.get(this.uploadDocumentsSelector)
       .find("input[type=file]")
       .selectFile(
         {
@@ -36,8 +36,8 @@ export class UploadDocuments {
       );
   }
   selectDummyFileOfType(filename: string, fileType: string, contentSize: number): void {
-    cy.get(this.addButtonSelector).click();
-    cy.get(this.selector)
+    cy.get(this.addDocumentButtonSelector).click();
+    cy.get(this.uploadDocumentsSelector)
       .find("input[type=file]")
       .selectFile(
         {
@@ -50,7 +50,7 @@ export class UploadDocuments {
   }
 
   errorMessage(): Cypress.Chainable {
-    return cy.get(`${this.selector} .p-fileupload .p-message-error`);
+    return cy.get(`${this.uploadDocumentsSelector} .p-fileupload .p-message-error`);
   }
   dismissErrorMessage(): void {
     this.errorMessage().find(".p-message-close-icon").click();
--- dataland-frontend/tests/sharedUtils/components/UploadReports.ts
+++ dataland-frontend/tests/sharedUtils/components/UploadReports.ts
@@ -1,17 +1,17 @@
 import { UploadDocuments } from "@sharedUtils/components/UploadDocuments";
 
 export class UploadReports extends UploadDocuments {
-  private override selector: string;
+  private uploadReportsSelector: string;
   constructor(name: string = "UploadReports") {
     super(name);
-    this.selector = `div[data-test="upload-reports-${name}"]`;
+    this.uploadReportsSelector = `div[data-test="upload-reports-${name}"]`;
   }
 
   fillAllFormsOfReportsSelectedForUpload(expectedNumberOfReportsToUpload?: number): void {
     if (expectedNumberOfReportsToUpload) {
       this.validateNumberOfReportsSelectedForUpload(expectedNumberOfReportsToUpload);
     }
-    cy.get(`${this.selector} [data-test="report-to-upload-form"]`).each((element) => {
+    cy.get(`${this.uploadReportsSelector} [data-test="report-to-upload-form"]`).each((element) => {
       cy.wrap(element).find(`[data-test="reportDate"] button`).should("have.class", "p-datepicker-trigger").click();
       cy.get("div.p-datepicker").find('button[aria-label="Previous Month"]').click();
       cy.get("div.p-datepicker").find(`span:contains("12")`).click();
@@ -21,42 +21,42 @@ export class UploadReports extends UploadDocuments {
   }
 
   validateReportToUploadHasContainerInTheFileSelector(reportName: string): void {
-    cy.get(`${this.selector} [data-test="${reportName}FileUploadContainer"]`).should("exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}FileUploadContainer"]`).should("exist");
   }
 
   validateReportIsNotInFileSelector(reportName: string): void {
-    cy.get(`${this.selector} [data-test="${reportName}FileUploadContainer"]`).should("not.exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}FileUploadContainer"]`).should("not.exist");
   }
 
   validateNumberOfReportsSelectedForUpload(expectedNumberOfReportsToUpload: number): void {
-    cy.get(`${this.selector} [data-test="report-to-upload-form"]`).should(
+    cy.get(`${this.uploadReportsSelector} [data-test="report-to-upload-form"]`).should(
       "have.length",
       expectedNumberOfReportsToUpload,
     );
   }
 
   removeAlreadyUploadedReport(reportName: string): Cypress.Chainable {
-    return cy.get(`${this.selector} [data-test="${reportName}AlreadyUploadedContainer"] button`).click();
+    return cy.get(`${this.uploadReportsSelector} [data-test="${reportName}AlreadyUploadedContainer"] button`).click();
   }
 
   removeReportFromSelectionForUpload(reportName: string): void {
-    cy.get(`${this.selector} [data-test="${reportName}FileUploadContainer"] button`).click();
-    cy.get(`${this.selector} [data-test="${reportName}FileUploadContainer"]`).should("not.exist");
-    cy.get(`${this.selector} [data-test="${reportName}ToUploadContainer"]`).should("not.exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}FileUploadContainer"] button`).click();
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}FileUploadContainer"]`).should("not.exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}ToUploadContainer"]`).should("not.exist");
   }
 
   validateReportIsNotAlreadyUploadedOrSelectedForUpload(reportName: string): void {
-    cy.get(`${this.selector} [data-test="${reportName}FileUploadContainer"]`).should("not.exist");
-    cy.get(`${this.selector} [data-test="${reportName}ToUploadContainer"]`).should("not.exist");
-    cy.get(`${this.selector} [data-test="${reportName}AlreadyUploadedContainer"]`).should("not.exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}FileUploadContainer"]`).should("not.exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}ToUploadContainer"]`).should("not.exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}AlreadyUploadedContainer"]`).should("not.exist");
   }
 
   validateReportIsListedAsAlreadyUploaded(reportName: string): void {
-    cy.get(`${this.selector} [data-test="${reportName}AlreadyUploadedContainer`).should("exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}AlreadyUploadedContainer`).should("exist");
   }
 
   validateReportToUploadHasContainerWithInfoForm(reportName: string): void {
-    cy.get(`${this.selector} [data-test="${reportName}ToUploadContainer"]`).should("exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}ToUploadContainer"]`).should("exist");
   }
 
   validateReportToUploadIsListedInFileSelectorAndHasInfoForm(reportName: string): void {
@@ -65,7 +65,7 @@ export class UploadReports extends UploadDocuments {
   }
 
   validateReportHasNoContainerWithInfoForm(reportName: string): void {
-    cy.get(`${this.selector} [data-test="${reportName}ToUploadContainer"]`).should("not.exist");
+    cy.get(`${this.uploadReportsSelector} [data-test="${reportName}ToUploadContainer"]`).should("not.exist");
   }
 
   validateReportIsNotInFileSelectorAndHasNoInfoForm(reportName: string): void {
