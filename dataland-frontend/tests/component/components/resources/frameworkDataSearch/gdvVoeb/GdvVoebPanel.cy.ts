f7dcaec19f757868beb3d20dbe5654db5371e10e
Marcel Rasbach <marcel.rasbach@d-fine.com>
2023-12-15 13:53:40 +0100

    component test fix

--- dataland-frontend/tests/component/components/resources/frameworkDataSearch/gdvVoeb/GdvVoebPanel.cy.ts
+++ dataland-frontend/tests/component/components/resources/frameworkDataSearch/gdvVoeb/GdvVoebPanel.cy.ts
@@ -145,7 +145,7 @@ describe("Component Test for the GDV-VÖB view Page with its componenets", () =>
 
     cy.get("span").contains("Beschreibung des Berichts");
     for (const oneListElement of listData) {
-      cy.get("div").contains(oneListElement.value);
+      cy.get("div").contains(oneListElement.value as string);
       cy.get("div").contains(oneListElement.dataSource?.fileName as string);
     }
     cy.get('span[data-test="Report-Download-Policy"]').should("exist");
