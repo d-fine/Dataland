d496fe60cd498ed8c0cd019c2c71984747057612
Florian Soffel <florian.soffel@d-fine.de>
2023-12-15 11:28:42 +0100

    update client generation

--- dataland-automated-qa-service/generate_clients.sh
+++ dataland-automated-qa-service/generate_clients.sh
@@ -6,7 +6,7 @@ PROJECT_ROOT="$(dirname "$0")"/..
 SUBPROJECT_ROOT="$PROJECT_ROOT"/dataland-automated-qa-service
 
 echo Building clients
-openapi-python-client generate --path "$PROJECT_ROOT"/dataland-internal-storage/internalStorageOpenApi.json
+openapi-python-client generate --path "$PROJECT_ROOT"/dataland-document-manager/documentManagerOpenApi.json
 openapi-python-client generate --path "$PROJECT_ROOT"/dataland-backend/backendOpenApi.json
 
 echo Reconstructing build directory
@@ -14,4 +14,4 @@ rm -r "$SUBPROJECT_ROOT"/build || true
 mkdir -p "$SUBPROJECT_ROOT"/build/clients
 echo Moving clients into build directory
 mv dataland-backend-api-documentation-client "$SUBPROJECT_ROOT"/build/clients
-mv dataland-internal-storage-api-documentation-client "$SUBPROJECT_ROOT"/build/clients
+mv dataland-document-manager-api-documentation-client "$SUBPROJECT_ROOT"/build/clients
