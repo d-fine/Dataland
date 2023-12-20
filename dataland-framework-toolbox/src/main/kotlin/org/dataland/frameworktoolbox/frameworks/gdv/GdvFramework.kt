2843d6dde07060d293d06069951e545622b437c6
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-12-20 11:44:32 +0100

    fix broken berichtsPlficht YesNo customization

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/gdv/GdvFramework.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/gdv/GdvFramework.kt
@@ -203,9 +203,9 @@ class GdvFramework : InDevelopmentPavedRoadFramework( // TODO in the end it shou
             sectionUploadConfigBuilder.addStandardUploadConfigCell(
                 frameworkUploadOptions = null,
                 component = component,
-                uploadComponentName = "MultiSelectFormField",
+                uploadComponentName = "YesNoFormField",
                 validation = FrameworkUploadOptions(
-                    body = "\"is:YesNo\"",
+                    body = "\"is:Yes\"",
                     imports = null,
                 ),
             )
