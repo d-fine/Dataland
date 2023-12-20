1119cf85708df6ee424c74c68b235655a0dbf57c
Florian Soffel <florian.soffel@d-fine.de>
2023-12-20 14:10:52 +0100

    dummy test for ci test

--- dataland-automated-qa-service/src/test/message_processing_test.py
+++ dataland-automated-qa-service/src/test/message_processing_test.py
@@ -1,21 +1,67 @@
+import logging
+import pika
 import unittest
-import unittest.mock as mock
+from unittest.mock import Mock, MagicMock
 from infrastructure.messaging import process_qa_request
+import infrastructure.properties as p
+from dataland_backend_api_documentation_client.models.qa_status import QaStatus
+from infrastructure.qa_exceptions import AutomaticQaNotPossibleError
+
+
+def mock_validate_raise_automated_qa_not_possible_error(resource, correlation_id):
+    raise AutomaticQaNotPossibleError("Test")
+    # return QaStatus.ACCEPTED
+
+
+def mock_basic_publish(
+        exchange: str,
+        routing_key: str,
+        body: str,
+        properties: pika.BasicProperties,
+        mandatory: bool,
+):
+    print("this worked")
 
 
 class MessageProcessingTest(unittest.TestCase):
-    def test_something(self):
-        mock
+    def test_working(self):
+        print("START")
+        mock_channel = MagicMock()
+        mock_channel.basic_publish = mock_basic_publish
+        mock_method = Mock()
+        mock_properties = Mock()
+        mock_properties.headers = {"cloudEvents:id": "dummy"}
+        mock_resource = Mock()
         process_qa_request(
-            channel=channel,
-            method=method,
-            routing_key=routing_key,
+            channel=mock_channel,
+            method=mock_method,
+            properties=mock_properties,
+            routing_key=p.mq_data_key,
+            resource_type="data",
             bypass_qa=False,
-            resource=resource,
-            resource_type=resource_type,
-            properties=properties,
-            validate=lambda: True,
+            resource=mock_resource,
+            validate=mock_validate_raise_automated_qa_not_possible_error,
         )
-        print("HELLO")
+        print("END")
+
+    def test_failing(self):
+        print("START")
+        mock_channel = MagicMock()
+        mock_channel.basic_publish = mock_basic_publish
+        mock_method = Mock()
+        mock_properties = Mock()
+        mock_properties.headers = {"cloudEvents:id": "dummy"}
+        mock_resource = Mock()
+        process_qa_request(
+            channel=mock_channel,
+            method=mock_method,
+            properties=mock_properties,
+            routing_key=p.mq_data_key,
+            resource_type="data",
+            bypass_qa=False,
+            resource=mock_resource,
+            validate=mock_validate_raise_automated_qa_not_possible_error,
+        )
+        print("END")
         self.assertTrue(False)
 
