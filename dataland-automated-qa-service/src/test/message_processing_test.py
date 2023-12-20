dc9d817a2eaffddc6e4f4785a567b36b9204e828
Florian Soffel <florian.soffel@d-fine.de>
2023-12-20 10:35:50 +0100

    exceptions uses message now

--- dataland-automated-qa-service/src/test/message_processing_test.py
+++ dataland-automated-qa-service/src/test/message_processing_test.py
@@ -1,8 +1,21 @@
-import unittest as t
+import unittest
+import unittest.mock as mock
+from infrastructure.messaging import process_qa_request
 
 
-class MessageProcessingTest(t.TestCase):
+class MessageProcessingTest(unittest.TestCase):
     def test_something(self):
+        mock
+        process_qa_request(
+            channel=channel,
+            method=method,
+            routing_key=routing_key,
+            bypass_qa=False,
+            resource=resource,
+            resource_type=resource_type,
+            properties=properties,
+            validate=lambda: True,
+        )
         print("HELLO")
         self.assertTrue(False)
 
