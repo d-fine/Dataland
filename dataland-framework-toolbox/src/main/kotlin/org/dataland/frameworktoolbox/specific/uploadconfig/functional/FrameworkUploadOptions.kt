5259ff1b2d504f994e5bdcd83d11c93dcd307baf
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2023-12-20 15:52:20 +0100

    comment adjustment

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/specific/uploadconfig/functional/FrameworkUploadOptions.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/specific/uploadconfig/functional/FrameworkUploadOptions.kt
@@ -1,7 +1,8 @@
 package org.dataland.frameworktoolbox.specific.uploadconfig.functional
 
 /**
- * A helper class to generate JS from options of multi-select components
+ * A helper class to pass typescript code to some property of a field in an Upload Config as a lambda body,
+ * as well as import statements at the top of the Upload Config.
  */
 class FrameworkUploadOptions(body: String, imports: Set<String>? = emptySet()) : FrameworkUploadBase(
     body = body,
