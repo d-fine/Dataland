5ce9ed58905fc5b4d8ab4811862a8bc4414ad967
Florian Soffel <florian.soffel@d-fine.de>
2024-01-02 12:37:58 +0100

    using coverage reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,6 +82,7 @@ sonar {
         property("sonar.coverage.jacoco.xmlReportPaths", file("$buildDir/reports/jacoco/test/jacocoTestReport.xml"))
         property("sonar.qualitygate.wait", true)
         property("sonar.javascript.lcov.reportPaths", fileTree("$projectDir/fe-coverage").files)
+        property("sonar.python.coverage.reportPaths", file("$projectDir/dataland-automated-qa-service/coverage.xml"))
         // TODO add reportspath here
         property(
             "sonar.coverage.exclusions",
