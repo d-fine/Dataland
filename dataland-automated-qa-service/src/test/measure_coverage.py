b1f81532c18950c93ebd80cfe88ff8d67848afa1
Florian Soffel <florian.soffel@d-fine.de>
2024-01-03 14:12:00 +0100

    start service via test

---
+++ dataland-automated-qa-service/src/test/measure_coverage.py
@@ -0,0 +1,12 @@
+import unittest
+
+from main.entrypoint import main
+
+
+class MeasureCoverage(unittest.TestCase):
+    def test_something(self):
+        main()
+        self.assertEqual(1,2)
+
+
+
