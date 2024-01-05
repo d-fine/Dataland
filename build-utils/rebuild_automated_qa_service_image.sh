23dc12c09fb812cc305ef48f90a37b09e7669c97
Florian Soffel <florian.soffel@d-fine.de>
2024-01-05 17:27:41 +0100

    split dockerfile

--- build-utils/rebuild_automated_qa_service_image.sh
+++ build-utils/rebuild_automated_qa_service_image.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service_base ./dataland-automated-qa-service/DockerfileBase ./dataland-automated-qa-service/
+
+set -o allexport
+source ./*github_env.log
+set +o allexport
+
 ./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service ./dataland-automated-qa-service/Dockerfile ./dataland-automated-qa-service/
