b43c4adeb9d27fd95275c4a8d3a93d0e19d5c164
Florian Soffel <florian.soffel@d-fine.de>
2024-01-05 18:00:47 +0100

    new production dockerfile

--- build-utils/rebuild_automated_qa_service_image.sh
+++ build-utils/rebuild_automated_qa_service_image.sh
@@ -7,4 +7,5 @@ set -o allexport
 source ./*github_env.log
 set +o allexport
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service ./dataland-automated-qa-service/Dockerfile ./dataland-automated-qa-service/
+./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service_test ./dataland-automated-qa-service/DockerfileTest ./dataland-automated-qa-service/
+./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service_production ./dataland-automated-qa-service/Dockerfile ./dataland-automated-qa-service/
