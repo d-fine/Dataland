9c0907e2c9e1212aa5f73136dd1652992cbb808b
Florian Soffel <florian.soffel@d-fine.de>
2024-01-05 15:36:02 +0100

    test waiting

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -62,7 +62,7 @@ echo XXXXXXXX 1
 docker exec dala-e2e-test-automated-qa-service-1 pkill -f --signal SIGINT coverage
 sleep 5
 echo XXXXXXXX 2
-timeout 90 sh -c "docker logs dala-e2e-test-automated-qa-service-1 --follow" > /dev/null
+#timeout 90 sh -c "docker logs dala-e2e-test-automated-qa-service-1 --follow" > /dev/null
 echo XXXXXXXX 3
 docker cp dala-e2e-test-automated-qa-service-1:/usr/src/app/.coverage ./automated-qa-service-bootRun-${CYPRESS_TEST_GROUP}.coverage
 
