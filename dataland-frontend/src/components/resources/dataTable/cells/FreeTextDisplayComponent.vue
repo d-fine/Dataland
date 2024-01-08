222ef4ed7919cd9c6a0b97e2162af9641a04c121
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2024-01-08 17:06:21 +0100

    Implemented toggle for hiding long texts in the FreeTextDisplayComponent

--- dataland-frontend/src/components/resources/dataTable/cells/FreeTextDisplayComponent.vue
+++ dataland-frontend/src/components/resources/dataTable/cells/FreeTextDisplayComponent.vue
@@ -1,5 +1,9 @@
 <template>
-  <span class="preserve-line-wrap" v-if="content.displayValue">{{ content.displayValue }}</span>
+  <span data-test="freetext-full" v-if="expanded || !teaserText" class="preserve-line-wrap">{{
+    content.displayValue
+  }}</span>
+  <span data-test="freetext-collapsed" v-else class="preserve-line-wrap">{{ teaserText }}</span>
+  <span data-test="freetext-toggle" v-if="teaserText" @click="expanded = !expanded" class="link">{{ toggleText }}</span>
 </template>
 
 <style scoped>
@@ -15,12 +19,29 @@ import {
   type MLDTDisplayObject,
 } from "@/components/resources/dataTable/MultiLayerDataTableCellDisplayer";
 
+const CUTOFF_CHARACTER_COUNT = 200;
+
 export default defineComponent({
   name: "FreeTextDisplayComponent",
   props: {
     content: {
       type: Object as () => MLDTDisplayObject<MLDTDisplayComponentName.FreeTextDisplayComponent>,
       required: true,
+    },
+  },
+  data() {
+    return {
+      expanded: false,
+    };
+  },
+  computed: {
+    teaserText(): string | null {
+      if (this.content.displayValue.length <= CUTOFF_CHARACTER_COUNT) return null;
+
+      return this.content.displayValue.slice(0, CUTOFF_CHARACTER_COUNT);
+    },
+    toggleText(): string {
+      return this.expanded ? "Show less" : "Show more";
     },
   },
 });
