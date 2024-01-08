4a9cf589c1c2a53874d67be80ca32101bbbc8b82
swaldow <stephan.waldow@d-fine.de>
2024-01-08 17:50:40 +0100

    small error fix

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/AdminDataManipulationApi.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/AdminDataManipulationApi.kt
@@ -7,9 +7,10 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 
 /**
  * Defines the restful dataland-backend API regarding deletion endpoint for admin users
@@ -39,7 +40,9 @@ fun interface AdminDataManipulationApi {
     )
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     fun deleteCompanyAssociatedData(
-        @Valid @RequestBody
+        @PathVariable("dataId")
+        @Valid
+        @RequestBody
         dataId: String,
     ):
         ResponseEntity<String>
