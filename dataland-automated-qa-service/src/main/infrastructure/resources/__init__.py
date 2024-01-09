30ef4f70cd8745002530edc0a6ff6eaa3ef64449
Florian Soffel <florian.soffel@d-fine.de>
2024-01-09 14:27:00 +0100

    dummy

--- dataland-automated-qa-service/src/main/infrastructure/resources/__init__.py
+++ dataland-automated-qa-service/src/main/infrastructure/resources/__init__.py
@@ -2,4 +2,4 @@ from .resource import Resource
 from .data_resource import DataResource
 from .document_resource import DocumentResource
 
-__all__ =  ["Resource", "DataResource", "DocumentResource"]
+__all__ = ["Resource", "DataResource", "DocumentResource"]
