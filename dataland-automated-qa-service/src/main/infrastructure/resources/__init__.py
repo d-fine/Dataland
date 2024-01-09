b49561253b69c5b95483c0766f94424094cd790c
Florian Soffel <florian.soffel@d-fine.de>
2024-01-09 12:58:56 +0100

    linting issue

--- dataland-automated-qa-service/src/main/infrastructure/resources/__init__.py
+++ dataland-automated-qa-service/src/main/infrastructure/resources/__init__.py
@@ -1,3 +1,5 @@
 from .resource import Resource
 from .data_resource import DataResource
 from .document_resource import DocumentResource
+
+__all__ = ["Resource", "DataResource", "DocumentResource"]
