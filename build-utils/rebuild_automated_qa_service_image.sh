6fcc627278ce6bccb85b1fa0e6efbbcc0583cda3
Florian Soffel <florian.soffel@d-fine.de>
2024-01-09 12:48:23 +0100

    cleanup

--- build-utils/rebuild_automated_qa_service_image.sh
+++ build-utils/rebuild_automated_qa_service_image.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service_base ./dataland-automated-qa-service/DockerfileBase ./dataland-automated-qa-service/
+./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service_base ./dataland-automated-qa-service/DockerfileBase ./dataland-automated-qa-service/ \
+          ./dataland-backend/backendOpenApi.json ./dataland-document-manager/documentManagerOpenApi.json
 
 set -o allexport
 source ./*github_env.log
 set +o allexport
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service_test ./dataland-automated-qa-service/DockerfileTest ./dataland-automated-qa-service/
-./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service_production ./dataland-automated-qa-service/Dockerfile ./dataland-automated-qa-service/
+./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service_test ./dataland-automated-qa-service/DockerfileTest ./dataland-automated-qa-service/ \
+          ./dataland-backend/backendOpenApi.json ./dataland-document-manager/documentManagerOpenApi.json
+./build-utils/base_rebuild_single_docker_image.sh dataland_automated_qa_service_production ./dataland-automated-qa-service/Dockerfile ./dataland-automated-qa-service/ \
+          ./dataland-backend/backendOpenApi.json ./dataland-document-manager/documentManagerOpenApi.json
