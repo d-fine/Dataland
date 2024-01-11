06b94cc8f2f20bad6103fc45de38bbee68ed1c12
Florian Soffel <florian.soffel@d-fine.de>
2024-01-11 10:34:35 +0100

    bugfix

--- dataland-automated-qa-service/src/test/measure_coverage.py
+++ dataland-automated-qa-service/src/test/measure_coverage.py
@@ -4,5 +4,6 @@ from main.entrypoint import main
 
 
 class MeasureCoverage(unittest.TestCase):
-    def test_entrypoint(self) -> None:  # noqa: PLR6301
+    def test_entrypoint(self) -> None:
         main()
+        self.assertTrue(False)
