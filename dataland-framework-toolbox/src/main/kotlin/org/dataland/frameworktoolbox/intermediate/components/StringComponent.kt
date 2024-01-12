14de4aa389e41b1b5ac50f2c8e62942806204d16
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-01-12 10:33:19 +0100

    sonar issue - reduce duplications by introducing a base-component for text-components in the devtools

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/intermediate/components/StringComponent.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/intermediate/components/StringComponent.kt
@@ -1,8 +1,7 @@
 package org.dataland.frameworktoolbox.intermediate.components
 
 import org.dataland.frameworktoolbox.intermediate.FieldNodeParent
-import org.dataland.frameworktoolbox.specific.datamodel.TypeReference
-import org.dataland.frameworktoolbox.specific.datamodel.elements.DataClassBuilder
+import org.dataland.frameworktoolbox.intermediate.components.basecomponents.TextBaseComponent
 import org.dataland.frameworktoolbox.specific.fixturegenerator.elements.FixtureSectionBuilder
 import org.dataland.frameworktoolbox.specific.uploadconfig.elements.UploadCategoryBuilder
 import org.dataland.frameworktoolbox.specific.viewconfig.elements.SectionConfigBuilder
@@ -15,17 +14,7 @@ import org.dataland.frameworktoolbox.specific.viewconfig.functional.FrameworkDis
 class StringComponent(
     identifier: String,
     parent: FieldNodeParent,
-) : ComponentBase(identifier, parent) {
-
-    override fun generateDefaultDataModel(dataClassBuilder: DataClassBuilder) {
-        dataClassBuilder.addProperty(
-            identifier,
-            documentSupport.getJvmTypeReference(
-                TypeReference("String", isNullable),
-                isNullable,
-            ),
-        )
-    }
+) : TextBaseComponent(identifier, parent) {
 
     override fun generateDefaultViewConfig(sectionConfigBuilder: SectionConfigBuilder) {
         sectionConfigBuilder.addStandardCellWithValueGetterFactory(
