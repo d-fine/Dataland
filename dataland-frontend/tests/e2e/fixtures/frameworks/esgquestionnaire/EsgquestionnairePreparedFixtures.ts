9ea56c96b7788cd57dbc939459adc672def0267f
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-01-15 12:45:45 +0100

    introduce prepared fixtures

--- dataland-frontend/tests/e2e/fixtures/frameworks/esgquestionnaire/EsgquestionnairePreparedFixtures.ts
+++ dataland-frontend/tests/e2e/fixtures/frameworks/esgquestionnaire/EsgquestionnairePreparedFixtures.ts
@@ -1,5 +1,5 @@
 import { type FixtureData } from "@sharedUtils/Fixtures";
-import { type EsgquestionnaireData } from "@clients/backend";
+import {BerichterstattungEnergieverbrauchValues, type EsgquestionnaireData, YesNo} from "@clients/backend";
 import { generateEsgquestionnaireFixtures } from "./EsgquestionnaireDataFixtures";
 
 /**
@@ -10,14 +10,145 @@ import { generateEsgquestionnaireFixtures } from "./EsgquestionnaireDataFixtures
 export function generateEsgquestionnairePreparedFixtures(): Array<FixtureData<EsgquestionnaireData>> {
   const preparedFixtures = [];
   // Note: Put the code for prepared fixture generation below. This file will not be overwritten automatically
-
-  const manipulatorFunctions: Array<(input: FixtureData<EsgquestionnaireData>) => FixtureData<EsgquestionnaireData>> =
-    [];
-  const preparedFixturesBeforeManipulation = generateEsgquestionnaireFixtures(manipulatorFunctions.length);
-
-  for (let i = 0; i < manipulatorFunctions.length; i++) {
-    preparedFixtures.push(manipulatorFunctions[i](preparedFixturesBeforeManipulation[i]));
-  }
-
+  preparedFixtures.push(generateFixtureForNoNullFieldsAndOnlyYesAnswers());
   return preparedFixtures;
+}
+
+/**
+ * Generates an esg-questionnaire fixture with no null values and all YesNo fields set to "Yes"
+ * @returns the fixture
+ */
+function generateFixtureForNoNullFieldsAndOnlyYesAnswers(): FixtureData<EsgquestionnaireData> {
+  const newFixture = generateEsgquestionnaireFixtures(1, 0)[0];
+  newFixture.companyInformation.companyName = "Esgquestionnaire-dataset-with-no-null-fields";
+
+  modifyPreparedFixturesPartOne(newFixture);
+  modifyPreparedFixturesPartTwo(newFixture);
+  return newFixture;
+}
+
+/**
+ * Modifies the prepared fixtures
+ * @param newFixture generated prepared fixture to modify
+ */
+function modifyPreparedFixturesPartOne(newFixture: FixtureData<EsgquestionnaireData>): void {
+  if (newFixture.t.general?.masterData) {
+    newFixture.t.general.masterData.berichtspflichtUndEinwilligungZurVeroeffentlichung = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.esgZiele?.existenzVonEsgZielen) {
+    newFixture.t.allgemein.esgZiele.existenzVonEsgZielen = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.sektoren?.sektorenMitHohenKlimaauswirkungen) {
+    newFixture.t.allgemein.sektoren.sektorenMitHohenKlimaauswirkungen = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.esgBerichte?.nachhaltigkeitsberichte) {
+    newFixture.t.allgemein.esgBerichte.nachhaltigkeitsberichte = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.unGlobalConceptPrinzipien?.mechanismenZurUeberwachungDerEinhaltungDerUngcp) {
+    newFixture.t.allgemein.unGlobalConceptPrinzipien.mechanismenZurUeberwachungDerEinhaltungDerUngcp = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.oecdLeitsaetze?.mechanismenZurUeberwachungDerEinhaltungDerOecdLeitsaetze) {
+    newFixture.t.allgemein.oecdLeitsaetze.mechanismenZurUeberwachungDerEinhaltungDerOecdLeitsaetze = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.sonstige?.ausschlusslistenAufBasisVonEsgKriterien) {
+    newFixture.t.allgemein.sonstige.ausschlusslistenAufBasisVonEsgKriterien = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.fuehrungsstandards?.oekologischeSozialeFuehrungsstandardsOderPrinzipien) {
+    newFixture.t.allgemein.fuehrungsstandards.oekologischeSozialeFuehrungsstandardsOderPrinzipien = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.rechtsstreitigkeiten?.esgBezogeneRechtsstreitigkeiten) {
+    newFixture.t.allgemein.rechtsstreitigkeiten.esgBezogeneRechtsstreitigkeiten = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.rechtsstreitigkeiten?.rechtsstreitigkeitenMitBezugZuE) {
+    newFixture.t.allgemein.rechtsstreitigkeiten.rechtsstreitigkeitenMitBezugZuE = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.rechtsstreitigkeiten?.rechtsstreitigkeitenMitBezugZuS) {
+    newFixture.t.allgemein.rechtsstreitigkeiten.rechtsstreitigkeitenMitBezugZuS = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.rechtsstreitigkeiten?.rechtsstreitigkeitenMitBezugZuG) {
+    newFixture.t.allgemein.rechtsstreitigkeiten.rechtsstreitigkeitenMitBezugZuG = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.rating?.esgRating) {
+    newFixture.t.allgemein.rating.esgRating = YesNo.Yes;
+  }
+}
+
+/**
+ * Modifies the prepared fixtures
+ * @param newFixture generated prepared fixture to modify
+ */
+function modifyPreparedFixturesPartTwo(newFixture: FixtureData<EsgquestionnaireData>): void {
+  if (newFixture.t.allgemein?.rating?.ratingbericht) {
+    newFixture.t.allgemein.rating.ratingbericht.value = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.anleihen?.grueneSozialeUndOderNachhaltigeEmissionen) {
+    newFixture.t.allgemein.anleihen.grueneSozialeUndOderNachhaltigeEmissionen = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.anleihen?.sustainibilityLinkedDebt) {
+    newFixture.t.allgemein.anleihen.sustainibilityLinkedDebt = YesNo.Yes;
+  }
+  if (newFixture.t.allgemein?.esgBerichte?.aktuelleBerichte) {
+    newFixture.t.allgemein?.esgBerichte?.aktuelleBerichte.push({
+      value: "Report with null data source",
+      dataSource: null,
+    });
+  }
+  if (newFixture.t.umwelt?.produktion?.produkteZurVerringerungDerUmweltbelastung) {
+    newFixture.t.umwelt.produktion.produkteZurVerringerungDerUmweltbelastung = YesNo.Yes;
+  }
+  if (newFixture.t.umwelt?.produktion?.oekologischerMindestStandardFuerProduktionsprozesse) {
+    newFixture.t.umwelt.produktion.oekologischerMindestStandardFuerProduktionsprozesse = YesNo.Yes;
+  }
+  if (newFixture.t.umwelt?.energieverbrauch?.berichterstattungEnergieverbrauch) {
+    const currentYear = newFixture.t.umwelt.energieverbrauch.berichterstattungEnergieverbrauch.currentYear;
+    newFixture.t.umwelt.energieverbrauch.berichterstattungEnergieverbrauch.yearlyData =
+        createEnergieverbrauchWithNullNumbers(currentYear);
+  }
+  if (newFixture.t.umwelt?.biodiversitaet?.negativeAktivitaetenFuerDieBiologischeVielfalt) {
+    newFixture.t.umwelt.biodiversitaet.negativeAktivitaetenFuerDieBiologischeVielfalt = YesNo.Yes;
+  }
+  if (newFixture.t.umwelt?.biodiversitaet?.positiveAktivitaetenFuerDieBiologischeVielfalt) {
+    newFixture.t.umwelt.biodiversitaet.positiveAktivitaetenFuerDieBiologischeVielfalt = YesNo.Yes;
+  }
+  if (newFixture.t.umwelt?.fossileBrennstoffe?.einnahmenAusFossilenBrennstoffen) {
+    newFixture.t.umwelt.fossileBrennstoffe.einnahmenAusFossilenBrennstoffen = YesNo.Yes;
+  }
+  if (
+      newFixture.t.soziales?.unternehmensstrukturaenderungen?.vorhandenseinKuerzlicherAenderungenDerUnternehmensstruktur
+  ) {
+    newFixture.t.soziales.unternehmensstrukturaenderungen.vorhandenseinKuerzlicherAenderungenDerUnternehmensstruktur =
+        YesNo.Yes;
+  }
+  if (newFixture.t.soziales?.audit) {
+    newFixture.t.soziales.audit.auditsZurEinhaltungVonArbeitsstandards = YesNo.Yes;
+  }
+  if (newFixture.t.unternehmensfuehrungGovernance?.sonstige?.trennungVonCeoOderVorsitzenden) {
+    newFixture.t.unternehmensfuehrungGovernance.sonstige.trennungVonCeoOderVorsitzenden = YesNo.Yes;
+  }
+  if (newFixture.t.unternehmensfuehrungGovernance?.stakeholder?.einbeziehungVonStakeholdern) {
+    newFixture.t.unternehmensfuehrungGovernance.stakeholder.einbeziehungVonStakeholdern = YesNo.Yes;
+  }
+  if (newFixture.t.unternehmensfuehrungGovernance?.lieferantenauswahl?.esgKriterienUndUeberwachungDerLieferanten) {
+    newFixture.t.unternehmensfuehrungGovernance.lieferantenauswahl.esgKriterienUndUeberwachungDerLieferanten =
+        YesNo.Yes;
+  }
+}
+
+/**
+ * Creates the contents for a time series based on a current year value and sets null for all numbers
+ * @param currentYear is the base year for the time series
+ * @returns the map of years to time series data objects
+ */
+function createEnergieverbrauchWithNullNumbers(currentYear: number): {
+  [p: string]: BerichterstattungEnergieverbrauchValues;
+} {
+  const objectToReturn: { [p: string]: BerichterstattungEnergieverbrauchValues } = {};
+  for (let i = -3; i <= 3; i++) {
+    objectToReturn[(currentYear + i).toString()] = {
+      energieverbrauch: null,
+      prozentDesVerbrauchsErneuerbarerEnergien: null,
+      ggfProzentDerErneuerbarenEnergieerzeugung: null,
+    };
+  }
+  return objectToReturn;
 }
