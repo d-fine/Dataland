9f2a0156ad95faf270afdd5f1d0ab5fba3086ab8
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-01-15 13:10:17 +0100

    increasing coverage

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/DataOwnerControllerTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/DataOwnerControllerTest.kt
@@ -48,7 +48,12 @@ class DataOwnerControllerTest {
         assertFailingApiUploadToCompany(secondCompanyId, frameworkSampleData, false)
 
         jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.Admin)
-        apiAccessor.companyDataControllerApi.postDataOwner(firstCompanyId, dataReaderUserId)
+        val dataOwnersAfterPostRequest = apiAccessor.companyDataControllerApi.postDataOwner(firstCompanyId, dataReaderUserId)
+        validateDataOwnersForCompany(firstCompanyId, listOf(dataReaderUserId), dataOwnersAfterPostRequest)
+        val dataOwnersAfterDuplicatePostRequest = apiAccessor.companyDataControllerApi
+            .postDataOwner(firstCompanyId, dataReaderUserId)
+        validateDataOwnersForCompany(firstCompanyId, listOf(dataReaderUserId), dataOwnersAfterDuplicatePostRequest)
+
         jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.Reader)
 
         assertSucceedingApiUploadToCompany(firstCompanyId, frameworkSampleData)
