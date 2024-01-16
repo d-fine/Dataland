b3fa6a3080c39914088a0b4c053810ed32e8bf21
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-01-16 11:35:48 +0100

    multiple reports component from gdv added

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/heimathafen/HeimathafenFramework.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/heimathafen/HeimathafenFramework.kt
@@ -2,9 +2,12 @@ package org.dataland.frameworktoolbox.frameworks.heimathafen
 
 import HeimathafenComponentGenerationUtils
 import org.dataland.frameworktoolbox.frameworks.InDevelopmentPavedRoadFramework
+import org.dataland.frameworktoolbox.frameworks.heimathafen.custom.HeimathafenListOfBaseDataPointComponent
 import org.dataland.frameworktoolbox.intermediate.Framework
 import org.dataland.frameworktoolbox.intermediate.group.ComponentGroup
+import org.dataland.frameworktoolbox.intermediate.group.create
 import org.dataland.frameworktoolbox.intermediate.group.edit
+import org.dataland.frameworktoolbox.intermediate.group.getOrNull
 import org.dataland.frameworktoolbox.template.components.ComponentGenerationUtils
 import org.springframework.stereotype.Component
 import java.io.File
@@ -32,5 +35,26 @@ class HeimathafenFramework : InDevelopmentPavedRoadFramework(
                 viewPageExpandOnPageLoad = true
             }
         }
+        val componentGroupGeneral = framework.root.getOrNull<ComponentGroup>("General")
+        if (componentGroupGeneral != null) {
+            componentGroupGeneral.getOrNull<ComponentGroup>("Methodik")
+                ?.create<HeimathafenListOfBaseDataPointComponent>("datenquelle") {
+                    label = "Datenquelle"
+                    explanation = "Welche Quellen werden für die Datenerhebung verwendet? Angabe von Quellen für die" +
+                        " Datenerhebung, zum Beispiel Nachhaltigkeitsberichte von Unternehmen, Daten von NGOs etc."
+                    descriptionColumnHeader = "Beschreibung des Berichts"
+                    documentColumnHeader = "Datenquelle"
+                }
+        }
+
+        framework.root.getOrNull<ComponentGroup>("Environmental")
+            ?.getOrNull<ComponentGroup>("Nachhaltigskeitsrisiken")
+            ?.create<HeimathafenListOfBaseDataPointComponent>("quelle") {
+                label = "Quelle"
+                explanation = "Welche Quellen werden für die Erfassung von Nachhaltigkeitsrisiken im Bereich Umwelt" +
+                    "verwendet? Angabe von Quellen, zum Beispiel Geschäftsberichte von Unternehmen," +
+                    " Daten von NGOs etc."
+                documentColumnHeader = "Quelle"
+            }
     }
 }
