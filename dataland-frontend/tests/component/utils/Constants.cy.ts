e191a7f3a29ee9a56f0e4e2705b9ed21f9f2b2b7
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-01-17 13:23:59 +0100

    fix more compilation errors

--- dataland-frontend/tests/component/utils/Constants.cy.ts
+++ dataland-frontend/tests/component/utils/Constants.cy.ts
@@ -3,18 +3,18 @@ import { DataTypeEnum } from "@clients/backend";
 
 describe("Unit test for the data type sorting in the Constants", () => {
   it("Check if esg questionnaire is put at the end of a list of data type enums", () => {
-    const testDataTypeEnums = [DataTypeEnum.P2p, DataTypeEnum.Esgquestionnaire, DataTypeEnum.Lksg, DataTypeEnum.Sme];
+    const testDataTypeEnums = [DataTypeEnum.P2p, DataTypeEnum.EsgQuestionnaire, DataTypeEnum.Lksg, DataTypeEnum.Sme];
     const sortedTestDataTypeEnums = putEsgQuestionnaireAtTheEndOfList(testDataTypeEnums);
     expect(sortedTestDataTypeEnums.length).to.equal(testDataTypeEnums.length);
     expect(sortedTestDataTypeEnums[0]).to.equal(DataTypeEnum.P2p);
     expect(sortedTestDataTypeEnums[1]).to.equal(DataTypeEnum.Lksg);
     expect(sortedTestDataTypeEnums[2]).to.equal(DataTypeEnum.Sme);
-    expect(sortedTestDataTypeEnums[3]).to.equal(DataTypeEnum.Esgquestionnaire);
+    expect(sortedTestDataTypeEnums[3]).to.equal(DataTypeEnum.EsgQuestionnaire);
   });
 
   it("Check if the order stays the same if a list of data type enums does not contain esgquestionnaire", () => {
     const allDataTypeEnumsButEsgquestionnaire = Object.values(DataTypeEnum).filter(
-      (it) => it !== DataTypeEnum.Esgquestionnaire,
+      (it) => it !== DataTypeEnum.EsgQuestionnaire,
     );
     const sortedAllDataTypeEnumsButEsgquestionnaire = putEsgQuestionnaireAtTheEndOfList(
       allDataTypeEnumsButEsgquestionnaire,
