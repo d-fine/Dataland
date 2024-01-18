126d26ecc60bf2a1165ab965d1cc71d8b1598157
swaldow <stephan.waldow@d-fine.de>
2024-01-18 09:56:13 +0100

    script deletes data as intended

--- developer-tools/deleteData.sh
+++ developer-tools/deleteData.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-key=$1
-#key="MTM2YTkzOTQtNDg3My00YTYxLWEyNWItNjViMWU4ZTdjYzJm_e74cf78e106fe605299138e432512b791485367753c03a60d9088ce421f0141189f945fe428c8a7f_2733262788"
+#The script is intended to provide a fast way for the developer to delete datasets using the company isin
+#Currently this script is written for eutaxonomy-non-financials and eutaxnomy-financials
+#Inputs necessary for this script are the server environment (e.g. dev1.dataland.com) and the api key on this environment
+
+server=$1
+key=$2
 isin_file=isin_non_financial.txt
 company_file=company.txt
 data_file=data.txt
-
 #TODO create backup before deleting the actual data
 #TODO make sure it works for the intended data
 #TODO check that the companies are gone from the company search page
@@ -16,15 +19,13 @@ for framework in eutaxonomy-non-financials eutaxonomy-financials; do
   isin_file=isin_$framework.txt
 while read -r isin; do
   echo "Processing ISIN $isin"
-  echo "https://local-dev.dataland.com/api/companies?searchString=$isin"
-  curl -X 'GET' "https://local-dev.dataland.com/api/companies?searchString=$isin" -H 'accept: application/json' -H "Authorization: Bearer $key" | python -m json.tool --sort-keys > "$company_file"
+  curl -X GET "https://$server/api/companies?searchString=$isin" -H 'accept: application/json' -H "Authorization: Bearer $key" | python -m json.tool --sort-keys > "$company_file"
   company_id=$(grep "companyId" "$company_file" | cut -d'"' -f4 | sort -u)
   echo "Found company ID $company_id for ISIN $isin"
-  curl -X 'GET' "https://local-dev.dataland.com/api/data/$framework/companies/$company_id?showOnlyActive=true" -H 'accept: application/json' -H "Authorization: Bearer $key" | python -m json.tool --sort-keys > "$data_file"
+  curl -X 'GET' "https://$server/api/data/$framework/companies/$company_id?showOnlyActive=true" -H 'accept: application/json' -H "Authorization: Bearer $key" | python -m json.tool --sort-keys > "$data_file"
   data_id=$(grep "dataId" "$data_file" | cut -d'"' -f4 | sort -u)
   echo "Found data ID $data_id for ISIN $isin"
-  curl -X 'DELETE' "https://local-dev.dataland.com/api/data/$data_id" -H 'accept: */*' -H "Authorization: Bearer $key"
+  curl -X 'DELETE' "https://$server/api/data/$data_id" -H 'accept: */*' -H "Authorization: Bearer $key"
 
-  #TODO add command for deletion
 done < $isin_file
 done
