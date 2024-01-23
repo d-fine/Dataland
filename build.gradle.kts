76dda3da0cf5de0bfa98b7bf9dc213979e20bb60
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-01-23 14:18:04 +0100

    sonar exlcusion

--- build.gradle.kts
+++ build.gradle.kts
@@ -131,7 +131,11 @@ sonar {
                 "dataland-frontend/src/components/generics/TheNewFooter.vue" +
                 "dataland-frontend/src/components/pages/AboutPage.vue" +
                 "dataland-frontend/src/components/generics/TheNewHeader.vue" +
-                "dataland-frontend/src/components/resources/aboutPage/**",
+                "dataland-frontend/src/components/resources/aboutPage/TheAboutPrinciples.vue" +
+                "dataland-frontend/src/components/resources/aboutPage/TheAboutIntro.vue" +
+                "dataland-frontend/src/components/resources/newLandingPage/TheIntro.vue" +
+                "dataland-frontend/src/components/generics/TheNewHeader.vue" +
+                "dataland-frontend/src/components/pages/AboutPage.vue",
         )
     }
 }
