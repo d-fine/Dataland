90e7bf079671ab9a97b79d1fa645b2c249bed9d1
Florian Soffel <florian.soffel@d-fine.de>
2024-01-24 11:17:21 +0100

    fix unit test

--- dataland-api-key-manager/src/test/kotlin/org/datalandapikeymanager/services/ApiKeyManagerTest.kt
+++ dataland-api-key-manager/src/test/kotlin/org/datalandapikeymanager/services/ApiKeyManagerTest.kt
@@ -13,9 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.ComponentScan
 
-@ComponentScan(basePackages = ["org.dataland"])
 @SpringBootTest(classes = [DatalandApiKeyManager::class], properties = ["spring.profiles.active=nodb"])
 @Transactional
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
