5bc0a4b29af41c6f815adbe4b20f984fb7622eba
Florian Soffel <florian.soffel@d-fine.de>
2024-01-25 10:24:12 +0100

    Add unit tests for email builders

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/email/DataOwnershipRequestEmailBuilderTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/email/DataOwnershipRequestEmailBuilderTest.kt
@@ -1,13 +1,18 @@
 package org.dataland.datalandbackend.email
 
+import org.dataland.datalandbackend.services.DataOwnershipRequestEmailBuilder
 import org.dataland.datalandbackendutils.email.BaseEmailBuilder
 import org.dataland.datalandbackendutils.email.Email
 import org.dataland.datalandbackendutils.email.EmailContact
+import org.dataland.keycloakAdapter.auth.DatalandAuthentication
+import org.dataland.keycloakAdapter.auth.DatalandJwtAuthentication
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 
-class BaseEmailBuilderTest {
+class DataOwnershipRequestEmailBuilderTest {
     @Test
     fun `validate that the output of the base email builder is correctly formatted`() {
         val senderEmail = "sender@dataland.com"
@@ -19,33 +24,25 @@ class BaseEmailBuilderTest {
         val ccEmails = listOf(
             "cc1@dataland.com",
         )
-        val subject = "The Subject"
-        val textTitle = "The Text Title"
-        val htmlTitle = "The HTML Title"
-        val properties = mapOf(
-            "first" to "1",
-            "second" to "2",
-        )
-        val email = object : BaseEmailBuilder(
+        val environment = "test.dataland.com"
+        val mockAuthentication = mock(DatalandJwtAuthentication::class.java)
+        val companyId = "8"
+        val comment = "This is a comment"
+        val userId = "user-id"
+        val username = "i.love@dataland.com"
+        `when`(mockAuthentication.userId).thenReturn(userId)
+        `when`(mockAuthentication.username).thenReturn(username)
+        val email = DataOwnershipRequestEmailBuilder(
+            proxyPrimaryUrl = environment,
             senderEmail = senderEmail,
             senderName = senderName,
             semicolonSeparatedReceiverEmails = receiverEmails.joinToString(";"),
             semicolonSeparatedCcEmails = ccEmails.joinToString(";"),
-        ) {
-            fun build(): Email {
-                return Email(
-                    senderEmailContact,
-                    receiverEmailContacts,
-                    ccEmailContacts,
-                    buildPropertyStyleEmailContent(
-                        subject,
-                        textTitle,
-                        htmlTitle,
-                        properties,
-                    )
-                )
-            }
-        }.build()
+        ).buildDataOwnershipRequest(
+            companyId,
+            mockAuthentication,
+            comment
+        )
 
         assertEquals(EmailContact(senderEmail, senderName), email.sender)
         assertEquals(receiverEmails.size, email.receivers.size)
@@ -56,15 +53,13 @@ class BaseEmailBuilderTest {
         ccEmails.forEachIndexed { index, it ->
             assertEquals(EmailContact(it), email.cc!![index])
         }
-        assertTrue(email.content.textContent.contains(textTitle))
-        assertTrue(!email.content.textContent.contains(htmlTitle))
-        assertTrue(!email.content.htmlContent.contains(textTitle))
-        assertTrue(email.content.htmlContent.contains("<div class=\"header\">$htmlTitle</div>"))
-        properties.forEach {
+        mapOf(
+            "Environment" to environment,
+            "User" to "User $username (Keycloak ID: $userId)",
+            "Company (Dataland ID)" to companyId,
+            "Comment" to comment,
+        ).forEach {
             assertTrue(email.content.textContent.contains("${it.key}: ${it.value}"))
-            assertTrue(email.content.htmlContent.contains("""
-                <div class="section"> <span class="bold">${it.key}: </span> ${it.value} </div>
-            """.trimIndent()))
         }
     }
 }
