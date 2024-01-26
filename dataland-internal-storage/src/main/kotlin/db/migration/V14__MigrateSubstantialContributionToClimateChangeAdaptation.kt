0d3ab1e1e8cba65559eae119b97dfa4af9ff50e5
Stephan Schulte <stephan.schulte@d-fine.com>
2024-01-26 09:52:58 +0100

    fixed some ktlint errors

--- dataland-internal-storage/src/main/kotlin/db/migration/V14__MigrateSubstantialContributionToClimateChangeAdaptation.kt
+++ dataland-internal-storage/src/main/kotlin/db/migration/V14__MigrateSubstantialContributionToClimateChangeAdaptation.kt
@@ -2,9 +2,7 @@ package db.migration
 
 import db.migration.utils.DataTableEntity
 import db.migration.utils.getOrJavaNull
-import db.migration.utils.getOrJsonNull
 import db.migration.utils.migrateCompanyAssociatedDataOfDatatype
-import org.dataland.datalandbackend.openApiClient.model.Activity
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
 import org.json.JSONObject
@@ -13,9 +11,10 @@ import org.json.JSONObject
  * This migration script updates the existing SFDR datasets, more specifically it introduces an additional layer to
  * each sector in the high-impact climate sectors
  */
-class V14__MigrateSubstantialContributionToClimateChangeAdaption : BaseJavaMigration() {
+class V14__MigrateSubstantialContributionToClimateChangeAdaptation : BaseJavaMigration() {
     private val mapOfOldToNewFieldNames = mapOf(
-        "substantialContributionToClimateChangeAdaptionInPercent" to "substantialContributionToClimateChangeAdaptationInPercent",
+        "substantialContributionToClimateChangeAdaptionInPercent"
+            to "substantialContributionToClimateChangeAdaptationInPercent",
     )
     private val mapOfOldToNewdDnshFieldName = mapOf(
         "dnshToClimateChangeAdaption" to "dnshToClimateChangeAdaptation",
@@ -31,18 +30,24 @@ class V14__MigrateSubstantialContributionToClimateChangeAdaption : BaseJavaMigra
 
         listOf("revenue", "capex", "opex").forEach { cashFlowType ->
             mapOfOldToNewFieldNames.forEach {
-                euTaxoDataset2.getJSONObject(cashFlowType).put(it.value, euTaxoDataset2.getJSONObject(cashFlowType).getInt(it.key))
+                euTaxoDataset2.getJSONObject(cashFlowType).put(
+                    it.value,
+                    euTaxoDataset2.getJSONObject(cashFlowType).getInt(it.key),
+                )
                 euTaxoDataset2.getJSONObject(cashFlowType).remove(it.key)
                 euTaxoDataset2.getJSONObject(cashFlowType).getJSONArray("alignedActivities").forEach { actitivy ->
                     actitivy as JSONObject
                     actitivy.put(it.value, actitivy.get(it.key))
                     actitivy.remove(it.key)
-                    actitivy.put(mapOfOldToNewdDnshFieldName.values.first(), actitivy.get(mapOfOldToNewdDnshFieldName.keys.first()))
+                    actitivy.put(
+                        mapOfOldToNewdDnshFieldName.values.first(),
+                        actitivy.get(mapOfOldToNewdDnshFieldName.keys.first()),
+                    )
                     actitivy.remove(mapOfOldToNewdDnshFieldName.keys.first())
                 }
             }
         }
-        euTaxoDataset.put("data",euTaxoDataset2)
+        euTaxoDataset.put("data", euTaxoDataset2)
         dataTableEntity.companyAssociatedData.put("data", euTaxoDataset.toString())
         println(dataTableEntity.companyAssociatedData)
     }
