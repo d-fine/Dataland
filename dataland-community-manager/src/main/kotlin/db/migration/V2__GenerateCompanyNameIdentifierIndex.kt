431f03a5228ef8cf0501d8cf1fe1f2cbd3146860
Florian Soffel <florian.soffel@d-fine.de>
2024-01-26 11:38:51 +0100

    add migration scripts

---
+++ dataland-community-manager/src/main/kotlin/db/migration/V2__GenerateCompanyNameIdentifierIndex.kt
@@ -0,0 +1,19 @@
+package db.migration
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+/**
+ * This migration script creates an index for a faster search for company names and identifiers
+ */
+class V2__ExtendDataRequest : BaseJavaMigration() {
+    override fun migrate(context: Context?) {
+        context!!.connection.createStatement().execute(
+            "ALTER TABLE data_requests " +
+                "ADD COLUMN reporting_period varchar(255) NOT NULL DEFAULT '2022', " +
+                "ADD COLUMN message_history varchar(4000), " +
+                "ADD COLUMN last_modified_date bigint NOT NULL DEFAULT (extract(epoch from now()) * 1000)::bigint, " +
+                "ADD COLUMN request_status varchar(255) NOT NULL DEFAULT 'Open'",
+        )
+    }
+}
