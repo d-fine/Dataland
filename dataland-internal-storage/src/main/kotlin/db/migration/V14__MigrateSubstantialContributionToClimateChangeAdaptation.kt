acf15ce0badcdf396f7b14094766938c1b0eb546
Stephan Schulte <stephan.schulte@d-fine.com>
2024-01-26 09:14:13 +0100

    corrected the final typo in the exptected.json

--- dataland-internal-storage/src/main/kotlin/db/migration/V14__MigrateSubstantialContributionToClimateChangeAdaptation.kt
+++ dataland-internal-storage/src/main/kotlin/db/migration/V14__MigrateSubstantialContributionToClimateChangeAdaptation.kt
@@ -17,47 +17,35 @@ class V14__MigrateSubstantialContributionToClimateChangeAdaption : BaseJavaMigra
     private val mapOfOldToNewFieldNames = mapOf(
         "substantialContributionToClimateChangeAdaptionInPercent" to "substantialContributionToClimateChangeAdaptationInPercent",
     )
-
+    private val mapOfOldToNewdDnshFieldName = mapOf(
+        "dnshToClimateChangeAdaption" to "dnshToClimateChangeAdaptation",
+    )
 
     /**
      * Migrates substantialContributionToClimateChangeAdaption field to substantialContributionToClimateChangeAdaptation
      */
     fun migrateSubstantialContributionToClimateChangeAdaption(dataTableEntity: DataTableEntity) {
         val companyAssociatedDatasetAsString = dataTableEntity.companyAssociatedData
-        println("Here")
-        println(companyAssociatedDatasetAsString)
-        println("Here2")
         val euTaxoDataset = JSONObject(companyAssociatedDatasetAsString.getString("data"))
-        println(euTaxoDataset)
-        println("Here3")
         val euTaxoDataset2 = euTaxoDataset.getOrJavaNull("data") as JSONObject
-        println(euTaxoDataset2)
 
         listOf("revenue", "capex", "opex").forEach { cashFlowType ->
             mapOfOldToNewFieldNames.forEach {
                 euTaxoDataset2.getJSONObject(cashFlowType).put(it.value, euTaxoDataset2.getJSONObject(cashFlowType).getInt(it.key))
                 euTaxoDataset2.getJSONObject(cashFlowType).remove(it.key)
-                println(euTaxoDataset2.getJSONObject(cashFlowType).getJSONArray("alignedActivities").getJSONObject(0).get("substantialContributionToClimateChangeAdaptionInPercent"))
-                println(euTaxoDataset2.getJSONObject(cashFlowType).getJSONArray("alignedActivities").getJSONObject(0).get("activityName"))
                 euTaxoDataset2.getJSONObject(cashFlowType).getJSONArray("alignedActivities").forEach { actitivy ->
                     actitivy as JSONObject
                     actitivy.put(it.value, actitivy.get(it.key))
-                    actitivy.remove((it.key))
+                    actitivy.remove(it.key)
+                    actitivy.put(mapOfOldToNewdDnshFieldName.values.first(), actitivy.get(mapOfOldToNewdDnshFieldName.keys.first()))
+                    actitivy.remove(mapOfOldToNewdDnshFieldName.keys.first())
                 }
             }
         }
-        println("Here5")
-        println(euTaxoDataset2)
-        println("Here6")
         euTaxoDataset.put("data",euTaxoDataset2)
-        println(euTaxoDataset)
         dataTableEntity.companyAssociatedData.put("data", euTaxoDataset.toString())
         println(dataTableEntity.companyAssociatedData)
     }
-    /**
-     * This function iterates through all cash flow categories in order to migrate the "DataSource" Object
-     */
-
 
     override fun migrate(context: Context?) {
         migrateCompanyAssociatedDataOfDatatype(
