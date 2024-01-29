f8defd55c880196f266f8ec4c75bf247e67df60d
Benedikt Laubmann <Benedikt.Laubmann@d-fine.at>
2024-01-29 13:15:57 +0100

    style panels

--- dataland-frontend/src/components/resources/companyCockpit/ClaimOwnershipPanel.vue
+++ dataland-frontend/src/components/resources/companyCockpit/ClaimOwnershipPanel.vue
@@ -48,19 +48,12 @@ export default defineComponent({
   border-radius: $radius-xxs;
   text-align: left;
   box-shadow: 0 0 12px var(--gray-300);
-  cursor: pointer;
 
-  &:hover {
-    box-shadow: 0 0 32px 8px #1e1e1e14;
-
-    .summary-panel__separator {
-      border-bottom-color: var(--primary-color);
-    }
-  }
 
   &__ownership-question {
     font-size: 21px;
     font-weight: 700;
   }
+
 }
 </style>
