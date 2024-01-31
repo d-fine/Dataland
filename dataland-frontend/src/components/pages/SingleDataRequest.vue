66b5e9d6a189c514370bc59200a1f84cb3657dcc
Aliabdoli77 <aliabdoli77@yahoo.com>
2024-01-31 17:12:21 +0100

    Some progress on the request page

--- dataland-frontend/src/components/pages/SingleDataRequest.vue
+++ dataland-frontend/src/components/pages/SingleDataRequest.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Test text</div>
+  <div class="col-9">
+    <div data-test="provideContactDetails" class="bg-white radius-1 p-4">
+      <h4 class="p-0">Provide Contact Details</h4>
+      <label for="Email" class="label-with-optional"> Email <span class="optional-text">Optional</span> </label>
+      <FormKit type="text" name="Email" />
+      <p class="gray-text font-italic" style="text-align: left">
+        By specifying a contact person here, your data request will be directed accordingly.<br />
+        this increases the chances of expediting the fulfillment of your request.
+      </p>
+      <p class="gray-text font-italic" style="text-align: left">
+        If you don't have a specific contact person, no worries.<br />
+        We are committed to fulfilling your request to the best of our ability.
+      </p>
+      <label for="Message" class="label-with-optional"> Message <span class="optional-text">Optional</span> </label>
+      <FormKit type="textarea" name="Message" />
+      <p class="gray-text font-italic" style="text-align: left">
+        Let your contact know what exactly your are looking for.
+      </p>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.label-with-optional {
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+}
+
+.optional-text {
+  font-style: italic;
+  color: var(--primary-orange);
+  margin-left: 8px;
+}
+</style>
