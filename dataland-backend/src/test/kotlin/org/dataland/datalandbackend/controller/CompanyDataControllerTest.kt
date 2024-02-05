a1f528ed46c4a63eb5d71d7a0e43bf04427ef9e8
Florian Soffel <florian.soffel@d-fine.de>
2024-02-05 18:23:38 +0100

    remove unneccessary tests

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/controller/CompanyDataControllerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/controller/CompanyDataControllerTest.kt
@@ -1,6 +1,5 @@
 package org.dataland.datalandbackend.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.transaction.Transactional
 import org.dataland.datalandbackend.DatalandBackend
 import org.dataland.datalandbackend.model.companies.CompanyInformation
@@ -9,8 +8,6 @@ import org.dataland.datalandbackend.repositories.CompanyIdentifierRepository
 import org.dataland.datalandbackend.services.CompanyAlterationManager
 import org.dataland.datalandbackend.services.CompanyQueryManager
 import org.dataland.datalandbackend.services.DataOwnersManager
-import org.dataland.datalandbackend.utils.CompanyUploader
-import org.dataland.datalandbackend.utils.TestDataProvider
 import org.dataland.datalandbackendutils.exceptions.ResourceNotFoundApiException
 import org.dataland.keycloakAdapter.auth.DatalandRealmRole
 import org.dataland.keycloakAdapter.utils.AuthenticationMock
@@ -22,59 +19,19 @@ import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 
 @SpringBootTest(classes = [DatalandBackend::class], properties = ["spring.profiles.active=nodb"])
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @Transactional
-@AutoConfigureMockMvc
-@ActiveProfiles(profiles = ["unprotected"])
 internal class CompanyDataControllerTest(
-    @Autowired var mockMvc: MockMvc,
-    @Autowired var objectMapper: ObjectMapper,
     @Autowired val companyAlterationManager: CompanyAlterationManager,
     @Autowired val companyQueryManager: CompanyQueryManager,
     @Autowired val companyIdentifierRepositoryInterface: CompanyIdentifierRepository,
     @Autowired val dataOwnersManager: DataOwnersManager,
 ) {
-
-    private final val testDataProvider = TestDataProvider(objectMapper)
-    val testCompanyInformation = testDataProvider.getCompanyInformationWithoutIdentifiers(1).first()
-
-    @Test
-    fun `company can be posted`() {
-        CompanyUploader().uploadCompany(mockMvc, objectMapper, testCompanyInformation)
-    }
-
-    @Test
-    fun `meta info about a specific company can be retrieved by its company Id`() {
-        // TODO this test fails because the first company of "CompanyInformationWithEutaxonomyNonFinancialsData.json"
-        // does not have "isTeaserCompany: true" and therefore cannot be retrieved without authentication.
-        // This unit test therefore throws a 403 error. Either we fix it, or we remove the unit test.
-        // Discussion needed with the author of the test.
-        /*
-        val storedCompany = CompanyUploader().uploadCompany(mockMvc, objectMapper, testCompanyInformation)
-        mockMvc.perform(
-            get("/companies/${storedCompany.companyId}")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON),
-        )
-            .andExpectAll(
-                status().isOk,
-                content().contentType(MediaType.APPLICATION_JSON),
-            ) */ // TODO commenting out test for now to have proper CI feedback.  Discussion postponed for now
-        Assertions.assertEquals( // TODO dummy pass for test for now
-            true,
-            true,
-        )
-    }
-
     private final val testLei = "testLei"
     val companyWithTestLei = CompanyInformation(
         companyName = "Test Company",
