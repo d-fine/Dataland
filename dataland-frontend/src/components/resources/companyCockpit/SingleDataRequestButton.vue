7d5ac81f47e0c9d48f7dd0ba6687cd8e951aa9f9
Benedikt Laubmann <Benedikt.Laubmann@d-fine.at>
2024-02-07 14:15:42 +0100

    make button only viewable for the right roles

--- dataland-frontend/src/components/resources/companyCockpit/SingleDataRequestButton.vue
+++ dataland-frontend/src/components/resources/companyCockpit/SingleDataRequestButton.vue
@@ -1,21 +1,45 @@
 <template>
-  <PrimeButton @click="onClick" class="uppercase p-button p-button-sm mr-3" data-test="singleDataRequestButton">
+  <PrimeButton
+    v-if="isUserAllowed"
+    @click="onClick"
+    class="uppercase p-button p-button-sm mr-3"
+    data-test="singleDataRequestButton"
+  >
     <span class="d-letters pl-2"> Request Data </span>
   </PrimeButton>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, inject } from "vue";
 import PrimeButton from "primevue/button";
+import type Keycloak from "keycloak-js";
+import { checkIfUserHasRole, KEYCLOAK_ROLE_UPLOADER } from "@/utils/KeycloakUtils";
 
 export default defineComponent({
   name: "SingleDataRequestButton",
   components: { PrimeButton },
+  setup() {
+    return {
+      getKeycloakPromise: inject<() => Promise<Keycloak>>("getKeycloakPromise"),
+    };
+  },
   props: {
     companyId: {
       type: String,
       required: true,
     },
+  },
+  mounted() {
+    checkIfUserHasRole(KEYCLOAK_ROLE_UPLOADER, this.getKeycloakPromise)
+      .then((result) => {
+        this.isUserAllowed = result;
+      })
+      .catch((error) => console.log(error));
+  },
+  data() {
+    return {
+      isUserAllowed: false,
+    };
   },
   methods: {
     /**
