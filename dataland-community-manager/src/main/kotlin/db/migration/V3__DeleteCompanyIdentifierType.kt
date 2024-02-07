3451a1cdc69487bd4090236f32c17dbc40d8aca9
d93171 <hussain.roker@d-fine.com>
2024-02-07 13:04:28 +0100

    init

---
+++ dataland-community-manager/src/main/kotlin/db/migration/V3__DeleteCompanyIdentifierType.kt
@@ -0,0 +1,16 @@
+package db.migration
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+/**
+ * This migration script extends the data_requests table
+ */
+class V3__DeleteCompanyIdentifierType : BaseJavaMigration() {
+    override fun migrate(context: Context?) {
+        context!!.connection.createStatement().execute(
+            "ALTER TABLE data_requests " +
+                    "DROP COLUMN data_request_company_identifier_type",
+        )
+    }
+}
