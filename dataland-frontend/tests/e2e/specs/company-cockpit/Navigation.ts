67f9c50ddc339314a06fb52d123184130b57242b
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-02-13 16:34:48 +0100

    refactor Navigation.ts to be able to understand it better

--- dataland-frontend/tests/e2e/specs/company-cockpit/Navigation.ts
+++ dataland-frontend/tests/e2e/specs/company-cockpit/Navigation.ts
@@ -4,8 +4,8 @@ import { getKeycloakToken } from "@e2e/utils/Auth";
 import { type CompanyIdAndName, DataTypeEnum } from "@clients/backend";
 
 describe("As a user, I expect the navigation around the company cockpit to work as expected", () => {
-  let someCompanyIdAndName: CompanyIdAndName;
-  let otherCompanyIdAndName: CompanyIdAndName;
+  let alphaCompanyIdAndName: CompanyIdAndName;
+  let betaCompanyIdAndName: CompanyIdAndName;
 
   const companyCockpitRegex = /\/companies\/[0-9a-fA-F-]{36}$/;
 
@@ -16,11 +16,11 @@ describe("As a user, I expect the navigation around the company cockpit to work 
       })
       .then((basicCompanyInfos) => {
         expect(basicCompanyInfos).to.be.not.empty;
-        someCompanyIdAndName = {
+        alphaCompanyIdAndName = {
           companyId: basicCompanyInfos[0].companyId,
           companyName: basicCompanyInfos[0].companyName,
         };
-        otherCompanyIdAndName = {
+        betaCompanyIdAndName = {
           companyId: basicCompanyInfos[1].companyId,
           companyName: basicCompanyInfos[1].companyName,
         };
@@ -29,54 +29,60 @@ describe("As a user, I expect the navigation around the company cockpit to work 
 
   it("From the landing page visit the company cockpit via the searchbar", () => {
     cy.visitAndCheckAppMount("/");
-    searchCompanyAndChooseFirstSuggestion(someCompanyIdAndName.companyName);
+    searchCompanyAndChooseFirstSuggestion(alphaCompanyIdAndName.companyName);
     cy.url().should("match", companyCockpitRegex);
     cy.get("h1").should("exist");
   });
 
   it("From the company cockpit page visit the company cockpit of a different company", () => {
-    visitSomeCompanyCockpit();
-    searchCompanyAndInterceptRequest(otherCompanyIdAndName);
-    cy.url().should("not.contain", `/companies/${someCompanyIdAndName.companyId}`);
+    visitCockpitForCompanyAlpha();
+    cy.intercept("GET", `**/api/companies/${betaCompanyIdAndName.companyId}/aggregated-framework-data-summary`).as(
+      "fetchAggregatedFrameworkSummaryForBeta",
+    );
+    searchCompanyAndChooseFirstSuggestion(betaCompanyIdAndName.companyName);
+    cy.wait("@fetchAggregatedFrameworkSummaryForBeta");
+    cy.url().should("not.contain", `/companies/${alphaCompanyIdAndName.companyId}`);
     cy.url().should("match", companyCockpitRegex);
   });
 
   it("From the company cockpit page visit a view page", () => {
     cy.ensureLoggedIn(uploader_name, uploader_pw);
-    visitSomeCompanyCockpit();
+    visitCockpitForCompanyAlpha();
     cy.get("[data-test='eutaxonomy-non-financials-summary-panel']").click();
     cy.url().should(
       "contain",
-      `/companies/${someCompanyIdAndName.companyId}/frameworks/${DataTypeEnum.EutaxonomyNonFinancials}`,
+      `/companies/${alphaCompanyIdAndName.companyId}/frameworks/${DataTypeEnum.EutaxonomyNonFinancials}`,
     );
   });
 
-  it("From the company cockpit page visit a view page", () => {
+  it("From the company cockpit page visit an upload page", () => {
     cy.ensureLoggedIn(uploader_name, uploader_pw);
-    visitSomeCompanyCockpit();
+    visitCockpitForCompanyAlpha();
     cy.get("[data-test='eutaxonomy-financials-summary-panel'] a").click();
     cy.url().should(
       "contain",
-      `/companies/${someCompanyIdAndName.companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}/upload`,
+      `/companies/${alphaCompanyIdAndName.companyId}/frameworks/${DataTypeEnum.EutaxonomyFinancials}/upload`,
     );
   });
   it("From the company cockpit page claim data ownership via the panel and context menu", () => {
     cy.ensureLoggedIn(uploader_name, uploader_pw);
-    visitSomeCompanyCockpit();
+    visitCockpitForCompanyAlpha();
     cy.get("[data-test='claimOwnershipPanelLink']").click();
-    claimOwnershipDialog("This is a test message for claiming ownership via panel.");
+    submitOwnershipClaimForCompanyAlpha("This is a test message for claiming ownership via panel.");
     cy.get("[data-test='contextMenuButton']").click();
     cy.get("[data-test='contextMenuItem']").should("contain.text", "Claim").click();
-    claimOwnershipDialog("This is a test message for claiming ownership via context menu in company info");
+    submitOwnershipClaimForCompanyAlpha(
+      "This is a test message for claiming ownership via context menu in company info",
+    );
   });
 
   /**
    * Go through the dialog and claim company ownership with a message
    * @param message message to send with the request
    */
-  function claimOwnershipDialog(message: string): void {
+  function submitOwnershipClaimForCompanyAlpha(message: string): void {
     cy.get("[data-test='claimOwnershipDialogMessage']").should("exist");
-    cy.get("[data-test='claimOwnershipDialogMessage']").should("contain.text", someCompanyIdAndName.companyName);
+    cy.get("[data-test='claimOwnershipDialogMessage']").should("contain.text", alphaCompanyIdAndName.companyName);
     cy.get("[data-test='messageInputField']").should("exist").type(message);
     cy.get("[data-test='submitButton']").should("exist").click();
     cy.get("[data-test='claimOwnershipDialogSubmittedMessage']").should("exist");
@@ -88,9 +94,9 @@ describe("As a user, I expect the navigation around the company cockpit to work 
   /**
    * Visit the company cockpit of a predefined company
    */
-  function visitSomeCompanyCockpit(): void {
-    cy.visit(`/companies/${someCompanyIdAndName.companyId}`);
-    cy.contains("h1", someCompanyIdAndName.companyName).should("exist");
+  function visitCockpitForCompanyAlpha(): void {
+    cy.visit(`/companies/${alphaCompanyIdAndName.companyId}`);
+    cy.contains("h1", alphaCompanyIdAndName.companyName).should("exist");
   }
 
   /**
@@ -100,17 +106,5 @@ describe("As a user, I expect the navigation around the company cockpit to work 
   function searchCompanyAndChooseFirstSuggestion(searchTerm: string): void {
     cy.get("input#company_search_bar_standard").type(searchTerm);
     cy.get(".p-autocomplete-item").first().click();
-  }
-
-  /**
-   * Searches a company visits the first suggestion's cockpit and waits for displayed data
-   * @param companyToSearch the company to navigate to via the search bar
-   */
-  function searchCompanyAndInterceptRequest(companyToSearch: CompanyIdAndName): void {
-    cy.intercept("GET", `**/api/companies/${companyToSearch.companyId}/aggregated-framework-data-summary`).as(
-      "apiRequest",
-    );
-    searchCompanyAndChooseFirstSuggestion(companyToSearch.companyName);
-    cy.wait("@apiRequest");
   }
 });
