0acdc1978499c06de52b5768d0ba6b06c6f51d66
Aliabdoli77 <aliabdoli77@yahoo.com>
2024-02-14 09:01:11 +0100

    sql command to rename column

--- dataland-community-manager/src/main/kotlin/db/migration/V3__DeleteCompanyIdentifierType.kt
+++ dataland-community-manager/src/main/kotlin/db/migration/V3__DeleteCompanyIdentifierType.kt
@@ -10,7 +10,8 @@ class V3__DeleteCompanyIdentifierType : BaseJavaMigration() {
     override fun migrate(context: Context?) {
         context!!.connection.createStatement().execute(
             "ALTER TABLE data_requests " +
-                "DROP COLUMN data_request_company_identifier_type",
+                "DROP COLUMN data_request_company_identifier_type, " +
+                "RENAME COLUMN dataRequestCompanyIdentifierValue TO datalandCompanyId"
         )
     }
 }
