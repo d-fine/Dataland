624c704079db76ab75d7171ae65b0bb81101fb10
Benedikt Laubmann <Benedikt.Laubmann@d-fine.at>
2024-02-14 18:23:00 +0100

    do fixture test only with new fixtures

--- dataland-frontend/tests/e2e/specs/community/SingleDataRequest.ts
+++ dataland-frontend/tests/e2e/specs/community/SingleDataRequest.ts
@@ -56,12 +56,21 @@ describeIf(
       cy.url().should("contain", `/singledatarequest/${testStoredCompany.companyId}`);
     });
 
-    it("Navigate to the single request page via the view page and verify that the viewed framework is preselected.", () => {
-      cy.visitAndCheckAppMount(`/companies/${testStoredCompany.companyId}/frameworks/${DataTypeEnum.Lksg}`);
-      cy.get('[data-test="singleDataRequestButton"]').should("exist").click();
-      cy.url().should("contain", `/singledatarequest/${testStoredCompany.companyId}`);
-      cy.get('[data-test="datapoint-framework"]').should("have.value", "lksg");
-    });
+    describeIf(
+      "As a premium user I want to be able to navigate to the single data request page via the view page",
+      {
+        executionEnvironments: ["developmentLocal", "ci", "developmentCd"],
+        onlyExecuteOnDatabaseReset: true,
+      },
+      () => {
+        it("Navigate to the single request page via the view page and verify that the viewed framework is preselected.", () => {
+          cy.visitAndCheckAppMount(`/companies/${testStoredCompany.companyId}/frameworks/${DataTypeEnum.Lksg}`);
+          cy.get('[data-test="singleDataRequestButton"]').should("exist").click();
+          cy.url().should("contain", `/singledatarequest/${testStoredCompany.companyId}`);
+          cy.get('[data-test="datapoint-framework"]').should("have.value", "lksg");
+        });
+      },
+    );
 
     it("Fill out the request page and check correct validation, request and success message", () => {
       cy.intercept("POST", "**/community/requests/single").as("postRequestData");
