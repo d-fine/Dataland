8c3dda5834f8e15e639f86ea893194e551da6751
Florian Soffel <florian.soffel@d-fine.de>
2024-02-15 18:45:11 +0100

    fix migration

--- dataland-community-manager/src/main/kotlin/db/migration/V2__ExtendDataRequest.kt
+++ dataland-community-manager/src/main/kotlin/db/migration/V2__ExtendDataRequest.kt
@@ -8,12 +8,12 @@ import org.flywaydb.core.api.migration.Context
  */
 class V2__ExtendDataRequest : BaseJavaMigration() {
     override fun migrate(context: Context?) {
-//        context!!.connection.createStatement().execute(
-//            "ALTER TABLE data_requests " +
-//                "ADD COLUMN reporting_period varchar(255) NOT NULL DEFAULT '2022', " +
-//                "ADD COLUMN message_history text, " +
-//                "ADD COLUMN last_modified_date bigint NOT NULL DEFAULT (extract(epoch from now()) * 1000)::bigint, " +
-//                "ADD COLUMN request_status varchar(255) NOT NULL DEFAULT 'Open'",
-//        )
+        context!!.connection.createStatement().execute(
+            "ALTER TABLE data_requests " +
+                "ADD COLUMN reporting_period varchar(255) NOT NULL DEFAULT '2022', " +
+                "ADD COLUMN message_history text, " +
+                "ADD COLUMN last_modified_date bigint NOT NULL DEFAULT (extract(epoch from now()) * 1000)::bigint, " +
+                "ADD COLUMN request_status varchar(255) NOT NULL DEFAULT 'Open'",
+        )
     }
 }
--- dataland-community-manager/src/main/kotlin/db/migration/V3__MigrateMessageHistory.kt
+++ dataland-community-manager/src/main/kotlin/db/migration/V3__MigrateMessageHistory.kt
@@ -4,26 +4,30 @@ import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
 
 /**
- * This migration script migrates the
+ * This migration script migrates the message history to a separate table
  */
 class V3__MigrateMessageHistory : BaseJavaMigration() {
     override fun migrate(context: Context?) {
         // The message_history is simply dropped because there is no data on for this, yet.
-//        context!!.connection.createStatement().execute(
-//            "ALTER TABLE data_requests " +
-//                "RENAME COLUMN data_type_name to data_type, " +
-//                "DROP COLUMN message_history"
-//        )
-//        context.connection.createStatement().execute(
-//            "CREATE TABLE messages (" +
-//                "message_id varchar(255) NOT NULL, " +
-//                "data_request_id varchar(255) NOT NULL, " +
-//                "ordinal int NOT NULL, " +
-//                "contacts text, " +
-//                "message text, " +
-//                "last_modified_date bigint NOT NULL, " +
-//                "PRIMARY KEY (message_id)" +
-//                ")",
-//        )
+        context!!.connection.createStatement().execute(
+            "ALTER TABLE data_requests " +
+                "RENAME COLUMN data_type_name to data_type"
+        )
+        context.connection.createStatement().execute(
+            "ALTER TABLE data_requests " +
+                "DROP COLUMN message_history"
+        )
+        context.connection.createStatement().execute(
+            "CREATE TABLE messages (" +
+                "message_id varchar(255) NOT NULL, " +
+                "data_request_id varchar(255) NOT NULL, " +
+                "ordinal integer NOT NULL, " +
+                "contacts text, " +
+                "message text, " +
+                "last_modified_date bigint NOT NULL, " +
+                "PRIMARY KEY (message_id), " +
+                "CONSTRAINT fk_data_request FOREIGN KEY(data_request_id) REFERENCES data_requests(data_request_id)" +
+                ")",
+        )
     }
 }
