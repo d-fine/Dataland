6a92c354a5d433906f49f7a49356b5b1266a0735
Florian Soffel <florian.soffel@d-fine.de>
2024-02-19 10:11:13 +0100

    cleanup

--- dataland-email/src/main/kotlin/org/dataland/datalandemail/email/EmailContact.kt
+++ dataland-email/src/main/kotlin/org/dataland/datalandemail/email/EmailContact.kt
@@ -51,12 +51,19 @@ fun TransactionalEmail.TransactionalEmailBuilder
  */
 fun String.isEmailAddress() = Regex("^[a-zA-Z0-9_.!-]+@([a-zA-Z0-9-]+.){1,2}[a-z]{2,}\$").matches(this)
 
+/**
+ * Validates that a string is an email address and throws an exception if not
+ * @throws InvalidEmailFormatApiException if the email format is violated
+ */
 fun String.validateIsEmailAddress() {
-    if(!isEmailAddress()) {
+    if (!isEmailAddress()) {
         throw InvalidEmailFormatApiException(this)
     }
 }
 
+/**
+ * An API exception which should be raised if an email format is violated
+ */
 class InvalidEmailFormatApiException(email: String) : InvalidInputApiException(
     "Invalid email address \"$email\"",
     "The email address \"$email\" you have provided has an invalid format.",
