104f5f4fa035067922aff2bdbb9ceadf43f979f0
Benedikt Laubmann <Benedikt.Laubmann@d-fine.at>
2024-02-26 19:36:26 +0100

    ktlint format

--- dataland-framework-toolbox/src/test/kotlin/org/dataland/frameworktoolbox/integration/IntegrationTestFramework.kt
+++ dataland-framework-toolbox/src/test/kotlin/org/dataland/frameworktoolbox/integration/IntegrationTestFramework.kt
@@ -19,7 +19,7 @@ class IntegrationTestFramework : PavedRoadFramework(
     enabledFeatures = setOf(
         FrameworkGenerationFeatures.DataModel,
         FrameworkGenerationFeatures.ViewPage,
-     //   FrameworkGenerationFeatures.FakeFixtures, // todo
+        //   FrameworkGenerationFeatures.FakeFixtures,
     ),
     order = 0,
 )
