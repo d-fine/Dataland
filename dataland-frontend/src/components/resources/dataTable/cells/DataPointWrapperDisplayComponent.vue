c30ca35db9b66194779b90d74c4da9d23dd852aa
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-02-28 15:17:53 +0100

    display component fix

--- dataland-frontend/src/components/resources/dataTable/cells/DataPointWrapperDisplayComponent.vue
+++ dataland-frontend/src/components/resources/dataTable/cells/DataPointWrapperDisplayComponent.vue
@@ -10,7 +10,7 @@
     </a>
     <DocumentLink
       v-else-if="dataPointProperties.dataSource"
-      label=""
+      :label="dataPointProperties.value"
       :download-name="dataPointProperties.dataSource.fileName ?? dataPointProperties.dataSource.fileReference"
       :file-reference="dataPointProperties.dataSource.fileReference"
       show-icon
