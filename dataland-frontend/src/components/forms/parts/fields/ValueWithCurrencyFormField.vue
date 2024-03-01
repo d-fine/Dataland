055e5aefa93617711c39e3a131fba819f3550536
swaldow <stephan.waldow@d-fine.de>
2024-03-01 10:04:07 +0100

    fixes styling on the upload page

--- dataland-frontend/src/components/forms/parts/fields/ValueWithCurrencyFormField.vue
+++ dataland-frontend/src/components/forms/parts/fields/ValueWithCurrencyFormField.vue
@@ -1,30 +1,32 @@
 <template>
-  <FormKit type="group" :name="name" :input-class="inputClass">
-    <div class="grid">
-      <div class="col-12">
-        <UploadFormHeader :label="label" :description="description ?? ''" :is-required="required" />
+  <div :class="classes" :data-test="name">
+    <FormKit type="group" :name="name">
+      <div class="grid">
+        <div class="col-12">
+          <UploadFormHeader :label="label" :description="description ?? ''" :is-required="required" />
+        </div>
+        <div class="col-4">
+          <NumberFormField
+            :name="'value'"
+            :validation-label="validationLabel"
+            :validation="validation"
+            :unit="unit"
+            input-class="col-12"
+          />
+        </div>
+        <div class="col-4">
+          <FormKit
+            type="select"
+            name="currency"
+            placeholder="Currency"
+            :options="getDataset(DropdownDatasetIdentifier.CurrencyCodes)"
+            outer-class="long"
+            data-test="ValueWithCurrencyFormField-currency"
+          />
+        </div>
       </div>
-      <div class="col-4">
-        <NumberFormField
-          :name="'value'"
-          :validation-label="validationLabel"
-          :validation="validation"
-          :unit="unit"
-          input-class="col-12"
-        />
-      </div>
-      <div class="col-4">
-        <FormKit
-          type="select"
-          name="currency"
-          placeholder="Currency"
-          :options="getDataset(DropdownDatasetIdentifier.CurrencyCodes)"
-          outer-class="long"
-          data-test="ValueWithCurrencyFormField-currency"
-        />
-      </div>
-    </div>
-  </FormKit>
+    </FormKit>
+  </div>
 </template>
 
 <script lang="ts">
@@ -47,6 +49,10 @@ export default defineComponent({
     ...FormFieldPropsWithPlaceholder,
     unit: {
       type: String,
+    },
+    classes: {
+      type: String,
+      default: "form-field",
     },
   },
   methods: {
