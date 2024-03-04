7b85a1bf76cb93634b60847c807d0e2a876af4f5
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-03-04 16:38:52 +0100

    fix CompanyCockpitPage.cy.ts

--- dataland-frontend/tests/component/components/pages/CompanyCockpitPage.cy.ts
+++ dataland-frontend/tests/component/components/pages/CompanyCockpitPage.cy.ts
@@ -2,7 +2,7 @@ import CompanyCockpitPage from "@/components/pages/CompanyCockpitPage.vue";
 import { minimalKeycloakMock } from "@ct/testUtils/Keycloak";
 import { type AggregatedFrameworkDataSummary, type CompanyInformation, type SmeData } from "@clients/backend";
 import { type FixtureData } from "@sharedUtils/Fixtures";
-import { AggregatedDataRequestDataTypeEnum } from "@clients/communitymanager";
+import { type AggregatedDataRequestDataTypeEnum } from "@clients/communitymanager";
 import { KEYCLOAK_ROLE_UPLOADER, KEYCLOAK_ROLE_USER, KEYCLOAK_ROLE_PREMIUM_USER } from "@/utils/KeycloakUtils";
 import type * as Cypress from "cypress";
 import { setMobileDeviceViewport } from "@sharedUtils/TestSetupUtils";
@@ -143,9 +143,7 @@ describe("Component test for the company cockpit", () => {
           aggregatedFrameworkDataSummary.numberOfProvidedReportingPeriods.toString(),
         );
 
-        const isSmeFramework = frameworkName === AggregatedDataRequestDataTypeEnum.Sme;
-
-        if (isProvideDataButtonExpected && !isSmeFramework) {
+        if (isProvideDataButtonExpected) {
           cy.get(`${frameworkSummaryPanelSelector} a[data-test="${frameworkName}-provide-data-button"]`).should(
             "exist",
           );
