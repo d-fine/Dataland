df1cf47ff7349a9f84dde5ecfab1e064e235e8b1
TWinter <torben.winter@d-fine.com>
2024-03-05 14:19:31 +0100

    Unit Tests for community manager

--- dataland-community-manager/src/test/kotlin/org/dataland/datalandcommunitymanager/email/SingleDataRequestEmailBuilderTest.kt
+++ dataland-community-manager/src/test/kotlin/org/dataland/datalandcommunitymanager/email/SingleDataRequestEmailBuilderTest.kt
@@ -1,57 +1,108 @@
 package org.dataland.datalandbackend.email
 
-import org.dataland.datalandbackend.openApiClient.api.CompanyDataControllerApi
-import org.dataland.datalandbackend.openApiClient.model.CompanyInformation
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.dataland.datalandbackend.openApiClient.model.DataTypeEnum
-import org.dataland.datalandcommunitymanager.services.SingleDataRequestEmailBuilder
-import org.dataland.datalandemail.email.EmailContact
-import org.dataland.datalandemail.utils.assertEmailContactInformationEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.dataland.datalandcommunitymanager.DatalandCommunityManager
+import org.dataland.datalandmessagequeueutils.cloudevents.CloudEventMessageHandler
+import org.dataland.datalandmessagequeueutils.constants.ExchangeName
+import org.dataland.datalandmessagequeueutils.constants.MessageType
+import org.dataland.datalandmessagequeueutils.constants.RoutingKeyNames
+import org.dataland.datalandmessagequeueutils.messages.InternalEmailMessage
+import org.dataland.keycloakAdapter.auth.DatalandJwtAuthentication
+import org.dataland.keycloakAdapter.auth.DatalandRealmRole
+import org.dataland.keycloakAdapter.utils.AuthenticationMock
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+import java.util.*
 
-class SingleDataRequestEmailBuilderTest {
-    private val proxyPrimaryUrl = "local-dev.dataland.com"
-    private val requesterEmail = "requester@dataland.com"
-    private val senderEmail = "sender@dataland.com"
-    private val senderName = "Test"
-    private val receiverEmail = "receiver1@dataland.com"
-    private val companyId = "8"
+@SpringBootTest(classes = [DatalandCommunityManager::class], properties = ["spring.profiles.active=nodb"])
+class SingleDataRequestEmailBuilderTest(@Autowired val objectMapper: ObjectMapper) {
+    private lateinit var authenticationMock: DatalandJwtAuthentication
+    private val cloudEventMessageHandlerMock = mock(CloudEventMessageHandler::class.java)
     private val companyName = "Test Inc."
     private val reportingPeriods = setOf("2022", "2023")
-    private val message = "This is a comment"
+    private val datalandCompanyId = "59f05156-e1ba-4ea8-9d1e-d4833f6c7afc"
+    private val correlationId = UUID.randomUUID().toString()
+
+    fun buildUserInfo(
+        userAuthentication: DatalandJwtAuthentication,
+    ): String {
+        return "User ${userAuthentication.username} (Keycloak ID: ${userAuthentication.userId})"
+    }
+
+    fun formatReportingPeriods(reportingPeriods: Set<String>) =
+        reportingPeriods.toList().sorted().joinToString(", ")
+
+    @BeforeEach
+    fun setupAuthentication() {
+        val mockSecurityContext = mock(SecurityContext::class.java)
+        authenticationMock = AuthenticationMock.mockJwtAuthentication(
+            "requester@bigplayer.com",
+            "1234-221-1111elf",
+            setOf(DatalandRealmRole.ROLE_USER),
+        )
+        `when`(mockSecurityContext.authentication).thenReturn(authenticationMock)
+        `when`(authenticationMock.credentials).thenReturn("")
+        SecurityContextHolder.setContext(mockSecurityContext)
+    }
 
     @Test
-    fun `validate that the output of the single data request email builder is correctly formatted`() {
-        val mockCompanyApi = mock(CompanyDataControllerApi::class.java)
-        val mockCompanyInformation = mock(CompanyInformation::class.java)
-        `when`(mockCompanyInformation.companyName).thenReturn(companyName)
-        `when`(mockCompanyApi.getCompanyInfo(anyString())).thenReturn(mockCompanyInformation)
-        val email = SingleDataRequestEmailBuilder(proxyPrimaryUrl, senderEmail, senderName, mockCompanyApi)
-            .buildSingleDataRequestEmail(
-                requesterEmail = requesterEmail,
-                receiverEmail = receiverEmail,
-                companyId = companyId,
-                dataType = DataTypeEnum.lksg,
-                reportingPeriods = reportingPeriods,
-                rawMessage = message,
-            )
-        assertEmailContactInformationEquals(
-            EmailContact(senderEmail, senderName),
-            setOf(EmailContact(receiverEmail)),
-            emptySet(),
-            email,
-        )
-        assertTrue(email.content.htmlContent.contains(requesterEmail))
-        assertTrue(email.content.htmlContent.contains(companyName))
-        assertTrue(email.content.htmlContent.contains("LkSG"))
-        assertTrue(email.content.htmlContent.contains(reportingPeriods.joinToString(", ")))
-        assertTrue(
-            email.content.htmlContent.contains(
-                "href=\"https://$proxyPrimaryUrl/companies/$companyId\"",
+    fun `validate that the output of the data request email message sender is correctly formatted`() {
+        `when`(
+            cloudEventMessageHandlerMock.buildCEMessageAndSendToQueue(
+                anyString(),
+                anyString(),
+                anyString(),
+                anyString(),
+                anyString(),
             ),
+        ).then() {
+            val arg1 = objectMapper.readValue(it.getArgument<String>(0), InternalEmailMessage::class.java)
+            val arg2 = it.getArgument<String>(1)
+            val arg3 = it.getArgument<String>(2)
+            val arg4 = it.getArgument<String>(3)
+            val arg5 = it.getArgument<String>(4)
+
+            assertEquals("Dataland Single Data Request", arg1.subject)
+            assertEquals("A single data request has been submitted", arg1.textTitle)
+            assertEquals("Single Data Request", arg1.htmlTitle)
+            assertEquals(buildUserInfo(authenticationMock), arg1.properties.getValue("User"))
+            assertEquals("lksg", arg1.properties.getValue("Data Type"))
+            assertEquals("2022, 2023", arg1.properties.getValue("Reporting Periods"))
+            assertEquals(datalandCompanyId, arg1.properties.getValue("Dataland Company ID"))
+            assertEquals(companyName, arg1.properties.getValue("Company Name"))
+            assertEquals(MessageType.SendInternalEmail, arg2)
+            assertEquals(correlationId, arg3)
+            assertEquals(ExchangeName.SendEmail, arg4)
+            assertEquals(RoutingKeyNames.internalEmail, arg5)
+        }
+        cloudEventMessageHandlerMock.buildCEMessageAndSendToQueue(
+            objectMapper.writeValueAsString(
+                InternalEmailMessage(
+                    "Dataland Single Data Request",
+                    "A single data request has been submitted",
+                    "Single Data Request",
+                    mapOf(
+                        "User" to buildUserInfo(authenticationMock),
+                        "Data Type" to DataTypeEnum.lksg.toString(),
+                        "Reporting Periods" to formatReportingPeriods(reportingPeriods),
+                        "Dataland Company ID" to datalandCompanyId,
+                        "Company Name" to companyName,
+                    ),
+                ),
+            ),
+            MessageType.SendInternalEmail,
+            correlationId,
+            ExchangeName.SendEmail,
+            RoutingKeyNames.internalEmail,
         )
     }
 }
