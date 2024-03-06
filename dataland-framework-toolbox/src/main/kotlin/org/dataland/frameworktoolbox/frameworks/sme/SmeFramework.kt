7ced61cc2ffa966f8f4cab3e5035a6d514c0e674
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-03-06 13:47:44 +0100

    review comment: Switch SmeFramework.kt to PavedRoadFramework

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/sme/SmeFramework.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/sme/SmeFramework.kt
@@ -1,6 +1,6 @@
 package org.dataland.frameworktoolbox.frameworks.sfdr
 
-import org.dataland.frameworktoolbox.frameworks.InDevelopmentPavedRoadFramework
+import org.dataland.frameworktoolbox.frameworks.PavedRoadFramework
 import org.springframework.stereotype.Component
 import java.io.File
 
@@ -8,7 +8,7 @@ import java.io.File
  * Definition of the SME framework
  */
 @Component
-class SmeFramework : InDevelopmentPavedRoadFramework(
+class SmeFramework : PavedRoadFramework(
     identifier = "sme",
     label = "SME",
     explanation = "Small and medium-sized enterprises questionnaire",
