3f26397cb9c9867e00176a4e0c6fc607420f7094
Johannes Haerkötter <159615830+jhaerDfine@users.noreply.github.com>
2024-03-06 10:21:39 +0100

    Dala 1532 list of requested datasets page requester perspective (#452)
    * created RequestedDatasetsPage.vue and added /requestedDatasets in index.ts and DatasetsTabMenu.vue
    
    * added /todo
    
    * update requestedDatasetsPage
    
    * update requestedDatasetsPage
    
    * update requestedDatasetsPage
    
    * added ExtendendDataRequest Type, changed endpoint getRequestsFor(Requesting)User
    
    * fixed sorting
    
    * cypress component test
    
    * merged main
    
    * kt lint
    
    * genarted OpenApi
    
    * fix routing
    
    * fix tests
    
    * sonar
    
    * spelling
    
    * linting
    
    * update tests
    
    * sonar
    
    * added subtitles for frameworks
    
    * sonar
    
    * sonar
    
    * test coverage
    
    * readability
    
    * refeactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * added esg questionarire to subtitles
    
    * readability
    
    * updated sorting
    
    * coursor bei resolve
    bulk data request falls keine requests
    year --> reporting period
    
    * requested date -> requested
    some names
    
    * updated tests
    
    * fix sorting
    
    * fix sorting
    
    * remove console log
    
    * typo
    
    * goToLandingPage -> goToMyRequests
    
    * goToLandingPage -> goToMyRequests
    
    * small fix
    
    * rename
    
    * rename
    
    * fix sort
    
    * renamed .vue, .cy.ts
    
    * renamed .vue, .cy.ts
    
    * small fix
    
    * small fix
    
    * small fix
    
    * small fix
    
    * fix sort
    
    * moved date converter to utils
    
    * rename
    
    * remove waiting for searchresult
    
    * more tests
    
    * more tests
    
    * small fixes
    
    * test update
    
    * test update
    
    * style
    
    * small changes
    
    * small changes
    
    * sorting
    remove not needed html classes
    
    * sorting
    remove not needed html classes
    
    * sorting
    remove not needed html classes
    loading
    
    * updated tests for sorting
    
    * changed date format
    
    * check for BulkDataRequest button
    
    * check for BulkDataRequest button
    
    * renaming test companies
    
    * main merge
    renaming testcompanies
    
    * small fix
    
    * small fix
    
    * minimal mock companies

---
+++ dataland-frontend/tests/component/components/pages/MyDataRequestsOverview.cy.ts
@@ -0,0 +1,209 @@
+import { type ExtendedStoredDataRequest, RequestStatus } from "@clients/communitymanager";
+import RequestedDatasetsPage from "@/components/pages/MyDataRequestsOverview.vue";
+import { minimalKeycloakMock } from "@ct/testUtils/Keycloak";
+import { DataTypeEnum } from "@clients/backend";
+
+const mockDataRequests: ExtendedStoredDataRequest[] = [];
+const expectedHeaders = ["COMPANY", "REPORTING PERIOD", "FRAMEWORK", "REQUESTED", "LAST UPDATED", "STATUS"];
+
+before(function () {
+  mockDataRequests.push({
+    datalandCompanyId: "compA",
+    companyName: "companyAnswered",
+    dataType: DataTypeEnum.P2p,
+    reportingPeriod: "2020",
+    creationTimestamp: 1709204495770,
+    lastModifiedDate: 1709204495770,
+    requestStatus: RequestStatus.Answered,
+  } as ExtendedStoredDataRequest);
+  mockDataRequests.push({
+    companyName: "companyNotAnsweredSME",
+    dataType: DataTypeEnum.Sme,
+    reportingPeriod: "2022",
+    creationTimestamp: 1709204495770,
+    lastModifiedDate: 1709204495770,
+    requestStatus: RequestStatus.Open,
+  } as ExtendedStoredDataRequest);
+  mockDataRequests.push({
+    companyName: "z-company-that-will-always-be-sorted-to-bottom",
+    dataType: DataTypeEnum.EutaxonomyFinancials,
+    reportingPeriod: "3021",
+    creationTimestamp: 1809204495770,
+    lastModifiedDate: 1609204495770,
+    requestStatus: RequestStatus.Closed,
+  } as ExtendedStoredDataRequest);
+  mockDataRequests.push({
+    companyName: "companyNotAnsweredEU",
+    dataType: DataTypeEnum.EutaxonomyNonFinancials,
+    reportingPeriod: "2021",
+    creationTimestamp: 1709204495770,
+    lastModifiedDate: 1709204495770,
+    requestStatus: RequestStatus.Open,
+  } as ExtendedStoredDataRequest);
+  mockDataRequests.push({
+    companyName: "a-company-that-will-always-be-sorted-to-top",
+    dataType: DataTypeEnum.EsgQuestionnaire,
+    reportingPeriod: "1021",
+    creationTimestamp: 1609204495770,
+    lastModifiedDate: 1809204495770,
+    requestStatus: RequestStatus.Answered,
+  } as ExtendedStoredDataRequest);
+});
+describe("Component tests for the data requests search page", function (): void {
+  it("Check sorting", function (): void {
+    cy.intercept("**community/requests/user", {
+      body: mockDataRequests,
+      status: 200,
+    }).as("UserRequests");
+    cy.mountWithPlugins(RequestedDatasetsPage, {
+      keycloak: minimalKeycloakMock({}),
+    });
+    const sortingColumHeader = ["COMPANY", "REPORTING PERIOD", "REQUESTED", "STATUS"];
+    sortingColumHeader.forEach((value) => {
+      cy.get(`table th:contains(${value})`).should("exist").click();
+      cy.get('[data-test="requested-Datasets-table"]')
+        .find("tr")
+        .find("td")
+        .contains("a-company-that-will-always-be-sorted-to-top")
+        .parent()
+        .invoke("index")
+        .should("eq", 0);
+      cy.get(`table th:contains(${value})`).should("exist").click();
+      cy.get('[data-test="requested-Datasets-table"]')
+        .find("tr")
+        .find("td")
+        .contains("z-company-that-will-always-be-sorted-to-bottom")
+        .parent()
+        .invoke("index")
+        .should("eq", 0);
+    });
+  });
+
+  it("Check page when there are no requested datasets", function (): void {
+    cy.intercept("**community/requests/user", {
+      body: [],
+      status: 200,
+    }).as("UserRequests");
+    cy.mountWithPlugins(RequestedDatasetsPage, {
+      keycloak: minimalKeycloakMock({}),
+    }).then((mounted) => {
+      cy.get('[data-test="requested-Datasets-table"]').should("not.exist");
+      cy.get('[data-test="bulkDataRequestButton"]').should("exist").should("be.visible").click();
+      cy.wrap(mounted.component).its("$route.path").should("eq", "/bulkdatarequest");
+    });
+  });
+
+  it("Check static layout of the search page", function () {
+    const placeholder = "Search by company name";
+    const inputValue = "A company name";
+
+    cy.intercept("**community/requests/user", {
+      body: mockDataRequests,
+      status: 200,
+    }).as("UserRequests");
+    cy.mountWithPlugins(RequestedDatasetsPage, {
+      keycloak: minimalKeycloakMock({}),
+    });
+
+    cy.get('[data-test="requested-Datasets-table"]').should("exist");
+    expectedHeaders.forEach((value) => {
+      cy.get(`table th:contains(${value})`).should("exist");
+    });
+    cy.get('[data-test="requested-Datasets-searchbar"]')
+      .should("exist")
+      .should("not.be.disabled")
+      .type(inputValue)
+      .should("have.value", inputValue)
+      .invoke("attr", "placeholder")
+      .should("contain", placeholder);
+    cy.get('[data-test="requested-Datasets-frameworks"]').should("exist");
+  });
+
+  it("Check the content of the data table", function (): void {
+    const expectedCompanys = [
+      "companyAnswered",
+      "companyNotAnsweredSME",
+      "companyNotAnsweredEU",
+      "z-company-that-will-always-be-sorted-to-bottom",
+      "a-company-that-will-always-be-sorted-to-top",
+    ];
+    const expectedReportingPeriods = ["2020", "2021", "2022"];
+
+    cy.intercept("**community/requests/user", {
+      body: mockDataRequests,
+      status: 200,
+    }).as("UserRequests");
+
+    cy.mountWithPlugins(RequestedDatasetsPage, {
+      keycloak: minimalKeycloakMock({}),
+    });
+
+    expectedCompanys.forEach((value) => {
+      cy.get('[data-test="requested-Datasets-table"]').find("tr").find("td").contains(value).should("exist");
+    });
+    cy.get('[data-test="requested-Datasets-table"]').find("tr").find("td").contains("DummyName").should("not.exist");
+    expectedReportingPeriods.forEach((value) => {
+      cy.get('[data-test="requested-Datasets-table"]').find("tr").find("td").contains(value).should("exist");
+    });
+    cy.get('[data-test="requested-Datasets-table"]').find("tr").find("td").contains("2019").should("not.exist");
+  });
+
+  it("Check existence and functionality of searchbar and resolve button", function (): void {
+    cy.intercept("**community/requests/user", {
+      body: mockDataRequests,
+      status: 200,
+    }).as("UserRequests");
+
+    cy.mountWithPlugins(RequestedDatasetsPage, {
+      keycloak: minimalKeycloakMock({}),
+    }).then((mounted) => {
+      cy.get('[data-test="requested-Datasets-searchbar"]')
+        .should("exist")
+        .should("not.be.disabled")
+        .clear()
+        .type("companyNotAnswered");
+      cy.get('[data-test="requested-Datasets-Resolve"]').should("not.exist");
+      cy.get('[data-test="requested-Datasets-searchbar"]')
+        .should("exist")
+        .should("not.be.disabled")
+        .clear()
+        .type("companyAnswered");
+      cy.get('[data-test="requested-Datasets-Resolve"]').should("exist").should("be.visible").click();
+      cy.wrap(mounted.component).its("$route.path").should("eq", "/companies/compA/frameworks/p2p");
+    });
+  });
+
+  it("Check filter functionality and reset button", function (): void {
+    const expectedFrameworks = [
+      "WWF",
+      "SME",
+      "EU Taxonomy",
+      "Pathways to Paris",
+      "for financial companies",
+      "for non-financial companies",
+      "ESG Questionnaire",
+      "für Corporate Schuldscheindarlehen",
+    ];
+
+    cy.intercept("**community/requests/user", {
+      body: mockDataRequests,
+      status: 200,
+    }).as("UserRequests");
+
+    cy.mountWithPlugins(RequestedDatasetsPage, {
+      keycloak: minimalKeycloakMock({}),
+    }).then((mounted) => {
+      void mounted.wrapper.setData({
+        selectedFrameworks: [],
+      });
+      expectedFrameworks.forEach((value) => {
+        cy.get(`table tbody:contains(${value})`).should("not.exist");
+      });
+      cy.get("[data-test=reset-filter]").should("exist").click();
+      expectedFrameworks.forEach((value) => {
+        cy.get(`table tbody:contains(${value})`).should("exist");
+      });
+      cy.get(`table tbody:contains("SFDR")`).should("not.exist");
+    });
+  });
+});
