ef91817286ca6db66b4a6a1ec073431022eea7b4
Benedikt Laubmann <Benedikt.Laubmann@d-fine.at>
2024-03-08 16:36:43 +0100

    ktlint

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/KeycloakUserControllerApiService.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/KeycloakUserControllerApiService.kt
@@ -25,8 +25,7 @@ class KeycloakUserControllerApiService(
 
         @JsonProperty("id")
         val userId: String,
-
-        )
+    )
 
     /**
      * gets the email address of a user in keycloak given the user id
