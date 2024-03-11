8ea1705b9f9ffb98e1c3b32625d18a7dd6a6aec6
Benedikt Laubmann <Benedikt.Laubmann@d-fine.at>
2024-03-11 11:02:10 +0100

    remove unused parameter

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/DataRequestUpdater.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/DataRequestUpdater.kt
@@ -69,7 +69,6 @@ class DataRequestUpdater(
     fun changeRequestStatusAfterUpload(
         @Payload jsonString: String,
         @Header(MessageHeaderKey.Type) type: String,
-        @Header(MessageHeaderKey.CorrelationId) correlationId: String,
     ) {
         messageUtils.validateMessageType(type, MessageType.QaCompleted)
         val qaCompletedMessage = objectMapper.readValue(jsonString, QaCompletedMessage::class.java)
@@ -97,7 +96,7 @@ class DataRequestUpdater(
             sendDataRequestedAnsweredEmails(dataRequestEntities, metaData.companyId)
             logger.info(
                 "Changed Request Status for company Id ${metaData.companyId}, " +
-                    "reporting period ${metaData.reportingPeriod} and framework ${metaData.dataType.name}",
+                        "reporting period ${metaData.reportingPeriod} and framework ${metaData.dataType.name}",
             )
         }
     }
