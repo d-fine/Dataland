c5b9f4d59d14c589c018bbe364fe4bce9c9c6271
Benedikt Laubmann <Benedikt.Laubmann@d-fine.at>
2024-03-11 17:31:56 +0100

    Add view request page wip

---
+++ dataland-frontend/src/components/pages/ViewDataRequestPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <AuthenticationWrapper>
+    <TheHeader />
+    <div class="sheet">
+      <div class="headline">
+        <BackButton />
+      </div>
+      <h1 class="text-left">Data Request</h1>
+    </div>
+    <div class="py-4 justify-content-center">
+      <div class="col-6">Test blabla {{ requestId }}</div>
+    </div>
+    <TheFooter :is-light-version="true" />
+  </AuthenticationWrapper>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AuthenticationWrapper from "@/components/wrapper/AuthenticationWrapper.vue";
+import TheHeader from "@/components/generics/TheHeader.vue";
+import BackButton from "@/components/general/BackButton.vue";
+import TheFooter from "@/components/generics/TheFooter.vue";
+export default defineComponent({
+  name: "ViewDataRequest",
+  components: { BackButton, AuthenticationWrapper, TheHeader, TheFooter },
+  props: {
+    requestId: {
+      type: String,
+      required: false, //todo
+      default: "12345",
+    },
+  },
+});
+</script>
