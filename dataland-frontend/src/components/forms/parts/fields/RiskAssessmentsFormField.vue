04882c37ad043f9e986bf8d6871663ee2ef91217
Benedikt Laubmann <Benedikt.Laubmann@d-fine.at>
2024-03-13 15:29:44 +0100

    wip RiskAssessment frontend components

---
+++ dataland-frontend/src/components/forms/parts/fields/RiskAssessmentsFormField.vue
@@ -0,0 +1,27 @@
+<template>
+  <FormListFormField
+    :name="name"
+    :label="label"
+    :description="description"
+    :required="required"
+    :validation="validation"
+    :validation-label="validationLabel"
+    display-one-sub-form-per-default
+    sub-form-component="RiskAssessmentFormElement"
+    data-test-add-button="addNewRiskAssessmentButton"
+    label-add-button="ADD NEW Risk"
+    data-test-sub-form="RiskAssessmentSection"
+    data-test-remove-button="removeItemFromListOfRiskAssessments"
+  />
+</template>
+
+<script lang="ts">
+import FormListFormField from "@/components/forms/parts/fields/FormListFormField.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "RiskAssessmentsFormField",
+  components: {
+    FormListFormField,
+  },
+});
+</script>
