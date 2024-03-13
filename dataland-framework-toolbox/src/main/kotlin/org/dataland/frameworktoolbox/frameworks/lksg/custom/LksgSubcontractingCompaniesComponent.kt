229e109a32ac5b6fddb8ffd123b90963c17d9b9a
Florian Soffel <florian.soffel@d-fine.de>
2024-03-13 11:43:31 +0100

    fixed docstring

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/lksg/custom/LksgSubcontractingCompaniesComponent.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/lksg/custom/LksgSubcontractingCompaniesComponent.kt
@@ -18,7 +18,7 @@ import org.dataland.frameworktoolbox.specific.viewconfig.functional.FrameworkDis
 import org.dataland.frameworktoolbox.utils.typescript.TypeScriptImport
 
 /**
- * Represents the ProcurementCategories component
+ * Represents the SubcontractingCompanies component
  */
 class LksgSubcontractingCompaniesComponent(
     identifier: String,
