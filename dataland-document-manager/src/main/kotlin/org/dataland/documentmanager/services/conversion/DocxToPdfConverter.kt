68c708eb8dcb781172aec3202021d9e6b1da9166
Johannes Haerkoetter <johannes.haerkoetter@d-fine.com>
2024-03-18 14:17:46 +0100

    doxc converter

--- dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/services/conversion/DocxToPdfConverter.kt
+++ dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/services/conversion/DocxToPdfConverter.kt
@@ -1,17 +1,28 @@
 
+import com.itextpdf.io.image.ImageDataFactory
+import com.itextpdf.kernel.colors.Color
+import com.itextpdf.kernel.colors.DeviceRgb
 import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.kernel.pdf.PdfWriter
 import com.itextpdf.layout.Document
+import com.itextpdf.layout.element.Image
 import com.itextpdf.layout.element.Paragraph
+import com.itextpdf.layout.element.Table
+import com.itextpdf.layout.element.Text
+import org.apache.commons.lang3.StringUtils.substring
+import org.apache.poi.xwpf.usermodel.UnderlinePatterns
 import org.apache.poi.xwpf.usermodel.XWPFDocument
 import org.apache.poi.xwpf.usermodel.XWPFParagraph
+import org.apache.poi.xwpf.usermodel.XWPFPicture
 import org.apache.poi.xwpf.usermodel.XWPFRun
+import org.apache.poi.xwpf.usermodel.XWPFTable
 import org.dataland.documentmanager.services.conversion.FileConverter
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayOutputStream
+import java.io.FileOutputStream
 
 @Component
 class DocxToPdfConverter: FileConverter() {
@@ -28,18 +39,91 @@ class DocxToPdfConverter: FileConverter() {
         val docx = XWPFDocument(file.inputStream)
         val pdfDocument = PdfDocument(PdfWriter(outputStream))
         val document = Document(pdfDocument)
-
-        for (paragraph: XWPFParagraph in docx.paragraphs) {
-            val text = StringBuilder()
-            for (run: XWPFRun in paragraph.runs) {
-                text.append(run.text())
+        for(element in docx.bodyElementsIterator){
+            if(element is XWPFParagraph){
+                val pdfParagraph = createPdfParagraph(element)
+                document.add(pdfParagraph)
             }
-            document.add(Paragraph(text.toString()))
+            if(element is XWPFTable){
+                val pdfTable = createPdfTable(element)
+                document.add(pdfTable)
+            }
+            if(element is XWPFPicture){
+                val pictureData = element.pictureData.data
+                val picture = Image(ImageDataFactory.create(pictureData))
+                picture.setWidth(element.width.toFloat())
+                document.add(picture)
+            }
+            //todo error message?
         }
         document.close()
         pdfDocument.close()
 
+        //todo remove saving
+        val outputFile = "MeineTestDocxToPdf.pdf"
+        try {
+            val fileOutputStream = FileOutputStream(outputFile)
+            fileOutputStream.write(outputStream.toByteArray())
+            fileOutputStream.close()
+            println("PDF-Datei erfolgreich gespeichert: $outputFile")
+        } catch (e: Exception) {
+            println("Fehler beim Speichern der PDF-Datei: ${e.message}")
+        }
+
         return outputStream.toByteArray()
+    }
+
+    private fun createPdfParagraph(xwpfParagraph: XWPFParagraph):Paragraph {
+        val pdfParagraph = Paragraph()
+        for (run: XWPFRun in xwpfParagraph.runs) {
+            if (!run.embeddedPictures.isEmpty()) {
+                val pictures = run.embeddedPictures
+                pictures.forEach{
+                    val pictureData = it.pictureData.data
+                    val picture = Image(ImageDataFactory.create(pictureData))
+                    picture.setWidth(it.width.toFloat())
+                    pdfParagraph.add(picture)
+                }
+            } else {
+
+
+            val txt = Text(run.text())
+
+            if (run.color != null) {
+                val r: Int = substring(run.color, 0, 2).toInt(16) // 16 for hex
+                val g: Int = substring(run.color, 2, 4).toInt(16) // 16 for hex
+                val b: Int = substring(run.color, 4, 6).toInt(16) // 16 for hex
+                val fontColor: Color = DeviceRgb(r, g, b)
+                txt.setFontColor(fontColor)
+            }
+            if (run.isBold) {
+                txt.setBold()
+            }
+
+            if (run.isItalic) {
+                txt.setItalic()
+            }
+            if (run.underline == UnderlinePatterns.SINGLE) {
+                txt.setUnderline()
+            }
+            pdfParagraph.add(txt)
+        }
+    }
+        return pdfParagraph
+    }
+    fun createPdfTable(table: XWPFTable): Table {
+        val pdfTable = Table(table.rows[0].tableCells.size)
+        for (row in table.rows) {
+            for (cell in row.tableCells) {
+                val pdfCell = com.itextpdf.layout.element.Cell()
+                pdfCell.setWidth(cell.widthDecimal.toFloat())
+                for(paragraph in cell.paragraphs){
+                    pdfCell.add(createPdfParagraph(paragraph))
+                }
+                pdfTable.addCell(pdfCell)
+            }
+        }
+        return pdfTable
     }
 
 }
