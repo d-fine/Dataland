72e0ee88797b2ef6a25a540011d1378156b34180
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-03-18 13:51:50 +0100

    frontend components

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/lksg/custom/LksgRiskAssessmentsComponent.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/lksg/custom/LksgRiskAssessmentsComponent.kt
@@ -1,27 +1,27 @@
 package org.dataland.frameworktoolbox.frameworks.lksg.custom
 
-import org.apache.commons.text.StringEscapeUtils
 import org.dataland.frameworktoolbox.intermediate.FieldNodeParent
-import org.dataland.frameworktoolbox.intermediate.components.ComponentBase
-import org.dataland.frameworktoolbox.intermediate.components.addStandardCellWithValueGetterFactory
-import org.dataland.frameworktoolbox.intermediate.components.addStandardUploadConfigCell
 import org.dataland.frameworktoolbox.intermediate.components.requireDocumentSupportIn
 import org.dataland.frameworktoolbox.intermediate.datapoints.NoDocumentSupport
 import org.dataland.frameworktoolbox.specific.datamodel.TypeReference
 import org.dataland.frameworktoolbox.specific.datamodel.elements.DataClassBuilder
-import org.dataland.frameworktoolbox.specific.fixturegenerator.elements.FixtureSectionBuilder
-import org.dataland.frameworktoolbox.specific.uploadconfig.elements.UploadCategoryBuilder
-import org.dataland.frameworktoolbox.specific.viewconfig.elements.SectionConfigBuilder
-import org.dataland.frameworktoolbox.specific.viewconfig.elements.getTypescriptFieldAccessor
-import org.dataland.frameworktoolbox.specific.viewconfig.functional.FrameworkDisplayValueLambda
-import org.dataland.frameworktoolbox.utils.typescript.TypeScriptImport
+
 /**
  * Represents the Lksg risk positions component
  */
 class LksgRiskAssessmentsComponent(
     identifier: String,
     parent: FieldNodeParent,
-) : ComponentBase(identifier, parent) {
+) : LksgSimpleCustomComponentBase(
+    identifier = identifier,
+    parent = parent,
+    viewFormattingFunctionName = "formatLksgRiskPositionsForDisplay",
+    uploadComponentName = "RiskAssessmentsFormField",
+    guaranteedFixtureExpression = "dataGenerator.guaranteedArray(() => " +
+        "dataGenerator.generateLksgRiskOrViolationAssessment(), 0, 5)",
+    randomFixtureExpression = "dataGenerator.randomArray(() => " +
+        "dataGenerator.generateLksgRiskOrViolationAssessment(), 0, 5)",
+) {
     override fun generateDefaultDataModel(dataClassBuilder: DataClassBuilder) {
         requireDocumentSupportIn(setOf(NoDocumentSupport))
         dataClassBuilder.addProperty(
@@ -37,42 +37,6 @@ class LksgRiskAssessmentsComponent(
                 ),
             ),
 
-        )
-    }
-
-    override fun generateDefaultViewConfig(sectionConfigBuilder: SectionConfigBuilder) {
-        requireDocumentSupportIn(setOf(NoDocumentSupport))
-        sectionConfigBuilder.addStandardCellWithValueGetterFactory(
-            this,
-            FrameworkDisplayValueLambda(
-                "formatLksgRiskPositionsForDisplay(${getTypescriptFieldAccessor(true)}, \"${
-                    StringEscapeUtils.escapeEcmaScript(
-                        label,
-                    )
-                }\")",
-                setOf(
-                    TypeScriptImport(
-                        "formatLksgRiskPositionsForDisplay",
-                        "@/components/resources/dataTable/conversion/lksg/LksgDisplayValueGetters",
-                    ),
-                ),
-            ),
-        )
-    }
-
-    override fun generateDefaultUploadConfig(uploadCategoryBuilder: UploadCategoryBuilder) {
-        requireDocumentSupportIn(setOf(NoDocumentSupport))
-        uploadCategoryBuilder.addStandardUploadConfigCell(
-            component = this,
-            uploadComponentName = "RiskAssessmentsFormField",
-        )
-    }
-
-    override fun generateDefaultFixtureGenerator(sectionBuilder: FixtureSectionBuilder) {
-        requireDocumentSupportIn(setOf(NoDocumentSupport))
-        sectionBuilder.addAtomicExpression(
-            identifier,
-            "dataGenerator.randomArray(() => dataGenerator.generateLksgRiskOrViolationAssessment(), 0, 5)",
         )
     }
 }
