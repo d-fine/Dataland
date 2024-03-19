bdd97659c998c1a2a0250f054eca6c6431c0a9e0
Florian Soffel <florian.soffel@d-fine.de>
2024-03-19 11:51:37 +0100

    quicksave

--- dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/services/conversion/OdsToOdsConverter.kt
+++ dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/services/conversion/OdsToOdsConverter.kt
@@ -1,8 +1,7 @@
 package org.dataland.documentmanager.services.conversion
 
 import org.dataland.datalandbackendutils.exceptions.InvalidInputApiException
-import org.odftoolkit.odfdom.doc.OdfSpreadsheetDocument
-import org.odftoolkit.odfdom.doc.table.OdfTable
+import org.odftoolkit.simple.SpreadsheetDocument
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -20,42 +19,19 @@ class OdsToOdsConverter : FileConverter() {
 
     override fun validateFileContent(file: MultipartFile, correlationId: String) {
         logger.info("Validating that ods file is not empty. (correlation ID: $correlationId)")
-        println("@validation")
         file.inputStream.use { inputStream ->
-            val document = OdfSpreadsheetDocument.loadDocument(inputStream)
-            document.spreadsheetTables.forEach { table ->
-                validateTable(table)
-            }
-        }
-        println("done")
-        throw InvalidInputApiException(
-            "An empty spreadsheet was provided",
-            "An empty spreadsheet was provided",
-        )
-    }
-
-    private fun validateTable(table: OdfTable) {
-        println("table ${table.tableName}")
-//        println("new sheet with ${table.rowList.filter {
-//            println("row filter")
-//            println("hello ${it}")
-//            it.cellCount > 0}} rows")
-//        if(table.rowCount > 0) {
-//            println("@return")
-//            return
-//        }
-        val numColumns = table.columnCount
-        table.rowList.forEach { row ->
-//            val cellRange = table.getCellRangeByPosition(0, row.rowIndex, numColumns-1, row.rowIndex)
-            println("row ${row.rowIndex} of ${table.rowCount}")
-//            println("with ${row.cellCount} cells")
-            for (i in 0 until numColumns) {
-                val cell = row.getCellByIndex(i)
-                if (cell.stringValue?.isNotBlank() == true) {
+            val document = SpreadsheetDocument.loadDocument(inputStream)
+            for (sheetIndex in 0 until document.sheetCount) {
+                val sheet = document.getSheetByIndex(sheetIndex)
+                if (sheet.rowCount > 0) {
                     return
                 }
             }
         }
+        throw InvalidInputApiException(
+            "An empty spreadsheet was provided",
+            "An empty spreadsheet was provided",
+        )
     }
 
     override fun convert(file: MultipartFile, correlationId: String): ByteArray {
