ef2c8b7589b315da3fbd575834816d0b92784f4a
Florian Soffel <florian.soffel@d-fine.de>
2024-03-21 16:46:48 +0100

    fix bug

--- dataland-document-manager/src/test/kotlin/org/dataland/documentmanager/services/conversion/Utils.kt
+++ dataland-document-manager/src/test/kotlin/org/dataland/documentmanager/services/conversion/Utils.kt
@@ -1,14 +1,13 @@
 package org.dataland.documentmanager.services.conversion
 
+import org.mockito.Mockito.any
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
-import org.mockito.kotlin.any
 import xyz.capybara.clamav.ClamavClient
 import xyz.capybara.clamav.commands.scan.result.ScanResult
-import java.io.InputStream
 
 fun mockClamAvClient(): ClamavClient {
     return mock(ClamavClient::class.java).also {
-        `when`(it.scan(any() as InputStream)).thenReturn(ScanResult.OK)
+        `when`(it.scan(any() ?: "TEST".encodeToByteArray().inputStream())).thenReturn(ScanResult.OK)
     }
 }
