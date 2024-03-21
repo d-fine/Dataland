f3f47065995d2bddd2d9a892cf03718aa169f3b6
Johannes Haerkoetter <johannes.haerkoetter@d-fine.com>
2024-03-21 11:30:35 +0100

    txt to pdf converterTest

--- dataland-document-manager/src/test/kotlin/org/dataland/documentmanager/services/conversion/TextToPdfConverterTest.kt
+++ dataland-document-manager/src/test/kotlin/org/dataland/documentmanager/services/conversion/TextToPdfConverterTest.kt
@@ -1,9 +1,11 @@
 package org.dataland.documentmanager.services.conversion
 
 import org.apache.tika.Tika
+import org.dataland.datalandbackendutils.exceptions.InvalidInputApiException
 import org.dataland.documentmanager.services.TestUtils
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.http.MediaType
 import org.springframework.mock.web.MockMultipartFile
 
@@ -12,6 +14,7 @@ class TextToPdfConverterTest {
     private val correlationId = "test-correlation-id"
     private val textToPdfConverter = TextToPdfConverter()
     private val testTxt = "sampleFiles/sample.txt"
+    private val emptyTxt = "sampleFiles/emptyTxt.txt"
 
     @Test
     fun `verify that a txt file is converted to pdf`() {
@@ -22,7 +25,23 @@ class TextToPdfConverterTest {
             TestUtils().loadFileBytes(testTxt),
         )
         assertEquals("text/plain", Tika().detect(testInput.bytes))
+        textToPdfConverter.validateFile(testInput, correlationId)
         val convertedDocument = textToPdfConverter.convertFile(testInput, correlationId)
         assertEquals("application/pdf", Tika().detect(convertedDocument))
+    }
+
+    @Test
+    fun `check that an empty txt file is not validated`() {
+        val testInput = MockMultipartFile(
+            "test.txt",
+            "test.txt",
+            MediaType.TEXT_PLAIN_VALUE,
+            TestUtils().loadFileBytes(emptyTxt),
+        )
+        assertEquals("text/plain", Tika().detect(testInput.bytes))
+        val exception = assertThrows<InvalidInputApiException> {
+            textToPdfConverter.validateFile(testInput, correlationId)
+        }
+        assertEquals("Provided file is empty.", exception.message)
     }
 }
