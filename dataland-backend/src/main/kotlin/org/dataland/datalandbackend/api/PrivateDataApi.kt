8c5f2a2dd3c9f3adf33c300801362b17da3277be
swaldow <stephan.waldow@d-fine.de>
2024-03-25 09:09:02 +0100

    wip openapi definition

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/PrivateDataApi.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/api/PrivateDataApi.kt
@@ -1,15 +1,18 @@
 package org.dataland.datalandbackend.api
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.media.Schema
+import org.dataland.datalandbackend.frameworks.sme.model.SmeData
+import org.dataland.datalandbackend.model.companies.CompanyAssociatedData
 import org.dataland.datalandbackend.model.metainformation.DataMetaInformation
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 /**
@@ -34,16 +37,34 @@ interface PrivateDataApi {
             ApiResponse(responseCode = "200", description = "S."), // TODO
         ],
     )
-    @PostMapping(
-        produces = [MediaType.APPLICATION_JSON_VALUE],
-        consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
-    )
+    /*     @PostMapping(
+            produces = [MediaType.APPLICATION_JSON_VALUE],
+            consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
+        )
+       fun postSmeJsonAndDocuments(
+            @RequestPart
+            companyAssociatedSmeDataAsString: CompanyAssociatedData<SmeData>,
+            @RequestPart documents: Array<MultipartFile>? = null,
+        ):
+            ResponseEntity<DataMetaInformation>
+   */
+   /* @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE],consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun postSmeJsonAndDocuments(
-        @RequestPart
-        companyAssociatedSmeDataAsString: String,
-        @RequestPart documents: Array<MultipartFile>,
-    ):
-        ResponseEntity<DataMetaInformation>
+        @RequestPart(value = "companyAssociatedSmeDataAsString") @Parameter(
+            schema = Schema(
+                type = "string",
+                format = "binary"
+            )
+        ) companyAssociatedSmeDataAsString: CompanyAssociatedData<SmeData>,
+        @RequestPart(value = "file") documents: Array<MultipartFile>? = null,
+    ): ResponseEntity<DataMetaInformation>
+*/
+    @PostMapping()
+    fun postSmeJsonAndDocuments(
+        @RequestBody() data: CompanyAssociatedData<SmeData>,
+        @RequestParam() documents: Array<MultipartFile>?
+    ): ResponseEntity<DataMetaInformation>
+
 }
 
 // TODO nginx beschwert sich noch wenn die pdfs zu groß sind!  dort musst du auch die maxxfile size einstellen
