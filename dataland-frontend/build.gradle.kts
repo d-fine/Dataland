bd4a36e31664207e4645b4b09c97b12fac63716f
Aliabdoli77 <aliabdoli77@yahoo.com>
2024-03-27 11:17:28 +0100

    update node version

--- dataland-frontend/build.gradle.kts
+++ dataland-frontend/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 node {
     download.set(true)
-    version.set("20.11.1")
+    version.set("20.12.0")
 }
 
 val backendOpenApiFile = "${project.rootDir}/dataland-backend/backendOpenApi.json"
