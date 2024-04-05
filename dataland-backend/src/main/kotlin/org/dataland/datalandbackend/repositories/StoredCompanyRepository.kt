4686ee00f5b0ad5de184f855f0fbcc145d022e59
Johannes Haerkoetter <johannes.haerkoetter@d-fine.com>
2024-04-05 19:47:33 +0200

    query update

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/repositories/StoredCompanyRepository.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/repositories/StoredCompanyRepository.kt
@@ -259,6 +259,7 @@ interface StoredCompanyRepository : JpaRepository<StoredCompanyEntity, String> {
         @Param("resultLimit") resultLimit: Int = 100,
         @Param("resultOffset") resultOffset: Int = 0,
     ): List<BasicCompanyInformation>
+    //todo merge queries
 
     /**
      * A function for counting the number of companies by various filters:
