fb8f8b0a0d1a71c2e5d67f74a9dc54fe0c6eae28
Johannes Haerkoetter <johannes.haerkoetter@d-fine.com>
2024-04-05 17:33:00 +0200

    query update

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/repositories/StoredCompanyRepository.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/repositories/StoredCompanyRepository.kt
@@ -71,10 +71,66 @@ interface StoredCompanyRepository : JpaRepository<StoredCompanyEntity, String> {
             " AND (:#{#searchFilter.sectorFilterSize} = 0 OR sector IN :#{#searchFilter.sectorFilter}) " +
             " AND (:#{#searchFilter.countryCodeFilterSize} = 0" +
             " OR country_code IN :#{#searchFilter.countryCodeFilter}) " +
-            // " AND company_id IN (" +
-            // check for searchstring
+            " AND company_id IN"+
+                "(" +
+        // Fuzzy-Search Company Name
+        " (SELECT stored_companies.company_id, max(stored_companies.company_name) AS company_name," +
+                " max(CASE " +
+                " WHEN company_name = :#{#searchString} THEN 10" +
+                " WHEN company_name ILIKE :#{escape(#searchString)}% ESCAPE :#{escapeCharacter()} THEN 5" +
+                " ELSE 1" +
+                " END) match_quality, " +
+                " max(CASE WHEN data_id IS NOT null THEN 2 else 1 END) AS dataset_rank" +
+                " FROM stored_companies" +
+                " LEFT JOIN data_meta_information " +
+                " ON stored_companies.company_id = data_meta_information.company_id AND currently_active = true" +
+                " WHERE company_name ILIKE %:#{escape(#searchString)}% ESCAPE :#{escapeCharacter()}" +
+                " GROUP BY stored_companies.company_id" +
+                " ORDER BY" +
+                " dataset_rank DESC," +
+                " match_quality DESC, stored_companies.company_id LIMIT :#{#resultLimit})" +
 
-            // limit the results
+                " UNION " +
+                // Fuzzy-Search Company Alternative Name
+                " (SELECT " +
+                " stored_company_entity_company_id AS company_id," +
+                " max(stored_companies.company_name) AS company_name," +
+                " max(CASE " +
+                " WHEN company_alternative_names = :#{#searchString} THEN 9" +
+                " WHEN company_alternative_names ILIKE :#{escape(#searchString)}% ESCAPE :#{escapeCharacter()} THEN 4" +
+                " ELSE 1 " +
+                " END) match_quality, " +
+                " max(CASE WHEN data_id IS NOT null THEN 2 else 1 END) AS dataset_rank" +
+                " FROM stored_company_entity_company_alternative_names" +
+                " JOIN stored_companies ON stored_companies.company_id = " +
+                " stored_company_entity_company_alternative_names.stored_company_entity_company_id  " +
+                " LEFT JOIN data_meta_information " +
+                " ON stored_company_entity_company_id = data_meta_information.company_id AND currently_active = true" +
+                " WHERE company_alternative_names ILIKE %:#{escape(#searchString)}% ESCAPE :#{escapeCharacter()}" +
+                " GROUP BY stored_company_entity_company_id" +
+                " ORDER BY " +
+                " dataset_rank DESC," +
+                " match_quality DESC, stored_company_entity_company_id LIMIT :#{#resultLimit})" +
+
+                " UNION" +
+                // Fuzzy-Search Company Identifier
+                " (SELECT company_identifiers.company_id, max(stored_companies.company_name) AS company_name," +
+                " max(CASE " +
+                " WHEN identifier_value = :#{#searchString} THEN 10" +
+                " WHEN identifier_value ILIKE :#{escape(#searchString)}% ESCAPE :#{escapeCharacter()} THEN 3" +
+                " ELSE 0" +
+                " END) AS match_quality, " +
+                " max(CASE WHEN data_id IS NOT null THEN 2 else 1 END) AS dataset_rank" +
+                " FROM company_identifiers" +
+                " JOIN stored_companies ON stored_companies.company_id = company_identifiers.company_id " +
+                " LEFT JOIN data_meta_information " +
+                " ON company_identifiers.company_id = data_meta_information.company_id AND currently_active = true" +
+                " WHERE identifier_value ILIKE %:#{escape(#searchString)}% ESCAPE :#{escapeCharacter()} " +
+                " GROUP BY company_identifiers.company_id" +
+                " ORDER BY " +
+                " dataset_rank DESC," +
+                " match_quality DESC, company_identifiers.company_id LIMIT :#{#resultLimit})"+
+                ") " +
             " ORDER BY company_name ASC LIMIT :#{#resultLimit} OFFSET :#{#resultOffset}) AS filtered_data" +
             " LEFT JOIN (" +
             // get all LEI identifiers
