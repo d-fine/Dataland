e8134073f48bab2af0d79a237760493d6af1f141
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-04-10 17:37:40 +0200

    re-styling

--- dataland-frontend/src/components/forms/parts/fields/YesNoFormField.vue
+++ dataland-frontend/src/components/forms/parts/fields/YesNoFormField.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mb-3" :class="classes" :data-test="name">
+  <div :class="classes" :data-test="name">
     <UploadFormHeader :label="label" :description="description" :is-required="required" />
     <CheckboxesListFormElement
       :name="name"
