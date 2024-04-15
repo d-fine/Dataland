7893ea46c4a634ded7664b33de91597820a7b601
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-04-15 17:42:57 +0200

    using constant

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/utils/ConstantDailyDataRequestLimit.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/utils/ConstantDailyDataRequestLimit.kt
@@ -1,3 +1,10 @@
 package org.dataland.datalandcommunitymanager.utils
 
-const val MAX_NUMBER_OF_DATA_REQUESTS_PER_DAY_FOR_ROLE_USER = 10
+private const val MAX_NUMBER_OF_DATA_REQUESTS_PER_DAY_FOR_ROLE_USER_DEFAULT = 10
+
+private val getEnv = System.getenv("MAX_NUMBER_OF_DATA_REQUESTS_PER_DAY_FOR_ROLE_USER")
+val MAX_NUMBER_OF_DATA_REQUESTS_PER_DAY_FOR_ROLE_USER = if (getEnv.isNullOrEmpty()) {
+    MAX_NUMBER_OF_DATA_REQUESTS_PER_DAY_FOR_ROLE_USER_DEFAULT
+} else {
+    getEnv.toInt()
+}
