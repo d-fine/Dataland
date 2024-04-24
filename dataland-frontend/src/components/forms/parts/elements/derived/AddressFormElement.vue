062909f165afe7c4be7c2e31ee558bde147ab8ea
AdrianGardijan <adrian.gardijan@d-fine.com>
2024-04-24 14:06:48 +0200

    FormKit replaced in AddressFormElement.vue

--- dataland-frontend/src/components/forms/parts/elements/derived/AddressFormElement.vue
+++ dataland-frontend/src/components/forms/parts/elements/derived/AddressFormElement.vue
@@ -2,15 +2,17 @@
   <FormKit type="group" :name="name" :label="name">
     <FormKit type="text" name="streetAndHouseNumber" placeholder="Street, House number" />
     <div class="next-to-each-other">
-      <FormKit
-        type="select"
-        name="country"
-        validation-label="Country"
-        :validation="required"
-        placeholder="Country"
-        :options="allCountry"
-        data-test="country"
-      />
+      <div class="formkit-outer shortish-hard">
+        <SingleSelectFormElement
+          name="country"
+          validation-label="Country"
+          :validation="required"
+          placeholder="Country"
+          :options="allCountry"
+          data-test="country"
+          input-class="long normal-line-height"
+        />
+      </div>
       <FormKit type="text" name="city" validation-label="City" :validation="required" placeholder="City" />
       <FormKit type="text" name="postalCode" placeholder="Postal Code" />
     </div>
@@ -21,10 +23,11 @@
 import { defineComponent } from "vue";
 import { FormKit } from "@formkit/vue";
 import { DropdownDatasetIdentifier, getDataset } from "@/utils/PremadeDropdownDatasets";
+import SingleSelectFormElement from "@/components/forms/parts/elements/basic/SingleSelectFormElement.vue";
 
 export default defineComponent({
   name: "AddressFormElement",
-  components: { FormKit },
+  components: { SingleSelectFormElement, FormKit },
   data() {
     return {
       allCountry: getDataset(DropdownDatasetIdentifier.CountryCodesIso2),
