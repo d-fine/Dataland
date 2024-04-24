855f5bd4e3791933876648fd0f040277f11cc763
swaldow <stephan.waldow@d-fine.de>
2024-04-24 15:25:15 +0200

    fixes persisting issue of meta data

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/DataIdToAssetIdCompositeKey.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/DataIdToAssetIdCompositeKey.kt
@@ -1,12 +1,18 @@
 package org.dataland.datalandbackend.model
+import jakarta.persistence.Embeddable
 import java.io.Serializable
 
 /**
  * The primary key in the dataId-assetId-mapping-table is a composite key. In order to define that composite key, this
  * class here is required.
  */
-class DataIdToAssetIdCompositeKey : Serializable { // TODO name ok?
-    val dataId: String? = null // TODO why nullable
-    val assetId: String? = null // TODO why nullable   TODO Emanuel: Naming?  Ist das nicht nur noch hash oder "JSON"?
-    val eurodatId: String? = null // TODO why nullable   TODO Emanuel: Naming??
+@Embeddable
+data class DataIdToAssetIdCompositeKey(
+    var dataId: String,
+    var assetId: String,
+    var eurodatId: String,
+) : Serializable {
+    companion object {
+        private const val serialVersionUID: Long = 138765
+    }
 }
