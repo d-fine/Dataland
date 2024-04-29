2c9f03f4f8c1d54a6be228f3f53b8b9d8bdccc7b
AdrianGardijan <adrian.gardijan@d-fine.com>
2024-04-29 17:00:35 +0200

    hopefully fixed tests CurrencyDataPointFormField.cy.ts

--- dataland-frontend/tests/component/components/forms/parts/fields/CurrencyDataPointFormField.cy.ts
+++ dataland-frontend/tests/component/components/forms/parts/fields/CurrencyDataPointFormField.cy.ts
@@ -4,10 +4,10 @@ describe("Component test for BigDecimalExtendedDataPointFormField", () => {
   it("Unit field should be visible when options are defined and Quality field should be NA if the value field has no value", () => {
     cy.mountWithPlugins(CurrencyDataPointFormField, {}).then(() => {
       cy.get('[data-test="dataPointToggleButton"]').click();
-      cy.get('select[name="currency"]').should("exist");
-      cy.get('select[name="currency"]').should("contain", "EUR");
+      cy.get('div[name="currency"]').should("exist");
+      cy.get('div[name="currency"]').find("span").should("contain", "EUR");
       cy.get('input[name="currency"][type="hidden"]').should("not.exist");
-      cy.get('div[data-test="dataQuality"] select[name="quality"]').should("have.value", "NA");
+      cy.get('div[data-test="dataQuality"] div[name="quality"]').find("span").should("have.value", "NA");
       cy.get('div[data-test="dataQuality"] .form-field-label span.asterisk').should("not.exist");
     });
   });
