8d3997d17e0ecd87b77848ea170454b0d0886bc0
AdrianGardijan <adrian.gardijan@d-fine.com>
2024-04-29 16:47:42 +0200

    hopefully fixed tests added Dropdown selector

---
+++ dataland-frontend/tests/component/testUtils/Dropdown.ts
@@ -0,0 +1,37 @@
+/**
+ * Selects an option from a SingleSelectFormElement by value
+ * @param dropdownDiv a div with only a single SingleSelectFormElement inside of it, on which an option will be selected
+ * @param valueToSelect string, number or RegExp of the option to select
+ * @param exactMatchNotNeeded boolean, if true, the first option to contain the given value will be selected
+ */
+export function selectItemFromDropdownByValue(
+  dropdownDiv: Cypress.Chainable<JQuery<HTMLElement>>,
+  valueToSelect: string | number | RegExp,
+  exactMatchNotNeeded?: boolean,
+): void {
+  dropdownDiv.find(".p-dropdown-trigger").click();
+  if (exactMatchNotNeeded) {
+    cy.get(".p-dropdown-items").contains(valueToSelect).should("contain.text", valueToSelect);
+    cy.get(".p-dropdown-items").contains(valueToSelect).click();
+  } else {
+    cy.get(".p-dropdown-items")
+      .contains(new RegExp(`^${valueToSelect}$`))
+      .should("have.text", valueToSelect);
+    cy.get(".p-dropdown-items")
+      .contains(new RegExp(`^${valueToSelect}$`))
+      .trigger("click");
+  }
+}
+
+/**
+ * Selects an option from a SingleSelectFormElement by index
+ * @param dropdownDiv a div with only a single SingleSelectFormElement inside of it, on which an option will be selected
+ * @param indexToSelect number, index of the option to be selected
+ */
+export function selectItemFromDropdownByIndex(
+  dropdownDiv: Cypress.Chainable<JQuery<HTMLElement>>,
+  indexToSelect: number,
+): void {
+  dropdownDiv.find(".p-dropdown-trigger").click();
+  cy.get(".p-dropdown-items").find("li").eq(indexToSelect).click();
+}
