73cd49e39244dd62a2a6b7f2b52e11875f1084a1
AdrianGardijan <adrian.gardijan@d-fine.com>
2024-05-02 14:20:44 +0200

    updated SingleDataRequest.ts

--- dataland-frontend/tests/sharedUtils/components/SingleDataRequest.ts
+++ dataland-frontend/tests/sharedUtils/components/SingleDataRequest.ts
@@ -1,4 +1,5 @@
 import { DataTypeEnum } from "@clients/backend";
+import { selectItemFromDropdownByValue } from "@sharedUtils/Dropdown";
 
 export const singleDataRequestPage = {
   chooseReportingPeriod(reportingPeriod: string = "2023"): void {
@@ -13,15 +14,13 @@ export const singleDataRequestPage = {
   },
   chooseFrameworkLksg(): void {
     const numberOfFrameworks = Object.keys(DataTypeEnum).length;
-    cy.get('[data-test="selectFramework"]')
-      .should("exist")
-      .get('[data-type="select"]')
-      .should("exist")
-      .click()
-      .get('[data-test="datapoint-framework"]')
-      .select(DataTypeEnum.Lksg);
-    cy.get('[data-test="datapoint-framework"]')
-      .children()
-      .should("have.length", numberOfFrameworks + 1);
+    selectItemFromDropdownByValue(
+      cy.get('[data-test="selectFramework"]').should("exist").get('[data-test="datapoint-framework"]'),
+      "LkSG",
+      true,
+    );
+    cy.get('[data-test="datapoint-framework"]').get(".p-dropdown-trigger").click();
+    cy.get(".p-dropdown-items").find("li").should("have.length", numberOfFrameworks);
+    cy.get('[data-test="datapoint-framework"]').get(".p-dropdown-trigger").click();
   },
 };
