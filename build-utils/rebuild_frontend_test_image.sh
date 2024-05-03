087f99337cf77cb6da73f2777ee2d1258ea1f303
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2024-05-03 20:23:46 +0200

    streamline frontend container

--- build-utils/rebuild_frontend_test_image.sh
+++ build-utils/rebuild_frontend_test_image.sh
@@ -2,8 +2,8 @@
 set -euxo pipefail
 
 gradle_dependencies=$(grep gradle_dependencies ./build-utils/common.conf | cut -d'=' -f2)
-dependencies="./dataland-frontend/ ./dataland-backend/backendOpenApi.json ./dataland-document-manager/documentManagerOpenApi.json ./dataland-api-key-manager/apiKeyManagerOpenApi.json"
-dependencies+=" ./dataland-qa-service/qaServiceOpenApi.json ./dataland-community-manager/communityManagerOpenApi.json ./environments/.env.uncritical $gradle_dependencies"
+frontend_dependencies=$(grep frontend_dependencies ./build-utils/common.conf | cut -d'=' -f2)
+dependencies="$frontend_dependencies $gradle_dependencies"
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_frontend_test ./dataland-frontend/DockerfileTest $dependencies
 
