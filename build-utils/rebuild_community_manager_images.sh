03bf5e5ea3aa9880ed430854d3d483c56e67473f
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2024-05-03 20:12:01 +0200

    introduce dependencies everywhere

--- build-utils/rebuild_community_manager_images.sh
+++ build-utils/rebuild_community_manager_images.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_community_manager_base ./dataland-community-manager/DockerfileBase \
-          ./dataland-backend-utils/ ./dataland-backend/ ./dataland-api-key-manager/ ./dataland-keycloak-adapter/ \
-          ./dataland-community-manager/ ./build.gradle.kts ./gradle.properties ./settings.gradle.kts ./versions.properties \
-          ./dataland-message-queue-utils
+gradle_dependencies=$(grep gradle_dependencies ./build-utils/common.conf | cut -d'=' -f2)
+dependencies="./dataland-backend-utils/ ./dataland-backend/ ./dataland-api-key-manager/ ./dataland-keycloak-adapter/ ./dataland-community-manager/ ./dataland-message-queue-utils $gradle_dependencies"
+
+./build-utils/base_rebuild_single_docker_image.sh dataland_community_manager_base ./dataland-community-manager/DockerfileBase $dependencies
 
 set -o allexport
 source ./*github_env.log
 set +o allexport
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_community_manager_production ./dataland-community-manager/Dockerfile ./dataland-community-manager/ \
-        ./dataland-backend-utils/ ./dataland-backend/ ./dataland-api-key-manager/ ./dataland-keycloak-adapter/ \
-        ./build.gradle.kts ./gradle.properties ./settings.gradle.kts ./versions.properties ./dataland-message-queue-utils
+./build-utils/base_rebuild_single_docker_image.sh dataland_community_manager_production ./dataland-community-manager/Dockerfile $dependencies
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_community_manager_test ./dataland-community-manager/DockerfileTest ./dataland-community-manager/ \
-        ./dataland-backend-utils/ ./dataland-backend/ ./dataland-api-key-manager/ ./dataland-keycloak-adapter/ \
-        ./build.gradle.kts ./gradle.properties ./settings.gradle.kts ./versions.properties ./dataland-message-queue-utils
+./build-utils/base_rebuild_single_docker_image.sh dataland_community_manager_test ./dataland-community-manager/DockerfileTest $dependencies
--- build-utils/rebuild_internal_storage_images.sh
+++ build-utils/rebuild_internal_storage_images.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_internal_storage_base \
-          ./dataland-internal-storage/DockerfileBase ./dataland-internal-storage/ \
-          ./dataland-backend/backendOpenApi.json ./dataland-document-manager/documentManagerOpenApi.json \
-          ./dataland-backend-utils/ ./dataland-message-queue-utils/ \
-          ./build.gradle.kts ./gradle.properties ./settings.gradle.kts ./versions.properties
+gradle_dependencies=$(grep gradle_dependencies ./build-utils/common.conf | cut -d'=' -f2)
+dependencies="./dataland-internal-storage/ ./dataland-backend-utils/ ./dataland-message-queue-utils/ $gradle_dependencies"
+dependencies+=" ./dataland-backend/backendOpenApi.json ./dataland-document-manager/documentManagerOpenApi.json"
+
+./build-utils/base_rebuild_single_docker_image.sh dataland_internal_storage_base ./dataland-internal-storage/DockerfileBase $dependencies
 
 set -o allexport
 source ./*github_env.log
 set +o allexport
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_internal_storage_test \
-          ./dataland-internal-storage/DockerfileTest ./dataland-internal-storage/ \
-          ./dataland-backend/backendOpenApi.json ./dataland-document-manager/documentManagerOpenApi.json \
-          ./dataland-backend-utils/ ./dataland-message-queue-utils/ \
-          ./build.gradle.kts ./gradle.properties ./settings.gradle.kts ./versions.properties
+./build-utils/base_rebuild_single_docker_image.sh dataland_internal_storage_test ./dataland-internal-storage/DockerfileTest $dependencies
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_internal_storage_production \
-          ./dataland-internal-storage/Dockerfile ./dataland-internal-storage/ \
-          ./dataland-backend/backendOpenApi.json ./dataland-document-manager/documentManagerOpenApi.json \
-          ./dataland-backend-utils/ ./dataland-message-queue-utils/ \
-          ./build.gradle.kts ./gradle.properties ./settings.gradle.kts ./versions.properties
+./build-utils/base_rebuild_single_docker_image.sh dataland_internal_storage_production ./dataland-internal-storage/Dockerfile $dependencies
