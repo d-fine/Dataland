21301c530da030b7c0d6e5ae02279cd39499c68e
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2024-05-08 13:20:28 +0200

    Fix regex and add tests (#484)
    regex fix and added tests

---
+++ dataland-backend-utils/src/test/kotlin/org/dataland/datalandbackendutils/services/utils/EmailUtilsTest.kt
@@ -0,0 +1,30 @@
+package org.dataland.datalandbackendutils.services.utils
+
+import org.dataland.datalandbackendutils.utils.isEmailAddress
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class EmailUtilsTest {
+
+    @Test
+    fun `validate that the email regex correctly matches dots`() {
+        assertEquals("test.test@test.com".isEmailAddress(), true)
+        assertEquals("test.test@dev.test.com".isEmailAddress(), true)
+        assertEquals("test@test.com".isEmailAddress(), true)
+        assertEquals("test@testcom".isEmailAddress(), false)
+    }
+
+    @Test
+    fun `validate that the email regex is not case sensitive`() {
+        assertEquals("Test.Test@test.com".isEmailAddress(), true)
+        assertEquals("test.test@TesT.CoM".isEmailAddress(), true)
+        assertEquals("TEST@TEST.COM".isEmailAddress(), true)
+        assertEquals("TEST@TESTCOM".isEmailAddress(), false)
+    }
+
+    @Test
+    fun `validate that the email regex enforces correct placement of the at symbol`() {
+        assertEquals("@test.com".isEmailAddress(), false)
+        assertEquals("test@test.com@".isEmailAddress(), false)
+    }
+}
