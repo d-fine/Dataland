4721b34f674d677ed60235b26ebcc86c35fe8849
Johannes Haerkoetter <johannes.haerkoetter@d-fine.com>
2024-05-10 11:44:07 +0200

    try @Transactional as needed

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/DataManager.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/DataManager.kt
@@ -85,8 +85,7 @@ class DataManager(
      * @param correlationId the correlationId of the request
      * @return ID of the newly stored data in the data store
      */
-    // todo discuss @Transactional
-    @Transactional
+    @Transactional(propagation = Propagation.NEVER)
     fun storeDataSetInMemoryAndSendReceptionMessageAndPersistMetaInfo(
         storableDataSet: StorableDataSet,
         bypassQa: Boolean,
