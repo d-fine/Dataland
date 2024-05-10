446f33bd3ab5255b228f2fed3c2f844c445d250b
brunoneumannsaavedra <165763416+brunoneumannsaavedra@users.noreply.github.com>
2024-05-10 11:05:22 +0200

    Created sql statements in migration class for request status history.

--- dataland-community-manager/src/main/kotlin/db/migration/V7__MigrateRequestStatusHistory.kt
+++ dataland-community-manager/src/main/kotlin/db/migration/V7__MigrateRequestStatusHistory.kt
@@ -1,4 +1,4 @@
-package db.migration;
+package db.migration
 
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
@@ -6,9 +6,21 @@ import org.flywaydb.core.api.migration.Context
 /**
  * This migration script migrates the request status history to a separate table
  */
-public class V7__MigrateRequestStatusHistory: BaseJavaMigration() {
-
+public class V7__MigrateRequestStatusHistory : BaseJavaMigration() {
     override fun migrate(context: Context?) {
-        // todo
+        context!!.connection.createStatement().execute(
+            "ALTER TABLE data_requests " +
+                "DROP COLUMN request_status",
+        )
+        context.connection.createStatement().execute(
+            "CREATE TABLE request_status_history (" +
+                "status_history_id varchar(255) NOT NULL, " +
+                "data_request_id varchar(255) NOT NULL, " +
+                "request_status varchar(255) NOT NULL, " +
+                "creation_timestamp bigint NOT NULL, " +
+                "PRIMARY KEY (status_history_id), " +
+                "CONSTRAINT fk_data_request FOREIGN KEY(data_request_id) REFERENCES data_requests(data_request_id)" +
+                ")",
+        )
     }
 }
