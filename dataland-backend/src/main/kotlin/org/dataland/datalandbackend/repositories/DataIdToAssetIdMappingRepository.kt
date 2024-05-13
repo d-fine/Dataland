154675eb0b13e60dfb8fb50ad07e4b5e4c9c8c10
swaldow <stephan.waldow@d-fine.de>
2024-05-13 14:30:05 +0200

    changes due to review comments

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/repositories/DataIdToAssetIdMappingRepository.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/repositories/DataIdToAssetIdMappingRepository.kt
@@ -2,15 +2,13 @@ package org.dataland.datalandbackend.repositories
 
 import org.dataland.datalandbackend.entities.DataIdToAssetIdMappingEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 /**
- * A JPA repository for accessing DataIdToAssetIdMappingEntity
+ * A JPA repository for accessing DataIdToAssetIdMappingEntities
  */
 interface DataIdToAssetIdMappingRepository : JpaRepository<DataIdToAssetIdMappingEntity, String> {
     /**
      * Retrieves the eurodatId of a document, which is uniquely identified by the combination of dataId and assetId
      */
-    @Query("SELECT m FROM DataIdToAssetIdMappingEntity m WHERE m.dataId = :dataId AND m.assetId = :assetId")
     fun findByDataIdAndAssetId(dataId: String, assetId: String): List<DataIdToAssetIdMappingEntity>
 }
