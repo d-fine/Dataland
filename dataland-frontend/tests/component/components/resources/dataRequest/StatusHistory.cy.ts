eac42732ec4133ff6e3106e0ffbd9ef7cfc6e32e
AdrianGardijan <adrian.gardijan@d-fine.com>
2024-05-14 11:31:41 +0200

    added test for correct time formatting in StatusHistory.cy.ts

--- dataland-frontend/tests/component/components/resources/dataRequest/StatusHistory.cy.ts
+++ dataland-frontend/tests/component/components/resources/dataRequest/StatusHistory.cy.ts
@@ -4,6 +4,12 @@ import { minimalKeycloakMock } from "@ct/testUtils/Keycloak";
 
 describe("Component tests for the Status History", function (): void {
   const expectedStatus = ["Open", "Answered", "Resolved", "Closed"];
+  const expectedDates = [
+    "Sun, 28 Apr 2024, 16:37",
+    "Mon, 29 Apr 2024, 20:24",
+    "Thu, 2 May 2024, 03:57",
+    "Thu, 2 May 2024, 15:04",
+  ];
   const dummyStatusHistory = [
     {
       status: RequestStatus.Open,
@@ -38,9 +44,12 @@ describe("Component tests for the Status History", function (): void {
 
     cy.get('[data-test="creation_timestamp"]').should("be.visible");
     cy.get('[data-test="creation_timestamp"]').should("have.length", 4);
+    cy.get('[data-test="creation_timestamp"]').each(($el, index) => {
+      expect($el.text()).to.eq(expectedDates[index]);
+    });
+
     cy.get('[data-test="request_status"]').should("be.visible");
     cy.get('[data-test="request_status"]').should("have.length", 4);
-
     cy.get('[data-test="request_status"]').each(($el, index) => {
       expect($el.text()).to.eq(expectedStatus[index]);
     });
