5a72dcfd0998f4cbaf99112fba533270573574bf
AdrianGardijan <adrian.gardijan@d-fine.com>
2024-05-14 14:03:17 +0200

    fixed test for correct time formatting in StatusHistory.cy.ts

--- dataland-frontend/tests/component/components/resources/dataRequest/StatusHistory.cy.ts
+++ dataland-frontend/tests/component/components/resources/dataRequest/StatusHistory.cy.ts
@@ -1,15 +1,10 @@
 import StatusHistoryComponent from "@/components/resources/dataRequest/StatusHistory.vue";
 import { RequestStatus, type StoredDataRequestStatusObject } from "@clients/communitymanager";
 import { minimalKeycloakMock } from "@ct/testUtils/Keycloak";
+import { convertUnixTimeInMsToDateString } from "@/utils/DataFormatUtils";
 
 describe("Component tests for the Status History", function (): void {
   const expectedStatus = ["Open", "Answered", "Resolved", "Closed"];
-  const expectedDates = [
-    "Sun, 28 Apr 2024, 16:37",
-    "Mon, 29 Apr 2024, 20:24",
-    "Thu, 2 May 2024, 03:57",
-    "Thu, 2 May 2024, 15:04",
-  ];
   const dummyStatusHistory = [
     {
       status: RequestStatus.Open,
@@ -45,7 +40,7 @@ describe("Component tests for the Status History", function (): void {
     cy.get('[data-test="creation_timestamp"]').should("be.visible");
     cy.get('[data-test="creation_timestamp"]').should("have.length", 4);
     cy.get('[data-test="creation_timestamp"]').each(($el, index) => {
-      expect($el.text()).to.eq(expectedDates[index]);
+      expect($el.text()).to.eq(convertUnixTimeInMsToDateString(dummyStatusHistory[index].creationTimestamp));
     });
 
     cy.get('[data-test="request_status"]').should("be.visible");
