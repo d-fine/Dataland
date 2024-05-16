fdd5d4ce91a1c02217c90213145362b09994ba55
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-05-16 16:00:28 +0200

    testing a rethrow of an exception

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/validator/DocumentExists.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/validator/DocumentExists.kt
@@ -6,19 +6,23 @@ import jakarta.validation.ConstraintValidatorContext
 import jakarta.validation.Payload
 import org.dataland.datalandbackend.interfaces.documents.BaseDocumentReference
 import org.dataland.datalandbackend.interfaces.documents.ExtendedDocumentReference
-
 import org.dataland.datalandbackendutils.exceptions.ResourceNotFoundApiException
 import org.dataland.documentmanager.openApiClient.api.DocumentControllerApi
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import kotlin.reflect.KClass
 
 /**
- * Annotation for the validation of an ExtendedDataPoint<*> holding a number todo documentation
+ * Annotation for the validation of Base- and ExtendedDataPoints holding an existing document
  */
 @Target(AnnotationTarget.FIELD)
-@Constraint(validatedBy = [BaseDocumentReferenceExistsValidator::class,
-    ExtendedDocumentReferenceExistsValidator::class])
+@Constraint(
+    validatedBy = [
+        BaseDocumentReferenceExistsValidator::class,
+        ExtendedDocumentReferenceExistsValidator::class,
+    ],
+)
 annotation class DocumentExists(
     val message: String = "Input validation failed: The document reference doesn't exist",
     val groups: Array<KClass<*>> = [],
@@ -26,39 +30,47 @@ annotation class DocumentExists(
 )
 
 /**
- * Class holding the validation logic for a class that implements BaseDataPoint<*> Interface featuring a number
- * todo documentation
+ * Class holding the validation logic for an BaseDocumentReference. It checks if the referenced document is valid
  */
 class BaseDocumentReferenceExistsValidator(
     @Qualifier("getDocumentControllerApi")
     @Autowired
     val documentControllerApi: DocumentControllerApi,
 ) : ConstraintValidator<DocumentExists, BaseDocumentReference> {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun isValid(documentReference: BaseDocumentReference?, context: ConstraintValidatorContext?): Boolean {
         if (documentReference == null) return true
-        var valid: Boolean
-        try {
-            documentControllerApi.checkDocument(documentReference.fileReference!!)
-            valid = true
-        } catch (exception: ResourceNotFoundApiException) {
-            valid = false
-        }
-        return valid
+        return callDocumentApiAndCheckFileReference(documentReference.fileReference!!, documentControllerApi, logger)
     }
 }
 
+/**
+ * Class holding the validation logic for an ExtendedDocumentReference. It checks if the referenced document is valid
+ */
 class ExtendedDocumentReferenceExistsValidator(
     @Qualifier("getDocumentControllerApi")
     @Autowired
     val documentControllerApi: DocumentControllerApi,
 ) : ConstraintValidator<DocumentExists, ExtendedDocumentReference> {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun isValid(documentReference: ExtendedDocumentReference?, context: ConstraintValidatorContext?): Boolean {
         if (documentReference == null) return true
-        try {
-            documentControllerApi.checkDocument(documentReference.fileReference!!)
-            return true
-        } catch (exception: ResourceNotFoundApiException) {
-            return false
-        }
+        return callDocumentApiAndCheckFileReference(documentReference.fileReference!!, documentControllerApi, logger)
     }
+}
+
+private fun callDocumentApiAndCheckFileReference(
+    fileReference: String,
+    documentControllerApi: DocumentControllerApi,
+    logger: org.slf4j.Logger,
+): Boolean {
+    try {
+        documentControllerApi.checkDocument(fileReference)
+    } catch (exception: ResourceNotFoundApiException) {
+        logger.info("The referenced document doesn't have a valid reference or doesn't exist at all.")
+        return false
+    }
+    return true
 }
