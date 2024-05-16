29beb06f0277215df7679cc15aac326b60d5aae0
AdrianGardijan <adrian.gardijan@d-fine.com>
2024-05-16 17:25:29 +0200

    tried adding content length to response header

--- dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/controller/DocumentController.kt
+++ dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/controller/DocumentController.kt
@@ -34,11 +34,16 @@ class DocumentController(
 
     override fun getDocument(documentId: String): ResponseEntity<InputStreamResource> {
         val document = documentManager.retrieveDocumentById(documentId)
+        val contentLength = document.content.contentLength()
         return ResponseEntity.ok()
             .contentType(document.type.mediaType)
             .header(
                 HttpHeaders.CONTENT_DISPOSITION,
                 "attachment; filename=${document.documentId}.${document.type.fileExtension}",
+            )
+            .header(
+                HttpHeaders.CONTENT_LENGTH,
+                contentLength.toString(),
             )
             .body(document.content)
     }
