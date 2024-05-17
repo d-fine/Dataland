96d3fa12828eab39b126726493d65b51b3d1e744
MichBlank <michael.blank@d-fine.com>
2024-05-17 17:14:43 +0200

    begin to brainstorm/write a test for the validator

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/DataControllerTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/DataControllerTest.kt
@@ -1,14 +1,25 @@
 package org.dataland.e2etests.tests
 
 import org.dataland.datalandbackend.openApiClient.infrastructure.ClientException
+import org.dataland.datalandbackend.openApiClient.model.BaseDataPointYesNo
+import org.dataland.datalandbackend.openApiClient.model.BaseDocumentReference
 import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataEutaxonomyNonFinancialsData
+import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataLksgData
+import org.dataland.datalandbackend.openApiClient.model.LksgAttachment
+import org.dataland.datalandbackend.openApiClient.model.LksgAttachmentAttachment
+import org.dataland.datalandbackend.openApiClient.model.LksgData
+import org.dataland.datalandbackend.openApiClient.model.LksgGeneral
+import org.dataland.datalandbackend.openApiClient.model.LksgGeneralMasterData
+import org.dataland.datalandbackend.openApiClient.model.YesNo
 import org.dataland.e2etests.auth.TechnicalUser
 import org.dataland.e2etests.utils.ApiAccessor
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.lang.IllegalArgumentException
+import java.time.LocalDate
 
 class DataControllerTest {
 
@@ -24,6 +35,71 @@ class DataControllerTest {
         testCompanyInformation.copy(isTeaserCompany = false)
     private val testCompanyInformationTeaser =
         testCompanyInformation.copy(isTeaserCompany = true)
+
+    @Test
+    fun `post a dummy company with lksg data and check if invalid document references are allowed`() {
+        val companyInfo = apiAccessor.uploadOneCompanyWithRandomIdentifier()
+
+        val correctFileReference = "50a36c418baffd520bb92d84664f06f9732a21f4e2e5ecee6d9136f16e7e0b63"
+        val wrongFileReference = "abcd"
+
+        val minimalLksgData = LksgData(
+            general = LksgGeneral(
+                masterData =
+                LksgGeneralMasterData(dataDate = LocalDate.of(2022, 5, 20)),
+            ),
+            attachment = LksgAttachment(
+                attachment = LksgAttachmentAttachment(
+                    attachment = BaseDataPointYesNo(
+                        value = YesNo.Yes,
+                        dataSource = BaseDocumentReference(
+                            fileReference = correctFileReference,
+                            fileName = "Test name",
+                        ),
+                    ),
+                ),
+            ),
+        )
+
+        val minimalLksgDataFail = LksgData(
+            general = LksgGeneral(
+                masterData =
+                LksgGeneralMasterData(dataDate = LocalDate.of(2022, 5, 20)),
+            ),
+            attachment = LksgAttachment(
+                attachment = LksgAttachmentAttachment(
+                    attachment = BaseDataPointYesNo(
+                        value = YesNo.Yes,
+                        dataSource = BaseDocumentReference(
+                            fileReference = wrongFileReference,
+                            fileName = "Test name",
+                        ),
+                    ),
+                ),
+            ),
+        )
+
+        var companyAssociatedLksgData = CompanyAssociatedDataLksgData(
+            companyInfo.actualStoredCompany.companyId,
+            "2018",
+            minimalLksgData,
+        )
+        var companyAssociatedLksgDataFail = CompanyAssociatedDataLksgData(
+            companyInfo.actualStoredCompany.companyId,
+            "2019",
+            minimalLksgDataFail,
+        )
+
+        val exception =
+            assertThrows<ClientException> {
+                apiAccessor.dataControllerApiForLksgData.postCompanyAssociatedLksgData(companyAssociatedLksgDataFail)
+            }
+        assertEquals("Client error : 400 ", exception.message)
+
+        assertDoesNotThrow {
+            apiAccessor.dataControllerApiForLksgData.postCompanyAssociatedLksgData(companyAssociatedLksgData)
+        }
+    }
 
     @Test
     fun `post a dummy company and a data set for it and check if that dummy data set can be retrieved`() {
