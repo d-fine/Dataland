e94a7babe56f9c61898646dcc2be213ede05ce4e
brunoneumannsaavedra <165763416+brunoneumannsaavedra@users.noreply.github.com>
2024-05-22 11:05:31 +0200

    Revised Sfdr e2e-test

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/frameworks/Sfdr.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/frameworks/Sfdr.kt
@@ -1,16 +1,27 @@
 package org.dataland.e2etests.tests.frameworks
 
 import org.dataland.e2etests.utils.ApiAccessor
+import org.dataland.e2etests.utils.DocumentManagerAccessor
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class Sfdr {
 
     private val apiAccessor = ApiAccessor()
+    private val documentManagerAccessor = DocumentManagerAccessor()
 
     private val listOfOneSfdrDataSet = apiAccessor.testDataProviderForSfdrData.getTData(1)
     private val listOfOneCompanyInformation = apiAccessor.testDataProviderForSfdrData
         .getCompanyInformationWithoutIdentifiers(1)
+
+    @BeforeAll
+    fun postTestDocuments() {
+        println("++-+-+-+-+ posting test documents") // TODO remove later
+        documentManagerAccessor.uploadAllTestDocumentsAndAssurePersistence()
+    }
 
     @Test
     fun `post a company with Sfdr data and check if the data can be retrieved correctly`() {
