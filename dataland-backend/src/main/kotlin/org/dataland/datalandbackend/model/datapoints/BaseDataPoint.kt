6b25be8d8f643a7a9bc096cb6160e09fb11498f4
SiegfriedSobkowiak <siegfried.sobkowiak@d-fine.de>
2024-05-23 15:18:24 +0200

    change to unified validator on string basis

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/datapoints/BaseDataPoint.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/datapoints/BaseDataPoint.kt
@@ -2,7 +2,6 @@ package org.dataland.datalandbackend.model.datapoints
 
 import org.dataland.datalandbackend.interfaces.datapoints.BaseDataPoint
 import org.dataland.datalandbackend.model.documents.BaseDocumentReference
-import org.dataland.datalandbackend.validator.DocumentExists
 
 /**
  * --- API model ---
@@ -10,6 +9,5 @@ import org.dataland.datalandbackend.validator.DocumentExists
  */
 data class BaseDataPoint<T>(
     override val value: T?,
-    @field:DocumentExists
     override val dataSource: BaseDocumentReference? = null,
 ) : BaseDataPoint<T>
