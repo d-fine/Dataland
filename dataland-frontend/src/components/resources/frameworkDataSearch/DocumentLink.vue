475976c8cab89f5bf80477800e0ec68d9c373a43
swaldow <stephan.waldow@d-fine.de>
2024-05-27 11:09:47 +0200

    wip DocumentLink.vue

--- dataland-frontend/src/components/resources/frameworkDataSearch/DocumentLink.vue
+++ dataland-frontend/src/components/resources/frameworkDataSearch/DocumentLink.vue
@@ -40,7 +40,7 @@ export default defineComponent({
   data() {
     return {
       percentCompleted: undefined as number | undefined,
-        requestedDataId: String,
+      requestedDataId: String,
     };
   },
   //TODO implementation of provide and inject dataId is bad, needs refactoring. For some reason direct injection
@@ -71,26 +71,6 @@ export default defineComponent({
       this.percentCompleted = 0;
       try {
         const docUrl = document.createElement("a");
-        const documentControllerApi = new ApiClientProvider(assertDefined(this.getKeycloakPromise)()).apiClients
-          .documentController;
-        await documentControllerApi
-          .getDocument(fileReference, {
-            responseType: "arraybuffer",
-            onDownloadProgress: (progressEvent) => {
-              if (progressEvent.total != null)
-                this.percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-            },
-          } as AxiosRequestConfig)
-          .then((getDocumentsFromStorageResponse) => {
-            this.percentCompleted = 100;
-            const fileExtension = this.getFileExtensionFromHeaders(getDocumentsFromStorageResponse.headers);
-            const mimeType = this.getMimeTypeFromHeaders(getDocumentsFromStorageResponse.headers);
-            const newBlob = new Blob([getDocumentsFromStorageResponse.data], { type: mimeType });
-            docUrl.href = URL.createObjectURL(newBlob);
-            docUrl.setAttribute("download", `${this.downloadName}.${fileExtension}`);
-            document.body.appendChild(docUrl);
-            docUrl.click();
-          });
         const publicFramework = getAllPublicFrameworkIdentifiers();
         if (publicFramework.includes(this.datatype!)) {
           const documentControllerApi = new ApiClientProvider(assertDefined(this.getKeycloakPromise)()).apiClients
@@ -98,8 +78,13 @@ export default defineComponent({
           await documentControllerApi
             .getDocument(fileReference, {
               responseType: "arraybuffer",
+              onDownloadProgress: (progressEvent) => {
+                if (progressEvent.total != null)
+                  this.percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+              },
             } as AxiosRequestConfig)
             .then((getDocumentsFromStorageResponse) => {
+              this.percentCompleted = 100;
               const fileExtension = this.getFileExtensionFromHeaders(getDocumentsFromStorageResponse.headers);
               const mimeType = this.getMimeTypeFromHeaders(getDocumentsFromStorageResponse.headers);
               const newBlob = new Blob([getDocumentsFromStorageResponse.data], { type: mimeType });
@@ -109,6 +94,7 @@ export default defineComponent({
               docUrl.click();
             });
         } else {
+          //TODO Check if the percentage is correctly displayed in the frontend for both kind of frameworks
           const apiClientProvider = new ApiClientProvider(assertDefined(this.getKeycloakPromise)());
           let SmeDataControllerApi: PrivateFrameworkDataApi<SmeData>;
           const frameworkDefinition = getBasePrivateFrameworkDefinition(DataTypeEnum.Sme);
@@ -119,7 +105,12 @@ export default defineComponent({
             );
             await SmeDataControllerApi.getPrivateDocument(this.requestedDataId.toString(), fileReference, {
               responseType: "arraybuffer",
+              onDownloadProgress: (progressEvent) => {
+                if (progressEvent.total != null)
+                  this.percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+              },
             } as AxiosRequestConfig).then((getDocumentsFromStorageResponse) => {
+              this.percentCompleted = 100;
               const fileExtension = this.getFileExtensionFromHeaders(getDocumentsFromStorageResponse.headers);
               const mimeType = this.getMimeTypeFromHeaders(getDocumentsFromStorageResponse.headers);
               const newBlob = new Blob([getDocumentsFromStorageResponse.data], { type: mimeType });
