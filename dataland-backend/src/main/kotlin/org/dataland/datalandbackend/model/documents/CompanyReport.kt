a4dd6a4baf0617080922355578e26695b2a85f0d
brunoneumannsaavedra <165763416+brunoneumannsaavedra@users.noreply.github.com>
2024-05-27 11:09:06 +0200

    Added valid field to Company Report

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/documents/CompanyReport.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/documents/CompanyReport.kt
@@ -13,14 +13,14 @@ import java.time.LocalDate
  */
 data class CompanyReport(
     @field:JsonProperty(required = true)
+    @field:NotBlank
+    @field:DocumentExists
     override val fileReference: String,
 
     override val fileName: String? = null,
 
     // The following annotation is required due to a known issue with the openApi generator for fields starting with is
     @field:JsonProperty()
-    @field:NotBlank
-    @field:DocumentExists
     val isGroupLevel: YesNoNa? = null,
 
     val reportDate: LocalDate? = null,
