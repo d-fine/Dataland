b828eb197053a7be3c6b0e3f07a6ff6204527448
brunoneumannsaavedra <165763416+brunoneumannsaavedra@users.noreply.github.com>
2024-05-28 14:21:55 +0200

    Removed sh file

--- emptyCommitLoop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Loop to create 5 empty commits
-for ((i=1; i<=5; i++)); do
-    echo "Creating empty commit $i"
-    git commit --allow-empty -m "Empty commit $i"
-    git push origin instable-e2e-test  
-done
