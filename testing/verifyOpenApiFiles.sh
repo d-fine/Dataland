e8218387a1c512aea126efc5082b26c297f567c6
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2024-05-28 14:54:51 +0200

    Add debug output to verifyOpenApiFiles

--- testing/verifyOpenApiFiles.sh
+++ testing/verifyOpenApiFiles.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-function getOpenApiSha1Sum() {
-  find * -name "*OpenApi.json" -type f -exec bash -c 'jq -S . $1 > $1.formatted.json' shell {} \;
+function getFormattedOpenApiSha1Sum() {
   find * -name "*OpenApi.json.formatted.json" -type f | \
   sort -u | \
-  xargs sha1sum | \
+  xargs sha1sum
+}
+
+function getOpenApiSha1Sum() {
+  find * -name "*OpenApi.json" -type f -exec bash -c 'jq -S . $1 > $1.formatted.json' shell {} \;
+
+  getFormattedOpenApiSha1Sum | \
   awk '{print $1}' | \
   sha1sum | \
   awk '{print $1}'
 }
 
 sha1SumBeforeRegenerate=$(getOpenApiSha1Sum)
+echo "sha1sum before regenerate: $sha1SumBeforeRegenerate"
+echo "detailed sha1sum before:"
+getFormattedOpenApiSha1Sum
+
 ./gradlew generateOpenApiDocs --no-daemon --stacktrace
 sha1SumAfterRegenerate=$(getOpenApiSha1Sum)
 echo "sha1sum before regenerate: $sha1SumBeforeRegenerate"
+echo "detailed sha1sum before:"
+getFormattedOpenApiSha1Sum
+
 echo "sha1sum after regenerate: $sha1SumAfterRegenerate"
+echo "detailed sha1sum after:"
+getFormattedOpenApiSha1Sum
 
 if [[ "$sha1SumBeforeRegenerate" == "$sha1SumAfterRegenerate" ]]; then
   echo "OpenApi Files OK!"
