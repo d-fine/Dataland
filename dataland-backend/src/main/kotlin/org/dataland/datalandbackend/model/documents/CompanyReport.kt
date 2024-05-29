f37da6932e49fbbb4c7408f088673a63955eea56
brunoneumannsaavedra <165763416+brunoneumannsaavedra@users.noreply.github.com>
2024-05-29 14:52:57 +0200

    Removed todos

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/documents/CompanyReport.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/documents/CompanyReport.kt
@@ -1,8 +1,10 @@
 package org.dataland.datalandbackend.model.documents
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.NotBlank
 import org.dataland.datalandbackend.interfaces.documents.BaseDocumentReference
 import org.dataland.datalandbackend.model.enums.commons.YesNoNa
+import org.dataland.datalandbackend.validator.DocumentExists
 import java.time.LocalDate
 
 /**
@@ -11,8 +13,8 @@ import java.time.LocalDate
  */
 data class CompanyReport(
     @field:JsonProperty(required = true)
-    // todo check if annotation leads to problems with OpenApi @field:NotBlank
-    // todo check if annotation leads to problems with OpenApi @field:DocumentExists
+    @field:NotBlank
+    @field:DocumentExists
     override val fileReference: String,
 
     override val fileName: String? = null,
