f59c5eafcaf0d8a0794cc023740feeeec2aebf2c
swaldow <stephan.waldow@d-fine.de>
2024-05-29 10:56:08 +0200

    addresses todos in DocumentLink.vue

--- dataland-frontend/src/components/resources/frameworkDataSearch/DocumentLink.vue
+++ dataland-frontend/src/components/resources/frameworkDataSearch/DocumentLink.vue
@@ -60,66 +60,79 @@ export default defineComponent({
      * Method to download available reports
      */
     async downloadDocument() {
-      // TODO Emanuel: so eine lange FUnktion sollten wir vermeiden
       const fileReference: string = this.fileReference;
       this.percentCompleted = 0;
       try {
         const docUrl = document.createElement("a");
         const privateFramework = getAllPrivateFrameworkIdentifiers();
         if (privateFramework.includes(this.dataType)) {
-          //TODO Check if the percentage is correctly displayed in the frontend for both kind of frameworks
-          const apiClientProvider = new ApiClientProvider(assertDefined(this.getKeycloakPromise)());
-          let SmeDataControllerApi: PrivateFrameworkDataApi<SmeData>;
-          const frameworkDefinition = getBasePrivateFrameworkDefinition(DataTypeEnum.Sme);
-          if (frameworkDefinition) {
-            SmeDataControllerApi = frameworkDefinition.getPrivateFrameworkApiClient(
-              undefined,
-              apiClientProvider.axiosInstance,
-            );
-            await SmeDataControllerApi.getPrivateDocument(this.dataId.toString(), fileReference, {
-              // TODO AChtung, undefined case behandeln
-              responseType: "arraybuffer",
-              onDownloadProgress: (progressEvent) => {
-                if (progressEvent.total != null)
-                  this.percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total); // TODO funktioniert percentage noch?
-              },
-            } as AxiosRequestConfig).then((getDocumentsFromStorageResponse) => {
-              this.percentCompleted = 100;
-              const fileExtension = this.getFileExtensionFromHeaders(getDocumentsFromStorageResponse.headers);
-              const mimeType = this.getMimeTypeFromHeaders(getDocumentsFromStorageResponse.headers);
-              const newBlob = new Blob([getDocumentsFromStorageResponse.data], { type: mimeType });
-              docUrl.href = URL.createObjectURL(newBlob);
-              docUrl.setAttribute("download", `${this.downloadName}.${fileExtension}`);
-              document.body.appendChild(docUrl);
-              docUrl.click();
-            });
-          }
+          await this.handlePrivateDocumentDownload(fileReference, docUrl);
         } else {
-          const documentControllerApi = new ApiClientProvider(assertDefined(this.getKeycloakPromise)()).apiClients
-            .documentController;
-          await documentControllerApi
-            .getDocument(fileReference, {
-              responseType: "arraybuffer",
-              onDownloadProgress: (progressEvent) => {
-                if (progressEvent.total != null)
-                  this.percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-              },
-            } as AxiosRequestConfig)
-            .then((getDocumentsFromStorageResponse) => {
-              this.percentCompleted = 100;
-              const fileExtension = this.getFileExtensionFromHeaders(getDocumentsFromStorageResponse.headers);
-              const mimeType = this.getMimeTypeFromHeaders(getDocumentsFromStorageResponse.headers);
-              const newBlob = new Blob([getDocumentsFromStorageResponse.data], { type: mimeType });
-              docUrl.href = URL.createObjectURL(newBlob);
-              docUrl.setAttribute("download", `${this.downloadName}.${fileExtension}`);
-              document.body.appendChild(docUrl);
-              docUrl.click();
-            });
+          await this.handlePublicDocumentDownload(fileReference, docUrl);
         }
       } catch (error) {
         console.error(error);
       }
       this.percentCompleted = undefined;
+    },
+    /**
+     * This method retrieves the documents for private data frameworks
+     * @param fileReference hash of the document to be retrieved
+     * @param docUrl initial reference of the document reference
+     */
+    async handlePrivateDocumentDownload(fileReference: string, docUrl: HTMLAnchorElement) {
+      const apiClientProvider = new ApiClientProvider(assertDefined(this.getKeycloakPromise)());
+      let SmeDataControllerApi: PrivateFrameworkDataApi<SmeData>;
+      const frameworkDefinition = getBasePrivateFrameworkDefinition(DataTypeEnum.Sme);
+      if (frameworkDefinition) {
+        SmeDataControllerApi = frameworkDefinition.getPrivateFrameworkApiClient(
+          undefined,
+          apiClientProvider.axiosInstance,
+        );
+        await SmeDataControllerApi.getPrivateDocument(this.dataId.toString(), fileReference, {
+          responseType: "arraybuffer",
+          onDownloadProgress: (progressEvent) => {
+            if (progressEvent.total != null)
+              this.percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+          },
+        } as AxiosRequestConfig).then((getDocumentsFromStorageResponse) => {
+          this.percentCompleted = 100;
+          const fileExtension = this.getFileExtensionFromHeaders(getDocumentsFromStorageResponse.headers);
+          const mimeType = this.getMimeTypeFromHeaders(getDocumentsFromStorageResponse.headers);
+          const newBlob = new Blob([getDocumentsFromStorageResponse.data], { type: mimeType });
+          docUrl.href = URL.createObjectURL(newBlob);
+          docUrl.setAttribute("download", `${this.downloadName}.${fileExtension}`);
+          document.body.appendChild(docUrl);
+          docUrl.click();
+        });
+      }
+    },
+    /**
+     * This method retrieves the documents for public data frameworks
+     * @param fileReference hash of the document to be retrieved
+     * @param docUrl initial reference of the document reference
+     */
+    async handlePublicDocumentDownload(fileReference: string, docUrl: HTMLAnchorElement) {
+      const documentControllerApi = new ApiClientProvider(assertDefined(this.getKeycloakPromise)()).apiClients
+        .documentController;
+      await documentControllerApi
+        .getDocument(fileReference, {
+          responseType: "arraybuffer",
+          onDownloadProgress: (progressEvent) => {
+            if (progressEvent.total != null)
+              this.percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+          },
+        } as AxiosRequestConfig)
+        .then((getDocumentsFromStorageResponse) => {
+          this.percentCompleted = 100;
+          const fileExtension = this.getFileExtensionFromHeaders(getDocumentsFromStorageResponse.headers);
+          const mimeType = this.getMimeTypeFromHeaders(getDocumentsFromStorageResponse.headers);
+          const newBlob = new Blob([getDocumentsFromStorageResponse.data], { type: mimeType });
+          docUrl.href = URL.createObjectURL(newBlob);
+          docUrl.setAttribute("download", `${this.downloadName}.${fileExtension}`);
+          document.body.appendChild(docUrl);
+          docUrl.click();
+        });
     },
     /**
      * Extracts the file extension from the http response headers
