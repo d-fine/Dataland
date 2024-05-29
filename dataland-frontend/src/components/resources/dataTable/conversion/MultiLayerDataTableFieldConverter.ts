e2ca3f5a6f1f2d56b3778192c2f41ff282c1408a
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-05-29 15:52:22 +0200

    TODO Anpassungen

--- dataland-frontend/src/components/resources/dataTable/conversion/MultiLayerDataTableFieldConverter.ts
+++ dataland-frontend/src/components/resources/dataTable/conversion/MultiLayerDataTableFieldConverter.ts
@@ -41,7 +41,13 @@ const formFieldValueGetterFactoryMap: { [key: string]: ValueGetterFactory } = {
   IntegerExtendedDataPointFormField: numberDataPointValueGetterFactory,
   BigDecimalExtendedDataPointFormField: numberDataPointValueGetterFactory,
   CurrencyDataPointFormField: currencyDataPointValueGetterFactory,
-  // TODO Emanuel: no value getter for document link? Wie kann P2p sof unktionieren? (Livestock Farming)
+  /* TODO Emanuel: Am Montag hatten wir ja mal testen wollen, ob die DocumentLink Komponente noch für die public
+  frameworks funktioniert, wenn sie quasi "direkt" gemountet wird auf der view page.
+  Wir hatten dafür mal das "Tierfutter-Zertifikat" von P2P unter "Livestock Farming" rausgepickt, um einfach mal
+  draufzuklicken und zu versuchen, das PDF herunterzuladen. Das hatte nicht funktioniert am Montag!
+
+  Daher => Check am Ende, ob das Feld funktioniert, oder ob das ein bug ist!
+  */
 };
 
 /**
