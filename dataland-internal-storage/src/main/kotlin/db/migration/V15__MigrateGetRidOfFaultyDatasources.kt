89e4170c7af93344df58561312fef06810cd0bbe
MichBlank <michael.blank@d-fine.com>
2024-05-30 10:59:40 +0200

    Add migration script to fix file references

---
+++ dataland-internal-storage/src/main/kotlin/db/migration/V15__MigrateGetRidOfFaultyDatasources.kt
@@ -0,0 +1,181 @@
+package db.migration
+
+import db.migration.utils.DataTableEntity
+import db.migration.utils.getOrJavaNull
+import db.migration.utils.migrateCompanyAssociatedDataOfDatatype
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+import org.json.JSONObject
+import org.slf4j.LoggerFactory
+import java.io.IOException
+
+/**
+ * This migration script removes all file references that are not hashes and all
+ * file references that are hashes but do not exist.
+ */
+class V15__MigrateGetRidOfFaultyDatasources : BaseJavaMigration() {
+
+    private val regex = "^[a-fA-F0-9]{64}$".toRegex()
+
+    private val dataTypesToMigrate = listOf(
+        "eutaxonomy-non-financials",
+        "eutaxonomy-financials",
+        "sfdr",
+        "lksg",
+        "esg-questionnaire",
+        "heimathafen",
+        "sme",
+
+    )
+
+    private val logger = LoggerFactory.getLogger("Migration V15")
+
+    // Array containing all valid file references
+    var fileReferencesExisting: ArrayList<String> = ArrayList()
+
+    /**
+     * Get all valid fileReferences from database and add them to fileReferencesExisting
+     *
+     * blob_id in the blob_items table is the same as the fileReference, the actual hash value of the document.
+     * Entries in blob_items are assumed to be consistent with the document_meta_info table of the DocumentManagerDB.
+     */
+    fun getExistingFileReferences(context: Context?) {
+        val statement = context!!.connection.createStatement()
+
+        if (statement.execute("SELECT blob_id FROM blob_items")) {
+            val result = statement.resultSet
+
+            while (result.next()) {
+                var temp: String = result.getString("blob_id")
+                fileReferencesExisting.add(temp)
+                logger.info("Adding to known documents list: " + temp)
+            }
+        } else {
+            throw IOException("Error while accessing blob_items table in database")
+        }
+    }
+
+    override fun migrate(context: Context?) {
+        // get up to date list of file references
+        getExistingFileReferences(context)
+        // now check datasets
+        dataTypesToMigrate.forEach {
+            migrateCompanyAssociatedDataOfDatatype(
+                context,
+                it,
+                this::migrateFaultyFileReferences,
+            )
+        }
+    }
+
+    private fun isFaultyFileReference(fileReference: String): Boolean {
+        return (fileReference !in fileReferencesExisting) || (!isSha256(fileReference))
+    }
+
+    /**
+     * Remove company reports with invalid fileReference from map of company reports.
+     * If no company reports are left, replace referencedReports with null.
+     */
+    private fun replaceFaultyFileReferenceReferencedReports(
+        companyReportMap: JSONObject,
+        obj: JSONObject,
+        targetObjectName: String,
+    ) {
+        val keysToBeRemoved: ArrayList<String> = ArrayList()
+
+        // Find the keys of all reports with invalid fileReference
+        val keys: Iterator<String> = companyReportMap.keys()
+        while (keys.hasNext()) {
+            val companyReportKey = keys.next()
+            val companyReport = companyReportMap.getOrJavaNull(companyReportKey) as JSONObject?
+            if (companyReport !== null) {
+                val fileReference = companyReport.get("fileReference") as String
+                if (isFaultyFileReference(fileReference)) {
+                    logger.info(
+                        "Remove reference to document from CompanyReport Map." +
+                            " Broken file reference: " + fileReference,
+                    )
+                    keysToBeRemoved.add(companyReportKey)
+                }
+            }
+        }
+        for (key in keysToBeRemoved) {
+            companyReportMap.remove(key)
+        }
+        // If no report left, replace empty map with null
+        if (companyReportMap.isEmpty) {
+            obj.put(targetObjectName, null as Any?)
+        }
+    }
+
+    /**
+     * Replace dataSource with null if fileReference is invalid
+     */
+    private fun replaceFaultyFileReferenceDataSource(
+        dataSource: JSONObject,
+        obj: JSONObject,
+        targetObjectName: String,
+    ) {
+        val fileReference = dataSource.get("fileReference") as String
+        if (isFaultyFileReference(fileReference)) {
+            logger.info("Replace reference to document with null. Broken file reference: " + fileReference)
+            obj.put(targetObjectName, null as Any?)
+        }
+    }
+
+    /**
+     * Recursively iterate over the elements of a dataset and find dataSources or referencedReports
+     */
+    private fun checkForFaultyFileReferenceAndIterateFurther(
+        dataset: JSONObject,
+        objectName: String,
+        targetObjectName: String,
+        fileReferenceCheckAndReplacementHandler: (JSONObject, JSONObject, String) -> Unit,
+    ) {
+        val obj = dataset.getOrJavaNull(objectName)
+        if (obj !== null && obj is JSONObject) {
+            val dataSourceOrCompanyInfoList = obj.getOrJavaNull(targetObjectName) as JSONObject?
+            if (dataSourceOrCompanyInfoList !== null) {
+                fileReferenceCheckAndReplacementHandler(dataSourceOrCompanyInfoList, obj, targetObjectName)
+            } else {
+                obj.keys().forEach {
+                    checkForFaultyFileReferenceAndIterateFurther(
+                        obj, it, targetObjectName,
+                        fileReferenceCheckAndReplacementHandler,
+                    )
+                }
+            }
+        }
+    }
+
+    private fun isSha256(fileReference: String): Boolean {
+        return fileReference.matches(regex)
+    }
+
+    /**
+     * Migrate the data points
+     *
+     * There are three objects storing fileReferences: Base and ExtendedDocumentReference and CompanyReport.
+     * ExtendedDocumentReference and BaseDocumentReference are consistently referred to as "dataSource".
+     * CompanyReport only occurs list-like as Map<String, CompanyReport>? which is consistently referred to as
+     * "referencedReports".
+     * Both cases are handled separately.
+     *
+     */
+    fun migrateFaultyFileReferences(dataTableEntity: DataTableEntity) {
+        val dataset = dataTableEntity.dataJsonObject
+        dataset.keys().forEach {
+            checkForFaultyFileReferenceAndIterateFurther(
+                dataset, it, "dataSource",
+                ::replaceFaultyFileReferenceDataSource,
+            )
+        }
+        dataset.keys().forEach {
+            checkForFaultyFileReferenceAndIterateFurther(
+                dataset, it, "referencedReports",
+                ::replaceFaultyFileReferenceReferencedReports,
+            )
+        }
+        dataTableEntity.companyAssociatedData.put("data", dataset.toString())
+    }
+}
---
+++ dataland-internal-storage/src/test/kotlin/db/migration/V15__MigrateGetRidOfFaultyDatasourcesTest.kt
@@ -0,0 +1,40 @@
+package db.migration
+
+import db.migration.utils.TestUtils
+import org.junit.jupiter.api.Test
+
+class V15__MigrateGetRidOfFaultyDatasourcesTest {
+    @Test
+    fun `check migration script for faulty file references in lksg works properly`() {
+        var instance = V15__MigrateGetRidOfFaultyDatasources()
+        instance.fileReferencesExisting.add("50a36c418baffd520bb92d84664f06f9732a21f4e2e5ecee6d9136f16e7e0b63")
+        TestUtils().testMigrationOfSingleDataset(
+            "lksg",
+            "V15/originalLksgDatabaseEntry.json",
+            "V15/expectedLksgDatabaseEntry.json",
+            instance::migrateFaultyFileReferences,
+        )
+    }
+
+    @Test
+    fun `check migration script for faulty file references in sfdr works properly`() {
+        TestUtils().testMigrationOfSingleDataset(
+            "sfdr",
+            "V15/originalSfdrDatabaseEntry.json",
+            "V15/expectedSfdrDatabaseEntry.json",
+            V15__MigrateGetRidOfFaultyDatasources()::migrateFaultyFileReferences,
+        )
+    }
+
+    @Test
+    fun `check migration script for faulty file references in sfdr with multiple CompanyReports works properly`() {
+        val instance = V15__MigrateGetRidOfFaultyDatasources()
+        instance.fileReferencesExisting.add("50a36c418baffd520bb92d84664f06f9732a21f4e2e5ecee6d9136f16e7e0b63")
+        TestUtils().testMigrationOfSingleDataset(
+            "sfdr",
+            "V15/originalSfdrDatabaseEntryMultipleCompanyReports.json",
+            "V15/expectedSfdrDatabaseEntryMultipleCompanyReports.json",
+            instance::migrateFaultyFileReferences,
+        )
+    }
+}
