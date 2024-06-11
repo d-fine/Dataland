4795399414b6c9ba7f2dcd5f1fdd3f3c14cf1642
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-06-11 17:25:15 +0200

    merge update

--- dataland-frontend/src/components/resources/dataTable/conversion/sme/SmeDisplayValueGetters.ts
+++ dataland-frontend/src/components/resources/dataTable/conversion/sme/SmeDisplayValueGetters.ts
@@ -18,11 +18,32 @@ export const smeModalColumnHeaders = {
     emissionInKilograms: "Amount of Emission in kg",
     releaseMedium: "Release Medium",
   },
+  wasteClassification: {
+    classificationOfWaste: "Classification of waste",
+    typeOfWaste: "Type of Waste",
+    totalAmountTons: "Total amout in tons",
+    wasteRecycleOrReuseTons: "Waste recycle or reuse tons",
+    wasteDisposalTons: "Waste disposal in tons",
+    totalAmountCubicMeters: "Total amount in cubic meters",
+    wasteRecycleOrReuseInCubicMeters:"Waste recycle or reuse in m³",
+    wasteDisposalCubicMeters: "Waste disposal in m³"
+  }
 };
 interface SmePollutionEmissionDisplayFormat {
   pollutionType: string;
   emissionInKilograms: number;
   releaseMedium: ReleaseMedium;
+}
+
+interface SmeWasteClassificationDisplayFormat {
+  wasteClassification: string,
+  typeWaste: string,
+  totalAmountTons: number;
+  wasteRecycleOrReuseTons: number;
+  wasteDisposalTons: number;
+  totalAmountCubicMeters: number;
+  wasteRecycleOrReuseInCubicMeters: number;
+  wasteDisposalCubicMeters: number;
 }
 
 /**
@@ -97,7 +118,47 @@ export function formatSmePollutionEmissionForDisplay(
   };
 }
 /**
- * Convert an object of type SmePollutionEmission into a list that can be displayed using the standard
+ * Generates a display modal component for all waste classification components
+ * @param input list of sme waste classifications
+ * @param fieldLabel Field label for the corresponding object
+ * @returns ModalLinkDisplayComponent to the modal (if any data is present).
+ */
+export function formatSmeWasteClassificationObjectForDisplay(
+    input: SmeWasteClassificationObject[] | null | undefined,
+    fieldLabel: string,
+): AvailableMLDTDisplayObjectTypes {
+  let convertedValueForModal = null;
+  if (!input) {
+    return MLDTDisplayObjectForEmptyString;
+  } else {
+    convertedValueForModal = convertSmeWasteClassificationToListForModal(input);
+  }
+
+  return <MLDTDisplayObject<MLDTDisplayComponentName.ModalLinkDisplayComponent>>{
+    displayComponentName: MLDTDisplayComponentName.ModalLinkDisplayComponent,
+    displayValue: {
+      label: `Show ${fieldLabel}`,
+      modalComponent: DetailsCompanyDataTable,
+      modalOptions: {
+        props: {
+          header: fieldLabel,
+          modal: true,
+          dismissableMask: true,
+        },
+        data: {
+          listOfRowContents: convertedValueForModal,
+          kpiKeyOfTable: "wasteClassification",
+          columnHeaders: smeModalColumnHeaders,
+        },
+      },
+    },
+  };
+}
+
+
+
+/**
+ * Convert an object of type LksgSubcontractingCompanies into a list that can be displayed using the standard
  * modal DataTable
  * @param datasetValue the value of the dataset
  * @returns the converted list
@@ -108,6 +169,29 @@ function convertSmePollutionEmissionToListForModal(datasetValue: SmePollutionEmi
       pollutionType: humanizeStringOrNumber(item.pollutionType),
       emissionInKilograms: item.emissionInKilograms!,
       releaseMedium: item.releaseMedium!,
+      //TODO come back here and see if ! are really necessary
+    };
+    return humanizedItem;
+  });
+}
+
+/**
+ * Convert an object of type SmeWasteClassificationObject into a list that can be displayed using the standard
+ * modal DataTable
+ * @param datasetValue the value of the dataset
+ * @returns the converted list
+ */
+function convertSmeWasteClassificationToListForModal(datasetValue: SmeWasteClassificationObject[]): SmeWasteClassificationObject[] {
+  return datasetValue.map((item) => {
+    const humanizedItem: SmeWasteClassificationDisplayFormat = {
+      wasteClassification: humanizeStringOrNumber(item.wasteClassification),
+      typeWaste: humanizeStringOrNumber(item.typeWaste),
+      totalAmountTons: item.totalAmountTons!,
+      wasteRecycleOrReuseTons: item.wasteRecycleOrReuseTons!,
+      wasteDisposalTons: item.wasteDisposalTons!,
+      totalAmountCubicMeters: item.totalAmountCubicMeters!,
+      wasteRecycleOrReuseInCubicMeters: item.wasteRecycleOrReuseCubicMeters!,
+      wasteDisposalCubicMeters: item.wasteDisposalCubicMeters!
       //TODO come back here and see if ! are really necessary
     };
     return humanizedItem;
