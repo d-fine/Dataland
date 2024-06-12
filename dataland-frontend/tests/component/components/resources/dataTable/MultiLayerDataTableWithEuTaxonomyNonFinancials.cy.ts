498a75868ac3103760ea8b7d9cbe709b9b0e16e2
Aliabdoli77 <aliabdoli77@yahoo.com>
2024-06-12 10:56:27 +0200

    fix frontend test

--- dataland-frontend/tests/component/components/resources/dataTable/MultiLayerDataTableWithEuTaxonomyNonFinancials.cy.ts
+++ dataland-frontend/tests/component/components/resources/dataTable/MultiLayerDataTableWithEuTaxonomyNonFinancials.cy.ts
@@ -167,7 +167,7 @@ describe("Component test for the Eu-Taxonomy-Non-Financials view page", () => {
       cy.get(`[data-test="frameworkNewDataTableTitle"`).contains(
         `Data extracted from the company report. Company Reports(${expectedLatestReportingPeriod})`,
       );
-      cy.get('[data-test="documentLinkTest"]').contains(nameOfFirstReportOfExpectedLatestReportingPeriod);
+      cy.get('[data-test="referencedReportLink"]').contains(nameOfFirstReportOfExpectedLatestReportingPeriod);
 
       cy.get(`[data-test="previousReportsLinkToModal"]`).contains("Previous years reports").click();
 
@@ -179,7 +179,9 @@ describe("Component test for the Eu-Taxonomy-Non-Financials view page", () => {
           if (reportingPeriodOfDataset != expectedLatestReportingPeriod) {
             cy.get(`[data-test="previousReportsList"]`).contains(`Company Reports (${reportingPeriodOfDataset})`);
             for (const reportKey in reportsForDataset) {
+              cy.get(`[data-test='report-link-${reportKey}']`).first().click();
               cy.get(`[data-test='Report-Download-${reportKey}']`).contains(reportKey);
+              cy.get('button[data-pc-section="closebutton"]').last().click();
             }
           }
         }
