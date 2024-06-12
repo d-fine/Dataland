f0b30a92c2563a8d5dfe16539957dfd3fe1020a9
Johannes Haerkoetter <johannes.haerkoetter@d-fine.com>
2024-06-12 14:10:30 +0200

    tooltip

--- dataland-frontend/src/components/resources/dataTable/MultiLayerDataTable.vue
+++ dataland-frontend/src/components/resources/dataTable/MultiLayerDataTable.vue
@@ -16,7 +16,21 @@
               :data-dataset-index="idx"
             >
               <div class="p-column-header-content">
-                <span class="p-column-title">{{ singleDataAndMetaInfo.metaInfo.reportingPeriod }}</span>
+                <span class="p-column-title" style="display: flex; align-items: center"
+                  >{{ singleDataAndMetaInfo.metaInfo.reportingPeriod }}
+                  <i
+                    class="material-icons pl-2"
+                    aria-hidden="true"
+                    style="cursor: pointer"
+                    :title="singleDataAndMetaInfo.metaInfo.reportingPeriod"
+                    data-pd-tooltip="true"
+                    v-tooltip.top="{
+                      value: reportingYearToolTip(singleDataAndMetaInfo),
+                      class: 'd-tooltip',
+                    }"
+                    >info</i
+                  ></span
+                >
               </div>
             </th>
           </tr>
@@ -42,15 +56,83 @@
 }
 </style>
 
-<script setup lang="ts" generic="T">
+<script lang="ts" generic="T">
+import { defineComponent } from "vue";
 import { type MLDTConfig } from "@/components/resources/dataTable/MultiLayerDataTableConfiguration";
 import MultiLayerDataTableBody from "@/components/resources/dataTable/MultiLayerDataTableBody.vue";
 import { type DataAndMetaInformation } from "@/api-models/DataAndMetaInformation";
+import Tooltip from "primevue/tooltip";
+import { convertUnixTimeInMsToDateString } from "@/utils/DataFormatUtils";
+import {
+  type CompanyReport,
+  type EuTaxonomyDataForFinancials,
+  type EutaxonomyNonFinancialsData,
+  type SfdrData,
+} from "@clients/backend";
 
-defineProps<{
-  config: MLDTConfig<T>;
-  dataAndMetaInfo: Array<DataAndMetaInformation<T>>;
-  ariaLabel?: string;
-  inReviewMode: boolean;
-}>();
+export default defineComponent({
+  components: {
+    MultiLayerDataTableBody,
+  },
+  directives: {
+    tooltip: Tooltip,
+  },
+  methods: {
+    /**
+     * Generates the toolTip for reportingYear given DataAndMetaInformation.
+     * @param singleDataAndMetaInfo DataAndMetaInformation of a framework.
+     * @returns string the toolTip
+     */
+    reportingYearToolTip(singleDataAndMetaInfo: DataAndMetaInformation<T>): string {
+      let latestDate = null;
+      let referencedReports = null;
+      switch (singleDataAndMetaInfo.metaInfo.dataType) {
+        case "sfdr":
+          referencedReports = (singleDataAndMetaInfo.data as SfdrData).general?.general.referencedReports;
+          break;
+        case "eutaxonomy-financials":
+          referencedReports = (singleDataAndMetaInfo.data as EuTaxonomyDataForFinancials).referencedReports;
+          break;
+        case "eutaxonomy-non-financials":
+          referencedReports = (singleDataAndMetaInfo.data as EutaxonomyNonFinancialsData).general?.referencedReports;
+          break;
+        default:
+          referencedReports = null;
+          break;
+      }
+      if (referencedReports) {
+        for (const key in referencedReports) {
+          const companyReport: CompanyReport | undefined = referencedReports[key];
+          const reportDate = companyReport?.reportDate;
+          if (reportDate && (!latestDate || reportDate > latestDate)) {
+            latestDate = reportDate;
+          }
+        }
+      }
+      const mostRecentSourceToolTip = latestDate ? `Publication date of most recent source:\n ${latestDate}\n\n` : "";
+      const datasetPublishedToolTip =
+        "Dataset published on Dataland:\n " +
+        convertUnixTimeInMsToDateString(singleDataAndMetaInfo.metaInfo.uploadTime);
+      return mostRecentSourceToolTip + datasetPublishedToolTip;
+    },
+  },
+  props: {
+    config: {
+      type: Object as () => MLDTConfig<T>,
+      required: true,
+    },
+    dataAndMetaInfo: {
+      type: Array as () => Array<DataAndMetaInformation<T>>,
+      required: true,
+    },
+    ariaLabel: {
+      type: String,
+      required: false,
+    },
+    inReviewMode: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
 </script>
