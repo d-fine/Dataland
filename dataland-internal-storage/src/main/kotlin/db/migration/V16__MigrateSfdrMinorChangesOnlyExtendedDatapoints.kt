5a01d335de705778317da7f6537df1daf6c656b9
MichBlank <michael.blank@d-fine.com>
2024-06-12 09:13:33 +0200

    Add migration script

---
+++ dataland-internal-storage/src/main/kotlin/db/migration/V16__MigrateSfdrMinorChangesOnlyExtendedDatapoints.kt
@@ -0,0 +1,98 @@
+package db.migration
+
+import db.migration.utils.DataTableEntity
+import db.migration.utils.getOrJavaNull
+import db.migration.utils.migrateCompanyAssociatedDataOfDatatype
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+import org.json.JSONObject
+
+/**
+ * This migration script updates the existing SFDR datasets and migrates all existing BaseDataPoints to Extended ones
+ * Furthermore, some Points are moved from Wastes to Biodiversity
+ */
+class V16__MigrateSfdrMinorChangesOnlyExtendedDatapoints : BaseJavaMigration() {
+
+    private val listOfWasteToBiodiversity = listOf(
+        "manufactureOfAgrochemicalPesticidesProducts",
+        "landDegradationDesertificationSoilSealingExposure",
+        "sustainableAgriculturePolicy",
+        "sustainableOceansAndSeasPolicy",
+        "threatenedSpeciesExposure",
+        "biodiversityProtectionPolicy",
+        "deforestationPolicy",
+    )
+
+    /**
+     * Move the required fields from wastes to biodiversity
+     */
+    private fun migrateWasteToBiodiversity(dataset: JSONObject) {
+        val wastes = dataset.getOrJavaNull("wastes") as JSONObject? ?: return
+        val biodiversity = dataset.getOrJavaNull("biodiversity") as JSONObject? ?: JSONObject()
+
+        val keysToBeRemoved: MutableList<String> = mutableListOf()
+        wastes.keys().forEach { subFieldKey ->
+            if (subFieldKey in listOfWasteToBiodiversity) {
+                val subField = wastes.getOrJavaNull(subFieldKey)
+                biodiversity.put(subFieldKey, subField ?: JSONObject.NULL)
+                keysToBeRemoved.add(subFieldKey)
+            }
+        }
+        keysToBeRemoved.forEach { wastes.remove(it) }
+        // listOfWasteToBiodiversity.forEach {subFieldKey ->
+
+        // }
+        dataset.put("biodiversity", biodiversity)
+        if (wastes.isEmpty) dataset.remove("wastes")
+    }
+
+    /**
+     * Find the data points which are not extended and therefore do not have a mandatory
+     * "quality" key.
+     * If an object has "dataSource" or "value" keys without a "quality" key, they are considered BaseDataPoints
+     * and are updated with a "quality" value
+     */
+    private fun checkRecursivelyForBaseDataPoint(
+        dataset: JSONObject,
+        objectName: String,
+    ) {
+        val obj = dataset.getOrJavaNull(objectName)
+        if (obj !== null && obj is JSONObject) {
+            var hasDataSourceOrValue = false
+            var hasQuality = false
+            obj.keys().forEach { key ->
+                if (key == "dataSource") hasDataSourceOrValue = true
+                if (key == "value") hasDataSourceOrValue = true
+                if (key == "quality") hasQuality = true
+            }
+            if (hasDataSourceOrValue && !hasQuality) {
+                obj.put("quality", "NA")
+            }
+            obj.keys().forEach {
+                checkRecursivelyForBaseDataPoint(obj, it)
+            }
+        }
+    }
+
+    /**
+     * Move some fields and make all datapoints extended
+     */
+    fun migrateSfdrData(dataTableEntity: DataTableEntity) {
+        val dataset = dataTableEntity.dataJsonObject
+        val environmental = dataset.getOrJavaNull("environmental") as JSONObject?
+        if (environmental !== null) migrateWasteToBiodiversity(environmental)
+
+        dataset.keys().forEach {
+            checkRecursivelyForBaseDataPoint(dataset, it)
+        }
+        dataTableEntity.companyAssociatedData.put("data", dataset.toString())
+    }
+
+    override fun migrate(context: Context?) {
+        migrateCompanyAssociatedDataOfDatatype(
+            context,
+            "sfdr",
+            this::migrateSfdrData,
+        )
+    }
+}
---
+++ dataland-internal-storage/src/test/kotlin/db/migration/V16__MigrateSfdrMinorChangesOnlyExtendedDatapointsTest.kt
@@ -0,0 +1,36 @@
+package db.migration
+
+import db.migration.utils.TestUtils
+import org.junit.jupiter.api.Test
+
+class V16__MigrateSfdrMinorChangesOnlyExtendedDatapointsTest {
+    @Test
+    fun `check migration script for moving all expected elements from waste to biodiversity`() {
+        TestUtils().testMigrationOfSingleDataset(
+            "eutaxonomy-non-financials",
+            "V16/originalSfdrDatabaseEntry.json",
+            "V16/expectedSfdrDatabaseEntry.json",
+            V16__MigrateSfdrMinorChangesOnlyExtendedDatapoints()::migrateSfdrData,
+        )
+    }
+
+    @Test
+    fun `check migration script when wastes is not empty at the end`() {
+        TestUtils().testMigrationOfSingleDataset(
+            "eutaxonomy-non-financials",
+            "V16/originalSfdrDatabaseEntryNoEmptyWaste.json",
+            "V16/expectedSfdrDatabaseEntryNoEmptyWaste.json",
+            V16__MigrateSfdrMinorChangesOnlyExtendedDatapoints()::migrateSfdrData,
+        )
+    }
+
+    @Test
+    fun `check migration script when wastes is present`() {
+        TestUtils().testMigrationOfSingleDataset(
+            "eutaxonomy-non-financials",
+            "V16/originalSfdrDatabaseEntryNoWaste.json",
+            "V16/expectedSfdrDatabaseEntryNoWaste.json",
+            V16__MigrateSfdrMinorChangesOnlyExtendedDatapoints()::migrateSfdrData,
+        )
+    }
+}
