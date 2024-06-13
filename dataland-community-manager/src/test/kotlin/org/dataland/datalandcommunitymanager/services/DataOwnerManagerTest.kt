95d6c66de13834149d9591ebbaf5c7bc06d023d8
Johannes Haerkoetter <johannes.haerkoetter@d-fine.com>
2024-06-13 11:18:29 +0200

    adapt dataOwnerMAnagerTest

--- dataland-community-manager/src/test/kotlin/org/dataland/datalandcommunitymanager/services/DataOwnerManagerTest.kt
+++ dataland-community-manager/src/test/kotlin/org/dataland/datalandcommunitymanager/services/DataOwnerManagerTest.kt
@@ -1,23 +1,29 @@
 package org.dataland.datalandcommunitymanager.services
 
 import org.dataland.datalandbackend.openApiClient.api.CompanyDataControllerApi
+import org.dataland.datalandbackend.openApiClient.infrastructure.ClientException
+import org.dataland.datalandbackend.openApiClient.model.CompanyInformation
 import org.dataland.datalandbackend.openApiClient.model.StoredCompany
 import org.dataland.datalandbackend.services.messaging.DataOwnershipEmailMessageSender
 import org.dataland.datalandbackend.services.messaging.DataOwnershipSuccessfullyEmailMessageSender
 import org.dataland.datalandbackendutils.exceptions.InvalidInputApiException
 import org.dataland.datalandbackendutils.exceptions.ResourceNotFoundApiException
+import org.dataland.datalandcommunitymanager.entities.CompanyDataOwnersEntity
 import org.dataland.datalandcommunitymanager.repositories.DataOwnerRepository
+import org.dataland.datalandcommunitymanager.utils.CompanyIdValidator
 import org.dataland.keycloakAdapter.auth.DatalandRealmRole
 import org.dataland.keycloakAdapter.utils.AuthenticationMock
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mockito
-import org.mockito.Mockito.any
+import org.mockito.Mockito.doNothing
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verifyNoInteractions
 import org.mockito.Mockito.`when`
+import org.springframework.http.HttpStatus
 import java.util.*
 
 class DataOwnerManagerTest {
@@ -26,9 +32,17 @@ class DataOwnerManagerTest {
     lateinit var mockDataOwnersRepository: DataOwnerRepository
     lateinit var dataOwnershipSuccessfullyEmailMessageSender: DataOwnershipSuccessfullyEmailMessageSender
     lateinit var mockCompanyDataControllerApi: CompanyDataControllerApi
+    lateinit var mockCompanyIdValidator: CompanyIdValidator
 
     private val testUserId = UUID.randomUUID().toString()
+
     private val testCompanyName = "Test Company AG"
+    private val testCompanyInformation = CompanyInformation(
+        companyName = testCompanyName,
+        headquarters = "dummyHeadquarters",
+        identifiers = emptyMap(),
+        countryCode = "dummyCountryCode",
+    )
     private val mockAuthentication = AuthenticationMock.mockJwtAuthentication(
         "username",
         testUserId,
@@ -40,59 +54,65 @@ class DataOwnerManagerTest {
         mockDataOwnersRepository = mock(DataOwnerRepository::class.java)
         dataOwnershipSuccessfullyEmailMessageSender = mock(DataOwnershipSuccessfullyEmailMessageSender::class.java)
         mockCompanyDataControllerApi = mock(CompanyDataControllerApi::class.java)
+        mockCompanyIdValidator = CompanyIdValidator(mockCompanyDataControllerApi)
         dataOwnerManager = DataOwnerManager(
             mockCompanyDataControllerApi,
-                    mockDataOwnersRepository,
+            mockCompanyIdValidator,
+            mockDataOwnersRepository,
             mock(DataOwnershipEmailMessageSender::class.java),
             dataOwnershipSuccessfullyEmailMessageSender,
         )
 
-        Mockito.doNothing().`when`(dataOwnershipSuccessfullyEmailMessageSender)
+        doNothing().`when`(dataOwnershipSuccessfullyEmailMessageSender)
             .sendDataOwnershipAcceptanceExternalEmailMessage(
-                ArgumentMatchers.anyString(),
-                ArgumentMatchers.anyString(), ArgumentMatchers.anyString(), ArgumentMatchers.anyString(),
+                anyString(),
+                anyString(), anyString(), anyString(),
             )
     }
 
     @Test
     fun `check that a data ownership can only be requested for existing companies`() {
-        val         mockStoredCompany = mock(StoredCompany::class.java)
-
-        `when`(mockCompanyDataControllerApi.getCompanyById(any())).thenReturn(mockStoredCompany)
+        `when`(mockCompanyDataControllerApi.getCompanyById("non-existing-company-id")).thenThrow(
+            ClientException("Client error", HttpStatus.NOT_FOUND.value()),
+        )
         val exception = assertThrows<ResourceNotFoundApiException> {
             dataOwnerManager.checkCompanyForDataOwnership(
                 "non-existing-company-id",
             )
         }
-        assertTrue(exception.summary.contains("Company is invalid"))
+        assertTrue(exception.summary.contains("Company not found"))
     }
-/*
+
     @Test
     fun `check that a data ownership can only be requested if the user is not already a data owner`() {
-        val         mockStoredCompany = mock(StoredCompany::class.java)
-        `when`(mockStoredCompany.companyInformation.companyName).thenReturn(testCompanyName)
-        `when`(mockCompanyRepository.findById(any())).thenReturn(Optional.of(mockStoredCompany))
-        `when`(mockDataOwnersRepository.findById(any())).thenReturn(
+        val mockStoredCompany = mock(StoredCompany::class.java)
+        val existingCompanyId = "indeed-existing-company-id"
+        `when`(mockStoredCompany.companyInformation).thenReturn(testCompanyInformation)
+        `when`(mockCompanyDataControllerApi.getCompanyById(existingCompanyId)).thenReturn(mockStoredCompany)
+        `when`(mockDataOwnersRepository.findById(existingCompanyId)).thenReturn(
             Optional.of(
                 CompanyDataOwnersEntity(
-                    "indeed-existing-company-id",
+                    existingCompanyId,
                     mutableListOf(testUserId),
                 ),
             ),
         )
         val exception = assertThrows<InvalidInputApiException> {
             dataOwnerManager.sendDataOwnershipRequestIfNecessary(
-                "indeed-existing-company-id",
+                existingCompanyId,
                 mockAuthentication,
                 null,
                 "",
             )
         }
         assertTrue(exception.summary.contains("User is already a data owner for company."))
-    } TODO later*/
+    }
 
     @Test
     fun `check that email for users becoming company data owner is not generated if company does not exist`() {
+        `when`(mockCompanyDataControllerApi.getCompanyById(anyString())).thenThrow(
+            ClientException("Client error", HttpStatus.NOT_FOUND.value()),
+        )
         val exception = assertThrows<ResourceNotFoundApiException> {
             dataOwnerManager.addDataOwnerToCompany(
                 companyId = UUID.randomUUID().toString(),
@@ -100,14 +120,14 @@ class DataOwnerManagerTest {
                 companyName = testCompanyName,
             )
         }
-        assertTrue(exception.summary.contains("Company is invalid"))
+        verifyNoInteractions(dataOwnershipSuccessfullyEmailMessageSender)
+        assertTrue(exception.summary.contains("Company not found"))
     }
-/*
+
     @Test
     fun `check that email generated for users becoming company data owner are generated`() {
         val companyId = UUID.randomUUID().toString()
-
-        `when`(mockCompanyRepository.existsById(companyId)).thenReturn(true)
+        `when`(mockCompanyIdValidator.checkIfCompanyIdIsValid(companyId)).thenReturn(null)
         `when`(mockDataOwnersRepository.existsById(companyId)).thenReturn(false)
         `when`(
             dataOwnershipSuccessfullyEmailMessageSender.getNumberOfOpenDataRequestsForCompany(
@@ -128,12 +148,12 @@ class DataOwnerManagerTest {
 
         Mockito.verify(dataOwnershipSuccessfullyEmailMessageSender, Mockito.times(1))
             .sendDataOwnershipAcceptanceExternalEmailMessage(
-                ArgumentMatchers.anyString(),
-                ArgumentMatchers.anyString(),
-                ArgumentMatchers.anyString(),
-                ArgumentMatchers.anyString(),
+                anyString(),
+                anyString(),
+                anyString(),
+                anyString(),
             )
-    } TODO later */
+    }
 
     private fun <T> any(type: Class<T>): T = Mockito.any<T>(type)
 }
