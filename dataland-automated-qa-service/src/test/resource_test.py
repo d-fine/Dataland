90b8996f9d4e651b4afc1177194c28bedbf9e8ab
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-06-17 15:04:36 +0200

    fix wrong naming in python test

--- dataland-automated-qa-service/src/test/resource_test.py
+++ dataland-automated-qa-service/src/test/resource_test.py
@@ -75,7 +75,7 @@ class ResourceTest(unittest.TestCase):
         self.assertIsInstance(data_resource.data, SmeData)
         self.assertEqual(
         1,
-        data_resource.data.basic.energyAndGreenhousGasEmissions.energyFossilFuels
+        data_resource.data.basic.energy_and_greenhous_gas_emissions.energyFossilFuels
         )
 
     def test_document_is_fetched_correctly(self) -> None:
