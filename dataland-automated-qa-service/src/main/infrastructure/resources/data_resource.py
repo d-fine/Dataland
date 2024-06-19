a26c8c8f30e1266939f7e5262098058d445a94aa
swaldow <stephan.waldow@d-fine.de>
2024-06-19 12:25:27 +0200

    python again

--- dataland-automated-qa-service/src/main/infrastructure/resources/data_resource.py
+++ dataland-automated-qa-service/src/main/infrastructure/resources/data_resource.py
@@ -67,7 +67,7 @@ def _get_data(data_type: DataTypeEnum, data_id: str, client: AuthenticatedClient
         DataTypeEnum.LKSG: CompanyAssociatedDataLksgData,
         DataTypeEnum.SFDR: CompanyAssociatedDataSfdrData,
         DataTypeEnum.P2P: CompanyAssociatedDataPathwaysToParisData,
-        DataTypeEnum.SME: CompanyAssociatedDataSmeData,
+        DataTypeEnum.SME: CompanyAssociatedDataVsmeData,
         DataTypeEnum.ESG_QUESTIONNAIRE: CompanyAssociatedDataEsgQuestionnaireData,
         DataTypeEnum.HEIMATHAFEN: CompanyAssociatedDataHeimathafenData,
     }
