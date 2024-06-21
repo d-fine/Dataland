ea345149bf331bb6dc8ff20498ffd85c297755ed
Marcel Rasbach <marcel.rasbach@d-fine.com>
2024-06-21 14:49:58 +0200

    toolbox compilation fix

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/specific/uploadconfig/FrameworkUploadConfigBuilder.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/specific/uploadconfig/FrameworkUploadConfigBuilder.kt
@@ -74,8 +74,6 @@ class FrameworkUploadConfigBuilder(
 
         buildUploadConfig(uploadConfigTsPath)
 
-        into.gradleInterface.executeGradleTasks(listOf(":dataland-frontend:npm_run_typecheck"))
-
         EsLintRunner(into, listOf(uploadConfigTsPath)).run()
     }
 }
