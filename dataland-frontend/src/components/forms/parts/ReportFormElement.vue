8c3db17e5032694e20f8e6ac2fceba8f36de8d61
swaldow <stephan.waldow@d-fine.de>
2024-06-21 12:17:05 +0200

    removes quickfix

--- dataland-frontend/src/components/forms/parts/ReportFormElement.vue
+++ dataland-frontend/src/components/forms/parts/ReportFormElement.vue
@@ -20,7 +20,6 @@
     </div>
 
     <FormKit type="text" :modelValue="fileReference" name="fileReference" :outer-class="{ 'hidden-input': true }" />
-    <FormKit type="text" :modelValue="name" name="fileName" :outer-class="{ 'hidden-input': true }" />
 
     <!-- Currency used in the report -->
     <SingleSelectFormField
