747ab363697133b5ad4e710caf128a0b1611cd2d
Johannes Haerkoetter <johannes.haerkoetter@d-fine.com>
2024-06-25 10:34:13 +0200

    v18

---
+++ dataland-internal-storage/src/main/kotlin/db/migration/V18__CompleteListOfReferencedReports.kt
@@ -0,0 +1,9 @@
+package db.migration
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+class V18__CompleteListOfReferencedReports : BaseJavaMigration() {
+    override fun migrate(context: Context?) {
+    }
+}
