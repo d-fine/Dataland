c55be42d4f95b21391f1e1cd1453f0562ed674b8
Laurin Strelow <1918073+LaurinStrelow@users.noreply.github.com>
2024-06-27 15:46:45 +0200

    Revert "add validator class for SFDR data"
    This reverts commit defa4c26f0f7f159596301e3b030e328421e12e9.

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/validator/ReferencedReportsListIsComplete.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/validator/ReferencedReportsListIsComplete.kt
@@ -1,51 +1,14 @@
 package org.dataland.datalandbackend.validator
 
 import jakarta.validation.Constraint
-import jakarta.validation.ConstraintValidator
-import jakarta.validation.ConstraintValidatorContext
-import jakarta.validation.Payload
-import org.dataland.datalandbackend.frameworks.sfdr.model.SfdrData
-import org.dataland.datalandbackend.model.documents.CompanyReport
-import org.dataland.datalandbackend.model.documents.ExtendedDocumentReference
 import kotlin.reflect.KClass
+import jakarta.validation.Payload
 
 @Constraint(validatedBy = [SfdrDataValidator::class])
-@Target(AnnotationTarget.CLASS)
+@Target(AnnotationTarget.FIELD, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ReferencedReportsListIsComplete(
-    // todo rename Annotation (specific for Sfdr)
-    val message: String = "The list of referenced reports is not complete.",
+    val message: String = "There are company reports that are ",
     val groups: Array<KClass<*>> = [],
-    val payload: Array<KClass<out Payload>> = [],
+    val payload: Array<KClass<out Payload>> = []
 )
-
-class SfdrDataValidator : ConstraintValidator<ReferencedReportsListIsComplete, SfdrData> {
-    // todo add this annotation in the toolbox
-    override fun isValid(value: SfdrData?, context: ConstraintValidatorContext?): Boolean {
-        if (value == null) {
-            return false
-        }
-        val referencedReports = getAllReferencedReports(value)
-        val referencedReportsFileReference = getFileReferencesFromReports(referencedReports)
-        val extendedDocumentReferences = getAllExtendedDocumentsReference(value)
-        return extendedDocumentReferences.any {
-            !referencedReportsFileReference.contains(it.fileReference)
-            // todo discuss: check for more than just existence of file reference? maybe name (i.e. it.name == key of map)?
-        }
-    }
-    private fun getFileReferencesFromReports(map: Map<String, CompanyReport>?): List<String> {
-        val referencedReportsList = mutableListOf<String>()
-        for (entry in map?.entries!!) {
-            referencedReportsList.add(entry.value.fileReference)
-        }
-        return referencedReportsList
-    }
-
-    private fun getAllReferencedReports(value: SfdrData): Map<String, CompanyReport>? {
-        return value.general.general.referencedReports
-    }
-
-    private fun getAllExtendedDocumentsReference(sfdrData: SfdrData): List<ExtendedDocumentReference> {
-        // todo implement
-        return emptyList()
-    }
-}
