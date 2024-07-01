6312a70727d73f674c1ab2c63d55608eac680d43
Aliabdoli77 <aliabdoli77@yahoo.com>
2024-07-01 16:41:27 +0200

    adjustments to method

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/CompanyOwnershipChecker.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/CompanyOwnershipChecker.kt
@@ -82,7 +82,7 @@ class CompanyOwnershipChecker(
      * @param patch the fields to be patched
      * @return a Boolean indicating whether the patch complies with the access requirements
      */
-    fun areOnlyAuthorizedFieldsPatched(patch: CompanyInformationPatch): Boolean {
+    fun areOnlyAuthorizedFieldsPatched(patch: CompanyInformationPatch) {
         val unauthorizedFields = CompanyInformationPatch::class.memberProperties
             .filter { property ->
                 property.get(patch) != null &&
@@ -98,6 +98,5 @@ class CompanyOwnershipChecker(
                     " ${unauthorizedFields.joinToString(", ")}",
             )
         }
-        return true
     }
 }
