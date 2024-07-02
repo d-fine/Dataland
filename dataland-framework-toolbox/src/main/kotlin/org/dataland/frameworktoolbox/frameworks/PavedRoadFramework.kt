b5be9a91d18d62c780fed56c41fcdbb2b33a9090
Laurin Strelow <1918073+LaurinStrelow@users.noreply.github.com>
2024-07-02 08:28:55 +0200

    Refactor

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/PavedRoadFramework.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/PavedRoadFramework.kt
@@ -6,6 +6,7 @@ import org.dataland.frameworktoolbox.intermediate.components.ComponentBase
 import org.dataland.frameworktoolbox.intermediate.components.ReportPreuploadComponent
 import org.dataland.frameworktoolbox.specific.datamodel.Annotation
 import org.dataland.frameworktoolbox.specific.datamodel.FrameworkDataModelBuilder
+import org.dataland.frameworktoolbox.specific.datamodel.elements.ReferencedReportValidatorBuilder
 import org.dataland.frameworktoolbox.specific.fixturegenerator.FrameworkFixtureGeneratorBuilder
 import org.dataland.frameworktoolbox.specific.frameworkregistryimports.FrameworkRegistryImportsUpdater
 import org.dataland.frameworktoolbox.specific.uploadconfig.FrameworkUploadConfigBuilder
@@ -19,12 +20,10 @@ import org.dataland.frameworktoolbox.template.components.TemplateComponentFactor
 import org.dataland.frameworktoolbox.utils.DatalandRepository
 import org.dataland.frameworktoolbox.utils.LoggerDelegate
 import org.dataland.frameworktoolbox.utils.diagnostic.DiagnosticManager
-import org.dataland.frameworktoolbox.utils.freemarker.FreeMarker
 import org.springframework.beans.factory.getBean
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.io.File
-import java.io.FileWriter
 
 /**
  * A PavedRoadFramework is the simplest way to integrate a new framework into Dataland or
@@ -177,14 +176,24 @@ abstract class PavedRoadFramework(
         customizeDataModel(dataModel)
 
         val referencedReports = framework.root.nestedChildren.find { it is ReportPreuploadComponent }
-
         if (referencedReports != null) {
-            val pathToValidator = dataModel.rootPackageBuilder.fullyQualifiedName
-            generateReferencedReportsListValidatorIfNeeded(referencedReports, dataModel.rootDataModelClass.fullyQualifiedName, pathToValidator)
+
+            val referencedReportsPath = referencedReports.getTypescriptFieldAccessor()
+            val extendedDocumentFileReferences =
+                framework.root.nestedChildren.flatMap { it.getExtendedDocumentReference() }.toList()
+
+            val validatorPackage = dataModel.rootPackageBuilder.addPackage("validator")
+            val referencedReportValidatorBuilder = ReferencedReportValidatorBuilder(
+                validatorPackage,
+                framework.identifier,
+                dataModel.rootDataModelClass.name,
+                dataModel.rootDataModelClass.fullyQualifiedName,
+                referencedReportsPath,
+                extendedDocumentFileReferences)
+            validatorPackage.childElements.add(referencedReportValidatorBuilder)
+
             dataModel.rootDataModelClass.annotations.add(
-                Annotation(
-                    "${pathToValidator}.validator.ValidateReferencedReportsList",
-                ),
+                Annotation(referencedReportValidatorBuilder.fullyQualifiedName),
             )
         }
 
@@ -197,26 +206,7 @@ abstract class PavedRoadFramework(
 
     private fun generateReferencedReportsListValidatorIfNeeded(referencedReports: ComponentBase,
                                                                dataModel: String, validatorPath: String) {
-        val extendedDocumentFileReferences =
-            framework.root.nestedChildren.flatMap { it.getExtendedDocumentReference() }.toList()
-        val pathOfReferenceReports = referencedReports.getTypescriptFieldAccessor()
-        val freemarkerTemplate = FreeMarker.configuration.getTemplate(
-            "/specific/datamodel/elements/ValidateReferencedReportsList.kt.ftl",
-        )
-        val classPath = "./dataland-backend/src/main/kotlin/org/dataland/datalandbackend/frameworks/eutaxonomynonfinancials/model/validator/ReferencedReportsListValidator.kt"
-        val writer = FileWriter(classPath)
-        val capitalFrameworkName = framework.identifier.replaceFirstChar { it.uppercaseChar() }
-        freemarkerTemplate.process(
-            mapOf(
-                "package" to "${validatorPath}.validator",
-                "framework" to capitalFrameworkName,
-                "frameworkSpecificImport" to
-                    dataModel,
-                "referencedReportsMap" to pathOfReferenceReports,
-                "extendedDocumentsFileReferences" to extendedDocumentFileReferences.joinToString(),
-            ),
-            writer,
-        )
+
     }
 
     private fun compileViewModel(datalandProject: DatalandRepository) {
---
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/specific/datamodel/elements/ReferencedReportValidatorBuilder.kt
@@ -0,0 +1,41 @@
+package org.dataland.frameworktoolbox.specific.datamodel.elements
+
+import org.dataland.frameworktoolbox.utils.DatalandRepository
+import org.dataland.frameworktoolbox.utils.freemarker.FreeMarker
+import java.io.FileWriter
+import kotlin.io.path.div
+
+class ReferencedReportValidatorBuilder(
+    override val parentPackage: PackageBuilder,
+    val frameworkIdentifier: String,
+    val dataModelClassName: String,
+    val dataModelFullyQualifiedName: String,
+    val referencedReportsPath: String,
+    val extendedDocumentFileReferences: List<String>
+) : DataModelElement {
+
+    override val name: String = "ReferencedReportsListValidator"
+
+    val fullyQualifiedName: String
+        get() = parentPackage.fullyQualifiedName + "." + name
+
+    override fun build(into: DatalandRepository) {
+        val classPath = into.backendKotlinSrc / "${fullyQualifiedName.replace(".", "/")}.kt"
+
+        val freemarkerTemplate = FreeMarker.configuration.getTemplate(
+            "/specific/datamodel/elements/ValidateReferencedReportsList.kt.ftl",
+        )
+        val writer = FileWriter(classPath.toFile())
+        freemarkerTemplate.process(
+            mapOf(
+                "package" to parentPackage.fullyQualifiedName,
+                "framework" to frameworkIdentifier,
+                "dataModelFullyQualifiedName" to dataModelFullyQualifiedName,
+                "dataModelClassName" to dataModelClassName,
+                "referencedReportsPath" to referencedReportsPath,
+                "extendedDocumentsFileReferences" to extendedDocumentFileReferences,
+            ),
+            writer,
+        )
+    }
+}
