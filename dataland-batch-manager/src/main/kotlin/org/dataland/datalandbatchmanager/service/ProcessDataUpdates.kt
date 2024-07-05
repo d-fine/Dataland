23b4a05b0dd2e3a2996e7b240a3ac6bcb4472ed6
swaldow <stephan.waldow@d-fine.de>
2024-07-05 17:22:49 +0200

    Revert "test schedule"
    This reverts commit 6c61fb3a7886419d3ed2293443732be98d62b4c8.

--- dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
+++ dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
@@ -127,7 +127,7 @@ class ProcessDataUpdates(
     }
 
     @Suppress("UnusedPrivateMember") // Detect does not recognise the scheduled execution of this function
-    @Scheduled(cron = "0 0 16 1-7 1,4,7,10 FRI")
+    @Scheduled(cron = "0 0 5 1-7 1,4,7,10 SUN")
     // TODO discuss with someone if this cron job is correct
     private fun processNorthDataUpdates() {
         logger.info("Running scheduled update of NorthData data.")
