caf827aa00a6bfb304b20e84b49d60e248978885
MichBlank <michael.blank@d-fine.com>
2024-07-08 15:56:14 +0200

    Explicitly copy test data to container

--- dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/NorthdataDataIngestor.kt
+++ dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/NorthdataDataIngestor.kt
@@ -65,7 +65,7 @@ class NorthdataDataIngestor(
      */
     @Synchronized
     fun processNorthdataFile(downloadFile: (file: File) -> Unit) {
-        val zipFile = File(javaClass.getResource("/NorthdataTestData.zip")!!.path)
+        val zipFile = File("/NorthdataTestData.zip")
         val duration = measureTime {
             try {
                 downloadFile(zipFile)
