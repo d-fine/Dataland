e877eb967fe56e440f5b73a7921f6b6926c7db95
swaldow <stephan.waldow@d-fine.de>
2024-07-10 09:04:41 +0200

    Revert "tests schedule"
    This reverts commit 07b4f0bd55d5dab943fe182f84f66beb43c8b616.

--- dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
+++ dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
@@ -51,7 +51,8 @@ class ProcessDataUpdates(
      */
     @EventListener(ApplicationReadyEvent::class)
     fun processExternalCompanyDataIfEnabled() {
-        processFullGoldenCopyFileIfEnableprocessNorthDataFullGoldenCopyFileIfEnabled()
+        processFullGoldenCopyFileIfEnabled()
+        processNorthDataFullGoldenCopyFileIfEnabled()
     }
 
     /**
@@ -122,8 +123,7 @@ class ProcessDataUpdates(
     }
 
     @Suppress("UnusedPrivateMember") // Detect does not recognise the scheduled execution of this function
-    @Scheduled(cron = "0 0 8 8-15 1,4,7,10 WED")
-    //@Scheduled(cron = "0 0 5 1-7 1,4,7,10 SUN")
+    @Scheduled(cron = "0 0 5 1-7 1,4,7,10 SUN")
     private fun processNorthDataUpdates() {
         logger.info("Running scheduled update of NorthData data.")
         waitForBackend()
