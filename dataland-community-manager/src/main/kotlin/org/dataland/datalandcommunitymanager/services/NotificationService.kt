96243fe97bbfd852973b99fc651e31a4d40ec13c
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-07-23 23:41:19 +0200

    revert last change

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/NotificationService.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/NotificationService.kt
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.Duration
 import java.time.Instant
 import java.util.*
@@ -52,6 +53,7 @@ constructor(
      * Checks if notification event shall be created or not.
      * If yes, it creates it and sends a message to the queue to trigger notification emails.
      */
+    @Transactional
     fun notifyOfElementaryEvents(elementaryEvents: List<ElementaryEventEntity>, correlationId: String) {
         checkNotificationRequirementsAndDetermineNotificationEmailType(elementaryEvents)?.let { notificationEmailType ->
             logger.info(
