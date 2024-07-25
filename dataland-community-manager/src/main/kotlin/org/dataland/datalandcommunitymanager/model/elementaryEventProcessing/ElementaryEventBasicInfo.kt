7f464d17b1246eb93335db295b7c2f55f2099fea
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-07-25 14:30:49 +0200

    forgot to commit renamed class

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/model/elementaryEventProcessing/ElementaryEventBasicInfo.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/model/elementaryEventProcessing/ElementaryEventBasicInfo.kt
@@ -6,7 +6,7 @@ import java.util.*
 /**
  Represents the meta info in an elementary events message payload.
  */
-data class ElementaryEventPayloadMetaInfo(
+data class ElementaryEventBasicInfo(
     val companyId: UUID,
     val framework: DataTypeEnum,
     val reportingPeriod: String,
