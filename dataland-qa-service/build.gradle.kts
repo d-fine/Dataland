26c4fbacfc5bd3d3c566f8f6987722b94036e496
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2024-07-30 09:57:07 +0200

    Revert "Integrated flyway into QA Service. Added migration script for adding the QAReports table."
    This reverts commit 5433071b251261ecdac82de47e26812e70e6d605.

--- dataland-qa-service/build.gradle.kts
+++ dataland-qa-service/build.gradle.kts
@@ -28,8 +28,6 @@ plugins {
 dependencies {
     implementation(project(":dataland-backend-utils"))
     implementation(project(":dataland-message-queue-utils"))
-    implementation(libs.flyway)
-    implementation(libs.flyway.core)
     implementation(Spring.boot.web)
     implementation(Spring.boot.actuator)
     implementation(Spring.boot.amqp)
