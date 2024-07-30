f7932877e1b86f98dbae6904fc7c518b17bc6e7a
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2024-07-30 11:22:18 +0200

    Sonar issue and typo fix

--- dataland-framework-toolbox/src/test/kotlin/org/dataland/frameworktoolbox/specific/datamodel/ImportFormatterTest.kt
+++ dataland-framework-toolbox/src/test/kotlin/org/dataland/frameworktoolbox/specific/datamodel/ImportFormatterTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class ImportFormatterTest {
+
     @Test
     fun `short imports should not be split`() {
         val import = "hello.there"
@@ -13,7 +14,7 @@ class ImportFormatterTest {
     @Test
     fun `long imports should be split`() {
         val import = "a".repeat(110) + ".thisisverylong"
-        val expectedOutput = "import " + "a".repeat(110) + "\n    .thisisverylong"
+        val expectedOutput = "import " + "a".repeat(110) + "${LINE_PREFIX}thisisverylong"
         assertEquals(expectedOutput, ImportFormatter.splitLongImport(import))
     }
 
@@ -21,9 +22,13 @@ class ImportFormatterTest {
     fun `long imports should be split across many lines`() {
         val import = "a".repeat(110) + "." + "b".repeat(110) + "." + "c".repeat(110) + "." + "d".repeat(110)
         val expectedOutput = "import " + "a".repeat(110) +
-            "\n    ." + "b".repeat(110) +
-            "\n    ." + "c".repeat(110) +
-            "\n    ." + "d".repeat(110)
+            LINE_PREFIX + "b".repeat(110) +
+            LINE_PREFIX + "c".repeat(110) +
+            LINE_PREFIX + "d".repeat(110)
         assertEquals(expectedOutput, ImportFormatter.splitLongImport(import))
+    }
+
+    companion object {
+        const val LINE_PREFIX = "\n    ."
     }
 }
