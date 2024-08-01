a1ea4ebb672f6683be82c85e2fb7eeeaf42b24d8
Aliabdoli77 <aliabdoli77@yahoo.com>
2024-08-01 13:58:31 +0200

    tests to cover some exceptions

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/QaReportControllerTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/QaReportControllerTest.kt
@@ -1,16 +1,20 @@
 package org.dataland.e2etests.tests
 
 import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataSfdrData
+import org.dataland.datalandqaservice.openApiClient.infrastructure.ClientError
+import org.dataland.datalandqaservice.openApiClient.infrastructure.ClientException
 import org.dataland.datalandqaservice.openApiClient.model.QaReportStatusPatch
 import org.dataland.e2etests.auth.TechnicalUser
 import org.dataland.e2etests.utils.ApiAccessor
 import org.dataland.e2etests.utils.DocumentManagerAccessor
 import org.dataland.e2etests.utils.QaApiAccessor
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import java.util.*
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -58,7 +62,7 @@ class QaReportControllerTest {
     }
 
     @Test
-    fun `post a QA report and change its status`() {
+    fun `post a QA report and make sure its status can be changed`() {
         val dataId = postSfdrDatasetAndRetrieveDataId()
         val sfdrData = qaApiAccessor.createQaSfdrDataWithOneFullQaDataPoint()
         apiAccessor.jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.Reviewer)
@@ -71,13 +75,71 @@ class QaReportControllerTest {
                 qaReportStatusPatch = QaReportStatusPatch(false),
             )
         }
+        assertEquals(
+            qaApiAccessor.sfdrQaReportControllerApi.getAllQaReportsForDataset(
+                dataId = dataId,
+                showInactive = true,
+            ).first().metaInfo.active,
+            false,
+        )
+    }
 
-        val newReportStatus = qaApiAccessor.sfdrQaReportControllerApi.getAllQaReportsForDataset(
-            dataId = dataId,
-            showInactive = true,
-        ).first().metaInfo.active
+    @Test
+    fun `make sure changing the status of a report as a reviewer other than the reporter throws an exception`() {
+        val dataId = postSfdrDatasetAndRetrieveDataId()
+        val sfdrData = qaApiAccessor.createQaSfdrDataWithOneFullQaDataPoint()
+        apiAccessor.jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.Admin)
+        val qaReportMetaInfo = qaApiAccessor.sfdrQaReportControllerApi.postQaReport(dataId, sfdrData)
 
-        assertEquals(newReportStatus, false)
+        apiAccessor.jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.Reviewer)
+        val exception = assertThrows<ClientException> {
+            qaApiAccessor.sfdrQaReportControllerApi.setQaReportStatus(
+                dataId = dataId,
+                qaReportId = qaReportMetaInfo.qaReportId,
+                qaReportStatusPatch = QaReportStatusPatch(false),
+            )
+        }
+        val responseBody = (exception.response as ClientError<*>).body as String
+        Assertions.assertTrue(
+            responseBody.contains(
+                "You do not have the required access rights to update QA report with the id:" +
+                    " ${qaReportMetaInfo.qaReportId}",
+            ),
+        )
+    }
+
+    @Test
+    fun `try posting a QA report with an incorrect data type and data id and assert an exception is thrown`() {
+        postSfdrDatasetAndRetrieveDataId()
+        /* ------> can be readded after we have eu taxo data <---------
+
+
+        val NonFinancialData = qaApiAccessor.createFullQaNonFinancialData()
+
+        apiAccessor.jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.Reviewer)
+        val exception1 = assertThrows<InvalidInputApiException> {
+            qaApiAccessor.sfdrQaReportControllerApi.postQaReport(dataId, NonFinancialData)
+        }
+        val responseBody1 = (exception1.summary as ClientError<*>).body as String
+        Assertions.assertTrue(
+            responseBody1.contains(
+                "Data type mismatch",
+            ),
+        )
+        */
+
+        val sfdrData = qaApiAccessor.createQaSfdrDataWithOneFullQaDataPoint()
+        val falseId = UUID.randomUUID().toString()
+        apiAccessor.jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.Reviewer)
+        val exception2 = assertThrows<ClientException> {
+            qaApiAccessor.sfdrQaReportControllerApi.postQaReport(falseId, sfdrData)
+        }
+        val responseBody2 = (exception2.response as ClientError<*>).body as String
+        Assertions.assertTrue(
+            responseBody2.contains(
+                "No data set with the id: $falseId could be found.",
+            ),
+        )
     }
 
     private fun postSfdrDatasetAndRetrieveDataId(): String {
