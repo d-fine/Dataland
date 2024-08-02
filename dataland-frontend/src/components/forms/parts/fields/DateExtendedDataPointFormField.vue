43b5b41cffda3a61884382130e1003e60a5702f9
d93476 <jakob.kraus@d-fine.com>
2024-08-02 18:26:24 +0200

    Created DateExtendedDataPointFormField.vue

---
+++ dataland-frontend/src/components/forms/parts/fields/DateExtendedDataPointFormField.vue
@@ -0,0 +1,45 @@
+<template>
+  <ExtendedDataPointFormField
+    ref="extendedDataPointFormField"
+    :name="name"
+    :description="description"
+    :label="label"
+    :required="required"
+    :input-class="inputClass"
+    :check-value-validity="hasDataPointProperValue"
+    :isDataPointToggleable="isDataPointToggleable"
+  >
+    <div class="mb-2">
+      <DateFormField
+        :name="'value'"
+        :validation-label="validationLabel"
+        :validation="validation"
+        :placeholder="placeholder"
+        :today-as-max="todayAsMax"
+        :input-class="inputClass"
+      />
+    </div>
+  </ExtendedDataPointFormField>
+</template>
+
+<script lang="ts">
+// @ts-nocheck
+import { defineComponent } from 'vue';
+import { BaseFormFieldProps } from '@/components/forms/parts/fields/FormFieldProps';
+import ExtendedDataPointFormField from '@/components/forms/parts/elements/basic/ExtendedDataPointFormField.vue';
+import DateFormField from '@/components/forms/parts/fields/DateFormField.vue';
+import { hasDataPointProperValue } from '@/utils/DataPoint';
+
+export default defineComponent({
+  name: 'DateExtendedDataPointFormField',
+  components: { DateFormField, ExtendedDataPointFormField },
+  props: {
+    ...BaseFormFieldProps,
+    isDataPointToggleable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  methods: { hasDataPointProperValue },
+});
+</script>
