0734681bf2dc6647d96fe0dec7916bfb9f67b58b
d93476 <jakob.kraus@d-fine.com>
2024-08-02 15:11:56 +0200

    Made "Date" to ExtendedDataPoint and tested it

--- dataland-internal-storage/src/main/kotlin/db/migration/V19__MigrateEutaxonomyNonFinancialsExtendedDatapoints.kt
+++ dataland-internal-storage/src/main/kotlin/db/migration/V19__MigrateEutaxonomyNonFinancialsExtendedDatapoints.kt
@@ -29,6 +29,7 @@ class V19__MigrateEutaxonomyNonFinancialsExtendedDatapoints : BaseJavaMigration(
         "substantialContributionToPollutionPreventionAndControlInPercent",
         "substantialContributionToProtectionAndRestorationOfBiodiversityAndEcosystemsInPercent",
         "absoluteShare",
+        "fiscalYearEnd",
     )
 
     /**
