713d33865df4c1bdad950c13f521e683898f5324
swaldow <stephan.waldow@d-fine.de>
2024-08-05 17:02:21 +0200

    tests decreasing the wait time

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/communityManager/accessRequests/AccessRequestTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/communityManager/accessRequests/AccessRequestTest.kt
@@ -116,11 +116,11 @@ class AccessRequestTest {
 
         assertEquals(AccessStatus.Pending, newlyStoredRequests[0].accessStatus)
         assertEquals(RequestStatus.Open, newlyStoredRequests[0].requestStatus)
-
-        createVSMEDataAndPostAsAdminCompanyOwner(companyId)
         val timestampBeforeSingleRequestSecond = retrieveTimeAndWaitOneMillisecond()
+        createVSMEDataAndPostAsAdminCompanyOwner(companyId)
+
         jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.PremiumUser)
-        Thread.sleep(1000)
+        Thread.sleep(100)
         val newlyStoredRequestsSecond = requestControllerApi.getDataRequestsForRequestingUser().filter {
                 storedDataRequest ->
             storedDataRequest.lastModifiedDate > timestampBeforeSingleRequestSecond
