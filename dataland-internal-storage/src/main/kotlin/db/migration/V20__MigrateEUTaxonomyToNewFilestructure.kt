e671961748e79a802dc04af81a132a6e8d42ea94
Tayfun Dertsiz <Tayfun.Dertsiz@d-fine.com>
2024-08-09 16:37:20 +0200

    Changed migration script

--- dataland-internal-storage/src/main/kotlin/db/migration/V20__MigrateEUTaxonomyToNewFilestructure.kt
+++ dataland-internal-storage/src/main/kotlin/db/migration/V20__MigrateEUTaxonomyToNewFilestructure.kt
@@ -7,6 +7,7 @@ import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
 import org.json.JSONArray
 import org.json.JSONObject
+import java.nio.file.CopyOption
 
 /**
  * This migration script updates existing EUTaxonomyNonFinancial Files to the new
@@ -28,11 +29,11 @@ class V20__MigrateEUTaxonomyToNewFilestructure : BaseJavaMigration() {
 
     private val newFieldsRenamed = listOf(
         "substantialContributionToClimateChangeMitigationInPercentAligned",
-        "Substantial Contribution to Climate Change Adaptation In Percent - Aligned",
-        "Substantial Contribution to Sustainable Use and Protection of Water and Marine Resources In Percent - Aligned",
-        "Substantial Contribution to Transition to a Circular Economy In Percent - Aligned",
-        "Substantial Contribution to Pollution Prevention and Control In Percent - Aligned",
-        "Substantial Contribution to Protection and Restoration of Biodiversity and Ecosystems In Percent - Aligned",
+        "substantialContributionToClimateChangeAdaptationInPercentAligned",
+        "substantialContributionToSustainableUseAndProtectionOfWaterAndMarineResourcesInPercentAligned",
+        "substantialContributionToTransitionToACircularEconomyInPercentAligned",
+        "substantialContributionToPollutionPreventionAndControlInPercentAligned",
+        "substantialContributionToProtectionAndRestorationOfBiodiversityAndEcosystemsInPercentAligned",
     )
 
     private val newFieldAddition = listOf(
@@ -65,18 +66,80 @@ class V20__MigrateEUTaxonomyToNewFilestructure : BaseJavaMigration() {
      * for the final green block just rename
      */
 
+
+    private fun replaceOldKeyWithNewOne(oldString: String, jsonObject: JSONObject, jsonCopy: JSONObject) {
+        println("I want to see what gets into this function: " + oldString)
+
+
+        val value = jsonObject.get(oldString)
+
+        val indexOfOldString = oldFieldsToRename.indexOf(oldString)
+        jsonCopy.put(newFieldsRenamed[indexOfOldString], value)
+
+
+    }
+
+    private fun migrateToAligned(jsonObject: JSONObject, it: String) {
+        val value = jsonObject.getOrJavaNull(it)
+        val valueAligned = jsonObject.getOrJavaNull("substantialContributionToClimateChangeMitigationInPercentAligned") as JSONObject? ?: JSONObject()
+        valueAligned.put("substantialContributionToClimateChangeMitigationInPercentAligned",value)
+
+        jsonObject.put("substantialContributionToClimateChangeMitigationInPercentAligned",valueAligned)
+    }
+
     fun checkForRelevantFieldsInJsonObjectKeys(jsonObject: JSONObject) {
 
-        var keylist = jsonObject.keys()
-        keylist.forEach {
+        val keysToBeRemoved: MutableList<String> = mutableListOf()
+        val keyStringList = jsonObject.keys().iterator().asSequence().toList()
+        val valueAligned = jsonObject.getOrJavaNull("zzzzzzzzzzzzzz") as JSONObject? ?: JSONObject()
+
+        for(i in 0 until keyStringList.size) {
+
+            if(oldFieldsToRename.contains(keyStringList[i])) {
+
+                replaceOldKeyWithNewOne(keyStringList[i], jsonObject, valueAligned)
+
+                // if the keyListString element is in the list of leemnts to be replaced ONLY THEN do something
+//                println("=======================================ULTRA TEST")
+//                val value = jsonObject.getOrJavaNull("substantialContributionToClimateChangeMitigationInPercent")
+//                valueAligned.put("substantialContributionToClimateChangeMitigationInPercentAligned", value)
+
+            }
+            checkRecursivelyForBaseDataPointsInJsonObject(jsonObject, keyStringList[i])
+        }
+
+        if(valueAligned.length() > 0){
+            if(valueAligned.length() ==6){
+                println("==============================================================lets see it")
+            }
+        }
+
+        println("What happens in the end?")
+
+            /*
+            .forEach {
             println("ALLE KEYS:" + it)
 
-            if (it == "substantialContributionToClimateChangeMitigationInPercent") {
-                // funktion welche diesen Eintrag durch den neuen ersetzt
+            if(it == "substantialContributionToClimateChangeMitigationInPercent"){
+                println("!!!WATCH FROM HERE!!!")
+                migrateToAligned(jsonObject, it)
             }
+
             checkRecursivelyForBaseDataPointsInJsonObject(jsonObject, it)
         }
+
+             */
+/*
+            if (it == "substantialContributionToClimateChangeMitigationInPercent") {
+                replaceOldKeyForNewKey(jsonObject,it)
+            }
+
+        }
+         */
     }
+
+
+
 
     private fun checkRecursivelyForBaseDataPointsInJsonArray(jsonArray: JSONArray) {
         for (i in 0 until jsonArray.length()) {
