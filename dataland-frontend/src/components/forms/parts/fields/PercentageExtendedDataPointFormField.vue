89aeadb636cbca3c89789df3aac2fdb000fa49d8
d93476 <jakob.kraus@d-fine.com>
2024-08-12 16:37:09 +0200

    Made ExtendedDataPoints toggleable

--- dataland-frontend/src/components/forms/parts/fields/PercentageExtendedDataPointFormField.vue
+++ dataland-frontend/src/components/forms/parts/fields/PercentageExtendedDataPointFormField.vue
@@ -39,7 +39,7 @@ export default defineComponent({
     ...BaseFormFieldProps,
     isDataPointToggleable: {
       type: Boolean,
-      default: false,
+      default: true,
     },
     placeholder: {
       type: String,
