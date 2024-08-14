f421968f10ffdb1c6076006f58eb65e113b2ae14
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-08-14 11:00:48 +0200

    remove another unnecessary sfdr fixture and re-generate fake fixtures

--- dataland-frontend/tests/e2e/fixtures/frameworks/sfdr/SfdrPreparedFixtures.ts
+++ dataland-frontend/tests/e2e/fixtures/frameworks/sfdr/SfdrPreparedFixtures.ts
@@ -20,7 +20,6 @@ export function generateSfdrPreparedFixtures(): Array<FixtureData<SfdrData>> {
   );
   preparedFixtures.push(manipulateFixtureForNoNullFields(generateSfdrDataWithoutNulls()));
   preparedFixtures.push(manipulateFixtureForInvalidCurrencyInput(generateSfdrDataWithoutNulls()));
-  preparedFixtures.push(manipulateFixtureForInvalidBigDecimalDataPointInput(generateSfdrDataWithoutNulls()));
   preparedFixtures.push(manipulateFixtureForInvalidLongDataPointInput(generateSfdrDataWithoutNulls()));
   preparedFixtures.push(manipulateFixtureForEmptyStringDocumentReference(generateSfdrDataWithoutNulls()));
   preparedFixtures.push(manipulateFixtureForInvalidPercentageInput(generateSfdrDataWithoutNulls()));
@@ -46,18 +45,6 @@ function generateSfdrDataWithoutNulls(): FixtureData<SfdrData> {
 function manipulateFixtureForInvalidCurrencyInput(input: FixtureData<SfdrData>): FixtureData<SfdrData> {
   input.companyInformation.companyName = 'Sfdr-dataset-with-invalid-currency-input';
   input.t.social!.socialAndEmployeeMatters!.averageGrossHourlyEarningsFemaleEmployees!.value = -100;
-  return input;
-}
-
-/**
- * Sets the company name to a specific value to be able to pick this dataset from the prepared fixtures.
- * sets an extendedDataPoint<BigDecimal> to an invalid negative value>
- * @param input Fixture data to be manipulated
- * @returns the manipulated fixture data
- */
-function manipulateFixtureForInvalidBigDecimalDataPointInput(input: FixtureData<SfdrData>): FixtureData<SfdrData> {
-  input.companyInformation.companyName = 'Sfdr-dataset-with-invalid-negative-big-decimal-input';
-  input.t.social!.socialAndEmployeeMatters!.workdaysLostInDays!.value = -1;
   return input;
 }
 
