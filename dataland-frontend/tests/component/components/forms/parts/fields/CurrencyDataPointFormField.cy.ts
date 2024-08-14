7f9074f23abc1b9bfbcfff52fecae166a462f860
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-08-14 10:51:28 +0200

    reduce CurrencyDataPointFormField.cy.ts test because there is unnecessary test code in it

--- dataland-frontend/tests/component/components/forms/parts/fields/CurrencyDataPointFormField.cy.ts
+++ dataland-frontend/tests/component/components/forms/parts/fields/CurrencyDataPointFormField.cy.ts
@@ -4,32 +4,18 @@ import { selectItemFromDropdownByValue } from '@sharedUtils/Dropdown';
 
 describe('Component test for CurrencyDataPointFormField', () => {
   /**
-   * Ensures that the data quality field is inactive and that the field is not shown as required with an asterix
+   * Ensures that the data quality field exists and does not have an asterisk, showing that it is not mandatory
    */
-  function checkDataQualityFieldNotMandatory(): void {
+  function validateDataQualityField(): void {
     cy.get('div[data-test="dataQuality"] div[name="quality"]').find('span').should('have.text', 'Data quality');
-    cy.get('div[data-test="dataQuality"] .form-field-label span.asterisk').should('not.exist');
-  }
-
-  /**
-   * Ensures that the data quality field is active and that the field is shown
-   */
-  function checkDataQualityFieldActive(): void {
-    cy.get('div[data-test="dataQuality"] div[name="quality"]')
-      .should('not.have.class', 'p-disabled')
-      .find('span')
-      .should('have.text', 'Data quality');
     cy.get('div[data-test="dataQuality"] .form-field-label span.asterisk').should('not.exist');
   }
 
   it("Quality field should be 'Data quality' if the value field has no value and currency field should work as expected", () => {
     cy.mountWithPlugins(CurrencyDataPointFormField, {}).then(() => {
       cy.get('[data-test="dataPointToggleButton"]').click();
-
-      checkDataQualityFieldNotMandatory();
+      validateDataQualityField();
       cy.get('input[name="value"]').should('be.visible').type('123');
-      checkDataQualityFieldActive();
-
       selectItemFromDropdownByValue(cy.get('div[name="currency"]').should('be.visible'), 'EUR', true);
     });
   });
