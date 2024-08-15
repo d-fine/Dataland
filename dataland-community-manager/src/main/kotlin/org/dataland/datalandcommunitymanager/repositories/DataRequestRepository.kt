6de52888447e19ba4c0ce2257b2567bb474b34cc
swaldow <stephan.waldow@d-fine.de>
2024-08-15 14:48:22 +0200

    tests updated query part 2

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/repositories/DataRequestRepository.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/repositories/DataRequestRepository.kt
@@ -93,10 +93,10 @@ interface DataRequestRepository : JpaRepository<DataRequestEntity, String> {
             "OR d.data_type = :#{#searchFilter.dataTypeFilter}) AND " +
             "(:#{#searchFilter.userIdFilterLength} = 0 " +
             "OR d.user_Id = :#{#searchFilter.userIdFilter}) AND " +
-            "(:#{#searchFilter.requestStatus} IS NULL " +
-            "OR status_table.request_status = :#{#searchFilter.requestStatus}) AND " +
-            "(:#{#searchFilter.accessStatus} IS NULL " +
-            "OR status_table.access_status = :#{#searchFilter.accessStatus}) AND " +
+            "(CAST(:#{#searchFilter.requestStatus} AS varchar) IS NULL " +
+            "OR status_table.request_status = CAST(:#{#searchFilter.requestStatus} AS varchar)) AND " +
+            "(CAST(:#{#searchFilter.accessStatus} AS varchar) IS NULL " +
+            "OR status_table.access_status = CAST(:#{#searchFilter.accessStatus}  AS varchar) ) AND " +
             "(:#{#searchFilter.reportingPeriodFilterLength} = 0 " +
             "OR d.reporting_period = :#{#searchFilter.reportingPeriodFilter}) AND " +
             "(:#{#searchFilter.datalandCompanyIdFilterLength} = 0 " +
