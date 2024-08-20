fe9b098983709c443edde7182ecb85d60a17375d
Laurin Strelow <1918073+LaurinStrelow@users.noreply.github.com>
2024-08-20 14:21:59 +0200

    Add initial draft of PavedRoadFramework subclass

---
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/additionalcompanyinformation/AdditionalCompanyInformationFramework.kt
@@ -0,0 +1,19 @@
+package org.dataland.frameworktoolbox.frameworks.additionalcompanyinformation
+
+import org.dataland.frameworktoolbox.frameworks.FrameworkGenerationFeatures
+import org.dataland.frameworktoolbox.frameworks.InDevelopmentPavedRoadFramework
+import org.springframework.stereotype.Component
+import java.io.File
+
+/**
+ * The additional company information framework
+ */
+@Component
+class AdditionalCompanyInformationFramework : InDevelopmentPavedRoadFramework(
+    identifier = "additional-company-information",
+    label = "Additional Company Information",
+    explanation = "Additional Company Information",
+    File("./dataland-framework-toolbox/inputs/additional-company-information/additional-company-information.xlsx"),
+    order = 2,
+    enabledFeatures = FrameworkGenerationFeatures.ENTRY_SET,
+)
