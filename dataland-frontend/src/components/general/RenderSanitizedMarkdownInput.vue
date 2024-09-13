a3cfb1e856487df065d9f0ba2437111f4cbc35d9
d93476 <jakob.kraus@d-fine.com>
2024-09-13 13:28:24 +0200

    Re-allowed raw URLs

--- dataland-frontend/src/components/general/RenderSanitizedMarkdownInput.vue
+++ dataland-frontend/src/components/general/RenderSanitizedMarkdownInput.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import DOMPurify from 'dompurify';
 import MarkdownRender from 'vue-markdown-render';
 
@@ -13,9 +13,22 @@ const props = defineProps<{
   text: string;
 }>();
 
+const errorMessage = ref('');
+
+/**
+ * Method to replace raw HTTP URLs with Markdown-type URLs: [URL presentation](URL)
+ * @param text Input text
+ * @returns a converted input text (text remains unchanged if no matching HTTP URLS are found)
+ */
+function convertRawUrlsToMarkdown(text: string): string {
+  const urlPattern = /https:\/\/\S+/; // Regex pattern to match URLs
+  return text.replace(urlPattern, (url) => `[${url}](${url})`);
+}
+
 const sanitizedMarkdown = computed(() => {
   try {
-    return DOMPurify.sanitize(props.text);
+    const markdownWithLinks = convertRawUrlsToMarkdown(props.text);
+    return DOMPurify.sanitize(markdownWithLinks);
   } catch (error) {
     console.error('Error processing markdown:', error);
     return '';
