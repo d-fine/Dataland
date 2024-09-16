e2d59a74c11d94fd0fc1373c162e6f64c8425c20
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-09-16 10:58:02 +0200

    refactoring

--- dataland-qa-service/src/test/kotlin/org/dataland/datalandqaservice/utils/NoBackendRequestQaReportSecurityPolicy.kt
+++ dataland-qa-service/src/test/kotlin/org/dataland/datalandqaservice/utils/NoBackendRequestQaReportSecurityPolicy.kt
@@ -1,7 +1,7 @@
 package org.dataland.datalandqaservice.utils
 
 import org.dataland.datalandqaservice.org.dataland.datalandqaservice.services.QaReportSecurityPolicy
-import org.dataland.datalandqaservice.org.dataland.datalandqaservice.services.UserAuthenticatedApiService
+import org.dataland.datalandqaservice.org.dataland.datalandqaservice.services.UserAuthenticatedBackendClient
 import org.dataland.keycloakAdapter.auth.DatalandAuthentication
 
 /**
@@ -9,8 +9,8 @@ import org.dataland.keycloakAdapter.auth.DatalandAuthentication
  * for QA reports for testing.
  */
 class NoBackendRequestQaReportSecurityPolicy(
-    userAuthenticatedApiService: UserAuthenticatedApiService,
-) : QaReportSecurityPolicy(userAuthenticatedApiService) {
+    userAuthenticatedBackendClient: UserAuthenticatedBackendClient,
+) : QaReportSecurityPolicy(userAuthenticatedBackendClient) {
     override fun ensureUserCanViewQaReportForDataId(dataId: String, user: DatalandAuthentication) {
         // Do nothing. This function is checked in an E2E-test.
     }
