1037b6334cc2c9f57205f2abae10ba568f1e0d4e
swaldow <stephan.waldow@d-fine.de>
2024-09-17 17:17:36 +0200

    removes qa prefix in requestmapping

--- dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/api/QaApi.kt
+++ dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/api/QaApi.kt
@@ -13,14 +13,12 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import java.util.*
 
 /**
  * Defines the restful dataland qa service API
  */
-@RequestMapping("/qa")
 @SecurityRequirement(name = "default-bearer-auth")
 @SecurityRequirement(name = "default-oauth")
 interface QaApi {
