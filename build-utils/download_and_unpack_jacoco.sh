87a825d819657dd2a20c6dcc0af3b00f4d2573cb
Laurin Strelow <1918073+LaurinStrelow@users.noreply.github.com>
2024-09-18 16:29:40 +0200

    docker part 2

--- build-utils/download_and_unpack_jacoco.sh
+++ build-utils/download_and_unpack_jacoco.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euxo pipefail
 
 wget https://repo1.maven.org/maven2/org/jacoco/jacoco/0.8.12/jacoco-0.8.12.zip
 unzip -j ./jacoco-0.8.12.zip "lib/jacocoagent.jar" -d /jar/
