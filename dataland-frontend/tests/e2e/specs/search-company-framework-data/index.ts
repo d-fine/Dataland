d816357cbe1bf33efd1cfed12a0234e3d07ac806
Julia Cialkowski <julia.cialkowski@d-fine.de>
2024-09-19 11:22:32 +0200

    commented failing e2e test.

--- dataland-frontend/tests/e2e/specs/search-company-framework-data/index.ts
+++ dataland-frontend/tests/e2e/specs/search-company-framework-data/index.ts
@@ -2,6 +2,7 @@
  * As a user I want to be able to search for companies with framework data on the central Dataland search page.
  * I want to use the provided filters on that page and navigate through the search results.
  */
+//CI failed see https://github.com/d-fine/Dataland/actions/runs/10895483704
 describe('Taxonomy Company Metadata tests', () => {
   require('./SearchCompaniesForFrameworkData');
   require('./SearchCompaniesForFrameworkDataDropdownFilter');
