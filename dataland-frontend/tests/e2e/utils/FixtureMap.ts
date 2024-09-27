e732f90416c86ed85f100547d7eaeaaae83bd37d
Laurin Strelow <1918073+LaurinStrelow@users.noreply.github.com>
2024-09-27 15:35:28 +0200

    fix prepopulation

--- dataland-frontend/tests/e2e/utils/FixtureMap.ts
+++ dataland-frontend/tests/e2e/utils/FixtureMap.ts
@@ -1,6 +1,5 @@
 import { DataTypeEnum } from '@clients/backend';
 
 export const frameworkFixtureMap = {
-  [DataTypeEnum.EuTaxonomyFinancials]: 'CompanyInformationWithEuTaxonomyDataForFinancials',
   [DataTypeEnum.P2p]: 'CompanyInformationWithP2pData',
 };
