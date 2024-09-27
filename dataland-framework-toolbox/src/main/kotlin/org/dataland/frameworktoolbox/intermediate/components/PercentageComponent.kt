bd9296c002b767d86121ce27550f7529785e98a8
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-09-27 13:26:56 +0200

    fix broken UploadConfig.ts by making PercentageComponent able to handle "ExtendedDocumentSupport" in its uploadConfigCell-generator

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/intermediate/components/PercentageComponent.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/intermediate/components/PercentageComponent.kt
@@ -2,6 +2,8 @@ package org.dataland.frameworktoolbox.intermediate.components
 
 import org.dataland.frameworktoolbox.intermediate.FieldNodeParent
 import org.dataland.frameworktoolbox.intermediate.components.basecomponents.NumberBaseComponent
+import org.dataland.frameworktoolbox.intermediate.datapoints.ExtendedDocumentSupport
+import org.dataland.frameworktoolbox.intermediate.datapoints.NoDocumentSupport
 import org.dataland.frameworktoolbox.intermediate.datapoints.addPropertyWithDocumentSupport
 import org.dataland.frameworktoolbox.specific.datamodel.TypeReference
 import org.dataland.frameworktoolbox.specific.datamodel.elements.DataClassBuilder
@@ -13,6 +15,9 @@ import org.dataland.frameworktoolbox.specific.viewconfig.elements.getTypescriptF
 import org.dataland.frameworktoolbox.specific.viewconfig.functional.FrameworkDisplayValueLambda
 import org.dataland.frameworktoolbox.utils.typescript.TypeScriptImport
 
+private const val MIN_PERCENTAGE: Long = 0
+private const val MAX_PERCENTAGE: Long = 100
+
 /**
  * A PercentageComponent represents a decimal percentage between 0 % and 100 %.
  */
@@ -20,6 +25,8 @@ class PercentageComponent(
     identifier: String,
     parent: FieldNodeParent,
 ) : NumberBaseComponent(identifier, parent) {
+
+    override var constantUnitSuffix: String? = "%"
 
     override fun generateDefaultDataModel(dataClassBuilder: DataClassBuilder) {
         dataClassBuilder.addPropertyWithDocumentSupport(
@@ -58,11 +65,21 @@ class PercentageComponent(
     }
 
     override fun generateDefaultUploadConfig(uploadCategoryBuilder: UploadCategoryBuilder) {
+        requireDocumentSupportIn(setOf(NoDocumentSupport, ExtendedDocumentSupport))
+        val uploadComponent = when (documentSupport) {
+            is NoDocumentSupport -> "NumberFormField"
+            is ExtendedDocumentSupport -> "PercentageExtendedDataPointFormField"
+            else -> throw IllegalArgumentException(
+                "Upload-page generation for this component " +
+                    "does not support $documentSupport",
+            )
+        }
+
         uploadCategoryBuilder.addStandardUploadConfigCell(
             component = this,
-            uploadComponentName = "NumberFormField",
-            validation = "between:0,100",
-            unit = "%",
+            uploadComponentName = uploadComponent,
+            unit = constantUnitSuffix,
+            validation = getMinMaxValidationRule(MIN_PERCENTAGE, MAX_PERCENTAGE),
         )
     }
 
