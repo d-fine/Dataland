ff457a290d6812ce7c4e89137e64b449f130345c
Julia Cialkowski <julia.cialkowski@d-fine.de>
2024-10-17 13:46:11 +0200

    displaying number of economic activities in UI.

--- dataland-frontend/src/components/resources/frameworkDataSearch/nuclearAndGas/NuclearAndGasActivityNames.ts
+++ dataland-frontend/src/components/resources/frameworkDataSearch/nuclearAndGas/NuclearAndGasActivityNames.ts
@@ -1,5 +1,5 @@
 /**
- * An enum that holds the EU taxonomy activities with their names as values
+ * An enum that holds the nuclear and gas economic activities with their names as values
  */
 export enum ActivityName {
   taxonomyAlignedShareDenominatorNAndG426 = " Proportion of taxonomy-aligned economic activity referred to in Section 4.26 of Annexes I and II to Delegated Regulation 2021/2139 in the denominator of the applicable KPI",
