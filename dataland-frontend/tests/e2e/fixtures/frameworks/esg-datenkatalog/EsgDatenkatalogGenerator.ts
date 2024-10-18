8fc6de0c2ed1e8a5bf186b22d67a0a5c82483fe7
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2024-10-18 15:46:14 +0200

    remove unused code in generator

--- dataland-frontend/tests/e2e/fixtures/frameworks/esg-datenkatalog/EsgDatenkatalogGenerator.ts
+++ dataland-frontend/tests/e2e/fixtures/frameworks/esg-datenkatalog/EsgDatenkatalogGenerator.ts
@@ -1,10 +1,4 @@
 import { DEFAULT_PROBABILITY, Generator } from '@e2e/utils/FakeFixtureUtils';
-interface YearlyTimeseriesData<InnerObj> {
-  currentYear: number;
-  yearlyData: { [key: string]: InnerObj };
-}
-
-type MappedOptionalDecimal<KeyList extends string> = { [K in KeyList]?: number | null };
 
 export class EsgDatenkatalogGenerator extends Generator {
   dataDate: string;
@@ -12,40 +6,5 @@ export class EsgDatenkatalogGenerator extends Generator {
   constructor(nullProbability = DEFAULT_PROBABILITY) {
     super(nullProbability);
     this.dataDate = this.guaranteedFutureDate();
-  }
-
-  randomDecimalYearlyTimeseriesData<T extends string>(
-    keys: T[],
-    nYearsIntoPast: number,
-    nYearsIntoFuture: number
-  ): YearlyTimeseriesData<MappedOptionalDecimal<T>> | null {
-    return this.valueOrNull(this.guaranteedDecimalYearlyTimeseriesData(keys, nYearsIntoPast, nYearsIntoFuture));
-  }
-  guaranteedDecimalYearlyTimeseriesData<T extends string>(
-    keys: T[],
-    nYearsIntoPast: number,
-    nYearsIntoFuture: number
-  ): YearlyTimeseriesData<MappedOptionalDecimal<T>> {
-    const baseYear = parseInt(this.dataDate.substring(0, 4));
-    const yearlyData: { [key: string]: MappedOptionalDecimal<T> } = {};
-
-    for (let year = baseYear - nYearsIntoPast; year <= baseYear + nYearsIntoFuture; year++) {
-      const value = this.valueOrNull(this.guaranteedDecimalDataObject(keys));
-      if (value != null) {
-        yearlyData[`${year}`] = value;
-      }
-    }
-
-    return {
-      currentYear: baseYear,
-      yearlyData: yearlyData,
-    };
-  }
-  guaranteedDecimalDataObject<T extends string>(keys: T[]): MappedOptionalDecimal<T> {
-    const dataObject = {} as MappedOptionalDecimal<T>;
-    for (const key of keys) {
-      dataObject[key] = this.randomFloat();
-    }
-    return dataObject;
   }
 }
