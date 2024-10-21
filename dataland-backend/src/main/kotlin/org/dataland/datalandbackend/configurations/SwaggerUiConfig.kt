cc679c9fdeb1b9b7a1699330b87ba579d90fc881
emanueldfine <99255296+emanueldfine@users.noreply.github.com>
2024-10-21 10:53:18 +0200

    Dala 5023 gdv voeb update (#743)
    Update of the ESG-Datenkatalog für Großunternehmen.
    
    ---------
    
    Co-authored-by: SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
    Co-authored-by: LaurinStrelow <1918073+LaurinStrelow@users.noreply.github.com>

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/configurations/SwaggerUiConfig.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/configurations/SwaggerUiConfig.kt
@@ -1,6 +1,6 @@
 package org.dataland.datalandbackend.configurations
 
-import org.dataland.datalandbackend.frameworks.esgquestionnaire.custom.OpenApiEsgQuestionnaireExampleCustomizer
+import org.dataland.datalandbackend.frameworks.esgdatenkatalog.custom.OpenApiEsgDatenkatalogExampleCustomizer
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -15,13 +15,13 @@ class SwaggerUiConfig {
      * This method returns all public endpoints to be displayed in the swagger ui
      */
     @Bean
-    fun publicApi(esgQuestionnaireCustomizer: OpenApiEsgQuestionnaireExampleCustomizer): GroupedOpenApi? =
+    fun publicApi(esgDatenkatalogCustomizer: OpenApiEsgDatenkatalogExampleCustomizer): GroupedOpenApi? =
         GroupedOpenApi
             .builder()
             .group("public")
             .pathsToExclude("/internal/**")
             .addOpenApiCustomizer(
                 DataTypeSchemaCustomizer(),
-            ).addOpenApiCustomizer(esgQuestionnaireCustomizer)
+            ).addOpenApiCustomizer(esgDatenkatalogCustomizer)
             .build()
 }
