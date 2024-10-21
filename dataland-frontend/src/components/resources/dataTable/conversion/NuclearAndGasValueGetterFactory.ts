fd9c7ff58f2c9d088b7be08dcaa1d87105cff57f
Julia Cialkowski <julia.cialkowski@d-fine.de>
2024-10-21 07:27:54 +0200

    Jsdocs.

--- dataland-frontend/src/components/resources/dataTable/conversion/NuclearAndGasValueGetterFactory.ts
+++ dataland-frontend/src/components/resources/dataTable/conversion/NuclearAndGasValueGetterFactory.ts
@@ -1,12 +1,5 @@
-/**
- * Formats a NuclearAndGas component for display in the multi-layer-data-table.
- * @param input the list of nuclear and gas economic activities
- * @param fieldLabel the label of the respective field in the framework
- * @returns the display object for the multi-layer-data-table to render a modal to display the
- * nuclear and gas economic activities.
- */
 import {
-  AvailableMLDTDisplayObjectTypes,
+  type AvailableMLDTDisplayObjectTypes,
   MLDTDisplayComponentName,
   type MLDTDisplayObject,
   MLDTDisplayObjectForEmptyString,
@@ -19,25 +12,56 @@ import type {
   ExtendedDataPointNuclearAndGasNonEligible,
 } from '@clients/backend';
 
+/**
+ * Checks if the provided NuclearAndGasData represents a non-eligible economic activity.
+ * @param nuclearAndGasData The input data representing one of the Nuclear and Gas data points.
+ * @returns Returns true if the data represents a non-eligible activity, otherwise false.
+ */
 function isNonEligible(
-    nuclearAndGasData: any
+  nuclearAndGasData:
+    | ExtendedDataPointNuclearAndGasAlignedDenominator
+    | ExtendedDataPointNuclearAndGasAlignedNumerator
+    | ExtendedDataPointNuclearAndGasEligibleButNotAligned
+    | ExtendedDataPointNuclearAndGasNonEligible
 ): nuclearAndGasData is ExtendedDataPointNuclearAndGasNonEligible {
   return (
-      typeof nuclearAndGasData === 'object' &&
-      nuclearAndGasData !== null &&
-      'taxonomyNonEligibleShareNAndG426' in nuclearAndGasData.value
+    typeof nuclearAndGasData === 'object' &&
+    nuclearAndGasData !== null &&
+    nuclearAndGasData.value !== null &&
+    typeof nuclearAndGasData.value === 'object' &&
+    'taxonomyNonEligibleShareNAndG426' in nuclearAndGasData.value
   );
 }
 
+export const nuclearAndGasModalColumnHeaders = {
+  nuclearAndGas: {
+    economicActivity: 'Economic Activity',
+    ccmCca: 'CCM + CCA',
+    ccm: 'CCM',
+    cca: 'CCA',
+  },
+  nuclearAndGasNonEligible: {
+    economicActivity: 'Economic Activity',
+    proportion: 'Proportion',
+  },
+};
+
+/**
+ * Formats a NuclearAndGas component for display in the multi-layer-data-table.
+ * @param nuclearAndGasData the list of nuclear and gas economic activities
+ * @param fieldLabel the label of the respective field in the framework
+ * @returns the display object for the multi-layer-data-table to render a modal to display the
+ * nuclear and gas economic activities.
+ */
 export function formatNuclearAndGasTaxonomyShareDataForTable(
-    nuclearAndGasData:
-        | ExtendedDataPointNuclearAndGasAlignedDenominator
-        | ExtendedDataPointNuclearAndGasAlignedNumerator
-        | ExtendedDataPointNuclearAndGasEligibleButNotAligned
-        | ExtendedDataPointNuclearAndGasNonEligible
-        | null
-        | undefined,
-    fieldLabel: string
+  nuclearAndGasData:
+    | ExtendedDataPointNuclearAndGasAlignedDenominator
+    | ExtendedDataPointNuclearAndGasAlignedNumerator
+    | ExtendedDataPointNuclearAndGasEligibleButNotAligned
+    | ExtendedDataPointNuclearAndGasNonEligible
+    | null
+    | undefined,
+  fieldLabel: string
 ): AvailableMLDTDisplayObjectTypes {
   if (!nuclearAndGasData || !nuclearAndGasData.value) {
     return MLDTDisplayObjectForEmptyString;
@@ -45,18 +69,23 @@ export function formatNuclearAndGasTaxonomyShareDataForTable(
     let activityCount: number;
 
     if (isNonEligible(nuclearAndGasData)) {
-      activityCount = Object.values(nuclearAndGasData.value).filter(value => value !== null).length;
+      activityCount = Object.values(nuclearAndGasData.value).filter((value) => value !== null).length;
     } else {
       activityCount = Object.values(nuclearAndGasData.value).filter(
-          field => field && Object.values(field).some(value => value !== null)
+        (field) => field && Object.values(field).some((value) => value !== null)
       ).length;
     }
+
+    const columnHeaders = isNonEligible(nuclearAndGasData)
+      ? nuclearAndGasModalColumnHeaders.nuclearAndGasNonEligible
+      : nuclearAndGasModalColumnHeaders.nuclearAndGas;
 
     return <MLDTDisplayObject<MLDTDisplayComponentName.ModalLinkWithDataSourceDisplayComponent>>{
       displayComponentName: MLDTDisplayComponentName.ModalLinkWithDataSourceDisplayComponent,
       displayValue: {
         label: `Show ${activityCount} activit${activityCount > 1 ? 'ies' : 'y'}`,
         modalComponent: NuclearAndGasDataTable,
+        columnHeaders: columnHeaders,
         modalOptions: {
           props: {
             header: fieldLabel,
