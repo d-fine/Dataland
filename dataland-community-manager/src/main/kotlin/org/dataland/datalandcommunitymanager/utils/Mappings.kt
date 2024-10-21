b9abba5f8f94ffc6aa761a86af8e0cebee58560e
Laurin Strelow <1918073+LaurinStrelow@users.noreply.github.com>
2024-10-21 10:46:52 +0200

    fix test

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/utils/Mappings.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/utils/Mappings.kt
@@ -6,7 +6,7 @@ val readableFrameworkNameMapping: Map<DataTypeEnum, String> =
     mapOf(
         DataTypeEnum.eutaxonomyMinusFinancials to "EU Taxonomy for financial companies",
         DataTypeEnum.eutaxonomyMinusNonMinusFinancials to "EU Taxonomy for non-financial companies",
-        DataTypeEnum.nuclearMinusAndMinusGas to "EU Taxonomy nuclear and gas",
+        DataTypeEnum.nuclearMinusAndMinusGas to "EU Taxonomy Nuclear and Gas",
         DataTypeEnum.lksg to "LkSG",
         DataTypeEnum.sfdr to "SFDR",
         DataTypeEnum.vsme to "VSME",
