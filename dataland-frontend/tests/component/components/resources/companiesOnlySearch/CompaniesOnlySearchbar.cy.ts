5222991e9f892a9720d8db1467f5115b53e8af60
Emanuel Alexandi <emanuel.alexandi@d-fine.de>
2024-10-24 10:03:58 +0200

    CompaniesOnlySearchbar.cy.ts refactoring

--- dataland-frontend/tests/component/components/resources/companiesOnlySearch/CompaniesOnlySearchbar.cy.ts
+++ dataland-frontend/tests/component/components/resources/companiesOnlySearch/CompaniesOnlySearchbar.cy.ts
@@ -1,24 +1,60 @@
 import { minimalKeycloakMock } from '@ct/testUtils/Keycloak';
 import { getMountingFunction } from '@ct/testUtils/Mount';
 import CompaniesOnlySearchBar from '@/components/resources/companiesOnlySearch/CompaniesOnlySearchBar.vue';
+import { type CompanyIdAndName } from '@clients/backend';
+
+/**
+ * Types the input into the search bar
+ * @param input to type
+ */
+function typeIntoSearchBar(input: string): void {
+  cy.get(`input[id="company_search_bar_standard"]`).type(input);
+}
+
+/**
+ * Checks if the warning is there or is not there, based on the boolean passed to the function.
+ * @param isWarningExpectedToExist decides whether the warning is expected to be displayed or not
+ */
+function validateSearchStringWarning(isWarningExpectedToExist: boolean): void {
+  cy.contains('span', 'Please type at least 3 characters').should(isWarningExpectedToExist ? 'exist' : 'not.exist');
+}
+
+/**
+ * Checks if the autocomplete panel is there or is not there, based on the boolean passed to the function.
+ * @param isPanelExpectedToExist decides whether the panel is expected to be displayed or not
+ */
+function validateAutocompletePanel(isPanelExpectedToExist: boolean): void {
+  cy.get('div.p-autocomplete-panel').should(isPanelExpectedToExist ? 'exist' : 'not.exist');
+}
 
 describe('Component test for companies search bar', () => {
   it('No search occurs and a warning is displayed if search string is too short', () => {
+    const mockSearchResults: CompanyIdAndName[] = [
+      { companyName: 'abc123', companyId: crypto.randomUUID() },
+      { companyName: 'abc456', companyId: crypto.randomUUID() },
+      { companyName: 'abc789', companyId: crypto.randomUUID() },
+    ];
+
     getMountingFunction({ keycloak: minimalKeycloakMock() })(CompaniesOnlySearchBar);
-    cy.contains('span', 'Please type at least 3 characters').should('not.exist');
+    validateSearchStringWarning(false);
+    validateAutocompletePanel(false);
 
-    cy.get(`input[id="company_search_bar_standard"]`).type('a');
-    cy.contains('span', 'Please type at least 3 characters').should('exist');
+    typeIntoSearchBar('a');
+    validateSearchStringWarning(true);
+    validateAutocompletePanel(false);
 
-    cy.get(`input[id="company_search_bar_standard"]`).type('b');
-    cy.contains('span', 'Please type at least 3 characters').should('exist');
+    typeIntoSearchBar('b');
+    validateSearchStringWarning(true);
+    validateAutocompletePanel(false);
 
-    cy.intercept(`**/companies/names?searchString=abc&resultLimit=100`, []).as('searchForAbc');
-    cy.get(`input[id="company_search_bar_standard"]`).type('c');
-    cy.contains('span', 'Please type at least 3 characters').should('not.exist');
+    cy.intercept(`**/companies/names?searchString=abc&resultLimit=100`, mockSearchResults).as('searchForAbc');
+    typeIntoSearchBar('c');
+    validateSearchStringWarning(false);
     cy.wait('@searchForAbc');
+    validateAutocompletePanel(true);
 
     cy.get(`input[id="company_search_bar_standard"]`).clear();
-    cy.contains('span', 'Please type at least 3 characters').should('not.exist');
+    validateSearchStringWarning(false);
+    validateAutocompletePanel(false);
   });
 });
