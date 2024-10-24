2738e0091f5b045e480cb49dd936c4d935e7cf65
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2024-10-24 22:43:27 +0200

    add first set of files

--- build-utils/rebuild_specification_service_image.sh
+++ build-utils/rebuild_specification_service_image.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_specification_service ./dataland-specification-service/Dockerfile
+dependencies="./dataland-specification-service ./dataland-inbound-proxy/config/utils"
+
+./build-utils/base_rebuild_single_docker_image.sh dataland_specification_service ./dataland-specification-service/Dockerfile $dependencies
