3994c4ecaafb210b7cbbcb37e217b6aebb8fa93f
Adrian Hess <adrian.hess@d-fine.com>
2024-10-30 13:22:46 +0100

    added a unsubscription mail to stakeholders. Fixed cycle formation of dependencies

--- dataland-email-service/src/main/kotlin/org/dataland/datalandemailservice/controller/EmailController.kt
+++ dataland-email-service/src/main/kotlin/org/dataland/datalandemailservice/controller/EmailController.kt
@@ -1,7 +1,9 @@
 package org.dataland.datalandemailservice.controller
 
 import org.dataland.datalandemailservice.api.EmailApi
+import org.dataland.datalandemailservice.repositories.EmailSubscriptionRepository
 import org.dataland.datalandemailservice.services.EmailSubscriptionService
+import org.dataland.datalandemailservice.services.UnsubscriptionEmailToStakeholdersSender
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
@@ -15,20 +17,33 @@ import java.util.UUID
 @RestController
 class EmailController(
     @Autowired val emailSubscriptionService: EmailSubscriptionService,
+    @Autowired private val emailSubscriptionRepository: EmailSubscriptionRepository,
+    @Autowired private val unsubscriptionEmailToStakeholdersSender: UnsubscriptionEmailToStakeholdersSender,
 ) : EmailApi {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     /**
-     * Unsubscribes a user from email communications based on the provided UUID.
+     * Unsubscribes a user from email communications based on the provided UUID and
+     * sends a mail to the stakeholders that this email address is now unsubscribed.
      *
      * @param subscriptionUuid The UUID of the subscription to be unsubscribed.
      * @return A response entity indicating the result of the unsubscription.
      */
     @Transactional
     override fun unsubscribeUuid(subscriptionUuid: UUID): ResponseEntity<String> {
-        logger.info("Received request to unsubscribe with UUID: $subscriptionUuid")
+        logger.info("Received request to unsubscribe user with UUID: $subscriptionUuid")
 
-        emailSubscriptionService.unsubscribeEmailWithUuid(subscriptionUuid)
+        val emailSubscription = emailSubscriptionRepository.findByUuid(subscriptionUuid)
+
+        if (emailSubscription != null) {
+            emailSubscriptionService.unsubscribeEmailWithUuid(emailSubscription.uuid)
+            logger.info("User with UUID: $subscriptionUuid has been successfully unsubscribed")
+
+            unsubscriptionEmailToStakeholdersSender.sendUnsubscriptionEmail(emailSubscription.emailAddress)
+            logger.info("Stakeholders have been informed that user with UUID: $subscriptionUuid has unsubscribed")
+        } else {
+            logger.info("No user with UUID: $subscriptionUuid exists")
+        }
 
         return ResponseEntity.ok("Successfully unsubscribed")
     }
