438e1d264db07041383b6474be9a5fec2539f90b
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2024-11-06 11:41:09 +0100

    Fix SpringBoot Configuration Issue

--- dataland-qa-service/src/main/kotlin/db/migration/V1__MigrateQaStatusToEnumValues.kt
+++ dataland-qa-service/src/main/kotlin/db/migration/V1__MigrateQaStatusToEnumValues.kt
@@ -1,4 +1,4 @@
-package org.dataland.datalandqaservice.db.migration
+package db.migration
 
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
@@ -10,12 +10,16 @@ import org.flywaydb.core.api.migration.Context
 class V1__MigrateQaStatusToEnumValues : BaseJavaMigration() {
     override fun migrate(context: Context?) {
         context!!.connection.createStatement().execute(
-            "CREATE TYPE qa_status AS ENUM('Pending', 'Accepted', 'Rejected')",
+            "ALTER TABLE review_information " +
+                "ALTER COLUMN qa_status SET DATA TYPE VARCHAR(255) " +
+                "USING qa_status::VARCHAR(255)",
         )
         context.connection.createStatement().execute(
-            "ALTER TABLE review_information " +
-                "ALTER COLUMN qa_status SET DATA TYPE qa_status " +
-                "USING qa_status::qa_status",
+            "UPDATE review_information " +
+                "SET qa_status = CASE " +
+                "WHEN qa_status = '1' THEN 'Accepted' " +
+                "WHEN qa_status = '2' THEN 'Rejected' " +
+                "END;",
         )
     }
 }
