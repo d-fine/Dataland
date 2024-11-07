da90da04971fb41e76bf2d37e37eab030c0f70a0
Laurin Strelow <1918073+LaurinStrelow@users.noreply.github.com>
2024-11-07 14:33:08 +0100

    add converter

---
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/converters/DataTypEnumAttributeConverter.kt
@@ -0,0 +1,20 @@
+package org.dataland.datalandcommunitymanager.converters
+
+import jakarta.persistence.AttributeConverter
+import jakarta.persistence.Converter
+import org.dataland.datalandbackend.openApiClient.model.DataTypeEnum
+
+/**
+ *  This converter is used for database entities to convert the DataType enum to strings when saving to the database.
+ *  and to convert the string to the DataType when loading data from the database.
+ *  As autoApply=true, this should work for all stored DataTypeEnums.
+ */
+@Converter(autoApply = true)
+class DataTypEnumAttributeConverter : AttributeConverter<DataTypeEnum, String> {
+    override fun convertToDatabaseColumn(dataType: DataTypeEnum?): String? = dataType?.value
+
+    override fun convertToEntityAttribute(string: String?): DataTypeEnum? =
+        string?.let {
+            DataTypeEnum.entries.find { it.value == string } ?: throw IllegalArgumentException("Unknown dataType $string")
+        }
+}
