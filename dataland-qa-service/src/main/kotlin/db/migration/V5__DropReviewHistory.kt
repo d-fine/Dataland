fb4aee34afe49f3a7ed15ac81f68da3957221d48
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2024-11-21 21:23:56 +0100

    undo change for test

--- dataland-qa-service/src/main/kotlin/db/migration/V5__DropReviewHistory.kt
+++ dataland-qa-service/src/main/kotlin/db/migration/V5__DropReviewHistory.kt
@@ -4,13 +4,13 @@ import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
 
 /**
- * Drops legacy tables review_information and review_queue containing the review QA history for older datasets
+ * Drops legacy table review_information containing the review QA history for older datasets
  */
 @Suppress("ClassName")
 class V5__DropReviewHistory : BaseJavaMigration() {
     override fun migrate(context: Context?) {
-        val statement = context!!.connection.createStatement()
-        statement.execute("DROP TABLE review_information;")
-        statement.execute("DROP TABLE review_queue;")
+        context!!.connection.createStatement().execute(
+            "DROP TABLE review_information;",
+        )
     }
 }
