fcb539639bc07b9cee9b19f26be939a07a8d3ca8
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2024-11-21 21:44:36 +0100

    Drop Review Queue

--- dataland-qa-service/src/main/kotlin/db/migration/V5__DropReviewHistoryAndQueue.kt
+++ dataland-qa-service/src/main/kotlin/db/migration/V5__DropReviewHistoryAndQueue.kt
@@ -7,10 +7,14 @@ import org.flywaydb.core.api.migration.Context
  * Drops legacy table review_information containing the review QA history for older datasets
  */
 @Suppress("ClassName")
-class V5__DropReviewHistory : BaseJavaMigration() {
+class V5__DropReviewHistoryAndQueue : BaseJavaMigration() {
     override fun migrate(context: Context?) {
         context!!.connection.createStatement().execute(
             "DROP TABLE review_information;",
+        )
+
+        context.connection.createStatement().execute(
+            "DROP TABLE review_queue;",
         )
     }
 }
