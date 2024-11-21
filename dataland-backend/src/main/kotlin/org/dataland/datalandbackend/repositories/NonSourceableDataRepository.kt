d8d773ef0134b9a233a6d1d2eb46804e5e5b569c
Julia Cialkowski <julia.cialkowski@d-fine.de>
2024-11-21 16:01:28 +0100

    non sourceable data repo.

---
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/repositories/NonSourceableDataRepository.kt
@@ -0,0 +1,27 @@
+package org.dataland.datalandbackend.repositories
+
+import org.dataland.datalandbackend.entities.NonSourceableEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+/**
+ * A JPA repository for accessing NonSourceableEntities
+ */
+interface NonSourceableDataRepository : JpaRepository<NonSourceableEntity, String> {
+    /**
+     * Retrieves non sourceable datasets for the given triplet of reporting Period, company and dataType
+     */
+    @Query(
+        "SELECT nonSourceableData FROM NonSourceableEntity nonSourceableData " +
+            "WHERE nonSourceableData.reportingPeriod = :#{#reportingPeriod} " +
+            "AND nonSourceableData.companyId = :#{#companyId} " +
+            "AND nonSourceableData.dataType = :#{#dataType} " +
+            "AND nonSourceableData.nonSourceable = true ",
+    )
+    fun getNonSourceableDataByTriple(
+        @Param("companyId") companyId: String,
+        @Param("dataType") dataType: String,
+        @Param("reportingPeriod") reportingPeriod: String,
+    )
+}
