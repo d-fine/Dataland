23aa369273c0a0546eaff2249ab59734ada32b2e
Julia Cialkowski <julia.cialkowski@d-fine.de>
2024-12-02 16:49:20 +0100

    extended tests.

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/NonSourceableDataManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/NonSourceableDataManagerTest.kt
@@ -8,16 +8,24 @@ import org.dataland.datalandbackend.model.metainformation.NonSourceableInfo
 import org.dataland.datalandbackend.repositories.NonSourceableDataRepository
 import org.dataland.datalandbackendutils.exceptions.ResourceNotFoundApiException
 import org.dataland.datalandmessagequeueutils.cloudevents.CloudEventMessageHandler
+import org.dataland.keycloakAdapter.auth.DatalandRealmRole
+import org.dataland.keycloakAdapter.utils.AuthenticationMock
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doNothing
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.test.annotation.DirtiesContext
 import java.time.Instant
 
@@ -78,5 +86,71 @@ class NonSourceableDataManagerTest(
             "Dataland does not know the company ID $nonExistingCompanyId",
             thrown.message,
         )
+    }
+
+    @Test
+    fun `check that processSourceabilityDataStorageRequest does store an NonSourceableEntity when no dataMetaInfo exists`() {
+        val mockCompanyQueryManager = mock(CompanyQueryManager::class.java)
+        val expectedSetOfRolesForUploader =
+            setOf(DatalandRealmRole.ROLE_UPLOADER)
+        mockSecurityContext("testUserId", expectedSetOfRolesForUploader)
+
+        nonSourceableDataManager =
+            NonSourceableDataManager(
+                cloudEventMessageHandler = mockCloudEventMessageHandler,
+                objectMapper = objectMapper,
+                dataMetaInformationManager = mockDataMetaInformationManager,
+                nonSourceableDataRepository = nonSourceableDataRepository,
+                companyQueryManager = mockCompanyQueryManager,
+            )
+        val nonSourceableInfo =
+            NonSourceableInfo(
+                companyId = "existingCompanyId",
+                dataType = DataType("eutaxonomy-financials"),
+                reportingPeriod = "2023",
+                isNonSourceable = true,
+                reason = "Test reason",
+            )
+        doNothing()
+            .whenever(mockCompanyQueryManager)
+            .verifyCompanyIdExists(nonSourceableInfo.companyId)
+        whenever(
+            mockDataMetaInformationManager.searchDataMetaInfo(
+                companyId = nonSourceableInfo.companyId,
+                dataType = nonSourceableInfo.dataType,
+                showOnlyActive = false,
+                reportingPeriod = nonSourceableInfo.reportingPeriod,
+                uploaderUserIds = null,
+                qaStatus = null,
+            ),
+        ).thenReturn(emptyList())
+        nonSourceableDataManager.processSourceabilityDataStorageRequest(nonSourceableInfo)
+
+        verify(mockDataMetaInformationManager).searchDataMetaInfo(
+            companyId = nonSourceableInfo.companyId,
+            dataType = nonSourceableInfo.dataType,
+            showOnlyActive = false,
+            reportingPeriod = nonSourceableInfo.reportingPeriod,
+            uploaderUserIds = null,
+            qaStatus = null,
+        )
+        verify(mockCloudEventMessageHandler).buildCEMessageAndSendToQueue(
+            any(), any(), any(), any(), any(),
+        )
+    }
+
+    private fun mockSecurityContext(
+        userId: String,
+        roles: Set<DatalandRealmRole>,
+    ) {
+        val mockAuthentication =
+            AuthenticationMock.mockJwtAuthentication(
+                "mocked_uploader",
+                userId,
+                roles,
+            )
+        val mockSecurityContext = Mockito.mock(SecurityContext::class.java)
+        Mockito.`when`(mockSecurityContext.authentication).thenReturn(mockAuthentication)
+        SecurityContextHolder.setContext(mockSecurityContext)
     }
 }
