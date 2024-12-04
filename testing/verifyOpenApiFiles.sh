c910f287dd765393ef85eb99ab36097aac69f807
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2024-12-04 15:12:01 +0100

    fix test

--- testing/verifyOpenApiFiles.sh
+++ testing/verifyOpenApiFiles.sh
@@ -8,17 +8,20 @@ find . -name "*OpenApi.json" -type f -exec bash -c 'jq -S . $1 > $1.formatted-be
 find . -name "*OpenApi.json" -type f -exec bash -c 'jq -S . $1 > $1.formatted-after.json' shell {} \;
 
 error=0
+broken_files=""
 while IFS= read -r -d '' file
 do
   hash_before=$(sha1sum "$file" | awk '{ print $1 }' )
   hash_after=$(sha1sum "${file/.formatted-before.json/.formatted-after.json}" | awk '{ print $1 }' )
   if [[ "$hash_before" != "$hash_after" ]]; then
     echo "OpenApi Files do not match for $file!"
-    ((error++))
+    error=$((error+1))
+    broken_files+="$file "
   fi
 done < <(find . -name "*OpenApi.json.formatted-before.json" -type f -print0)
 
 if [[ $error -ne 0 ]]; then
-  echo "There are $error OpenApi files that do not match!"
+  echo "There are $error OpenApi files that do not match: $broken_files."
+
   exit 1
 fi
