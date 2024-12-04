f44c2fe2ba79755e9df399594b9a732fa04d73dc
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2024-12-04 22:07:20 +0100

    fix test config

--- dataland-qa-service/src/test/kotlin/org/dataland/datalandqaservice/utils/TestJwtSecurityConfig.kt
+++ dataland-qa-service/src/test/kotlin/org/dataland/datalandqaservice/utils/TestJwtSecurityConfig.kt
@@ -1,6 +1,5 @@
 package org.dataland.datalandqaservice.utils
 
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,7 +9,7 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 @Configuration
 class TestJwtSecurityConfig {
     @Bean
-    fun jwtDecoder(
-        @Qualifier("oAuth2ResourceServerProperties") properties: OAuth2ResourceServerProperties,
-    ): JwtDecoder = NimbusJwtDecoder.withJwkSetUri(properties.jwt.jwkSetUri).build()
+    fun jwtDecoder(properties: OAuth2ResourceServerProperties): JwtDecoder {
+        return NimbusJwtDecoder.withJwkSetUri(properties.jwt.jwkSetUri).build()
+    }
 }
