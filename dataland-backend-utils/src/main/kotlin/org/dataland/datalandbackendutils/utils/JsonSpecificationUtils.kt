07e7b3fbe9557c509aa032a69befec9f2a2db276
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2024-12-10 14:52:20 +0100

    Implemented specification hydration and dehydration in the dataland-backend utils.

---
+++ dataland-backend-utils/src/main/kotlin/org/dataland/datalandbackendutils/utils/JsonSpecificationUtils.kt
@@ -0,0 +1,107 @@
+package org.dataland.datalandbackendutils.utils
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import com.fasterxml.jackson.databind.node.ObjectNode
+import org.dataland.datalandbackendutils.exceptions.InvalidInputApiException
+
+data class JsonSpecificationLeaf(
+    val dataPointId: String,
+    val jsonPath: String,
+    val content: JsonNode
+)
+
+/**
+ * Utilities for working with JSON framework specifications.
+ */
+object JsonSpecificationUtils {
+    private fun isTerminalNode(node: ObjectNode): Boolean {
+        return node.fieldNames().asSequence().toSet() == setOf("id", "ref")
+    }
+
+    /**
+     * Hydrates a JSON specification with data from a data function. During hydration, leaf nodes of the
+     * specification are replaced with data requested from the data function.
+     * @param jsonSpecification The JSON specification to hydrate (from the specification service)
+     * @param dataFunction A function that takes a dataPointId and returns the data for that key
+     * @return The hydrated JSON specification (i.e., the constructed dataset)
+     */
+    fun hydrateJsonSpecification(jsonSpecification: ObjectNode, dataFunction: (String) -> JsonNode?): JsonNode? {
+        return hydrateJsonSpecificationRecursive(jsonSpecification, dataFunction)
+    }
+
+    private fun hydrateJsonSpecificationRecursive(
+        currentSpecificationNode: ObjectNode,
+        dataFunction: (String) -> JsonNode?
+    ): JsonNode? {
+        if (isTerminalNode(currentSpecificationNode)) {
+            val dataPointId = currentSpecificationNode.get("id").asText()
+            return dataFunction(dataPointId)
+        } else {
+            val returnNode = JsonNodeFactory.instance.objectNode()
+            var allNull = true
+            for ((fieldName, childSpecificationNode) in currentSpecificationNode.fields()) {
+                require(childSpecificationNode is ObjectNode) {
+                    "Specification node must be an object node"
+                }
+                val hydratedChild = hydrateJsonSpecificationRecursive(childSpecificationNode, dataFunction)
+                if (hydratedChild != null) {
+                    allNull = false
+                    returnNode.set<JsonNode>(fieldName, hydratedChild)
+                }
+            }
+            return if (allNull) null else returnNode
+        }
+    }
+
+    /**
+     * Dehydrates a JSON specification into a map of dataPointIds to data. During dehydration, the data
+     * from the data object is extracted and stored in a map. Unknown keys cause an exception.
+     * @param jsonSpecification The JSON specification to dehydrate (from the specification service)
+     * @param dataObject The data object to dehydrate
+     * @return A map of dataPointIds to data
+     */
+    fun dehydrateJsonSpecification(jsonSpecification: ObjectNode, dataObject: ObjectNode): Map<String, JsonSpecificationLeaf> {
+        val returnMap = mutableMapOf<String, JsonSpecificationLeaf>()
+        dehydrateJsonSpecificationRecursive("", jsonSpecification, dataObject, returnMap)
+        return returnMap
+    }
+
+
+
+    private fun dehydrateJsonSpecificationRecursive(
+        currentPath: String,
+        currentSpecificationNode: ObjectNode,
+        currentDataNode: JsonNode,
+        dataMap: MutableMap<String, JsonSpecificationLeaf>
+    ) {
+        if (isTerminalNode(currentSpecificationNode)) {
+            val dataPointId = currentSpecificationNode.get("id").asText()
+            dataMap[dataPointId] = JsonSpecificationLeaf(dataPointId, currentPath, currentDataNode)
+        } else {
+            if (currentDataNode !is ObjectNode) {
+                throw InvalidInputApiException(
+                    summary = "Data object does not match specification",
+                    "Unexpected leaf at JSON path: $currentPath"
+                )
+            }
+            for ((fieldName, jsonNode) in currentDataNode.fields()) {
+                val matchingSpecificationNode = currentSpecificationNode.get(fieldName)
+                    ?: throw InvalidInputApiException(
+                    summary = "Data object does not match specification",
+                    "Unexpected field at JSON path: $currentPath.$fieldName"
+                )
+                require(matchingSpecificationNode is ObjectNode) {
+                    "Specification node must be an object node"
+                }
+                dehydrateJsonSpecificationRecursive(
+                    currentPath="$currentPath.$fieldName",
+                    currentSpecificationNode=matchingSpecificationNode,
+                    currentDataNode=jsonNode,
+                    dataMap=dataMap
+                )
+            }
+
+        }
+    }
+}
---
+++ dataland-backend-utils/src/test/kotlin/org/dataland/datalandbackendutils/services/utils/JsonSpecificationUtilsTest.kt
@@ -0,0 +1,101 @@
+package org.dataland.datalandbackendutils.services.utils
+
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.dataland.datalandbackendutils.exceptions.InvalidInputApiException
+import org.dataland.datalandbackendutils.utils.JsonSpecificationUtils
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class JsonSpecificationUtilsTest {
+
+    private val objectmapper = jacksonObjectMapper()
+    private val demoSpecification = objectmapper.readTree("""
+        {
+            "field": {
+                "id": "field1",
+                "ref": "field1"
+            },
+            "group": {
+                "subgroup": {
+                    "subgroupfield": {
+                        "id": "subgroupfield",
+                        "ref": "subgroupfield"
+                    }
+                },
+                "groupfield": {
+                    "id": "groupfield",
+                    "ref": "groupfield"
+                }
+            }
+        }
+    """) as ObjectNode
+
+    private val demoValueMap = mapOf(
+        "field1" to objectmapper.readTree("""{"value": "field1"}"""),
+        "groupfield" to objectmapper.readTree("12"),
+        "subgroupfield" to objectmapper.readTree("\"test\"")
+    )
+    @Test
+    fun `hydration should work for a simple specification`() {
+        val hydrated = JsonSpecificationUtils.hydrateJsonSpecification(demoSpecification) { demoValueMap[it] }
+        val expected = objectmapper.readTree("""
+            {
+                "field": {"value": "field1"},
+                "group": {
+                    "subgroup": {"subgroupfield": "test"},
+                    "groupfield": 12
+                }
+            }
+        """.trimIndent())
+        assertEquals(expected, hydrated)
+    }
+
+    @Test
+    fun `hydration should omit empty subgroups`() {
+        val hydrated = JsonSpecificationUtils.hydrateJsonSpecification(demoSpecification) {
+            if (it == "subgroupfield") null else demoValueMap[it]
+        }
+        val expected = objectmapper.readTree("""
+            {
+                "field": {"value": "field1"},
+                "group": {
+                    "groupfield": 12
+                }
+            }
+        """.trimIndent())
+        assert(hydrated == expected)
+    }
+
+    @Test
+    fun `dehydration should work for a simple specification`() {
+        val hydrated = JsonSpecificationUtils.hydrateJsonSpecification(demoSpecification) { demoValueMap[it] }
+        val dehydrated = JsonSpecificationUtils.dehydrateJsonSpecification(demoSpecification, hydrated as ObjectNode)
+        val transformedMap = dehydrated.mapValues { it.value.content }
+        assertEquals(demoValueMap, transformedMap)
+    }
+
+    @Test
+    fun `dehydration should work for a simple specification with missing values`() {
+        val partialValueMap = demoValueMap.toMutableMap()
+        partialValueMap.remove("groupfield")
+        val hydrated = JsonSpecificationUtils.hydrateJsonSpecification(demoSpecification) { partialValueMap[it] }
+        val dehydrated = JsonSpecificationUtils.dehydrateJsonSpecification(demoSpecification, hydrated as ObjectNode)
+        val transformedMap = dehydrated.mapValues { it.value.content }
+        assertEquals(partialValueMap, transformedMap)
+    }
+
+    @Test
+    fun `dehydration should fail on unexpected inputs`() {
+        val unexpectedInput = objectmapper.readTree("""
+            {
+                "unexpectedGreeting": "Hello from Marc ;)"
+            }
+        """.trimIndent())
+        val exception = assertThrows<InvalidInputApiException> {
+            JsonSpecificationUtils.dehydrateJsonSpecification(demoSpecification, unexpectedInput as ObjectNode)
+        }
+        assert(exception.message.contains("unexpectedGreeting"))
+    }
+}
