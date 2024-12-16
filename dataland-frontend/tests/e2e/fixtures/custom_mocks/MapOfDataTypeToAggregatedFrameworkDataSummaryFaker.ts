96178e5538d77cd4247a98c6e444e8b973de2816
hessad <hessad@protonmail.ch>
2024-12-16 11:13:15 +0100

    fixed fake fixtures

--- dataland-frontend/tests/e2e/fixtures/custom_mocks/MapOfDataTypeToAggregatedFrameworkDataSummaryFaker.ts
+++ dataland-frontend/tests/e2e/fixtures/custom_mocks/MapOfDataTypeToAggregatedFrameworkDataSummaryFaker.ts
@@ -1,5 +1,5 @@
 import { type AggregatedFrameworkDataSummary } from '@clients/backend';
-import { AggregatedDataRequestDataTypeEnum } from '@clients/communitymanager';
+import { GetAggregatedOpenDataRequestsDataTypesEnum } from '@clients/communitymanager';
 import { generateInt } from '@e2e/fixtures/common/NumberFixtures';
 
 /**
@@ -14,7 +14,7 @@ export function generateMapOfFrameworkNameToAggregatedFrameworkDataSummary(): Re
     string,
     { numberOfProvidedReportingPeriods: number }
   > = {};
-  Object.values(AggregatedDataRequestDataTypeEnum).forEach((frameworkName) => {
+  Object.values(GetAggregatedOpenDataRequestsDataTypesEnum).forEach((frameworkName) => {
     mapOfFrameworkNameToAggregatedFrameworkDataSummary[frameworkName] = {
       numberOfProvidedReportingPeriods: generateInt(30),
     };
