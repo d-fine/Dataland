8026a4714301c7a022aa3518a869bc65218b8754
hessad <hessad@protonmail.ch>
2024-12-17 11:26:58 +0100

    added migration script for request priorities

---
+++ dataland-community-manager/src/main/kotlin/db/migration/V12__MigrateRequestPriorities.kt
@@ -0,0 +1,23 @@
+package db.migration
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+/**
+ * This migration script migrates the request priorities in the following way:
+ * Normal -> Low
+ * Very High -> High
+ * all other request priorites remain unchanged
+ */
+@Suppress("ClassName")
+class V12__MigrateRequestPriorities : BaseJavaMigration() {
+    override fun migrate(context: Context?) {
+        context!!.connection.createStatement().execute(
+            "UPDATE data_requests " +
+                "SET request_priority = CASE " +
+                "WHEN request_priority = 'Normal' THEN 'Low' " +
+                "WHEN request_priority = 'Very High' THEN 'High' " +
+                "ELSE request_priority END",
+        )
+    }
+}
