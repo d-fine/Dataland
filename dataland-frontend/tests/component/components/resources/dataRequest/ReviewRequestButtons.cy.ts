1caaa778a4dbca56f388e67ebd7f34cc32a233bf
Stephan Wezorke <186339469+StephanWezorke@users.noreply.github.com>
2024-12-17 16:06:22 +0100

    Fix component test of ReviewRequestButtons

--- dataland-frontend/tests/component/components/resources/dataRequest/ReviewRequestButtons.cy.ts
+++ dataland-frontend/tests/component/components/resources/dataRequest/ReviewRequestButtons.cy.ts
@@ -101,13 +101,13 @@ describe('Component tests for the data request review buttons', function (): voi
     );
     assert(requestFor2022 !== undefined);
 
-    cy.intercept(`**/community/requests/${requestFor2022!.dataRequestId}`, {
+    cy.intercept("GET",`**/community/requests/${requestFor2022!.dataRequestId}`, {
       body: {
         messageHistory: messageHistory,
       },
       status: 200,
     }).as('fetchSingleDataRequests');
-    cy.intercept(`**/community/requests/user`, {
+    cy.intercept("GET",`**/community/requests/user`, {
       body: mockedRequests,
     }).as('fetchUserRequests');
   }
@@ -115,18 +115,25 @@ describe('Component tests for the data request review buttons', function (): voi
    * Mocks the answer for patching the request status
    */
   function interceptPatchRequestsOnMounted(): void {
-    cy.intercept(`**/requestStatus?requestStatus=Resolved`, {
-      body: {
-        requestStatus: RequestStatus.Resolved,
-      } as StoredDataRequest,
-      status: 200,
-    }).as('closeUserRequest');
-    cy.intercept(`**/requestStatus?requestStatus=Open**`, {
-      body: {
-        requestStatus: RequestStatus.Open,
-      } as StoredDataRequest,
-      status: 200,
-    }).as('reOpenUserRequest');
+    cy.intercept("PATCH", "**/community/requests/*", (request) => {
+      if (request.body.requestStatus === "Resolved") {
+        request.alias = "closeUserRequest";
+        request.reply({
+          body: {
+            requestStatus: RequestStatus.Resolved,
+          } as StoredDataRequest,
+          status: 200,
+        })
+      } else if (request.body.requestStatus === "Open") {
+        request.alias = "reOpenUserRequest";
+        request.reply({
+          body: {
+            requestStatus: RequestStatus.Open,
+          } as StoredDataRequest,
+          status: 200,
+        })
+      }
+    });
   }
   /**
    * Mount review request button component with given props
