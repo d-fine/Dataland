75f890aa6b3cc2911f56a7eb76592bb902188ef3
hessad <hessad@protonmail.ch>
2024-12-19 08:33:51 +0100

    use definition of keycloak api document. Cron Job 8 utc

--- dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
+++ dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
@@ -135,7 +135,7 @@ class ProcessDataUpdates(
     }
 
     @Suppress("UnusedPrivateMember") // Detect does not recognise the scheduled execution of this function
-    @Scheduled(cron = "0 0 2 * * *")
+    @Scheduled(cron = "0 0 8 * * *")
     private fun processRequestPriorityUpdates() {
         logger.info("Running scheduled update of request priorities.")
         waitForCommunityManager()
