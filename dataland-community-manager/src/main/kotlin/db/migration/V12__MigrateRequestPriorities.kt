7220587e9a1463513be143bc5f101df3e5c3605d
Stephan Wezorke <186339469+StephanWezorke@users.noreply.github.com>
2025-01-03 15:32:12 +0100

    Fix db migration script

--- dataland-community-manager/src/main/kotlin/db/migration/V12__MigrateRequestPriorities.kt
+++ dataland-community-manager/src/main/kotlin/db/migration/V12__MigrateRequestPriorities.kt
@@ -15,9 +15,20 @@ class V12__MigrateRequestPriorities : BaseJavaMigration() {
         context!!.connection.createStatement().execute(
             "UPDATE data_requests " +
                 "SET request_priority = CASE " +
-                "WHEN request_priority = 'Normal' AND request_status = 'Open' THEN 'Low' " +
-                "WHEN request_priority = 'Very High' AND request_status = 'Open' THEN 'High' " +
-                "ELSE request_priority END",
+                "WHEN request_priority = 'Normal' THEN 'Low' " +
+                "WHEN request_priority = 'Very High' THEN 'High' " +
+                "ELSE request_priority END" +
+                "WHERE data_request_id IN (" +
+                "SELECT H1.data_request_id" +
+                "FROM data_requests AS R" +
+                "INNER JOIN request_status_history AS H1" +
+                "ON R.data_request_id = H1.data_request_id" +
+                "LEFT JOIN request_status_history AS H2" +
+                "ON (R.data_request_id = H2.data_request_id)" +
+                "AND (H1.creation_timestamp < H2.creation_timestamp)" +
+                "WHERE (H2.data_request_id IS NULL)" +
+                "AND (H1.request_status = 'Open')" +
+                ")",
         )
     }
 }
