3c6c4c8a2973f7066f541d6ae161c623f08cb5ba
Adrian Hess <99552863+hessad@users.noreply.github.com>
2025-01-06 15:19:57 +0100

    DALA-5211 Automate Setting of Request Priorities (#869)
    Dala-5211: automated setting of request priorities

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/entities/AggregatedDataRequest.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/entities/AggregatedDataRequest.kt
@@ -7,12 +7,15 @@ package org.dataland.datalandcommunitymanager.entities
  * @param reportingPeriod
  * @param datalandCompanyId contains the compandID sotred on dataland
  * @param requestStatus contains the request status for which the count is valid
+ * @param priority contains the request priority for which the count is valid
+ * @param requestStatus contains the request status for which the count is valid
  * @param count the count of existing data requests for this framework, identifierType and identifierValue
  */
 interface AggregatedDataRequest {
     val dataType: String
     val reportingPeriod: String
     val datalandCompanyId: String
+    val priority: String
     val requestStatus: String
     val count: Long
 }
