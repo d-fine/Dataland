062fd21110a37447e1d1235b2fafe89259047cd2
Antje Neumann <103188513+antjedf@users.noreply.github.com>
2025-01-13 13:10:59 +0100

    WIP health check for alloy, loki, grafana

--- build-utils/rebuild_grafana_image.sh
+++ build-utils/rebuild_grafana_image.sh
@@ -2,6 +2,6 @@
 set -euxo pipefail
 
 echo "{\"commit\":\"$(git rev-parse HEAD)\",\"time\":\"$(git show -s --format=%aI HEAD)\",\"branch\":\"$(git rev-parse --abbrev-ref HEAD)\"}" > gitinfo
-dependencies="./dataland-grafana/ ./gitinfo ./environments/.env.uncritical ./versions.properties /etc/grafana"
+dependencies="./dataland-grafana/ ./gitinfo ./environments/.env.uncritical ./versions.properties"
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_grafana ./dataland-grafana/Dockerfile $dependencies
