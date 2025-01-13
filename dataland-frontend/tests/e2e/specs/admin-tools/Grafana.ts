d2f3c54b7fd009e66a8e02241be8482d86c62c8d
Antje Neumann <103188513+antjedf@users.noreply.github.com>
2025-01-13 15:02:08 +0100

    Improve Grafana E2E test

--- dataland-frontend/tests/e2e/specs/admin-tools/Grafana.ts
+++ dataland-frontend/tests/e2e/specs/admin-tools/Grafana.ts
@@ -1,11 +1,15 @@
 import { getStringCypressEnv } from '@e2e/utils/Cypress';
 
 describe('As a developer, I expect Grafana to be available to me', () => {
-  it('Checks if Grafana is available and the login page is shown', () => {
+  it('Checks if Grafana is available loki logs are shown', () => {
     cy.visit('http://dataland-admin:6789/grafana');
-    cy.get('input[name=email]').should('exist').type('admin@dataland.com');
+    cy.get('input[name=user]').should('exist').type('admin@dataland.com');
     cy.get('input[name=password]').should('exist').type(getStringCypressEnv('GRAFANA_PASSWORD'));
-    cy.get('button[name=internal_button]').should('contain.text', 'Log in').click();
-    cy.get('span[class=file-name]').should('contain.text', 'Grafana');
+    cy.get('button[data-testid="data-testid Login button"]').should('contain.text', 'Log in').click();
+    cy.get('[data-testid="data-testid panel content"]').should('contain.text', 'Grafana');
+    cy.get('button[aria-label="Expand section Explore"]').should('exist').click();
+    cy.get('[data-testid="data-testid Nav menu item"]').contains('Logs').click();
+    cy.get('[data-testid="header-container"]').should('contain.text', 'dataland-');
+    cy.get('[data-testid="data-testid panel content"]').should('exist');
   });
 });
