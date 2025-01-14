167dab6363903c51b37db4c93c300f7852742eac
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2025-01-14 09:32:04 +0100

    Fix description of E2E tests

--- dataland-frontend/tests/e2e/specs/data_download/index.ts
+++ dataland-frontend/tests/e2e/specs/data_download/index.ts
@@ -1,6 +1,6 @@
 /**
  * Data Download Tests
  */
-describe('As a user, I want to be able to download data via the frontend', () => {
+describe('Data Export Tests', () => {
   require('./DataDownload');
 });
