a1a22c847f66b639ef9bbbf29867f372ac84ff86
juliacialkowski <166627669+juliacialkowski@users.noreply.github.com>
2025-01-15 11:23:09 +0100

    Dala 418 data download (#872)
    DALA-418  Data Download as CSV or as JSON 
    ---------
    Co-authored-by: Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
    Co-authored-by: Dr. Saskia Zailskas <saskia.zailskas@outlook.de>
    Co-authored-by: SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>

---
+++ dataland-backend-utils/src/main/kotlin/org/dataland/datalandbackendutils/model/ExportFileType.kt
@@ -0,0 +1,21 @@
+package org.dataland.datalandbackendutils.model
+
+import io.swagger.v3.oas.annotations.media.Schema
+import org.springframework.http.MediaType
+import org.springframework.util.MimeType
+
+/**
+ * Enum used for defining file types allowed for exporting datasets
+ */
+@Schema(
+    enumAsRef = true,
+)
+enum class ExportFileType(
+    val fileExtension: String,
+    val mediaType: MediaType = MediaType.APPLICATION_OCTET_STREAM,
+    val description: String = "",
+) {
+    CSV("csv", MediaType.asMediaType(MimeType.valueOf("text/csv")), "Comma-separated Values"),
+    EXCEL("xlsx", MediaType.asMediaType(MediaType.valueOf("application/vnd.ms-excel")), "Excel File"),
+    JSON("json", MediaType.APPLICATION_JSON, "JavaScript Object Notation"),
+}
---
+++ dataland-frontend/src/components/general/DownloadDatasetModal.vue
@@ -0,0 +1,181 @@
+<template>
+  <PrimeDialog
+    v-model:visible="isModalVisible"
+    style="text-align: center; width: 20%"
+    header="Download dataset"
+    :show-header="true"
+    :closable="true"
+    :dismissable-mask="true"
+    :modal="true"
+    :close-on-escape="true"
+    data-test="downloadModal"
+    @after-hide="closeDialog"
+  >
+    <FormKit type="form" class="formkit-wrapper" :actions="false">
+      <label for="reportingYearSelector">
+        <b style="margin-bottom: 8px; font-weight: normal">Reporting year</b>
+      </label>
+      <FormKit
+        v-model="selectedReportingPeriod"
+        type="select"
+        name="reportingYearSelector"
+        data-test="reportingYearSelector"
+        :options="reportingPeriods"
+        placeholder="Select a reporting year"
+      />
+      <p v-show="showReportingPeriodError" class="text-danger text-xs" data-test="reportingYearError">
+        Please select a reporting period.
+      </p>
+      <label for="formatSelector">
+        <b style="margin-bottom: 8px; margin-top: 5px; font-weight: normal">File format</b>
+      </label>
+      <FormKit
+        v-model="selectedFileFormat"
+        type="select"
+        name="formatSelector"
+        data-test="formatSelector"
+        :options="fileTypeSelectionOptions"
+        placeholder="Select a file format"
+      />
+      <p v-show="showFileFormatError" class="text-danger text-xs" data-test="fileFormatError">
+        Please select a file format.
+      </p>
+    </FormKit>
+
+    <div>
+      <PrimeButton
+        data-test="downloadDataButtonInModal"
+        @click="onDownloadButtonClick()"
+        style="width: 100%; justify-content: center"
+        label="DOWNLOAD"
+        class="d-letters"
+      >
+      </PrimeButton>
+    </div>
+  </PrimeDialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue';
+import PrimeDialog from 'primevue/dialog';
+import PrimeButton from 'primevue/button';
+import type { DataMetaInformation } from '@clients/backend';
+import { ExportFileTypes } from '@/types/ExportFileTypes.ts';
+
+export default defineComponent({
+  components: { PrimeDialog, PrimeButton },
+  name: 'DownloadDatasetModal',
+  props: {
+    isDownloadModalOpen: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    /**
+     * mapOfReportingPeriodToActiveDataset is filled if the ViewFrameworkBase component displays multiple datasets.
+     * If ViewFrameworkBase is used to display a single dataset, singleDataMetaInfoToDisplay is populated instead.
+     */
+    mapOfReportingPeriodToActiveDataset: {
+      type: Map as PropType<Map<string, DataMetaInformation>>,
+      required: true,
+    },
+    singleDataMetaInfoToDisplay: {
+      type: Object as PropType<DataMetaInformation>,
+    },
+  },
+  emits: ['closeDownloadModal', 'downloadDataset'],
+  data() {
+    return {
+      reportingPeriods: [] as Array<string>,
+      exportFileTypes: [ExportFileTypes.CsvFile, ExportFileTypes.ExcelFile, ExportFileTypes.JsonFile],
+      selectedReportingPeriod: '',
+      selectedFileFormat: '',
+      isModalVisible: false,
+      showReportingPeriodError: false,
+      showFileFormatError: false,
+    };
+  },
+
+  computed: {
+    fileTypeSelectionOptions() {
+      return Object.values(this.exportFileTypes).map((fileType) => ({
+        value: fileType.identifier,
+        label: `${fileType.description} (.${fileType.fileExtension})`,
+      }));
+    },
+  },
+
+  watch: {
+    isDownloadModalOpen(newValue: boolean): void {
+      this.isModalVisible = newValue;
+    },
+    mapOfReportingPeriodToActiveDataset() {
+      this.updateReportingPeriods();
+    },
+    singleDataMetaInfoToDisplay() {
+      this.updateReportingPeriods();
+    },
+    selectedReportingPeriod() {
+      this.showReportingPeriodError = false;
+    },
+    selectedFileFormat() {
+      this.showFileFormatError = false;
+    },
+  },
+
+  methods: {
+    /**
+     * Update the reportingPeriods whenever the DataMetaInformation has finished loading and the passed prop
+     * 'mapOfReportingPeriodToActiveDataset' is filled
+     */
+    updateReportingPeriods() {
+      if (this.singleDataMetaInfoToDisplay) {
+        this.reportingPeriods = [this.singleDataMetaInfoToDisplay.reportingPeriod];
+      } else {
+        this.reportingPeriods = Array.from(this.mapOfReportingPeriodToActiveDataset.keys()).sort();
+      }
+    },
+
+    /**
+     * Handle the clickEvent of the Download Button
+     */
+    onDownloadButtonClick() {
+      this.checkIfShowErrors();
+
+      if (this.showReportingPeriodError || this.showFileFormatError) {
+        return;
+      }
+
+      this.$emit('downloadDataset', this.selectedReportingPeriod, this.selectedFileFormat);
+      this.closeDialog();
+    },
+
+    /**
+     * Validates the form's fields and displays errors if necessary
+     */
+    checkIfShowErrors() {
+      this.showReportingPeriodError = this.selectedReportingPeriod.length === 0;
+      this.showFileFormatError = this.selectedFileFormat.length === 0;
+    },
+
+    /**
+     * Resets selections and error messages and closes the download modal
+     */
+    closeDialog() {
+      this.resetProps();
+      this.isModalVisible = false;
+      this.$emit('closeDownloadModal');
+    },
+
+    /**
+     * Resets the props, e.g. the selections and error messages.
+     */
+    resetProps() {
+      this.selectedReportingPeriod = '';
+      this.selectedFileFormat = '';
+      this.showReportingPeriodError = false;
+      this.showFileFormatError = false;
+    },
+  },
+});
+</script>
