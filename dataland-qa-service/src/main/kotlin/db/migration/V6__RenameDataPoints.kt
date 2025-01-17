2023b90acbf9626caefaa7831081292716ed4aa3
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2025-01-17 08:39:26 +0100

    add logging

--- dataland-qa-service/src/main/kotlin/db/migration/V6__RenameDataPoints.kt
+++ dataland-qa-service/src/main/kotlin/db/migration/V6__RenameDataPoints.kt
@@ -2,17 +2,21 @@ package org.dataland.datalandqaservice.db.migration
 
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
+import org.slf4j.LoggerFactory
 
 /**
  * Change column names to new version
  */
 @Suppress("ClassName")
-class V6__RenameDataPointColumns : BaseJavaMigration() {
+class V6__RenameDataPoints : BaseJavaMigration() {
+    private val logger = LoggerFactory.getLogger("Migration V6")
     override fun migrate(context: Context?) {
+        logger.info("Renaming data point columns")
         val connection = context!!.connection
         val resultSetReports = connection.metaData.getTables(null, null, "data_point_qa_reports", null)
 
         if (resultSetReports.next()) {
+            logger.info("Found table data_point_qa_reports")
             connection.createStatement().execute(
                 "ALTER TABLE data_point_qa_reports RENAME COLUMN data_id TO data_point_id;" +
                     "ALTER TABLE data_point_qa_reports RENAME COLUMN data_point_identifier TO data_point_type;",
@@ -20,6 +24,7 @@ class V6__RenameDataPointColumns : BaseJavaMigration() {
         }
         val resultSetReview = connection.metaData.getTables(null, null, "data_point_qa_review", null)
         if (resultSetReview.next()) {
+            logger.info("Found table data_point_qa_review")
             connection.createStatement().execute(
                 "ALTER TABLE data_point_qa_review RENAME COLUMN data_id TO data_point_id;" +
                     "ALTER TABLE data_point_qa_review RENAME COLUMN data_point_identifier TO data_point_type;",
