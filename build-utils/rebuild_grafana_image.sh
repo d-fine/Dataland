6638dd0e1c5c373c97322e8803a4e33bfb6fb419
Antje Neumann <103188513+antjedf@users.noreply.github.com>
2025-01-20 13:27:55 +0100

    remove gitinfo entry from new rebuild scripts

--- build-utils/rebuild_grafana_image.sh
+++ build-utils/rebuild_grafana_image.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-echo "{\"commit\":\"$(git rev-parse HEAD)\",\"time\":\"$(git show -s --format=%aI HEAD)\",\"branch\":\"$(git rev-parse --abbrev-ref HEAD)\"}" > gitinfo
 dependencies="./dataland-grafana/ ./gitinfo ./environments/.env.uncritical ./versions.properties"
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_grafana ./dataland-grafana/Dockerfile $dependencies
