6df0e0daa77a111b124f33880972f67e5400e071
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2025-01-20 15:14:05 +0100

    remove gitinfo from dependencies

--- build-utils/rebuild_alloy_image.sh
+++ build-utils/rebuild_alloy_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-dependencies="./dataland-alloy/ ./gitinfo ./environments/.env.uncritical ./versions.properties"
+dependencies="./dataland-alloy/ ./environments/.env.uncritical ./versions.properties"
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_alloy ./dataland-alloy/Dockerfile $dependencies
--- build-utils/rebuild_grafana_image.sh
+++ build-utils/rebuild_grafana_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-dependencies="./dataland-grafana/ ./gitinfo ./environments/.env.uncritical ./versions.properties"
+dependencies="./dataland-grafana/ ./environments/.env.uncritical ./versions.properties"
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_grafana ./dataland-grafana/Dockerfile $dependencies
