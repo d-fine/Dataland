ce6b2b6edc72d26dba34416ea6bf6fc81576162f
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2025-01-27 11:18:11 +0100

    Add comments

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/DataMetaInfoAlterationManager.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/DataMetaInfoAlterationManager.kt
@@ -12,48 +12,52 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
+/**
+ * A Service class used to update dataMetaInformation
+ */
 @Service
-class DataMetaInfoAlterationManager @Autowired constructor(
-    private val objectMapper: ObjectMapper,
-    private val dataMetaInformationManager: DataMetaInformationManager,
-    private val dataManager: DataManager,
-    private val messageQueuePublications: MessageQueuePublications,
-    private val keycloakUserService: KeycloakUserService,
-) {
+class DataMetaInfoAlterationManager
+    @Autowired
+    constructor(
+        private val objectMapper: ObjectMapper,
+        private val dataMetaInformationManager: DataMetaInformationManager,
+        private val dataManager: DataManager,
+        private val messageQueuePublications: MessageQueuePublications,
+        private val keycloakUserService: KeycloakUserService,
+    ) {
+        private val logger = LoggerFactory.getLogger(DataMetaInfoAlterationManager::class.java)
 
-    private val logger = LoggerFactory.getLogger(DataMetaInfoAlterationManager::class.java)
-
-    /**
-     * Patch dataMetaInformation for dataset with given [dataId]
-     * This function retrieves the full dataset from either temporary or internal storage, patches it, and then sends a
-     * message to the internal storage to persist the changed dataset.
-     * Currently, only public datasets are supported.
-     */
-    @Transactional
-    fun patchDataMetaInformation(
-        dataId: String,
-        dataMetaInformationPatch: DataMetaInformationPatch,
-        correlationId: String,
-    ): DataMetaInformationEntity {
-        val dataMetaInformation: DataMetaInformationEntity =
-            dataMetaInformationManager.getDataMetaInformationByDataId(dataId)
+        /**
+         * Patch dataMetaInformation for dataset with given [dataId]
+         * This function retrieves the full dataset from either temporary or internal storage, patches it, and then sends a
+         * message to the internal storage to persist the changed dataset.
+         * Currently, only public datasets are supported.
+         */
+        @Transactional
+        fun patchDataMetaInformation(
+            dataId: String,
+            dataMetaInformationPatch: DataMetaInformationPatch,
+            correlationId: String,
+        ): DataMetaInformationEntity {
+            val dataMetaInformation: DataMetaInformationEntity =
+                dataMetaInformationManager.getDataMetaInformationByDataId(dataId)
 //        if (!dataManager.isDataSetPublic(dataId)) throw InvalidInputApiException(
 //            summary = "Not a public dataset.",
 //            message = "The provided dataId does not belong to a public dataset. Patching of meta information is only " +
 //                    "supported for public dataset."
 //        )
 
-        val uploaderUserId = dataMetaInformationPatch.uploaderUserId?.let {
-            keycloakUserService.getUser(it).userId.isEmpty()
-        }?: throw InvalidInputApiException(
-            summary = "UploaderUserId invalid.",
-            message = "The provided uploaderUserId could not be found."
-        )
-        logger.info("Updating uploaderUserId to $uploaderUserId")
+            val uploaderUserId =
+                dataMetaInformationPatch.uploaderUserId?.let {
+                    keycloakUserService.getUser(it).userId.isEmpty()
+                } ?: throw InvalidInputApiException(
+                    summary = "UploaderUserId invalid.",
+                    message = "The provided uploaderUserId could not be found.",
+                )
+            logger.info("Updating uploaderUserId to $uploaderUserId")
 
-
-        val storableDataSet: StorableDataSet =
-            dataManager.getPublicDataSet(dataId, DataType.valueOf(dataMetaInformation.dataType), correlationId)
+            val storableDataSet: StorableDataSet =
+                dataManager.getPublicDataSet(dataId, DataType.valueOf(dataMetaInformation.dataType), correlationId)
 
 //        messageQueuePublications.publishDataMetaInfoPatchMessage(
 //            dataId,
@@ -62,6 +66,6 @@ class DataMetaInfoAlterationManager @Autowired constructor(
 //        )
 //        dataMetaInformationPatch.uploaderUserId?.let { dataMetaInformationEntity.uploaderUserId = it }
 //        return dataMetaInformationRepository.save(dataMetaInformationEntity)
-        return dataMetaInformation
+            return dataMetaInformation
+        }
     }
-}
