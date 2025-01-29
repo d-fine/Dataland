76dbafbb63bc3b8ca3d631a1efa37e4f67c4e6a6
Antje Neumann <103188513+antjedf@users.noreply.github.com>
2025-01-29 13:14:14 +0100

    WIP: write health status of docker container in log file

--- deployment/start_and_deploy_to_server.sh
+++ deployment/start_and_deploy_to_server.sh
@@ -33,8 +33,8 @@ ssh ubuntu@"$target_server_url" "chmod +x \"$location/dataland-keycloak/migrate_
 ssh ubuntu@"$target_server_url" "\"$location/dataland-keycloak/migrate_keycloak_users.sh\" \"$location\" \"$keycloak_user_dir\" \"$keycloak_backup_dir\" \"$persistent_keycloak_backup_dir\""
 
 # Transfer the scripts to the server
-scp ./healthCheck.sh ubuntu@"$target_server_url":/usr/local/bin/healthCheck.sh
-scp ./health-check.service ubuntu@"$target_server_url":/etc/systemd/system/health-check.service
+sudo scp ./healthCheck.sh ubuntu@"$target_server_url":/usr/local/bin/healthCheck.sh
+sudo scp ./health-check.service ubuntu@"$target_server_url":/etc/systemd/system/health-check.service
 sudo chmod +x /usr/local/bin/healthCheck.sh  # Make the script executable
 sudo systemctl daemon-reload  # Reload the systemd daemon configuration
 sudo systemctl enable health-check.service  # Enable the service
