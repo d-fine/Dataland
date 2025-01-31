1672d36f0079c432e0147bb4578faed11ed55468
Julia Cialkowski <julia.cialkowski@d-fine.de>
2025-01-31 09:06:56 +0100

    draft component test.

---
+++ dataland-frontend/tests/component/components/pages/BulkDataRequest.cy.ts
@@ -0,0 +1,74 @@
+import BulkDataRequest from '@/components/pages/BulkDataRequest.vue';
+
+describe('Component tests for the BulkDataRequest page', () => {
+  it('Should display the BulkRequestSummary with internal data', () => {
+    cy.mountWithPlugins(BulkDataRequest, {
+      data() {
+        return {
+          bulkDataRequestModel: {},
+          availableFrameworks: ['sfdr', 'p2p'],
+          submittingSucceeded: true,
+          submittingInProgress: false,
+          postBulkDataRequestObjectProcessed: true,
+          message: '',
+          rejectedCompanyIdentifiers: ['REJECT123'],
+          existingDataSets: [
+            {
+              userProvidedCompanyId: '123',
+              companyName: 'Company A',
+              reportingPeriod: '2023',
+              framework: 'sfdr',
+              datasetId: 'dataset-1',
+              datasetUrl: 'https://dataland.com/dataset-1',
+            },
+          ],
+          createdRequests: [
+            {
+              userProvidedCompanyId: '456',
+              companyName: 'Company B',
+              requestId: 'request-1',
+              reportingPeriod: '2022',
+              framework: 'p2p',
+              requestUrl: 'https://dataland.com/request-1',
+            },
+          ],
+          existingRequests: [
+            {
+              userProvidedCompanyId: '789',
+              companyName: 'Company C',
+              requestId: 'request-2',
+              reportingPeriod: '2023',
+              framework: 'vsme',
+              requestUrl: 'https://dataland.com/request-2',
+            },
+          ],
+        };
+      },
+    });
+
+    cy.get('.summary-section-heading').contains('CREATED REQUESTS').should('exist');
+    cy.get('.summary-section-heading').contains('SKIPPED REQUESTS - DATA ALREADY EXISTS').should('exist');
+    cy.get('.summary-section-heading').contains('SKIPPED REQUESTS - REQUESTS ALREADY EXIST').should('exist');
+    cy.get('.summary-section-heading').contains('REJECTED IDENTIFIERS').should('exist');
+
+    cy.get('.badge').contains('1').should('exist');
+    cy.get('.badge').contains('1').should('exist');
+    cy.get('.badge').contains('1').should('exist');
+    cy.get('.badge').contains('1').should('exist');
+
+    cy.get('.grid-container').within(() => {
+      cy.get('.bold-text').contains('Company B').should('exist');
+      cy.get('.bold-text').contains('2022').should('exist');
+      cy.get('.text-primary').contains('VIEW REQUEST').should('exist');
+    });
+
+    cy.get('.grid-container').within(() => {
+      cy.get('.bold-text').contains('Company A').should('exist');
+      cy.get('.text-primary').contains('VIEW DATA').should('exist');
+    });
+
+    cy.get('.grid-container').within(() => {
+      cy.get('.bold-text').contains('REJECT123').should('exist');
+    });
+  });
+});
