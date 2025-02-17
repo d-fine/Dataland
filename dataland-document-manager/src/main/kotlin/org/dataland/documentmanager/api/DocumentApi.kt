249f0d38a981435ce9074bc42bda08b9102e604e
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2025-02-17 11:06:47 +0100

    Next attempt to fix the bug

--- dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/api/DocumentApi.kt
+++ dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/api/DocumentApi.kt
@@ -3,7 +3,6 @@ package org.dataland.documentmanager.api
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.headers.Header
 import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.parameters.RequestBody
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestPart
