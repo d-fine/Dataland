b4e59a468a7e2543528aad4dbae5e4aa11ca7256
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2025-02-17 10:19:48 +0100

    Add @JsonProperty annotations

--- dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/model/DocumentMetaInfoPatch.kt
+++ dataland-document-manager/src/main/kotlin/org/dataland/documentmanager/model/DocumentMetaInfoPatch.kt
@@ -2,6 +2,7 @@ package org.dataland.documentmanager.model
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.dataland.datalandbackendutils.model.DocumentCategory
 import java.time.LocalDate
 
@@ -15,18 +16,23 @@ import java.time.LocalDate
  * @param reportingPeriod only for informative purposes
  */
 data class DocumentMetaInfoPatch(
+    @field:JsonProperty(required = false)
     override val documentName: String?,
+    @field:JsonProperty(required = false)
     override val documentCategory: DocumentCategory?,
+    @field:JsonProperty(required = false)
     override val companyIds: Set<String>?,
+    @field:JsonProperty(required = false)
     @field:JsonFormat(pattern = "yyyy-MM-dd")
     override val publicationDate: LocalDate?,
+    @field:JsonProperty(required = false)
     override val reportingPeriod: String?,
 ) : BasicDocumentMetaInfo {
     /**
      * Returns true if all fields are null or empty; false otherwise
      */
     @JsonIgnore
-    fun isNullOrEmpty() =
+    fun isNullOrEmpty(): Boolean =
         documentName.isNullOrEmpty() &&
             documentCategory == null &&
             companyIds.isNullOrEmpty() &&
