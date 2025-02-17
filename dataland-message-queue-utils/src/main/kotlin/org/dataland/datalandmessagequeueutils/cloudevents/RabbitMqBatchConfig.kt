abcb52ecdf51ee361584e30a48748858ba261b74
MarcTM01 <108792843+MarcTM01@users.noreply.github.com>
2025-02-17 12:21:43 +0100

    Fix batch processor starting when spring boot is in testing mode

--- dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/cloudevents/RabbitMqBatchConfig.kt
+++ dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/cloudevents/RabbitMqBatchConfig.kt
@@ -2,6 +2,7 @@ package org.dataland.datalandmessagequeueutils.cloudevents
 
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Fallback
@@ -21,13 +22,17 @@ class RabbitMqBatchConfig {
      */
     @Bean
     @Fallback
-    fun consumerBatchContainerFactory(connectionFactory: ConnectionFactory): SimpleRabbitListenerContainerFactory {
+    fun consumerBatchContainerFactory(
+        connectionFactory: ConnectionFactory,
+        @Value("\${spring.rabbitmq.listener.simple.auto-startup:true}") autoStartup: Boolean,
+    ): SimpleRabbitListenerContainerFactory {
         val factory = SimpleRabbitListenerContainerFactory()
         factory.setConnectionFactory(connectionFactory)
         factory.setBatchListener(true)
         factory.setBatchSize(RABBIT_PREFERRED_BATCH_SIZE)
         factory.setConsumerBatchEnabled(true)
         factory.setReceiveTimeout(RABBIT_BATCH_RECEIVER_TIMEOUT)
+        factory.setAutoStartup(autoStartup)
         return factory
     }
 }
