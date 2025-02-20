5beea99329b83e977669ce4b0ed40d7700403c25
Antje Neumann <103188513+antjedf@users.noreply.github.com>
2025-02-20 12:07:50 +0100

    review comments: reduce comments

--- build-utils/rebuild_grafana_image.sh
+++ build-utils/rebuild_grafana_image.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Dependencies required for building the image
 dependencies="./dataland-grafana/ ./environments/.env.uncritical ./versions.properties"
 
-# Build the Docker image using the adjusted dependencies and Dockerfile
 ./build-utils/base_rebuild_single_docker_image.sh dataland_grafana ./dataland-grafana/Dockerfile $dependencies
