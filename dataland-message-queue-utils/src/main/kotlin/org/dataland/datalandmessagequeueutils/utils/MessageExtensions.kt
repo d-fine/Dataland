f9bd95a1bfdda642fa39b6f7ab901c53425a57b1
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2025-02-20 13:35:30 +0100

    fix sonar issues

--- dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/utils/MessageExtensions.kt
+++ dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/utils/MessageExtensions.kt
@@ -15,9 +15,9 @@ inline fun <reified T> Message.readMessagePayload(objectMapper: ObjectMapper): T
 /**
  * Get message correlation id from headers
  */
-fun Message.getCorrelationId(): String = this.messageProperties.headers[MessageHeaderKey.CORRELATION_ID] as String
+fun Message.getCorrelationId(): String = this.messageProperties.headers[MessageHeaderKey.CORRELATION_ID].toString()
 
 /**
  * Get message type from headers
  */
-fun Message.getType(): String = this.messageProperties.headers[MessageHeaderKey.TYPE] as String
+fun Message.getType(): String = this.messageProperties.headers[MessageHeaderKey.TYPE].toString()
