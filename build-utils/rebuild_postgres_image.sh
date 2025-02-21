492ea58884d021497c7f65ae9c4aaa3c5b731224
Julia Cialkowski <166627669+juliacialkowski@users.noreply.github.com>
2025-02-21 14:39:14 +0100

    adapt rebuild postgres image.

--- build-utils/rebuild_postgres_image.sh
+++ build-utils/rebuild_postgres_image.sh
@@ -3,4 +3,5 @@ set -euxo pipefail
 
 dependencies="./dataland-postgres/"
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres/Dockerfile $dependencies
+./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres-v1/Dockerfile $dependencies
+./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres-v2/Dockerfile $dependencies
