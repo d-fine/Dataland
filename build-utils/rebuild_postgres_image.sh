065120b45f9e7e460d8f387b85661975b3076e09
Julia Cialkowski <166627669+juliacialkowski@users.noreply.github.com>
2025-02-21 15:11:58 +0100

    without number in naming.

--- build-utils/rebuild_postgres_image.sh
+++ build-utils/rebuild_postgres_image.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 
 dependencies="./dataland-postgres/"
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres-v1/Dockerfile $dependencies
-./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres-v2/Dockerfile $dependencies
+./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres-version-one/Dockerfile $dependencies
+./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres-version-two/Dockerfile $dependencies
