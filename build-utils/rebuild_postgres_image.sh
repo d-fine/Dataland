45b65c9d90123000f45a93a73bd89ddf60c70e99
Julia Cialkowski <166627669+juliacialkowski@users.noreply.github.com>
2025-02-21 10:04:21 +0100

    correct healtcheck and remove pull and push script from rebuild process.

--- build-utils/rebuild_postgres_image.sh
+++ build-utils/rebuild_postgres_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-dependencies="./dataland-postgres/ ./environments/.env.uncritical ./versions.properties"
+dependencies="./dataland-postgres/"
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres/Dockerfile $dependencies
