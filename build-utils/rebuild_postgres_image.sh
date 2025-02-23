eca71286e87b8622475d047c5b8a9a5abd026ab9
Julia Cialkowski <166627669+juliacialkowski@users.noreply.github.com>
2025-02-23 22:07:08 +0100

    renaming

--- build-utils/rebuild_postgres_image.sh
+++ build-utils/rebuild_postgres_image.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-dependencies="./dataland-postgres/"
+dependencies_v1="./dataland-postgres-v1/"
+dependencies_v2="./dataland-postgres-v2/"
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres-version-one/Dockerfile $dependencies
-./build-utils/base_rebuild_single_docker_image.sh dataland_postgres ./dataland-postgres-version-two/Dockerfile $dependencies
+./build-utils/base_rebuild_single_docker_image.sh dataland_postgres_v1 ./dataland-postgres-v1/Dockerfile $dependencies_v1
+./build-utils/base_rebuild_single_docker_image.sh dataland_postgres_v2 ./dataland-postgres-17/Dockerfile $dependencies_v2
