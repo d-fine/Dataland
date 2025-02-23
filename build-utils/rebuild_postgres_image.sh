992d12e9e1695e551abb58093c7237a2d041943e
Julia Cialkowski <166627669+juliacialkowski@users.noreply.github.com>
2025-02-23 22:12:03 +0100

    typo.

--- build-utils/rebuild_postgres_image.sh
+++ build-utils/rebuild_postgres_image.sh
@@ -5,4 +5,4 @@ dependencies_v1="./dataland-postgres-v1/"
 dependencies_v2="./dataland-postgres-v2/"
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_postgres_v1 ./dataland-postgres-v1/Dockerfile $dependencies_v1
-./build-utils/base_rebuild_single_docker_image.sh dataland_postgres_v2 ./dataland-postgres-17/Dockerfile $dependencies_v2
+./build-utils/base_rebuild_single_docker_image.sh dataland_postgres_v2 ./dataland-postgres-v2/Dockerfile $dependencies_v2
