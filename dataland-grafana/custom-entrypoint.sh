845dea3dc76fd991d280e5b4571c7f0c96643bcd
Antje Neumann <103188513+antjedf@users.noreply.github.com>
2025-02-24 11:59:46 +0100

    use TARGETSERVER_URL for env label of grafana alerts

--- dataland-grafana/custom-entrypoint.sh
+++ dataland-grafana/custom-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Replace $ENV in the template file with environment name; make sure /etc/grafana/provisioning has correct permissions
-sed 's|"\$ENV"|'\"${ENVNAME}\"'|g' /etc/grafana/provisioning/alerting/alert-rules-template.yaml > /etc/grafana/provisioning/alerting/alert-rules.yaml
+# Compute the environment label from TARGETSERVER_URL
+env_value=$(echo "${TARGETSERVER_URL}" | awk -F. '{print $1}')
+env_label=$([ "$env_value" = "dataland" ] && echo "prod" || echo "$env_value")
+
+# Replace $ENV in the template file; make sure /etc/grafana/provisioning has correct permissions
+sed 's|"\$ENV"|'\"${env_label}\"'|g' /etc/grafana/provisioning/alerting/alert-rules-template.yaml > /etc/grafana/provisioning/alerting/alert-rules.yaml
 
 # Start Grafana
 exec grafana server
--- verifyEnvironmentVariables.sh
+++ verifyEnvironmentVariables.sh
@@ -61,6 +61,7 @@ expected_vars=( \
   LOKI_VOLUME \
   SLACK_ALERT_URL \
   SLACK_CRITICAL_ALERT_URL \
+  TARGETSERVER_URL \
 )
 
 for var in "${expected_vars[@]}"
