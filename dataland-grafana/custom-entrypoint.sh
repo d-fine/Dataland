a4f8653be0e4787f92ac186ea82e34e0da2689d4
Antje Neumann <103188513+antjedf@users.noreply.github.com>
2025-02-24 11:20:28 +0100

    fix remove left over from using TARGETSERVER_URL instead of HOSTNAME

--- dataland-grafana/custom-entrypoint.sh
+++ dataland-grafana/custom-entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Get hostname as environment label for alert template
-env_label=$(echo "${ENVNAME}" | awk -F. '{print $1}')
-
-# Replace $ENV in the template file; make sure /etc/grafana/provisioning has correct permissions
-sed 's|"\$ENV"|'\"${env_label}\"'|g' /etc/grafana/provisioning/alerting/alert-rules-template.yaml > /etc/grafana/provisioning/alerting/alert-rules.yaml
+# Replace $ENV in the template file with environment name; make sure /etc/grafana/provisioning has correct permissions
+sed 's|"\$ENV"|'\"${ENVNAME}\"'|g' /etc/grafana/provisioning/alerting/alert-rules-template.yaml > /etc/grafana/provisioning/alerting/alert-rules.yaml
 
 # Start Grafana
 exec grafana server
