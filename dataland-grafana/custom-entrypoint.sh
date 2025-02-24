8027f7e84fbea53559e4e6c2c2920f06876d60a2
Antje Neumann <103188513+antjedf@users.noreply.github.com>
2025-02-24 10:17:39 +0100

    fix HOSTNAME as ENVLABEL for grafana alerts

--- dataland-grafana/custom-entrypoint.sh
+++ dataland-grafana/custom-entrypoint.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # Get hostname as environment label for alert template
-env_label=$(echo "${HOSTNAME}" | awk -F. '{print $1}')
+env_label=$(echo "${ENVNAME}" | awk -F. '{print $1}')
 
 # Replace $ENV in the template file; make sure /etc/grafana/provisioning has correct permissions
 sed 's|"\$ENV"|'\"${env_label}\"'|g' /etc/grafana/provisioning/alerting/alert-rules-template.yaml > /etc/grafana/provisioning/alerting/alert-rules.yaml
