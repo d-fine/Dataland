2dc70b2430a8e61e89e28cf680deb58e93d02231
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2025-02-25 14:57:37 +0100

    Fix MultipleDatasetDisplayBase and modify respective test

--- dataland-frontend/tests/component/components/pages/ViewMultipleDatasetsDisplayBase.cy.ts
+++ dataland-frontend/tests/component/components/pages/ViewMultipleDatasetsDisplayBase.cy.ts
@@ -1,16 +1,11 @@
 // @ts-nocheck
 import { minimalKeycloakMock } from '@ct/testUtils/Keycloak';
 import ViewMultipleDatasetsDisplayBase from '@/components/generics/ViewMultipleDatasetsDisplayBase.vue';
-import {
-  type DataAndMetaInformationLksgData,
-  type DataMetaInformation,
-  DataTypeEnum,
-  type LksgData,
-  QaStatus,
-} from '@clients/backend';
+import { type DataMetaInformation, DataTypeEnum, type LksgData, QaStatus } from '@clients/backend';
 import { type FixtureData, getPreparedFixture } from '@sharedUtils/Fixtures';
 import router from '@/router';
 import { KEYCLOAK_ROLE_UPLOADER } from '@/utils/KeycloakRoles';
+import { type FrameworkData } from '@/utils/GenericFrameworkTypes.ts';
 
 describe('Component test for the view multiple dataset display base component', () => {
   let preparedFixtures: Array<FixtureData<LksgData>>;
@@ -21,89 +16,86 @@ describe('Component test for the view multiple dataset display base component', 
     });
   });
 
-  it('Checks, if the toggle of hidden fields works for empty and conditional fields', () => {
+  it('Check if the toggle of hidden fields works for empty and conditional fields', () => {
     const preparedFixture = getPreparedFixture('lksg-with-nulls-and-no-child-labor-under-18', preparedFixtures);
-    const mockedData = constructCompanyApiResponseForLksg(preparedFixture.t);
-    const companyInformationObject = preparedFixture.companyInformation;
-    cy.intercept(`/api/companies/mock-company-id/info`, companyInformationObject);
+    const mockDataAndMetaInfo: DataAndMetaInformation<LksgData> = constructCompanyApiResponse(preparedFixture.t);
+    const companyInformation = preparedFixture.companyInformation;
+
+    cy.intercept('/community/requests/user', {});
+    cy.intercept('/api/companies/mock-company-id/info', companyInformation);
+    cy.intercept('/api/data/lksg/companies/mock-company-id', [mockDataAndMetaInfo]);
     cy.intercept(`/api/data/lksg/dataset-a`, {
-      companyId: mockedData.metaInfo.companyId,
-      reportingPeriod: mockedData.metaInfo.reportingPeriod,
-      data: mockedData.data,
+      companyId: mockDataAndMetaInfo.metaInfo.companyId,
+      reportingPeriod: mockDataAndMetaInfo.metaInfo.reportingPeriod,
+      data: mockDataAndMetaInfo.data,
     });
-    cy.intercept(`/api/metadata?companyId=mock-company-id`, [mockedData.metaInfo]);
+    cy.intercept(`/api/metadata?companyId=mock-company-id`, [mockDataAndMetaInfo.metaInfo]);
+
     cy.mountWithPlugins(ViewMultipleDatasetsDisplayBase, {
       keycloak: minimalKeycloakMock({}),
-
-      // @ts-ignore
       props: {
-        companyId: mockedData.metaInfo.companyId,
+        companyId: mockDataAndMetaInfo.metaInfo.companyId,
         dataType: DataTypeEnum.Lksg,
-        reportingPeriod: mockedData.metaInfo.reportingPeriod,
+        reportingPeriod: mockDataAndMetaInfo.metaInfo.reportingPeriod,
         viewInPreviewMode: false,
       },
     });
+
     checkToggleEmptyFieldsSwitch('Number of Employees');
     cy.get('tr[data-section-label="Social"]');
     cy.get('tr[data-section-label="Child labor"]');
     cy.get('td[data-cell-label="Employee(s) Under 15"]').should('not.exist');
   });
 
-  it('Check whether Edit Data button has dropdown with 2 different Reporting Periods', () => {
+  it('Check whether Edit Data button has dropdown with two different Reporting Periods', () => {
     const preparedFixture = getPreparedFixture('lksg-with-nulls-and-no-child-labor-under-18', preparedFixtures);
-    const mockedData2024 = constructCompanyApiResponseForLksg(preparedFixture.t);
+    const mockedData2024 = constructCompanyApiResponse(preparedFixture.t);
     mockedData2024.metaInfo.dataId = 'id-2024';
     mockedData2024.metaInfo.reportingPeriod = '2024';
-    const mockedData2023 = constructCompanyApiResponseForLksg(preparedFixture.t);
+    const mockedData2023 = constructCompanyApiResponse(preparedFixture.t);
     mockedData2023.metaInfo.dataId = 'id-2023';
     mockedData2023.metaInfo.reportingPeriod = '2023';
+    cy.intercept('/community/requests/user', {});
     cy.intercept(`/api/companies/*/info`, preparedFixture.companyInformation);
     cy.intercept(`/api/data/lksg/companies/mock-company-id`, [mockedData2024, mockedData2023]);
-    cy.intercept(`/api/metadata?companyId=mock-company-id`, {
-      status: 200,
-      body: [mockedData2024.metaInfo, mockedData2023.metaInfo],
-    });
+
     cy.spy(router, 'push').as('routerPush');
     cy.mountWithPlugins(ViewMultipleDatasetsDisplayBase, {
       keycloak: minimalKeycloakMock({ roles: [KEYCLOAK_ROLE_UPLOADER] }),
       router: router,
-
-      // @ts-ignore
       props: {
         companyId: mockedData2023.metaInfo.companyId,
         dataType: DataTypeEnum.Lksg,
         viewInPreviewMode: false,
       },
-    }).then(() => {
-      cy.get('[data-test="editDatasetButton"').find('.material-icons-outlined').should('exist').click();
-      cy.get('[data-test="select-reporting-period-dialog"')
-        .should('exist')
-        .get('[data-test="reporting-periods"')
-        .last()
-        .should('contain', '2024')
-        .should('contain', '2023')
-        .click();
-
-      cy.get('@routerPush').should(
-        'have.been.calledWith',
-        `/companies/mock-company-id/frameworks/lksg/upload?reportingPeriod=${mockedData2023.metaInfo.reportingPeriod}`
-      );
     });
+    cy.get('[data-test="editDatasetButton"').find('.material-icons-outlined').should('exist').click();
+    cy.get('[data-test="select-reporting-period-dialog"')
+      .should('exist')
+      .get('[data-test="reporting-periods"')
+      .last()
+      .should('contain', '2024')
+      .should('contain', '2023')
+      .within(() => {
+        cy.contains('2023').click();
+      });
+
+    cy.get('@routerPush').should(
+      'have.been.calledWith',
+      `/companies/mock-company-id/frameworks/lksg/upload?reportingPeriod=${mockedData2023.metaInfo.reportingPeriod}`
+    );
   });
 });
 
 /**
- * This functions imitates an api response of the /data/lksg/companies/mock-company-id endpoint
- * to include 6 active Lksg datasets from different years to test the simultaneous display of multiple Lksg
- * datasets (constructed datasets range from 2023 to 2028)
- * @param baseDataset the lksg dataset used as a basis for constructing the 6 mocked ones
- * @returns a mocked api response
+ * Create a DataAndMetaInformation object for LksgData and a defined DataMetaInformation mock
+ * @param baseDataset the lksg dataset
+ * @returns a mocked DataAndMetaInformation object
  */
-function constructCompanyApiResponseForLksg(baseDataset: LksgData): DataAndMetaInformationLksgData {
+function constructCompanyApiResponse<T extends FrameworkData>(baseDataset: T): DataAndMetaInformation<T> {
   const reportingYear = 2023;
-  const reportingDate = `${reportingYear}-01-01`;
-  const lksgData: LksgData = structuredClone(baseDataset);
-  lksgData.general.masterData.dataDate = reportingDate;
+  const lksgData: T = structuredClone(baseDataset);
+  lksgData.general.masterData.dataDate = `${reportingYear}-01-01`;
   const metaData: DataMetaInformation = {
     dataId: `dataset-a`,
     reportingPeriod: reportingYear.toString(),
