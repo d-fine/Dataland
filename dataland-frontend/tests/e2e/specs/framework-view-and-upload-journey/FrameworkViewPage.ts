737ba0c2e906433274fdb0c593ed3e07ebf03478
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2025-03-06 13:21:55 +0100

    Revert "Fix first failing E2E-Test"
    This reverts commit 225da0ebc54f9690d641a64a144f006f3753cf75.

--- dataland-frontend/tests/e2e/specs/framework-view-and-upload-journey/FrameworkViewPage.ts
+++ dataland-frontend/tests/e2e/specs/framework-view-and-upload-journey/FrameworkViewPage.ts
@@ -430,7 +430,7 @@ describeIf(
       cy.ensureLoggedIn();
       cy.visit(`/companies/${companyIdOfAlpha}/frameworks/${DataTypeEnum.EutaxonomyFinancials}/${nonExistingDataId}`);
 
-      getElementAndAssertExistence('noDataCouldBeLoadedErrorIndicator', 'exist');
+      getElementAndAssertExistence('noDataForThisDataIdPresentErrorIndicator', 'exist');
       getElementAndAssertExistence('claimOwnershipPanelLink', 'not.exist');
 
       cy.visit(
