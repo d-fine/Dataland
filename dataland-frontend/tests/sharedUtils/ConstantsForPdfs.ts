7f39c9444cf0584dfcf3c2565089e45ed3352b69
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2025-03-25 09:15:42 +0100

    DALA-5560 Add page references to document links (#1082)
    Add page references to the document links, so that PDF files can open at the referenced page
    
    ---------
    
    Co-authored-by: Diego Semmler <51290274+drsemmler@users.noreply.github.com>

--- dataland-frontend/tests/sharedUtils/ConstantsForPdfs.ts
+++ dataland-frontend/tests/sharedUtils/ConstantsForPdfs.ts
@@ -4,8 +4,12 @@
 
 export const TEST_PDF_FILE_NAME = 'some-document';
 
+export const TEST_PDF_REPORT_FILE_NAME = 'test-report';
+
 export const TEST_PDF_FILE_BASEPATH = 'testing/data/documents';
 
 export const TEST_PDF_FILE_PATH = `${TEST_PDF_FILE_BASEPATH}/${TEST_PDF_FILE_NAME}.pdf`;
+
+export const TEST_PDF_REPORT_FILE_PATH = `${TEST_PDF_FILE_BASEPATH}/${TEST_PDF_REPORT_FILE_NAME}.pdf`;
 
 export const TEST_PRIVATE_PDF_FILE_PATH = `${TEST_PDF_FILE_BASEPATH}/${TEST_PDF_FILE_NAME}-private.pdf`;
