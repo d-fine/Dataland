cab3b308b8387d01dbcfe6344ed4a41f2fb4d46f
Stephan Wezorke <186339469+StephanWezorke@users.noreply.github.com>
2025-04-03 13:47:26 +0200

    Add e2e-tests for PortfolioEnrichmentService

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/UserServiceTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/UserServiceTest.kt
@@ -1,5 +1,7 @@
 package org.dataland.e2etests.tests
 
+import org.dataland.datalandbackend.openApiClient.model.DataTypeEnum
+import org.dataland.datalandbackend.openApiClient.model.StoredCompany
 import org.dataland.e2etests.auth.GlobalAuth
 import org.dataland.e2etests.auth.TechnicalUser
 import org.dataland.e2etests.utils.ApiAccessor
@@ -17,24 +19,56 @@ class UserServiceTest {
     private val apiAccessor = ApiAccessor()
     private val companyId: String = apiAccessor.uploadOneCompanyWithRandomIdentifier().actualStoredCompany.companyId
 
+    private fun createDummyPortfolioUpload(companyIds: Set<String>): PortfolioUpload =
+        PortfolioUpload(
+            portfolioName = "Test Portfolio ${UUID.randomUUID()}",
+            companyIds = companyIds,
+            frameworks =
+                setOf(
+                    PortfolioUpload.Frameworks.sfdr,
+                    PortfolioUpload.Frameworks.additionalMinusCompanyMinusInformation,
+                    PortfolioUpload.Frameworks.nuclearMinusAndMinusGas,
+                ),
+        )
+
     @Test
     fun `test that creating and retrieving a portfolio for an existing company works as expected`() {
-        val portfolio =
-            PortfolioUpload(
-                portfolioName = "Test Portfolio ${UUID.randomUUID()}",
-                companyIds = setOf(companyId),
-                frameworks =
-                    setOf(
-                        PortfolioUpload.Frameworks.sfdr,
-                        PortfolioUpload.Frameworks.additionalMinusCompanyMinusInformation,
-                        PortfolioUpload.Frameworks.nuclearMinusAndMinusGas,
-                    ),
-            )
+        val portfolio = createDummyPortfolioUpload(setOf(companyId))
 
         GlobalAuth.withTechnicalUser(TechnicalUser.Reader) {
             ApiAwait.waitForSuccess { UserService.portfolioControllerApi.createPortfolio(portfolio) }
             val portfolioResponse = assertDoesNotThrow { UserService.portfolioControllerApi.getAllPortfoliosForCurrentUser() }
             assertEquals(1, portfolioResponse.size)
+        }
+    }
+
+    private fun uploadDummyCompaniesAndDatasets(): List<StoredCompany> {
+        GlobalAuth.withTechnicalUser(TechnicalUser.Admin) {
+            val storedCompanyInformation =
+                (1..3).map { apiAccessor.uploadOneCompanyWithRandomIdentifier().actualStoredCompany }
+
+            apiAccessor.uploadDummyFrameworkDataset(storedCompanyInformation[0].companyId, DataTypeEnum.sfdr, "2023")
+            apiAccessor.uploadDummyFrameworkDataset(storedCompanyInformation[1].companyId, DataTypeEnum.sfdr, "2023")
+            apiAccessor.uploadDummyFrameworkDataset(storedCompanyInformation[1].companyId, DataTypeEnum.eutaxonomyMinusFinancials, "2023")
+            apiAccessor.uploadDummyFrameworkDataset(storedCompanyInformation[0].companyId, DataTypeEnum.sfdr, "2024")
+
+            return storedCompanyInformation
+        }
+    }
+
+    @Test
+    fun `test that enriched portfolios are build as expected`() {
+        val storedCompanies = uploadDummyCompaniesAndDatasets()
+        val portfolioUpload = createDummyPortfolioUpload(storedCompanies.map { it.companyId }.toSet())
+
+        GlobalAuth.withTechnicalUser(TechnicalUser.Reader) {
+            // ApiAwait.waitForSuccess { UserService.portfolioControllerApi.createPortfolio(portfolioUpload) }
+            UserService.portfolioControllerApi.createPortfolio(portfolioUpload)
+            val portfolioResponse = assertDoesNotThrow { UserService.portfolioControllerApi.getAllPortfoliosForCurrentUser() }
+            val enrichedPortfolio = UserService.portfolioControllerApi.getEnrichedPortfolio(portfolioResponse.first().portfolioId)
+
+            assertEquals(enrichedPortfolio.portfolioName, portfolioUpload.portfolioName)
+            assertEquals(6, enrichedPortfolio.propertyEntries.size)
         }
     }
 }
