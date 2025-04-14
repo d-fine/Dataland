e87cc1d923627464f47125332f63b75b6bcc21f0
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2025-04-14 10:27:47 +0200

    change scheduler and threshold for tests

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/messaging/NotificationScheduler.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/messaging/NotificationScheduler.kt
@@ -31,7 +31,7 @@ class NotificationScheduler
          * Scheduled method to send emails for unprocessed notification events.
          * Runs every Sunday at midnight.
          */
-        @Scheduled(cron = "0 */3 * * * ?")
+        @Scheduled(cron = "0 */5 * * * ?")
         fun scheduledWeeklyEmailSending() {
             processNotificationEvents(
                 "Investor Relations",
