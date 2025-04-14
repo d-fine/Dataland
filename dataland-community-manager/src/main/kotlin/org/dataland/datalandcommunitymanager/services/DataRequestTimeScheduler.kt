c29797b355cbcfb84da0a7ac5c60b69025d14ea0
AlexanderBors <137779015+AlexanderBors@users.noreply.github.com>
2025-04-14 17:23:05 +0200

    Changed scheduler intervals for sprint review.

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/DataRequestTimeScheduler.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/services/DataRequestTimeScheduler.kt
@@ -6,10 +6,8 @@ import org.dataland.datalandcommunitymanager.repositories.DataRequestRepository
 import org.dataland.datalandcommunitymanager.utils.DataRequestsFilter
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
-import java.time.Duration
 import java.time.Instant
 import java.util.UUID
 
@@ -17,32 +15,28 @@ import java.util.UUID
  * Implementation of a time scheduler for data requests
  * @param dataRequestUpdateManager DataRequestAlterationManager
  * @param dataRequestRepository DataRequestRepository,
- * @param staleDaysThreshold limit for answered request to remain answered
  */
 
 @Service("DataRequestTimeScheduler")
 class DataRequestTimeScheduler(
     @Autowired private val dataRequestUpdateManager: DataRequestUpdateManager,
     @Autowired private val dataRequestRepository: DataRequestRepository,
-    @Value("\${dataland.community-manager.data-request.answered.stale-days-threshold}")
-    private val staleDaysThreshold: Long,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     /**
      * Cron job that identifies stale answered requests, patches them to closed and triggers an email notification
      */
-    @Scheduled(cron = "0 0 12 * * *")
+    @Scheduled(cron = "30 */1 * * * ?")
     fun patchStaleAnsweredRequestToClosed() {
         val correlationId = UUID.randomUUID().toString()
         logger.info("Searching for stale answered data request. CorrelationId: $correlationId")
-        val thresholdTime = Instant.now().minus(Duration.ofDays(staleDaysThreshold)).toEpochMilli()
         val searchFilterForAnsweredDataRequests =
             DataRequestsFilter(requestStatus = setOf(RequestStatus.Answered))
         val staleAnsweredRequests =
             dataRequestRepository
                 .searchDataRequestEntity(searchFilterForAnsweredDataRequests)
-                .filter { it.lastModifiedDate < thresholdTime }
+                .filter { it.lastModifiedDate < Instant.now().toEpochMilli() }
         staleAnsweredRequests.forEach {
             logger.info(
                 "Patching stale answered data request ${it.dataRequestId} to closed and " +
