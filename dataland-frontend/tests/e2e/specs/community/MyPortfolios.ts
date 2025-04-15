300d4ed0c82b4322cf594a76ee0920143363583c
Diego Semmler <51290274+drsemmler@users.noreply.github.com>
2025-04-15 09:58:45 +0200

    Test space (DALA-5530)

---
+++ dataland-frontend/tests/e2e/specs/community/MyPortfolios.ts
@@ -0,0 +1,32 @@
+import { admin_name, admin_pw } from '@e2e/utils/Cypress';
+import { describeIf } from '@e2e/support/TestUtility';
+import { IdentifierType } from '@clients/backend';
+import { getKeycloakToken } from '@e2e/utils/Auth';
+import { generateDummyCompanyInformation, uploadCompanyViaApi } from '@e2e/utils/CompanyUpload';
+import { assertDefined } from '@/utils/TypeScriptUtils';
+
+describeIf(
+  'As a user I want to be able to create, edit and delete my portfolios',
+  {
+    executionEnvironments: ['developmentLocal', 'ci', 'developmentCd'],
+  },
+  () => {
+    let permIdOfExistingCompany: string;
+    before(() => {
+      getKeycloakToken(admin_name, admin_pw).then(async (token) => {
+        const companyToUpload = generateDummyCompanyInformation(`Test Co. ${new Date().getTime()}`);
+        permIdOfExistingCompany = assertDefined(companyToUpload.identifiers[IdentifierType.PermId][0]);
+        await uploadCompanyViaApi(token, companyToUpload);
+      });
+    });
+
+    beforeEach(() => {
+      cy.ensureLoggedIn(admin_name, admin_pw);
+      cy.visitAndCheckAppMount('/portfolios');
+    });
+
+    it('Creates, alters and deletes a portfolio', () => {
+      console.log(permIdOfExistingCompany);
+    });
+  }
+);
