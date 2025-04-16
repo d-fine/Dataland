f6f7437dd03b4c09163dd729a7256b71f05bae34
AlexanderBors <137779015+AlexanderBors@users.noreply.github.com>
2025-04-16 17:43:00 +0200

    DALA-5755: Flyway script for "notify me immediately" (#1138)
    Added flyway script for notifyMeImmediately.

---
+++ dataland-community-manager/src/main/kotlin/db/migration/V14__AddNotifyMeImmediatelyColumn.kt
@@ -0,0 +1,24 @@
+package db.migration
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+/**
+ * The "notify me immediately" Boolean flag of DataRequestEntity objects
+ * is added manually in this script, as it appears to not be added in the database
+ * in case of a deployment to Clone or Prod.
+ */
+@Suppress("ClassName")
+class V14__AddNotifyMeImmediatelyColumn : BaseJavaMigration() {
+    override fun migrate(context: Context?) {
+        val sql =
+            """
+            ALTER TABLE data_requests ADD COLUMN IF NOT EXISTS 
+            notify_me_immediately BOOLEAN NOT NULL DEFAULT false;
+            """.trimIndent()
+
+        context!!.connection.createStatement().use { statement ->
+            statement.execute(sql)
+        }
+    }
+}
