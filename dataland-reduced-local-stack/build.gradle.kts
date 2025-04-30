84ec73df8c0ace2ddd010362c18816fcd3f7ac2c
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2025-04-30 13:00:48 +0200

    Fix issue with sonar

--- dataland-reduced-local-stack/build.gradle.kts
+++ dataland-reduced-local-stack/build.gradle.kts
@@ -1,4 +1,5 @@
-val jacocoSources by extra(sourceSets.asMap.values.flatMap { sourceSet -> sourceSet.allSource })
+val sonarSources by extra(sourceSets.asMap.values.flatMap { sourceSet -> sourceSet.allSource })
+val jacocoSources by extra(sonarSources)
 val jacocoClasses by extra(
     sourceSets.asMap.values.flatMap { sourceSet ->
         sourceSet.output.classesDirs.flatMap {
