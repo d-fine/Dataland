fd1300ee68fcab851272e0830fd83007096c9595
Fabian-dfine <Fabian-dfine@users.noreply.github.com>
2025-05-05 17:58:42 +0200

    Work on download button

--- dataland-frontend/src/components/resources/portfolio/PortfolioDownload.vue
+++ dataland-frontend/src/components/resources/portfolio/PortfolioDownload.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="portfolio-download-content">
-    <BasicFormSection header="Select framework">
-      <Dropdown
-        name="FrameworkSelection"
-        placeholder="Please select"
-        :options="availableFrameworks"
-        optionValue="value"
-        optionLabel="label"
+    <FormKit type="form" class="formkit-wrapper" :actions="false">
+      <label for="fileTypeSelector">
+        <b style="margin-bottom: 8px; margin-top: 5px; font-weight: normal">Framework</b>
+      </label>
+      <FormKit
         v-model="selectedFramework"
-        class="long"
-        @change="onFrameworkChange"
+        type="select"
+        name="fileTypeSelector"
+        data-test="fileTypeSelector"
+        :options="availableFrameworks"
+        placeholder="Select a file type"
       />
-    </BasicFormSection>
-    <div class="select-group">
-      <BasicFormSection header="Select Reporting Period(s)">
+      <p v-show="showFileTypeError" class="text-danger text-xs" data-test="fileTypeError">Please select a file type.</p>
+
+      <label for="reportingYearSelector">
+        <b style="margin-bottom: 8px; font-weight: normal">Reporting year</b>
+      </label>
+      <div class="reporting-periods-selector">
         <template v-if="selectedFramework">
-          <div v-if="dynamicReportingPeriods.length > 0" class="flex flex-wrap mt-4 py-2">
+          <div v-if="dynamicReportingPeriods.length > 0" class="flex flex-wrap py-2">
             <ToggleChipFormInputs
               :name="'listOfReportingPeriods'"
               :options="dynamicReportingPeriods"
-              @changed="selectedReportingPeriodsError = false"
+              @changed="showReportingPeriodsError = false"
             />
           </div>
           <p v-else class="text-sm text-danger mt-2">
@@ -31,99 +35,89 @@
             Selection of reporting periods is available upon framework selection.
           </p>
         </template>
-        <p v-if="selectedReportingPeriodsError" class="text-danger text-xs mt-2">
-          Select at least one reporting period.
-        </p>
-      </BasicFormSection>
-      <BasicFormSection header="File type">
-          <Dropdown
-            name="fileType"
-            v-model="selectedFileType"
-            :options="fileTypeOptions"
-            optionValue="value"
-            optionLabel="label"
-            placeholder="Select"
-            class="long"
-          />
-      <p  v-if="!canDownload" class="red-text font-italic text-xs m-0 errorMessage" data-test="errorMessage">
-        Select Framework, at least one reporting period and a file type to download.
-      </p>
-      </BasicFormSection>
-      <PrimeButton
-        label="Download Portfolio"
-        icon="pi pi-download"
-        @click="downloadPortfolio"
-        class="primary-button downloadButton"
-        :data-test="'downloadButton'"
-        title="Download the selected frameworks and reporting periods for current portfolio"
-        style="width: max-content"
-        :disabled="!canDownload"
+      </div>
+      <p v-if="showReportingPeriodsError" class="text-danger text-xs mt-2">Select at least one reporting period.</p>
+
+      <label for="fileTypeSelector">
+        <b style="margin-bottom: 8px; margin-top: 5px; font-weight: normal">File Type</b>
+      </label>
+      <FormKit
+        v-model="selectedFileType"
+        type="select"
+        name="fileTypeSelector"
+        data-test="fileTypeSelector"
+        :options="fileTypeSelectionOptions"
+        placeholder="Select a file type"
       />
-    </div>
+      <p v-show="showFileTypeError" class="text-danger text-xs" data-test="fileTypeError">Please select a file type.</p>
+    </FormKit>
+    <PrimeButton
+      label="Download Portfolio"
+      icon="pi pi-download"
+      @click="onDownloadButtonClick"
+      class="primary-button downloadButton"
+      :data-test="'downloadButton'"
+      title="Download the selected frameworks and reporting periods for current portfolio"
+      style="width: max-content"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, inject, onMounted, type Ref, ref } from 'vue';
+import { inject, onMounted, type Ref, ref, watch } from 'vue';
 import PrimeButton from 'primevue/button';
-import BasicFormSection from '@/components/general/BasicFormSection.vue';
 import ToggleChipFormInputs from '@/components/general/ToggleChipFormInputs.vue';
-import { type CompanyIdAndName, ExportFileType } from '@clients/backend';
-import type { DynamicDialogInstance } from 'primevue/dynamicdialogoptions';
 import { type EnrichedPortfolio, type EnrichedPortfolioEntry } from '@clients/userservice';
-import { ApiClientProvider } from '@/services/ApiClients.ts';
-import { assertDefined } from '@/utils/TypeScriptUtils.ts';
-import { type PublicFrameworkDataApi } from '@/utils/api/UnifiedFrameworkDataApi.ts';
-import { getFrameworkDataApiForIdentifier } from '@/frameworks/FrameworkApiUtils.ts';
-import { type FrameworkData } from '@/utils/GenericFrameworkTypes.ts';
-import type Keycloak from 'keycloak-js';
-import Dropdown from 'primevue/dropdown';
 import { forceFileDownload } from '@/utils/FileDownloadUtils.ts';
+import { type DynamicDialogInstance } from 'primevue/dynamicdialogoptions';
+import {type CompanyIdAndName, ExportFileType} from '@clients/backend';
+import {ApiClientProvider} from "@/services/ApiClients.ts";
+import {assertDefined} from "@/utils/TypeScriptUtils.ts";
+import {type PublicFrameworkDataApi} from "@/utils/api/UnifiedFrameworkDataApi.ts";
+import type Keycloak from "keycloak-js";
+import {type FrameworkData} from "@/utils/GenericFrameworkTypes.ts";
+import {getFrameworkDataApiForIdentifier} from "@/frameworks/FrameworkApiUtils.ts";
+
 const dynamicReportingPeriods = ref<{ name: string; value: boolean }[]>([]);
 const availableFrameworks = [
   { value: 'sfdr', label: 'SFDR' },
-  { value: 'eutaxonomy-financials', label: 'EU Taxonomy Financials' },
-  { value: 'eutaxonomy-non-financials', label: 'EU Taxonomy Non-Financials' },
+  { value: 'eutaxonomy-financials', label: 'EU Taxonomy Non-Financials' },
+  { value: 'eutaxonomy-non-financials', label: 'EU Taxonomy Financials' },
   { value: 'nuclear-and-gas', label: 'Nuclear and Gas' },
 ];
 
-const getKeycloakPromise = inject<() => Promise<Keycloak>>('getKeycloakPromise');
 const dialogRef = inject<Ref<DynamicDialogInstance>>('dialogRef');
-
+const getKeycloakPromise = inject<() => Promise<Keycloak>>('getKeycloakPromise');
 const portfolioId = ref<string | undefined>(undefined);
-const portfolioCompanies = ref<CompanyIdAndName[]>([]);
-const selectedReportingPeriodsError = ref(false);
-const selectedFramework = ref<string | undefined>(undefined);
 const portfolioEntries = ref<EnrichedPortfolioEntry[]>([]);
+const selectedFramework = ref<string | undefined>(undefined);
+const portfolioCompanies = ref<CompanyIdAndName[]>([]);
+const showFileTypeError = ref(false);
+const showReportingPeriodsError = ref(false);
 const selectedFileType = ref<ExportFileType | null>(null);
-const fileTypeOptions = [
+const fileTypeSelectionOptions = [
   { label: 'CSV', value: ExportFileType.Csv },
   { label: 'Excel', value: ExportFileType.Excel },
 ];
-
-const canDownload = computed(() => {
-  return (
-    selectedFramework.value !== undefined &&
-    selectedFileType.value !== null &&
-    getSelectedReportingPeriods().length > 0
-  );
-});
 
 onMounted(() => {
   const data = dialogRef?.value.data;
   if (data?.portfolio) {
     const portfolio = data.portfolio as EnrichedPortfolio;
     portfolioId.value = portfolio.portfolioId;
-    portfolioCompanies.value = getUniqueSortedCompanies(portfolio.entries);
     portfolioEntries.value = portfolio.entries;
   }
+});
+
+watch(selectedFramework, () => {
+  onFrameworkChange();
 });
 
 /**
  * Retrieves currently selected framework
  */
 function getFrameworkLabel(value: string | undefined): string {
-  const framework = availableFrameworks.find(fw => fw.value === value);
+  const framework = availableFrameworks.find((fw) => fw.value === value);
   return framework?.label ?? value ?? 'selected framework';
 }
 
@@ -135,7 +129,7 @@ function onFrameworkChange(): void {
 }
 
 /**
- * Set available reporting periods based on the selected framework
+ * When the framework changes, update the available reporting periods based on the selected framework
  */
 function setReportingPeriods(entries: EnrichedPortfolioEntry[]): void {
   if (!selectedFramework.value) return;
@@ -146,12 +140,13 @@ function setReportingPeriods(entries: EnrichedPortfolioEntry[]): void {
       periodsSet.add(entry.latestReportingPeriod);
     }
   });
-  const sortedPeriods = Array.from(periodsSet).sort((a, b) => b.localeCompare(a));
 
-  dynamicReportingPeriods.value = sortedPeriods.map((period) => ({
-    name: period,
-    value: false,
-  }));
+  dynamicReportingPeriods.value = Array.from(periodsSet)
+    .sort((a, b) => parseInt(b) - parseInt(a))
+    .map((period) => ({
+      name: period,
+      value: false,
+    }));
 }
 
 /**
@@ -162,6 +157,18 @@ function getSelectedReportingPeriods(): string[] {
 }
 
 /**
+ * Handle the clickEvent of the Download Button
+ */
+async function onDownloadButtonClick(): Promise<void> {
+  checkIfShowErrors();
+
+  if (showReportingPeriodsError.value || showFileTypeError.value) {
+    return;
+  }
+  await downloadPortfolio();
+}
+
+/**
  * Extracts company IDs from selected portfolio
  */
 function getCompanyIds(): string[] {
@@ -169,15 +176,15 @@ function getCompanyIds(): string[] {
 }
 
 /**
- * Retrieve array of unique and sorted companyIdAndNames from EnrichedPortfolioEntry
+ * Checks for visible errors
  */
-function getUniqueSortedCompanies(entries: CompanyIdAndName[]): CompanyIdAndName[] {
-  const companyMap = new Map(entries.map((entry) => [entry.companyId, entry]));
-  return Array.from(companyMap.values()).sort((a, b) => a.companyName.localeCompare(b.companyName));
+function checkIfShowErrors(): void {
+  showReportingPeriodsError.value = getSelectedReportingPeriods().length === 0;
+  showFileTypeError.value = !selectedFileType.value;
 }
 
 /**
- * Download portfolio with the selected reporting period as a file in the selected format
+ * Handles download of portfolio
  */
 async function downloadPortfolio(): Promise<void> {
   try {
@@ -221,9 +228,31 @@ async function downloadPortfolio(): Promise<void> {
     alert('Download failed');
   }
 }
+
 </script>
 
 <style scoped lang="scss">
+
+.downloadButton {
+  width: max-content;
+  margin-left: auto;
+  justify-content: end;
+}
+.reporting-periods-selector {
+  margin-top: 0.5em;
+  margin-bottom: 1em;
+  min-height: 4em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+label {
+  display: block;
+  margin-top: 10px;
+  margin-bottom: 5px;
+  font-weight: normal;
+}
 
 .portfolio-download-content {
   width: 28em;
@@ -232,70 +261,11 @@ async function downloadPortfolio(): Promise<void> {
   padding: 1.5rem;
 }
 
-.portfolio-dialog-content {
-  width: 28em;
-  border-radius: 0.25rem;
-  background-color: white;
-  padding: 1.5rem;
-}
-
-.framework-pill-wrapper {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 0.5rem;
-  margin-top: 0.5rem;
-}
-
-.form-list-item {
-  background-color: #f0f0f0;
-  border-radius: 20px;
-  padding: 0.3rem 0.75rem;
-  display: flex;
-  align-items: center;
-  font-size: 0.875rem;
-}
-
-.form-list-item em {
-  font-style: normal;
-  margin-left: 0.5rem;
-  cursor: pointer;
-  color: #888;
-}
-
-.no-framework {
-  color: #aaa;
-  font-style: italic;
-}
-.buttonbar {
-  display: flex;
-  gap: 1rem;
-  margin-top: 1em;
-  margin-left: auto;
-  justify-content: end;
-}
-
-label {
-  margin-top: 1.5em;
-}
-
-ul {
-  padding-inline-start: 0;
-  height: 7.5em;
-  overflow-x: hidden;
-  overflow-y: auto;
-}
-
-.downloadButton {
-  min-width: fit-content;
-  padding: 1em;
-}
-
-:deep(.basic-form-section) {
-  margin-bottom: 0.5rem !important;
-  padding-bottom: 0 !important;
-}
-
-.errorMessage{
+.errorMessage {
   gap: 10em;
+}
+
+.text-danger {
+  color: red;
 }
 </style>
