1b0c9bd917b08cf71bb07d123ecedf275d876be9
AlexanderBors <137779015+AlexanderBors@users.noreply.github.com>
2025-05-05 10:10:35 +0200

    Create DocumentMetaInfoEntityTest to test the method isViewableByUser and make Sonar happy.

--- dataland-document-manager/src/test/kotlin/org/dataland/documentmanager/entities/DocumentMetaInfoEntityTest.kt
+++ dataland-document-manager/src/test/kotlin/org/dataland/documentmanager/entities/DocumentMetaInfoEntityTest.kt
@@ -3,21 +3,70 @@ package org.dataland.documentmanager.entities
 import org.dataland.datalandbackendutils.model.DocumentCategory
 import org.dataland.datalandbackendutils.model.DocumentType
 import org.dataland.datalandbackendutils.model.QaStatus
+import org.dataland.keycloakAdapter.auth.DatalandJwtAuthentication
 import org.dataland.keycloakAdapter.auth.DatalandRealmRole
 import org.dataland.keycloakAdapter.utils.AuthenticationMock
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.reset
 import org.mockito.kotlin.whenever
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
+import java.util.stream.Stream
 
 class DocumentMetaInfoEntityTest {
     private val mockSecurityContext = mock<SecurityContext>()
     private lateinit var documentMetaInfoEntity: DocumentMetaInfoEntity
+
+    companion object {
+        private const val DUMMY_USER_ID = "dummy-user-id"
+
+        fun <T> cartesianProduct(lists: List<List<T>>): List<List<T>> =
+            lists.fold(listOf(listOf())) { acc, list ->
+                acc.flatMap { accList ->
+                    list.map { accList + it }
+                }
+            }
+
+        @JvmStatic
+        fun provideParametersForTestingIsViewableByUser(): Stream<Arguments> {
+            val possibleQaStatus = listOf(QaStatus.Accepted, QaStatus.Pending)
+            val possibleUploaderIds = listOf(DUMMY_USER_ID, "dummy-uploader-id")
+            val possibleIsUserLoggedInValues = listOf(true, false)
+            val possibleUserRoleSets =
+                listOf(
+                    setOf<DatalandRealmRole>(),
+                    setOf(DatalandRealmRole.ROLE_ADMIN),
+                    setOf(DatalandRealmRole.ROLE_REVIEWER),
+                    setOf(DatalandRealmRole.ROLE_ADMIN, DatalandRealmRole.ROLE_REVIEWER),
+                )
+
+            val cartesianProductOfPossibilities =
+                cartesianProduct(
+                    listOf(
+                        possibleQaStatus, possibleUploaderIds, possibleIsUserLoggedInValues, possibleUserRoleSets,
+                    ),
+                )
+
+            val argumentsList = mutableListOf<Arguments>()
+
+            cartesianProductOfPossibilities.forEach {
+                argumentsList.add(
+                    Arguments.of(
+                        it[0], it[1], it[2], it[3],
+                    ),
+                )
+            }
+
+            return argumentsList.stream()
+        }
+    }
 
     @BeforeEach
     fun setUp() {
@@ -25,23 +74,23 @@ class DocumentMetaInfoEntityTest {
     }
 
     @ParameterizedTest
-    @CsvSource(
-        delimiter = ';',
-        value = [
-            "Accepted;dummy-user-id;[ROLE_ADMIN,ROLE_REVIEWER]",
-        ],
-    )
+    @MethodSource("provideParametersForTestingIsViewableByUser")
     fun `check that the method isViewableByUser shows the correct output behavior`(
         qaStatus: QaStatus,
         uploaderId: String,
+        isUserLoggedIn: Boolean,
         userRoles: Set<DatalandRealmRole>,
     ) {
         val mockAuthentication =
-            AuthenticationMock.mockJwtAuthentication(
-                username = "data_uploader",
-                userId = "dummy-user-id",
-                roles = userRoles,
-            )
+            if (isUserLoggedIn) {
+                AuthenticationMock.mockJwtAuthentication(
+                    username = "dummy-user",
+                    userId = DUMMY_USER_ID,
+                    roles = userRoles,
+                )
+            } else {
+                mock<DatalandJwtAuthentication>()
+            }
 
         doReturn(mockAuthentication).whenever(mockSecurityContext).authentication
         SecurityContextHolder.setContext(mockSecurityContext)
@@ -59,5 +108,27 @@ class DocumentMetaInfoEntityTest {
                 reportingPeriod = null,
                 qaStatus = qaStatus,
             )
+
+        if (qaStatus == QaStatus.Accepted) {
+            assertTrue(
+                documentMetaInfoEntity.isViewableByUser(),
+                "Method returned false although QaStatus is Accepted.",
+            )
+        } else if (!isUserLoggedIn) {
+            assertFalse(
+                documentMetaInfoEntity.isViewableByUser(),
+                "Method returned true although QaStatus is Pending and user is not logged in.",
+            )
+        } else if (uploaderId != DUMMY_USER_ID && userRoles.isEmpty()) {
+            assertFalse(
+                documentMetaInfoEntity.isViewableByUser(),
+                "Method returned true although all conditions failed.",
+            )
+        } else {
+            assertTrue(
+                documentMetaInfoEntity.isViewableByUser(),
+                "Method returned false although user should have the right to view the non-accepted document.",
+            )
+        }
     }
 }
