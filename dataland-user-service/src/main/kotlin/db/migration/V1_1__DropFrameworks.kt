7cdc13df6cdd660e5f6f48f1cc79d1234b25cb7c
AlexanderBors <137779015+AlexanderBors@users.noreply.github.com>
2025-05-07 17:02:13 +0200

    Change flyway migration script version to 1.1 so the script gets executed on the default baseline version of 1.

--- dataland-user-service/src/main/kotlin/db/migration/V1_1__DropFrameworks.kt
+++ dataland-user-service/src/main/kotlin/db/migration/V1_1__DropFrameworks.kt
@@ -7,7 +7,7 @@ import org.flywaydb.core.api.migration.Context
  * This migration script handles the creation of the initial database table
  */
 @Suppress("ClassName")
-class V1__DropFrameworks : BaseJavaMigration() {
+class V1_1__DropFrameworks : BaseJavaMigration() {
     override fun migrate(context: Context?) {
         context!!.connection.createStatement().execute(
             "DROP TABLE IF EXISTS data_types",
