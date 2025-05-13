706f2bf77a0720d9b8e988b64d3b0ccf0361d8cb
Fabian-dfine <Fabian-dfine@users.noreply.github.com>
2025-05-13 17:10:51 +0200

    DALA-5336: Adjusted PortfolioUtils for sonar

--- dataland-frontend/src/utils/PortfolioUtils.ts
+++ dataland-frontend/src/utils/PortfolioUtils.ts
@@ -29,7 +29,7 @@ export function getAvailableReportingPeriodsAsString(
     return 'No data available';
   }
 
-  return portfolioEntry.availableReportingPeriods[frameworkName] || 'No data available';
+  return portfolioEntry.availableReportingPeriods[frameworkName] ?? 'No data available';
 }
 
 /**
