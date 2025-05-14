8171f35b56b8607fa97b13ecd38290abccd33ac8
Antje Neumann <103188513+antjedf@users.noreply.github.com>
2025-05-14 13:44:09 +0200

    WIP: add loops for some flaky E2E tests

--- dataland-frontend/tests/e2e/specs/admin-tools/AdminConsoleSecurity.ts
+++ dataland-frontend/tests/e2e/specs/admin-tools/AdminConsoleSecurity.ts
@@ -1,31 +1,33 @@
 import { getStringCypressEnv } from '@e2e/utils/Cypress';
 
-describe('As a user I expect the admin console only to be reachable using admin-proxy and not from remote', (): void => {
-  /**
-   * Verifies that an error page is shown when navigating to the provided url
-   * @param url the url to navigate to expecting an error
-   */
-  function checkThatUrlResolvesToErrorPage(url: string): void {
-    cy.visit(url);
-    cy.get('h2').should('exist').should('contain', 'Sorry an error occurred!');
-    cy.url().should('contain', 'nocontent');
-  }
+Cypress._.times(20, () => {
+  describe('As a user I expect the admin console only to be reachable using admin-proxy and not from remote', (): void => {
+    /**
+     * Verifies that an error page is shown when navigating to the provided url
+     * @param url the url to navigate to expecting an error
+     */
+    function checkThatUrlResolvesToErrorPage(url: string): void {
+      cy.visit(url);
+      cy.get('h2').should('exist').should('contain', 'Sorry an error occurred!');
+      cy.url().should('contain', 'nocontent');
+    }
 
-  it(`Test Admin Console not reachable from remote`, () => {
-    checkThatUrlResolvesToErrorPage('/keycloak/admin');
-  });
+    it(`Test Admin Console not reachable from remote`, () => {
+      checkThatUrlResolvesToErrorPage('/keycloak/admin');
+    });
 
-  it(`Master Realm not reachable from remote`, () => {
-    checkThatUrlResolvesToErrorPage('/keycloak/realms/master');
-  });
+    it(`Master Realm not reachable from remote`, () => {
+      checkThatUrlResolvesToErrorPage('/keycloak/realms/master');
+    });
 
-  it(`Test Admin Console is reachable via dataland-admin`, () => {
-    cy.visit('http://dataland-admin:6789/keycloak/admin');
-    cy.get('h1').should('exist').should('contain', 'Sign in to your account');
-    cy.url().should('contain', 'realms/master');
-    cy.get('#username').should('exist').type(getStringCypressEnv('KEYCLOAK_ADMIN'), { force: true });
-    cy.get('#password').should('exist').type(getStringCypressEnv('KEYCLOAK_ADMIN_PASSWORD'), { force: true });
-    cy.get('#kc-login').should('exist').click();
-    cy.get('h1').should('exist').should('contain', 'master realm');
+    it(`Test Admin Console is reachable via dataland-admin`, () => {
+      cy.visit('http://dataland-admin:6789/keycloak/admin');
+      cy.get('h1').should('exist').should('contain', 'Sign in to your account');
+      cy.url().should('contain', 'realms/master');
+      cy.get('#username').should('exist').type(getStringCypressEnv('KEYCLOAK_ADMIN'), { force: true });
+      cy.get('#password').should('exist').type(getStringCypressEnv('KEYCLOAK_ADMIN_PASSWORD'), { force: true });
+      cy.get('#kc-login').should('exist').click();
+      cy.get('h1').should('exist').should('contain', 'master realm');
+    });
   });
 });
--- dataland-frontend/tests/e2e/specs/admin-tools/RabbitMQAdmin.ts
+++ dataland-frontend/tests/e2e/specs/admin-tools/RabbitMQAdmin.ts
@@ -23,27 +23,29 @@ const queues = [
   'community-manager.queue.nonSourceableData',
 ];
 
-describe('As a developer, I expect the RabbitMQ GUI console to be available to me. Also check if all expected channels exist.', () => {
-  it('Checks if the RabbitMQ Management GUI is available and the login page is shown. Then check that all expected queues exist.', () => {
-    cy.visit('http://dataland-admin:6789/rabbitmq');
-    cy.get('input[name=username]').should('exist').type(getStringCypressEnv('RABBITMQ_USER'));
-    cy.get('input[name=password]').should('exist').type(getStringCypressEnv('RABBITMQ_PASS'));
-    cy.get('input[type=submit]').should('contain.value', 'Login').click();
-    cy.get('#logout').contains('Log out').should('contain.value', 'Log out');
-    cy.get("ul[id='tabs'").find("a[href='#/queues']").click();
-    cy.contains('table th', 'Overview')
-      .invoke('parents', 'table')
-      .find('tbody tr')
-      .its('length')
-      .then((rowCount) => {
-        cy.wrap(queues.length).should('eq', rowCount);
-      });
-    cy.get('table[class=list]')
-      .should('exist')
-      .then(() => {
-        queues.forEach((queue) => {
-          cy.get('table[class=list]').contains(queue).should('contain.text', queue);
+Cypress._.times(20, () => {
+  describe('As a developer, I expect the RabbitMQ GUI console to be available to me. Also check if all expected channels exist.', () => {
+    it('Checks if the RabbitMQ Management GUI is available and the login page is shown. Then check that all expected queues exist.', () => {
+      cy.visit('http://dataland-admin:6789/rabbitmq');
+      cy.get('input[name=username]').should('exist').type(getStringCypressEnv('RABBITMQ_USER'));
+      cy.get('input[name=password]').should('exist').type(getStringCypressEnv('RABBITMQ_PASS'));
+      cy.get('input[type=submit]').should('contain.value', 'Login').click();
+      cy.get('#logout').contains('Log out').should('contain.value', 'Log out');
+      cy.get("ul[id='tabs'").find("a[href='#/queues']").click();
+      cy.contains('table th', 'Overview')
+        .invoke('parents', 'table')
+        .find('tbody tr')
+        .its('length')
+        .then((rowCount) => {
+          cy.wrap(queues.length).should('eq', rowCount);
         });
-      });
+      cy.get('table[class=list]')
+        .should('exist')
+        .then(() => {
+          queues.forEach((queue) => {
+            cy.get('table[class=list]').contains(queue).should('contain.text', queue);
+          });
+        });
+    });
   });
 });
