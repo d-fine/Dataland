c768b631e856a6335c7a40af6774995ad1cbd20e
Fabian-dfine <Fabian-dfine@users.noreply.github.com>
2025-05-19 17:08:38 +0200

    DALA-5872: Added Grafana to CD testing

--- dataland-frontend/tests/e2e/specs/admin-tools/Grafana.ts
+++ dataland-frontend/tests/e2e/specs/admin-tools/Grafana.ts
@@ -1,6 +1,13 @@
 import { getStringCypressEnv } from '@e2e/utils/Cypress';
+import { describeIf } from '@e2e/support/TestUtility';
+import type { ExecutionConfig } from '@e2e/support/TestUtility';
 
-describe('As a developer, I expect Grafana to be available to me', () => {
+// Only works locally if Grafan is set up correctly
+const executionConfig: ExecutionConfig = {
+  executionEnvironments: ['developmentLocal', 'developmentCd'],
+};
+
+describeIf('As a developer, I expect Grafana to be available to me', executionConfig, () => {
   beforeEach(() => {
     cy.setExceptionContext('grafana');
 
@@ -10,7 +17,6 @@ describe('As a developer, I expect Grafana to be available to me', () => {
       urlShouldInclude: '/grafana',
     });
 
-    // Login before each test
     cy.url().then((url) => {
       if (url.includes('/grafana/login')) {
         cy.get('input[name=user]').should('exist').type(getStringCypressEnv('GRAFANA_ADMIN'));
@@ -22,25 +28,18 @@ describe('As a developer, I expect Grafana to be available to me', () => {
   });
 
   it('Checks if Grafana is available loki logs are shown', () => {
-    // Navigate to the Logs in the Drilldown section
-    cy.get('button[aria-label="Expand section Drilldown"]').should('exist');
     cy.get('button[aria-label="Expand section Drilldown"]').should('exist').click();
     cy.get('[data-testid="data-testid Nav menu item"]').contains('Logs').click();
     cy.url().should('include', '/grafana-lokiexplore');
-
-    // Check that there are some logs displayed
     cy.get('[data-testid="header-container"]').should('contain.text', 'dataland-');
     cy.get('[data-testid="data-testid panel content"]').should('exist');
   });
 
   it('Checks the health status of the preconfigured alerts', () => {
-    // Navigate to the Alert rules in the Alerting section
     cy.get('a[href="/grafana/alerting"]').click();
     cy.url().should('include', '/grafana/alerting');
     cy.get('a[href="/grafana/alerting/list"]').click();
     cy.url().should('include', '/grafana/alerting/list');
-
-    // Validate the health status of the alerts
     cy.get('[data-testid="rule-group"]').should('contain.text', 'Error Alerts');
     cy.get('[data-testid="data-testid group-collapse-toggle"]').should('exist').click();
     cy.get('[data-testid="rules-table"]').find('[data-testid="row"]').should('have.length', 4);
@@ -50,13 +49,10 @@ describe('As a developer, I expect Grafana to be available to me', () => {
   });
 
   it('Checks the preconfigured Slack contact points', () => {
-    // Navigate to the Contact Pomits in the Alerting section
     cy.get('a[href="/grafana/alerting"]').click();
     cy.url().should('include', '/grafana/alerting');
     cy.get('a[href="/grafana/alerting/notifications"]').click();
     cy.url().should('include', '/grafana/alerting/notifications');
-
-    // Validate the Slack contact points configuration with sending test notifications
     cy.get('[data-testid="contact-point"]').should('have.length', 3);
     testProvisionedContactPoint('Slack Alert Bot');
     testProvisionedContactPoint('Slack Critical Alerts');
@@ -68,7 +64,6 @@ describe('As a developer, I expect Grafana to be available to me', () => {
  * @param contactPointName - The name of the contact point to test.
  */
 function testProvisionedContactPoint(contactPointName: string): void {
-  // View details of contactPointName
   cy.get('[data-testid="contact-point"]')
     .contains(contactPointName)
     .parents('[data-testid="contact-point"]')
@@ -77,7 +72,6 @@ function testProvisionedContactPoint(contactPointName: string): void {
       cy.get('[data-testid="view-action"]').click();
     });
   cy.contains('This contact point cannot be edited through the UI').should('exist');
-  // go back to contact points overview
   cy.get('a[href="/grafana/alerting/notifications"]').click();
   cy.url().should('include', '/grafana/alerting/notifications');
 }
