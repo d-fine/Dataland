caa2775f100a41ec6786541b23fe8ff8ab563839
Leonard Wienke <206227392+LeonardWienke@users.noreply.github.com>
2025-05-27 17:34:37 +0200

    DALA-5634: Set Scheduler

--- dataland-data-exporter/src/main/kotlin/org/dataland/datalanddataexporter/services/CsvExporter.kt
+++ dataland-data-exporter/src/main/kotlin/org/dataland/datalanddataexporter/services/CsvExporter.kt
@@ -52,7 +52,7 @@ class CsvExporter(
     }
 
     @Suppress("UnusedPrivateMember") // Detect does not recognise the scheduled execution of this function
-    @Scheduled(cron = "0 0 1 * * *")
+    @Scheduled(cron = "0 */10 * * * *")
     private fun triggerExport() {
         val outputDirectory = "/var/export/csv/sql_server"
         exportSfdrData(outputDirectory)
