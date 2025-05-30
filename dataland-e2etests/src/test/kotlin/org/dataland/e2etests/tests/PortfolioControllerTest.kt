8deabf225f6e875dcbdb39134c2f6e337246deb9
AlexanderBors <137779015+AlexanderBors@users.noreply.github.com>
2025-05-30 17:50:24 +0200

    Add E2E tests for checking that the two new endpoints can only be used by admins.

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/PortfolioControllerTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/PortfolioControllerTest.kt
@@ -1,8 +1,10 @@
 package org.dataland.e2etests.tests
 
 import org.dataland.e2etests.PREMIUM_USER_ID
+import org.dataland.e2etests.auth.GlobalAuth
 import org.dataland.e2etests.auth.TechnicalUser
 import org.dataland.e2etests.utils.api.UserService
+import org.dataland.userService.openApiClient.infrastructure.ClientException
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -14,11 +16,27 @@ class PortfolioControllerTest {
     @ParameterizedTest
     @EnumSource(TechnicalUser::class)
     fun `check that only admins can use the endpoint for getting all portfolios of a user by userId`(technicalUser: TechnicalUser) {
-        if (technicalUser == TechnicalUser.Admin) {
-            assertDoesNotThrow { UserService.portfolioControllerApi.getPortfoliosForUser(PREMIUM_USER_ID) }
-        } else {
-            assertThrows<AccessDeniedException> {
-                UserService.portfolioControllerApi.getPortfoliosForUser(PREMIUM_USER_ID)
+        GlobalAuth.withTechnicalUser(technicalUser) {
+            if (technicalUser == TechnicalUser.Admin) {
+                assertDoesNotThrow { UserService.portfolioControllerApi.getPortfoliosForUser(PREMIUM_USER_ID) }
+            } else {
+                assertThrows<ClientException> {
+                    UserService.portfolioControllerApi.getPortfoliosForUser(PREMIUM_USER_ID)
+                }
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(TechnicalUser::class)
+    fun `check that only admins can use the endpoint for getting all portfolios`(technicalUser: TechnicalUser) {
+        GlobalAuth.withTechnicalUser(technicalUser) {
+            if (technicalUser == TechnicalUser.Admin) {
+                assertDoesNotThrow { UserService.portfolioControllerApi.getAllPortfolios() }
+            } else {
+                assertThrows<ClientException> {
+                    UserService.portfolioControllerApi.getAllPortfolios()
+                }
             }
         }
     }
