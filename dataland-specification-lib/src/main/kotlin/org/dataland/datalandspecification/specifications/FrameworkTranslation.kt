1d81bc8a67bdca50d83f057ef9ddc371eaa5d379
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-05-30 11:24:55 +0200

    add validate integrity function

--- dataland-specification-lib/src/main/kotlin/org/dataland/datalandspecification/specifications/FrameworkTranslation.kt
+++ dataland-specification-lib/src/main/kotlin/org/dataland/datalandspecification/specifications/FrameworkTranslation.kt
@@ -1,7 +1,9 @@
 package org.dataland.datalandspecification.specifications
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import com.fasterxml.jackson.databind.node.ObjectNode
+import org.dataland.datalandspecification.specifications.VerificationUtils.flattenSchema
 
 /**
  * A specification for a framework
@@ -9,4 +11,21 @@ import com.fasterxml.jackson.databind.node.ObjectNode
 data class FrameworkTranslation(
     val id: String,
     val schema: ObjectNode = JsonNodeFactory.instance.objectNode(),
-)
+) {
+    /**
+     * A flattened version of the schema
+     */
+    @get:JsonIgnore
+    val flattenedSchema: List<FrameworkSchemaEntry>
+        get() = flattenSchema(schema, "").toList()
+
+    /**
+     * Validates the integrity of the framework specification.
+     */
+    fun validateIntegrity() {
+        val schemaExportAliases = flattenedSchema.map { it.value }.toList()
+        val duplicatedAliases =
+            schemaExportAliases.groupingBy { it }.eachCount().filter { it.value > 1 }
+        check(duplicatedAliases.isEmpty()) { "The following exportAliases are duplicates: $duplicatedAliases" }
+    }
+}
