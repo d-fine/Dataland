8fdcc411ffb2bb18f5d0876a7a061d91eb9d83b4
S-Hofmann1 <192322604+SimonHofmann12@users.noreply.github.com>
2025-06-03 14:08:12 +0200

    Update time

--- dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
+++ dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
@@ -119,7 +119,7 @@ class ProcessDataUpdates
         }
 
         @Suppress("UnusedPrivateMember") // Detect does not recognise the scheduled execution of this function
-        @Scheduled(cron = "0 20 14 * * *")
+        @Scheduled(cron = "0 22 16 * * *")
         private fun processUpdates() {
             logger.info("Running scheduled update of GLEIF data.")
             waitForBackend()
