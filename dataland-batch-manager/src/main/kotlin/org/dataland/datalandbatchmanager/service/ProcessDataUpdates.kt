cd06a474534292870627996f532815f65068abee
S-Hofmann1 <192322604+SimonHofmann12@users.noreply.github.com>
2025-06-03 15:21:25 +0200

    Update time only retry timeouts

--- dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
+++ dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
@@ -119,7 +119,7 @@ class ProcessDataUpdates
         }
 
         @Suppress("UnusedPrivateMember") // Detect does not recognise the scheduled execution of this function
-        @Scheduled(cron = "0 30 12 * * *")
+        @Scheduled(cron = "0 35 13 * * *")
         private fun processUpdates() {
             logger.info("Running scheduled update of GLEIF data.")
             waitForBackend()
