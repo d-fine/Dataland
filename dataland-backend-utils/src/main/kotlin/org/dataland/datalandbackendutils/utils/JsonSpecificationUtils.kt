3612e9a90adac94b370f5b660345ced8b98f04d5
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-06-03 09:39:30 +0200

    ugly fix json specification utils

--- dataland-backend-utils/src/main/kotlin/org/dataland/datalandbackendutils/utils/JsonSpecificationUtils.kt
+++ dataland-backend-utils/src/main/kotlin/org/dataland/datalandbackendutils/utils/JsonSpecificationUtils.kt
@@ -18,7 +18,10 @@ data class JsonSpecificationLeaf(
  * Utilities for working with JSON framework specifications.
  */
 object JsonSpecificationUtils {
-    private fun isTerminalNode(node: JsonNode): Boolean = node.fieldNames().asSequence().toSet() == setOf("id", "ref", "aliasExport")
+    private fun isTerminalNode(node: JsonNode): Boolean =
+        node.fieldNames().asSequence().toSet() == setOf("id", "ref", "aliasExport") ||
+            node.fieldNames().asSequence().toSet() == setOf("id", "ref") &&
+            node.findValue("id").asText() == "referencedReports"
 
     /**
      * Hydrates a JSON specification with data from a data function. During hydration, leaf nodes of the
