9934a45083b58825c7f2e9f5a47e2af1f156f380
AlexanderBors <137779015+AlexanderBors@users.noreply.github.com>
2025-06-04 16:28:15 +0200

    More changes to the modal.

--- dataland-frontend/src/components/general/AddCompanyToPortfoliosModal.vue
+++ dataland-frontend/src/components/general/AddCompanyToPortfoliosModal.vue
@@ -1,22 +1,22 @@
 <template>
   <PrimeDialog
-    v-model:visible="isModalVisible"
-    style="text-align: center; width: 30em"
-    header="Add company to portfolios"
-    :show-header="true"
-    :closable="true"
-    :dismissable-mask="true"
-    :modal="true"
-    :close-on-escape="true"
-    data-test="addCompanyToPortfoliosModal"
-    @after-hide="closeDialog"
+      v-model:visible="isModalVisible"
+      style="text-align: center; width: 30em"
+      header="Add company to portfolios"
+      :show-header="true"
+      :closable="true"
+      :dismissable-mask="true"
+      :modal="true"
+      :close-on-escape="true"
+      data-test="addCompanyToPortfoliosModal"
+      @after-hide="closeDialog"
   >
     <MultiSelect
-      v-model="selectedPortfolios"
-      :options="allUserPortfolios"
-      optionLabel="portfolioName"
-      placeholder="Select portfolios"
-      class="w-full md:w-80"
+        v-model="selectedPortfolioNames"
+        :options="allUserPortfolios"
+        optionLabel="portfolioName"
+        placeholder="Select portfolios"
+        class="w-full md:w-80"
     />
     <PrimeButton class="primary-button" aria-label="Add Company" @click="handleCompanyAddition">
       <span>Add company to portfolio(s)</span>
@@ -25,10 +25,10 @@
 </template>
 
 <script setup lang="ts">
-import { defineEmits, inject, ref, watch } from 'vue';
-import { type BasePortfolio } from '@clients/userservice';
-import { ApiClientProvider } from '@/services/ApiClients.ts';
-import { assertDefined } from '@/utils/TypeScriptUtils.ts';
+import {defineEmits, inject, ref, watch} from 'vue';
+import {type BasePortfolio} from '@clients/userservice';
+import {ApiClientProvider} from '@/services/ApiClients.ts';
+import {assertDefined} from '@/utils/TypeScriptUtils.ts';
 import MultiSelect from 'primevue/multiselect';
 import PrimeButton from 'primevue/button';
 import PrimeDialog from 'primevue/dialog';
@@ -45,7 +45,7 @@ const apiClientProvider = new ApiClientProvider(assertDefined(getKeycloakPromise
 
 const isModalVisible = ref<boolean>(false);
 let allUserPortfolios: ReducedBasePortfolio[] = [];
-let selectedPortfolios: ReducedBasePortfolio[] = [];
+const selectedPortfolioNames = ref<string[]>([]);
 
 const props = defineProps({
   companyId: {
@@ -62,13 +62,13 @@ const props = defineProps({
 const emit = defineEmits(['closePortfolioModal']);
 
 watch<boolean>(
-  () => props.isModalOpen,
-  async (newValue) => {
-    if (newValue) {
-      await fetchUserPortfolios();
+    () => props.isModalOpen,
+    async (newValue) => {
+      if (newValue) {
+        await fetchUserPortfolios();
+      }
+      isModalVisible.value = newValue;
     }
-    isModalVisible.value = newValue;
-  }
 );
 
 const convertToReducedBasePortfolio = (basePortfolio: BasePortfolio): ReducedBasePortfolio => {
@@ -81,25 +81,30 @@ const convertToReducedBasePortfolio = (basePortfolio: BasePortfolio): ReducedBas
 
 const fetchUserPortfolios = async (): Promise<void> => {
   const allUserBasePortfolios = (
-    await apiClientProvider.apiClients.portfolioController.getAllPortfoliosForCurrentUser()
+      await apiClientProvider.apiClients.portfolioController.getAllPortfoliosForCurrentUser()
   ).data;
   allUserPortfolios = allUserBasePortfolios.map(convertToReducedBasePortfolio);
 };
 
 const handleCompanyAddition = (): void => {
-  if (selectedPortfolios.length === 0) return;
-  selectedPortfolios.forEach(async (portfolio) => {
-    await apiClientProvider.apiClients.portfolioController.replacePortfolio(portfolio.portfolioId, {
-      portfolioName: portfolio.portfolioName,
-      // as unknown as Set<string> cast required to ensure proper json is created
-      companyIds: [...portfolio.companyIds, props.companyId] as unknown as Set<string>,
-    });
+  if (selectedPortfolioNames.value.length === 0) return;
+  selectedPortfolioNames.value.forEach(async (portfolioName) => {
+    const reducedBasePortfolio = allUserPortfolios.find(
+        (reducedBasePortfolio) => reducedBasePortfolio.portfolioName === portfolioName
+    );
+    await apiClientProvider.apiClients.portfolioController.replacePortfolio(
+        reducedBasePortfolio!!.portfolioId,
+        {
+          portfolioName: portfolioName,
+          // as unknown as Set<string> cast required to ensure proper json is created
+          companyIds: [...reducedBasePortfolio!!.companyIds, props.companyId] as unknown as Set<string>,
+        });
   });
   closeDialog();
 };
 
 const closeDialog = (): void => {
-  selectedPortfolios = [];
+  selectedPortfolioNames.value = [];
   isModalVisible.value = false;
   emit('closePortfolioModal');
 };
