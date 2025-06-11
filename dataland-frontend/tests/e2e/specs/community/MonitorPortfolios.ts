ac2f7a2e08fa1b6d7fd31a2d63043d8afb2ab273
Fabian-dfine <Fabian-dfine@users.noreply.github.com>
2025-06-11 19:56:43 +0200

    DALA-5780: safen test

--- dataland-frontend/tests/e2e/specs/community/MonitorPortfolios.ts
+++ dataland-frontend/tests/e2e/specs/community/MonitorPortfolios.ts
@@ -91,7 +91,7 @@ describe('Portfolio Monitoring Modal', () => {
         cy.get('[data-test="saveButton"]').click();
 
         cy.wait(Cypress.env('short_timeout_in_ms') as number);
-        cy.get('[data-test="monitor-portfolio"]').filter(':visible').click({ force: true });
+        cy.get('[data-test="monitor-portfolio"]').click({ force: true });
         cy.get('[data-test="activateMonitoringToggle"]').click();
         cy.get('[data-test="listOfReportingPeriods"]').click();
         cy.get('.p-dropdown-item').contains('2023').click();
