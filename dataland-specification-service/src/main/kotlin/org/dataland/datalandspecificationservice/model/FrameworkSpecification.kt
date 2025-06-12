a208fc71bba27f8e95aba7075ac1c1f668b230ac
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-06-12 18:01:10 +0200

    fix specification service

--- dataland-specification-service/src/main/kotlin/org/dataland/datalandspecificationservice/model/FrameworkSpecification.kt
+++ dataland-specification-service/src/main/kotlin/org/dataland/datalandspecificationservice/model/FrameworkSpecification.kt
@@ -1,13 +1,12 @@
 package org.dataland.datalandspecificationservice.model
 
-import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import com.fasterxml.jackson.databind.node.ObjectNode
 import org.dataland.datalandspecification.database.SpecificationDatabase
 import org.dataland.datalandspecification.specifications.Framework
 import org.dataland.datalandspecification.specifications.FrameworkTranslation
-import java.io.IOException
 
 /**
  * Get the reference for this framework specification.
@@ -21,31 +20,18 @@ fun Framework.getRef(baseUrl: String): IdWithRef =
 /**
  * Get the reference and an alias for this framework specification.
  */
-fun FrameworkTranslation.getRefAndAlias(baseUrl: String): IdWithRefAndAlias {
-    val translationFile = "resources/specifications/translations/${this.id}.json"
-    val translation =
-        try {
-            val resource = this::class.java.classLoader.getResourceAsStream(translationFile)
-            if (resource != null) {
-                val objectMapper = ObjectMapper()
-                objectMapper.readTree(resource)
-            } else {
-                null
-            }
-        } catch (e: IOException) {
-            println("I/O error while reading translation file $translationFile: ${e.message}")
-            null
-        } catch (e: JsonProcessingException) {
-            println("JSON parsing error in translation file $translationFile: ${e.message}")
-            null
-        }
-    val alias = translation?.get(this.id)?.asText() ?: null
+fun FrameworkTranslation.getAlias(path: String): String? {
+    var currentNode: JsonNode? = this.schema
 
-    return IdWithRefAndAlias(
-        id = this.id,
-        ref = "https://$baseUrl/specifications/frameworks/${this.id}",
-        aliasExport = alias,
-    )
+    val elements = path.split('.')
+    for (element in elements) {
+        if (currentNode == null) {
+            return null
+        }
+        currentNode =
+            currentNode.path(element)
+    }
+    return currentNode?.asText()
 }
 
 /**
@@ -56,22 +42,24 @@ private fun translateSchema(
     baseUrl: String,
     database: SpecificationDatabase,
     framework: String,
+    currentPath: String,
 ) {
-    val frameworkSpec = database.translations[framework]?.getRefAndAlias(baseUrl)
     for ((key, value) in schema.properties()) {
+        val fieldPath = if (currentPath.isEmpty()) key else "$currentPath.$key"
         if (value.isObject) {
-            translateSchema(value as ObjectNode, baseUrl, database, framework)
+            translateSchema(value as ObjectNode, baseUrl, database, framework, fieldPath)
         } else {
             assert(value.isTextual)
             val dataPointSpec =
                 database.dataPointTypes[value.asText()]
                     ?: throw IllegalArgumentException("Data point type id ${value.asText()} does not exist in the database dataPointTypes.")
             val idWithRef = dataPointSpec.getRef(baseUrl)
+            val aliasExport = database.translations[framework]?.getAlias(fieldPath)
 
             val idWithRefNode: ObjectNode = JsonNodeFactory.instance.objectNode()
             idWithRefNode.put("id", idWithRef.id)
             idWithRefNode.put("ref", idWithRef.ref)
-            idWithRefNode.put("aliasExport", frameworkSpec?.aliasExport)
+            idWithRefNode.put("aliasExport", aliasExport)
 
             schema.set<ObjectNode>(key, idWithRefNode)
         }
@@ -87,7 +75,7 @@ fun Framework.toDto(
 ): FrameworkSpecification {
     val newSchema: ObjectNode = this.schema.deepCopy()
     val framework = this.id
-    translateSchema(newSchema, baseUrl, database, framework)
+    translateSchema(newSchema, baseUrl, database, framework, "")
     return FrameworkSpecification(
         framework = this.getRef(baseUrl),
         name = this.name,
