9cd1ce337e073a8b28826eaaa473827fccd300dc
Leonard Wienke <206227392+LeonardWienke@users.noreply.github.com>
2025-06-12 11:17:44 +0200

    DALA-5780: Refactoring

--- dataland-user-service/src/main/kotlin/org/dataland/datalanduserservice/configurations/ApiClients.kt
+++ dataland-user-service/src/main/kotlin/org/dataland/datalanduserservice/configurations/ApiClients.kt
@@ -3,6 +3,8 @@ package org.dataland.datalanduserservice.configurations
 import okhttp3.OkHttpClient
 import org.dataland.datalandbackend.openApiClient.api.CompanyDataControllerApi
 import org.dataland.datalandbackend.openApiClient.api.MetaDataControllerApi
+import org.dataland.datalandcommunitymanager.openApiClient.api.RequestControllerApi
+import org.dataland.
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -14,6 +16,7 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class ApiClients(
     @Value("\${dataland.backend.base-url}") private val backendBaseUrl: String,
+    @Value("\${dataland.community-manager.base-url}") private val communitymanagerBaseUrl: String,
 ) {
     /**
      * Creates an auto-authenticated version of the CompanyDataControllerApi of the backend
@@ -30,4 +33,12 @@ class ApiClients(
     fun getMetaDataControllerApi(
         @Qualifier("AuthenticatedOkHttpClient") authenticatedOkHttpClient: OkHttpClient,
     ): MetaDataControllerApi = MetaDataControllerApi(backendBaseUrl, authenticatedOkHttpClient)
+
+    /**
+     * Creates an auto-authenticated version of the RequestControllerApi of the community manager
+     */
+    @Bean
+    fun getRequestsControllerApi(
+        @Qualifier("AuthenticatedOkHttpClient") authenticatedOkHttpClient: OkHttpClient,
+    ): RequestControllerApi = RequestControllerApi(communitymanagerBaseUrl, authenticatedOkHttpClient)
 }
