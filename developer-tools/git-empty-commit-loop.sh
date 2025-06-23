d1d300fe5c52eab7d5b4beb4c43a28bcacbf3c34
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2025-06-23 13:50:42 +0200

    Modify empty-commit-loop bash script

--- developer-tools/git-empty-commit-loop.sh
+++ developer-tools/git-empty-commit-loop.sh
@@ -12,15 +12,15 @@ echo "start creating $NUM_COMMITS commits with delay $DELAY"
 # Loop to create and push empty commits
 for ((i=1; i<=NUM_COMMITS; i++))
 do
-    sleep $DELAY
-
     echo "Creating commit #$i"
-    # Create an empty commit; running commit-hooks is not necessary
+    # Create an empty commit
     git commit --allow-empty -m "CI run ($i)" --no-verify
 
     echo "Pushing commit #$i"
     # Push the commit to the remote repository
     git push
+
+    sleep $DELAY
 done
 
 echo "All done!"
