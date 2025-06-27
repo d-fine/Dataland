ef7ce889958f16bd71f981a71e1dcdbef30cf46b
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-06-27 17:33:15 +0200

    Switched to Composition API

--- dataland-frontend/src/components/general/NuclearAndGasDataTable.vue
+++ dataland-frontend/src/components/general/NuclearAndGasDataTable.vue
@@ -14,8 +14,8 @@
   </DataTable>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed, inject, onMounted, ref, type Ref } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import { type DynamicDialogInstance } from 'primevue/dynamicdialogoptions';
@@ -50,81 +50,71 @@ interface NuclearAndGasRowDefinition {
   proportion?: string;
 }
 
-export default defineComponent({
-  name: 'TaxonomyShareDataTable',
-  components: { DataTable, Column },
-  inject: ['dialogRef'],
-  data() {
-    return {
-      nuclearAndGasData: {} as NuclearAndGasType,
-      columnHeaders: {} as { [key: string]: [columnName: string] },
-      listOfRowContents: [] as NuclearAndGasRowDefinition[],
-    };
-  },
-  computed: {
-    dialogRefData(): DialogRefData {
-      const dialogRefToDisplay = this.dialogRef as DynamicDialogInstance;
-      return dialogRefToDisplay.data as DialogRefData;
-    },
-    mainColumnDefinitions() {
-      if (isNonEligible(this.nuclearAndGasData)) {
-        return [
-          { field: 'economicActivity', header: 'Economic Activity' },
-          { field: 'proportion', header: 'Proportion' },
-        ];
-      } else {
-        return [
-          { field: 'economicActivity', header: 'Economic Activity' },
-          { field: 'mitigation', header: 'CCM' },
-          { field: 'adaptation', header: 'CCA' },
-          { field: 'mitigationAndAdaptation', header: 'CCM + CCA' },
-        ];
-      }
-    },
-  },
-  mounted() {
-    this.nuclearAndGasData = this.dialogRefData.input;
-    this.columnHeaders = this.dialogRefData.columnHeaders;
-    this.generateRowContents();
-  },
-  methods: {
-    /**
-     * Creates the rows to be displayed in the data table from the nuclearAndGasData input and populates the component's
-     * listOfRowContents
-     */
-    generateRowContents() {
-      if (!this.nuclearAndGasData) {
-        return;
-      }
+const dialogRef = inject<Ref<DynamicDialogInstance>>('dialogRef');
 
-      let listOfActivities: NuclearAndGasActivityDescription[];
-      if (isAlignedDenominator(this.nuclearAndGasData))
-        listOfActivities = nuclearAndGasActivityNames.alignedDenominator;
-      else if (isAlignedNumerator(this.nuclearAndGasData))
-        listOfActivities = nuclearAndGasActivityNames.alignedNumerator;
-      else if (isEligibleButNotAlignedNumerator(this.nuclearAndGasData))
-        listOfActivities = nuclearAndGasActivityNames.eligibleButNotAligned;
-      else if (isNonEligible(this.nuclearAndGasData)) listOfActivities = nuclearAndGasActivityNames.nonEligible;
+const nuclearAndGasData = ref<NuclearAndGasType>({} as NuclearAndGasType);
+const columnHeaders = ref<{ [key: string]: [string] }>({});
+const listOfRowContents = ref<NuclearAndGasRowDefinition[]>([]);
 
-      Object.keys(this.nuclearAndGasData).forEach((key, idx) => {
-        if (isNonEligible(this.nuclearAndGasData)) {
-          this.listOfRowContents.push({
-            economicActivity: listOfActivities[idx].description,
-            proportion: formatPercentageNumberAsString(this.nuclearAndGasData[key as keyof NuclearAndGasNonEligible]),
-          });
-        } else {
-          const objective = this.nuclearAndGasData[
-            key as keyof typeof this.nuclearAndGasData
-          ] as NuclearAndGasEnvironmentalObjective;
-          this.listOfRowContents.push({
-            economicActivity: listOfActivities[idx].description,
-            mitigation: formatPercentageNumberAsString(objective?.mitigation),
-            adaptation: formatPercentageNumberAsString(objective?.adaptation),
-            mitigationAndAdaptation: formatPercentageNumberAsString(objective?.mitigationAndAdaptation),
-          });
-        }
-      });
-    },
-  },
+const mainColumnDefinitions = computed(() => {
+  if (isNonEligible(nuclearAndGasData.value)) {
+    return [
+      { field: 'economicActivity', header: 'Economic Activity' },
+      { field: 'proportion', header: 'Proportion' },
+    ];
+  } else {
+    return [
+      { field: 'economicActivity', header: 'Economic Activity' },
+      { field: 'mitigation', header: 'CCM' },
+      { field: 'adaptation', header: 'CCA' },
+      { field: 'mitigationAndAdaptation', header: 'CCM + CCA' },
+    ];
+  }
 });
+
+const dialogRefData = computed(() => {
+  return (dialogRef?.value as DynamicDialogInstance).data as DialogRefData;
+});
+
+onMounted(() => {
+  nuclearAndGasData.value = dialogRefData.value.input;
+  columnHeaders.value = dialogRefData.value.columnHeaders;
+  generateRowContents();
+});
+
+/**
+ * Creates the rows to be displayed in the data table from the nuclearAndGasData input and populates the component's
+ * listOfRowContents
+ */
+function generateRowContents(): void {
+  if (!nuclearAndGasData.value) {
+    return;
+  }
+
+  let listOfActivities: NuclearAndGasActivityDescription[];
+  if (isAlignedDenominator(nuclearAndGasData.value)) listOfActivities = nuclearAndGasActivityNames.alignedDenominator;
+  else if (isAlignedNumerator(nuclearAndGasData.value)) listOfActivities = nuclearAndGasActivityNames.alignedNumerator;
+  else if (isEligibleButNotAlignedNumerator(nuclearAndGasData.value))
+    listOfActivities = nuclearAndGasActivityNames.eligibleButNotAligned;
+  else if (isNonEligible(nuclearAndGasData.value)) listOfActivities = nuclearAndGasActivityNames.nonEligible;
+
+  Object.keys(nuclearAndGasData.value).forEach((key, idx) => {
+    if (isNonEligible(nuclearAndGasData.value)) {
+      listOfRowContents.value.push({
+        economicActivity: listOfActivities[idx].description,
+        proportion: formatPercentageNumberAsString(nuclearAndGasData.value[key as keyof NuclearAndGasNonEligible]),
+      });
+    } else {
+      const objective = nuclearAndGasData.value[
+        key as keyof typeof nuclearAndGasData.value
+      ] as NuclearAndGasEnvironmentalObjective;
+      listOfRowContents.value.push({
+        economicActivity: listOfActivities[idx].description,
+        mitigation: formatPercentageNumberAsString(objective?.mitigation),
+        adaptation: formatPercentageNumberAsString(objective?.adaptation),
+        mitigationAndAdaptation: formatPercentageNumberAsString(objective?.mitigationAndAdaptation),
+      });
+    }
+  });
+}
 </script>
