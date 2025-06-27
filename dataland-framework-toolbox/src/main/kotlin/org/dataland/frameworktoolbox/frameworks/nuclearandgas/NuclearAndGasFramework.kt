c6452927ecee999c98cc064f5fa0622e30e7df8a
Fabian-dfine <Fabian-dfine@users.noreply.github.com>
2025-06-27 11:24:28 +0200

    DALA-5898: Toolbox run for NuG

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/nuclearandgas/NuclearAndGasFramework.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/nuclearandgas/NuclearAndGasFramework.kt
@@ -18,5 +18,5 @@ class NuclearAndGasFramework :
                 " 2021/2178, Annex XII ",
         File("./dataland-framework-toolbox/inputs/nuclear-and-gas/nuclear-and-gas.xlsx"),
         order = 4,
-        enabledFeatures = FrameworkGenerationFeatures.allExcept(FrameworkGenerationFeatures.Translations),
+        enabledFeatures = FrameworkGenerationFeatures.ENTRY_SET,
     )
