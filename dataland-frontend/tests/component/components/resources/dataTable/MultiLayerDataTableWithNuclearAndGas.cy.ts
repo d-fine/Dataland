7354672d22ffcbbd2bb11153a06c9c6a0fec8336
Fabian-dfine <Fabian-dfine@users.noreply.github.com>
2025-06-30 11:05:30 +0200

    DALA-5146: extended testing

--- dataland-frontend/tests/component/components/resources/dataTable/MultiLayerDataTableWithNuclearAndGas.cy.ts
+++ dataland-frontend/tests/component/components/resources/dataTable/MultiLayerDataTableWithNuclearAndGas.cy.ts
@@ -9,15 +9,16 @@ import { mountMLDTFrameworkPanelFromFakeFixture } from '@ct/testUtils/MultiLayer
 import { nuclearAndGasViewConfiguration } from '@/frameworks/nuclear-and-gas/ViewConfig';
 
 describe('Component Test for the nuclear and gas view Page with its components', () => {
-  let preparedFixtures: Array<FixtureData<NuclearAndGasData>>;
-  before(function () {
-    cy.fixture('CompanyInformationWithNuclearAndGasPreparedFixtures.json').then(function (jsonContent) {
-      preparedFixtures = jsonContent as Array<FixtureData<NuclearAndGasData>>;
+  let preparedFixturesNuG: Array<FixtureData<NuclearAndGasData>>;
+
+  before(function() {
+    cy.fixture('CompanyInformationWithNuclearAndGasPreparedFixtures.json').then(function(jsonContent) {
+      preparedFixturesNuG = jsonContent as Array<FixtureData<NuclearAndGasData>>;
     });
   });
 
-  it('Check that on the nuclear and gas view page the extended nuclearAndGas components work properly', () => {
-    const preparedFixture = getPreparedFixture('All-fields-defined-for-EU-NuclearAndGas-Framework', preparedFixtures);
+  it('Check that on the nuclear and gas view page the extended nuclearAndGas components work properly for aligned activities', () => {
+    const preparedFixture = getPreparedFixture('All-fields-defined-for-EU-NuclearAndGas-Framework', preparedFixturesNuG);
     mountMLDTFrameworkPanelFromFakeFixture(DataTypeEnum.NuclearAndGas, nuclearAndGasViewConfiguration, [
       preparedFixture,
     ]);
@@ -36,6 +37,31 @@ describe('Component Test for the nuclear and gas view Page with its components',
             expect(nextText.trim()).to.equal(
               preparedFixture.t.general?.taxonomyAlignedNumerator?.nuclearAndGasTaxonomyAlignedRevenueNumerator?.value
                 ?.taxonomyAlignedShareNumeratorNAndG426?.mitigation + ' %'
+            );
+          });
+      });
+  });
+
+  it('Check that on the nuclear and gas view page the extended nuclearAndGas components work properly for non aligned activities', () => {
+    const preparedFixture = getPreparedFixture('All-fields-defined-for-EU-NuclearAndGas-Framework', preparedFixturesNuG);
+    mountMLDTFrameworkPanelFromFakeFixture(DataTypeEnum.NuclearAndGas, nuclearAndGasViewConfiguration, [
+      preparedFixture,
+    ]);
+    getSectionHead('Taxonomy-non-eligible').should('exist');
+    getCellValueContainer('Nuclear and Gas Taxonomy-non-eligible Revenue').contains('Show').click();
+
+    cy.get('td')
+      .contains(
+        'Proportion of economic activity referred to in row 1 of Template 1 that is taxonomy-non-eligible in accordance with Section 4.26 of Annexes I and II to Delegated Regulation 2021/2139 in the denominator of the applicable KPI.'
+      )
+      .then(($td) => {
+        cy.wrap($td)
+          .next()
+          .invoke('text')
+          .then((nextText) => {
+            expect(nextText.trim()).to.equal(
+              preparedFixture.t.general?.taxonomyNonEligible?.nuclearAndGasTaxonomyNonEligibleRevenue?.value
+                ?.taxonomyNonEligibleShareNAndG426 + ' %'
             );
           });
       });
