3e2d81c161901d1196ba999738575aa222e3ffe9
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-06-30 14:32:39 +0200

    Created a function for component testing

--- dataland-frontend/tests/component/components/resources/dataTable/MultiLayerDataTableWithNuclearAndGas.cy.ts
+++ dataland-frontend/tests/component/components/resources/dataTable/MultiLayerDataTableWithNuclearAndGas.cy.ts
@@ -8,6 +8,37 @@ import {
 import { mountMLDTFrameworkPanelFromFakeFixture } from '@ct/testUtils/MultiLayerDataTableComponentTestUtils';
 import { nuclearAndGasViewConfiguration } from '@/frameworks/nuclear-and-gas/ViewConfig';
 
+/**
+ * Tests whether values in Nuclear and Gas tables are equal to their expected value
+ * @param fixture the fake fixture that is used for the expected value
+ * @param sectionHead the section header for the tested table
+ * @param cellValueContainer the name of the tested table
+ * @param activity the activity in the tested table
+ * @param expectedValue the value that is expected in the test
+ */
+function testTableValueNuclearAndGas(
+  fixture: FixtureData<NuclearAndGasData>,
+  sectionHead: string,
+  cellValueContainer: string,
+  activity: string,
+  expectedValue: string
+): void {
+  mountMLDTFrameworkPanelFromFakeFixture(DataTypeEnum.NuclearAndGas, nuclearAndGasViewConfiguration, [fixture]);
+  getSectionHead(sectionHead).should('exist');
+  getCellValueContainer(cellValueContainer).contains('Show').click();
+
+  cy.get('td')
+    .contains(activity)
+    .then(($td) => {
+      cy.wrap($td)
+        .next()
+        .invoke('text')
+        .then((nextText) => {
+          expect(nextText.trim()).to.equal(expectedValue);
+        });
+    });
+}
+
 describe('Component Test for the nuclear and gas view Page with its components', () => {
   let preparedFixturesNuG: Array<FixtureData<NuclearAndGasData>>;
 
@@ -22,27 +53,14 @@ describe('Component Test for the nuclear and gas view Page with its components',
       'All-fields-defined-for-EU-NuclearAndGas-Framework',
       preparedFixturesNuG
     );
-    mountMLDTFrameworkPanelFromFakeFixture(DataTypeEnum.NuclearAndGas, nuclearAndGasViewConfiguration, [
+    testTableValueNuclearAndGas(
       preparedFixture,
-    ]);
-    getSectionHead('Taxonomy-aligned (numerator)').should('exist');
-    getCellValueContainer('Nuclear and Gas Taxonomy-aligned Revenue (numerator)').contains('Show').click();
-
-    cy.get('td')
-      .contains(
-        'Proportion of taxonomy-aligned economic activity referred to in Section 4.26 of Annexes I and II to Delegated Regulation 2021/2139 in the numerator of the applicable KPI.'
-      )
-      .then(($td) => {
-        cy.wrap($td)
-          .next()
-          .invoke('text')
-          .then((nextText) => {
-            expect(nextText.trim()).to.equal(
-              preparedFixture.t.general?.taxonomyAlignedNumerator?.nuclearAndGasTaxonomyAlignedRevenueNumerator?.value
-                ?.taxonomyAlignedShareNumeratorNAndG426?.mitigation + ' %'
-            );
-          });
-      });
+      'Taxonomy-aligned (numerator)',
+      'Nuclear and Gas Taxonomy-aligned Revenue (numerator)',
+      'Proportion of taxonomy-aligned economic activity referred to in Section 4.26 of Annexes I and II to Delegated Regulation 2021/2139 in the numerator of the applicable KPI.',
+      preparedFixture.t.general?.taxonomyAlignedNumerator?.nuclearAndGasTaxonomyAlignedRevenueNumerator?.value
+        ?.taxonomyAlignedShareNumeratorNAndG426?.mitigation + ' %'
+    );
   });
 
   it('Check that on the nuclear and gas view page the extended nuclearAndGas components work properly for non aligned activities', () => {
@@ -50,26 +68,13 @@ describe('Component Test for the nuclear and gas view Page with its components',
       'All-fields-defined-for-EU-NuclearAndGas-Framework',
       preparedFixturesNuG
     );
-    mountMLDTFrameworkPanelFromFakeFixture(DataTypeEnum.NuclearAndGas, nuclearAndGasViewConfiguration, [
+    testTableValueNuclearAndGas(
       preparedFixture,
-    ]);
-    getSectionHead('Taxonomy-non-eligible').should('exist');
-    getCellValueContainer('Nuclear and Gas Taxonomy-non-eligible Revenue').contains('Show').click();
-
-    cy.get('td')
-      .contains(
-        'Proportion of economic activity referred to in row 1 of Template 1 that is taxonomy-non-eligible in accordance with Section 4.26 of Annexes I and II to Delegated Regulation 2021/2139 in the denominator of the applicable KPI.'
-      )
-      .then(($td) => {
-        cy.wrap($td)
-          .next()
-          .invoke('text')
-          .then((nextText) => {
-            expect(nextText.trim()).to.equal(
-              preparedFixture.t.general?.taxonomyNonEligible?.nuclearAndGasTaxonomyNonEligibleRevenue?.value
-                ?.taxonomyNonEligibleShareNAndG426 + ' %'
-            );
-          });
-      });
+      'Taxonomy-non-eligible',
+      'Nuclear and Gas Taxonomy-non-eligible Revenue',
+      'Proportion of economic activity referred to in row 1 of Template 1 that is taxonomy-non-eligible in accordance with Section 4.26 of Annexes I and II to Delegated Regulation 2021/2139 in the denominator of the applicable KPI.',
+      preparedFixture.t.general?.taxonomyNonEligible?.nuclearAndGasTaxonomyNonEligibleRevenue?.value
+        ?.taxonomyNonEligibleShareNAndG426 + ' %'
+    );
   });
 });
