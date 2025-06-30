da33e7889b5496c22e6a32c9e5f20982f3d90856
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-06-30 13:30:25 +0200

    Added BaseDatabaseMigrationTest class to prevent code duplication

--- dataland-qa-service/src/test/kotlin/db/migration/V10__UnifyNfrdMandatoryFieldTest.kt
+++ dataland-qa-service/src/test/kotlin/db/migration/V10__UnifyNfrdMandatoryFieldTest.kt
@@ -1,36 +1,15 @@
 package db.migration
 
-import org.flywaydb.core.api.migration.Context
-import org.junit.jupiter.api.BeforeEach
+import db.migration.utils.BaseDatabaseMigrationTest
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
-import org.mockito.kotlin.reset
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
-import java.sql.Connection
-import java.sql.DatabaseMetaData
-import java.sql.PreparedStatement
-import java.sql.ResultSet
-import java.sql.Statement
 
 @Suppress("ClassName")
-class V10__UnifyNfrdMandatoryFieldTest {
+class V10__UnifyNfrdMandatoryFieldTest : BaseDatabaseMigrationTest() {
     private val migration = V10__UnifyNfrdMandatoryField()
-    private val mockContext = mock<Context>()
-    private val mockConnection = mock<Connection>()
-    private val mockStatement = mock<Statement>()
-    private val mockMetaData = mock<DatabaseMetaData>()
-    private val mockResultSet = mock<ResultSet>()
-    private val mockPreparedStatement = mock<PreparedStatement>()
-
-    @BeforeEach
-    fun setup() {
-        reset(mockContext, mockConnection, mockStatement, mockMetaData, mockResultSet, mockPreparedStatement)
-        whenever(mockContext.connection).thenReturn(mockConnection)
-        whenever(mockConnection.metaData).thenReturn(mockMetaData)
-    }
 
     @Test
     fun `check that migration does not start if tables are missing`() {
--- dataland-qa-service/src/test/kotlin/db/migration/V9__UpdateSfdrCurrencyFieldsTest.kt
+++ dataland-qa-service/src/test/kotlin/db/migration/V9__UpdateSfdrCurrencyFieldsTest.kt
@@ -1,8 +1,7 @@
 package db.migration
 
-import org.flywaydb.core.api.migration.Context
+import db.migration.utils.BaseDatabaseMigrationTest
 import org.json.JSONObject
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.mockito.kotlin.any
@@ -10,32 +9,14 @@ import org.mockito.kotlin.argThat
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
-import org.mockito.kotlin.reset
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
-import java.sql.Connection
-import java.sql.DatabaseMetaData
-import java.sql.PreparedStatement
 import java.sql.ResultSet
-import java.sql.Statement
 
 @Suppress("ClassName")
-class V9__UpdateSfdrCurrencyFieldsTest {
+class V9__UpdateSfdrCurrencyFieldsTest : BaseDatabaseMigrationTest() {
     private val migration = V9__UpdateSfdrCurrencyFields()
-    private val mockContext = mock<Context>()
-    private val mockConnection = mock<Connection>()
-    private val mockStatement = mock<Statement>()
-    private val mockMetaData = mock<DatabaseMetaData>()
-    private val mockResultSet = mock<ResultSet>()
-    private val mockPreparedStatement = mock<PreparedStatement>()
-
-    @BeforeEach
-    fun setup() {
-        reset(mockContext, mockConnection, mockStatement, mockMetaData, mockResultSet, mockPreparedStatement)
-        whenever(mockContext.connection).thenReturn(mockConnection)
-        whenever(mockConnection.metaData).thenReturn(mockMetaData)
-    }
 
     @Test
     fun `check that migration does not start if tables are missing`() {
