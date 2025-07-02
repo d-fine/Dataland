f5a9057a72dc32baa0a3a4686a5a2f1d36cfe369
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-07-02 14:53:08 +0200

    annotations for nuclear and gas components

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/intermediate/components/JsonExamples.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/intermediate/components/JsonExamples.kt
@@ -160,4 +160,144 @@ object JsonExamples {
     } ] """
 
     const val EXAMPLE_PLAIN_EU_TAXONOMY_REPORTING_ASSURANCE_COMPONENT = """ "LimitedAssurance" """
+
+    const val EXAMPLE_PLAIN_NUCLEAR_AND_GAS_ALIGNED_DENOMINATOR_COMPONENT = """ {
+      "taxonomyAlignedShareDenominatorNAndG426" : {
+        "mitigationAndAdaptation" : 0.2,
+        "mitigation" : 0.3,
+        "adaptation" : 0.1
+      },
+      "taxonomyAlignedShareDenominatorNAndG427" : {
+        "mitigationAndAdaptation" : 1.2,
+        "mitigation" : 0.4,
+        "adaptation" : 0.2
+      },
+      "taxonomyAlignedShareDenominatorNAndG428" : {
+        "mitigationAndAdaptation" : 1,
+        "mitigation" : null,
+        "adaptation" : null
+      },
+      "taxonomyAlignedShareDenominatorNAndG429" : {
+        "mitigationAndAdaptation" : null,
+        "mitigation" : null,
+        "adaptation" : null
+      },
+      "taxonomyAlignedShareDenominatorNAndG430" : {
+        "mitigationAndAdaptation" : 0.3,
+        "mitigation" : 2.4,
+        "adaptation" : 0.5
+      },
+      "taxonomyAlignedShareDenominatorNAndG431" : {
+        "mitigationAndAdaptation" : 0.2,
+        "mitigation" : 0.5,
+        "adaptation" : 0.6
+      },
+      "taxonomyAlignedShareDenominatorOtherActivities" : {
+        "mitigationAndAdaptation" : 1.1,
+        "mitigation" : 1.4,
+        "adaptation" : 1.1
+      },
+      "taxonomyAlignedShareDenominator" : {
+        "mitigationAndAdaptation" : 4,
+        "mitigation" : 5,
+        "adaptation" : 2.5
+      }
+    }"""
+
+    const val EXAMPLE_PLAIN_NUCLEAR_AND_GAS_ALIGNED_NUMERATOR_COMPONENT = """{
+      "taxonomyAlignedShareNumeratorNAndG426" : {
+        "mitigationAndAdaptation" : 5,
+        "mitigation" : 6,
+        "adaptation" : 4
+      },
+      "taxonomyAlignedShareNumeratorNAndG427" : {
+        "mitigationAndAdaptation" : 30,
+        "mitigation" : 8,
+        "adaptation" : 8
+      },
+      "taxonomyAlignedShareNumeratorNAndG428" : {
+        "mitigationAndAdaptation" : 25,
+        "mitigation" : null,
+        "adaptation" : null
+      },
+      "taxonomyAlignedShareNumeratorNAndG429" : {
+        "mitigationAndAdaptation" : null,
+        "mitigation" : null,
+        "adaptation" : null
+      },
+      "taxonomyAlignedShareNumeratorNAndG430" : {
+        "mitigationAndAdaptation" : 7.5,
+        "mitigation" : 48,
+        "adaptation" : 20
+      },
+      "taxonomyAlignedShareNumeratorNAndG431" : {
+        "mitigationAndAdaptation" : 5,
+        "mitigation" : 10,
+        "adaptation" : 24
+      },
+      "taxonomyAlignedShareNumeratorOtherActivities" : {
+        "mitigationAndAdaptation" : 27.5,
+        "mitigation" : 28,
+        "adaptation" : 44
+      },
+      "taxonomyAlignedShareNumerator" : {
+        "mitigationAndAdaptation" : 100,
+        "mitigation" : 100,
+        "adaptation" : 100
+      }
+    }"""
+
+    const val EXAMPLE_PLAIN_NUCLEAR_AND_GAS_ELIGIBLE_BUT_NOT_ALIGNED_COMPONENT = """  {
+      "taxonomyEligibleButNotAlignedShareNAndG426" : {
+        "mitigationAndAdaptation" : 1.2,
+        "mitigation" : 1.3,
+        "adaptation" : 1.1
+      },
+      "taxonomyEligibleButNotAlignedShareNAndG427" : {
+        "mitigationAndAdaptation" : 2.2,
+        "mitigation" : 1.4,
+        "adaptation" : 1.2
+      },
+      "taxonomyEligibleButNotAlignedShareNAndG428" : {
+        "mitigationAndAdaptation" : 2,
+        "mitigation" : 1,
+        "adaptation" : 1
+      },
+      "taxonomyEligibleButNotAlignedShareNAndG429" : {
+        "mitigationAndAdaptation" : null,
+        "mitigation" : null,
+        "adaptation" : null
+      },
+      "taxonomyEligibleButNotAlignedShareNAndG430" : {
+        "mitigationAndAdaptation" : 1.3,
+        "mitigation" : 3.4,
+        "adaptation" : 1.5
+      },
+      "taxonomyEligibleButNotAlignedShareNAndG431" : {
+        "mitigationAndAdaptation" : 1.2,
+        "mitigation" : 1.5,
+        "adaptation" : 1.6
+      },
+      "taxonomyEligibleButNotAlignedShareOtherActivities" : {
+        "mitigationAndAdaptation" : 2.2,
+        "mitigation" : 2.1,
+        "adaptation" : 2
+      },
+      "taxonomyEligibleButNotAlignedShare" : {
+        "mitigationAndAdaptation" : 10.1,
+        "mitigation" : 10.7,
+        "adaptation" : 8.4
+      }
+    } """
+
+    const val EXAMPLE_PLAIN_NUCLEAR_AND_GAS_NON_ELIGIBLE_COMPONENT = """ {
+      "taxonomyNonEligibleShareNAndG426" : 1.1,
+      "taxonomyNonEligibleShareNAndG427" : 1.2,
+      "taxonomyNonEligibleShareNAndG428" : 0.6,
+      "taxonomyNonEligibleShareNAndG429" : 0.5,
+      "taxonomyNonEligibleShareNAndG430" : 2.8,
+      "taxonomyNonEligibleShareNAndG431" : 1.2,
+      "taxonomyNonEligibleShareOtherActivities" : 1.9,
+      "taxonomyNonEligibleShare" : 9.3
+    } """
 }
