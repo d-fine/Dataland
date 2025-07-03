32d47ef291c504b5406d004225e1f93e42e087a4
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-07-03 16:17:30 +0200

    Added descriptions and examples for Users service

--- dataland-user-service/src/main/kotlin/org/dataland/datalanduserservice/model/BasePortfolioName.kt
+++ dataland-user-service/src/main/kotlin/org/dataland/datalanduserservice/model/BasePortfolioName.kt
@@ -1,6 +1,8 @@
 package org.dataland.datalanduserservice.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+import org.dataland.datalandbackendutils.utils.UsersOpenApiDescriptionsAndExamples
 
 /**
  * --- API model ---
@@ -9,8 +11,16 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class BasePortfolioName(
     @field:JsonProperty(required = true)
+    @field:Schema(
+        description = UsersOpenApiDescriptionsAndExamples.PORTFOLIO_ID_DESCRIPTION,
+        example = UsersOpenApiDescriptionsAndExamples.PORTFOLIO_ID_EXAMPLE,
+    )
     val portfolioId: String,
     @field:JsonProperty(required = true)
+    @field:Schema(
+        description = UsersOpenApiDescriptionsAndExamples.PORTFOLIO_NAME_DESCRIPTION,
+        example = UsersOpenApiDescriptionsAndExamples.PORTFOLIO_NAME_EXAMPLE,
+    )
     override val portfolioName: String,
 ) : PortfolioName {
     constructor(portfolio: BasePortfolio) : this(
