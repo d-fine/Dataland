ca1b27ec336ca09dccca3c86a3a17afb06ae0f25
Leonard Wienke <206227392+LeonardWienke@users.noreply.github.com>
2025-07-06 18:39:41 +0200

    Update Fixtures

--- dataland-frontend/tests/e2e/specs/prepopulation/AwaitPrepopulation.ts
+++ dataland-frontend/tests/e2e/specs/prepopulation/AwaitPrepopulation.ts
@@ -2,60 +2,63 @@ import { countCompaniesAndDatasetsForDataType } from '@e2e/utils/GeneralApiUtils
 import { getKeycloakToken } from '@e2e/utils/Auth';
 import { reader_name, reader_pw } from '@e2e/utils/Cypress';
 import { describeIf } from '@e2e/support/TestUtility';
+import { frameworkFixtureMap } from '@e2e/utils/FixtureMap';
 import { getAllPublicFrameworkIdentifiers } from '@/frameworks/BasePublicFrameworkRegistry';
 import { convertKebabCaseToPascalCase } from '@/utils/StringFormatter';
-import { DataTypeEnum } from '@clients/backend';
 
 describeIf(
-    'I want to ensure that the prepopulation has finished before executing any further tests',
-    {
-        executionEnvironments: ['developmentLocal', 'ci', 'developmentCd'],
-    },
-    () => {
-        let expectedNumberOfCompanies = 0;
-        let prepopulatedDataTypes: string[] = [];
+  'I want to ensure that the prepopulation has finished before executing any further tests',
+  {
+    executionEnvironments: ['developmentLocal', 'ci', 'developmentCd'],
+  },
+  () => {
+    let expectedNumberOfCompanies = 0;
+    let prepopulatedDataTypes: string[] = [];
 
-        before(function () {
-            prepopulatedDataTypes = getAllPublicFrameworkIdentifiers();
-            const fixtureFiles = prepopulatedDataTypes.map((dataType) =>
-                `CompanyInformationWith${convertKebabCaseToPascalCase(dataType)}Data`
+    before(function () {
+      const dataTypesWithoutToolboxSupport = Object.keys(frameworkFixtureMap);
+      const dataTypesWithToolboxSupport = getAllPublicFrameworkIdentifiers();
+      prepopulatedDataTypes = dataTypesWithoutToolboxSupport.concat(dataTypesWithToolboxSupport);
+      const fixtures = Object.values(frameworkFixtureMap).concat(
+        dataTypesWithToolboxSupport.map((dataType) =>
+          `CompanyInformationWith${convertKebabCaseToPascalCase(dataType)}Data`.replace('-', '')
+        )
+      );
+      fixtures.forEach((fixtureFile) => {
+        cy.fixture(fixtureFile).then(function (companies: []) {
+          expectedNumberOfCompanies += companies.length;
+        });
+      });
+    });
+
+    it(
+      'Should wait until prepopulation has finished',
+      {
+        retries: {
+          runMode: Cypress.env('AWAIT_PREPOPULATION_RETRIES') as number,
+          openMode: Cypress.env('AWAIT_PREPOPULATION_RETRIES') as number,
+        },
+      },
+      () => {
+        const delayToWaitForPrepopulationSoThatNotAllRetriesAreWastedInstantly = 5000;
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(delayToWaitForPrepopulationSoThatNotAllRetriesAreWastedInstantly)
+          .then(() => getKeycloakToken(reader_name, reader_pw))
+          .then({ timeout: 150000 }, async (token) => {
+            const responsePromises = prepopulatedDataTypes.map((key) =>
+              countCompaniesAndDatasetsForDataType(token, key as keyof typeof frameworkFixtureMap)
             );
 
-            fixtureFiles.forEach((fixtureFile) => {
-                cy.fixture(fixtureFile).then((companies: []) => {
-                    expectedNumberOfCompanies += companies.length;
-                });
-            });
-        });
+            const totalCompanies = (await Promise.all(responsePromises))
+              .map((it) => it.numberOfCompaniesForDataType)
+              .reduce((x, y) => x + y, 0);
 
-        it(
-            'Should wait until prepopulation has finished',
-            {
-                retries: {
-                    runMode: Cypress.env('AWAIT_PREPOPULATION_RETRIES') as number,
-                    openMode: Cypress.env('AWAIT_PREPOPULATION_RETRIES') as number,
-                },
-            },
-            () => {
-                const delayToWaitForPrepopulationSoThatNotAllRetriesAreWastedInstantly = 5000;
-
-                cy.wait(delayToWaitForPrepopulationSoThatNotAllRetriesAreWastedInstantly)
-                    .then(() => getKeycloakToken(reader_name, reader_pw))
-                    .then({ timeout: 120000 }, async (token) => {
-                        const responsePromises = prepopulatedDataTypes.map((key) =>
-                            countCompaniesAndDatasetsForDataType(token, DataTypeEnum[key as keyof typeof DataTypeEnum])
-                        );
-
-                        const totalCompanies = (await Promise.all(responsePromises))
-                            .map((res) => res.numberOfCompaniesForDataType)
-                            .reduce((acc, val) => acc + val, 0);
-
-                        assert(
-                            totalCompanies >= expectedNumberOfCompanies,
-                            `Found ${totalCompanies} companies (Expecting at least ${expectedNumberOfCompanies})`
-                        );
-                    });
-            }
-        );
-    }
+            assert(
+              totalCompanies >= expectedNumberOfCompanies,
+              `Found ${totalCompanies} companies (Expecting at least ${expectedNumberOfCompanies})`
+            );
+          });
+      }
+    );
+  }
 );
---
+++ dataland-frontend/tests/e2e/utils/FixtureMap.ts
@@ -0,0 +1,5 @@
+import { DataTypeEnum } from '@clients/backend';
+
+export const frameworkFixtureMap = {
+    [DataTypeEnum.Vsme]: 'CompanyInformationWithVsmeData',
+};
