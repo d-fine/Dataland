b353f9ce652dabf1d1bae1367f32292a6005400b
Dennis Groh <173401487+dennis-dfine@users.noreply.github.com>
2025-07-11 16:14:44 +0200

    Fix portfolio details test

--- dataland-frontend/src/assets/theme/dataland-theme.ts
+++ dataland-frontend/src/assets/theme/dataland-theme.ts
@@ -69,6 +69,9 @@ export const DatalandPreset = definePreset(Aura, {
       select: {
         borderRadius: '{border.radius.none}',
       },
+      popover: {
+        borderRadius: '{border.radius.none}',
+      },
     },
     navigation: {
       item: {
