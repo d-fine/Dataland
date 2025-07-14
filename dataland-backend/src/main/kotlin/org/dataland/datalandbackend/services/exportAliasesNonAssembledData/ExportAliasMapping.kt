966072d439dc1b3c5065a036d4ee6b11d86da1e6
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-07-14 16:29:10 +0200

    remove unused interface

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/exportAliasesNonAssembledData/ExportAliasMapping.kt
+++
@@ -1,8 +0,0 @@
-package org.dataland.datalandbackend.services.exportAliasesNonAssembledData
-
-/**
- *  An interface for the hardcoded mapping fieldname to export alias
- */
-interface ExportAliasMapping {
-    val fieldNameToReadableName: Map<String, String>
-}
