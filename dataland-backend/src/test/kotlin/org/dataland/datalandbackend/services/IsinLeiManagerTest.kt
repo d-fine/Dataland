54af09dc17b54162bbea09bdd726de6bf7a31921
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-07-18 16:08:04 +0200

    Changed IsinLeiManagerTest.kt to use PostgreSQLContainer

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/IsinLeiManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/IsinLeiManagerTest.kt
@@ -6,27 +6,55 @@ import org.dataland.datalandbackend.entities.StoredCompanyEntity
 import org.dataland.datalandbackend.model.IsinLeiMappingData
 import org.dataland.datalandbackend.model.companies.CompanyInformation
 import org.dataland.datalandbackend.model.enums.company.IdentifierType
+import org.dataland.datalandbackend.repositories.CompanyIdentifierRepository
 import org.dataland.datalandbackend.repositories.IsinLeiRepository
 import org.dataland.datalandbackend.repositories.StoredCompanyRepository
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import javax.sql.DataSource
 
-@SpringBootTest(classes = [DatalandBackend::class], properties = ["spring.profiles.active=nodb"])
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@SpringBootTest(classes = [DatalandBackend::class])
+@Testcontainers
 class IsinLeiManagerTest(
     @Autowired private val storedCompanyRepository: StoredCompanyRepository,
     @Autowired private val isinLeiRepository: IsinLeiRepository,
     @Autowired private val companyAlterationManager: CompanyAlterationManager,
+    @Autowired private val companyIdentifierRepository: CompanyIdentifierRepository,
     @Autowired private val dataSource: DataSource,
 ) {
+    companion object {
+        @Container
+        @JvmStatic
+        val postgres: PostgreSQLContainer<*> =
+            PostgreSQLContainer("postgres:15-alpine")
+                .withDatabaseName("dataland_test")
+                .withUsername("test")
+                .withPassword("test")
+                .withReuse(true)
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun configureProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgres::getJdbcUrl)
+            registry.add("spring.datasource.username", postgres::getUsername)
+            registry.add("spring.datasource.password", postgres::getPassword)
+            registry.add("spring.datasource.driver-class-name") { "org.postgresql.Driver" }
+            registry.add("spring.jpa.database-platform") { "org.hibernate.dialect.PostgreSQLDialect" }
+            registry.add("spring.jpa.hibernate.ddl-auto") { "create-drop" }
+            registry.add("spring.jpa.show-sql") { "false" }
+            registry.add("spring.jpa.properties.hibernate.format_sql") { "false" }
+            registry.add("spring.flyway.enabled") { "false" }
+        }
+    }
+
     val dummyLei1 = "LEI123456789"
     val dummyLei2 = "LEI987654321"
     val dummyIsin1 = "123456789"
@@ -89,6 +117,9 @@ class IsinLeiManagerTest(
 
     @BeforeEach
     fun setup() {
+        isinLeiRepository.deleteAll()
+        companyIdentifierRepository.deleteAll()
+        storedCompanyRepository.deleteAll()
         isinLeiManager =
             IsinLeiManager(
                 storedCompanyRepository = storedCompanyRepository,
