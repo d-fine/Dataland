fdc459ff1ae8493fa902d0af8d139f96df3d1c16
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-07-22 09:14:14 +0200

    Added Async Configuration class

---
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/configurations/AsyncConfig.kt
@@ -0,0 +1,8 @@
+package org.dataland.datalandbackend.configurations
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@Configuration
+@EnableAsync
+class AsyncConfig
