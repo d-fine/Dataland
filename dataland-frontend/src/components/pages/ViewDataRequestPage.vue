2ae48b31c1a476908e009d658a80b92480cba422
Fabian-dfine <Fabian-dfine@users.noreply.github.com>
2025-07-30 14:04:50 +0200

    DALA-3674: back button

--- dataland-frontend/src/components/pages/ViewDataRequestPage.vue
+++ dataland-frontend/src/components/pages/ViewDataRequestPage.vue
@@ -1,9 +1,9 @@
 <template>
   <AuthenticationWrapper>
-    <BackButton />
     <TheContent class="min-h-screen flex sheet">
       <TheHeader />
       <div class="headline" style="margin-left: 1rem; margin-top: 0.5rem">
+        <BackButton style="display: flex"/>
         <h1 class="text-left">Data Request</h1>
       </div>
 
