17b4e9ad783c665597e2b7f17f9c47d057d9709b
S-Hofmann1 <192322604+SimonHofmann12@users.noreply.github.com>
2025-08-05 18:07:01 +0200

    Fixed PortfolioDetails.cy.ts

--- dataland-frontend/tests/component/components/resources/portfolio/PortfolioDetails.cy.ts
+++ dataland-frontend/tests/component/components/resources/portfolio/PortfolioDetails.cy.ts
@@ -115,7 +115,7 @@ describe('Check the portfolio details view', function (): void {
       props: { portfolioId: portfolioFixture.portfolioId },
     }).then(() => {
       cy.wait('@downloadComplete').then(() => {
-        cy.get('[data-test="monitor-portfolio"]').should('be.disabled').and('contain.text', 'Edit Monitoring');
+        cy.get('[data-test="monitor-portfolio"]').should('be.disabled').and('contain.text', 'Active Monitoring');
       });
     });
   });
@@ -129,7 +129,7 @@ describe('Check the portfolio details view', function (): void {
       props: { portfolioId: portfolioFixture.portfolioId },
     }).then(() => {
       cy.wait('@downloadComplete').then(() => {
-        cy.get('[data-test="monitor-portfolio"]').should('be.visible').and('contain.text', 'ACTIVE MONITORING');
+        cy.get('[data-test="monitor-portfolio"]').should('be.visible').and('contain.text', 'Active Monitoring');
         cy.get('[data-test="is-not-monitored-badge"]')
           .should('be.visible')
           .and('contain.text', 'Portfolio not actively monitored');
