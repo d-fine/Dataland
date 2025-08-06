049e872df1931235915041ae5111c1e0c7b33219
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-08-06 14:32:02 +0200

    Revert "Only use one fetch query"
    This reverts commit cfb517b758041d5fbe4198003ae7788d6ac08f65.

--- dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
+++ dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/ProcessDataUpdates.kt
@@ -114,7 +114,7 @@ class ProcessDataUpdates
 
         // ToDo remove before merge
         @Suppress("UnusedPrivateMember") // Detect does not recognise the scheduled execution of this function
-        @Scheduled(cron = "0 50 11 * * *")
+        @Scheduled(cron = "0 50 7 * * *")
         private fun testIsinLeiMapping() {
             processUpdates()
         }
