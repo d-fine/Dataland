0e2208f687ac1d12135028414c22c3aa2319d76d
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2025-08-11 10:51:43 +0200

    create tables first

--- dataland-backend/src/main/kotlin/db/migration/V8__GenerateIsinLeiIndex.kt
+++ dataland-backend/src/main/kotlin/db/migration/V8__GenerateIsinLeiIndex.kt
@@ -10,7 +10,13 @@ import org.flywaydb.core.api.migration.Context
 class V8__GenerateIsinLeiIndex : BaseJavaMigration() {
     override fun migrate(context: Context?) {
         context!!.connection.createStatement().execute(
-            "CREATE INDEX idx_isin " +
+            "CREATE TABLE isin_lei_mapping (" +
+                "isin varchar(255) NOT NULL, " +
+                "lei varchar(255), " +
+                "company_id varchar(255), " +
+                "PRIMARY KEY (isin)" +
+                "); " +
+                "CREATE INDEX idx_isin " +
                 "ON isin_lei_mapping " +
                 "USING gin (isin gin_trgm_ops);",
         )
