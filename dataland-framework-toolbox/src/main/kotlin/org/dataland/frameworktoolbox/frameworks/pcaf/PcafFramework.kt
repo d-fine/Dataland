2f56ebaed383a1c5b99105f1f50e3ec4c54244b1
Stephan Wezorke <186339469+StephanWezorke@users.noreply.github.com>
2025-08-21 17:49:22 +0200

    First draft for PCAF framework in toolbox

---
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/pcaf/PcafFramework.kt
@@ -0,0 +1,76 @@
+package org.dataland.frameworktoolbox.frameworks.pcaf
+
+import org.dataland.frameworktoolbox.frameworks.FrameworkGenerationFeatures
+import org.dataland.frameworktoolbox.frameworks.PavedRoadFramework
+import org.dataland.frameworktoolbox.intermediate.Framework
+import org.dataland.frameworktoolbox.intermediate.components.SingleSelectComponent
+import org.dataland.frameworktoolbox.intermediate.group.ComponentGroup
+import org.dataland.frameworktoolbox.intermediate.group.ComponentGroupApi
+import org.dataland.frameworktoolbox.intermediate.group.edit
+import org.dataland.frameworktoolbox.specific.viewconfig.elements.LabelBadgeColor
+import org.springframework.stereotype.Component
+import java.io.File
+
+/**
+ * Definition of the PCAF framework
+ */
+@Component
+class PcafFramework :
+    PavedRoadFramework(
+        identifier = "pcaf",
+        label = "PCAF",
+        explanation = "Partnership for Carbon Accounting Financials",
+        File("./dataland-framework-toolbox/inputs/pcaf/pcaf.xlsx"),
+        order = 6,
+        enabledFeatures = FrameworkGenerationFeatures.ENTRY_SET,
+    ) {
+    override fun customizeHighLevelIntermediateRepresentation(framework: Framework) {
+        /*framework.root.edit<ComponentGroup>("general") {
+            edit<ComponentGroup>("general") {
+                edit<ReportPreuploadComponent>("referencedReports") {
+                    isPartOfQaReport = false
+                }
+            }
+        }*/
+        setSectionColorsAndExpansion(framework.root)
+        overwriteDataPointSpecificationForEnums(framework.root)
+    }
+
+    private fun overwriteDataPointSpecificationForEnums(root: ComponentGroupApi) {
+        root.edit<ComponentGroup>("general") {
+            edit<ComponentGroup>("general") {
+                edit<SingleSelectComponent>("fiscalYearDeviation") {
+                    specificationGenerator = { categoryBuilder ->
+                        categoryBuilder.addDefaultDatapointAndSpecification(
+                            this,
+                            "Enum",
+                            "plainEnumFiscalYearDeviation",
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setSectionColorsAndExpansion(root: ComponentGroupApi) {
+        root.edit<ComponentGroup>("general") {
+            viewPageLabelBadgeColor = LabelBadgeColor.Orange
+            viewPageExpandOnPageLoad = true
+
+            uploadPageLabelBadgeColor = LabelBadgeColor.Orange
+            edit<ComponentGroup>("general") {
+                viewPageExpandOnPageLoad = true
+            }
+        }
+
+        root.edit<ComponentGroup>("companyValue") {
+            viewPageLabelBadgeColor = LabelBadgeColor.Yellow
+            uploadPageLabelBadgeColor = LabelBadgeColor.Yellow
+        }
+
+        root.edit<ComponentGroup>("environmental") {
+            viewPageLabelBadgeColor = LabelBadgeColor.Green
+            uploadPageLabelBadgeColor = LabelBadgeColor.Green
+        }
+    }
+}
