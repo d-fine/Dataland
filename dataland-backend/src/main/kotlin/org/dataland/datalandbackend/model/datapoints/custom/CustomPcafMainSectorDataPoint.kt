0fe3c7a2d829d62768049b5d9f0c6f9a12a60ac1
Stephan Wezorke <186339469+StephanWezorke@users.noreply.github.com>
2025-08-25 10:20:37 +0200

    Fix backend tests

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/datapoints/custom/CustomPcafMainSectorDataPoint.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/datapoints/custom/CustomPcafMainSectorDataPoint.kt
@@ -3,8 +3,9 @@ package org.dataland.datalandbackend.model.datapoints.custom
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.Valid
 import org.dataland.datalandbackend.frameworks.pcaf.model.general.company.PcafGeneralCompanyMainPcafSectorOptions
-import org.dataland.datalandbackend.interfaces.datapoints.BaseDataPoint
+import org.dataland.datalandbackend.interfaces.datapoints.ExtendedDataPoint
 import org.dataland.datalandbackend.model.documents.ExtendedDocumentReference
+import org.dataland.datalandbackend.model.enums.data.QualityOptions
 
 /**
  * --- API model ---
@@ -13,7 +14,9 @@ import org.dataland.datalandbackend.model.documents.ExtendedDocumentReference
 data class CustomPcafMainSectorDataPoint(
     @field:JsonProperty(required = true)
     override val value: PcafGeneralCompanyMainPcafSectorOptions,
+    override val quality: QualityOptions? = null,
+    override val comment: String? = null,
     @field:Valid()
     override val dataSource: ExtendedDocumentReference? = null,
     val provider: String? = null,
-) : BaseDataPoint<PcafGeneralCompanyMainPcafSectorOptions>
+) : ExtendedDataPoint<PcafGeneralCompanyMainPcafSectorOptions>
