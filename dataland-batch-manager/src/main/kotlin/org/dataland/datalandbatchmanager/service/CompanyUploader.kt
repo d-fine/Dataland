3d6e69bcab85dfddd5f73a80da3da6ee03030063
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-08-28 11:48:28 +0200

    add further log statements for debugging

--- dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/CompanyUploader.kt
+++ dataland-batch-manager/src/main/kotlin/org/dataland/datalandbatchmanager/service/CompanyUploader.kt
@@ -137,6 +137,7 @@ class CompanyUploader(
             try {
                 logger.info("Uploading company data for ${companyInformation.getNameAndIdentifier()} ")
                 companyDataControllerApi.postCompany(companyInformation.toCompanyPost())
+                logger.info("Post for ${companyInformation.getNameAndIdentifier()}: ${companyInformation.toCompanyPost()}")
             } catch (exception: ClientException) {
                 val (conflictingCompanyId, conflictingIdentifiers) =
                     checkForDuplicateIdentifierAndGetConflictingCompanyId(exception)
@@ -169,12 +170,15 @@ class CompanyUploader(
         var existingAlternativeNames: List<String>? = emptyList()
         executeWithRetryAndThrottling {
             logger.info("Getting single company data for ${companyInformation.getNameAndIdentifier()}")
-            existingAlternativeNames = companyDataControllerApi.getCompanyById(companyId).companyInformation.companyAlternativeNames
+            existingAlternativeNames =
+                companyDataControllerApi.getCompanyById(companyId).companyInformation.companyAlternativeNames
+            logger.info("Existing alternative names for ${companyInformation.getNameAndIdentifier()}: $existingAlternativeNames")
         }
         val companyPatch = companyInformation.toCompanyPatch(conflictingIdentifiers, existingAlternativeNames) ?: return
         executeWithRetryAndThrottling {
             logger.info("Patching single company data for ${companyInformation.getNameAndIdentifier()}")
             companyDataControllerApi.patchCompanyById(companyId, companyPatch)
+            logger.info("Patch for ${companyInformation.getNameAndIdentifier()}: $companyPatch")
         }
     }
 
