2b1a97a68e0f7b907db6182c29a39008b0a35f3e
Fabian-dfine <Fabian-dfine@users.noreply.github.com>
2025-09-03 22:39:26 +0200

    fixed test

--- dataland-frontend/tests/e2e/specs/search-company-framework-data/SearchCompaniesForFrameworkDataDropdownFilter.ts
+++ dataland-frontend/tests/e2e/specs/search-company-framework-data/SearchCompaniesForFrameworkDataDropdownFilter.ts
@@ -45,7 +45,7 @@ describe('As a user, I expect the search functionality on the /companies page to
   const frameworkTwo = ALL_FRAMEWORKS_IN_ENUM_CLASS_ORDER[1];
   const frameworkThree = ALL_FRAMEWORKS_IN_ENUM_CLASS_ORDER[2];
 
-  it('The framework filter synchronize between the search bar and the URL', { scrollBehavior: false }, () => {
+  it.only('The framework filter synchronize between the search bar and the URL', { scrollBehavior: false }, () => {
     cy.ensureLoggedIn();
     cy.intercept('**/api/companies/meta-information').as('companies-meta-information');
     cy.visit('/companies').wait('@companies-meta-information');
@@ -173,19 +173,19 @@ describe('As a user, I expect the search functionality on the /companies page to
       cy.intercept('**/api/companies/meta-information').as('companies-meta-information');
       cy.visit('/companies').wait('@companies-meta-information');
       verifySearchResultTableExists();
-      cy.get('#framework-filter').click();
+      cy.get('framework-filter').click();
       cy.get('div.p-multiselect-overlay').should('exist');
       cy.scrollTo(0, 500, { duration: 300 });
       cy.get('div.p-multiselect-overlay').should('not.exist');
-      cy.get('#framework-filter').click();
+      cy.get('framework-filter').click();
       cy.get('div.p-multiselect-overlay').should('exist');
       cy.scrollTo(0, 600, { duration: 300 });
       cy.get('div.p-multiselect-overlay').should('not.exist');
-      cy.get('#framework-filter').click();
+      cy.get('framework-filter').click();
       cy.get('div.p-multiselect-overlay').should('exist');
       cy.scrollTo(0, 500, { duration: 300 });
       cy.get('div.p-multiselect-overlay').should('not.exist');
-      cy.get('#framework-filter').click();
+      cy.get('framework-filter').click();
       cy.get('div.p-multiselect-overlay')
         .should('be.visible')
         .and('contain.text', humanizeStringOrNumber(frameworkOne))
