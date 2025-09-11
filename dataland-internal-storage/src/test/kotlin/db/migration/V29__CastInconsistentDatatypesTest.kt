2164900c1c437e6d6793624e4c21b2d9f0b9b35e
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-09-11 11:23:15 +0200

    Added tests for migration script

--- dataland-internal-storage/src/test/kotlin/db/migration/V29__CastInconsistentDatatypesTest.kt
+++ dataland-internal-storage/src/test/kotlin/db/migration/V29__CastInconsistentDatatypesTest.kt
@@ -1,6 +1,40 @@
 package db.migration
 
+import db.migration.utils.TestUtils
+import org.junit.jupiter.api.Test
+
 @Suppress("ClassName")
 class V29__CastInconsistentDatatypesTest {
-    // TODO: Implement test cases for V29__CastInconsistentDatatypes migration
+    @Test
+    fun `check that inconsistent datatypes are cast correctly`() {
+        TestUtils().testMigrationOfSingleDatapoint(
+            "extendedDecimalRateOfAccidents",
+            "extendedDecimalRateOfAccidents",
+            "V29/originalExtendedDecimalWithStringRepresentation.json",
+            "V29/expectedExtendedDecimalWithStringRepresentation.json",
+            V29__CastInconsistentDatatypes()::castDataPoint,
+        )
+    }
+
+    @Test
+    fun `check that data points with empty strings are cast correctly`() {
+        TestUtils().testMigrationOfSingleDatapoint(
+            "extendedDecimalScope1GhgEmissionsInTonnes",
+            "extendedDecimalScope1GhgEmissionsInTonnes",
+            "V29/originalDataPointWithEmptyStringValue.json",
+            "V29/expectedDataPointWithEmptyStringValue.json",
+            V29__CastInconsistentDatatypes()::castDataPoint,
+        )
+    }
+
+    @Test
+    fun `check that data points with integer page number are cast correctly`() {
+        TestUtils().testMigrationOfSingleDatapoint(
+            "extendedDecimalScope2GhgEmissionsInTonnes",
+            "extendedDecimalScope2GhgEmissionsInTonnes",
+            "V29/originalDataPointWithIntegerPageNumber.json",
+            "V29/expectedDataPointWithIntegerPageNumber.json",
+            V29__CastInconsistentDatatypes()::castDataPoint,
+        )
+    }
 }
