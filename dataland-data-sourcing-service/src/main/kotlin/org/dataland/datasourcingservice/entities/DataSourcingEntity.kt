f8e48921ec63d143e2b2e67a413321d87993543b
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-09-17 10:06:57 +0200

    introduce data sourcing entity

--- dataland-data-sourcing-service/src/main/kotlin/org/dataland/datasourcingservice/entities/DataSourcingEntity.kt
+++ dataland-data-sourcing-service/src/main/kotlin/org/dataland/datasourcingservice/entities/DataSourcingEntity.kt
@@ -1,17 +1,20 @@
 package org.dataland.datasourcingservice.entities
 
 import jakarta.persistence.Column
+import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.dataland.datasourcingservice.model.enums.DataSourcingState
+import java.util.Date
 import java.util.UUID
 
 /**
  * The database entity for storing data sourcing objects.
  */
+@SuppressWarnings("LongParameterList")
 @Entity
 @Table(name = "data_sourcing")
 class DataSourcingEntity(
@@ -27,4 +30,18 @@ class DataSourcingEntity(
     val dataType: String,
     @Column(name = "state")
     val state: DataSourcingState,
+    @ElementCollection
+    @Column(name = "document_id")
+    val documentIds: Set<UUID>? = null,
+    @ElementCollection
+    @Column(name = "expected_publication_date_of_documents")
+    val expectedPublicationDatesOfDocuments: Set<ExpectedPublicationDateOfDocument>? = null,
+    @Column(name = "date_document_sourcing_attempt")
+    val dateDocumentSourcingAttempt: Date? = null,
+    @Column(name = "document_collector")
+    val documentCollector: UUID? = null,
+    @Column(name = "data_extractor")
+    val dataExtractor: UUID? = null,
+    @Column(name = "admin_comment", length = 1000)
+    var adminComment: String? = null,
 )
