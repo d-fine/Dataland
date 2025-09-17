b30d5071fd9b33ee2e7b128548201ad183399537
S-Hofmann1 <192322604+SimonHofmann12@users.noreply.github.com>
2025-09-17 15:11:20 +0200

    On login the default should be the companies page for a user without a portfolio.

--- dataland-frontend/tests/e2e/utils/Auth.ts
+++ dataland-frontend/tests/e2e/utils/Auth.ts
@@ -41,7 +41,7 @@ export function login(username = reader_name, password = reader_pw, otpGenerator
       .should('exist')
       .click();
   }
-  cy.url({ timeout: Cypress.env('long_timeout_in_ms') as number }).should('eq', getBaseUrl() + '/portfolios');
+  cy.url({ timeout: Cypress.env('long_timeout_in_ms') as number }).should('eq', getBaseUrl() + '/companies');
   cy.wait('@getPortfolios', { timeout: Cypress.env('long_timeout_in_ms') as number }).then((interception) => {
     globalJwt = interception.request.headers['authorization'] as string;
   });
