072b00610999afcc376ecb605d78825886f8190d
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-09-19 17:02:37 +0200

    Update blocking comment

--- dataland-batch-manager/build.gradle.kts
+++ dataland-batch-manager/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("io.github.resilience4j:resilience4j-spring-boot2:2.3.0")
     implementation("io.github.resilience4j:resilience4j-ratelimiter:2.3.0")
     implementation("io.github.resilience4j:resilience4j-retry:2.3.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.5") // wait for jackson-module-kotlin update
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.20.0")
 }
 
