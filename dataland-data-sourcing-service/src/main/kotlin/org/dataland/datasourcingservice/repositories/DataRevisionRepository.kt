63dc13cd41a10838a35c0624740555f44b6797a4
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-09-29 15:11:56 +0200

    remove unchecked type cast

--- dataland-data-sourcing-service/src/main/kotlin/org/dataland/datasourcingservice/repositories/DataRevisionRepository.kt
+++ dataland-data-sourcing-service/src/main/kotlin/org/dataland/datasourcingservice/repositories/DataRevisionRepository.kt
@@ -20,15 +20,16 @@ class DataRevisionRepository(
     val entityManager: EntityManager,
 ) {
     /**
-     * List all revisions of an entity by its id.
+     * List all revisions of an entity of the provided class type by its id. E.g. providing the id of a data sourcing
+     * entity and the class type DataSourcingEntity::class.java will return all previous versions of that data sourcing entity,
      * @param id the id of the entity
      * @param classType the class type of the entity
      * @return a list of arrays containing the entity and its revision information
      */
-    private fun listDataRevisionsById(
+    private fun <T> listDataRevisionsById(
         id: UUID?,
-        classType: Class<*>,
-    ): List<Any> {
+        classType: Class<T>,
+    ): List<T> {
         val auditReader = AuditReaderFactory.get(entityManager)
 
         val revisions = auditReader.getRevisions(classType, id)
@@ -38,20 +39,16 @@ class DataRevisionRepository(
     }
 
     /**
-     * List all revisions of DataSourcingEntity by its id.
+     * List all previous versions of a DataSourcingEntity identified by its id.
      * @param id the id of the DataSourcingEntity
      * @return a list of DataSourcingEntity containing all revisions
      */
-    @Suppress("UNCHECKED_CAST")
-    fun listDataSourcingRevisionsById(id: UUID?): List<DataSourcingEntity> =
-        listDataRevisionsById(id, DataSourcingEntity::class.java) as List<DataSourcingEntity>
+    fun listDataSourcingRevisionsById(id: UUID?): List<DataSourcingEntity> = listDataRevisionsById(id, DataSourcingEntity::class.java)
 
     /**
-     * List all revisions of RequestEntity by its id.
+     * List all previous versions of a RequestEntity identified by its id.
      * @param id the id of the RequestEntity
      * @return a list of RequestEntity containing all revisions
      */
-    @Suppress("UNCHECKED_CAST")
-    fun listDataRequestRevisionsById(id: UUID?): List<RequestEntity> =
-        listDataRevisionsById(id, RequestEntity::class.java) as List<RequestEntity>
+    fun listDataRequestRevisionsById(id: UUID?): List<RequestEntity> = listDataRevisionsById(id, RequestEntity::class.java)
 }
