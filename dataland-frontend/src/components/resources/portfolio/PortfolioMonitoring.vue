6f2844c052e0ef11955e741afc0688f7c44e59ce
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-10-15 16:36:07 +0200

    regenerate open api docs and adjust frontend

--- dataland-frontend/src/components/resources/portfolio/PortfolioMonitoring.vue
+++ dataland-frontend/src/components/resources/portfolio/PortfolioMonitoring.vue
@@ -10,30 +10,6 @@
       />
     </div>
     <div>
-      <p class="header-styling">Starting Period</p>
-      <Select
-        v-model="selectedStartingYear"
-        :options="reportingPeriodsOptions"
-        option-label="label"
-        option-value="value"
-        data-test="listOfReportingPeriods"
-        placeholder="Select Starting Period"
-        :disabled="!isMonitoringActive"
-        @change="resetErrors"
-        :highlightOnSelect="false"
-        fluid
-      />
-      <Message
-        v-if="showReportingPeriodsError"
-        severity="error"
-        variant="simple"
-        size="small"
-        data-test="reportingPeriodsError"
-      >
-        Please select Starting Period.
-      </Message>
-    </div>
-    <div>
       <p class="header-styling">Frameworks</p>
       <div class="framework-switch-group">
         <div
@@ -80,23 +56,16 @@ import { assertDefined } from '@/utils/TypeScriptUtils.ts';
 import type { EnrichedPortfolio, PortfolioMonitoringPatch } from '@clients/userservice';
 import type Keycloak from 'keycloak-js';
 import PrimeButton from 'primevue/button';
-import Select from 'primevue/select';
 import type { DynamicDialogInstance } from 'primevue/dynamicdialogoptions';
 import { computed, inject, onMounted, type Ref, ref } from 'vue';
 import ToggleSwitch from 'primevue/toggleswitch';
 import Message from 'primevue/message';
-import { PORTFOLIO_MONITORING_REPORTING_PERIODS } from '@/utils/Constants.ts';
 
 type MonitoringOption = {
   value: string;
   label: string;
   isActive: boolean;
 };
-
-const reportingPeriodsOptions = PORTFOLIO_MONITORING_REPORTING_PERIODS.map((yearString) => ({
-  label: yearString,
-  value: +yearString,
-}));
 
 const dialogRef = inject<Ref<DynamicDialogInstance>>('dialogRef');
 const getKeycloakPromise = inject<() => Promise<Keycloak>>('getKeycloakPromise');
@@ -108,12 +77,9 @@ const availableFrameworkMonitoringOptions = ref<MonitoringOption[]>([
   { value: 'sfdr', label: 'SFDR', isActive: false },
   { value: 'eutaxonomy', label: 'EU Taxonomy', isActive: false },
 ]);
-const selectedStartingYear = ref<number | undefined>(undefined);
 const showFrameworksError = ref(false);
-const showReportingPeriodsError = ref(false);
 const portfolio = ref<EnrichedPortfolio>();
 const isMonitoringActive = ref(false);
-const previousStartingYear = ref<number | undefined>(undefined);
 const previousFrameworks = ref<Set<string>>(new Set());
 
 const selectedFrameworkOptions = computed(() => {
@@ -138,18 +104,14 @@ onMounted(() => {
  */
 function onMonitoringToggled(newValue: boolean): void {
   if (newValue) {
-    selectedStartingYear.value = previousStartingYear.value;
     availableFrameworkMonitoringOptions.value = availableFrameworkMonitoringOptions.value.map((option) => ({
       ...option,
       isActive: previousFrameworks.value.has(option.value),
     }));
   } else {
-    previousStartingYear.value = selectedStartingYear.value;
     previousFrameworks.value = new Set(
       availableFrameworkMonitoringOptions.value.filter((option) => option.isActive).map((option) => option.value)
     );
-
-    selectedStartingYear.value = undefined;
     availableFrameworkMonitoringOptions.value = availableFrameworkMonitoringOptions.value.map((option) => ({
       ...option,
       isActive: false,
@@ -162,7 +124,6 @@ function onMonitoringToggled(newValue: boolean): void {
  * Reset errors when either framework, reporting period or file type changes
  */
 function resetErrors(): void {
-  showReportingPeriodsError.value = false;
   showFrameworksError.value = false;
 }
 
@@ -172,15 +133,13 @@ function resetErrors(): void {
 async function patchPortfolioMonitoring(): Promise<void> {
   const portfolioMonitoringPatch: PortfolioMonitoringPatch = {
     isMonitored: isMonitoringActive.value,
-    startingMonitoringPeriod: selectedStartingYear.value?.toString() ?? '',
     monitoredFrameworks: selectedFrameworkOptions.value as unknown as Set<string>,
   };
 
   if (isMonitoringActive.value) {
-    showReportingPeriodsError.value = !selectedStartingYear.value;
     showFrameworksError.value = selectedFrameworkOptions.value.length === 0;
 
-    if (showReportingPeriodsError.value || showFrameworksError.value) {
+    if (showFrameworksError.value) {
       return;
     }
   } else {
@@ -204,16 +163,11 @@ function prefillModal(): void {
   isMonitoringActive.value = portfolio.value.isMonitored ?? false;
 
   if (!isMonitoringActive.value) {
-    selectedStartingYear.value = undefined;
     availableFrameworkMonitoringOptions.value = availableFrameworkMonitoringOptions.value.map((option) => ({
       ...option,
       isActive: false,
     }));
     return;
-  }
-
-  if (portfolio.value.startingMonitoringPeriod) {
-    selectedStartingYear.value = Number(portfolio.value.startingMonitoringPeriod);
   }
 
   const monitoredFrameworksRaw = portfolio.value.monitoredFrameworks as string[] | undefined;
