6b29c71609ab4a37af82f6fe8eb688767d27d764
Stephan Wezorke <186339469+StephanWezorke@users.noreply.github.com>
2025-10-21 16:08:27 +0200

    Add flyway migration tests

---
+++ dataland-backend/src/test/kotlin/db/migration/V10__RenamePcafDatapointsTest.kt
@@ -0,0 +1,119 @@
+package db.migration
+
+import org.dataland.datalandbackend.entities.DataPointMetaInformationEntity
+import org.dataland.datalandbackend.model.companies.CompanyInformation
+import org.dataland.datalandbackend.model.enums.company.IdentifierType
+import org.dataland.datalandbackend.repositories.DataPointMetaInformationRepository
+import org.dataland.datalandbackend.services.CompanyAlterationManager
+import org.dataland.datalandbackend.services.MessageQueuePublications
+import org.dataland.datalandbackendutils.model.QaStatus
+import org.dataland.datalandbackendutils.services.utils.BaseIntegrationTest
+import org.flywaydb.core.Flyway
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import java.util.UUID
+import javax.sql.DataSource
+
+@SpringBootTest(classes = [org.dataland.datalandbackend.DatalandBackend::class])
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Suppress("ClassName")
+class V10__RenamePcafDatapointsTest : BaseIntegrationTest() {
+    companion object {
+        val myPostgres = postgres.start()
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun enableFlywayMigrations(registry: DynamicPropertyRegistry) {
+            registry.add("spring.flyway.enabled") { "true" }
+        }
+    }
+
+    @Autowired
+    lateinit var applicationContext: ApplicationContext
+
+    @Autowired
+    lateinit var companyAlterationManager: CompanyAlterationManager
+
+    @Autowired
+    lateinit var dataPointMetaInformationRepository: DataPointMetaInformationRepository
+
+    @MockitoBean
+    lateinit var ignored: MessageQueuePublications
+
+    private val expectedRenaming =
+        mapOf(
+            "customEnumPcafMainSector" to "extendedEnumPcafMainSector",
+            "customEnumCompanyExchangeStatus" to "extendedEnumCompanyExchangeStatus",
+            "other" to "other",
+        )
+    private lateinit var dummyCompanyId: String
+    private lateinit var metaDataBeforeMigration: Map<String, DataPointMetaInformationEntity>
+
+    @BeforeAll
+    fun setupBeforeMigration() {
+        storeUnmigratedTestData()
+        val flyway =
+            Flyway
+                .configure()
+                .baselineOnMigrate(true)
+                .baselineVersion("9")
+                .dataSource(applicationContext.getBean(DataSource::class.java))
+                .load()
+        flyway.migrate()
+    }
+
+    private fun createDummyMetaData(dataPointType: String) =
+        DataPointMetaInformationEntity(
+            dataPointId = UUID.randomUUID().toString(),
+            companyId = dummyCompanyId,
+            dataPointType = dataPointType,
+            uploaderUserId = "dummy-uploader",
+            uploadTime = System.currentTimeMillis(),
+            reportingPeriod = "2023",
+            currentlyActive = true,
+            qaStatus = QaStatus.Pending,
+        )
+
+    private fun storeUnmigratedTestData() {
+        dummyCompanyId =
+            companyAlterationManager
+                .addCompany(
+                    CompanyInformation(
+                        companyName = "Dummy Company",
+                        headquarters = "Nowhere",
+                        countryCode = "US",
+                        identifiers = mapOf(IdentifierType.Lei to listOf("1234567890ABCDEFGH12)")),
+                    ),
+                ).companyId
+
+        metaDataBeforeMigration =
+            expectedRenaming.keys
+                .map {
+                    dataPointMetaInformationRepository.save(createDummyMetaData(it))
+                }.associateBy { it.dataPointId }
+    }
+
+    @Test
+    fun `Verify migration script renames PCAF data points correctly`() {
+        val migratedMetaInfo =
+            metaDataBeforeMigration.keys.map {
+                dataPointMetaInformationRepository.findById(it).get()
+            }
+
+        migratedMetaInfo.forEach {
+            val expectation =
+                metaDataBeforeMigration[it.dataPointId].let {
+                    it?.copy(dataPointType = expectedRenaming[it?.dataPointType]!!)
+                }
+            Assertions.assertEquals(expectation, it)
+        }
+    }
+}
