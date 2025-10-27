33953cc80beb2d0b50af95d1b374e1b86d9360dc
AlexanderBors <137779015+AlexanderBors@users.noreply.github.com>
2025-10-27 11:41:51 +0100

    Make CompanyRightId embeddable (necessary for using it as an ID class).

--- dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/model/companyRights/CompanyRightId.kt
+++ dataland-community-manager/src/main/kotlin/org/dataland/datalandcommunitymanager/model/companyRights/CompanyRightId.kt
@@ -1,11 +1,19 @@
 package org.dataland.datalandcommunitymanager.model.companyRights
 
+import jakarta.persistence.Embeddable
+import java.io.Serializable
 import java.util.UUID
 
 /**
- * ID class for CompanyRightEntity.
+ * ID class for CompanyRightEntity. Used instead of CompanyRightAssignment<UUID>, since
+ * Hibernate does not support generic types in @IdClass.
  */
+@Embeddable
 data class CompanyRightId(
     val companyId: UUID,
     val companyRight: CompanyRight,
-)
+) : Serializable {
+    companion object {
+        private const val serialVersionUID: Long = 1
+    }
+}
