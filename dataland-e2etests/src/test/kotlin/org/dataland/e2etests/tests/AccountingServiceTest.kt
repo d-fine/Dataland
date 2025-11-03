155e7daaeb30173220e1977c6bf84f7d02fca787
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-11-03 17:00:06 +0100

    add e2e-Tests

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/AccountingServiceTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/AccountingServiceTest.kt
@@ -1,25 +1,33 @@
 package org.dataland.e2etests.tests
 
 import org.dataland.accountingService.openApiClient.model.TransactionPost
+import org.dataland.communitymanager.openApiClient.model.CompanyRole
+import org.dataland.dataSourcingService.openApiClient.model.RequestState
 import org.dataland.dataSourcingService.openApiClient.model.SingleRequest
 import org.dataland.e2etests.auth.GlobalAuth
+import org.dataland.e2etests.auth.GlobalAuth.jwtHelper
 import org.dataland.e2etests.auth.TechnicalUser
 import org.dataland.e2etests.utils.ApiAccessor
-import org.junit.jupiter.api.BeforeEach
+import org.dataland.e2etests.utils.CompanyRolesTestUtils
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import java.math.BigDecimal
+import java.util.UUID
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AccountingServiceTest {
     private val apiAccessor = ApiAccessor()
-    private lateinit var dummyRequest: SingleRequest
 
-    @BeforeEach
-    fun setup() {
-        val companyId =
-            uploadCompanyAsUploader()
-        dummyRequest = SingleRequest(companyId, "sfdr", "2023", "dummy request")
+    private val companyRolesTestUtils = CompanyRolesTestUtils()
+    private val dataReaderUserId = UUID.fromString(TechnicalUser.Reader.technicalUserId)
+
+    private fun createDummyRequest(companyId: String): String {
+        val singleRequest = SingleRequest(companyId, "sfdr", "2023", "dummy request")
+        return apiAccessor.dataSourcingRequestControllerApi
+            .createRequest(
+                singleRequest,
+            ).requestId
     }
 
     private fun uploadCompanyAsUploader(): String =
@@ -31,18 +39,52 @@ class AccountingServiceTest {
     fun `post a transaction, then check the balance`() {
         val companyId =
             uploadCompanyAsUploader()
+        val initialCredit = BigDecimal(10.0)
         GlobalAuth.withTechnicalUser(TechnicalUser.Admin) {
             apiAccessor.accountingServiceCreditsControllerApi.postTransaction(
                 companyId = companyId,
                 transactionPost =
                     TransactionPost(
-                        valueOfChange = BigDecimal(10.0),
+                        valueOfChange = initialCredit,
                         reasonForChange = "Initial credit top-up",
                     ),
             )
             val balance =
                 apiAccessor.accountingServiceCreditsControllerApi.getBalance(companyId)
-            assert(balance == BigDecimal(10.0))
+            assertEquals(initialCredit, balance)
         }
+    }
+
+    @Test
+    fun `post a transaction, then add a request, set it to processing and check the balance`() {
+        jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.Reader)
+        val billableCompanyId =
+            uploadCompanyAsUploader()
+        val initialCredit = BigDecimal(10.0)
+        GlobalAuth.withTechnicalUser(TechnicalUser.Admin) {
+            companyRolesTestUtils.assignCompanyRole(CompanyRole.CompanyOwner, UUID.fromString(billableCompanyId), dataReaderUserId)
+            apiAccessor.accountingServiceCreditsControllerApi.postTransaction(
+                companyId = billableCompanyId,
+                transactionPost =
+                    TransactionPost(
+                        valueOfChange = initialCredit,
+                        reasonForChange = "Initial credit top-up",
+                    ),
+            )
+        }
+
+        val requestedCompanyId =
+            uploadCompanyAsUploader()
+        val requestId = createDummyRequest(requestedCompanyId)
+        GlobalAuth.withTechnicalUser(TechnicalUser.Admin) {
+            apiAccessor.dataSourcingRequestControllerApi.patchRequestState(
+                dataRequestId = requestId,
+                requestState = RequestState.Processing,
+            )
+        }
+
+        val balance =
+            apiAccessor.accountingServiceCreditsControllerApi.getBalance(billableCompanyId)
+        assertEquals(initialCredit - BigDecimal(1.0), balance)
     }
 }
