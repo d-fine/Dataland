71c6793a64de5a67f97684c8b8f434736dda0795
Yunseok <Yunseok.Lee@d-fine.com>
2025-11-04 12:44:58 +0100

    Rename enum value NoNotifications

--- dataland-user-service/src/main/kotlin/org/dataland/datalanduserservice/model/enums/NotificationFrequency.kt
+++ dataland-user-service/src/main/kotlin/org/dataland/datalanduserservice/model/enums/NotificationFrequency.kt
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.media.Schema
     enumAsRef = true,
 )
 enum class NotificationFrequency {
-    NoNotification,
+    NoNotifications,
     Daily,
     Weekly,
     Monthly,
