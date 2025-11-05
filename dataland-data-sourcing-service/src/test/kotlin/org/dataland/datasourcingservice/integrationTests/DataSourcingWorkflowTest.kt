0baa0f8bf6992db275ac228b592aac8b90c090d9
AlexanderBors <137779015+AlexanderBors@users.noreply.github.com>
2025-11-05 15:06:06 +0100

    Fix and refactor DataSourcingWorkflowTest.

--- dataland-data-sourcing-service/src/test/kotlin/org/dataland/datasourcingservice/integrationTests/DataSourcingWorkflowTest.kt
+++ dataland-data-sourcing-service/src/test/kotlin/org/dataland/datasourcingservice/integrationTests/DataSourcingWorkflowTest.kt
@@ -3,19 +3,23 @@ package org.dataland.datasourcingservice.integrationTests
 import org.dataland.datalandbackend.openApiClient.api.CompanyDataControllerApi
 import org.dataland.datalandbackend.openApiClient.model.BasicCompanyInformation
 import org.dataland.datalandbackend.openApiClient.model.CompanyIdentifierValidationResult
+import org.dataland.datalandbackend.openApiClient.model.CompanyInformation
+import org.dataland.datalandbackendutils.model.KeycloakUserInfo
 import org.dataland.datalandbackendutils.services.KeycloakUserService
+import org.dataland.datalandcommunitymanager.openApiClient.api.CompanyRolesControllerApi
+import org.dataland.datalandcommunitymanager.openApiClient.model.CompanyRole
+import org.dataland.datalandcommunitymanager.openApiClient.model.CompanyRoleAssignmentExtended
 import org.dataland.datasourcingservice.DatalandDataSourcingService
 import org.dataland.datasourcingservice.controller.DataSourcingController
 import org.dataland.datasourcingservice.controller.RequestController
-import org.dataland.datasourcingservice.entities.RequestEntity
 import org.dataland.datasourcingservice.model.enums.DataSourcingState
 import org.dataland.datasourcingservice.model.enums.RequestState
 import org.dataland.datasourcingservice.model.request.SingleRequest
-import org.dataland.datasourcingservice.services.RequestQueryManager
-import org.dataland.keycloakAdapter.auth.DatalandAuthentication
+import org.dataland.datasourcingservice.services.DataSourcingServiceMessageSender
 import org.dataland.keycloakAdapter.auth.DatalandRealmRole
 import org.dataland.keycloakAdapter.utils.AuthenticationMock
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertNotNull
 import org.mockito.kotlin.any
@@ -44,38 +48,99 @@ class DataSourcingWorkflowTest
         private lateinit var mockCompanyDataControllerApi: CompanyDataControllerApi
 
         @MockitoBean
-        private lateinit var mockRequestQueryManager: RequestQueryManager
+        private lateinit var mockCompanyRolesControllerApi: CompanyRolesControllerApi
+
+        @MockitoBean
+        private lateinit var companyDataControllerApi: CompanyDataControllerApi
+
+        @MockitoBean
+        private lateinit var mockDataSourcingServiceMessageSender: DataSourcingServiceMessageSender
+
+        private val adminUserId = UUID.randomUUID()
 
         private val mockSecurityContext = mock<SecurityContext>()
-        private lateinit var mockAuthentication: DatalandAuthentication
+        private val mockAuthentication =
+            AuthenticationMock.mockJwtAuthentication(
+                "data-admin",
+                adminUserId.toString(),
+                roles = setOf(DatalandRealmRole.ROLE_ADMIN, DatalandRealmRole.ROLE_UPLOADER),
+            )
 
-        @Test
-        fun `put three requests to processing then close data sourcing object`() {
-            reset(mockKeycloakUserService)
-            mockAuthentication =
-                AuthenticationMock.mockJwtAuthentication(
-                    "data-admin",
-                    "user-id",
-                    roles = setOf(DatalandRealmRole.ROLE_ADMIN, DatalandRealmRole.ROLE_UPLOADER),
-                )
+        private val userIds = List(3) { UUID.randomUUID() }
+
+        private val companyName = "New Company"
+        private val headquarters = "Location"
+        private val countryCode = "DE"
+
+        private val companyInfo =
+            CompanyInformation(
+                companyName = companyName,
+                headquarters = headquarters,
+                countryCode = countryCode,
+                identifiers = emptyMap(),
+            )
+
+        private val companyId = UUID.randomUUID().toString()
+        private val basicCompanyInfo = BasicCompanyInformation(companyId, companyName, headquarters, countryCode)
+        private val validationResult = CompanyIdentifierValidationResult("123LEI", basicCompanyInfo)
+
+        private val framework = "sfdr"
+        private val reportingPeriod = "2026"
+
+        private val singleRequest = SingleRequest(companyId, framework, reportingPeriod, null)
+
+        private val firstName = "Jane"
+        private val lastName = "Doe"
+
+        private fun generateEmail(userId: UUID): String = "$userId@example.com"
+
+        @BeforeEach
+        fun setup() {
+            reset(
+                mockKeycloakUserService,
+                mockCompanyDataControllerApi,
+                mockCompanyRolesControllerApi,
+                mockCompanyDataControllerApi,
+                mockDataSourcingServiceMessageSender,
+            )
+
             doReturn(mockAuthentication).whenever(mockSecurityContext).authentication
             SecurityContextHolder.setContext(mockSecurityContext)
 
-            val companyId = UUID.randomUUID().toString()
-            val companyInfo = BasicCompanyInformation(companyId, "New Company", "Location", "DE")
-            val validationResult = CompanyIdentifierValidationResult("123LEI", companyInfo)
-            whenever(mockCompanyDataControllerApi.postCompanyValidation(any()))
-                .thenReturn(listOf(validationResult))
-            whenever(
-                mockRequestQueryManager.transformRequestEntityToExtendedStoredRequest(any<RequestEntity>()),
-            ).thenAnswer { invocation ->
-                (invocation.arguments[0] as RequestEntity).toExtendedStoredRequest("New Company", null)
+            userIds.forEach {
+                doReturn(
+                    listOf(
+                        CompanyRoleAssignmentExtended(
+                            companyRole = CompanyRole.Member,
+                            companyId = UUID.randomUUID().toString(),
+                            userId = it.toString(),
+                            email = generateEmail(it),
+                            firstName = firstName,
+                            lastName = lastName,
+                        ),
+                    ),
+                ).whenever(mockCompanyRolesControllerApi).getExtendedCompanyRoleAssignments(userId = it)
+
+                doReturn(
+                    KeycloakUserInfo(
+                        userId = it.toString(),
+                        email = generateEmail(it),
+                        firstName = firstName,
+                        lastName = lastName,
+                    ),
+                ).whenever(mockKeycloakUserService).getUser(it.toString())
             }
 
-            val requests = List(3) { SingleRequest(companyId, "sfdr", "2026", null) }
+            doReturn(companyInfo).whenever(companyDataControllerApi).getCompanyInfo(companyId)
+
+            doReturn(listOf(validationResult)).whenever(mockCompanyDataControllerApi).postCompanyValidation(any())
+        }
+
+        @Test
+        fun `put three requests to processing then close data sourcing object`() {
             val requestIds =
-                requests.map {
-                    requestController.createRequest(it, UUID.randomUUID().toString()).body!!.requestId
+                userIds.map {
+                    requestController.createRequest(singleRequest, it.toString()).body!!.requestId
                 }
 
             requestIds.forEach {
@@ -98,7 +163,15 @@ class DataSourcingWorkflowTest
 
             Assertions.assertEquals(3, dataSourcingObject.associatedRequestIds.size)
 
-            val results = dataSourcingController.searchDataSourcings(companyId, "sfdr", "2026", chunkSize = 10, chunkIndex = 0).body!!
+            val results =
+                dataSourcingController
+                    .searchDataSourcings(
+                        companyId,
+                        framework,
+                        reportingPeriod,
+                        chunkSize = 10,
+                        chunkIndex = 0,
+                    ).body!!
             Assertions.assertEquals(1, results.size)
         }
     }
