9e2f8f9358b1c8e6db365450ac8f9d13a0300010
AlexanderBors <137779015+AlexanderBors@users.noreply.github.com>
2025-11-05 11:59:40 +0100

    Fix import order in GlobalAuth.

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/auth/GlobalAuth.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/auth/GlobalAuth.kt
@@ -1,5 +1,6 @@
 package org.dataland.e2etests.auth
 
+import org.dataland.accountingService.openApiClient.infrastructure.ApiClient as ApiClientAccountingService
 import org.dataland.communitymanager.openApiClient.infrastructure.ApiClient as ApiClientCommunityManager
 import org.dataland.dataSourcingService.openApiClient.infrastructure.ApiClient as ApiClientDataSourcingService
 import org.dataland.datalandapikeymanager.openApiClient.infrastructure.ApiClient as ApiClientApiKeyManager
@@ -7,7 +8,6 @@ import org.dataland.datalandbackend.openApiClient.infrastructure.ApiClient as Ap
 import org.dataland.datalandqaservice.openApiClient.infrastructure.ApiClient as ApiClientQaService
 import org.dataland.documentmanager.openApiClient.infrastructure.ApiClient as ApiClientDocumentManager
 import org.dataland.userService.openApiClient.infrastructure.ApiClient as ApiClientUserService
-import org.dataland.accountingService.openApiClient.infrastructure.ApiClient as ApiClientAccountingService
 
 object GlobalAuth {
     val jwtHelper = JwtAuthenticationHelper()
