dd0102e6c040955673e59248f957809c8bdd12f8
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-11-06 09:13:44 +0100

    Add file to cpd exclusions

--- build.gradle.kts
+++ build.gradle.kts
@@ -143,6 +143,8 @@ sonar {
                 "dataland-frontend/src/utils/RequestsOverviewPageUtilsLegacy.ts," +
                 "dataland-frontend/tests/component/components/pages/MyDataRequestsOverviewLegacy.cy.ts," +
                 "dataland-frontend/tests/component/components/pages/AdminAllRequestsOverviewLegacy.cy.ts," +
+                // temporary exclusion to merge DALA-6526
+                "dataland-frontend/src/components/pages/SingleDataRequest.vue," +
                 // backend
                 "dataland-backend/src/main/kotlin/db/migration/V1_1__CreateBackendTables.kt," +
                 "dataland-backend/src/test/kotlin/db/migration/V7__UnifyNfrdMandatoryFieldTest.kt," +
