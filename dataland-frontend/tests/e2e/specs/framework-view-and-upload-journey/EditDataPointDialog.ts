33a10b513fc78565e6dac73dffea4bc4ec29d876
fabian-dfine <fabian.dick@d-fine.com>
2025-11-06 11:53:09 +0100

    DALA-5670: change test location

--- dataland-frontend/tests/e2e/specs/framework-view-and-upload-journey/EditDataPointDialog.ts
+++ dataland-frontend/tests/e2e/specs/framework-view-and-upload-journey/EditDataPointDialog.ts
@@ -18,7 +18,6 @@ describeIf(
         let storedCompany: StoredCompany;
         let SfdrFixtureWithNoNullFields: FixtureData<SfdrData>;
 
-
         before(() => {
             cy.fixture('CompanyInformationWithSfdrPreparedFixtures').then((jsonContent) => {
                 const preparedFixturesSfdr = jsonContent as Array<FixtureData<SfdrData>>;
@@ -57,42 +56,82 @@ describeIf(
                 .find('button[data-test="edit-data-point-icon"]')
                 .click();
             cy.get('div.p-dialog-content').within(() => {
-                cy.get('[data-test="big-decimal-input"]').should('exist').should('be.visible')
+                cy.get('[data-test="big-decimal-input"]')
+                    .should('exist')
+                    .should('be.visible')
                     .find('input')
                     .should('have.value', '17,992.73');
 
-                cy.get('[data-test="quality-select"]').should('exist').should('be.visible')
+                cy.get('[data-test="quality-select"]')
+                    .should('exist')
+                    .should('be.visible')
                     .find('.p-select-label, .p-dropdown-label, .p-inputwrapper, .p-select')
                     .should('contain', 'Estimated');
 
-                cy.get('[data-test="page-number-input"]').should('exist').should('be.visible')
+                cy.get('[data-test="page-number-input"]')
+                    .should('exist')
+                    .should('be.visible')
                     .should('have.value', '1060-1150');
 
-                cy.get('[data-test="comment-textarea"]').should('exist').should('be.visible')
+                cy.get('[data-test="comment-textarea"]')
+                    .should('exist')
+                    .should('be.visible')
                     .should('have.value', 'connect haptic program');
             });
         });
 
-        it.only('should open EditDataPointDialog for a BigDecimalExtendedDataPointFormField modal and change the information', () => {
+        it.only('should open EditDataPointDialog, edit all fields and save changes successfully', () => {
+            const newValue = '1234.56';
+            const newComment = 'Updated via Cypress test';
+            const newPage = '42';
+            const newQuality = 'Reported';
+
             cy.visit(getBaseUrl() + `/companies/${storedCompany.companyId}/frameworks/${dataType}`);
             cy.get('button[data-test=editDatasetButton]').should('exist').click();
+
             cy.contains('span.table-left-label', 'Scope 1 GHG emissions')
                 .closest('td')
                 .next('td')
                 .find('button[data-test="edit-data-point-icon"]')
                 .click();
-            cy.get('div.p-dialog-content').within(() => {
-                cy.get('[data-test="big-decimal-input"]').should('exist').should('be.visible')
-                    .find('input')
-                    .clear()
-                    .type('1000');
 
-               // cy.get('[data-test="quality-select"]').should('exist').should('be.visible').click();
-              //  cy.contains('.p-dropdown-item', 'Reported').should('be.visible').click();
-               // cy.get('[data-test="quality-select"]').should('contain', 'Reported');
-                cy.get('[data-test="save-data-point-button"]').should('exist').should('be.visible').click();
+            cy.get('div.p-dialog-content').should('be.visible').within(() => {
+                cy.get('[data-test="big-decimal-input"] input')
+                    .should('exist')
+                    .should('be.visible')
+                    .should('have.value', '17,992.73');
+
             });
-        }
 
-    );
-});
+            cy.get('[data-test="quality-select"]')
+                .should('exist')
+                .should('be.visible')
+                .find('.p-select-label, .p-dropdown-label')
+                .should('contain', 'Estimated');
+
+            cy.get('[data-test="comment-textarea"]').should('have.value', 'connect haptic program');
+
+
+            cy.get('div.p-dialog-content').within(() => {
+                cy.get('[data-test="big-decimal-input"] input')
+                    .clear()
+                    .type(newValue)
+                    .blur();
+
+
+                cy.intercept('POST', '**/api/data-points?bypassQa=true').as('saveDataPoint');
+                cy.get('[data-test="save-data-point-button"]').should('be.visible').click();
+
+                cy.wait('@saveDataPoint').its('response.statusCode').should('be.oneOf', [200, 201]);
+
+                cy.get('div.p-dialog-content').should('not.exist');
+            });
+
+            cy.contains('span.table-left-label', 'Scope 1 GHG emissions')
+                .closest('td')
+                .next('td')
+                .should('contain', '1,234.56');
+        });
+
+    }
+);
