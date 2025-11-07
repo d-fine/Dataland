a619055cf9f8b4e5dcdcaef639a9a0e6b8093fec
fabian-dfine <fabian.dick@d-fine.com>
2025-11-07 21:37:38 +0100

    DALA-5670: enhance component test

--- dataland-frontend/tests/component/components/resources/dataTable/ExtendedDataPointFormFieldDialog.cy.ts
+++ dataland-frontend/tests/component/components/resources/dataTable/ExtendedDataPointFormFieldDialog.cy.ts
@@ -38,9 +38,40 @@ describe('As a user I want to have displayed the associated documents for the co
         },
       },
     });
+        cy.wait('@fetchDocuments');
+        cy.get('[data-test="page-number-input"]').should('be.disabled');
+        cy.get('[data-test="document-select"] .p-select-label').click();
+        cy.get('.p-select-option-label').should('contain', 'AnnualReport2022.pdf');
+        cy.get('.p-select-option-label').should('contain', 'SustainabilityReport2023.pdf');
+        cy.get('.p-select-option-label').contains('AnnualReport2022.pdf').click();
+        cy.get('[data-test="page-number-input"]').should('not.be.disabled');
+        cy.get('[data-test="page-number-input"]').type('123-456').should('have.value', '123-456');
+        cy.get('[data-test="document-select"] .p-select-label').click();
+  });
+
+  it('updates meta information when different documents are selected', () => {
+    cy.mountWithPlugins(ExtendedDataPointFormFieldDialog, {
+      keycloak: minimalKeycloakMock({}),
+      global: {
+        provide: {
+          companyId: dummyCompanyId,
+          dialogRef: { value: { close: cy.stub() } },
+          getKeycloakPromise: () => Promise.resolve(minimalKeycloakMock({})),
+        },
+      },
+    });
     cy.wait('@fetchDocuments');
     cy.get('[data-test="document-select"] .p-select-label').click();
-    cy.get('.p-select-option-label').should('contain', 'AnnualReport2022.pdf');
-    cy.get('.p-select-option-label').should('contain', 'SustainabilityReport2023.pdf');
+    cy.get('.p-select-option-label').contains('AnnualReport2022.pdf').click();
+    cy.get('.dataland-info-text').should('contain', 'Name: AnnualReport2022.pdf');
+    cy.get('.dataland-info-text').should('contain', 'Category: Financial');
+    cy.get('.dataland-info-text').should('contain', 'Publication Date: 2022-12-31');
+    cy.get('.dataland-info-text').should('contain', 'Reporting Period: 2022');
+    cy.get('[data-test="document-select"] .p-select-label').click();
+    cy.get('.p-select-option-label').contains('SustainabilityReport2023.pdf').click();
+    cy.get('.dataland-info-text').should('contain', 'Name: SustainabilityReport2023.pdf');
+    cy.get('.dataland-info-text').should('contain', 'Category: Sustainability');
+    cy.get('.dataland-info-text').should('contain', 'Publication Date: 2023-06-30');
+    cy.get('.dataland-info-text').should('contain', 'Reporting Period: 2023');
   });
 });
