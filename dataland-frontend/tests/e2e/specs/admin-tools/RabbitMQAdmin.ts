fb4b7c30d279cd72136751f564a14ee8913ae2a1
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-11-11 15:59:32 +0100

    add rabbitMq to docker compose for user service

--- dataland-frontend/tests/e2e/specs/admin-tools/RabbitMQAdmin.ts
+++ dataland-frontend/tests/e2e/specs/admin-tools/RabbitMQAdmin.ts
@@ -21,6 +21,9 @@ const queues = [
   'qa-service.qaDataPoints',
   'qa-service.migrateDatasets',
   'community-manager.queue.nonSourceableData',
+  'data-sourcing-service.datasetQaStatusUpdate',
+  'accounting-service.processMessageForDataReportedAsNonSourceable',
+  'accounting-service.processMessageForAvailableDataAndUpdates',
 ];
 
 /**
