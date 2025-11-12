85a3afd59f86e3f69a192440922e5cc260caae0c
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2025-11-12 16:14:00 +0100

    Potential changes

--- dataland-data-sourcing-service/src/main/kotlin/org/dataland/datasourcingservice/utils/AuthorizationUtils.kt
+++ dataland-data-sourcing-service/src/main/kotlin/org/dataland/datasourcingservice/utils/AuthorizationUtils.kt
@@ -2,11 +2,11 @@ package org.dataland.datasourcingservice.utils
 
 import org.dataland.datalandbackendutils.utils.ValidationUtils
 import org.dataland.datalandcommunitymanager.openApiClient.api.CompanyRolesControllerApi
+import org.dataland.datasourcingservice.model.datasourcing.StoredDataSourcing
 import org.dataland.datasourcingservice.model.enums.DataSourcingState
-import org.dataland.datasourcingservice.repositories.DataSourcingRepository
+import org.dataland.datasourcingservice.services.DataSourcingManager
 import org.dataland.keycloakAdapter.auth.DatalandAuthentication
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 /**
@@ -16,8 +16,8 @@ import org.springframework.stereotype.Component
 class AuthorizationUtils
     @Autowired
     constructor(
-        private val dataSourcingRepository: DataSourcingRepository,
         private val companyRolesControllerApi: CompanyRolesControllerApi,
+        private val dataSourcingManager: DataSourcingManager,
     ) {
         /**
          * Checks whether the logged-in user has a CompanyRole in the specified company.
@@ -42,22 +42,25 @@ class AuthorizationUtils
          * @return true if the user belongs to the document collector company, false otherwise
          */
         fun doesUserBelongToDocumentCollector(dataSourcingId: String): Boolean {
-            val dataSourcing = dataSourcingRepository.findByIdOrNull(ValidationUtils.convertToUUID(dataSourcingId))
+            val dataSourcing = dataSourcingManager.getStoredDataSourcing(ValidationUtils.convertToUUID(dataSourcingId))
 
-            return dataSourcing?.documentCollector?.let { doesUserBelongToCompany(it.toString()) } ?: false
+            return doesUserBelongTo(dataSourcing) { it.documentCollector }
         }
 
         /**
-         * Checks whether the logged-in user belongs to the data extractor company
+         * Generic helper function to check whether the logged-in user belongs to a company
          * associated with the specified data sourcing.
-         * @param dataSourcingId of the data sourcing in question
-         * @return true if the user belongs to the data extractor company, false otherwise
+         * @param dataSourcing in question
+         * @param companySelector function to select the company ID from the data sourcing
+         * @return true if the user belongs to the selected company, false otherwise
          */
-        fun doesUserBelongToDataExtractor(dataSourcingId: String): Boolean {
-            val dataSourcing = dataSourcingRepository.findByIdOrNull(ValidationUtils.convertToUUID(dataSourcingId))
-
-            return dataSourcing?.dataExtractor?.let { doesUserBelongToCompany(it.toString()) } ?: false
-        }
+        private fun doesUserBelongTo(
+            dataSourcing: StoredDataSourcing,
+            companySelector: (StoredDataSourcing) -> String?,
+        ): Boolean =
+            dataSourcing.let { ds ->
+                companySelector(ds)?.let { companyId -> doesUserBelongToCompany(companyId) } ?: false
+            }
 
         /**
          * Checks whether the logged-in user can patch the state of the specified data sourcing
@@ -70,12 +73,13 @@ class AuthorizationUtils
             dataSourcingId: String,
             state: DataSourcingState,
         ): Boolean {
+            val dataSourcing = dataSourcingManager.getStoredDataSourcing(ValidationUtils.convertToUUID(dataSourcingId))
             val result =
                 when (state) {
-                    DataSourcingState.DocumentSourcingDone -> doesUserBelongToDocumentCollector(dataSourcingId)
+                    DataSourcingState.DocumentSourcingDone -> doesUserBelongTo(dataSourcing) { it.documentCollector }
                     DataSourcingState.NonSourceable ->
-                        doesUserBelongToDocumentCollector(dataSourcingId) ||
-                            doesUserBelongToDataExtractor(dataSourcingId)
+                        doesUserBelongTo(dataSourcing) { it.documentCollector } ||
+                            doesUserBelongTo(dataSourcing) { it.dataExtractor }
                     else -> false
                 }
             return result
