7168629a6e7a573500f10141349624b69c78c12f
S-Hofmann1 <192322604+SimonHofmann12@users.noreply.github.com>
2025-11-12 17:15:23 +0100

    Add JsonProperty Annotation

--- dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/messages/SourceabilityMessage.kt
+++ dataland-message-queue-utils/src/main/kotlin/org/dataland/datalandmessagequeueutils/messages/SourceabilityMessage.kt
@@ -1,5 +1,7 @@
 package org.dataland.datalandmessagequeueutils.messages
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 /**
  * Payload of a message concerning the sourceability status of a dataset sent by the backend to the
  * BACKEND_DATA_NONSOURCEABLE exchange.
@@ -8,6 +10,7 @@ data class SourceabilityMessage(
     override val companyId: String,
     override val dataType: String,
     override val reportingPeriod: String,
+    @JsonProperty(value = "isNonSourceable")
     val isNonSourceable: Boolean,
     val reason: String,
 ) : MessageWithTriple
