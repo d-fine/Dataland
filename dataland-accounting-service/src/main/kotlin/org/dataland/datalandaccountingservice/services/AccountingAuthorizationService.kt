ac5183c50adf61a70cc3a760f89e121728aa1610
Wiebke Luenemann <208935942+WiebkeLuenemann-df@users.noreply.github.com>
2025-11-17 14:44:57 +0100

    throw 401 error when a user is not authorized

--- dataland-accounting-service/src/main/kotlin/org/dataland/datalandaccountingservice/services/AccountingAuthorizationService.kt
+++ dataland-accounting-service/src/main/kotlin/org/dataland/datalandaccountingservice/services/AccountingAuthorizationService.kt
@@ -3,6 +3,7 @@ package org.dataland.datalandaccountingservice.services
 import org.dataland.datalandbackendutils.utils.ValidationUtils
 import org.dataland.datalandcommunitymanager.openApiClient.api.CompanyRolesControllerApi
 import org.dataland.keycloakAdapter.auth.DatalandAuthentication
+import org.springframework.security.authentication.InsufficientAuthenticationException
 import org.springframework.stereotype.Service
 
 /**
@@ -15,8 +16,19 @@ class AccountingAuthorizationService(
     /**
      * Check whether the currently authenticated user has any role in the specified company.
      */
+
     fun hasUserSomeRoleInCompany(companyId: String): Boolean {
-        val userId = DatalandAuthentication.fromContext().userId
+        val authentication =
+            try {
+                DatalandAuthentication.fromContext()
+            } catch (ex: IllegalArgumentException) {
+                throw InsufficientAuthenticationException("No authentication found in context", ex)
+            }
+        val userId = authentication.userId
+        if (userId.isBlank()) {
+            throw InsufficientAuthenticationException("userId is blank or missing in authentication.")
+        }
+
         return companyRolesControllerApi
             .getExtendedCompanyRoleAssignments(
                 role = null,
