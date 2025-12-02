0e2b9a381fb274a3bb5dba7d2209357f60806f5c
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2025-12-02 17:03:11 +0100

    fix for accounting service

--- build-utils/rebuild_accounting_service_images.sh
+++ build-utils/rebuild_accounting_service_images.sh
@@ -5,12 +5,12 @@ gradle_dependencies=$(grep gradle_dependencies ./build-utils/common.conf | cut -
 dependencies="./dataland-accounting-service/ ./dataland-backend-utils/ ./dataland-message-queue-utils/ ./dataland-keycloak-adapter/ ./environments/.env.uncritical $gradle_dependencies"
 dependencies+=" ./dataland-community-manager/communityManagerOpenApi.json"
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_accounting_service_base ./dataland-accounting-service/DockerfileBase $dependencies
+./build-utils/base_rebuild_single_docker_image.sh dataland_accounting_service_base ./dataland-accounting-service/DockerfileBase "dataland-accounting-service:assemble" $dependencies
 
 set -o allexport
 source ./*github_env.log
 set +o allexport
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_accounting_service_production ./dataland-accounting-service/Dockerfile $dependencies
+./build-utils/base_rebuild_single_docker_image.sh dataland_accounting_service_production ./dataland-accounting-service/Dockerfile "" $dependencies
 
-./build-utils/base_rebuild_single_docker_image.sh dataland_accounting_service_test ./dataland-accounting-service/DockerfileTest $dependencies
+./build-utils/base_rebuild_single_docker_image.sh dataland_accounting_service_test ./dataland-accounting-service/DockerfileTest "" $dependencies
