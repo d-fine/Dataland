6a432e997a65cb6a0ded79320d158f26a3f94346
fabian-dfine <fabian.dick@d-fine.com>
2025-12-02 10:03:32 +0100

    DALA-6525: update script

--- dataland-internal-storage/src/main/kotlin/db/migration/V31__MigrateFiscalYearEnd.kt
+++ dataland-internal-storage/src/main/kotlin/db/migration/V31__MigrateFiscalYearEnd.kt
@@ -6,25 +6,19 @@ import java.sql.PreparedStatement
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-/**
- * Convert date format of data_point_items where data_point_type = extendedDateFiscalYearEnd
- * from 'YYYY-MM-DD' to 'MMM-dd' (e.g. '2025-03-31' -> 'Mar-31').
- */
 @Suppress("ClassName")
-class V31__ConvertExtendedDateFiscalYearEnd : BaseJavaMigration() {
+class V31__MigrateFiscalYearEnd : BaseJavaMigration() {
 
     private val inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
     private val outputFormat = DateTimeFormatter.ofPattern("MMM-dd")
 
     override fun migrate(context: Context) {
         val connection = context.connection
-
         val selectStatement = connection.prepareStatement(
             """
             SELECT data_point_id, data
             FROM data_point_items
             WHERE data_point_type = 'extendedDateFiscalYearEnd'
-              AND data IS NOT NULL
             """.trimIndent()
         )
 
@@ -42,13 +36,13 @@ class V31__ConvertExtendedDateFiscalYearEnd : BaseJavaMigration() {
             val id = resultSet.getLong("data_point_id")
             val oldDate = resultSet.getString("data")
 
-            val newFormatted = try {
+            val formatted = try {
                 LocalDate.parse(oldDate, inputFormat).format(outputFormat)
-            } catch (ex: Exception) {
+            } catch (_: Exception) {
                 continue
             }
 
-            updateStatement.setString(1, newFormatted)
+            updateStatement.setString(1, formatted)
             updateStatement.setLong(2, id)
             updateStatement.executeUpdate()
         }
