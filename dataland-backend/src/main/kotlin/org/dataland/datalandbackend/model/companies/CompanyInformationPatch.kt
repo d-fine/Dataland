74e97d06380664ca04a6959b1676f34e8cd75c2d
fabian-dfine <fabian.dick@d-fine.com>
2025-12-02 15:50:24 +0100

    DALA-6525: add migration script and change type of column

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/companies/CompanyInformationPatch.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/model/companies/CompanyInformationPatch.kt
@@ -7,7 +7,6 @@ import org.dataland.datalandbackend.model.enums.company.IdentifierType
 import org.dataland.datalandbackend.validator.ValidReportingPeriodShift
 import org.dataland.datalandbackendutils.utils.swaggerdocumentation.BackendOpenApiDescriptionsAndExamples
 import org.dataland.datalandbackendutils.utils.swaggerdocumentation.GeneralOpenApiDescriptionsAndExamples
-import java.time.LocalDate
 
 /**
  * --- API model ---
@@ -72,7 +71,7 @@ data class CompanyInformationPatch(
         description = BackendOpenApiDescriptionsAndExamples.FISCAL_YEAR_END_DESCRIPTION,
         example = BackendOpenApiDescriptionsAndExamples.FISCAL_YEAR_END_EXAMPLE,
     )
-    val fiscalYearEnd: LocalDate? = null,
+    val fiscalYearEnd: String? = null,
     @field:Schema(
         description = BackendOpenApiDescriptionsAndExamples.REPORTING_PERIOD_SHIFT_DESCRIPTION,
         example = BackendOpenApiDescriptionsAndExamples.REPORTING_PERIOD_SHIFT_EXAMPLE,
