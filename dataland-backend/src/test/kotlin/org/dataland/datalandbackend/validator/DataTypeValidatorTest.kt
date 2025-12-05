eb412251657e674c713b8164da596ec9d0104e36
Florian Hoeft <245823423+florian-hoeft@users.noreply.github.com>
2025-12-05 10:04:00 +0100

    Modified code according to code review

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/validator/DataTypeValidatorTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/validator/DataTypeValidatorTest.kt
@@ -3,7 +3,6 @@ package org.dataland.datalandbackend.validator
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
 
 class DataTypeValidatorTest {
     private val dataTypeValidator = DataTypeValidator()
