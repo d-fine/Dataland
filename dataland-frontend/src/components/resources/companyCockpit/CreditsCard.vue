1e01e6036dd6dd625750bcf231ee68ce8e60e9ef
fabian-dfine <fabian.dick@d-fine.com>
2025-12-10 14:00:47 +0100

    Use API Call for credits display

--- dataland-frontend/src/components/resources/companyCockpit/CreditsCard.vue
+++ dataland-frontend/src/components/resources/companyCockpit/CreditsCard.vue
@@ -1,19 +1,22 @@
 <template>
   <Card
-      style="width: 70%; margin: 0 auto; margin-top: var(--spacing-xl); margin-bottom: var(--spacing-xl)"
-      data-test="creditsBalance"
+    style="width: 70%; margin: 0 auto; margin-top: var(--spacing-xl); margin-bottom: var(--spacing-xl)"
+    data-test="creditsBalance"
   >
     <template #title>
-      <div style="display: flex; align-items: center;">
-        <i class="pi pi-credit-card" style="margin-right: .5rem; font-size: var(--font-size-lg); position: relative; top: 2px;"></i>
+      <div style="display: flex; align-items: center">
+        <i
+          class="pi pi-credit-card"
+          style="margin-right: 0.5rem; font-size: var(--font-size-lg); position: relative; top: 2px"
+        ></i>
         <span>Credits</span>
       </div>
     </template>
     <template #content>
-      <Divider/>
-      <div style="display: flex; justify-content: space-between; align-items: center;">
+      <Divider />
+      <div style="display: flex; justify-content: space-between; align-items: center">
         <span>Current Amount of Credits:</span>
-        <Chip label="275"/>
+        <Chip :label="creditsBalance.value" />
       </div>
       <div class="dataland-info-text small">{{ props.companyId }}</div>
     </template>
@@ -23,10 +26,10 @@
 <script setup lang="ts">
 import Card from 'primevue/card';
 import Divider from 'primevue/divider';
-import {defineProps, inject, onMounted, ref} from 'vue';
-import type Keycloak from "keycloak-js";
-import {ApiClientProvider} from "@/services/ApiClients.ts";
-import {assertDefined} from "@/utils/TypeScriptUtils.ts";
+import { defineProps, inject, onMounted, ref } from 'vue';
+import type Keycloak from 'keycloak-js';
+import { ApiClientProvider } from '@/services/ApiClients.ts';
+import { assertDefined } from '@/utils/TypeScriptUtils.ts';
 import Chip from 'primevue/chip';
 
 const creditsBalance = ref<number | any>(0);
@@ -36,7 +39,6 @@ const props = defineProps<{
 
 onMounted(async () => {
   await getCreditsBalanceForCompany();
-  console.log(creditsBalance.value);
 });
 
 const getKeycloakPromise = inject<() => Promise<Keycloak>>('getKeycloakPromise');
@@ -49,14 +51,11 @@ async function getCreditsBalanceForCompany(): Promise<void> {
   if (!props.companyId) return;
   try {
     const response = await apiClientProvider.apiClients.creditsController.getBalance(props.companyId);
-    console.log("CREDIT RESPONSE:", response);
     creditsBalance.value = response.data;
-    console.log("CREDIT AMOUNT:", creditsBalance.value);
   } catch (error) {
-    console.error("Failed to get credit balance:", error);
+    console.error('Failed to get credit balance:', error);
   }
 }
-
 </script>
 
 <style scoped></style>
