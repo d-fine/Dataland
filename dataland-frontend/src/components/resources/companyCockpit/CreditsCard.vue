be601430b53b38ca8a5a97284ffc8cb38e0f018d
fabian-dfine <fabian.dick@d-fine.com>
2025-12-10 12:00:56 +0100

    Adjust Styling

--- dataland-frontend/src/components/resources/companyCockpit/CreditsCard.vue
+++ dataland-frontend/src/components/resources/companyCockpit/CreditsCard.vue
@@ -1,14 +1,21 @@
 <template>
   <Card
-    style="width: 70%; margin: 0 auto; margin-top: var(--spacing-xl); margin-bottom: var(--spacing-xl)"
-    data-test="creditsBalance"
+      style="width: 70%; margin: 0 auto; margin-top: var(--spacing-xl); margin-bottom: var(--spacing-xl)"
+      data-test="creditsBalance"
   >
-    <template #title>Credits</template>
+    <template #title>
+      <div style="display: flex; align-items: center;">
+        <i class="pi pi-credit-card" style="margin-right: .5rem; font-size: var(--font-size-lg); position: relative; top: 2px;"></i>
+        <span>Credits</span>
+      </div>
+    </template>
     <template #content>
-      <Divider />
-      Current Amount of Credits:
+      <Divider/>
+      <div style="display: flex; justify-content: space-between; align-items: center;">
+        <span>Current Amount of Credits:</span>
+        <Chip label="275"/>
+      </div>
       <div class="dataland-info-text small">{{ props.companyId }}</div>
-      {{ creditsBalance }}
     </template>
   </Card>
 </template>
@@ -20,6 +27,7 @@ import {defineProps, inject, onMounted, ref} from 'vue';
 import type Keycloak from "keycloak-js";
 import {ApiClientProvider} from "@/services/ApiClients.ts";
 import {assertDefined} from "@/utils/TypeScriptUtils.ts";
+import Chip from 'primevue/chip';
 
 const creditsBalance = ref<number | any>(0);
 const props = defineProps<{
@@ -40,9 +48,10 @@ const apiClientProvider = new ApiClientProvider(assertDefined(getKeycloakPromise
 async function getCreditsBalanceForCompany(): Promise<void> {
   if (!props.companyId) return;
   try {
-  const response =  await apiClientProvider.apiClients.creditsController.getBalance(props.companyId);
-  creditsBalance.value = response;
-  console.log("CREDIT AMOUNT:",creditsBalance.value);
+    const response = await apiClientProvider.apiClients.creditsController.getBalance(props.companyId);
+    console.log("CREDIT RESPONSE:", response);
+    creditsBalance.value = response.data;
+    console.log("CREDIT AMOUNT:", creditsBalance.value);
   } catch (error) {
     console.error("Failed to get credit balance:", error);
   }
