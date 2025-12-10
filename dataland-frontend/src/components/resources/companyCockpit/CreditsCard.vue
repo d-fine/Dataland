e2034acdea6d3661b5fa2ffdb2ecdf496a3ecefa
fabian-dfine <fabian.dick@d-fine.com>
2025-12-10 10:07:50 +0100

    Add CreditsController Api

--- dataland-frontend/src/components/resources/companyCockpit/CreditsCard.vue
+++ dataland-frontend/src/components/resources/companyCockpit/CreditsCard.vue
@@ -8,6 +8,7 @@
       <Divider />
       Current Amount of Credits:
       <div class="dataland-info-text small">{{ props.companyId }}</div>
+      {{ creditsBalance.value }}
     </template>
   </Card>
 </template>
@@ -15,11 +16,37 @@
 <script setup lang="ts">
 import Card from 'primevue/card';
 import Divider from 'primevue/divider';
-import { defineProps } from 'vue';
+import {defineProps, inject, onMounted, ref} from 'vue';
+import type Keycloak from "keycloak-js";
+import {ApiClientProvider} from "@/services/ApiClients.ts";
+import {assertDefined} from "@/utils/TypeScriptUtils.ts";
 
+const creditsBalance = ref<number | any>(0);
 const props = defineProps<{
   companyId: string;
 }>();
+
+
+onMounted(async () => {
+  await getCreditsBalanceForCompany();
+  console.log(creditsBalance.value);
+});
+
+const getKeycloakPromise = inject<() => Promise<Keycloak>>('getKeycloakPromise');
+const apiClientProvider = new ApiClientProvider(assertDefined(getKeycloakPromise)());
+
+/**
+ * Gets the current balance of credits for the company.
+ */
+async function getCreditsBalanceForCompany(): Promise<void> {
+  if (!props.companyId) return;
+  try {
+  creditsBalance.value = apiClientProvider.apiClients.creditsController.getBalance(props.companyId);
+  } catch (error) {
+    console.error("Failed to get credit balance:", error);
+  }
+}
+
 </script>
 
 <style scoped></style>
--- dataland-frontend/src/services/ApiClients.ts
+++ dataland-frontend/src/services/ApiClients.ts
@@ -1,124 +1,127 @@
-import { ApiKeyControllerApi } from '@clients/apikeymanager';
+import {ApiKeyControllerApi} from '@clients/apikeymanager';
 import * as backendApis from '@clients/backend/api';
-import { type Configuration } from '@clients/backend/configuration';
+import {type Configuration} from '@clients/backend/configuration';
 import {
-  CompanyRolesControllerApi,
-  type CompanyRolesControllerApiInterface,
-  EmailAddressControllerApi,
-  InheritedRolesControllerApi,
-  CompanyRightsControllerApi,
-  RequestControllerApi as CommunityManagerRequestControllerApi,
+    CompanyRolesControllerApi,
+    type CompanyRolesControllerApiInterface,
+    EmailAddressControllerApi,
+    InheritedRolesControllerApi,
+    CompanyRightsControllerApi,
+    RequestControllerApi as CommunityManagerRequestControllerApi,
 } from '@clients/communitymanager';
-import { RequestControllerApi } from '@clients/datasourcingservice';
-import { DocumentControllerApi } from '@clients/documentmanager';
-import { EmailControllerApi } from '@clients/emailservice';
-import { QaControllerApi } from '@clients/qaservice';
-import { PortfolioControllerApi } from '@clients/userservice';
+import {RequestControllerApi} from '@clients/datasourcingservice';
+import {DocumentControllerApi} from '@clients/documentmanager';
+import {EmailControllerApi} from '@clients/emailservice';
+import {QaControllerApi} from '@clients/qaservice';
+import {PortfolioControllerApi} from '@clients/userservice';
 import type Keycloak from 'keycloak-js';
-import axios, { type AxiosInstance } from 'axios';
-import { updateTokenAndItsExpiryTimestampAndStoreBoth } from '@/utils/SessionTimeoutUtils';
-import { DataPointControllerApi } from '@clients/backend/api';
+import axios, {type AxiosInstance} from 'axios';
+import {updateTokenAndItsExpiryTimestampAndStoreBoth} from '@/utils/SessionTimeoutUtils';
+import {DataPointControllerApi} from '@clients/backend/api';
+import {CreditsControllerApi} from "@clients/accountingservice";
 
 interface ApiBackendClients {
-  actuator: backendApis.ActuatorApiInterface;
-  companyDataController: backendApis.CompanyDataControllerApiInterface;
-  metaDataController: backendApis.MetaDataControllerApiInterface;
-  userUploadsController: backendApis.UserUploadsControllerApiInterface;
+    actuator: backendApis.ActuatorApiInterface;
+    companyDataController: backendApis.CompanyDataControllerApiInterface;
+    metaDataController: backendApis.MetaDataControllerApiInterface;
+    userUploadsController: backendApis.UserUploadsControllerApiInterface;
 }
 
 interface ApiClients {
-  apiKeyController: ApiKeyControllerApi;
-  documentController: DocumentControllerApi;
-  requestController: RequestControllerApi;
-  communityManagerRequestController: CommunityManagerRequestControllerApi;
-  companyRolesController: CompanyRolesControllerApiInterface;
-  inheritedRolesController: InheritedRolesControllerApi;
-  qaController: QaControllerApi;
-  emailController: EmailControllerApi;
-  portfolioController: PortfolioControllerApi;
-  emailAddressController: EmailAddressControllerApi;
-  dataPointController: DataPointControllerApi;
-  companyRightsController: CompanyRightsControllerApi;
+    apiKeyController: ApiKeyControllerApi;
+    documentController: DocumentControllerApi;
+    requestController: RequestControllerApi;
+    communityManagerRequestController: CommunityManagerRequestControllerApi;
+    companyRolesController: CompanyRolesControllerApiInterface;
+    inheritedRolesController: InheritedRolesControllerApi;
+    qaController: QaControllerApi;
+    emailController: EmailControllerApi;
+    portfolioController: PortfolioControllerApi;
+    emailAddressController: EmailAddressControllerApi;
+    dataPointController: DataPointControllerApi;
+    companyRightsController: CompanyRightsControllerApi;
+    creditsController: CreditsControllerApi;
 }
 
 type ApiClientConstructor<T> = new (
-  configuration: Configuration | undefined,
-  basePath: string,
-  axios: AxiosInstance
+    configuration: Configuration | undefined,
+    basePath: string,
+    axios: AxiosInstance
 ) => T;
 type ApiClientFactory = <T>(constructor: ApiClientConstructor<T>) => T;
 
 export class ApiClientProvider {
-  private readonly keycloakPromise: Promise<Keycloak>;
-  readonly axiosInstance: AxiosInstance;
+    private readonly keycloakPromise: Promise<Keycloak>;
+    readonly axiosInstance: AxiosInstance;
 
-  readonly backendClients: ApiBackendClients;
-  readonly apiClients: ApiClients;
+    readonly backendClients: ApiBackendClients;
+    readonly apiClients: ApiClients;
 
-  constructor(keycloakPromise: Promise<Keycloak>) {
-    this.keycloakPromise = keycloakPromise;
-    this.axiosInstance = axios.create({});
-    this.registerAutoAuthenticatingAxiosInterceptor();
+    constructor(keycloakPromise: Promise<Keycloak>) {
+        this.keycloakPromise = keycloakPromise;
+        this.axiosInstance = axios.create({});
+        this.registerAutoAuthenticatingAxiosInterceptor();
 
-    this.backendClients = this.constructBackendClients();
-    this.apiClients = this.constructApiClients();
-  }
-
-  private registerAutoAuthenticatingAxiosInterceptor(): void {
-    this.axiosInstance.interceptors.request.use(
-      async (config) => {
-        const bearerToken = await this.getBearerToken();
-        if (bearerToken) {
-          config.headers['Authorization'] = `Bearer ${bearerToken}`;
-        }
-        return config;
-      },
-      (error) => {
-        return Promise.reject(new Error(error.message ?? 'Unknown error occurred'));
-      }
-    );
-  }
-
-  private constructBackendClients(): ApiBackendClients {
-    const backendClientFactory = this.getClientFactory('/api');
-    return {
-      actuator: backendClientFactory(backendApis.ActuatorApi),
-      companyDataController: backendClientFactory(backendApis.CompanyDataControllerApi),
-      metaDataController: backendClientFactory(backendApis.MetaDataControllerApi),
-      userUploadsController: backendClientFactory(backendApis.UserUploadsControllerApi),
-    };
-  }
-
-  private constructApiClients(): ApiClients {
-    return {
-      apiKeyController: this.getClientFactory('/api-keys')(ApiKeyControllerApi),
-      documentController: this.getClientFactory('/documents')(DocumentControllerApi),
-      requestController: this.getClientFactory('/data-sourcing')(RequestControllerApi),
-      communityManagerRequestController: this.getClientFactory('/community')(CommunityManagerRequestControllerApi),
-      companyRolesController: this.getClientFactory('/community')(CompanyRolesControllerApi),
-      inheritedRolesController: this.getClientFactory('/community')(InheritedRolesControllerApi),
-      qaController: this.getClientFactory('/qa')(QaControllerApi),
-      emailController: this.getClientFactory('/email')(EmailControllerApi),
-      portfolioController: this.getClientFactory('/users')(PortfolioControllerApi),
-      emailAddressController: this.getClientFactory('/community')(EmailAddressControllerApi),
-      dataPointController: this.getClientFactory('/api')(DataPointControllerApi),
-      companyRightsController: this.getClientFactory('/community')(CompanyRightsControllerApi),
-    };
-  }
-
-  private async getBearerToken(): Promise<string | undefined> {
-    const keycloak = await this.keycloakPromise;
-    if (keycloak.authenticated) {
-      await updateTokenAndItsExpiryTimestampAndStoreBoth(keycloak);
-      return keycloak.token;
-    } else {
-      return undefined;
+        this.backendClients = this.constructBackendClients();
+        this.apiClients = this.constructApiClients();
     }
-  }
 
-  private getClientFactory(basePath: string): ApiClientFactory {
-    return (constructor) => {
-      return new constructor(undefined, basePath, this.axiosInstance);
-    };
-  }
+    private registerAutoAuthenticatingAxiosInterceptor(): void {
+        this.axiosInstance.interceptors.request.use(
+            async (config) => {
+                const bearerToken = await this.getBearerToken();
+                if (bearerToken) {
+                    config.headers['Authorization'] = `Bearer ${bearerToken}`;
+                }
+                return config;
+            },
+            (error) => {
+                return Promise.reject(new Error(error.message ?? 'Unknown error occurred'));
+            }
+        );
+    }
+
+    private constructBackendClients(): ApiBackendClients {
+        const backendClientFactory = this.getClientFactory('/api');
+        return {
+            actuator: backendClientFactory(backendApis.ActuatorApi),
+            companyDataController: backendClientFactory(backendApis.CompanyDataControllerApi),
+            metaDataController: backendClientFactory(backendApis.MetaDataControllerApi),
+            userUploadsController: backendClientFactory(backendApis.UserUploadsControllerApi),
+        };
+    }
+
+    private constructApiClients(): ApiClients {
+        return {
+        apiKeyController: this.getClientFactory('/api-keys')(ApiKeyControllerApi),
+        documentController: this.getClientFactory('/documents')(DocumentControllerApi),
+        requestController: this.getClientFactory('/data-sourcing')(RequestControllerApi),
+        communityManagerRequestController: this.getClientFactory('/community')(CommunityManagerRequestControllerApi),
+        companyRolesController: this.getClientFactory('/community')(CompanyRolesControllerApi),
+        inheritedRolesController: this.getClientFactory('/community')(InheritedRolesControllerApi),
+        qaController: this.getClientFactory('/qa')(QaControllerApi),
+        emailController: this.getClientFactory('/email')(EmailControllerApi),
+        portfolioController: this.getClientFactory('/users')(PortfolioControllerApi),
+        emailAddressController: this.getClientFactory('/community')(EmailAddressControllerApi),
+        dataPointController: this.getClientFactory('/api')(DataPointControllerApi),
+        companyRightsController: this.getClientFactory('/community')(CompanyRightsControllerApi),
+        creditsController: this.getClientFactory('/accounting')(CreditsControllerApi),
+        };
+    }
+
+    private async getBearerToken(): Promise<string | undefined> {
+        const keycloak = await this.keycloakPromise;
+        if (keycloak.authenticated) {
+            await updateTokenAndItsExpiryTimestampAndStoreBoth(keycloak);
+            return keycloak.token;
+        } else {
+            return undefined;
+        }
+    }
+
+    private getClientFactory(basePath: string): ApiClientFactory {
+        return (constructor) => {
+            return new constructor(undefined, basePath, this.axiosInstance);
+        };
+    }
 }
