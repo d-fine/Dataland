ab7c5e272b4f3637beecd84552671f293a2ee721
fabian-dfine <fabian.dick@d-fine.com>
2025-12-16 10:14:01 +0100

    Fix Test

--- dataland-frontend/tests/component/components/pages/PortfolioOverview.cy.ts
+++ dataland-frontend/tests/component/components/pages/PortfolioOverview.cy.ts
@@ -3,67 +3,67 @@ import PortfolioOverview from '@/components/pages/PortfolioOverview.vue';
 import { type BasePortfolio, type BasePortfolioName, type EnrichedPortfolio } from '@clients/userservice';
 
 describe('Check the portfolio overview view', () => {
-    let basePortfolioNames: BasePortfolioName[];
-    let enrichedPortfolioResponse: EnrichedPortfolio;
-    const inheritedRolesResponse = {};
+  let basePortfolioNames: BasePortfolioName[];
+  let enrichedPortfolioResponse: EnrichedPortfolio;
+  const inheritedRolesResponse = {};
 
-    before(() => {
-        cy.fixture('enrichedPortfolio.json').then((jsonContent) => {
-            const portfolioFixture = jsonContent as BasePortfolio;
-            enrichedPortfolioResponse = jsonContent as EnrichedPortfolio;
-            basePortfolioNames = [
-                {
-                    portfolioId: portfolioFixture.portfolioId,
-                    portfolioName: portfolioFixture.portfolioName,
-                },
-            ];
-        });
+  before(() => {
+    cy.fixture('enrichedPortfolio.json').then((jsonContent) => {
+      const portfolioFixture = jsonContent as BasePortfolio;
+      enrichedPortfolioResponse = jsonContent as EnrichedPortfolio;
+      basePortfolioNames = [
+        {
+          portfolioId: portfolioFixture.portfolioId,
+          portfolioName: portfolioFixture.portfolioName,
+        },
+      ];
+    });
+  });
+
+  it('Should display portfolio information correctly', () => {
+    cy.intercept('GET', '**/users/portfolios/names', basePortfolioNames).as('basePortfolios');
+    cy.intercept('GET', '**/users/portfolios/*/enriched-portfolio', enrichedPortfolioResponse).as('enrichedPortfolio');
+    cy.intercept('GET', '**/community/inherited-roles/**', inheritedRolesResponse).as('inheritedRoles');
+
+    // @ts-ignore
+    cy.mountWithPlugins(PortfolioOverview, {
+      keycloak: minimalKeycloakMock({}),
     });
 
-    it('Should display portfolio information correctly', () => {
-        cy.intercept('GET', '**/users/portfolios/names', basePortfolioNames).as('basePortfolios');
-        cy.intercept('GET', '**/users/portfolios/*/enriched-portfolio', enrichedPortfolioResponse).as('enrichedPortfolio');
-        cy.intercept('GET', '**/community/inherited-roles/**', inheritedRolesResponse).as('inheritedRoles');
+    cy.wait(['@basePortfolios', '@enrichedPortfolio', '@inheritedRoles']);
 
-        // @ts-ignore
-        cy.mountWithPlugins(PortfolioOverview, {
-            keycloak: minimalKeycloakMock({}),
-        });
+    expect(basePortfolioNames).to.have.length.greaterThan(0);
+    cy.get('[data-test="add-portfolio"]').should('contain', 'ADD NEW PORTFOLIO');
+    cy.get('[data-test="portfolios"] [data-p-active="true"]').should('contain', basePortfolioNames[0]!.portfolioName);
+  });
 
-        cy.wait(['@basePortfolios', '@enrichedPortfolio', '@inheritedRoles']);
+  it('Should handle empty portfolio list', () => {
+    cy.intercept('GET', '**/users/portfolios/names', []).as('basePortfolios');
 
-        expect(basePortfolioNames).to.have.length.greaterThan(0);
-        cy.get('[data-test="add-portfolio"]').should('contain', 'ADD NEW PORTFOLIO');
-        cy.get('[data-test="portfolios"] [data-p-active="true"]').should('contain', basePortfolioNames[0]!.portfolioName);
+    // @ts-ignore
+    cy.mountWithPlugins(PortfolioOverview, {
+      keycloak: minimalKeycloakMock({}),
     });
 
-    it('Should handle empty portfolio list', () => {
-        cy.intercept('GET', '**/users/portfolios/names', []).as('basePortfolios');
+    cy.wait('@basePortfolios');
 
-        // @ts-ignore
-        cy.mountWithPlugins(PortfolioOverview, {
-            keycloak: minimalKeycloakMock({}),
-        });
+    cy.get('[data-pc-name="tablist-tab-list"]').should('have.length', 0);
+    cy.get('[data-test="add-portfolio"]').should('contain', 'ADD NEW PORTFOLIO');
+  });
 
-        cy.wait('@basePortfolios');
+  it('Should handle API error gracefully', () => {
+    cy.intercept('GET', '**/users/portfolios/names', {
+      statusCode: 500,
+      body: { message: 'Internal server error' },
+    }).as('basePortfolios');
 
-        cy.get('[data-pc-name="tablist-tab-list"]').should('have.length', 0);
-        cy.get('[data-test="add-portfolio"]').should('contain', 'ADD NEW PORTFOLIO');
+    // @ts-ignore
+    cy.mountWithPlugins(PortfolioOverview, {
+      keycloak: minimalKeycloakMock({}),
     });
 
-    it('Should handle API error gracefully', () => {
-        cy.intercept('GET', '**/users/portfolios/names', {
-            statusCode: 500,
-            body: { message: 'Internal server error' },
-        }).as('basePortfolios');
+    cy.wait('@basePortfolios');
 
-        // @ts-ignore
-        cy.mountWithPlugins(PortfolioOverview, {
-            keycloak: minimalKeycloakMock({}),
-        });
-
-        cy.wait('@basePortfolios');
-
-        cy.get('[data-test="portfolios"] ul').should('have.length', 0);
-    });
+    cy.get('[data-test="portfolios"] ul').should('have.length', 0);
+  });
 });
