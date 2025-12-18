8f793ea4b649827e42fd32660e865f9e0f2cdea3
fabian-dfine <fabian.dick@d-fine.com>
2025-12-18 18:12:56 +0100

    remove unused imports

--- dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/CompanyAlterationManagerTest.kt
+++ dataland-backend/src/test/kotlin/org/dataland/datalandbackend/services/CompanyAlterationManagerTest.kt
@@ -18,8 +18,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @SpringBootTest(
     classes = [DatalandBackend::class],
