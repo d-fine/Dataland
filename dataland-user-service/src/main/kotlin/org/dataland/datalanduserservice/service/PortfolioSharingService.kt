49c420154d1a91be881575741384e6ddf34b2626
Koen Moors <215665213+KoenMoors-dfine@users.noreply.github.com>
2026-01-08 17:37:46 +0100

    Update lastUpdateTimestamp when deleting sharing

--- dataland-user-service/src/main/kotlin/org/dataland/datalanduserservice/service/PortfolioSharingService.kt
+++ dataland-user-service/src/main/kotlin/org/dataland/datalanduserservice/service/PortfolioSharingService.kt
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.Instant
 import java.util.UUID
 
 /**
@@ -101,7 +102,7 @@ class PortfolioSharingService
                 originalPortfolio.toPortfolioEntity(
                     portfolioId = portfolioId.toString(),
                     creationTimestamp = originalPortfolio.creationTimestamp,
-                    lastUpdateTimestamp = originalPortfolio.lastUpdateTimestamp, // should timestamp be updated or not?
+                    lastUpdateTimestamp = Instant.now().toEpochMilli(),
                     sharedUserIds = originalPortfolio.sharedUserIds - userId,
                 )
 
