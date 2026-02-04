05397b68e1e878b5331da38d6ce503a08ac9f4fe
niklasfroehlich-dfine <256560958+niklasfroehlich-dfine@users.noreply.github.com>
2026-02-04 09:39:50 +0100

    Added new columns in admin view of viewDataRequestPage

--- dataland-frontend/src/components/resources/dataRequest/RequestStateHistory.vue
+++ dataland-frontend/src/components/resources/dataRequest/RequestStateHistory.vue
@@ -1,7 +1,7 @@
 <template>
-  <div data-test="stateHistoryContainer" style="pointer-events: none; min-width: 900px">
+  <div data-test="stateHistoryContainer" style="pointer-events: none; min-width: 1100px">
     <DataTable :value="combinedHistory" data-test="stateHistoryTable" scrollable class="p-datatable-sm">
-      <Column field="timestamp" header="Updated On" style="width: 15%">
+      <Column field="timestamp" header="Updated On" :style="isAdmin ? 'width: 20%' : 'width: 25%'">
         <template #body="{ data }">
           <span data-test="lastModifiedDate">
             {{ convertUnixTimeInMsToDateString(data.timestamp) }}
@@ -9,37 +9,37 @@
         </template>
       </Column>
 
-      <Column field="mixedState" header="State" :style="'width: 20%'">
+      <Column
+        field="mixedState"
+        :header="isAdmin ? 'Mixed State' : 'State'"
+        :style="isAdmin ? 'width: 20%' : 'width: 30%'"
+      >
         <template #body="{ data }">
           <DatalandTag
-            :severity="data.state || '-'"
-            :value="getDisplayedStateLabel(data.state)"
+            :severity="getMixedStatus(data.requestState, data.dataSourcingState) || '-'"
+            :value="getDisplayedStateLabel(getMixedStatus(data.requestState, data.dataSourcingState))"
             class="dataland-inline-tag"
           />
         </template>
       </Column>
 
-      <Column v-if="isAdmin" field="requestState" header="Request State" :style="'width: 20%'">
+      <Column v-if="isAdmin" field="requestState" header="Request State" :style="'width: 15%'">
         <template #body="{ data }">
-          <DatalandTag
-            :severity="data.state || '-'"
-            :value="data.type === 'Request' ? data.state : RequestState.Processing"
-            class="dataland-inline-tag"
-          />
+          <DatalandTag :severity="data.requestState || '-'" :value="data.requestState" class="dataland-inline-tag" />
         </template>
       </Column>
 
       <Column v-if="isAdmin" field="dataSourcingState" header="Data Sourcing State" :style="'width: 20%'">
         <template #body="{ data }">
           <DatalandTag
-            :severity="data.state || '-'"
-            :value="data.type === 'Data Sourcing' ? data.state : '-'"
+            :severity="data.dataSourcingState || ''"
+            :value="data.dataSourcingState || ''"
             class="dataland-inline-tag"
           />
         </template>
       </Column>
 
-      <Column field="adminComment" header="Comment" :style="'width: 25%'">
+      <Column field="adminComment" header="Comment" :style="isAdmin ? 'width: 25%' : 'width: 45%'">
         <template #body="{ data }">
           <div style="display: inline-flex" data-test="adminComment">
             {{ data.adminComment || '—' }}
@@ -55,9 +55,34 @@ import { computed } from 'vue';
 import { convertUnixTimeInMsToDateString } from '@/utils/DataFormatUtils';
 import Column from 'primevue/column';
 import DataTable from 'primevue/datatable';
-import { type DataSourcingWithoutReferences, RequestState, type StoredRequest } from '@clients/datasourcingservice';
+import {
+  type DataSourcingWithoutReferences,
+  RequestState,
+  type StoredRequest,
+  type DataSourcingState,
+} from '@clients/datasourcingservice';
 import DatalandTag from '@/components/general/DatalandTag.vue';
 import { getDisplayedStateLabel } from '@/utils/RequestsOverviewPageUtils.ts';
+
+/**
+ * Determines the mixed status based on request state and data sourcing state.
+ * If the request state is 'Withdrawn' or 'Open', or if data sourcing state is null,
+ * it returns the request state. Otherwise, it returns the data sourcing state.
+ *
+ * @param requestState - The current state of the request.
+ * @param dataSourcingState - The current state of data sourcing, which can be null.
+ * @returns The mixed status as either RequestState or DataSourcingState.
+ */
+function getMixedStatus(
+  requestState: RequestState,
+  dataSourcingState: DataSourcingState | null
+): RequestState | DataSourcingState {
+  if (requestState == RequestState.Withdrawn || requestState == RequestState.Open || dataSourcingState == null) {
+    return requestState;
+  } else {
+    return dataSourcingState;
+  }
+}
 
 const props = defineProps<{
   stateHistory: StoredRequest[];
@@ -108,6 +133,6 @@ const combinedHistory = computed<CombinedHistoryEntry[]>(() => {
     }
   }
 
-  return combined;
+  return combined.filter((entry) => !(entry.dataSourcingState === null && entry.requestState === 'Processing'));
 });
 </script>
