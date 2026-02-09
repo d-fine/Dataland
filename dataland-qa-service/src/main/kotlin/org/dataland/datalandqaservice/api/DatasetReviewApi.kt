4183859af4daec84cd29f6fa8a19e99bdce819c7
Yunseok <Yunseok.Lee@d-fine.com>
2026-02-09 09:58:15 +0100

    Add security requirement

--- dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/api/DatasetReviewApi.kt
+++ dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/api/DatasetReviewApi.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.dataland.datalandqaservice.org.dataland.datalandqaservice.model.DatasetReviewResponse
 import org.dataland.datalandqaservice.org.dataland.datalandqaservice.model.DatasetReviewState
 import org.springframework.http.ResponseEntity
@@ -12,10 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 /**
  * Defines the restful dataland dataset review API
  */
+@RequestMapping("/dataset-reviews")
+@SecurityRequirement(name = "default-bearer-auth")
+@SecurityRequirement(name = "default-oauth")
 interface DatasetReviewApi {
     /**
      * A method to retrieve a dataset review object
