4fca7ee117594809a22b09dd7d9b3b5368ff0edb
Yunseok <Yunseok.Lee@d-fine.com>
2026-02-11 16:16:25 +0100

    Put converter into seperate class

--- dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/converters/QaReportDataPointVerdictConverter.kt
+++ dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/converters/QaReportDataPointVerdictConverter.kt
@@ -3,7 +3,6 @@ package org.dataland.datalandqaservice.org.dataland.datalandqaservice.converters
 import jakarta.persistence.AttributeConverter
 import jakarta.persistence.Converter
 import org.dataland.datalandqaservice.model.reports.QaReportDataPointVerdict
-import org.dataland.datalandqaservice.org.dataland.datalandqaservice.model.DatasetReviewState
 
 /**
  * Converts QaReportDataPointVerdict entries in Entities to database values and vice versa
@@ -15,15 +14,4 @@ class QaReportDataPointVerdictConverter : AttributeConverter<QaReportDataPointVe
     override fun convertToEntityAttribute(qaStatusAsString: String): QaReportDataPointVerdict =
         QaReportDataPointVerdict
             .valueOf(qaStatusAsString)
-}
-
-/**
- * Converts DataType entries in Entities to database values and vice versa
- */
-@Converter
-class DatasetReviewStateConverter : AttributeConverter<DatasetReviewState, String> {
-    override fun convertToDatabaseColumn(datasetReviewState: DatasetReviewState?): String = datasetReviewState.toString()
-
-    override fun convertToEntityAttribute(datasetReviewStateAsString: String): DatasetReviewState =
-        DatasetReviewState.valueOf(datasetReviewStateAsString)
 }
