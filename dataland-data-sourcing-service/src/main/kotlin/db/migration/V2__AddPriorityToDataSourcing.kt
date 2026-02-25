dddbc573d08eafec610a20472d298fe2fbce6fb7
Desiree--dfine <223471543+td701@users.noreply.github.com>
2026-02-25 12:38:45 +0100

    Improve Flyway

--- dataland-data-sourcing-service/src/main/kotlin/db/migration/V2__AddPriorityToDataSourcing.kt
+++ dataland-data-sourcing-service/src/main/kotlin/db/migration/V2__AddPriorityToDataSourcing.kt
@@ -4,27 +4,17 @@ import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
 
 /**
- * Creates the data_sourcing table if it does not yet exist and adds the priority column.
+ * Adds the priority column to the data_sourcing table.
  */
 @Suppress("ClassName")
 class V2__AddPriorityToDataSourcing : BaseJavaMigration() {
     override fun migrate(context: Context?) {
-        context!!.connection.createStatement().execute(
-            "CREATE TABLE IF NOT EXISTS data_sourcing (" +
-                "data_sourcing_id uuid NOT NULL, " +
-                "company_id uuid NOT NULL, " +
-                "reporting_period varchar(255) NOT NULL, " +
-                "data_type varchar(255) NOT NULL, " +
-                "state varchar(255) NOT NULL, " +
-                "date_of_next_document_sourcing_attempt date, " +
-                "document_collector uuid, " +
-                "data_extractor uuid, " +
-                "admin_comment varchar(1000), " +
-                "PRIMARY KEY (data_sourcing_id), " +
-                "CONSTRAINT uq_data_sourcing UNIQUE (company_id, reporting_period, data_type)" +
-                ")",
-        )
-        context.connection.createStatement().execute(
+        val connection = context!!.connection
+        val hasTable = connection.metaData.getTables(null, null, "data_sourcing", null).next()
+
+        if (!hasTable) return
+
+        connection.createStatement().execute(
             "ALTER TABLE data_sourcing ADD COLUMN IF NOT EXISTS priority INTEGER NOT NULL DEFAULT 10;",
         )
     }
