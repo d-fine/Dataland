965865b7f4a99589e9a821f4d1a2709330737e0d
Desiree--dfine <223471543+td701@users.noreply.github.com>
2026-03-06 08:17:37 +0100

    Improve CompanyCockpitPage.cy.ts

--- dataland-frontend/tests/component/components/pages/CompanyCockpitPage.cy.ts
+++ dataland-frontend/tests/component/components/pages/CompanyCockpitPage.cy.ts
@@ -99,6 +99,7 @@ describe('Component test for the company cockpit', () => {
     frameworksToTest: Set<DataTypeEnum>,
     isCompanyOwner: boolean = false
   ): void {
+    const frameworksWithoutProvideDataButton = new Set(['lksg', 'eutaxonomy-financials-2026-73']);
     for (const frameworkName of frameworksToTest) {
       const frameworkSummaryPanelSelector = `div[data-test="${frameworkName}-summary-panel"]`;
       const frameworkDataSummary = new Map(Object.entries(mockMapOfDataTypeToAggregatedFrameworkDataSummary)).get(
@@ -122,7 +123,6 @@ describe('Component test for the company cockpit', () => {
         validateVsmeFrameworkSummaryPanel(isCompanyOwner);
         return;
       }
-      const frameworksWithoutProvideDataButton = new Set(['lksg', 'eutaxonomy-financials-2026-73']);
       if (isProvideDataButtonExpected) {
         if (!frameworksWithoutProvideDataButton.has(frameworkName)) {
           cy.get(`${frameworkSummaryPanelSelector} [data-test="${frameworkName}-provide-data-button"]`).should('exist');
