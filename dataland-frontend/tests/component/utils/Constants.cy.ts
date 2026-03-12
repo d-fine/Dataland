f4d75e5afdfee4fa312d0dbca6c098163fb320bc
Desiree--dfine <223471543+td701@users.noreply.github.com>
2026-03-12 09:45:49 +0100

    Regenerate qaServiceOpenApi.json; revert changes in Constants.cy.ts

--- dataland-frontend/tests/component/utils/Constants.cy.ts
+++ dataland-frontend/tests/component/utils/Constants.cy.ts
@@ -5,7 +5,7 @@ import { getAllPrivateFrameworkIdentifiers } from '@/frameworks/BasePrivateFrame
 
 describe('Unit test for the data type sorting in the Constants', () => {
   it('Check for the correct order of the frameworks', () => {
-    expect(ALL_FRAMEWORKS_IN_ENUM_CLASS_ORDER).to.have.length(9);
+    expect(ALL_FRAMEWORKS_IN_ENUM_CLASS_ORDER).to.have.length(8);
     expect(ALL_FRAMEWORKS_IN_ENUM_CLASS_ORDER[0]).to.equal(DataTypeEnum.Sfdr);
     expect(ALL_FRAMEWORKS_IN_ENUM_CLASS_ORDER[1]).to.equal(DataTypeEnum.EutaxonomyFinancials);
     expect(ALL_FRAMEWORKS_IN_ENUM_CLASS_ORDER[2]).to.equal(DataTypeEnum.EutaxonomyFinancials202673);
