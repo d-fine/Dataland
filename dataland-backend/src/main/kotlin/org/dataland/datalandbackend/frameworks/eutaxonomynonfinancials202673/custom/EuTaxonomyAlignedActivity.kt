1f5e7513d8025a93e3630a3825dfbc6a3d846b98
IvanGilardoni <262292830+ivan-gila@users.noreply.github.com>
2026-03-18 13:32:06 +0100

    added custom component

---
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/frameworks/eutaxonomynonfinancials202673/custom/EuTaxonomyAlignedActivity.kt
@@ -0,0 +1,28 @@
+package org.dataland.datalandbackend.frameworks.eutaxonomynonfinancials202673.custom
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.dataland.datalandbackend.frameworks.eutaxonomynonfinancials.custom.RelativeAndAbsoluteFinancialShare
+import org.dataland.datalandbackend.model.enums.commons.YesNo
+import org.dataland.datalandbackend.model.enums.eutaxonomy.nonfinancials.Activity
+import java.math.BigDecimal
+
+/**
+ * --- API model ---
+ * This class represents an activity related to the EU taxonomy framework
+ * with fields regarding the fulfillment of criteria regarding the alignment to EU taxonomy regulation
+ */
+data class EuTaxonomyAlignedActivity(
+    @JsonProperty(required = true)
+    val activityName: Activity,
+    val naceCodes: List<String>?,
+    val share: RelativeAndAbsoluteFinancialShare?,
+    val relativeEligibleShareInPercent: BigDecimal?,
+    val substantialContributionToClimateChangeMitigationInPercent: BigDecimal?,
+    val substantialContributionToClimateChangeAdaptationInPercent: BigDecimal?,
+    val substantialContributionToSustainableUseAndProtectionOfWaterAndMarineResourcesInPercent: BigDecimal?,
+    val substantialContributionToTransitionToACircularEconomyInPercent: BigDecimal?,
+    val substantialContributionToPollutionPreventionAndControlInPercent: BigDecimal?,
+    val substantialContributionToProtectionAndRestorationOfBiodiversityAndEcosystemsInPercent: BigDecimal?,
+    val enablingActivity: YesNo?,
+    val transitionalActivity: YesNo?,
+)
