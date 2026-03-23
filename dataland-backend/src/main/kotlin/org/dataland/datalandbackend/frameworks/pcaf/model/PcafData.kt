4f01f0420a11df5ea1e6bc085702f9f6144ea570
Lucas Kluge <262100399+lucas-kluge@users.noreply.github.com>
2026-03-23 09:24:55 +0100

    fix CI v4

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/frameworks/pcaf/model/PcafData.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/frameworks/pcaf/model/PcafData.kt
@@ -13,7 +13,7 @@ import org.dataland.datalandbackend.frameworks.pcaf.model.general.PcafGeneral
  * The root data-model for the Pcaf Framework
  */
 @Suppress("MagicNumber", "MaxLineLength")
-@DataType("pcaf", 7)
+@DataType("pcaf", 8)
 data class PcafData(
     @field:Valid()
     val general: PcafGeneral? = null,
