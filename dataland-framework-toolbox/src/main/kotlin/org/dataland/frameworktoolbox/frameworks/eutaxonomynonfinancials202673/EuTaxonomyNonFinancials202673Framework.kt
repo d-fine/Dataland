a38a0dc0694677aa7f9f9a0eb131ba067e510bb0
Lucas Kluge <262100399+lucas-kluge@users.noreply.github.com>
2026-03-25 17:52:40 +0100

    introduced parent classes for non financial framework and custom component to reduce duplication

--- dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/eutaxonomynonfinancials202673/EuTaxonomyNonFinancials202673Framework.kt
+++ dataland-framework-toolbox/src/main/kotlin/org/dataland/frameworktoolbox/frameworks/eutaxonomynonfinancials202673/EuTaxonomyNonFinancials202673Framework.kt
@@ -1,14 +1,8 @@
 package org.dataland.frameworktoolbox.frameworks.eutaxonomynonfinancials202673
 
+import org.dataland.frameworktoolbox.frameworks.EuTaxonomyNonFinancialsBaseFramework
+import org.dataland.frameworktoolbox.frameworks.EuTaxonomyNonFinancialsFrameworkConfig
 import org.dataland.frameworktoolbox.frameworks.FrameworkGenerationFeatures
-import org.dataland.frameworktoolbox.frameworks.PavedRoadFramework
-import org.dataland.frameworktoolbox.intermediate.Framework
-import org.dataland.frameworktoolbox.intermediate.components.ReportPreuploadComponent
-import org.dataland.frameworktoolbox.intermediate.components.SingleSelectComponent
-import org.dataland.frameworktoolbox.intermediate.group.ComponentGroup
-import org.dataland.frameworktoolbox.intermediate.group.ComponentGroupApi
-import org.dataland.frameworktoolbox.intermediate.group.edit
-import org.dataland.frameworktoolbox.specific.viewconfig.elements.LabelBadgeColor
 import org.springframework.stereotype.Component
 import java.io.File
 
@@ -17,52 +11,17 @@ import java.io.File
  */
 @Component
 class EuTaxonomyNonFinancials202673Framework :
-    PavedRoadFramework(
-        identifier = "eutaxonomy-non-financials-2026-73",
-        label = "EU Taxonomy Non-Financials (2026/73)",
-        explanation = "The EU-Taxonomy framework for non-financial companies as of Regulation (EU) 2026/73",
-        File("./dataland-framework-toolbox/inputs/eutaxonomy-non-financials-2026-73/eutaxonomy-non-financials-2026-73.xlsx"),
-        order = 5,
+    EuTaxonomyNonFinancialsBaseFramework(
+        EuTaxonomyNonFinancialsFrameworkConfig(
+            identifier = "eutaxonomy-non-financials-2026-73",
+            label = "EU Taxonomy Non-Financials (2026/73)",
+            explanation = "The EU-Taxonomy framework for non-financial companies as of Regulation (EU) 2026/73",
+            frameworkTemplateCsvFile =
+                File(
+                    "./dataland-framework-toolbox/inputs/eutaxonomy-non-financials-2026-73/" +
+                        "eutaxonomy-non-financials-2026-73.xlsx",
+                ),
+            order = 5,
+        ),
         enabledFeatures = FrameworkGenerationFeatures.allExcept(FrameworkGenerationFeatures.UploadPage),
-    ) {
-    private fun configureComponentGroupColorsAndExpansion(root: ComponentGroupApi) {
-        root.edit<ComponentGroup>("general") {
-            viewPageExpandOnPageLoad = true
-            uploadPageLabelBadgeColor = LabelBadgeColor.Orange
-            viewPageLabelBadgeColor = LabelBadgeColor.Orange
-        }
-
-        root.edit<ComponentGroup>("revenue") {
-            uploadPageLabelBadgeColor = LabelBadgeColor.Green
-            viewPageLabelBadgeColor = LabelBadgeColor.Green
-        }
-
-        root.edit<ComponentGroup>("capex") {
-            uploadPageLabelBadgeColor = LabelBadgeColor.Yellow
-            viewPageLabelBadgeColor = LabelBadgeColor.Yellow
-        }
-
-        root.edit<ComponentGroup>("opex") {
-            uploadPageLabelBadgeColor = LabelBadgeColor.Blue
-            viewPageLabelBadgeColor = LabelBadgeColor.Blue
-        }
-    }
-
-    override fun customizeHighLevelIntermediateRepresentation(framework: Framework) {
-        framework.root.edit<ComponentGroup>("general") {
-            edit<ReportPreuploadComponent>("referencedReports") {
-                isPartOfQaReport = false
-            }
-            edit<SingleSelectComponent>("fiscalYearDeviation") {
-                specificationGenerator = { categoryBuilder ->
-                    categoryBuilder.addDefaultDatapointAndSpecification(
-                        this,
-                        "Enum",
-                        "extendedEnumFiscalYearDeviation",
-                    )
-                }
-            }
-        }
-        configureComponentGroupColorsAndExpansion(framework.root)
-    }
-}
+    )
