fa74bd218df4984275b6636fe804f26cb789ad2d
TimTitze <262116330+TimTitze@users.noreply.github.com>
2026-04-02 16:34:32 +0200

    Fixed the docker build again

---
+++ dataland-frontend/src/types/ContentTypes.ts
@@ -0,0 +1,28 @@
+export interface Content {
+  pages: Page[];
+}
+
+export interface Page {
+  title: string;
+  url: string;
+  sections: Section[];
+}
+
+export interface Section {
+  title: string;
+  text: string[];
+  image?: string[];
+  cards?: Card[];
+}
+
+export interface Card {
+  thumbnail?: string;
+  icon?: string;
+  text?: string;
+  title?: string;
+  date?: string;
+  links?: Array<{
+    text: string;
+    url: string;
+  }>;
+}
