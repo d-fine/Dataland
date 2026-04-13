c17757c3c71f5a0b2085630b76ac6f657df36777
IvanGilardoni <262292830+ivan-gila@users.noreply.github.com>
2026-04-13 15:10:44 +0200

    Fixing e2e error

--- dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/repositories/DataPointQaReportRepository.kt
+++ dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/repositories/DataPointQaReportRepository.kt
@@ -78,15 +78,6 @@ interface DataPointQaReportRepository : JpaRepository<DataPointQaReportEntity, S
     ): List<Array<Any>>
 
     /**
-     * Projection interface for data point QA report counts.
-     * Maps query results from countByDataPointIdInGrouped to provide type-safe access to data point IDs and their active QA report counts.
-     */
-    interface DataPointIdCountProjection {
-        val dataPointId: String
-        val activeQaReportCount: Long
-    }
-
-    /**
      * Makes testing easier
      */
     @Query(
