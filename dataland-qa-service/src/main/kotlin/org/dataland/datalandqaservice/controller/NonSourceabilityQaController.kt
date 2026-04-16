64c3097e6617c4d2783c47770b1e072fd0c496cc
Lucas Kluge <262100399+lucas-kluge@users.noreply.github.com>
2026-04-16 16:27:49 +0200

    replacing query params in Non Sourceability QA post request

--- dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/controller/NonSourceabilityQaController.kt
+++ dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/controller/NonSourceabilityQaController.kt
@@ -2,6 +2,7 @@ package org.dataland.datalandqaservice.controller
 
 import org.dataland.datalandbackendutils.model.QaStatus
 import org.dataland.datalandqaservice.api.NonSourceabilityQaApi
+import org.dataland.datalandqaservice.model.NonSourceabilityDecisionRequest
 import org.dataland.datalandqaservice.model.NonSourceableQaReviewInformation
 import org.dataland.datalandqaservice.services.NonSourceabilityQaReviewManager
 import org.dataland.keycloakAdapter.auth.DatalandAuthentication
@@ -41,19 +42,18 @@ class NonSourceabilityQaController(
 
     override fun postNonSourceabilityDecision(
         nonSourceabilityId: String,
-        qaStatus: QaStatus,
-        qaComment: String?,
+        request: NonSourceabilityDecisionRequest,
     ): ResponseEntity<NonSourceableQaReviewInformation> {
         val reviewerUserId = DatalandAuthentication.fromContext().userId
         val correlationId = randomUUID().toString()
         logger.info(
-            "POST /nonSourceable/$nonSourceabilityId qaStatus=$qaStatus (correlationId=$correlationId)",
+            "POST /nonSourceable/$nonSourceabilityId qaStatus=${request.qaStatus} (correlationId=$correlationId)",
         )
         val result =
             nonSourceabilityQaReviewManager.postDecision(
                 nonSourceabilityId = nonSourceabilityId,
-                qaStatus = qaStatus,
-                qaComment = qaComment,
+                qaStatus = request.qaStatus,
+                qaComment = request.qaComment,
                 reviewerUserId = reviewerUserId,
                 correlationId = correlationId,
             )
