e60b210255710c63a5671fd5c46c8a7c2a2f546a
Lucas Kluge <262100399+lucas-kluge@users.noreply.github.com>
2026-04-27 08:14:23 +0200

    renamed deactivateForTriple to deactivateExistingNonSourceabilitiesForTriple

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/NonSourceabilityInformationManager.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/services/NonSourceabilityInformationManager.kt
@@ -249,7 +249,7 @@ class NonSourceabilityInformationManager(
      * meaning data now exists and the non-sourceability is no longer valid.
      */
     @Transactional
-    fun deactivateForTriple(
+    fun deactivateExistingNonSourceabilitiesForTriple(
         companyId: String,
         dataType: DataType,
         reportingPeriod: String,
