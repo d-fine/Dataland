5a5208d8fa6a719e5f45666e8cc6019eaf311403
Florin Boenkost <276548919+Florin-Boenkost@users.noreply.github.com>
2026-05-19 14:27:27 +0200

    Changes in execute_e2e_tests.sh to give a proper error message and doing 4 pulls now.

--- testing/execute_e2e_tests.sh
+++ testing/execute_e2e_tests.sh
@@ -3,14 +3,23 @@ set -euxo pipefail
 source "$(dirname "$0")"/../deployment/docker_utils.sh
 
 #Start E2E Test and wait for E2E Test completion
-# Try to pull images up to 3 times to handle ghcr.io network blips
-for i in 1 2 3; do
+# Try to pull images up to 4 times to handle ghcr.io network blips
+success=false
+for i in 1 2 3 4; do
   echo "Attempt $i to pull Docker images..."
-  docker compose --project-name dala-e2e-test --profile testing pull -q && break || {
+  docker compose --project-name dala-e2e-test --profile testing pull -q && success=true && break || {
     echo "Pull failed. Waiting 15 seconds before retrying..."
-    sleep 15
+    sleep 30
   }
 done
+
+# If all 3 attempts fail, manually trigger the exit code to stop the CI
+if [ "$success" = false ]; then
+  echo "Error: Failed to pull Docker images after 4 attempts."
+  exit 1
+fi
+
+
 docker compose --project-name dala-e2e-test --profile testing up -d || exit
 timeout 3600 sh -c "docker logs dala-e2e-test-e2etests-1 --follow"
 
