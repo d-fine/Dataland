8de8697d854d0fce11e7e18abb46004e901961a8
Oskar Koiner <17322484+okoiner@users.noreply.github.com>
2026-05-27 12:07:44 +0200

    Data Export Test: remove logging as fix seems to work

--- dataland-frontend/cypress.config.ts
+++ dataland-frontend/cypress.config.ts
@@ -194,12 +194,6 @@ export default defineConfig({
         },
       });
       on('task', {
-        log(message: string) {
-          console.log(message);
-          return null;
-        },
-      });
-      on('task', {
         createUniquePdfFixture() {
           const fs = require('fs');
           const path = require('path');
