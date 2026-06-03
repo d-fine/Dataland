c841da222dbb4c8e7d3aafa43fd0505edb480c41
Steven Braun <17625341+braun-steven@users.noreply.github.com>
2026-06-03 07:48:13 +0200

    Update assembled dataset e2e test to use existing calculation rules

--- dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/AssembledDataControllerTest.kt
+++ dataland-e2etests/src/test/kotlin/org/dataland/e2etests/tests/AssembledDataControllerTest.kt
@@ -1,66 +1,104 @@
 package org.dataland.e2etests.tests
 
-import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataSfdrData
-import org.dataland.datalandbackend.openApiClient.model.ExtendedDataPointBigDecimal
-import org.dataland.datalandbackend.openApiClient.model.SfdrData
-import org.dataland.datalandbackend.openApiClient.model.SfdrEnvironmental
-import org.dataland.datalandbackend.openApiClient.model.SfdrEnvironmentalGreenhouseGasEmissions
+import org.dataland.datalandbackend.openApiClient.model.CompanyAssociatedDataEutaxonomyFinancials202673Data
+import org.dataland.datalandbackend.openApiClient.model.CurrencyDataPoint
+import org.dataland.datalandbackend.openApiClient.model.EutaxonomyFinancials202673Data
+import org.dataland.datalandbackend.openApiClient.model.EutaxonomyFinancials202673InsuranceReinsurance
+import org.dataland.datalandbackend.openApiClient.model.EutaxonomyFinancials202673InsurancereinsuranceUnderwritingKpi
+import org.dataland.datalandbackend.openApiClient.model.UploadedDataPoint
 import org.dataland.e2etests.auth.TechnicalUser
 import org.dataland.e2etests.utils.ApiAccessor
 import org.dataland.e2etests.utils.api.ApiAwait
 import org.dataland.e2etests.utils.api.Backend
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpStatus
 import java.math.BigDecimal
 
 class AssembledDataControllerTest {
     private val apiAccessor = ApiAccessor()
+    private val taxonomyEligibleButNonAlignedPremiumsDataPointType =
+        "extendedCurrencyInsuranceReinsuranceTotalOfAbsolutePremiumsOfTaxonomyEligibleButTaxonomyNonAlignedActivities"
+
+    private fun assertBigDecimalEquals(
+        expected: BigDecimal,
+        actual: BigDecimal?,
+    ) {
+        val actualValue = actual ?: fail("Expected BigDecimal value to be present")
+        assertEquals(
+            0,
+            expected.compareTo(actualValue),
+            "Expected $expected but got $actualValue",
+        )
+    }
 
     @Test
-    fun `upload sfdr dataset without calculated field and verify it is computed on retrieval`() {
+    @Suppress("LongMethod")
+    fun `upload eutaxonomy financials dataset without calculated field and verify it is computed on retrieval`() {
         apiAccessor.jwtHelper.authenticateApiCallsWithJwtForTechnicalUser(TechnicalUser.Admin)
         val companyId = apiAccessor.uploadOneCompanyWithRandomIdentifier().actualStoredCompany.companyId
         val reportingPeriod = "2025"
+        val currency = "EUR"
 
-        val scope1 = BigDecimal("0.5")
-        val scope2 = BigDecimal("1.0")
+        val alignedPremiums = BigDecimal("0.5")
+        val eligibleButNonAlignedPremiums = BigDecimal("1.0")
+        val expectedEligiblePremiums = alignedPremiums + eligibleButNonAlignedPremiums
 
-        val sfdrData =
-            SfdrData(
-                environmental =
-                    SfdrEnvironmental(
-                        greenhouseGasEmissions =
-                            SfdrEnvironmentalGreenhouseGasEmissions(
-                                scope1GhgEmissionsInTonnes = ExtendedDataPointBigDecimal(value = scope1),
-                                scope2GhgEmissionsInTonnes = ExtendedDataPointBigDecimal(value = scope2),
-                                scope1And2GhgEmissionsInTonnes = null,
+        val eutaxonomyFinancialsData =
+            EutaxonomyFinancials202673Data(
+                insuranceReinsurance =
+                    EutaxonomyFinancials202673InsuranceReinsurance(
+                        underwritingKpi =
+                            EutaxonomyFinancials202673InsurancereinsuranceUnderwritingKpi(
+                                totalOfAbsolutePremiumsOfTaxonomyAlignedActivities =
+                                    CurrencyDataPoint(
+                                        value = alignedPremiums,
+                                        currency = currency,
+                                    ),
+                                totalOfAbsolutePremiumsOfTaxonomyEligibleActivities = null,
                             ),
                     ),
             )
 
-        Backend.sfdrDataControllerApi
-            .postCompanyAssociatedSfdrData(
-                CompanyAssociatedDataSfdrData(companyId, reportingPeriod, sfdrData),
+        Backend.eutaxonomyFinancials202673DataControllerApi
+            .postCompanyAssociatedEutaxonomyFinancials202673Data(
+                CompanyAssociatedDataEutaxonomyFinancials202673Data(
+                    companyId = companyId,
+                    reportingPeriod = reportingPeriod,
+                    data = eutaxonomyFinancialsData,
+                ),
+                bypassQa = true,
+            )
+
+        Backend.dataPointControllerApi
+            .postDataPoint(
+                UploadedDataPoint(
+                    dataPoint = """{"value": $eligibleButNonAlignedPremiums, "currency": "$currency"}""",
+                    dataPointType = taxonomyEligibleButNonAlignedPremiumsDataPointType,
+                    companyId = companyId,
+                    reportingPeriod = reportingPeriod,
+                ),
                 bypassQa = true,
             )
 
         ApiAwait.untilAsserted(retryOnHttpErrors = setOf(HttpStatus.NOT_FOUND)) {
             val response =
-                Backend.sfdrDataControllerApi
-                    .getCompanyAssociatedSfdrDataByDimensions(reportingPeriod, companyId)
+                Backend.eutaxonomyFinancials202673DataControllerApi
+                    .getCompanyAssociatedEutaxonomyFinancials202673DataByDimensions(reportingPeriod, companyId)
 
             val calculatedField =
                 response.data
-                    .environmental
-                    ?.greenhouseGasEmissions
-                    ?.scope1And2GhgEmissionsInTonnes
+                    .insuranceReinsurance
+                    ?.underwritingKpi
+                    ?.totalOfAbsolutePremiumsOfTaxonomyEligibleActivities
+                    ?: fail("Calculated field totalOfAbsolutePremiumsOfTaxonomyEligibleActivities should be present")
 
-            assertNotNull(calculatedField, "Calculated field scope1And2GhgEmissionsInTonnes should be present")
+            assertBigDecimalEquals(expectedEligiblePremiums, calculatedField.value)
             assertEquals(
-                0, BigDecimal("1.5").compareTo(calculatedField!!.value),
-                "Expected 1.5 as calculated sum but got ${calculatedField.value}",
+                currency,
+                calculatedField.currency,
+                "Expected calculated field currency to be $currency",
             )
         }
     }
