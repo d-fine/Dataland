f6f9e5b38c81164cf77484453fa70e1850f63d0d
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2026-06-05 08:59:21 +0200

    extend ignore list

--- dataland-backend/src/main/kotlin/org/dataland/datalandbackend/utils/DataAvailabilityIgnoredFieldsUtils.kt
+++ dataland-backend/src/main/kotlin/org/dataland/datalandbackend/utils/DataAvailabilityIgnoredFieldsUtils.kt
@@ -16,6 +16,7 @@ object DataAvailabilityIgnoredFieldsUtils {
             "extendedEnumYesNoIloCoreLabourStandards",
             "extendedEnumYesNoOecdGuidelinesForMultinationalEnterprisesCompliancePolicy",
             "extendedEnumYesNoUnGlobalCompactPrinciplesCompliancePolicy",
+            "extendedEnumYesNoIsEuTaxonomyDataReportedOnAMandatoryBasis",
         )
 
     /**
