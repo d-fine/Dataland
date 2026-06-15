ba55c7b8068dc07f5cee24c242eb6c4209690d70
Steven Braun <17625341+braun-steven@users.noreply.github.com>
2026-06-15 15:48:52 +0200

    Include the .env.dev in the docker builds

--- build-utils/base_rebuild_gradle_dockerfile.sh
+++ build-utils/base_rebuild_gradle_dockerfile.sh
@@ -2,6 +2,6 @@
 set -euxo pipefail
 
 gradle_dependencies=$(grep gradle_dependencies ./build-utils/common.conf | cut -d'=' -f2)
-dependencies="./gradle/ ./gradlew $gradle_dependencies"
+dependencies="./gradle/ ./gradlew ./environments/.env.dev $gradle_dependencies"
 
 ./build-utils/base_rebuild_single_docker_image.sh dataland_gradle_base ./base-dockerfiles/DockerfileGradle "" $dependencies
