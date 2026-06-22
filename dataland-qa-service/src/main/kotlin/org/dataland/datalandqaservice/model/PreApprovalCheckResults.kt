4e68ed9d220297475d781c0087f6d21fbb5d769a
Steven Braun <17625341+braun-steven@users.noreply.github.com>
2026-06-22 11:51:05 +0200

    Add pre-approval check results handling and schema integration

---
+++ dataland-qa-service/src/main/kotlin/org/dataland/datalandqaservice/model/PreApprovalCheckResults.kt
@@ -0,0 +1,21 @@
+package org.dataland.datalandqaservice.org.dataland.datalandqaservice.model
+
+/**
+ * Stores the outcome of each automatic pre-approval check for a data point.
+ *
+ * @property areAllQaReportsAccepted `true` if the data point has at least one QA report and all reports were accepted.
+ * @property isDataPointEligible `true` if the data point type is not excluded from automatic pre-approval.
+ * @property passesRandomSampling `true` if the data point was not filtered out by random sampling.
+ * @property passesSignificanceCheck `true` if the change from the live data point is not considered significant.
+ */
+data class PreApprovalCheckResults(
+    val areAllQaReportsAccepted: Boolean,
+    val isDataPointEligible: Boolean,
+    val passesRandomSampling: Boolean,
+    val passesSignificanceCheck: Boolean,
+) {
+    /**
+     * Returns whether all prerequisite checks for automatic pre-approval passed.
+     */
+    fun passes(): Boolean = areAllQaReportsAccepted && passesRandomSampling && isDataPointEligible && passesSignificanceCheck
+}
