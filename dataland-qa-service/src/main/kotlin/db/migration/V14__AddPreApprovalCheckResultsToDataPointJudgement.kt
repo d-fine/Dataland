92b8817e32aacc4517fd2afd914bb8700053d192
Steven Braun <17625341+braun-steven@users.noreply.github.com>
2026-06-23 08:12:12 +0200

    Fix pre-approval db migration for empty dbs

--- dataland-qa-service/src/main/kotlin/db/migration/V14__AddPreApprovalCheckResultsToDataPointJudgement.kt
+++ dataland-qa-service/src/main/kotlin/db/migration/V14__AddPreApprovalCheckResultsToDataPointJudgement.kt
@@ -8,11 +8,23 @@ import org.flywaydb.core.api.migration.Context
  */
 @Suppress("ClassName")
 class V14__AddPreApprovalCheckResultsToDataPointJudgement : BaseJavaMigration() {
+    companion object {
+        const val DATA_POINT_JUDGEMENT_TABLE = "dataset_judgement_entity_data_point_judgement"
+        const val PRE_APPROVAL_CHECK_RESULTS_COLUMN = "pre_approval_check_results"
+    }
+
     override fun migrate(context: Context) {
+        val metaData = context.connection.metaData
+        if (!metaData.getTables(null, null, DATA_POINT_JUDGEMENT_TABLE, null).next() ||
+            metaData.getColumns(null, null, DATA_POINT_JUDGEMENT_TABLE, PRE_APPROVAL_CHECK_RESULTS_COLUMN).next()
+        ) {
+            return
+        }
+
         context.connection.createStatement().execute(
             """
-            ALTER TABLE dataset_judgement_entity_data_point_judgement
-                ADD COLUMN pre_approval_check_results TEXT
+            ALTER TABLE $DATA_POINT_JUDGEMENT_TABLE
+                ADD COLUMN $PRE_APPROVAL_CHECK_RESULTS_COLUMN TEXT
             """.trimIndent(),
         )
     }
