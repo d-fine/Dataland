c4216f764c55c85bb4c567c87cc9f72922149591
Steven Braun <17625341+braun-steven@users.noreply.github.com>
2026-07-01 07:35:50 +0200

    Merge branch 'main' into DALA-7487_remove-eurodat
    # Conflicts:
    #	dataland-backend/build.gradle.kts
    #	dataland-dummy-eurodat-client/DockerfileTest
    #	dataland-dummy-eurodat-client/build.gradle.kts
    #	dataland-external-storage/Dockerfile
    #	dataland-external-storage/DockerfileTest
    #	dataland-external-storage/build.gradle.kts

--- dataland-website/src/content.config.ts
+++ dataland-website/src/content.config.ts
@@ -1,5 +1,4 @@
-import { defineCollection } from 'astro:content';
-import { z } from 'astro/zod';
+import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const successStories = defineCollection({
