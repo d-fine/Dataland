ad9199d785a27093556d92ef4a392bf5377ed36c
Julian Hoelz <286324748+julianhoelzdfine@users.noreply.github.com>
2026-07-14 13:09:45 +0200

    changed CI to enforce cleanup and right environment variable

--- dataland-frontend/tests/e2e/support/index.ts
+++ dataland-frontend/tests/e2e/support/index.ts
@@ -5,14 +5,8 @@ import {
   interceptAllDataPostsAndBypassQaIfPossible,
 } from '@e2e/utils/GeneralApiUtils';
 
-const cookieConsentValue =
-  '{"stamp":"ywkY3Xqqc7BU3ejitK/n5QHNOWOCUdobQFyL+wVWcgRXqNVgJ9XNXQ==","necessary":true,"preferences":true,"statistics":true,method:"explicit","marketing":true,"ver":1,utc:1776958428355,"region":"de"}';
 
 beforeEach(() => {
-  if (Cypress.expose('EXECUTION_ENVIRONMENT') !== 'developmentLocal') {
-    const appDomain = new URL(Cypress.config('baseUrl') as string).hostname;
-    cy.setCookie('CookieConsent', cookieConsentValue, { domain: appDomain });
-  }
   interceptAllAndCheckFor500Errors();
   if (!Cypress.expose('excludeBypassQaIntercept')) {
     interceptAllDataPostsAndBypassQaIfPossible();
