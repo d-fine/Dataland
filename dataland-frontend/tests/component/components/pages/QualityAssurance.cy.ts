a97dfb529883a2fc17d36c1b5e4eea4fd1c8c4ab
Florian Hoeft <245823423+florian-hoeft@users.noreply.github.com>
2026-07-22 14:13:36 +0200

    fixed tests in QualityAssurance.cy.ts after removing confirmation modal interaction when starting a review

--- dataland-frontend/tests/component/components/pages/QualityAssurance.cy.ts
+++ dataland-frontend/tests/component/components/pages/QualityAssurance.cy.ts
@@ -584,7 +584,6 @@ describe('Component tests for the Quality Assurance page', () => {
       });
     }).as('createDatasetReview');
     cy.get('button[data-test="goToReviewButton"]').not(`:contains(${reviewerUserName})`).click();
-    cy.get('[data-test="ok-confirmation-modal-button"]').should('be.visible').click();
     cy.wait('@createDatasetReview');
     cy.get('@routerPush').should('have.been.calledWith', `/qualityassurance/review/${datasetReviewIdAlpha}`);
   });
@@ -615,7 +614,7 @@ describe('Component tests for the Quality Assurance page', () => {
     cy.get('@routerPush').should('have.been.calledWith', `/qualityassurance/review/${datasetReviewIdBeta}`);
   });
 
-  it('Check display of error message.', () => {
+  it.only('Check display of error message.', () => {
     mountQaAssurancePageWithMocks();
     cy.intercept('POST', `**/qa/dataset-judgements/${dataIdAlpha}`, (request) => {
       request.reply(403, {
@@ -631,11 +630,12 @@ describe('Component tests for the Quality Assurance page', () => {
       });
     }).as('createDatasetReviewForbidden');
     cy.get('button[data-test="goToReviewButton"]').not(`:contains(${reviewerUserName})`).click();
-    cy.get('[data-test="ok-confirmation-modal-button"]').should('be.visible').click();
     cy.wait('@createDatasetReviewForbidden');
+    cy.pause();
     cy.get('[data-test="confirmation-modal-error-message"]')
       .should('be.visible')
       .and('contain', 'Access Denied: Access to this resource has been denied.');
+    cy.get('[data-test="ok-confirmation-modal-button"]').should('be.visible').click();
   });
 
   it('Check QA-overview-page for RESET FILTERS button behaviour', () => {
