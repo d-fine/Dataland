6fd1490b3818f29a5a3d8bca402c28bfe6b0ec3e
Lena Vo <299040167+LenattVo@users.noreply.github.com>
2026-08-04 11:39:39 +0200

    exclude ViewConfigs from sonar coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -139,6 +139,7 @@ sonar {
                 "**/tests/**," +
                 "**/LocalCorsConfig.kt," +
                 "dataland-frontend/src/main.ts," +
+                "dataland-frontend/src/frameworks/**/ViewConfig.ts," +
                 "dataland-backend/src/main/kotlin/db/migration/utils/**," +
                 "dataland-internal-storage/src/main/kotlin/db/migration/utils/**," +
                 "dataland-community-manager/src/main/kotlin/db/migration/V16__MigrateCompanyRolesWithConstraintUpdate.kt," +
