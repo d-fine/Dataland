c96fab1c69074e8d6210b9fa145a1e74f6c05a9f
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2026-08-05 15:17:54 +0200

    add first experimental version

---
+++ initialSetup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+set_java_version() {
+  echo PATH=/usr/lib/jvm/temurin-21-jdk-amd64/bin:$PATH >> ~/.bashrc
+  source_bashrc
+}
+
+install_node() {
+  curl -o- https://fnm.vercel.app/install | bash
+  source_bashrc
+  fnm install 24
+  npm install -g npm
+}
+
+update_opencode() {
+   opencode upgrade
+}
+
+source_bashrc() {
+  set +u
+  source ~/.bashrc
+  set -u
+}
+
+set_java_version
+install_node
+update_opencode
