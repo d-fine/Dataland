ff3d72a81511dd885d4d7d509121cb8bbf7ef506
Joshua Köck <49119664+JoshuaKoeck@users.noreply.github.com>
2026-08-11 17:23:03 +0200

    fix sonar on main

--- dataland-frontend/tests/component/components/pages/SearchPagination.cy.ts
+++ dataland-frontend/tests/component/components/pages/SearchPagination.cy.ts
@@ -4,31 +4,23 @@ import { prepareSimpleDataSearchStoredCompanyArray } from '@ct/testUtils/Prepare
 import { type BasicCompanyInformation } from '@clients/backend';
 
 /**
- * Loads mocked data as the intercept response and mounts the component.
- *
+ * Loads mocked data as the intercept response and mounts the component
  * @param mockedResponse inserts a custom dataset as a mocked response
  */
 function mockDataAndMountComponent(mockedResponse?: BasicCompanyInformation[]): void {
   const mockDataSearchStoredCompanyArray = prepareSimpleDataSearchStoredCompanyArray(200);
-  const companyResponse = mockedResponse ?? mockDataSearchStoredCompanyArray;
 
   cy.intercept('GET', '**/api/companies/numberOfCompanies?**', {
     statusCode: 200,
     body: 200,
-  }).as('getNumberOfCompanies');
+  });
 
-  cy.intercept('GET', '**/api/companies?**', {
-    statusCode: 200,
-    body: companyResponse,
-  }).as('getCompanies');
+  cy.intercept('GET', '**/api/companies?**', mockedResponse ?? mockDataSearchStoredCompanyArray).as('companies');
 
   cy.intercept('**/api/companies/meta-information', {
-    statusCode: 200,
-    body: {
-      countryCodes: ['CV'],
-      sectors: ['partnerships'],
-    },
-  }).as('getCompanyMetaInformation');
+    countryCodes: ['CV'],
+    sectors: ['partnerships'],
+  });
 
   const keycloakMock = minimalKeycloakMock({
     roles: ['ROLE_USER', 'ROLE_UPLOADER', 'ROLE_REVIEWER'],
@@ -37,69 +29,56 @@ function mockDataAndMountComponent(mockedResponse?: BasicCompanyInformation[]): 
   cy.mountWithPlugins<typeof SearchCompaniesForFrameworkData>(SearchCompaniesForFrameworkData, {
     keycloak: keycloakMock,
   }).then((mounted) => {
-    return mounted.wrapper.setData({
+    void mounted.wrapper.setData({
       resultArray: mockDataSearchStoredCompanyArray,
     });
-  });
 
-  cy.get('input[id="search-bar-input"]').should('be.visible');
+    cy.wait('@companies');
+  });
 }
 
 /**
- * Enters text into the search bar.
- *
+ * enter text into search bar
  * @param input search string
  */
 function enterSearchString(input: string): void {
-  cy.get('input[id="search-bar-input"]')
-    .should('be.visible')
-    .clear()
-    .type(input)
-    .type('{enter}')
-    .should('have.value', input);
+  cy.get('input[id=search-bar-input]').should('exist').type(input).type('{enter}').should('have.value', input).clear();
 }
 
 /**
- * Checks if the paginator exists.
+ * checks if paginator exists
  */
 function validateExistenceOfPaginator(): void {
   cy.get('table.p-datatable-table').should('exist');
-
-  cy.get('.p-paginator-current').should('contain.text', 'Showing 1 to 100 of').and('contain.text', 'entries');
-
+  cy.get('.p-paginator-current').should('contain.text', 'Showing 1 to 100 of').contains('entries');
   cy.scrollTo('top');
-
-  cy.contains('span', '1-100 of').should('be.visible');
+  cy.contains('span', '1-100 of');
 }
 
 /**
- * Checks if the paginator does not exist.
+ * checks if paginator does not exist
  */
 function validateAbsenceOfPaginator(): void {
   cy.get('div.p-paginator').should('not.exist');
-  cy.contains('span', 'No results').should('be.visible');
+  cy.contains('span', 'No results');
 }
 
 describe('As a user, I expect there to be multiple result pages if there are many results to be displayed', () => {
   it('Do a search with 0 matches, then assure that the paginator is gone and the page text says no results', () => {
     mockDataAndMountComponent([]);
-
     enterSearchString('ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678987654321');
     validateAbsenceOfPaginator();
   });
 
   it("Search for all companies containing 'abs' and verify that results are paginated, only first 100 are shown", () => {
     mockDataAndMountComponent();
-
     enterSearchString('abs');
     validateExistenceOfPaginator();
   });
 
-  it('Search for all companies, go to page 2 of the search results, then run another query and verify that the paginator and page text are reset', () => {
+  it('Search for all companies, go to page 2 of the search results, then run a another query and verify that paginator and the page text are reset', () => {
     mockDataAndMountComponent();
-
-    cy.get('.p-paginator-pages').find('button.p-paginator-page').eq(0).should('contain.text', '2').click();
-
+    cy.get('.p-paginator-pages').find('button[class="p-paginator-page"]').eq(0).should('contain.text', '2').click();
     enterSearchString('abs');
     validateExistenceOfPaginator();
   });
