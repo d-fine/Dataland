995311a89bb94669a2ce72152b27572b4ca20590
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2026-08-12 14:24:22 +0200

    update setup

--- initialSetup.sh
+++ initialSetup.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mode=${1:default}
+project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 set_java_version() {
+  echo "Setting Java version"
   echo PATH=/usr/lib/jvm/temurin-21-jdk-amd64/bin:$PATH >> ~/.bashrc
   source_bashrc
 }
 
 set_automatic_sourcing() {
-  project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  echo "Activate automatic sourcing of the .env.dev file"
   echo "set -a" >> ~/.bashrc
   echo "source $project_root/environments/.env.dev"  >> ~/.bashrc
   echo "set +a"  >> ~/.bashrc
 }
 
 install_node() {
+  echo "Install node and npm"
   curl -o- https://fnm.vercel.app/install | bash
   source_bashrc
   fnm install 24
@@ -23,6 +25,7 @@ install_node() {
 }
 
 update_opencode() {
+  echo "Update OpenCode"
    opencode upgrade
 }
 
@@ -33,6 +36,7 @@ source_bashrc() {
 }
 
 configure_git() {
+  echo "Configure git"
   git config --global credential.helper '!f() { if [ "$1" = get ]; then echo username=placeholder-github-user; echo password=placeholder-github-token; fi; }; f'
   read username
   git config user.name "$username"
@@ -40,11 +44,15 @@ configure_git() {
   git config user.email "$email"
 }
 
+start_stack() {
+  echo "Start stack using full reset and self-signed certificates"
+  cd "$project_root"
+  ./manageLocalStack.sh --reset --self-signed-certs
+}
+
 set_java_version
 install_node
 update_opencode
 set_automatic_sourcing
-
-if [[ $mode == "developer" ]]; then
-  configure_git
-fi
+configure_git
+start_stack
