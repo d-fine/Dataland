04e57e7bd3d05e99dce035fd6cb25d1f74d0dbc7
SiegfriedSobkowiak <71646830+SiegfriedSobkowiak@users.noreply.github.com>
2026-09-15 10:09:18 +0200

    update ssl retrieval

--- developer-tools/fetchSslCertsForSandbox.sh
+++ developer-tools/fetchSslCertsForSandbox.sh
@@ -10,12 +10,19 @@ if ! command -v sbx >/dev/null 2>&1; then
 fi
 
 cert_dir="./local/certs"
+sandbox_dir="$repo_path_in_sandbox/local"
 mkdir -p "$cert_dir"
 
 echo "Fetching SSL certificate files from letsencrypt.dataland.com via scp..."
 scp ubuntu@letsencrypt.dataland.com:/etc/letsencrypt/live/local-dev.dataland.com/* "$cert_dir"
 
-echo "Copying certificate files into sandbox '$sandbox_name' at $repo_path_in_sandbox/local/certs..."
-sbx cp "$cert_dir" "$sandbox_name:$repo_path_in_sandbox/local"
+echo "Copying certificate files into sandbox '$sandbox_name' at $sandbox_dir..."
+sbx exec -u agent $sandbox_name mkdir -p $cert_dir
+sbx cp "$cert_dir" "$sandbox_name:$sandbox_dir"
+
+for file in $cert_dir/*; do
+  echo "Changing ownership for $(basename $file)"
+  sbx exec $sandbox_name sudo chown agent:agent $cert_dir/$(basename $file)
+done
 
 echo "Done. SSL certificates are now available inside the sandbox at $repo_path_in_sandbox/local/certs."
