3466c201faa69d81a8e22dfdbbdbf495fb56d7b1
patrSch1154 <318575348+patrSch1154@users.noreply.github.com>
2026-09-22 12:06:00 +0000

    added flyway migration script

---
+++ dataland-community-manager/src/main/kotlin/db/migration/V18__RemoveLegacyRequestTables.kt
@@ -0,0 +1,21 @@
+package db.migration
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+/**
+ * Removes the database tables that backed the legacy "data request" feature, which has been replaced by
+ * the request functionality provided by the data-sourcing service. Also purges any notification events
+ * whose type is no longer supported.
+ */
+@Suppress("ClassName")
+class V18__RemoveLegacyRequestTables : BaseJavaMigration() {
+    override fun migrate(context: Context?) {
+        context!!.connection.createStatement().execute(
+            "DELETE FROM notification_events WHERE notification_event_type <> 'InvestorRelationsEvent'",
+        )
+        context.connection.createStatement().execute("DROP TABLE IF EXISTS messages")
+        context.connection.createStatement().execute("DROP TABLE IF EXISTS request_status_history")
+        context.connection.createStatement().execute("DROP TABLE IF EXISTS data_requests")
+    }
+}
