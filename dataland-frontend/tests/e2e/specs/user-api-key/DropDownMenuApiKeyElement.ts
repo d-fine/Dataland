446fce08f3f837ae9882aa1db7be8a42d5363dc2
Investigator <investigator@example.com>
2026-09-25 17:09:33 +0200

    fix test

--- dataland-frontend/tests/e2e/specs/user-api-key/DropDownMenuApiKeyElement.ts
+++ dataland-frontend/tests/e2e/specs/user-api-key/DropDownMenuApiKeyElement.ts
@@ -4,11 +4,11 @@ describe('As a user I expect api key link will be visible in the menu', () => {
 
     cy.visitAndCheckAppMount('/requests');
 
-    cy.get('div.user-menu-container').click();
+    cy.get('button.user-menu-container').click();
     cy.get('[data-test="profileMenu"]').should('be.visible');
-    cy.get('div.user-menu-container').click();
+    cy.get('button.user-menu-container').click();
     cy.get('[data-test="profileMenu"]').should('not.exist');
-    cy.get('div.user-menu-container').click();
+    cy.get('button.user-menu-container').click();
     cy.get('.p-menu-item-label').contains('API KEY').click();
     cy.url().should('include', '/api-key');
   });
